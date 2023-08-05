76cefb466a91bb07e82f5b92fdefbd405c816437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:40 +0000

    Add package infi.instruct/0.6.32/infi.instruct-0.6.32-py2-none-any.whl

---
+++ packages/infi.instruct/infi.instruct-0.6.32-py2-none-any.whl/infi.instruct-0.6.32.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+declarative structure marshalling
+
+
---
+++ packages/infi.instruct/infi.instruct-0.6.32-py2-none-any.whl/infi/instruct/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.32"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.6.32)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '98ed45406175e5e80ef6bcdd16ff9a5bf234cbc8'
+__git_head_subject__ = u'Finished Release v0.6.32'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-04-22 12:23:17'
