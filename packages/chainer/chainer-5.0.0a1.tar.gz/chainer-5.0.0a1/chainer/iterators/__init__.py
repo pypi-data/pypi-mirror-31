53b1dc138c94c148790916df60d52a9bb57a72a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package chainer/5.0.0a1/chainer-5.0.0a1.tar.gz

---
+++ packages/chainer/chainer-5.0.0a1.tar.gz/chainer-5.0.0a1/chainer/iterators/__init__.py
@@ -0,0 +1,4 @@
+# import classes and functions
+from chainer.iterators.multiprocess_iterator import MultiprocessIterator  # NOQA
+from chainer.iterators.multithread_iterator import MultithreadIterator  # NOQA
+from chainer.iterators.serial_iterator import SerialIterator  # NOQA
