ea98934ebd2b4beb6c9331acfd37bb9fca9aaf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package lynkz/1.1/lynkz-1.1.tar.gz

---
+++ packages/lynkz/lynkz-1.1.tar.gz/lynkz-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(
+    name='lynkz',
+    version='1.1',
+    description='Unofficial Python wrapper for the lynkz.me API',
+    long_description=long_description,
+    url='https://github.com/Ewpratten/lynkz',
+    author='Evan Pratten',
+    author_email='ewpratten@gmail.com',
+    license='MIT',
+    packages=['lynkz'],
+    install_requires=['requests', 'lxml'],
+    zip_safe=False)
