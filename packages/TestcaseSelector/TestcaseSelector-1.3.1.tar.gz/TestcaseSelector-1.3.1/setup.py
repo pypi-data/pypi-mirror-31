467d1ebe19d5bfe3c87ae4769bf39acfb52137d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package TestcaseSelector/1.3.1/TestcaseSelector-1.3.1.tar.gz

---
+++ packages/TestcaseSelector/TestcaseSelector-1.3.1.tar.gz/TestcaseSelector-1.3.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from pkgutil import walk_packages
+import TestcaseSelect
+
+def find_packages(path='.', prefix=""):
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
+setup(
+  name = 'TestcaseSelector',
+  packages = list(find_packages(TestcaseSelect.__path__, TestcaseSelect.__name__)),
+  version = '1.3.1',
+  description = 'Unittest test selector window',
+  author = 'Charles Whaples',
+  author_email = 'whaplescr@gmail.com',
+  url = 'https://github.com/Whaplescr/TestcaseSelector',
+  download_url = 'https://github.com/Whaplescr/TestcaseSelector/tarball/1.1.0',
+  keywords = ['appium', 'selenium', 'testing','unittest','tkinter','ttk'],
+  classifiers=[],
+  install_requires=[]
+)
