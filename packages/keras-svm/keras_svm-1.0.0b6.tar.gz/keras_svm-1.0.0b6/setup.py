2e661f83e88a62d0dee0dffb4c239a9aea080732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package keras-svm/1.0.0b6/keras_svm-1.0.0b6.tar.gz

---
+++ packages/keras-svm/keras_svm-1.0.0b6.tar.gz/keras_svm-1.0.0b6/setup.py
@@ -0,0 +1,44 @@
+"""
+A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+
+TO PUSH:
+rm -r dist/*
+tox
+python setup.py bdist_wheel --universal
+python setup.py sdist
+twine upload dist/*
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='keras_svm',
+    version='1.0.0b6',
+    description='A model to use keras models with Support Vector Machines',
+    url='https://github.com/Luttik/keras_svm/tree/master',  # Optional
+    author='Daan Luttik',  # Optional
+    author_email='d.t.luttik@gmail.com',  # Optional
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='keras sklearn svm ml',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    install_requires=['keras', 'scikit-learn'],  # Optional
+)
