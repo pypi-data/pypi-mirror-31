4c9bf92f7313c88bc397098bb6105c7680753e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:42 +0000

    Add package procrunner/0.4.0/procrunner-0.4.0.tar.gz

---
+++ packages/procrunner/procrunner-0.4.0.tar.gz/procrunner-0.4.0/HISTORY.rst
@@ -0,0 +1,24 @@
+=======
+History
+=======
+
+0.4.0 (2018-04-23)
+------------------
+
+* Python 2.7 support on Windows. Python3 not yet supported on Windows.
+
+0.3.0
+-----
+
+* run_process() renamed to run()
+* Python3 compatibility fixes
+
+0.2.0 (2018-03-12)
+------------------
+
+* Procrunner is now Python3 3.3-3.6 compatible.
+
+0.1.0 (2018-03-12)
+------------------
+
+* First release on PyPI.
---
+++ packages/procrunner/procrunner-0.4.0.tar.gz/procrunner-0.4.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['six']
+
+setup_requirements = ['pytest-runner', 'six']
+
+test_requirements = ['mock', 'pytest']
+
+setup(
+    author="Markus Gerstel",
+    author_email='scientificsoftware@diamond.ac.uk',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    description="Versatile utility function to run external processes",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='procrunner',
+    name='procrunner',
+    packages=find_packages(include=['procrunner']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/DiamondLightSource/python-procrunner',
+    version='0.4.0',
+    zip_safe=False,
+)
