d393d678d3c820e7ff3ca32ec96d211a9fa227a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:26 +0000

    Add package mozapkpublisher/0.6.0/mozapkpublisher-0.6.0.tar.gz

---
+++ packages/mozapkpublisher/mozapkpublisher-0.6.0.tar.gz/mozapkpublisher-0.6.0/mozapkpublisher/common/utils.py
@@ -0,0 +1,31 @@
+import requests
+import hashlib
+
+
+def load_json_url(url):
+    return requests.get(url).json()
+
+
+def download_file(url, local_file_path):
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+
+    with open(local_file_path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:   # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def file_sha512sum(file_path):
+    bs = 65536
+    hasher = hashlib.sha512()
+    with open(file_path, 'rb') as fh:
+        buf = fh.read(bs)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = fh.read(bs)
+    return hasher.hexdigest()
+
+
+def filter_out_identical_values(list_):
+    return list(set(list_))
