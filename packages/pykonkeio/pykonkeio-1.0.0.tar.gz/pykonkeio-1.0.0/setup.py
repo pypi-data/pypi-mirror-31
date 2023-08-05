c000639ced385c076b353e8c05d621da48987606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:53 +0000

    Add package pykonkeio/1.0.0/pykonkeio-1.0.0.tar.gz

---
+++ packages/pykonkeio/pykonkeio-1.0.0.tar.gz/pykonkeio-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pykonkeio',
+    version='1.0.0',
+    keywords=('konke', 'iot'),
+    description='Python library for interfacing with konke smart appliances',
+    long_description='',#open('README.md', 'rt').read(),
+    author='jedmeng',
+    author_email='jedm@jedm.cn',
+    url='https://github.com/jedmeng/python-konekeio',
+    license='MIT',
+    install_requires=[
+        'pycrypto'
+    ],
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.5',
+    include_package_data=True,
+    # entry_points={
+    #     'console_scripts': [
+    #         'konekeio=konekeio.__main__:main',
+    #     ]
+    # },
+)
