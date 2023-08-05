b76801898cb83f7f03af1b5c606ef128167fc21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:57 +0000

    Add package cauldron-notebook/0.3.0/cauldron_notebook-0.3.0-py2.py3-none-any.whl

---
+++ packages/cauldron-notebook/cauldron_notebook-0.3.0-py2.py3-none-any.whl/cauldron/steptest/__init__.py
@@ -0,0 +1,258 @@
+import os
+import tempfile
+import unittest
+import typing
+import inspect
+import time
+
+import cauldron as cd
+from cauldron import environ
+from cauldron import runner
+from cauldron.session import projects
+from cauldron.session import exposed
+from cauldron.cli import commander
+from cauldron.session.caching import SharedCache
+
+
+def find_project_directory(subdirectory: str) -> typing.Union[str, None]:
+    """
+    Finds the root project directory from a subdirectory within the project
+    folder.
+
+    :param subdirectory:
+        The subdirectory to use to search for the project directory. The
+        subdirectory may also be the project directory.
+    :return:
+        The project directory that contains the specified subdirectory or None
+        if no project directory was found.
+    """
+
+    if os.path.exists(os.path.join(subdirectory, 'cauldron.json')):
+        return subdirectory
+
+    parent = os.path.dirname(subdirectory)
+    if parent == subdirectory:
+        return None
+
+    if os.path.exists(os.path.join(parent, 'cauldron.json')):
+        return parent
+    return find_project_directory(parent)
+
+
+class StepTestRunResult:
+    """
+    This class contains information returned from running a step during testing.
+    """
+
+    def __init__(
+            self,
+            step: 'projects.ProjectStep',
+            response: 'environ.Response'
+    ):
+        self._step = step  # type: projects.ProjectStep
+        self._response = response  # type: environ.Response
+        self._locals = SharedCache().put(**step.test_locals)
+
+    @property
+    def local(self) -> SharedCache:
+        """
+        Container object that holds all of the local variables that were
+        defined within the run step
+        """
+
+        return self._locals
+
+    @property
+    def success(self) -> bool:
+        """
+        Whether or not the step was successfully run. This value will be
+        False if there as an uncaught exception during the execution of the
+        step.
+        """
+
+        return not self._response.failed
+
+    def echo_error(self) -> str:
+        """
+        Creates a string representation of the exception that cause the step
+        to fail if an exception occurred. Otherwise, an empty string is returned.
+
+        :return:
+            The string representation of the exception that caused the running
+            step to fail or a blank string if no exception occurred
+        """
+
+        if not self._response.errors:
+            return ''
+
+        return '{}'.format(self._response.errors[0].serialize())
+
+
+class StepTestCase(unittest.TestCase):
+    """
+    The base class to use for step "unit" testing. This class overrides
+    the default setup and tearDown methods from the unittest.TestCase to add
+    functionality for loading and unloading the Cauldron notebook settings
+    needed to run and test steps. If you override either of these methods
+    make sure that you call their super methods as well or the functionality
+    will be lost.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(StepTestCase, self).__init__(*args, **kwargs)
+        self.results_directory = None
+        self.temp_directories = dict()
+
+    def setUp(self):
+        """
+        A modified setup function that handles opening the project for testing.
+        If you override the setUp function in your tests, be sure to call the
+        super function so that this initialization happens properly
+        """
+
+        environ.modes.add(environ.modes.TESTING)
+        super(StepTestCase, self).setUp()
+        results_directory = tempfile.mkdtemp(
+            prefix='cd-step-test-results-{}--'.format(self.__class__.__name__)
+        )
+        self.results_directory = results_directory
+        environ.configs.put(results_directory=results_directory, persists=False)
+        self.temp_directories = dict()
+        self.open_project()
+
+        # Load libraries before calling test functions so that patching works
+        # correctly.
+        runner.reload_libraries()
+
+    def make_project_path(self, *args) -> str:
+        """
+        Returns an absolute path to the specified location within the Cauldron
+        project directory where this test file is located.
+
+        :param args:
+            Relative path components within the Cauldron project directory
+        :return:
+            The absolute path to the location within the project. If no path
+            components are specified, the location returned will be the
+            project directory itself.
+        """
+
+        filename = inspect.getfile(self.__class__)
+        project_directory = find_project_directory(filename)
+
+        if project_directory is None:
+            raise FileNotFoundError(' '.join([
+                'StepTest does not appear to be located within',
+                'a Cauldron project directory'
+            ]))
+
+        return os.path.join(project_directory, *args)
+
+    def open_project(self) -> 'exposed.ExposedProject':
+        """
+        Returns the Response object populated by the open project command
+        """
+
+        project_path = self.make_project_path()
+        res = environ.Response()
+        commander.execute(
+            'open', '{} --forget'.format(project_path),
+            res
+        )
+        res.thread.join()
+
+        # Prevent threading race conditions
+        check_count = 0
+        while res.success and not cd.project.internal_project:
+            if check_count > 100:
+                break
+
+            check_count += 1
+            time.sleep(0.25)
+
+        if res.failed or not cd.project.internal_project:
+            self.fail(
+                'Unable to open project at path "{}"'
+                .format(project_path)
+            )
+
+        os.chdir(cd.project.internal_project.source_directory)
+
+        return cd.project
+
+    def run_step(
+            self,
+            step_name: str,
+            allow_failure: bool = False
+    ) -> StepTestRunResult:
+        """
+        Runs the specified step by name its complete filename including extension
+
+        :param step_name:
+            The full filename of the step to be run including its extension.
+        :param allow_failure:
+            Whether or not to allow a failed result to be returned. If False,
+            a failed attempt to run a step will cause the current test to
+            fail immediately before returning a value from this function call.
+            Override this with a True value to have the step failure data
+            passed back for inspection.
+        :return:
+            A StepTestRunResult instance containing information about the
+            execution of the step.
+        """
+
+        project = cd.project.internal_project
+        if not project:
+            self.fail(
+                'No project was open. Unable to run step "{}"'
+                .format(step_name)
+            )
+
+        step = project.get_step(step_name)
+        if not step:
+            self.fail('No step named "{}" was found'.format(step_name))
+
+        print('RUNNING', step_name)
+        response = commander.execute('run', '"{}" --force'.format(step_name))
+        print('WAITING', step_name)
+        response.thread.join()
+        print('COMPLETE', step_name)
+
+        if not allow_failure and response.failed:
+            self.fail('Failed to run step "{}"'.format(step_name))
+
+        return StepTestRunResult(step, response)
+
+    def tearDown(self):
+        super(StepTestCase, self).tearDown()
+
+        # Close any open project so that it doesn't persist to the next test
+        closed = commander.execute('close', '')
+        closed.thread.join()
+
+        environ.configs.remove('results_directory', include_persists=False)
+
+        environ.systems.remove(self.results_directory)
+        self.results_directory = None
+
+        for key, path in self.temp_directories.items():
+            environ.systems.remove(path)
+
+        environ.modes.remove(environ.modes.TESTING)
+
+    def make_temp_path(self, identifier, *args) -> str:
+        """
+
+        :param identifier:
+        :param args:
+        :return:
+        """
+
+        if identifier not in self.temp_directories:
+            self.temp_directories[identifier] = tempfile.mkdtemp(
+                prefix='cd-step-test-{}'.format(identifier)
+            )
+
+        return os.path.realpath(
+            os.path.join(self.temp_directories[identifier], *args)
+        )
---
+++ packages/cauldron-notebook/cauldron_notebook-0.3.0-py2.py3-none-any.whl/cauldron/test/steptesting/test_steptest.py
@@ -0,0 +1,134 @@
+import os
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
+import cauldron as cd
+from cauldron import steptest
+from cauldron.steptest import StepTestCase
+
+
+class StepTest(StepTestCase):
+
+    def test_first_step(self):
+        """ should not be any null/NaN values in df """
+
+        self.assertIsNone(cd.shared.fetch('df'))
+        step = self.run_step('S01-first.py')
+        df = cd.shared.df
+        self.assertFalse(df.isnull().values.any())
+
+        error_echo = step.echo_error()
+        self.assertEqual(error_echo, '')
+
+    def test_second_step(self):
+        """ 
+        should fail without exception because of an exception raised in the 
+        source but failure is allowed
+        """
+
+        step = self.run_step('S02-errors.py', allow_failure=True)
+        self.assertFalse(step.success)
+
+        error_echo = step.echo_error()
+        self.assertGreater(len(error_echo), 0)
+
+    def test_second_step_strict(self):
+        """ 
+        should fail because of an exception raised in the source when strict
+        failure is enforced
+        """
+
+        with self.assertRaises(Exception):
+            self.run_step('S02-errors.py', allow_failure=False)
+
+    @patch('_testlib.patching_test')
+    def test_second_step_with_patching(self, patching_test: MagicMock):
+        """Should override the return value with the patch"""
+
+        patching_test.return_value = 12
+        cd.shared.value = 42
+
+        self.run_step('S03-lib-patching.py')
+        self.assertEqual(cd.shared.result, 12)
+
+    def test_second_step_without_patching(self):
+        """Should succeed running the step without patching"""
+
+        cd.shared.value = 42
+        self.run_step('S03-lib-patching.py')
+        self.assertEqual(cd.shared.result, 42)
+
+    def test_to_strings(self):
+        """ should convert list of integers to a list of strings """
+
+        before = [1, 2, 3]
+        step = self.run_step('S01-first.py')
+        after = step.local.to_strings(before)
+        self.assertTrue(step.success)
+        self.assertEqual(['1', '2', '3'], after)
+
+    def test_modes(self):
+        """ should be testing and not interactive or single run """
+
+        step = self.run_step('S01-first.py')
+        self.assertTrue(step.success)
+        self.assertTrue(step.local.is_testing)
+        self.assertFalse(step.local.is_interactive)
+        self.assertFalse(step.local.is_single_run)
+
+    def test_find_in_current_path(self):
+        """ should find a project in this file's directory """
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+        result = steptest.find_project_directory(directory)
+        self.assertEqual(directory, result)
+
+    def test_find_in_parent_path(self):
+        """ should find a project in the parent directory """
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+        subdirectory = os.path.join(directory, 'fake')
+        result = steptest.find_project_directory(subdirectory)
+        self.assertEqual(directory, result)
+
+    def test_find_in_grandparent_path(self):
+        """ should find a project in the grandparent directory """
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+        subdirectory = os.path.join(directory, 'fake', 'fake')
+        result = steptest.find_project_directory(subdirectory)
+        self.assertEqual(directory, result)
+
+    def test_find_failed_at_root(self):
+        """ should return None if top-level directory has no project """
+
+        directory = os.path.dirname(os.path.realpath(__file__))
+        subdirectory = os.path.join(directory, 'fake')
+
+        with patch('os.path.dirname', return_value=subdirectory) as func:
+            result = steptest.find_project_directory(subdirectory)
+            func.assert_called_once_with(subdirectory)
+        self.assertIsNone(result)
+
+    def test_make_temp_path(self):
+        """ should make a temp path for testing """
+
+        temp_path = self.make_temp_path('some-id', 'a', 'b.test')
+        self.assertTrue(temp_path.endswith('b.test'))
+
+    def test_no_such_step(self):
+        """ should fail if no such step exists """
+
+        with self.assertRaises(Exception):
+            self.run_step('FAKE-STEP.no-exists')
+
+    def test_no_such_project(self):
+        """ should fail if no project exists """
+
+        project = cd.project.internal_project
+        cd.project.load(None)
+
+        with self.assertRaises(Exception):
+            self.run_step('FAKE')
+
+        cd.project.load(project)
