9d48a5fd50f11fc1c6e8dac253123b8f5cd6ae5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:58 +0000

    Add package Neblio/0.1.1/Neblio-0.1.1.tar.gz

---
+++ packages/Neblio/Neblio-0.1.1.tar.gz/Neblio-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Neblio',
+    version='0.1.1',
+    description='',
+    long_description='',
+    url='https://www.nebl.io',
+    author='',
+    author_email='info@nebl.io',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
