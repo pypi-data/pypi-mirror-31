0b6cd14ac8bbe0d5cc4525e65fd3c8f1b50ef2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package ocrd/0.0.2/ocrd-0.0.2.tar.gz

---
+++ packages/ocrd/ocrd-0.0.2.tar.gz/ocrd-0.0.2/ocrd/model/__init__.py
@@ -0,0 +1,6 @@
+from .ocrd_xml_base import OcrdXmlDocument
+from .ocrd_mets import OcrdMets
+from .ocrd_page import OcrdPage
+from .ocrd_exif import OcrdExif
+from .ocrd_file import OcrdFile
+from .ocrd_swagger import OcrdSwagger
