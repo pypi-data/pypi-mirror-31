46a48d7c23c2ed403757f7188ed176eaa0abbe11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:56 +0000

    Add package python-selvpcclient/1.1.0/python-selvpcclient-1.1.0.tar.gz

---
+++ packages/python-selvpcclient/python-selvpcclient-1.1.0.tar.gz/python-selvpcclient-1.1.0/selvpcclient/resources/tokens.py
@@ -0,0 +1,25 @@
+from selvpcclient import base
+
+
+class Token(base.Resource):
+    """Represents a token."""
+
+
+class TokensManager(base.Manager):
+    """Manager class for manipulating token."""
+    resource_class = Token
+
+    def create(self, project_id=None, domain_name=None, return_raw=False):
+        """Create reseller token for project.
+
+        :param string project_id: Project_id.
+        :param return_raw: flag to force returning raw JSON instead of
+                Python object of self.resource_class
+        :rtype: :class:`Token`
+        """
+        body = {'token': {}}
+        if project_id:
+            body['token']['project_id'] = project_id
+        if domain_name:
+            body['token']['domain_name'] = domain_name
+        return self._post('/tokens', body, 'token', return_raw=return_raw)
