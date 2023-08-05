6a27474eda6cfd830c15d6604b6dd9f313ca9b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package toco/0.0.2/toco-0.0.2.tar.gz

---
+++ packages/toco/toco-0.0.2.tar.gz/toco-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+MAJOR_VERSION='0'
+MINOR_VERSION='0'
+PATCH_VERSION='2'
+
+VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
+
+setup(
+    name = 'toco',
+    packages = ['toco'],
+    version = VERSION,
+    description = 'Basic tools for interacting with DynamoDB.',
+    author = 'Steve Norum',
+    author_email = 'sn@drunkenrobotlabs.org',
+    url = 'https://github.com/stevenorum/toco',
+    download_url = 'https://github.com/stevenorum/toco/archive/{}.tar.gz'.format(VERSION),
+    keywords = ['python','aws','dynamodb'],
+    classifiers = [],
+)
