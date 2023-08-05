6472b77d66ca4408a0ba1c2244913047c92a1445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:56 +0000

    Add package eidreader/0.0.7/eidreader-0.0.7.tar.gz

---
+++ packages/eidreader/eidreader-0.0.7.tar.gz/eidreader-0.0.7/eidreader/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2018 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+import os
+
+SETUP_INFO = {}
+fn = os.path.join(os.path.dirname(__file__), 'setup_info.py')
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
+
+__version__ = SETUP_INFO['version']
+
+intersphinx_urls = dict(docs="http://eidreader.lino-framework.org")
+srcref_url = 'https://github.com/lino-framework/eidreader/blob/master/%s'
+
