aa046a568c3d1661c6215633fb817b5ea06d3c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:50 +0000

    Add package clizy/0.0.2/clizy-0.0.2.zip

---
+++ packages/clizy/clizy-0.0.2.zip/clizy-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (3, 6):
+    raise RuntimeError("Python < 3.6 is not supported!")
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='clizy',
+    version='0.0.2',
+    description="Command-line interface creation for lazy people using type hints.",
+    long_description=long_description,
+    url='https://github.com/prokopst/clizy',
+    packages=find_packages(include=['clizy']),
+    author="Stanislav Prokop",
+    license='Apache 2 License',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
