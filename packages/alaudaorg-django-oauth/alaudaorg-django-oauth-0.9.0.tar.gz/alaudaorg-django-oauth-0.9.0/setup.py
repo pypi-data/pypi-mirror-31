436daa3d193be9f9e003f2f6772e29aabacc7b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:38 +0000

    Add package alaudaorg-django-oauth/0.9.0/alaudaorg-django-oauth-0.9.0.tar.gz

---
+++ packages/alaudaorg-django-oauth/alaudaorg-django-oauth-0.9.0.tar.gz/alaudaorg-django-oauth-0.9.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('oauth2_provider')
+
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(
+    name="alaudaorg-django-oauth",
+    version=version,
+    description="OAuth2 goodies for Django",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Framework :: Django :: 1.4",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+    ],
+    keywords='django oauth oauth2 oauthlib',
+    author="Federico Frenguelli, Massimiliano Pippi",
+    author_email='synasius@gmail.com, mpippi@gmail.com',
+    url='https://github.com/evonove/django-oauth-toolkit',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='runtests',
+    install_requires=[
+        'django>=1.4',
+        'django-braces>=1.2.2',
+        'oauthlib==1.0.1',
+        'six',
+    ],
+    zip_safe=False,
+)
