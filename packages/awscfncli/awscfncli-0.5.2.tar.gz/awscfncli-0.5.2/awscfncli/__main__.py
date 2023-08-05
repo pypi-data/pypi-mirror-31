b42ac2f381af46f1442cc8ef9f5639daf46ba8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package awscfncli/0.5.2/awscfncli-0.5.2.tar.gz

---
+++ packages/awscfncli/awscfncli-0.5.2.tar.gz/awscfncli-0.5.2/awscfncli/__main__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+from awscfncli.cli import cfn
+
+__author__ = 'kotaimen'
+__date__ = '09/01/2017'
+
+
+def main():
+    cfn()
+
+
+if __name__ == '__main__':
+    main()
