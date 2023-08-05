503e4d74bcebb65c386cae72c751b5f3be4730b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package django-global-context/0.1.1/django-global-context-0.1.1.tar.gz

---
+++ packages/django-global-context/django-global-context-0.1.1.tar.gz/django-global-context-0.1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-global-context',
+      version='0.1.1',
+      description='Global context injector for Django',
+      author='Nik Nyby',
+      author_email='nnyby@columbia.edu',
+      url='https://github.com/ccnmtl/django-global-context',
+      packages=['globalcontext'],
+)
