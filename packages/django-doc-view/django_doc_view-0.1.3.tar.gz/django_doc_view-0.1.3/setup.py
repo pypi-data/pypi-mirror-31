aafd3e9356ff11b0331e1ae07d208af0ed1863d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:13 +0000

    Add package django-doc-view/0.1.3/django_doc_view-0.1.3.tar.gz

---
+++ packages/django-doc-view/django_doc_view-0.1.3.tar.gz/django_doc_view-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+long_description = ''
+try:
+    with open('./readme.md') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    pass
+
+setup(
+    name='django_doc_view',
+    version='0.1.3',
+    keywords=('django', 'api', 'view', 'document', 'doc'),
+    description='The simplest way to document your Django APIs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # https://packaging.python.org/tutorials/distributing-packages/#description
+    author='Ocavue',
+    author_email='ocavue@gmail.conm',
+    url='https://github.com/ocavue/django-doc-view',
+    license='MIT License',
+    packages=find_packages(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta', 'Programming Language :: Python :: 3'
+    ],
+)
