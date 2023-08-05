7023e50c495fb928e3b31b0fd781f86ce35667cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:18 +0000

    Add package flake8-invalid-escape-sequences/1.3/flake8_invalid_escape_sequences-1.3-py2.py3-none-any.whl

---
+++ packages/flake8-invalid-escape-sequences/flake8_invalid_escape_sequences-1.3-py2.py3-none-any.whl/flake8_invalid_escape_sequences/__init__.py
@@ -0,0 +1,54 @@
+"""Define a flake8 plugin to check for invalid escape sequences."""
+
+__version__ = '1.3'
+
+
+from re import compile as re_compile, VERBOSE
+from token import STRING
+from warnings import warn
+
+from pkg_resources import parse_version
+try:
+    from pycodestyle import __version__ as pycodestyle_version
+except ImportError:
+    pycodestyle_version = '0'
+
+
+if parse_version(pycodestyle_version) >= parse_version('2.4.0'):
+    warn('pycodestyle 2.4.0+ includes W605 rule. '
+         'flake8-invalid-escape-sequence plugin is not required anymore.')
+
+invalid_escape_sequence_match = re_compile(
+    r'''
+        # escape sequences in r'raw-strings' are all valid
+        [ufb]?
+        [\'"]
+        # a valid escape sequence or other character
+        (?:\\[\\\n\'"abfnrtv0-7xNuU]|[^\\])*
+        # invalid escape sequence
+        (\\[^\\\n\'"abfnrtv0-7xNuU])
+    ''',
+    VERBOSE,
+).match
+
+
+# noinspection PyUnusedLocal
+def plugin(tree, file_tokens):
+    """Walk the tree and detect invalid escape sequences."""
+    for token in file_tokens:
+        if token[0] != STRING:  # token[0] == token.type
+            continue
+        # token[1] == token.string # python 3
+        invalid_sequence_match = invalid_escape_sequence_match(token[1])
+        if invalid_sequence_match:
+            yield (
+                token[2][0],  # line_number
+                token[2][1],  # offset
+                'IES: invalid escape sequence %s' %
+                invalid_sequence_match.group(1),  # text
+                None  # check # unused
+            )
+
+
+plugin.name = 'flake8-invalid-escape-sequences'
+plugin.version = __version__
