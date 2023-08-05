168067bf1979ab503270a9283a03d104feaaf5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:35 +0000

    Add package django-wallets/0.1.31/django-wallets-0.1.31.tar.gz

---
+++ packages/django-wallets/django-wallets-0.1.31.tar.gz/django-wallets-0.1.31/wallets/signals.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import Signal
+from .services import generate_new_address
+
+get_webhook = Signal(providing_args=['from_address', 'to_address', 'symbol', 'event', 'transaction_id'])
