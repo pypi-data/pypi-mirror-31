d35449c5727c259251502243ed435db858617202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:24 +0000

    Add package typepy/0.0.26/typepy-0.0.26.tar.gz

---
+++ packages/typepy/typepy-0.0.26.tar.gz/typepy-0.0.26/typepy/__init__.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+import typepy.type
+
+from .__version__ import __author__, __copyright__, __email__, __license__, __version__
+from ._const import StrictLevel
+from ._error import TypeConversionError
+from ._function import (
+    is_empty_sequence, is_empty_string, is_hex, is_list_or_tuple, is_not_empty_sequence,
+    is_not_empty_string, is_not_null_string, is_null_string)
+from ._typecode import Typecode
