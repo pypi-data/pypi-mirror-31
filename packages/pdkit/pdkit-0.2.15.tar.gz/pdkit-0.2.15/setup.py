07f632d95d84a78c2ff8e0a38f9fa0d86fb05ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:33 +0000

    Add package pdkit/0.2.15/pdkit-0.2.15.tar.gz

---
+++ packages/pdkit/pdkit-0.2.15.tar.gz/pdkit-0.2.15/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='pdkit',
+    version='0.2.15',
+    description='Parkinson`s Disease Kit',
+    url='https://github.com/pdkit/pdkit',
+    long_description=readme(),
+    keywords='parkinson`s disease kit',
+    author='J.S. Pons',
+    author_email='joan@dcs.bbk.ac.uk',
+    license='MIT',
+    packages=['pdkit'],
+    install_requires=[
+        'numpy', 'pandas', 'scipy', 'PyWavelets'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ]
+)
