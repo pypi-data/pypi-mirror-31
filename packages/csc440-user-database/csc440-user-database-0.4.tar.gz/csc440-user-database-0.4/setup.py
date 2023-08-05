af587ea56cf552098da7675059a01080cc8a9291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:09 +0000

    Add package csc440-user-database/0.4/csc440-user-database-0.4.tar.gz

---
+++ packages/csc440-user-database/csc440-user-database-0.4.tar.gz/csc440-user-database-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+     name='csc440-user-database',    # This is the name of your PyPI-package.
+     version='0.4',                          # Update the version number for new releases
+     py_modules=['csc440user']                  # The name of your scipt, and also the command you'll be using for calling it
+)
