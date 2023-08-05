b68643bec109e0c2bf7c5110457639a2c197e2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package SKLogging/3.6.1.3/SKLogging-3.6.1.3.tar.gz

---
+++ packages/SKLogging/SKLogging-3.6.1.3.tar.gz/SKLogging-3.6.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='SKLogging',
+    version='3.6 1.3',
+    description='Logging package for my personal use.',
+    author='SuperKooks',
+    author_email='superkooks@superkooks.com',
+    packages=['SKLogging']
+)
