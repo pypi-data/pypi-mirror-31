5918bfe5a77ac424e1f7a6448a2a36c9006c3193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package csc440-albertm1-stage-a/1.2/csc440_albertm1_stage_a-1.2.tar.gz

---
+++ packages/csc440-albertm1-stage-a/csc440_albertm1_stage_a-1.2.tar.gz/csc440_albertm1_stage_a-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='csc440_albertm1_stage_a',
+    version='1.2',
+    description='User creation and deletion methods for a Flask wiki app',
+    packages=['csc440_albertm1_stage_a'],
+    url='',
+    license='',
+    author='Matthew Albert',
+    author_email='albertm1@nku.edu'
+)
