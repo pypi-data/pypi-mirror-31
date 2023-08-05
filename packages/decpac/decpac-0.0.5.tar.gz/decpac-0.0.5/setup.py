1fc038e5b93256e8bba7a2c0474170de1f628d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package decpac/0.0.5/decpac-0.0.5.tar.gz

---
+++ packages/decpac/decpac-0.0.5.tar.gz/decpac-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+GEN_version = '0.0.5'
+READ_name = 'decpac'
+
+setup(
+    name=READ_name,
+    version=GEN_version,
+    author='rendaw',
+    url='https://github.com/rendaw/decpac',
+    download_url='https://github.com/rendaw/decpac/tarball/v{}'.format(
+        GEN_version),
+    license='MIT',
+    description='Arch Linux declarative package management',
+    long_description=open('readme.md', 'r').read(),
+    classifiers=[],
+    py_modules=['decpac'],
+    install_requires=[
+        'luxem==0.0.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'decpac=decpac:main',
+        ],
+    },
+)
