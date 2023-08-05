ac5a0b991854ce7d2c88d1d141b906ef61848240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package thoth-storages/0.0.18/thoth_storages-0.0.18-py3-none-any.whl

---
+++ packages/thoth-storages/thoth_storages-0.0.18-py3-none-any.whl/thoth/storages/__init__.py
@@ -0,0 +1,11 @@
+"""Storage and database adapters for Thoth."""
+
+__name__ = 'thoth-storages'
+__version__ = '0.0.18'
+
+from .analyses import AnalysisResultsStore
+from .buildlogs import BuildLogsStore
+from .ceph import CephStore
+from .graph import GraphDatabase
+from .result_schema import RESULT_SCHEMA
+from .solvers import SolverResultsStore
