be70258b5a771499f41b5e9ac22310665cd1c464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package travislint/1.0/travislint-1.0.tar.gz

---
+++ packages/travislint/travislint-1.0.tar.gz/travislint-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='travislint',
+    version='1.0',
+    long_description=readme(),
+    description='An application to simplify linting of travis-ci config files.',
+    author='Paul D.Smith',
+    author_email='paul@pauldsmith.org.uk',
+    url='https://github.com/papadeltasierrra/travislint',
+    packages=['travislint'],
+    python_requires='>=2.7, <4',
+    install_requires=[
+        'requests',
+        'yamllint'        
+    ],
+    entry_points = {
+        'console_scripts': ['travislint=travislint.travislint:main'],
+    },
+    include_package_data=True
+)
