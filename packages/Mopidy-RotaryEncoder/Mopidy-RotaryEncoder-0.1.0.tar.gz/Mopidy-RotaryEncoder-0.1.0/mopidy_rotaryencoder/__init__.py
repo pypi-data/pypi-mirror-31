d396c6fd6d10ec465800543255de5262838acf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package Mopidy-RotaryEncoder/0.1.0/Mopidy-RotaryEncoder-0.1.0.tar.gz

---
+++ packages/Mopidy-RotaryEncoder/Mopidy-RotaryEncoder-0.1.0.tar.gz/Mopidy-RotaryEncoder-0.1.0/mopidy_rotaryencoder/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+from mopidy import config, ext
+
+__version__ = '0.1.0'
+
+logger = logging.getLogger(__name__)
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-RotaryEncoder'
+    ext_name = 'rotaryencoder'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['datapin'] = config.Integer()
+        schema['clkpin'] = config.Integer()
+        schema['swpin'] = config.Integer()
+        return schema
+
+    def setup(self, registry):
+        from .frontend import RotaryEncoderFrontend
+        registry.add('frontend', RotaryEncoderFrontend)
