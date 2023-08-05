86e40d5757b44db9e4b50ec2123e89fa3ee0604a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:19 +0000

    Add package prefixspan/0.2.3/prefixspan-0.2.3.tar.gz

---
+++ packages/prefixspan/prefixspan-0.2.3.tar.gz/prefixspan-0.2.3/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="prefixspan",
+    packages=["prefixspan"],
+    scripts=["prefixspan-cli"],
+    version="0.2.3",
+    description="PrefixSpan in Python 3",
+    author="Chuancong Gao",
+    author_email="chuancong@gmail.com",
+    url="https://github.com/chuanconggao/PrefixSpan-py",
+    download_url="https://github.com/chuanconggao/PrefixSpan-py/tarball/0.2.3",
+    keywords=[
+        "data mining",
+        "pattern mining"
+    ],
+    license="MIT",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires=">= 3",
+    install_requires=[
+        "docopt >= 0.6.2",
+    ]
+)
