71e1c9d415981fd6485bc7bcf417173de846e86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:07 +0000

    Add package vedis/0.7.0/vedis-0.7.0.tar.gz

---
+++ packages/vedis/vedis-0.7.0.tar.gz/vedis-0.7.0/setup.py
@@ -0,0 +1,34 @@
+import os
+import warnings
+
+from distutils.core import setup, Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cython_installed = False
+    warnings.warn('Cython not installed, using pre-generated C source file.')
+else:
+    cython_installed = True
+
+
+if cython_installed:
+    python_source = 'vedis.pyx'
+else:
+    python_source = 'vedis.c'
+    cythonize = lambda obj: obj
+
+library_source = 'src/vedis.c'
+vedis_extension = Extension(
+    'vedis',
+    sources=[python_source, library_source])
+
+setup(
+    name='vedis',
+    version='0.7.0',
+    description='Fast Python bindings for the Vedis embedded NoSQL database.',
+    author='Charles Leifer',
+    author_email='',
+    setup_requires=['cython'],
+    install_requires=['cython'],
+    ext_modules=cythonize([vedis_extension]),
+)
