284d5faf2a1cc4bdb7c28d2935e761dd493fc648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:47 +0000

    Add package proxy-list/0.3.2/proxy_list-0.3.2.zip

---
+++ packages/proxy-list/proxy_list-0.3.2.zip/proxy_list-0.3.2/proxy_list/__init__.py
@@ -0,0 +1,1 @@
+from .api import ProxyList, update, stop_update, get, check, check_many
---
+++ packages/proxy-list/proxy_list-0.3.2.zip/proxy_list-0.3.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'proxy_list',
+    packages = ['proxy_list'],
+    version = '0.3.2',
+    description = 'Free proxy lists',
+    author = 'retxxxirt',
+    author_email = 'retxxirt@gmail.com',
+    url = 'https://github.com/retxxxirt/proxy_list',
+    keywords = ['proxy', 'proxies', 'proxy list', 'free', 'free proxy', 'free proxies'],
+    install_requires = ['requests', 'pyquery', 'iso3166', 'js2py']
+)
