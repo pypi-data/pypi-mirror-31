d82efd763bb6d421adb1db1e9419ec8c3eb05500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:18 +0000

    Add package familyapp.py/0.0.12/familyapp.py-0.0.12.tar.gz

---
+++ packages/familyapp.py/familyapp.py-0.0.12.tar.gz/familyapp.py-0.0.12/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(name='familyapp.py',
+      author='Piotr Giedziun',
+      version='0.0.12',
+      packages=['familyapp',],
+      license='MIT',
+      author_email='piotrgiedziun@gmail.com',
+      url='https://github.com/piotrgiedziun/familyapp.py',
+      description='A python wrapper for the FamilyApp API',
+      install_requires=requirements,
+)
