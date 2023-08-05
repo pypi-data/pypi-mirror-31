ecaed3ec4a136041262bc41add519a654969530e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package foobot_async/0.3.1/foobot_async-0.3.1.tar.gz

---
+++ packages/foobot_async/foobot_async-0.3.1.tar.gz/foobot_async-0.3.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name="foobot_async",
+    version="0.3.1",
+    url="https://github.com/reefab/foobot_async",
+
+    author="Fabien Piuzzi",
+    author_email="fabien@reefab.net",
+
+    long_description='asyncio-friendly python API for Foobot Air Quality Monitors (https://foobot.io). Requires Python 3.5+',
+    license='MIT',
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['aiohttp>=2.3.10', 'async_timeout', 'typing>=3,<4'],
+    zip_safe=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
