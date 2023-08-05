522e409b774db89b25a6a50ba31c6296137eebd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:59 +0000

    Add package lalsuite/6.48.1.dev20180422/lalsuite-6.48.1.dev20180422-cp34-cp34m-manylinux1_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.48.1.dev20180422-cp34-cp34m-manylinux1_x86_64.whl/lalxml/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalxml import *
+
+__version__ = "1.2.4.1"
+
+## \addtogroup lalxml_python
+"""This package provides Python wrappings and extensions to LALXML"""
