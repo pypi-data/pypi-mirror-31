ff0cc14faba1939b156fff24b5d6b094af477a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:46 +0000

    Add package asn1tools/0.75.0/asn1tools-0.75.0.tar.gz

---
+++ packages/asn1tools/asn1tools-0.75.0.tar.gz/asn1tools-0.75.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import asn1tools
+
+setup(name='asn1tools',
+      version=asn1tools.__version__,
+      description='ASN.1 parsing, encoding and decoding.',
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords=['ASN.1', 'asn1'],
+      url='https://github.com/eerimoq/asn1tools',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'pyparsing>=2.2.0',
+          'prompt_toolkit'
+      ],
+      test_suite="tests",
+      entry_points = {
+          'console_scripts': ['asn1tools=asn1tools.__init__:_main']
+      })
