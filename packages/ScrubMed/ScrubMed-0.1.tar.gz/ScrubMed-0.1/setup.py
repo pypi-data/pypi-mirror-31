fa9b7b388b10137ee165cdb0578f9216190cda17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package ScrubMed/0.1/ScrubMed-0.1.tar.gz

---
+++ packages/ScrubMed/ScrubMed-0.1.tar.gz/ScrubMed-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ScrubMed",
+    version="0.1",
+    packages=find_packages(),
+    author="Forest Dussault",
+    author_email="forest.dussault@inspection.gc.ca",
+    url="https://github.com/forestdussault/ScrubMed",
+    scripts=['scrubmed.py'],
+    install_requires=['click', 'pandas', 'bs4', 'BioPython']
+)
