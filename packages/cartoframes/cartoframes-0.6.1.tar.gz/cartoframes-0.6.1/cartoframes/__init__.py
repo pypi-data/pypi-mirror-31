2d9bb4d16fd6ce6c268e0d6585fe1f567fe2044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package cartoframes/0.6.1/cartoframes-0.6.1.tar.gz

---
+++ packages/cartoframes/cartoframes-0.6.1.tar.gz/cartoframes-0.6.1/cartoframes/__init__.py
@@ -0,0 +1,5 @@
+from .context import CartoContext, BatchJobStatus
+from .credentials import Credentials
+from .layer import BaseMap, QueryLayer, Layer
+from .styling import BinMethod
+from .__version__ import __version__
