88cab85676d85de5838c28ee001f7edc01897bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:58 +0000

    Add package aws-toolkit/0.7/aws_toolkit-0.7.tar.gz

---
+++ packages/aws-toolkit/aws_toolkit-0.7.tar.gz/aws_toolkit-0.7/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, find_packages
+
+except ImportError:
+
+    from distutils.core import setup, find_packages
+
+setup(
+    name='aws_toolkit',
+    version='0.7',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An AWS Provisioning Tool',
+    long_description=open('README.md').read(),
+    install_requires=['boto3','paramiko','prettytable'],
+    url='https://github.com/cjaiwenwen/aws',
+    author='Chen Jun',
+    author_email='cjaiwenwen@gmail.com'
+)
