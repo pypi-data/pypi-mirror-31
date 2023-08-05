ea396d377e65f10976116826a3e7836185e4cc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package django-braces/1.13.0/django-braces-1.13.0.tar.gz

---
+++ packages/django-braces/django-braces-1.13.0.tar.gz/django-braces-1.13.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+import braces
+
+setup(
+    name="django-braces",
+    version=braces.__version__,
+    description="Reusable, generic mixins for Django",
+    long_description="Mixins to add easy functionality to Django class-based views, forms, and models.",
+    keywords="django, views, forms, mixins",
+    author="Kenneth Love <kenneth@brack3t.com>, Chris Jones <chris@brack3t.com>",
+    author_email="devs@brack3t.com",
+    url="https://github.com/brack3t/django-braces/",
+    license="BSD",
+    packages=["braces"],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: Web Environment",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 2.0"
+    ],
+)
