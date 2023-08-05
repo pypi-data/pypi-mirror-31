9cdb3bece68cc57412757e486fd75755a80f69cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:38 +0000

    Add package amadeus/1.0.0b8/amadeus-1.0.0b8-py2.py3-none-any.whl

---
+++ packages/amadeus/amadeus-1.0.0b8-py2.py3-none-any.whl/amadeus/__init__.py
@@ -0,0 +1,16 @@
+from .amadeus import Client
+from .version import version
+
+from .client.location import Location
+from .client.request import Request
+from .client.response import Response
+from .client.errors import ResponseError
+from .client.errors import ParserError, ServerError, AuthenticationError
+from .client.errors import NotFoundError, ClientError, NetworkError
+
+__all__ = [
+    'Client', 'Location', 'version', 'ResponseError',
+    'ParserError', 'ServerError', 'AuthenticationError',
+    'NotFoundError', 'ClientError', 'Request', 'Response',
+    'NetworkError'
+]
