d80d9b9794067677a85abe1fc3e287f3a6b94461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:46 +0000

    Add package storops/0.5.8/storops-0.5.8.tar.gz

---
+++ packages/storops/storops-0.5.8.tar.gz/storops-0.5.8/storops/unity/resource/storage_resource.py
@@ -0,0 +1,169 @@
+# coding=utf-8
+# Copyright (c) 2015 EMC Corporation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import unicode_literals
+
+from storops.exception import UnityStorageResourceNameInUseError, \
+    UnityConsistencyGroupNameInUseError
+from storops.unity import enums
+from storops.unity.resource import UnityResource, UnityResourceList
+import storops.unity.resource.snap
+
+__author__ = 'Cedric Zhuang'
+
+
+class UnityStorageResource(UnityResource):
+    @classmethod
+    def get(cls, cli, _id=None):
+        if not isinstance(_id, (cls, UnityConsistencyGroup)):
+            ret = cls(_id=_id, cli=cli)
+        else:
+            ret = _id
+        return ret
+
+    def action(self, action_name, **kwargs):
+        return self._cli.action(self.resource_class,
+                                self.get_id(),
+                                action_name,
+                                **kwargs)
+
+    def modify_fs(self, **kwargs):
+        return self.action('modifyFilesystem', **kwargs)
+
+
+class UnityStorageResourceList(UnityResourceList):
+    @classmethod
+    def get_resource_class(cls):
+        return UnityStorageResource
+
+
+class UnityConsistencyGroup(UnityResource):
+    @classmethod
+    def create(cls, cli, name, description=None, lun_list=None,
+               hosts=None):
+        lun_list = cls._wrap_lun_list(lun_list)
+        hosts = cls._wrap_host_list(hosts, cli)
+
+        req_body = cli.make_body(name=name,
+                                 description=description,
+                                 lunAdd=lun_list,
+                                 blockHostAccess=hosts)
+        resp = cli.type_action(UnityStorageResource().resource_class,
+                               'createConsistencyGroup', **req_body)
+
+        try:
+            resp.raise_if_err()
+        except UnityStorageResourceNameInUseError:
+            raise UnityConsistencyGroupNameInUseError()
+        except: # noqa
+            raise
+
+        return UnityConsistencyGroup(_id=resp.resource_id, cli=cli)
+
+    @property
+    def name(self):
+        if hasattr(self, '_name') and self._name is not None:
+            name = self._name
+        else:
+            if not self._is_updated():
+                self.update()
+            name = self._get_value_by_key('name')
+        return name
+
+    @name.setter
+    def name(self, new_name):
+        self.modify(name=new_name)
+
+    @staticmethod
+    def _wrap_lun_list(lun_list):
+        if lun_list is not None:
+            ret = list(map(lambda lun: {'lun': lun}, lun_list))
+        else:
+            ret = None
+        return ret
+
+    @staticmethod
+    def _wrap_host_list(host_list, cli):
+        def make_host_elem(host):
+            return cli.make_body(host=host,
+                                 accessMask=enums.HostLUNAccessEnum.BOTH)
+
+        if host_list is not None:
+            ret = list(map(make_host_elem, host_list))
+        else:
+            ret = None
+        return ret
+
+    def add_lun(self, *lun_list):
+        return self.modify(luns_to_add=lun_list)
+
+    def remove_lun(self, *lun_list):
+        return self.modify(luns_to_remove=lun_list)
+
+    def set_host_access(self, *hosts):
+        return self.modify(hosts=hosts)
+
+    def add_host_access(self, *hosts):
+        return self.modify(hosts_to_add=hosts)
+
+    def remove_host_access(self, *hosts):
+        return self.modify(hosts_to_remove=hosts)
+
+    def create_snap(self, name=None, description=None, is_auto_delete=None,
+                    retention_duration=None):
+        clz = storops.unity.resource.snap.UnitySnap
+        return clz.create(self._cli, self, name=name, description=description,
+                          is_auto_delete=is_auto_delete,
+                          retention_duration=retention_duration,
+                          is_read_only=None, fs_access_type=None)
+
+    @property
+    def snapshots(self):
+        clz = storops.unity.resource.snap.UnitySnapList
+        snaps = clz(cli=self._cli, storage_resource=self)
+        return list(filter(lambda snap: snap.snap_group is None, snaps))
+
+    def modify(self, name=None, description=None,
+               luns_to_add=None, luns_to_remove=None,
+               hosts_to_add=None, hosts_to_remove=None, hosts=None):
+        luns_to_add = self._wrap_lun_list(luns_to_add)
+        luns_to_remove = self._wrap_lun_list(luns_to_remove)
+
+        hosts_to_add = self._wrap_host_list(hosts_to_add, self._cli)
+        hosts = self._wrap_host_list(hosts, self._cli)
+
+        resp = self.action(
+            'modifyConsistencyGroup',
+            name=name, description=description,
+            lunAdd=luns_to_add, lunRemove=luns_to_remove,
+            blockHostAccess=hosts, addBlockHostAccess=hosts_to_add,
+            removeBlockHostAccess=hosts_to_remove)
+        resp.raise_if_err()
+        return resp
+
+
+class UnityConsistencyGroupList(UnityResourceList):
+    type_cg = enums.StorageResourceTypeEnum.CONSISTENCY_GROUP
+
+    def __init__(self, **the_filter):
+        the_filter['type'] = self.type_cg
+        super(UnityConsistencyGroupList, self).__init__(**the_filter)
+
+    @classmethod
+    def get_resource_class(cls):
+        return UnityConsistencyGroup
+
+    def _filter(self, item):
+        return item.type == self.type_cg
---
+++ packages/storops/storops-0.5.8.tar.gz/storops-0.5.8/storops/vnx/resource/mirror_view.py
@@ -0,0 +1,307 @@
+# coding=utf-8
+# Copyright (c) 2015 EMC Corporation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import unicode_literals
+
+from storops.exception import raise_if_err, \
+    VNXMirrorException, VNXMirrorImageNotFoundError
+from storops.lib.common import check_text, instance_cache
+from storops.vnx.enums import VNXMirrorViewRecoveryPolicy
+from storops.vnx.enums import VNXMirrorViewSyncRate
+import storops.vnx.resource.lun
+from storops.vnx.resource import VNXCliResource, VNXCliResourceList
+
+__author__ = 'Cedric Zhuang'
+
+
+class VNXMirrorViewImage(VNXCliResource):
+    @staticmethod
+    def get_id(image):
+        if isinstance(image, VNXMirrorViewImage):
+            image = image.uid
+        try:
+            image = check_text(image)
+        except ValueError:
+            raise ValueError('invalid image id supplied: {}'
+                             .format(image))
+        return image
+
+    @property
+    def wwn(self):
+        return self.uid
+
+
+class VNXMirrorViewImageList(VNXCliResourceList):
+    @classmethod
+    def get_resource_class(cls):
+        return VNXMirrorViewImage
+
+
+class VNXMirrorView(VNXCliResource):
+    def __init__(self, name=None, cli=None):
+        super(VNXMirrorView, self).__init__()
+        self._cli = cli
+        self._name = name
+
+    def _get_raw_resource(self):
+        return self._cli.get_mirror_view(name=self._name, poll=self.poll)
+
+    @classmethod
+    def create(cls, cli, name, src_lun, use_write_intent_log=True):
+        lun_clz = storops.vnx.resource.lun.VNXLun
+        lun_id = lun_clz.get_id(src_lun)
+        out = cli.create_mirror_view(name, lun_id, use_write_intent_log)
+        raise_if_err(out, default=VNXMirrorException)
+        return VNXMirrorView(name, cli=cli)
+
+    @classmethod
+    def get(cls, cli, name=None):
+        if name is None:
+            ret = VNXMirrorViewList(cli)
+        else:
+            ret = VNXMirrorView(name, cli)
+        return ret
+
+    def add_image(self, sp_ip, lun_id,
+                  recovery_policy=VNXMirrorViewRecoveryPolicy.AUTO,
+                  sync_rate=VNXMirrorViewSyncRate.HIGH):
+        if hasattr(sp_ip, 'spa_ip'):
+            sp_ip = sp_ip.spa_ip
+
+        lun_clz = storops.vnx.resource.lun.VNXLun
+        lun_id = lun_clz.get_id(lun_id)
+        out = self._cli.add_mirror_view_image(self._get_name(), sp_ip, lun_id,
+                                              recovery_policy, sync_rate,
+                                              poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def get_image(self, image_id):
+        for image in self.images:
+            if image.uid == image_id:
+                ret = image
+                break
+        else:
+            raise VNXMirrorImageNotFoundError(
+                'image {} not found in mirror view {}.'.format(
+                    image_id, self._get_name()))
+        return ret
+
+    @staticmethod
+    def _get_image_id(image_id):
+        return VNXMirrorViewImage.get_id(image_id)
+
+    @property
+    @instance_cache
+    def primary_image(self):
+        for image in self.images:
+            if image.is_primary:
+                ret = image
+                break
+        else:
+            ret = None
+        return ret
+
+    @property
+    @instance_cache
+    def secondary_image(self):
+        for image in self.images:
+            if not image.is_primary:
+                ret = image
+                break
+        else:
+            ret = None
+        return ret
+
+    @property
+    def is_primary(self):
+        return self.remote_mirror_status == 'Mirrored'
+
+    @property
+    def primary_image_id(self):
+        return self.primary_image.uid
+
+    @property
+    def secondary_image_id(self):
+        image = self.secondary_image
+        if image is None:
+            raise VNXMirrorImageNotFoundError(
+                'no secondary image exists for this mirror view.')
+        return image.uid
+
+    def remove_image(self, image_id=None):
+        if image_id is None:
+            image_id = self.secondary_image_id
+
+        image_id = self._get_image_id(image_id)
+        out = self._cli.delete_mirror_view_image(self._get_name(), image_id,
+                                                 poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def fracture_image(self, image_id=None):
+        if image_id is None:
+            image_id = self.secondary_image_id
+
+        image_id = self._get_image_id(image_id)
+        out = self._cli.mirror_view_fracture_image(self._get_name(), image_id,
+                                                   poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def sync_image(self, image_id=None):
+        if image_id is None:
+            image_id = self.secondary_image_id
+
+        image_id = self._get_image_id(image_id)
+        out = self._cli.mirror_view_sync_image(self._get_name(), image_id,
+                                               poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def promote_image(self, image_id=None):
+        if image_id is None:
+            image_id = self.secondary_image_id
+
+        image_id = self._get_image_id(image_id)
+        out = self._cli.mirror_view_promote_image(self._get_name(), image_id,
+                                                  poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def delete(self, force=False):
+        if force:
+            if self.secondary_image:
+                self.remove_image()
+
+        out = self._cli.delete_mirror_view(self._get_name())
+        raise_if_err(out, default=VNXMirrorException)
+
+
+class VNXMirrorViewList(VNXCliResourceList):
+    @classmethod
+    def get_resource_class(cls):
+        return VNXMirrorView
+
+    def __init__(self, cli=None, src_lun=None, tgt_lun=None):
+        super(VNXMirrorViewList, self).__init__()
+        self._cli = cli
+        self._src_lun = src_lun
+        self._tgt_lun = tgt_lun
+
+    def _filter(self, item):
+        if self._src_lun is None and self._tgt_lun is None:
+            ret = True
+        else:
+            ret = False
+            pi = item.primary_image
+            si = item.secondary_image
+            if self._src_lun is not None:
+                ret |= self._src_lun.wwn == pi.logical_unit_uid
+            if self._tgt_lun is not None and si is not None:
+                ret |= self._tgt_lun.wwn == si.logical_unit_uid
+        return ret
+
+    def _get_raw_resource(self):
+        return self._cli.get_mirror_view(poll=self.poll)
+
+
+class VNXMirrorGroup(VNXCliResource):
+    def __init__(self, cli, name=None):
+        super(VNXMirrorGroup, self).__init__()
+        self._cli = cli
+        self._name = name
+
+    def _get_raw_resource(self):
+        return self._cli.get_mirror_group(name=self._name, poll=self.poll)
+
+    @classmethod
+    def create(cls, cli, name, mirror=None):
+        out = cli.create_mirror_group(name)
+        raise_if_err(out, default=VNXMirrorException)
+        group = VNXMirrorGroup(cli=cli, name=name)
+        if mirror is not None:
+            # Add to mirror group as well
+            group.add_mirror(mirror)
+        return group
+
+    @classmethod
+    def get(cls, cli, name=None):
+        if name is None:
+            ret = VNXMirrorGroupList(cli)
+        else:
+            ret = VNXMirrorGroup(cli, name)
+        return ret
+
+    def add_mirror(self, mirror):
+        mirror_name = mirror._get_name()
+        out = self._cli.add_to_mirror_group(self._get_name(), mirror_name,
+                                            poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def remove_mirror(self, mirror):
+        mirror_name = mirror._get_name()
+        out = self._cli.remove_from_mirror_group(self._get_name(), mirror_name,
+                                                 poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def fracture_group(self):
+        out = self._cli.fracture_mirror_group(self._get_name(), poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def sync_group(self):
+        out = self._cli.sync_mirror_group(self._get_name(), poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def promote_group(self):
+        out = self._cli.promote_mirror_group(self._get_name(), poll=self.poll)
+        raise_if_err(out, default=VNXMirrorException)
+
+    def delete(self, force=False):
+        out = self._cli.delete_mirror_group(self._get_name(), force=force)
+        raise_if_err(out, default=VNXMirrorException)
+
+    @property
+    def mirrors(self):
+        mirrors = []
+        for m in self.group_mirrors:
+            mirrors.append(VNXMirrorView(cli=self._cli, name=m.mirror_name))
+        return mirrors
+
+
+class VNXMirrorGroupMirror(VNXCliResource):
+    def __init__(self, cli=None, name=None):
+        super(VNXMirrorGroupMirror, self).__init__()
+        self._cli = cli
+        self._name = name
+
+
+class VNXMirrorGroupMirrorList(VNXCliResourceList):
+    @classmethod
+    def get_resource_class(cls):
+        return VNXMirrorGroupMirror
+
+    def __init__(self, cli=None):
+        super(VNXMirrorGroupMirrorList, self).__init__()
+        self._cli = cli
+
+
+class VNXMirrorGroupList(VNXCliResourceList):
+    @classmethod
+    def get_resource_class(cls):
+        return VNXMirrorGroup
+
+    def __init__(self, cli=None, name=None):
+        super(VNXMirrorGroupList, self).__init__()
+        self._cli = cli
+        self._name = name
+
+    def _get_raw_resource(self):
+        return self._cli.get_mirror_group(name=self._name, poll=self.poll)
