91f97eae9c34e3a3b61f1ba0d7eb9e7aae46de33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:03 +0000

    Add package docker-compose-cleanup/1.1.0/docker_compose_cleanup-1.1.0-py2.py3-none-any.whl

---
+++ packages/docker-compose-cleanup/docker_compose_cleanup-1.1.0-py2.py3-none-any.whl/docker_compose_cleanup/cli.py
@@ -0,0 +1,20 @@
+""" Console script for docker-compose-cleanup.
+
+ATM the script doesn't support any parameters, so using Click is somewhat superfluous. For now.
+"""
+
+import click
+
+
+@click.command()
+@click.version_option()
+def main():
+    from .docker_compose_cleanup import clean
+    clean()
+
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
