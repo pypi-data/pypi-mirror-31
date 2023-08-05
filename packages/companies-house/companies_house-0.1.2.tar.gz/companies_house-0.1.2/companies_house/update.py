9c181a45d7cf47f67f6c55bfc8858b495f12b8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:34 +0000

    Add package companies-house/0.1.2/companies_house-0.1.2.tar.gz

---
+++ packages/companies-house/companies_house-0.1.2.tar.gz/companies_house-0.1.2/companies_house/update.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+from typing import List
+
+
+def update(
+        url: str='https://developer.companieshouse.gov.uk/api/docs/',
+        path: str=os.path.join(os.path.dirname(__file__), 'definition.csv')
+) -> None:
+    dfs: List[pd.DataFrame] = pd.read_html(url, encoding='utf-8')
+    df: pd.DataFrame = pd.concat(dfs)
+    df.reset_index(inplace=True)
+    df.to_csv(path, index=False, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    update()
