2a9c3018ff28b46674e968ff6f57835bcbb3d20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package mtianyan/0.0.2/mtianyan-0.0.2.zip

---
+++ packages/mtianyan/mtianyan-0.0.2.zip/mtianyan-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mtianyan",
+    version = "0.0.2",
+    keywords = ("pip", "mtianyan"),
+    description = "mtianyan's tool",
+    long_description = "mtianyan's tool for python",
+    license = "MIT Licence",
+
+    url = "http://blog.mtianyan.cn",
+    author = "mtianyan",
+    author_email = "mtianyan@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
