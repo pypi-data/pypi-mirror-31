9580c24365fbe69fb59e347f4411a1fb8f9472f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package hodorlive/1.0.5/hodorlive-1.0.5.tar.gz

---
+++ packages/hodorlive/hodorlive-1.0.5.tar.gz/hodorlive-1.0.5/setup.py
@@ -0,0 +1,28 @@
+import os
+from pip.download import PipSession
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+setup(
+    name='hodorlive',
+    version='1.0.5',
+    packages=find_packages(),
+    install_requires=reqs,
+    include_package_data=True,
+    license='MIT',
+    description='xpath/css based scraper with pagination',
+    keywords = ['hodor', 'cssselect', 'lxml', 'scraping'],
+    url='https://github.com/CompileInc/hodor',
+    download_url = 'https://github.com/CompileInc/hodor/tarball/1.0',
+    author='Compile Inc',
+    author_email='dev@compile.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
