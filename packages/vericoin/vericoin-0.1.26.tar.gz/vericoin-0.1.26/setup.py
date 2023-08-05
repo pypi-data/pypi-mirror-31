5ebc2e65fd9fccb0050897892335ba452c0ac6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:37 +0000

    Add package vericoin/0.1.26/vericoin-0.1.26.tar.gz

---
+++ packages/vericoin/vericoin-0.1.26.tar.gz/vericoin-0.1.26/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='vericoin',
+    version='0.1.26',
+    packages=[
+        'vericoin',
+        'vericoin.crypto'
+    ],
+    install_requires=[
+        'requests',
+        'pandas',
+        'ratelimit'
+    ],
+    url='https://github.com/sharebook-kr/vericoin',
+    author='jonghun.yoo',
+    author_email='jonghun.yoo@outlook.com',
+    description='암호화폐 Daily 가격 조회',
+)
