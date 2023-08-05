ea4f58b4def4f59eef78c188017bde92ff156a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package basecampy3/0.1.6/basecampy3-0.1.6.tar.gz

---
+++ packages/basecampy3/basecampy3-0.1.6.tar.gz/basecampy3-0.1.6/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), 'r') as infile:
+    long_description = infile.read()
+
+about = {}
+with open(os.path.join(here, 'basecampy3', '__version__.py'), 'r', encoding='utf-8') as infile:
+    exec(infile.read(), about)
+
+setup(
+    name='basecampy3',
+    version=about['__version__'],
+    packages=[
+        'basecampy3',
+        'basecampy3.endpoints'
+    ],
+    install_requires=[
+        "beautifulsoup4==4.6.0",
+        "certifi>=2018.1.18",
+        "chardet==3.0.4",
+        "idna==2.6",
+        "python-dateutil==2.7.2",
+        "requests==2.18.4",
+        "six==1.11.0",
+        "urllib3==1.22",
+    ],
+    entry_points={
+        'console_scripts': [
+            'bc3 = basecampy3.bc3_cli:main',
+        ],
+    },
+    url='https://github.com/phistrom/basecampy3',
+    license='MIT',
+    author='Phillip Stromberg',
+    author_email='phillip@4stromberg.com',
+    description='Aims to be the easiest to use version of the Basecamp 3 API',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ),
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
