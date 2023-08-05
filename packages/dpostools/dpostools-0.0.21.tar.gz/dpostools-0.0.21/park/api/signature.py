d001fb94855901c9e1d30799e6d485360a9ec327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package dpostools/0.0.21/dpostools-0.0.21.tar.gz

---
+++ packages/dpostools/dpostools-0.0.21.tar.gz/dpostools-0.0.21/park/api/signature.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from park.api.api import API
+
+class Signature(API):
+    def fee(self):
+        return self.get('api/signatures/fee')
+
+    def create(self, secret, secondSecret):
+        transaction = self.client.signatureBuilder().create(secret, secondSecret)
+
+        return self.client.transport().createTransaction(transaction)
---
+++ packages/dpostools/dpostools-0.0.21.tar.gz/dpostools-0.0.21/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='dpostools',
+    version='0.0.21',
+    packages=['dpostools', 'dpostools.tests', 'park', 'park.api', 'park.builder'],
+    url='https://github.com/BlockHub/blockhubdpostools.git',
+    license='MIT',
+    author='Charles',
+    author_email='karel@blockhub.nl',
+    description='generic toolkit for interacting with DPOS chains',
+    include_package_data=True,
+    package_data={'': ['yamls/*.yaml']},
+    install_requires=[
+          'pyyaml', 'psycopg2', 'arky', 'wheel', 'Naked', 'Jinja2', 'requests',
+          'MarkupSafe', 'certifi', 'urllib3', 'chardet', 'idna'
+      ],
+    dependency_links=['https://github.com/faustbrian/ARK-Python/archive/master.zip']
+)
