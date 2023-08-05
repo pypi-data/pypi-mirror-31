c1d57e04d46a9f500752f09cd7864714eb44dbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package mihto/0.0.2/mihto-0.0.2.tar.gz

---
+++ packages/mihto/mihto-0.0.2.tar.gz/mihto-0.0.2/setup.py
@@ -0,0 +1,28 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='mihto',
+    version='0.0.2',
+    description='Easy and simple evaluation language',
+    url='https://github.com/OrbitalAds/Mihto',
+    author='OrbitalAds',
+    author_email='dev@orbitalads.io',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Spanish',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='profiler profiling time',
+    packages=find_packages(exclude=['docs', 'examples', 'tests'])
+)
