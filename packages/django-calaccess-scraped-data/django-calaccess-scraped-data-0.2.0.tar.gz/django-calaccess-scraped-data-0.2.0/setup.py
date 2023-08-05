2c2f8ebd38bc8360bb1ff7c16d840ec4b4de5523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:30 +0000

    Add package django-calaccess-scraped-data/0.2.0/django-calaccess-scraped-data-0.2.0.tar.gz

---
+++ packages/django-calaccess-scraped-data/django-calaccess-scraped-data-0.2.0.tar.gz/django-calaccess-scraped-data-0.2.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            INSTALLED_APPS=('calaccess_scraped',),
+            MIDDLEWARE_CLASSES=()
+        )
+        from django.core.management import call_command
+        import django
+        django.setup()
+        call_command('test', 'calaccess_scraped')
+
+
+setup(
+    name='django-calaccess-scraped-data',
+    version='0.2.0',
+    license='MIT',
+    description='A Django app to scrape campaign-finance data from '
+                'the California Secretary of State’s CAL-ACCESS website',
+    author='California Civic Data Coalition',
+    url='http://django-calaccess.californiacivicdata.org',
+    author_email='cacivicdata@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,  # because we're including static files
+    install_requires=(
+        'django-calaccess-raw-data>=1.7.0',
+        'django>=1.11',
+        'beautifulsoup4>=4.6.0'
+    ),
+    cmdclass={'test': TestCommand,}
+)
