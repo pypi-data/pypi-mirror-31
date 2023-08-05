3bd8309e6c8e9d739765f598f54fd77f0fa55786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:54 +0000

    Add package cerium/1.0.4/cerium-1.0.4.tar.gz

---
+++ packages/cerium/cerium-1.0.4.tar.gz/cerium-1.0.4/cerium/exceptions.py
@@ -0,0 +1,54 @@
+"""
+Exceptions that may happen in all the androiddriver code.
+
+"""
+
+
+class AndroidDriverException(Exception):
+    """
+    Base androiddriver exception.
+    """
+    def __init__(self, msg=None):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class InvalidPATHException(AndroidDriverException):
+    """
+    Thrown when the adb PATH doesn't exist.
+    """
+    pass
+
+
+class DeviceConnectionException(AndroidDriverException):
+    """
+    Thrown when no devices are connected.
+    """
+    pass
+
+
+class CharactersException(AndroidDriverException):
+    """
+    Thrown when no devices are connected.
+    """
+    pass
+
+class ParametersException(AndroidDriverException):
+    """
+    Thrown when a parameter error occurs.
+    """
+    pass
+
+class PackageException(AndroidDriverException):
+    """
+    Thrown when the package does not exist.
+    """
+    pass
+
+class WLANConnectException(AndroidDriverException):
+    """
+    Thrown when the device is not connected to WLAN.
+    """
+    pass
