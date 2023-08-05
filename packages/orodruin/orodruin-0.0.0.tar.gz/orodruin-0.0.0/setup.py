ce794df200f94301212e65182424ee61b5cb5406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package orodruin/0.0.0/orodruin-0.0.0.tar.gz

---
+++ packages/orodruin/orodruin-0.0.0.tar.gz/orodruin-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='orodruin',
+      version='0.0.0',
+      description='distributed model deployments',
+      url='http://tbd',
+      author='bilbo baggins',
+      author_email='this@that.com',
+      license='GPL-3',
+      packages=[],
+      zip_safe=False)
