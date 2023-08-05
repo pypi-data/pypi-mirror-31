776fe819ec5d838daaa3104e3ac8a75759da1d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package texttovoice/0.1/texttovoice-0.1.tar.gz

---
+++ packages/texttovoice/texttovoice-0.1.tar.gz/texttovoice-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='texttovoice',
+      version='0.1',
+      description='Text to voice project',
+      url='http://github.com/mmehedin/TextToVoice.git',
+      author='Mihai Mehe',
+      author_email='mmehedin@uncc.edu',
+      license='MIT',
+      packages=['texttovoice'],
+      zip_safe=False)
