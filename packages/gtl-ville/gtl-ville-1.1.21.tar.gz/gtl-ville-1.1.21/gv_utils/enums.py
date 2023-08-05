d07264cc4babb15b8ee661f03e40179badd0199c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package gtl-ville/1.1.21/gtl-ville-1.1.21.tar.gz

---
+++ packages/gtl-ville/gtl-ville-1.1.21.tar.gz/gtl-ville-1.1.21/gv_utils/enums.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from enum import Enum, unique
+from itertools import chain
+
+
+@unique
+class RedisData(Enum):
+    CLUSTER = 'cluster'
+    CP = 'cp'
+    CPS = 'cps'
+    SECTION = 'section'
+    SECTIONS = 'sections'
+    SECTIONIDS = 'section_ids'
+    ZONE = 'zone'
+    EID = 'eid'
+    SOURCE = 'source'
+    NAME = 'name'
+    GEOM = 'geom'
+    ATT = 'att'
+    DATA = 'data'
+    DATATIMESTAMP = 'data_timestamp'
+    VALIDFROM = 'valid_from'
+
+
+@unique
+class Metric(Enum):
+    SPEED = 'Speed'
+    TRAVELTIME = 'Travel time'
+    RELATIVESPEED = 'Relative speed'
+    CONFIDENCE = 'Confidence'
+    FLOW = 'Flow'
+    OCCUPANCY = 'Occupancy'
+    FLUIDITY = 'Fluidity'
+    DENSITY = 'Density'
+
+
+@unique
+class AttributeEnum(Enum):
+    NAME = 'Name'
+    FRC = 'Road category'
+    MAXSPEED = 'Maximum speed'
+    FREEFLOWSPEED = 'Free flow speed'
+
+
+# Enum cannot be extended
+Attribute = Enum('Attribute', [(e.name, e.value) for e in chain(Metric, AttributeEnum)])
+
+
+@unique
+class Source(Enum):
+    TOMTOMFCD = 'TomTom FCD'
+    METROPME = 'Metro PME'
+    SECTIONS = 'Grenoble sections'
+    ZONES = 'Grenoble zones'
+    CLUSTER = 'Grenoble clusters'
+
+
+@unique
+class Channel(Enum):
+    TOMTOMFCD = 1
+    METROPME = 2
+    SECTIONS = 3
+    ZONES = 4
+    CLUSTER = 5
