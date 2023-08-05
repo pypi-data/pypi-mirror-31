65506f60b5dd166f630f12d5ae726992e3b2b4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:27 +0000

    Add package http-server-base/1.0.2/http-server-base-1.0.2.tar.gz

---
+++ packages/http-server-base/http-server-base-1.0.2.tar.gz/http-server-base-1.0.2/src/http_server_base/tools/logging.py
@@ -0,0 +1,23 @@
+import logging, logging.config
+import os
+import json
+
+def setup_logging \
+(
+    default_path='configs/logging.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
