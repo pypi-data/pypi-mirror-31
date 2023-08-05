8db439befde8fce80a34611ec3da59e9a5d611aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:40 +0000

    Add package infi.jira_cli/0.3.19/infi.jira_cli-0.3.19-py2-none-any.whl

---
+++ packages/infi.jira_cli/infi.jira_cli-0.3.19-py2-none-any.whl/infi/jira_cli/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.3.19"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.3.19)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f14bbae0d5335051a7e53c0312d056c08570e478'
+__git_head_subject__ = u'Finished Release v0.3.19'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-04-22 12:31:18'
