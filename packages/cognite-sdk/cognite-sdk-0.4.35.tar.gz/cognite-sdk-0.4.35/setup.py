1e3c27953ce8d0beb1d850a24d4fc321dc904b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:49 +0000

    Add package cognite-sdk/0.4.35/cognite-sdk-0.4.35.tar.gz

---
+++ packages/cognite-sdk/cognite-sdk-0.4.35.tar.gz/cognite-sdk-0.4.35/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+import cognite
+
+setup(name='cognite-sdk',
+      version=cognite.__version__,
+      description='Cognite API SDK for Python',
+      url='http://cognite-sdk-python.readthedocs.io/',
+      download_url='https://github.com/cognitedata/cognite-sdk-python/archive/{}.tar.gz'.format(cognite.__version__),
+      author='Erlend Vollset',
+      author_email='erlend.vollset@cognite.com',
+      packages=['cognite', 'cognite._protobuf_descriptors'],
+      install_requires=[
+          'requests',
+          'pandas',
+          'protobuf'
+      ],
+      zip_safe=False,
+      include_package_data=True)
