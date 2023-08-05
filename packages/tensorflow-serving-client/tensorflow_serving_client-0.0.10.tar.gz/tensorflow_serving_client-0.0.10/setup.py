61a0566a9329ec3d471e8b4edc832e6110717aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:15 +0000

    Add package tensorflow-serving-client/0.0.10/tensorflow_serving_client-0.0.10.tar.gz

---
+++ packages/tensorflow-serving-client/tensorflow_serving_client-0.0.10.tar.gz/tensorflow_serving_client-0.0.10/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class BuildPackageProtos(install):
+    def run(self):
+        install.run(self)
+        from grpc.tools import command
+        command.build_package_protos('')
+
+
+setup(
+    name='tensorflow_serving_client',
+    version='0.0.10',
+    description='Python client for tensorflow serving',
+    author='Triage Technologies Inc.',
+    author_email='ai@triage.com',
+    url='https://triage.com',
+    license='MIT',
+    packages=[
+        'tensorflow_serving_client',
+        'tensorflow_serving_client.protos',
+    ],
+    scripts=[
+        'bin/tensorflow_serving_client',
+    ],
+    setup_requires=[
+        'cython',
+    ],
+    install_requires=[
+        'grpcio',
+        'grpcio-tools',
+        'keras-model-specs'
+    ],
+    cmdclass={
+        'install': BuildPackageProtos,
+        'develop': BuildPackageProtos,
+    },
+)
