c555ce438db763ae66a3c4f8672ef6111cbf90fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:07 +0000

    Add package zeugma/0.39/zeugma-0.39.tar.gz

---
+++ packages/zeugma/zeugma-0.39.tar.gz/zeugma-0.39/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+try:
+    import Cython
+except ImportError:
+    print('Cython is required during installation')
+    sys.exit(1)
+
+setup(name='zeugma',
+      packages=['zeugma'],
+      version='0.39',
+      license='MIT',
+      description="Unified framework for word embeddings (Word2Vec, GloVe, FastText, ...) compatible with scikit-learn Pipeline",
+      author='Nicolas Thiebaut',
+      author_email='nkthiebaut@gmail.com',
+      url='https://github.com/nkthiebaut',
+      download_url='https://github.com/nkthiebaut/zeugma/archive/0.39.tar.gz',
+      keywords=['embeddings'],
+      classifiers=[],
+      setup_requires=[
+          'pytest-runner',
+          'numpy>=1.13.3',
+          'Cython>=0.27.3',
+      ],
+      install_requires=[
+          'numpy>=1.13.3',
+          'Cython>=0.27.3',
+          'pandas>=0.20.3',
+          'gensim>=3.2.0',
+          'scikit_learn>=0.19.1',
+          'tensorflow>=1.5.0',
+          'keras>=2.1.3',
+          'fastText',
+      ],
+      tests_require=['pytest>=3.3.2'],
+      dependency_links=['git+https://github.com/facebookresearch/fastText.git'],
+      )
