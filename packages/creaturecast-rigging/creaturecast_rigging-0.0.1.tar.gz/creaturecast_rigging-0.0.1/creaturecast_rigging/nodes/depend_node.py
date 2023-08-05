3ddd9a06d1f0ea7a86d7b45a02ff8605bbffd8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:41 +0000

    Add package creaturecast-rigging/0.0.1/creaturecast_rigging-0.0.1.tar.gz

---
+++ packages/creaturecast-rigging/creaturecast_rigging-0.0.1.tar.gz/creaturecast_rigging-0.0.1/creaturecast_rigging/nodes/depend_node.py
@@ -0,0 +1,26 @@
+from creaturecast_node import creaturecast_node as nod
+
+
+class DependNode(nod.Node):
+
+    default_data = dict(
+        node_type=None,
+        suffix='dep',
+        side=0,
+        index=0,
+        size=1.0,
+        icon='depend_node',
+        base_type='depend_node'
+    )
+
+    auto_rename = False
+
+    def __init__(self, *args, **kwargs):
+        super(DependNode, self).__init__(*args, **kwargs)
+        self.m_object = None
+
+    def __str__(self):
+        return str(self.data['name'])
+
+    def __repr__(self):
+        return self.__str__()
