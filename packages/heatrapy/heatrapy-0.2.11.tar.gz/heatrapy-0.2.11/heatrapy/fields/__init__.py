d5ebd32b31f4137804fd093a8d35c2bfb1d75004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:20 +0000

    Add package heatrapy/0.2.11/heatrapy-0.2.11.tar.gz

---
+++ packages/heatrapy/heatrapy-0.2.11.tar.gz/heatrapy-0.2.11/heatrapy/fields/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""Fields.
+
+This submodule includes functions for the operating mode of systems.
+
+"""
+from operating_mode import operating_mode
+
+__all__ = [operating_mode]
---
+++ packages/heatrapy/heatrapy-0.2.11.tar.gz/heatrapy-0.2.11/heatrapy/mats/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""Mats.
+
+This submodule access the properties of materials
+
+"""
+
+from calmatpro import calmatpro
+
+__all__ = [calmatpro]
