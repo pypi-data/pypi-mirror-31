34f49116a5fcdd5b04c4f0d7f4e1700b113ec2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:28 +0000

    Add package django-zero/0.0.16/django_zero-0.0.16-py3.6.egg

---
+++ packages/django-zero/django_zero-0.0.16-py3.6.egg/django_zero/urls.py
@@ -0,0 +1,29 @@
+import logging
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django_zero import views
+
+urlpatterns = [
+    path(settings.ADMIN_URL.strip('/') + '/', admin.site.urls),
+]
+
+for app in settings.INSTALLED_APPS:
+    if app.startswith('apps.'):
+        urlpatterns.append(path('', include(app + '.urls')))
+
+urlpatterns.append(path('', include('allauth.urls')))
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+
+        urlpatterns = [path(r'__debug__/', include(debug_toolbar.urls))] + urlpatterns
+    except ImportError as exc:
+        logging.getLogger(__name__).warning('Could not import debug_toolbar, skipping.')
+
+if settings.ZERO_ENABLE_DEMO:
+    # Default example views
+    urlpatterns.append(path('', views.example_feature_list_view, name='example_feature_list'))
+    urlpatterns.append(path('<slug>', views.example_feature_detail_view, name='example_feature_detail'))
