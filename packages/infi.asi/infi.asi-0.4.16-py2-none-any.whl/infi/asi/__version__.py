149d2894d34887e86a2196dab986652ff85cf8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:41 +0000

    Add package infi.asi/0.4.16/infi.asi-0.4.16-py2-none-any.whl

---
+++ packages/infi.asi/infi.asi-0.4.16-py2-none-any.whl/infi/asi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.4.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b8ff08673b9c87c9f6a64db6a9024a2fbcea89fb'
+__git_head_subject__ = u'Finished Release v0.4.16'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-04-22 12:38:29'
