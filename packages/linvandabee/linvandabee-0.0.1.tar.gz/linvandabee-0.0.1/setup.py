c130354f9fd97313dafd8a532d3f57aea148fe77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:33 +0000

    Add package linvandabee/0.0.1/linvandabee-0.0.1.tar.gz

---
+++ packages/linvandabee/linvandabee-0.0.1.tar.gz/linvandabee-0.0.1/setup.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='linvandabee',
+    version='0.0.1',
+    find_packages=find_packages(),
+    include_package_data=True,
+)
