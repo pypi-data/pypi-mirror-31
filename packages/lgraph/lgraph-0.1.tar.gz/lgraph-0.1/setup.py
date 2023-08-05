ab79db7486f16947ac8cf0305e08151d38d00222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:53 +0000

    Add package lgraph/0.1/lgraph-0.1.tar.gz

---
+++ packages/lgraph/lgraph-0.1.tar.gz/lgraph-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lgraph',
+      version='0.1',
+      description='LiveGraph for synthesis and Simulation',
+      url='https://github.com/masc-ucsc/lgraph',
+      author='MASC Group',
+      author_email='lgraph-group@ucsc.edu',
+      license='BSD-3-Clause',
+      packages=['lgraph'],
+      zip_safe=False)
