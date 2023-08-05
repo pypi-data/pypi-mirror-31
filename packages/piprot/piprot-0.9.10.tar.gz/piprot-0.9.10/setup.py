246499df539b95c64c0842448222446243d1e237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:17 +0000

    Add package piprot/0.9.10/piprot-0.9.10.tar.gz

---
+++ packages/piprot/piprot-0.9.10.tar.gz/piprot-0.9.10/setup.py
@@ -0,0 +1,50 @@
+import sys, os
+from setuptools import setup
+
+"""
+Use pandoc to convert README.md to README.rst before uploading
+   pandoc README.md -o README.rst
+"""
+
+
+if 'publish' in sys.argv:
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='piprot',
+    version='0.9.10',
+    author='Brenton Cleeland',
+    author_email='brenton@brntn.me',
+    packages=['piprot', 'piprot.providers'],
+    url='http://github.com/sesh/piprot',
+    license='MIT License',
+    description='How rotten are your requirements?',
+    long_description=readme,
+    entry_points={
+        'console_scripts': [
+            'piprot = piprot.piprot:piprot',
+        ]
+    },
+    install_requires=[
+        'requests',
+        'requests-futures',
+        'six'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
