4d69fb747af00c86ac519ae705bbb9fb14c8188e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:47 +0000

    Add package speechpy-fast/2.4/speechpy-fast-2.4.tar.gz

---
+++ packages/speechpy-fast/speechpy-fast-2.4.tar.gz/speechpy-fast-2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='speechpy-fast',
+      version='2.4',
+      description='A fork of the python package for extracting speech features.',
+      author='Amirsina Torfi, Matthew Scholefield',
+      author_email='matthew331199@gmail.com',
+      url='https://github.com/matthewscholefield/speechpy',
+      download_url = 'https://github.com/matthewscholefield/speechpy/archive/2.4.zip',
+      packages=find_packages(exclude=('tests', 'docs')),
+      include_package_data=True,
+      install_requires=[
+          'scipy',
+          'numpy',
+          'backports.functools_lru_cache;python_version<"3.2"'
+      ],
+      zip_safe=False)
