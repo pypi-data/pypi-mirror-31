bbd3c2da48035b05ab6012d6341a1d671bc8f3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:10 +0000

    Add package pymaniprob/0.1/pymaniprob-0.1.tar.gz

---
+++ packages/pymaniprob/pymaniprob-0.1.tar.gz/pymaniprob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pymaniprob',
+      version='0.1',
+      author='Daniel Tait',
+      url='http://github.com/danieljtait/pymaniprob',
+      license='MIT',
+      packages=['pymaniprob'],
+      zip_safe=False)
