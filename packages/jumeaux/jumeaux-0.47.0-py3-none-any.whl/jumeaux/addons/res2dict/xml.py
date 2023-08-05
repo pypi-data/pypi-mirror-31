d2058224e4575afc98fa1e9baea0667875f15dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:38 +0000

    Add package jumeaux/0.47.0/jumeaux-0.47.0-py3-none-any.whl

---
+++ packages/jumeaux/jumeaux-0.47.0-py3-none-any.whl/jumeaux/addons/res2dict/xml.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+import xmltodict
+from owlmixin import OwlMixin, TList
+
+from jumeaux.addons.res2dict import Res2DictExecutor
+from jumeaux.models import Res2DictAddOnPayload
+from jumeaux.logger import Logger
+
+logger: Logger = Logger(__name__)
+LOG_PREFIX = "[res2dict/xml]"
+
+
+class Config(OwlMixin):
+    force: bool = False
+    mime_types: TList[str] = [
+        'text/xml', 'application/xml'
+    ]
+
+
+class Executor(Res2DictExecutor):
+    def __init__(self, config: dict) -> None:
+        self.config: Config = Config.from_dict(config or {})
+
+    def exec(self, payload: Res2DictAddOnPayload) -> Res2DictAddOnPayload:
+        if not payload.result.is_none() and not self.config.force:
+            logger.debug(f"{LOG_PREFIX} Skipped because result is nothing.")
+            return payload
+
+        mime_type: str = payload.response.mime_type.get()
+
+        result: dict
+        if self.config.force:
+            logger.debug(f"{LOG_PREFIX} Force to convert to dict as xml")
+            result = xmltodict.parse(payload.response.text)
+        elif mime_type in self.config.mime_types:
+            logger.debug(f"{LOG_PREFIX} Convert to dict as xml becuase mime-type is one of {self.config.mime_types}.")
+            result = xmltodict.parse(payload.response.text)
+        else:
+            logger.debug(f"{LOG_PREFIX} Skipped because mime-type is not one of {self.config.mime_types}")
+            result = None
+
+        return Res2DictAddOnPayload.from_dict({
+            "response": payload.response,
+            "result": result
+        })
