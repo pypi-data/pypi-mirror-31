d411e7928048b3ad1686c7aa1ca91343c64c1bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package peerplays/0.1.20/peerplays-0.1.20.tar.gz

---
+++ packages/peerplays/peerplays-0.1.20.tar.gz/peerplays-0.1.20/peerplays/event.py
@@ -0,0 +1,52 @@
+from peerplays.instance import BlockchainInstance
+from .exceptions import EventDoesNotExistException
+from .blockchainobject import BlockchainObject
+
+
+class Event(BlockchainObject):
+    """ Read data about an event on the chain
+
+        :param str identifier: Identifier
+        :param peerplays blockchain_instance: PeerPlays() instance to use when
+            accesing a RPC
+
+    """
+    type_id = 18
+
+    def refresh(self):
+        data = self.blockchain.rpc.get_object(self.identifier)
+        if not data:
+            raise EventDoesNotExistException(self.identifier)
+        super(Event, self).__init__(data)
+        self.cached = True
+
+    @property
+    def eventgroup(self):
+        from .eventgroup import EventGroup
+        return EventGroup(self["event_group_id"])
+
+    @property
+    def bettingmarketgroups(self):
+        from .bettingmarketgroup import BettingMarketGroups
+        return BettingMarketGroups(self["id"])
+
+    def set_status(self, status, scores=[], **kwargs):
+        return self.blockchain.event_update_status(
+            self["id"],
+            status,
+            scores=scores,
+            **kwargs
+        )
+
+
+class Events(list):
+    """ List of all available events in an eventgroup
+    """
+    def __init__(self, eventgroup_id, *args, **kwargs):
+        BlockchainInstance.__init__(self, *args, **kwargs)
+        self.events = self.blockchain.rpc.list_events_in_group(eventgroup_id)
+
+        super(Events, self).__init__([
+            Event(x, lazy=True, blockchain_instance=self.blockchain)
+            for x in self.events
+        ])
