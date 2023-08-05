1df77f5f13d6f2a3da3a24df3d46bbf8973d8586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:41 +0000

    Add package deepaffects/1.1.0/deepaffects-1.1.0.tar.gz

---
+++ packages/deepaffects/deepaffects-1.1.0.tar.gz/deepaffects-1.1.0/setup.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+from pypandoc import convert
+
+NAME = "deepaffects"
+VERSION = "1.1.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "pymediainfo >= 2.1.9"]
+
+def readme():
+    return convert('README.md', 'rst')
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="DeepAffects APIs",
+    author_email="support@seernet.io",
+    url="https://github.com/SEERNET/deepaffects-python",
+    author="Sushant Hiray, Venkatesh Duppada",
+    setup_requires=[
+        "six >= 1.3.0",
+        "pillow >= 2.8.1"
+    ],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=readme()
+)
