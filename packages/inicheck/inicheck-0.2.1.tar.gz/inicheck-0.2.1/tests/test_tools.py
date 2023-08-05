39b15f7abdc53bee1fc4630984e44bbc25f63d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package inicheck/0.2.1/inicheck-0.2.1.tar.gz

---
+++ packages/inicheck/inicheck-0.2.1.tar.gz/inicheck-0.2.1/tests/test_tools.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `inicheck.tools` module."""
+
+import pytest
+
+from inicheck import inicheck
+
+def test_config_documentation(inicheck.__CoreConfig.ini):
