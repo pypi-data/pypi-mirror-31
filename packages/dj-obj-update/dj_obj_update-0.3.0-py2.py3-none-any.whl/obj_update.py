784de7489609ddd2a8a9280c2557cdee8bd31753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package dj-obj-update/0.3.0/dj_obj_update-0.3.0-py2.py3-none-any.whl

---
+++ packages/dj-obj-update/dj_obj_update-0.3.0-py2.py3-none-any.whl/obj_update.py
@@ -0,0 +1,101 @@
+from operator import itemgetter
+import logging
+
+
+__all__ = ['obj_update', 'obj_update_or_create']
+__version__ = '0.3.0'
+
+
+DIRTY = '_is_dirty'
+
+
+logger = logging.getLogger('obj_update')
+
+
+def set_field(obj, field_name, value):
+    """Fancy setattr with debugging."""
+    old = getattr(obj, field_name)
+    # is_relation is Django 1.8 only
+    if obj._meta.get_field(field_name).is_relation:
+        # If field_name is the `_id` field, then there is no 'pk' attr and
+        # old/value *is* the pk
+        old_repr = None if old is None else getattr(old, 'pk', old)
+        new_repr = None if value is None else getattr(value, 'pk', value)
+    else:
+        old_repr = None if old is None else str(old)
+        new_repr = None if value is None else str(value)
+    if old_repr != new_repr:
+        setattr(obj, field_name, value)
+        if not hasattr(obj, DIRTY):
+            setattr(obj, DIRTY, [])
+        getattr(obj, DIRTY).append(dict(
+            field_name=field_name,
+            old_value=old_repr,
+            new_value=new_repr,
+        ))
+
+
+def human_log_formatter(dirty_data):
+    return ''.join(
+        '[{field_name} {old_value}->{new_value}]'
+        .format(**x) for x in dirty_data
+    )
+
+
+def json_log_formatter(dirty_data):
+    return {x['field_name']: {'old': x['old_value'], 'new': x['new_value']}
+            for x in dirty_data}
+
+
+def obj_update(obj, data, *, save=True):
+    """
+    Fancy way to update `obj` with `data` dict.
+
+    Parameters
+    ----------
+    obj : Django model instance
+    data : dict
+        The data to update ``obj`` with
+    save : bool
+        If save=False, then don't actually save. This can be useful if you
+        just want to utilize the verbose logging.
+
+    Returns
+    -------
+    bool
+        True if data changed
+    """
+    for field_name, value in data.items():
+        set_field(obj, field_name, value)
+    dirty_data = getattr(obj, DIRTY, None)
+    if not dirty_data:
+        return False
+
+    logger.debug(
+        human_log_formatter(dirty_data),
+        extra={
+            'model': obj._meta.object_name,
+            'pk': obj.pk,
+            'changes': json_log_formatter(dirty_data),
+        }
+    )
+    update_fields = list(map(itemgetter('field_name'), dirty_data))
+    if save:
+        obj.save(update_fields=update_fields)
+    delattr(obj, DIRTY)
+    return True
+
+
+def obj_update_or_create(model, defaults=None, **kwargs):
+    """
+    Mimic queryset.update_or_create but using obj_update.
+    """
+    obj, created = model.objects.get_or_create(defaults=defaults, **kwargs)
+    if created:
+        logger.debug('CREATED %s %s',
+                     model._meta.object_name,
+                     obj.pk,
+                     extra={'pk': obj.pk})
+    else:
+        obj_update(obj, defaults)
+    return obj, created
