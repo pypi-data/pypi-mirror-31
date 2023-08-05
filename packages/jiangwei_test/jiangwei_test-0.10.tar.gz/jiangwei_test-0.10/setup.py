05129b15d32e6c9fae215e24f37ebb3a4b83c08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:28 +0000

    Add package jiangwei_test/0.10/jiangwei_test-0.10.tar.gz

---
+++ packages/jiangwei_test/jiangwei_test-0.10.tar.gz/jiangwei_test-0.10/setup.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+from setuptools import setup, find_packages  
+  
+setup(  
+      name='jiangwei_test',   #名称  
+      version='0.10',  #版本  
+      description="a console translation dictionary used dict.baidu.com Api", #描述  
+      keywords='python english translation dictionary terminal',  
+      author='蒋为',  #作者  
+      author_email='ok@xjiangwei.cn', #作者邮箱  
+      url='http://www.xjiangwei.cn', #作者链接  
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),  
+      include_package_data=True,  
+      zip_safe=False,  
+      install_requires=[      #需求的第三方模块  
+        'requests',  
+      ],  
+      entry_points={  
+        'console_scripts':[     #如果你想要以Linux命令的形式使用  
+            'bword = bword.bword:main'      
+        ]  
+      },  
+)  
