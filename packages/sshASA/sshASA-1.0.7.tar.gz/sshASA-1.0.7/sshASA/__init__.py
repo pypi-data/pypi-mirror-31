54ebede5971f10c9783fec4942ecfa9d0fcac2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package sshASA/1.0.7/sshASA-1.0.7.tar.gz

---
+++ packages/sshASA/sshASA-1.0.7.tar.gz/sshASA-1.0.7/sshASA/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys, socket, getpass
+from netmiko import ConnectHandler, NetMikoAuthenticationException
+
+def sshHost(hostname):
+    '''Cisco ASA ssh module based on netmiko. Example: "python3 sshHostModuleASA.py mydevice.lab"'''
+    username = input("Username: ")
+    password = getpass.getpass()
+    enable = getpass.getpass(prompt='Enable password: ')
+    hostIp = socket.gethostbyname(hostname)
+    try:
+        sshSession = ConnectHandler(device_type='cisco_asa', ip=hostIp, username=username, password=password, secret=enable)
+    except NetMikoAuthenticationException:
+        print('Authentication failed! Please retry')
+        sys.exit(1)
+    except ValueError:
+        print('Enable password wrong! Please retry')
+        sys.exit(1)
+    else:
+        versionHostname = sshSession.send_command("sh running-config | i ^hostname")
+        print('Session established with Hostname:{} IP:{}'.format(versionHostname.replace("hostname ","").strip(), hostIp))
+        return sshSession
+
+if __name__ == "__main__":
+    try:
+        sshHost(sys.argv[1])
+    except IndexError:
+        print('Please, specify hostname device. Example: "python3 sshHostModuleEOS.py mydevice.lab"')
