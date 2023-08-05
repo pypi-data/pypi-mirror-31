318094b1d466f825e3d19926f5c9a742001dc6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:46 +0000

    Add package zipf/1.0.4/zipf-1.0.4.tar.gz

---
+++ packages/zipf/zipf-1.0.4.tar.gz/zipf-1.0.4/zipf/factories/__init__.py
@@ -0,0 +1,5 @@
+from .zipf_from_list import zipf_from_list
+from .zipf_from_text import zipf_from_text
+from .zipf_from_file import zipf_from_file
+from .zipf_from_url import zipf_from_url
+from .zipf_from_dir import zipf_from_dir
