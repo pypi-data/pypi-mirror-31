1ffce9fbf4cec3329765cee9fc74fc8bf9b67396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package DustCli/0.4.3/DustCli-0.4.3.tar.gz

---
+++ packages/DustCli/DustCli-0.4.3.tar.gz/DustCli-0.4.3/DustCli/utils/dust_exec.py
@@ -0,0 +1,13 @@
+
+class DustExec:
+
+    @staticmethod
+    def exec(string, params, error_handler):
+        exec(string, globals())
+        main = globals().get('dust_main', None)
+        if not main:
+            error_handler('没有找到 dust_main 入口')
+            return None
+        return main(*params)
+
+
