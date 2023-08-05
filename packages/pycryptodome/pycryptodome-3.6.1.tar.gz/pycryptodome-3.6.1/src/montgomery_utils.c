446704d5e6296f4bfa482db2e3095cbb15337bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package pycryptodome/3.6.1/pycryptodome-3.6.1.tar.gz

---
+++ packages/pycryptodome/pycryptodome-3.6.1.tar.gz/pycryptodome-3.6.1/src/montgomery_utils.c
@@ -0,0 +1,157 @@
+#include "montgomery_utils.h"
+#include "siphash.h"
+
+#if 0
+static void print_words_w(const uint8_t *str, const uint64_t *x, unsigned words)
+{
+    int i;
+    printf("%s = 0x", str);
+    for (i=words-1; i>=0; i--) {
+        printf("%016" PRIx64, x[i]);
+    }
+    printf("\n");
+}
+#endif
+
+void bytes_to_words(uint64_t *x, const uint8_t *in, size_t len, size_t words)
+{
+    int i, j;
+    size_t partial;
+
+    if (words == 0 || len == 0) {
+        return;
+    }
+
+    assert(len<=words*8);
+    assert(len>(words-1)*8);
+
+    memset(x, 0, words*8);
+
+    partial = len % 8;
+    if (partial == 0) {
+        partial = 8;
+    }
+
+    for (j=0; j<partial; j++) {
+        x[words-1] = (x[words-1] << 8) | *in++;
+    }
+
+    if (words == 1) {
+        return;
+    }
+
+    for (i=words-2; i>=0; i--) {
+        for (j=0; j<8; j++) {
+            x[i] = (x[i] << 8) | *in++;
+        }
+    }
+}
+
+void words_to_bytes(uint8_t *out, const uint64_t *x, size_t len, size_t words)
+{
+    int i, j;
+    size_t partial;
+
+    if (words == 0 || len == 0) {
+        return;
+    }
+
+    assert(len<=words*8);
+    assert(len>(words-1)*8);
+
+    partial = len % 8;
+    if (partial == 0) {
+        partial = 8;
+    }
+
+    for (j=partial-1; j>=0; j--) {
+        *out++ = (uint8_t)(x[words-1] >> (8*j));
+    }
+
+    if (words == 1) {
+        return;
+    }
+
+    for (i=words-2; i>=0; i--) {
+        for (j=7; j>=0; j--) {
+            *out++ = x[i] >> (8*j);
+        }
+    }
+}
+
+void expand_seed(uint64_t seed_in, uint8_t* seed_out, size_t out_len)
+{
+    uint8_t counter[4];
+    int i;
+
+#define SIPHASH_LEN 16
+    
+    for (i=0 ;; i++, out_len-=SIPHASH_LEN) {
+        counter[0] = i;
+        counter[1] = i>>8;
+        counter[2] = i>>16;
+        counter[3] = i>>24;
+        if (out_len<SIPHASH_LEN)
+            break;
+        siphash(counter, 4, (const uint8_t*)&seed_in, seed_out, SIPHASH_LEN);
+        seed_out += 16;
+    }
+
+    if (out_len>0) {
+        uint8_t buffer[SIPHASH_LEN];
+        siphash(counter, 4, (const uint8_t*)&seed_in, buffer, SIPHASH_LEN);
+        memcpy(seed_out, buffer, out_len);
+    }
+
+#undef SIPHASH_LEN
+}
+
+struct BitWindow init_bit_window(int window_size, const uint8_t *exp, int exp_len)
+{
+    struct BitWindow bw;
+
+    bw.window_size = window_size;
+    bw.nr_windows = (exp_len*8+window_size-1)/window_size;
+
+    bw.tg = (exp_len*8) % window_size;
+    if (bw.tg == 0) {
+        bw.tg = window_size;
+    }
+
+    bw.available = 8;
+    bw.scan_exp = 0;
+    bw.exp = exp;
+
+    return bw;
+}
+
+int get_next_digit(struct BitWindow *bw)
+{
+    int tc, index;
+
+    /** Possibly move to the next byte **/
+    if (bw->available == 0) {
+        bw->available = 8;
+        bw->scan_exp++;
+    }
+
+    /** Try to consume as much as possible from the current byte **/
+    tc = MIN(bw->tg, bw->available);
+    
+    index = (bw->exp[bw->scan_exp] >> (bw->available - tc)) & ((1 << tc) - 1);
+    
+    bw->available -= tc;
+    bw->tg -= tc;
+        
+    /** A few bits (<8) might still be needed from the next byte **/
+    if (bw->tg > 0) {
+        bw->scan_exp++;
+        index = (index << bw->tg) | (bw->exp[bw->scan_exp] >> (8 - bw->tg));
+        bw->available = 8 - bw->tg;
+    }
+
+    bw->tg = bw->window_size;
+
+    return index;
+}
+
---
+++ packages/pycryptodome/pycryptodome-3.6.1.tar.gz/pycryptodome-3.6.1/src/multiply.h
@@ -0,0 +1,66 @@
+#include "pycrypto_common.h"
+
+/**
+ * Double-precision multiplication
+ */
+#if defined(HAVE_UINT128)
+
+#define DP_MULT(a,b,ol,oh) do { \
+    __uint128_t pr;             \
+    pr = (__uint128_t)(a)*(b);  \
+    ol = (__uint128_t)pr;       \
+    oh = pr >> 64;              \
+    } while (0)
+
+#elif defined(_MSC_VER) && defined(_WIN64)
+
+#include <windows.h>
+#define DP_MULT(a,b,ol,oh) do { ol = UnsignedMultiply128(a,b,&oh); } while (0)
+
+#else
+
+uint64_t static inline dp_mult_128_32(uint64_t a, uint64_t b, uint64_t *oh)
+#if defined(__GNUC__) && !defined(__clang__)
+__attribute__((optimize("-O3")))
+#endif
+;
+
+uint64_t static inline dp_mult_128_32(uint64_t a, uint64_t b, uint64_t *oh)
+{
+    uint32_t al = (uint32_t) a;
+    uint32_t ah = a >> 32;
+    uint32_t bl = (uint32_t) b;
+    uint32_t bh = b >> 32;
+
+    uint64_t sum0, sum1a, sum1b, sum2, sum3;
+
+    sum0 = (uint64_t)al*bl;
+    sum1a = (uint64_t)al*bh;
+    sum1b = (uint64_t)ah*bl;
+    sum2 = (uint64_t)ah*bh;
+
+    sum1a += sum0 >> 32;
+    sum1b += sum1a;
+    sum3 = sum1b < sum1a;
+    sum2 += sum1b >> 32;
+    sum3 += sum2 >> 32;
+
+    *oh = (sum3 << 32) + (uint32_t)sum2;
+    return (sum1b << 32) + (uint32_t)sum0;
+}
+
+#define DP_MULT(a,b,ol,oh) do { ol = dp_mult_128_32(a,b,&oh); } while (0)
+
+#endif
+
+size_t square_w(uint64_t *t, const uint64_t *a, size_t words)
+#if defined(__GNUC__) && !defined(__clang__)
+__attribute__((optimize("-O3")))
+#endif
+;
+
+size_t addmul128(uint64_t * RESTRICT t, const uint64_t * RESTRICT a, uint64_t b0, uint64_t b1, size_t words)
+#if defined(__GNUC__) && !defined(__clang__)
+__attribute__((optimize("-O3")))
+#endif
+;
