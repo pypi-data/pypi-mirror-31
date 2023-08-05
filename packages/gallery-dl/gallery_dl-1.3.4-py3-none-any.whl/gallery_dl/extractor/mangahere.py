3a0d1906508ffdba801553b3399f83ae531604ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package gallery-dl/1.3.4/gallery_dl-1.3.4-py3-none-any.whl

---
+++ packages/gallery-dl/gallery_dl-1.3.4-py3-none-any.whl/gallery_dl/extractor/mangahere.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015-2018 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract manga-chapters and entire manga from http://www.mangahere.co/"""
+
+from .common import ChapterExtractor, MangaExtractor
+from .. import text, util
+from urllib.parse import urljoin
+import re
+
+
+class MangahereMangaExtractor(MangaExtractor):
+    """Extractor for manga from mangahere.co"""
+    category = "mangahere"
+    pattern = [r"(?:https?://)?(?:www\.|m\.)?mangahere\.c[co]/manga/"
+               r"([^/]+)/?(?:#.*)?$"]
+    test = [
+        ("http://www.mangahere.cc/manga/aria/", {
+            "url": "e8971b1605d9888d978ebb2895adb1c7c37d663c",
+            "keyword": "951eef36a3775525a31ca78c9d9cea546f4cf2f5",
+        }),
+        ("http://www.mangahere.cc/manga/hiyokoi#50", {
+            "url": "6df27c0e105d9ee0b78a7aa77340d0891e6c7fc6",
+            "keyword": "9542283639bd082fabf3a14b6695697d3ef15111",
+        }),
+        ("http://www.mangahere.co/manga/aria/", None),
+        ("http://m.mangahere.co/manga/aria/", None),
+    ]
+
+    def __init__(self, match):
+        url = "http://www.mangahere.cc/manga/" + match.group(1) + "/"
+        MangaExtractor.__init__(self, match, url)
+
+    def chapters(self, page):
+        results = []
+        pos = page.index('<div class="detail_list">')
+        manga, pos = text.extract(page, '<h3>Read ', ' Online</h3>', pos)
+        manga = text.unescape(manga)
+
+        while True:
+            url, pos = text.extract(
+                page, '<a class="color_0077" href="', '"', pos)
+            if not url:
+                return results
+            chapter, dot, minor = url[:-1].rpartition("/c")[2].partition(".")
+            volume, pos = text.extract(page, 'span class="mr6">', '<', pos)
+            title, pos = text.extract(page, '/span>', '<', pos)
+            date, pos = text.extract(page, 'class="right">', '</span>', pos)
+            results.append((urljoin("http:", url), {
+                "manga": manga, "title": title, "date": date,
+                "volume": util.safe_int(volume.rpartition(" ")[2]),
+                "chapter": util.safe_int(chapter),
+                "chapter_minor": dot + minor,
+                "lang": "en", "language": "English",
+            }))
+
+
+class MangahereChapterExtractor(ChapterExtractor):
+    """Extractor for manga-chapters from mangahere.co"""
+    category = "mangahere"
+    pattern = [(r"(?:https?://)?(?:www\.|m\.)?mangahere\.c[co]/manga/"
+                r"([^/]+(?:/v0*(\d+))?/c0*(\d+)(\.\d+)?)")]
+    test = [
+        ("http://www.mangahere.cc/manga/dongguo_xiaojie/c003.2/", {
+            "keyword": "0c263b83f803524baa8717d2b4d841617aa8d775",
+            "content": "dd8454469429c6c717cbc3cad228e76ef8c6e420",
+            "options": (("verify", False),),
+        }),
+        ("http://www.mangahere.co/manga/dongguo_xiaojie/c003.2/", None),
+        ("http://m.mangahere.co/manga/dongguo_xiaojie/c003.2/", None),
+    ]
+    url_fmt = "http://www.mangahere.cc/manga/{}/{}.html"
+
+    def __init__(self, match):
+        self.part, self.volume, self.chapter, self.chminor = match.groups()
+        # remove ".html" for the first chapter page to avoid redirects
+        url = self.url_fmt.format(self.part, "")[:-5]
+        ChapterExtractor.__init__(self, url)
+
+    def get_metadata(self, page):
+        """Collect metadata for extractor-job"""
+        manga, pos = text.extract(page, '<title>', '</title>')
+        chid , pos = text.extract(page, '.net/store/manga/', '/', pos)
+        pages, pos = text.extract(page, ' class="wid60"', '</select>', pos)
+        count = re.findall(r">(\d+)<", pages)[-1]
+        manga = re.match((r"(.+) \d+(\.\d+)? - Read .+ Chapter "
+                          r"\d+(\.\d+)? Online"), manga).group(1)
+        return {
+            "manga": text.unescape(manga),
+            # "title": TODO,
+            "volume": util.safe_int(self.volume),
+            "chapter": util.safe_int(self.chapter),
+            "chapter_minor": self.chminor or "",
+            "chapter_id": util.safe_int(chid),
+            "count": util.safe_int(count),
+            "lang": "en",
+            "language": "English",
+        }
+
+    def get_images(self, page):
+        """Yield all image-urls for this chapter"""
+        pnum = 1
+        while True:
+            url, pos = text.extract(page, '<img src="', '"')
+            yield url, None
+            _  , pos = text.extract(page, '<img src="', '"', pos)
+            _  , pos = text.extract(page, '<img src="', '"', pos)
+            url, pos = text.extract(page, '<img src="', '"', pos)
+            yield url, None
+
+            pnum += 2
+            page = self.request(self.url_fmt.format(self.part, pnum)).text
---
+++ packages/gallery-dl/gallery_dl-1.3.4-py3-none-any.whl/gallery_dl/text.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015-2018 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Collection of functions that work in strings/text"""
+
+import sys
+import re
+import os.path
+import html
+import urllib.parse
+
+
+INVALID_XML_CHARS = (1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 14, 15, 16, 17, 18,
+                     19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31)
+
+
+def clean_xml(xmldata, repl=""):
+    """Replace/Remove invalid control characters in XML data"""
+    for char in INVALID_XML_CHARS:
+        char = chr(char)
+        if char in xmldata:
+            xmldata = xmldata.replace(char, repl)
+    return xmldata
+
+
+def remove_html(text):
+    """Remove html-tags from a string"""
+    return " ".join(re.sub("<[^>]+?>", " ", text).split())
+
+
+def filename_from_url(url):
+    """Extract the last part of an url to use as a filename"""
+    try:
+        return urllib.parse.urlsplit(url).path.rpartition("/")[2]
+    except ValueError:
+        return url
+
+
+def nameext_from_url(url, data=None):
+    """Extract the last part of an url and fill 'data' accordingly"""
+    if data is None:
+        data = {}
+    data["filename"] = unquote(filename_from_url(url))
+    data["name"], ext = os.path.splitext(data["filename"])
+    data["extension"] = ext[1:].lower()
+    return data
+
+
+def clean_path_windows(path):
+    """Remove illegal characters from a path-segment (Windows)"""
+    try:
+        return re.sub(r'[<>:"\\/|?*]', "_", path)
+    except TypeError:
+        return path
+
+
+def clean_path_posix(path):
+    """Remove illegal characters from a path-segment (Posix)"""
+    try:
+        return path.replace("/", "_")
+    except AttributeError:
+        return path
+
+
+def shorten_path(path, limit=255, encoding=sys.getfilesystemencoding()):
+    """Shorten a path segment to at most 'limit' bytes"""
+    return (path.encode(encoding)[:limit]).decode(encoding, "ignore")
+
+
+def shorten_filename(fname, limit=255, encoding=sys.getfilesystemencoding()):
+    """Shorten filename to at most 'limit' bytes while preserving extension"""
+    name, extension = os.path.splitext(fname)
+    bext = extension.encode(encoding)
+    bname = name.encode(encoding)[:limit-len(bext)]
+    return bname.decode(encoding, "ignore") + extension
+
+
+def extract(txt, begin, end, pos=0):
+    """Extract the text between 'begin' and 'end' from 'txt'
+
+    Args:
+        txt: String to search in
+        begin: First string to be searched for
+        end: Second string to be searched for after 'begin'
+        pos: Starting position for searches in 'txt'
+
+    Returns:
+        The string between the two search-strings 'begin' and 'end' beginning
+        with position 'pos' in 'txt' as well as the position after 'end'.
+
+        If at least one of 'begin' or 'end' is not found, None and the original
+        value of 'pos' is returned
+
+    Examples:
+        extract("abcde", "b", "d")    -> "c" , 4
+        extract("abcde", "b", "d", 3) -> None, 3
+    """
+    try:
+        first = txt.index(begin, pos) + len(begin)
+        last = txt.index(end, first)
+        return txt[first:last], last+len(end)
+    except ValueError:
+        return None, pos
+
+
+def extract_all(txt, rules, pos=0, values=None):
+    """Calls extract for each rule and returns the result in a dict"""
+    if values is None:
+        values = {}
+    for key, begin, end in rules:
+        result, pos = extract(txt, begin, end, pos)
+        if key:
+            values[key] = result
+    return values, pos
+
+
+def extract_iter(txt, begin, end, pos=0):
+    """Yield all values obtained by repeated calls to text.extract"""
+    while True:
+        value, pos = extract(txt, begin, end, pos)
+        if value is None:
+            return
+        yield value
+
+
+def parse_query(qs):
+    """Parse a query string into key-value pairs"""
+    result = {}
+    for key, value in urllib.parse.parse_qsl(qs):
+        if key not in result:
+            result[key] = value
+    return result
+
+
+if os.name == "nt":
+    clean_path = clean_path_windows
+else:
+    clean_path = clean_path_posix
+
+unquote = urllib.parse.unquote
+escape = html.escape
+
+try:
+    unescape = html.unescape
+except AttributeError:
+    import html.parser
+    unescape = html.parser.HTMLParser().unescape
