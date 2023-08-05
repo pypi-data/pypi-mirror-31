fe32717d7b89ed014580706e41bca4c92dd1d00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:34 +0000

    Add package progress-reporter/1.4.1/progress-reporter-1.4.1.tar.gz

---
+++ packages/progress-reporter/progress-reporter-1.4.1.tar.gz/progress-reporter-1.4.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import versioneer
+
+CLASSIFIERS = \
+"""Development Status :: 5 - Production/Stable
+Environment :: Console
+Environment :: Web Environment
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Natural Language :: English
+Operating System :: POSIX
+Operating System :: Microsoft :: Windows
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Topic :: Software Development :: User Interfaces
+Topic :: Software Development :: Widget Sets
+Topic :: Utilities"""
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+kw = dict(name='progress-reporter',
+          long_description=long_description,
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass(),
+          classifiers=[c for c in CLASSIFIERS.split('\n')],
+          install_requires=['tqdm>=4.23'],
+          packages=find_packages(),
+          package_data={'progress_reporter.tests': ['*.ipynb']},
+          url='https://github.com/marscher/progress_reporter',
+          keywords=['progress', 'reporting', 'eta', 'gui'],
+          license='MIT',
+          )
+if __name__ == '__main__':
+    setup(**kw)
