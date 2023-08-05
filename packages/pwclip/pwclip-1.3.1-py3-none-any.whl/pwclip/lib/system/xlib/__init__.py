1e3352b1ebe6deeff232ecaf49818f3cf74f1d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:08 +0000

    Add package pwclip/1.3.1/pwclip-1.3.1-py3-none-any.whl

---
+++ packages/pwclip/pwclip-1.3.1-py3-none-any.whl/pwclip/lib/system/xlib/__init__.py
@@ -0,0 +1,5 @@
+"""x window related functions"""
+from system.xlib.xinput import xinput, xgetpass
+from system.xlib.xmsgok import xmsgok
+from system.xlib.xyesno import xyesno
+from system.xlib.xnotify import xnotify
