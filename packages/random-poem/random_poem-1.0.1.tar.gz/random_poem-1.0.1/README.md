5a45f5b590b731ad97233aefe9d43b85fef1be9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package random-poem/1.0.1/random_poem-1.0.1.tar.gz

---
+++ packages/random-poem/random_poem-1.0.1.tar.gz/random_poem-1.0.1/README.md
@@ -0,0 +1,17 @@
+# random_poem
+A python package that returns a random poem from [poetryfoundation.org](poetryfoundation.org)
+
+## installation
+This package can be installed using pip 
+`pip install random_poem`
+
+## usage
+
+    import random_poem
+    random_poem.get_poem()
+    
+The call will return a random poem as a `\n` delimited string or `None` if it is not able to find one
+
+## license
+
+random_poem is distributed under the MIT license 
---
+++ packages/random-poem/random_poem-1.0.1.tar.gz/random_poem-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='random_poem',
+    version='1.0.1',
+    description='Returns a random poem from poetry foundation',
+	url='https://github.com/kartikye/random_poem/',
+	author='Kartikye Mittal',
+    author_email='kartikye.mittal+random_poem@gmail.com',
+    license='MIT',
+    packages=['random_poem'],
+    zip_safe=False,
+    python_requires='>=3',
+    install_requires=[
+		'beautifulsoup4',
+		'requests'
+	])
