a60a13b588e03b1e6aa1674bac1eb948763b1311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package pysmFISH/0/pysmFISH-0.0.5.tar.gz

---
+++ packages/pysmFISH/pysmFISH-0.0.5.tar.gz/pysmFISH-0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.0"
+exec(open('pysmFISH/_version.py').read())
+
+setup(
+    name="pysmFISH",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-image',
+        'scikit-learn',
+        'matplotlib',
+        'nd2reader==2.1.3',
+        'sympy',
+        'loompy',
+        'ruamel.yaml',
+        'codecs',
+        'h5py',
+        'dask',
+        'distributed==1.18.3',
+        'mpi4py',
+
+        
+    ],
+    entry_points='''
+        [console_scripts]
+        add_coords_to_experimental_metadata=add_coords_to_experimental_metadata
+        apply_stitching=apply_stitching
+        dots_coords_correction=dots_coords_correction
+        preprocessing_script=preprocessing_script
+        process_standalone_experiment=process_standalone_experiment
+        reference_registration=reference_registration
+        Run_dots_counting=Run_dots_counting
+        staining_segmentation=staining_segmentation
+    ''',
+
+    author="Simone Codeluppi",
+    author_email="simone.codeluppi@gmail.com",
+    keywords=["spatial transcriptomics", "singlecell", "bioinformatics", "transcriptomics"],
+    description="Analysis of large spatial transcriptomics data (osmFISH).",
+    license="MIT",
+    url="https://linnarssonlab.org/osmFISH/",
+)
