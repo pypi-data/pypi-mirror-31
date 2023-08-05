52c49eddc9449301ac81fce97e5f658af016510f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:24 +0000

    Add package quilt/2.9.4/quilt-2.9.4.tar.gz

---
+++ packages/quilt/quilt-2.9.4.tar.gz/quilt-2.9.4/quilt/test/test_import.py
@@ -0,0 +1,349 @@
+"""
+Tests for magic imports.
+"""
+
+import os
+import time
+
+import pandas as pd
+from six import string_types
+
+from quilt.nodes import GroupNode, DataNode
+from quilt.tools import command
+from quilt.tools.const import PACKAGE_DIR_NAME
+from quilt.tools.package import Package, PackageException
+from quilt.tools.store import PackageStore
+from .utils import patch, QuiltTestCase
+
+class ImportTest(QuiltTestCase):
+    def test_imports(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('foo/package', build_path)
+
+        # Good imports
+
+        from quilt.data.foo import package
+        from quilt.data.foo.package import dataframes
+        from quilt.data.foo.package import README
+
+        # Contents of the imports
+
+        assert isinstance(package, GroupNode)
+        assert isinstance(dataframes, GroupNode)
+        assert isinstance(dataframes.csv, DataNode)
+        assert isinstance(README, DataNode)
+
+        assert package.dataframes == dataframes
+        assert package.README == README
+
+        assert set(dataframes._keys()) == {'csv', 'nulls'}
+        assert set(dataframes._group_keys()) == set()
+        assert set(dataframes._data_keys()) == {'csv', 'nulls'}
+
+        assert isinstance(README(), string_types)
+        assert isinstance(README._data(), string_types)
+        assert isinstance(dataframes.csv(), pd.DataFrame)
+        assert isinstance(dataframes.csv._data(), pd.DataFrame)
+
+        str(package)
+        str(dataframes)
+        str(README)
+
+        # Bad attributes of imported packages
+
+        with self.assertRaises(AttributeError):
+            package.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.csv.foo
+
+        # Bad imports
+
+        with self.assertRaises(ImportError):
+            import quilt.data.foo.bad_package
+
+        with self.assertRaises(ImportError):
+            import quilt.data.bad_user.bad_package
+
+        with self.assertRaises(ImportError):
+            from quilt.data.foo.dataframes import blah
+
+        with self.assertRaises(ImportError):
+            from quilt.data.foo.baz import blah
+
+    def test_team_imports(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('test:bar/package', build_path)
+
+        # Good imports
+
+        from quilt.team.test.bar import package
+        from quilt.team.test.bar.package import dataframes
+        from quilt.team.test.bar.package import README
+
+        # Contents of the imports
+
+        assert isinstance(package, GroupNode)
+        assert isinstance(dataframes, GroupNode)
+        assert isinstance(dataframes.csv, DataNode)
+        assert isinstance(README, DataNode)
+
+        assert package.dataframes == dataframes
+        assert package.README == README
+
+        assert set(dataframes._keys()) == {'csv', 'nulls'}
+        assert set(dataframes._group_keys()) == set()
+        assert set(dataframes._data_keys()) == {'csv', 'nulls'}
+
+        assert isinstance(README(), string_types)
+        assert isinstance(README._data(), string_types)
+        assert isinstance(dataframes.csv(), pd.DataFrame)
+        assert isinstance(dataframes.csv._data(), pd.DataFrame)
+
+        str(package)
+        str(dataframes)
+        str(README)
+
+        # Bad attributes of imported packages
+
+        with self.assertRaises(AttributeError):
+            package.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.csv.foo
+
+        # Bad imports
+
+        with self.assertRaises(ImportError):
+            import quilt.team.test.bar.bad_package
+
+        with self.assertRaises(ImportError):
+            import quilt.team.test.bad_user.bad_package
+
+        with self.assertRaises(ImportError):
+            from quilt.team.test.bar.dataframes import blah
+
+        with self.assertRaises(ImportError):
+            from quilt.team.test.bar.baz import blah
+
+    def test_import_group_as_data(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build_group_data.yml')
+        command.build('foo/grppkg', build_path)
+
+        # Good imports
+        from quilt.data.foo.grppkg import dataframes
+        assert isinstance(dataframes, GroupNode)
+
+        # Make sure child dataframes were concatenated in the correct order (alphabetically by node name).
+        df = dataframes._data()
+        assert df['x'].tolist() == [1, 2, 3, 4]
+        assert df['y'].tolist() == [1, 4, 9, 16]
+
+        # Incompatible Schema
+        from quilt.data.foo.grppkg import incompatible
+        with self.assertRaises(PackageException):
+            incompatible._data()
+
+    def test_multiple_package_dirs(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')  # Contains 'dataframes'
+        simple_build_path = os.path.join(mydir, './build_simple.yml')  # Empty
+
+        new_build_dir = 'aaa/bbb/%s' % PACKAGE_DIR_NAME
+
+        # Build two packages:
+        # - First one exists in the default dir and the new dir; default should take priority.
+        # - Second one only exists in the new dir.
+
+        # First package.
+        command.build('foo/multiple1', build_path)
+
+        # First and second package in the new build dir.
+        with patch.dict(os.environ, {'QUILT_PRIMARY_PACKAGE_DIR': new_build_dir}):
+            command.build('foo/multiple1', simple_build_path)
+            command.build('foo/multiple2', simple_build_path)
+
+        # Cannot see the second package yet.
+        with self.assertRaises(ImportError):
+            from quilt.data.foo import multiple2
+
+        # Now search the new build dir.
+        dirs = 'foo/%s:%s' % (PACKAGE_DIR_NAME, new_build_dir)
+        with patch.dict(os.environ, {'QUILT_PACKAGE_DIRS': dirs}):
+            # Can import the second package now.
+            from quilt.data.foo import multiple2
+
+            # The first package contains data from the default dir.
+            from quilt.data.foo import multiple1
+            assert multiple1.dataframes
+
+    def test_save(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('foo/package1', build_path)
+
+        from quilt.data.foo import package1
+
+        # Build an identical package
+        command.build('foo/package2', package1)
+
+        from quilt.data.foo import package2
+        teststore = PackageStore(self._store_dir)
+        contents1 = open(os.path.join(teststore.package_path(None, 'foo', 'package1'),
+                                      Package.CONTENTS_DIR,
+                                      package1._package.get_hash())).read()
+        contents2 = open(os.path.join(teststore.package_path(None, 'foo', 'package2'),
+                                      Package.CONTENTS_DIR,
+                                      package2._package.get_hash())).read()
+        assert contents1 == contents2
+
+        # Rename an attribute
+        package1.dataframes2 = package1.dataframes
+        del package1.dataframes
+
+        # Modify an existing dataframe
+        csv = package1.dataframes2.csv._data()
+        csv.at[0, 'Int0'] = 42
+
+        # Add a new dataframe
+        df = pd.DataFrame(dict(a=[1, 2, 3]))
+        package1._set(['new', 'df'], df)
+        assert package1.new.df._data() is df
+
+        # Add a new file
+        file_path = os.path.join(mydir, 'data/foo.csv')
+        package1._set(['new', 'file'], 'data/foo.csv', build_dir=mydir)
+        assert package1.new.file._data() == file_path
+
+        # Add a new group
+        package1._add_group('newgroup')
+        assert isinstance(package1.newgroup, GroupNode)
+        package1.newgroup._add_group('foo')
+        assert isinstance(package1.newgroup.foo, GroupNode)
+
+        # Overwrite a leaf node
+        new_path = os.path.join(mydir, 'data/nuts.csv')
+        package1._set(['newgroup', 'foo'], 'data/nuts.csv', build_dir=mydir)
+        assert package1.newgroup.foo._data() == new_path
+
+        # Overwrite the whole group
+        package1._set(['newgroup'], 'data/nuts.csv', build_dir=mydir)
+        assert package1.newgroup._data() == new_path
+
+        # Built a new package and verify the new contents
+        command.build('foo/package3', package1)
+
+        from quilt.data.foo import package3
+
+        assert hasattr(package3, 'dataframes2')
+        assert not hasattr(package3, 'dataframes')
+
+        new_csv = package3.dataframes2.csv._data()
+        assert new_csv.xs(0)['Int0'] == 42
+
+        new_df = package3.new.df._data()
+        assert new_df.xs(2)['a'] == 3
+
+        new_file = package3.new.file._data()
+        assert isinstance(new_file, string_types)
+
+    def test_set_non_node_attr(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('foo/package4', build_path)
+
+        from quilt.data.foo import package4
+
+        # Assign a DataFrame as a node
+        # (should throw exception)
+        df = pd.DataFrame(dict(a=[1, 2, 3]))
+        with self.assertRaises(AttributeError):
+            package4.newdf = df
+
+    def test_load_update(self):
+        # also tests dynamic import
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('foo/package5', build_path)
+        from ..data.foo import package5
+
+        # make a copy, to prove we can
+        newpkgname = 'foo/copied_package'
+        command.build(newpkgname, package5)
+
+        newfilename = 'myfile'+str(int(time.time()))
+        with open(newfilename, 'w') as fh:
+            fh.write('hello world1')
+
+        module = command.load(newpkgname)
+        module._set([newfilename], newfilename)
+        command.build(newpkgname, module)
+
+        # current spec requires that build() *not* update the in-memory module tree.
+        newpath1 = getattr(module, newfilename)()
+        assert newpath1 == newfilename
+
+        # current spec requires that load() reload from disk, i.e. gets a reference
+        # to the local object store
+        # this is important because of potential changes to myfile
+        reloaded_module = command.load(newpkgname)
+        assert reloaded_module is not module
+        newpath2 = getattr(reloaded_module, newfilename)()
+        assert 'myfile' not in newpath2
+
+    def test_multiple_updates(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('foo/package6', build_path)
+        from ..data.foo import package6
+
+        newfilename1 = 'myfile1'+str(int(time.time()))
+        with open(newfilename1, 'w') as fh:
+            fh.write('hello world1')
+
+        package6._set([newfilename1], newfilename1)
+
+        newfilename2 = 'myfile2'+str(int(time.time()))
+        with open(newfilename2, 'w') as fh:
+            fh.write('hello world2')
+
+        package6._set([newfilename1], newfilename2)
+
+        assert getattr(package6, newfilename1)() == newfilename2
+
+    def test_team_non_team_imports(self):
+        mydir = os.path.dirname(__file__)
+        build_path1 = os.path.join(mydir, './build_simple.yml')
+        command.build('myteam:foo/team_imports', build_path1)
+        build_path2 = os.path.join(mydir, './build_empty.yml')
+        command.build('foo/team_imports', build_path2)
+
+        # Verify that both imports work, and packages are in fact different.
+
+        from ..team.myteam.foo import team_imports as pkg1
+        from ..data.foo import team_imports as pkg2
+
+        assert hasattr(pkg1, 'foo')
+        assert not hasattr(pkg2, 'foo')
+
+    def test_team_set_non_node_attr(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('test:bar/package4', build_path)
+
+        from quilt.team.test.bar import package4
+
+        # Assign a DataFrame as a node
+        # (should throw exception)
+        df = pd.DataFrame(dict(a=[1, 2, 3]))
+        with self.assertRaises(AttributeError):
+            package4.newdf = df
---
+++ packages/quilt/quilt-2.9.4.tar.gz/quilt-2.9.4/quilt/tools/store.py
@@ -0,0 +1,306 @@
+"""
+Build: parse and add user-supplied files to store
+"""
+import os
+
+from shutil import rmtree
+
+from .const import DEFAULT_TEAM, PACKAGE_DIR_NAME, QuiltException
+from .core import FileNode, RootNode, TableNode, find_object_hashes
+from .package import Package, PackageException
+from .util import BASE_DIR, sub_dirs, sub_files, is_nodename
+
+CHUNK_SIZE = 4096
+
+# Helper function to return the default package store path
+def default_store_location():
+    package_dir = os.path.join(BASE_DIR, PACKAGE_DIR_NAME)
+    return os.getenv('QUILT_PRIMARY_PACKAGE_DIR', package_dir)
+
+class StoreException(QuiltException):
+    """
+    Exception class for store I/O
+    """
+    pass
+
+
+class PackageStore(object):
+    """
+    Base class for managing Quilt data package repositories. This
+    class and its subclasses abstract file formats, file naming and
+    reading and writing to/from data files.
+    """
+    BUILD_DIR = 'build'
+    OBJ_DIR = 'objs'
+    TMP_OBJ_DIR = 'tmp'
+    PKG_DIR = 'pkgs'
+    CACHE_DIR = 'cache'
+    VERSION = '1.3'
+
+    def __init__(self, location=None):
+        if location is None:
+            location = default_store_location()
+
+        assert os.path.basename(os.path.abspath(location)) == PACKAGE_DIR_NAME, \
+            "Unexpected package directory: %s" % location
+        self._path = location
+
+        version = self._read_format_version()
+
+        if version == '1.2':
+            # Migrate to the teams format.
+            pkgdir = os.path.join(self._path, self.PKG_DIR)
+            old_dirs = sub_dirs(pkgdir)
+            os.mkdir(os.path.join(pkgdir, DEFAULT_TEAM))
+            for old_dir in old_dirs:
+                os.rename(os.path.join(pkgdir, old_dir), os.path.join(pkgdir, DEFAULT_TEAM, old_dir))
+            self._write_format_version()
+        elif version not in (None, self.VERSION):
+            msg = (
+                "The package repository at {0} is not compatible"
+                " with this version of quilt. Revert to an"
+                " earlier version of quilt or remove the existing"
+                " package repository."
+            )
+            raise StoreException(msg.format(self._path))
+
+    def create_dirs(self):
+        """
+        Creates the store directory and its subdirectories.
+        """
+        if not os.path.isdir(self._path):
+            os.makedirs(self._path)
+        for dir_name in [self.OBJ_DIR, self.TMP_OBJ_DIR, self.PKG_DIR, self.CACHE_DIR]:
+            path = os.path.join(self._path, dir_name)
+            if not os.path.isdir(path):
+                os.mkdir(path)
+        if not os.path.exists(self._version_path()):
+            self._write_format_version()
+
+    @classmethod
+    def find_store_dirs(cls):
+        """
+        Returns the primary package directory and any additional ones from QUILT_PACKAGE_DIRS.
+        """
+        store_dirs = [default_store_location()]
+        extra_dirs_str = os.getenv('QUILT_PACKAGE_DIRS')
+        if extra_dirs_str:
+            store_dirs.extend(extra_dirs_str.split(':'))
+        return store_dirs
+
+    @classmethod
+    def find_package(cls, team, user, package, store_dir=None):
+        """
+        Finds an existing package in one of the package directories.
+        """
+        cls.check_name(team, user, package)
+        dirs = cls.find_store_dirs()
+        for store_dir in dirs:
+            store = PackageStore(store_dir)
+            pkg = store.get_package(team, user, package)
+            if pkg is not None:
+                return pkg
+        return None
+
+    @classmethod
+    def check_name(cls, team, user, package, subpath=None):
+        if team is not None and not is_nodename(team):
+            raise StoreException("Invalid team name: %r" % team)
+        if not is_nodename(user):
+            raise StoreException("Invalid user name: %r" % user)
+        if not is_nodename(package):
+            raise StoreException("Invalid package name: %r" % package)
+        if subpath:
+            for element in subpath:
+                if not is_nodename(element):
+                    raise StoreException("Invalid element in subpath: %r" % element)
+
+    def _version_path(self):
+        return os.path.join(self._path, '.format')
+
+    def _read_format_version(self):
+        if os.path.exists(self._version_path()):
+            with open(self._version_path(), 'r') as versionfile:
+                version = versionfile.read()
+                return version
+        else:
+            return None
+
+    def _write_format_version(self):
+        with open(self._version_path(), 'w') as versionfile:
+            versionfile.write(self.VERSION)
+
+    # TODO: find a package instance other than 'latest', e.g. by
+    # looking-up by hash, tag or version in the local store.
+    def get_package(self, team, user, package):
+        """
+        Gets a package from this store.
+        """
+        self.check_name(team, user, package)
+        path = self.package_path(team, user, package)
+        if os.path.isdir(path):
+            try:
+                return Package(
+                    store=self,
+                    user=user,
+                    package=package,
+                    path=path
+                    )
+            except PackageException:
+                pass
+        return None
+
+    def install_package(self, team, user, package, contents):
+        """
+        Creates a new package in the default package store
+        and allocates a per-user directory if needed.
+        """
+        self.check_name(team, user, package)
+        assert contents is not None
+
+        self.create_dirs()
+
+        path = self.package_path(team, user, package)
+
+        # Delete any existing data.
+        try:
+            os.remove(path)
+        except OSError:
+            pass
+
+        return Package(
+            store=self,
+            user=user,
+            package=package,
+            path=path,
+            contents=contents
+        )
+
+    def create_package(self, team, user, package, dry_run=False):
+        """
+        Creates a new package and initializes its contents. See `install_package`.
+        """
+        if dry_run:
+            return Package(self, user, package, '.', RootNode(dict()))
+        contents = RootNode(dict())
+        return self.install_package(team, user, package, contents)
+
+    def remove_package(self, team, user, package):
+        """
+        Removes a package (all instances) from this store.
+        """
+        self.check_name(team, user, package)
+
+        path = self.package_path(team, user, package)
+        remove_objs = set()
+        # TODO: do we really want to delete invisible dirs?
+        if os.path.isdir(path):
+            # Collect objects from all instances for potential cleanup
+            contents_path = os.path.join(path, Package.CONTENTS_DIR)
+            for instance in os.listdir(contents_path):
+                pkg = Package(self, user, package, path, pkghash=instance)
+                remove_objs.update(find_object_hashes(pkg.get_contents()))
+            # Remove package manifests
+            rmtree(path)
+
+        return self.prune(remove_objs)
+
+    def iterpackages(self):
+        """
+        Return an iterator over all the packages in the PackageStore.
+        """
+        pkgdir = os.path.join(self._path, self.PKG_DIR)
+        if not os.path.isdir(pkgdir):
+            return
+        for team in sub_dirs(pkgdir):
+            for user in sub_dirs(self.team_path(team)):
+                for pkg in sub_dirs(self.user_path(team, user)):
+                    pkgpath = self.package_path(team, user, pkg)
+                    for hsh in sub_files(os.path.join(pkgpath, Package.CONTENTS_DIR)):
+                        yield Package(self, user, pkg, pkgpath, pkghash=hsh)
+
+    def ls_packages(self):
+        """
+        List packages in this store.
+        """
+        packages = []
+        pkgdir = os.path.join(self._path, self.PKG_DIR)
+        if not os.path.isdir(pkgdir):
+            return []
+        for team in sub_dirs(pkgdir):
+            for user in sub_dirs(self.team_path(team)):
+                for pkg in sub_dirs(self.user_path(team, user)):
+                    pkgpath = self.package_path(team, user, pkg)
+                    pkgmap = {h : [] for h in sub_files(os.path.join(pkgpath, Package.CONTENTS_DIR))}
+                    for tag in sub_files(os.path.join(pkgpath, Package.TAGS_DIR)):
+                        with open(os.path.join(pkgpath, Package.TAGS_DIR, tag), 'r') as tagfile:
+                            pkghash = tagfile.read()
+                            pkgmap[pkghash].append(tag)
+                    for pkghash, tags in pkgmap.items():
+                        # add teams here if any other than DEFAULT_TEAM should be hidden.
+                        team_token = '' if team in (DEFAULT_TEAM,) else team + ':'
+                        fullpkg = "{team}{owner}/{pkg}".format(team=team_token, owner=user, pkg=pkg)
+                        # Add an empty string tag for untagged hashes
+                        displaytags = tags if tags else [""]
+                        # Display a separate full line per tag like Docker
+                        for tag in displaytags:
+                            packages.append((fullpkg, str(tag), pkghash))
+
+        return packages
+
+    def team_path(self, team=None):
+        """
+        Returns the path to directory with the team's users' package repositories.
+        """
+        if team is None:
+            team = DEFAULT_TEAM
+        return os.path.join(self._path, self.PKG_DIR, team)
+
+    def user_path(self, team, user):
+        """
+        Returns the path to directory with the user's package repositories.
+        """
+        return os.path.join(self.team_path(team), user)
+
+    def package_path(self, team, user, package):
+        """
+        Returns the path to a package repository.
+        """
+        return os.path.join(self.user_path(team, user), package)
+
+    def object_path(self, objhash):
+        """
+        Returns the path to an object file based on its hash.
+        """
+        return os.path.join(self._path, self.OBJ_DIR, objhash)
+
+    def temporary_object_path(self, name):
+        """
+        Returns the path to a temporary object, before we know its hash.
+        """
+        return os.path.join(self._path, self.TMP_OBJ_DIR, name)
+
+    def cache_path(self, name):
+        """
+        Returns the path to a temporary object, before we know its hash.
+        """
+        return os.path.join(self._path, self.CACHE_DIR, name)
+
+    def prune(self, objs=None):
+        """
+        Clean up objects not referenced by any packages. Try to prune all
+        objects by default.
+        """
+        if objs is None:
+            objdir = os.path.join(self._path, self.OBJ_DIR)
+            objs = os.listdir(objdir)
+        remove_objs = set(objs)
+
+        for pkg in self.iterpackages():
+            remove_objs.difference_update(find_object_hashes(pkg.get_contents()))
+
+        for obj in remove_objs:
+            path = self.object_path(obj)
+            if os.path.exists(path):
+                os.remove(path)
+        return remove_objs
