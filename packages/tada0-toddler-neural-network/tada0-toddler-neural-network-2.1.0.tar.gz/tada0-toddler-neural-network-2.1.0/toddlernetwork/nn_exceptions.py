7487f92de9717f37a05ad91ba9a4093bb5aff6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package tada0-toddler-neural-network/2.1.0/tada0-toddler-neural-network-2.1.0.tar.gz

---
+++ packages/tada0-toddler-neural-network/tada0-toddler-neural-network-2.1.0.tar.gz/tada0-toddler-neural-network-2.1.0/toddlernetwork/nn_exceptions.py
@@ -0,0 +1,102 @@
+# __init__ exceptions
+
+def ConstructorArgumentsExceptionHandler_ExecutionAborted():
+    print("Object not created, critical error")
+    print("Program execution aborted")
+    exit(67656972)
+
+def ConstructorArgumentsExceptionHandler_ArgumentsNumber():
+    print("Wrong number of the parameters")
+    ConstructorArgumentsExceptionHandler_ExecutionAborted()
+
+def ConstructorArgumentsExceptionHandler_ArgumentsType():
+    print("Wrong type of the parameters")
+    ConstructorArgumentsExceptionHandler_ExecutionAborted()
+
+def ConstructorArgumentsExceptionHandler_ArgumentsSign():
+    print("Wrong value of the parameters")
+    ConstructorArgumentsExceptionHandler_ExecutionAborted()
+
+
+# set_learning_rate exceptions
+
+def SetLearningRateExceptionHandler_ExecutionAborted():
+    print("Could not change the learning rate value")
+
+
+def SetLearningRateExceptionHandler_ArgumentType():
+    print("Wrong type of the argument")
+    SetLearningRateExceptionHandler_ExecutionAborted()
+
+
+def SetLearningRateExceptionHandler_ArgumentSign():
+    print("Wrong value of the learning rate")
+    SetLearningRateExceptionHandler_ExecutionAborted()
+
+
+# feedforward exceptions
+
+def FeedforwardArgumentsExceptionHandler_ExecutionAborted():
+    print("Execution of feedforward method aborted")
+
+
+def FeedforwardArgumentsExceptionHandler_ArgumentType():
+    print("Wrong type of the argument")
+    FeedforwardArgumentsExceptionHandler_ExecutionAborted()
+
+
+def FeedforwardArgumentsExceptionHandler_ListElementsType():
+    print("Wrong type of list elements")
+    FeedforwardArgumentsExceptionHandler_ExecutionAborted()
+
+
+def FeedforwardArgumentsExceptionHandler_ListLength():
+    print("Wrong length of the list")
+    FeedforwardArgumentsExceptionHandler_ExecutionAborted()
+
+
+# train exceptions
+
+def TrainArgumentsExceptionHandler_ExecutionAborted():
+    print("Execution of train method aborted")
+
+
+def TrainArgumentsExceptionHandler_InputsListType():
+    print("Wrong type of inputs_array list")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+
+def TrainArgumentsExceptionHandler_InputsListElementsType():
+    print("Wrong type of inputs_array elements")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+
+def TrainArgumentsExceptionHandler_InputsListLength():
+    print("Wrong length of the inputs_array")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+
+def TrainArgumentsExceptionHandler_TargetsListType():
+    print("Wrong type of targets_array list")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+
+def TrainArgumentsExceptionHandler_TargetsListElementsType():
+    print("Wrong type of targets_array elements")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+
+def TrainArgumentsExceptionHandler_TargetsListLength():
+    print("Wrong length of the targets_array")
+    TrainArgumentsExceptionHandler_ExecutionAborted()
+
+# load and save exceptions
+
+def SaveFileCreationException():
+    print("Could not create file")
+    exit(9911099102)
+
+
+def LoadFileOpenException():
+    print("Could not open file")
+    exit(99110111102)
