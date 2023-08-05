e2c4640243717116ffd5c660b266b6b16b0b1432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:58 +0000

    Add package sanjose-twitter/0.1.dev0/sanjose-twitter-0.1.dev0.tar.gz

---
+++ packages/sanjose-twitter/sanjose-twitter-0.1.dev0.tar.gz/sanjose-twitter-0.1.dev0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="sanjose-twitter",
+  version="0.1dev",
+  description="Getting trends on Twitter",
+  author="sanjose",
+  scripts=["sanjose-twitter"],
+  install_requires=["twitter",]
+)
