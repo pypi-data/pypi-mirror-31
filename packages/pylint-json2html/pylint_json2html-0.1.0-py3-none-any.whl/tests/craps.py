3dd7d726fc2fad662684b5ee87e0a0db404f0db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package pylint-json2html/0.1.0/pylint_json2html-0.1.0-py3-none-any.whl

---
+++ packages/pylint-json2html/pylint_json2html-0.1.0-py3-none-any.whl/tests/craps.py
@@ -0,0 +1,14 @@
+
+def something():
+    if True:
+        print('It is true.')
+        pass
+    return True
+
+def iPreferCamelCase(what):
+    what_are_you_going_to_do_against_such_long_variable_name = 'i know right?'
+    return something() is not what
+
+THIS_LINE_IS_TOO_LONG = ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.'
+
+not_uppercase = 'IT IS NOT'
