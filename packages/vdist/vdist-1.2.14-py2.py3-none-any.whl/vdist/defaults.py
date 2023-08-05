91ee8b963deadc0834677ad39bfce774a038a7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package vdist/1.2.14/vdist-1.2.14-py2.py3-none-any.whl

---
+++ packages/vdist/vdist-1.2.14-py2.py3-none-any.whl/vdist/defaults.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+PYTHON_BASEDIR = '/opt'
+PYTHON_VERSION = '2.7.9'
+LOCAL_PROFILES_DIR = 'buildprofiles'
+LOCAL_PROFILES_FILE = 'profiles.json'
+VDIST_USERDIR = os.path.join(os.path.expanduser('~'), '.vdist')
+BUILD_BASEDIR = "/tmp/vdist"
+SCRATCH_BUILDSCRIPT_NAME = 'buildscript.sh'
+SCRATCH_DIR = 'scratch'
+SHARED_DIR = '/work'
+PACKAGE_INSTALL_ROOT = PYTHON_BASEDIR
+PACKAGE_TMP_ROOT = '/tmp'
+OUTPUT_FOLDER = "./"
+OUTPUT_SCRIPT = False
+
+PYTHON3_INTERPRETER = True if sys.version_info[0] == 3 else False
