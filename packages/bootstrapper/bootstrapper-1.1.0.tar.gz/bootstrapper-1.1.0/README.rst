d1dd6a55f7a5b5b1e4596cd6ac7b533ee2c14849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package bootstrapper/1.1.0/bootstrapper-1.1.0.tar.gz

---
+++ packages/bootstrapper/bootstrapper-1.1.0.tar.gz/bootstrapper-1.1.0/README.rst
@@ -0,0 +1,35 @@
+============
+bootstrapper
+============
+
+.. image:: https://img.shields.io/travis/playpauseandstop/bootstrapper/master.svg
+    :target: https://travis-ci.org/playpauseandstop/bootstrapper
+    :alt: Travis CI
+
+.. image:: https://img.shields.io/pypi/v/bootstrapper.svg
+    :target: https://warehouse.python.org/project/bootstrapper/
+    :alt: Latest Version
+
+.. image:: https://img.shields.io/pypi/pyversions/bootstrapper.svg
+    :target: https://warehouse.python.org/project/bootstrapper/
+    :alt: Python versions
+
+.. image:: https://img.shields.io/pypi/l/bootstrapper.svg
+    :target: https://github.com/playpauseandstop/bootstrapper/blob/master/LICENSE
+    :alt: BSD License
+
+.. image:: https://readthedocs.org/projects/bootstrapper/badge/?version=latest
+    :target: http://bootstrapper.readthedocs.org/en/latest/
+    :alt: Documentation
+
+Bootstrap Python projects or libraries by checking system pre-requirements if
+necessary, creating virtual environment, installing all requirements there and
+finally execute post-bootstrap hooks if any.
+
+* Works on Python 2.6+
+* BSD licensed
+* Latest documentation `on Read the Docs <http://bootstrapper.readthedocs.org/>`_
+* Source, issues, and pull requests `on GitHub
+  <https://github.com/playpauseandstop/bootstrapper>`_
+* Install with ``pip install bootstrapper``
+* Run with ``python -m bootstrapper``
---
+++ packages/bootstrapper/bootstrapper-1.1.0.tar.gz/bootstrapper-1.1.0/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+
+
+def rel(*parts):
+    return os.path.abspath(os.path.join(DIRNAME, *parts))
+
+
+with open(rel('README.rst')) as readme_file:
+    README = readme_file.read()
+
+with open(rel('bootstrapper.py')) as bootstrapper_file:
+    INIT_PY = bootstrapper_file.read()
+
+IS_PY26 = sys.version_info[:2] == (2, 6)
+VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
+
+
+setup(
+    name='bootstrapper',
+    version=VERSION,
+    description='Bootstrap Python projects with virtualenv and pip.',
+    long_description=README,
+    author='Igor Davydenko',
+    author_email='playpauseandstop@gmail.com',
+    url='https://github.com/playpauseandstop/bootstrapper',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords='bootstrap pip virtualenv',
+    license='BSD License',
+    entry_points={
+        'console_scripts': [
+            'bootstrapper=bootstrapper:main',
+        ]
+    },
+    install_requires=list(filter(None, [
+        'argparse>=1.3.0' if IS_PY26 else None,
+        'virtualenv>=1.10',
+    ])),
+    py_modules=[
+        'bootstrapper'
+    ],
+    test_suite='tests',
+    tests_require=list(filter(None, [
+        'unittest2==0.5.1' if IS_PY26 else None,
+    ]))
+)
