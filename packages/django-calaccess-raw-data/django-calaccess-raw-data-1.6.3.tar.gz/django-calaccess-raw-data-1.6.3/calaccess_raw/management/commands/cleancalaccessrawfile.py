d567ec0be488aa1c38b5ce112514cc6297f61a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package django-calaccess-raw-data/1.6.3/django-calaccess-raw-data-1.6.3.tar.gz

---
+++ packages/django-calaccess-raw-data/django-calaccess-raw-data-1.6.3.tar.gz/django-calaccess-raw-data-1.6.3/calaccess_raw/management/commands/cleancalaccessrawfile.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Clean a source CAL-ACCESS TSV file and reformat it as a CSV.
+"""
+from __future__ import unicode_literals
+import os
+import csv
+from io import StringIO
+from django.conf import settings
+from django.core.files import File
+from django.core.management.base import CommandError
+from django.utils import six
+from django.utils.timezone import now
+from csvkit import reader, writer
+from calaccess_raw.management.commands import CalAccessCommand
+from calaccess_raw.models.tracking import RawDataVersion, RawDataFile
+
+
+class Command(CalAccessCommand):
+    """
+    Clean a source CAL-ACCESS TSV file and reformat it as a CSV.
+    """
+    help = 'Clean a source CAL-ACCESS TSV file and reformat it as a CSV'
+
+    def add_arguments(self, parser):
+        """
+        Adds custom arguments specific to this command.
+        """
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            'file_name',
+            help="Name of the TSV file to be cleaned and discarded for a CSV"
+        )
+        parser.add_argument(
+            "--keep-file",
+            action="store_true",
+            dest="keep_file",
+            default=False,
+            help="Keep original TSV file"
+        )
+
+    def handle(self, *args, **options):
+        """
+        Make it happen.
+        """
+        super(Command, self).handle(*args, **options)
+
+        # Set options
+        self.file_name = options['file_name']
+
+        self.log_dir = os.path.join(self.data_dir, "log/")
+        self.error_log_path = os.path.join(
+            self.log_dir,
+            self.file_name.lower().replace("tsv", "errors.csv")
+        )
+
+        # get most recently extracted RawDataVersion
+        try:
+            version = RawDataVersion.objects.filter(
+                extract_start_datetime__isnull=False
+            ).latest('extract_start_datetime')
+        except RawDataVersion.DoesNotExist:
+            raise CommandError(
+                'No record of extracting files from download zip '
+                '(run `python manage.py extractcalaccessrawfiles`).'
+            )
+
+        # raise exception if extract step did not finish
+        if not version.extract_completed:
+            raise CommandError(
+                'Previous extraction files from download zip did not finish '
+                '(run `python manage.py extractcalaccessrawfiles`).'
+            )
+
+        # get the raw file record
+        self.raw_file = RawDataFile.objects.get(
+            file_name=self.file_name.replace('.TSV', ''), version=version
+        )
+        # store the start time for the clean
+        self.raw_file.clean_start_datetime = now()
+        # reset the finish time for the clean
+        self.raw_file.clean_finish_datetime = None
+        # save here in case command doesn't finish
+        self.raw_file.save()
+
+        if self.verbosity > 2:
+            self.log(" Cleaning %s" % self.file_name)
+
+        self.clean()
+
+        # unless keeping files, remove tsv files
+        if not options['keep_file']:
+            os.remove(os.path.join(self.tsv_dir, options['file_name']))
+
+        # store the finish time for the clean
+        self.raw_file.clean_finish_datetime = now()
+        # and save the RawDataFile
+        self.raw_file.save()
+
+    def clean(self):
+        """
+        Cleans the provided source TSV file and writes it out in CSV format.
+        """
+        # Up the CSV data limit
+        csv.field_size_limit(1000000000)
+
+        # Input and output paths
+        tsv_path = os.path.join(self.tsv_dir, self.file_name)
+        csv_path = os.path.join(
+            self.csv_dir,
+            self.file_name.lower().replace("tsv", "csv")
+        )
+
+        # Reader
+        tsv_file = open(tsv_path, 'rbU')
+
+        # Writer
+        csv_file = open(csv_path, 'w')
+        csv_writer = writer(csv_file)
+
+        # Pull and clean the headers
+        try:
+            headers = tsv_file.readline()
+        except StopIteration:
+            return
+        headers = headers.decode("ascii", "replace")
+        headers_csv = reader(StringIO(headers), delimiter=str('\t'))
+        try:
+            headers_list = next(headers_csv)
+        except StopIteration:
+            return
+        headers_count = len(headers_list)
+        csv_writer.writerow(headers_list)
+
+        log_rows = []
+
+        # Loop through the rest of the data
+        line_number = 1
+        for tsv_line in tsv_file:
+            line_number += 1
+            # Log empty lines, then skip
+            if (
+                tsv_line.decode("ascii", "replace") == '\n' or
+                tsv_line.decode("ascii", "replace") == '\r\n'
+            ):
+                if self.verbosity > 2:
+                    msg = '  Line %s is empty'
+                    self.failure(msg % (
+                        line_number,
+                    ))
+                continue
+
+            # Goofing around with the encoding while we're in there.
+            tsv_line = tsv_line.decode("ascii", "replace")
+            if six.PY2:
+                tsv_line = tsv_line.replace('\ufffd', '?')
+
+            # Nuke any null bytes
+            null_bytes = tsv_line.count('\x00')
+            if null_bytes:
+                tsv_line = tsv_line.replace('\x00', ' ')
+
+            # Nuke ASCII 26 char, the "substitute character"
+            # or chr(26) in python
+            sub_char = tsv_line.count('\x1a')
+            if sub_char:
+                tsv_line = tsv_line.replace('\x1a', '')
+
+            # Remove any extra newline chars
+            tsv_line = tsv_line.replace("\r\n", "").replace("\r", "").replace("\n", "")
+
+            # Split on tabs so we can later spit it back out as CSV
+            csv_field_list = tsv_line.split("\t")
+
+            # Check if our values line up with our headers
+            # and if not, see if CSVkit can sort out the problems
+            if not len(csv_field_list) == headers_count:
+                csv_field_list = next(reader(
+                    StringIO(tsv_line),
+                    delimiter=str('\t')
+                ))
+
+                if not len(csv_field_list) == headers_count:
+                    if self.verbosity > 2:
+                        msg = '  Bad parse of line %s (%s headers, %s values)'
+                        self.failure(msg % (
+                            line_number,
+                            len(headers_list),
+                            len(csv_field_list)
+                        ))
+                    log_rows.append([
+                        line_number,
+                        len(headers_list),
+                        len(csv_field_list),
+                        ','.join(csv_field_list)
+                    ])
+                    continue
+
+            # Write out the row
+            csv_writer.writerow(csv_field_list)
+
+        # Log errors if there are any
+        if log_rows:
+            if self.verbosity > 1:
+                msg = '  %s errors logged (not including empty lines)'
+                self.failure(msg % (len(log_rows)))
+            self.log_errors(log_rows)
+
+        # Add counts to raw_file_record
+        self.raw_file.download_columns_count = headers_count
+        self.raw_file.download_records_count = line_number - 1
+        self.raw_file.clean_columns_count = headers_count
+        self.raw_file.clean_records_count = line_number - 1 - len(log_rows)
+        self.raw_file.error_count = len(log_rows)
+
+        # Shut it down
+        tsv_file.close()
+        csv_file.close()
+
+        # Add file size to the raw_file_record
+        self.raw_file.download_file_size = os.path.getsize(tsv_path) or 0
+        self.raw_file.clean_file_size = os.path.getsize(csv_path) or 0
+
+        if getattr(settings, 'CALACCESS_STORE_ARCHIVE', False):
+            if self.verbosity > 2:
+                self.log(" Archiving {0}".format(os.path.basename(csv_path)))
+            # Remove previous .CSV and error log files
+            self.raw_file.clean_file_archive.delete()
+            self.raw_file.error_log_archive.delete()
+
+            # Open up the .CSV file so we can wrap it in the Django File obj
+            with open(csv_path, 'rb') as csv_file:
+                # Save the .CSV on the raw data file
+                self.raw_file.clean_file_archive.save(
+                    self.file_name.lower().replace("tsv", "csv"),
+                    File(csv_file),
+                )
+            # if there are any errors, archive the log too
+            if log_rows:
+                if self.verbosity > 2:
+                    self.log(" Archiving {0}".format(
+                        os.path.basename(self.error_log_path)
+                    ))
+                with open(self.error_log_path, 'rb') as error_file:
+                    self.raw_file.error_log_archive.save(
+                        os.path.basename(self.error_log_path),
+                        File(error_file),
+                    )
+
+    def log_errors(self, rows):
+        """
+        Log any errors to a CSV file.
+        """
+        # Make sure the log directory exists
+        os.path.exists(self.log_dir) or os.makedirs(self.log_dir)
+
+        # Log writer
+        log_file = open(self.error_log_path, 'w')
+        log_writer = writer(log_file, quoting=csv.QUOTE_ALL)
+
+        # Add the headers
+        log_writer.writerow([
+            'Line number',
+            'Headers len',
+            'Fields len',
+            'Line value'
+        ])
+
+        # Log out the rows
+        log_writer.writerows(rows)
+
+        # Shut it down
+        log_file.close()
---
+++ packages/django-calaccess-raw-data/django-calaccess-raw-data-1.6.3.tar.gz/django-calaccess-raw-data-1.6.3/calaccess_raw/management/commands/loadcalaccessrawfile.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Load clean CAL-ACCESS CSV file into a database model.
+"""
+from __future__ import unicode_literals
+import os
+import six
+from django.apps import apps
+from csvkit import reader
+from django.conf import settings
+from django.db import connections, router
+from django.core.management.base import CommandError
+from django.utils.timezone import now
+from calaccess_raw.management.commands import CalAccessCommand
+from calaccess_raw.models.tracking import RawDataFile
+
+
+class Command(CalAccessCommand):
+    """
+    Load clean CAL-ACCESS CSV file into a database model.
+    """
+    help = 'Load clean CAL-ACCESS CSV file into a database model'
+    # Trick for reformating date strings in source data so that they can
+    # be gobbled up by MySQL. You'll see how below.
+    date_sql = "DATE_FORMAT(str_to_date(@`%s`, '%%c/%%e/%%Y'), '%%Y-%%m-%%d')"
+    datetime_sql = "DATE_FORMAT(str_to_date(@`%s`, '%%c/%%e/%%Y \
+%%h:%%i:%%s %%p'), '%%Y-%%m-%%d  %%H:%%i:%%s')"
+
+    def add_arguments(self, parser):
+        """
+        Adds custom arguments specific to this command.
+        """
+        super(Command, self).add_arguments(parser)
+        # positional (required) arg
+        parser.add_argument(
+            'model_name',
+            help="Name of the model into which data will be loaded"
+        )
+        # keyword (optional) args
+        parser.add_argument(
+            "--c",
+            "--csv",
+            dest='csv',
+            default=None,
+            help="Path to comma-delimited file to be loaded. Defaults to one associated with model."
+        )
+        parser.add_argument(
+            "--keep-file",
+            action="store_true",
+            dest="keep_file",
+            default=False,
+            help="Keep clean CSV file after loading"
+        )
+        parser.add_argument(
+            "-a",
+            "--app-name",
+            dest="app_name",
+            default="calaccess_raw",
+            help="Name of Django app with models into which data will "
+                 "be imported (if other not calaccess_raw)"
+        )
+
+    # all BaseCommand subclasses require a handle() method that includes
+    #   the actual logic of the command
+    def handle(self, *args, **options):
+        """
+        Make it happen.
+        """
+        super(Command, self).handle(*args, **options)
+
+        # set / compute any attributes that multiple class methods need
+        self.keep_file = options["keep_file"]
+        # get model based on strings of app_name and model_name
+        self.model = apps.get_model(options["app_name"], options['model_name'])
+
+        # load from provided csv or csv mapped to model
+        self.csv = options["csv"] or self.model.objects.get_csv_path()
+
+        # load into database suggested for model by router
+        self.database = router.db_for_write(model=self.model)
+
+        # get most recently cleaned RawDataFile
+        try:
+            raw_file = RawDataFile.objects.filter(
+                file_name=self.model._meta.db_table,
+                clean_start_datetime__isnull=False
+            ).latest('clean_start_datetime')
+        except RawDataFile.DoesNotExist:
+            raise CommandError(
+                'No record of cleaning {0}.TSV (run `python manage.py '
+                'cleancalaccessrawfile {0}`).'.format(self.model._meta.db_table)
+            )
+        # raise exception if clean step did not finish
+        if not raw_file.clean_finish_datetime:
+            raise CommandError(
+                'Previous cleaning of {0}.TSV did not finish (run `python manage.py '
+                'cleancalaccessrawfile {0}`).'.format(self.model._meta.db_table)
+            )
+
+        # get the row count
+        row_count = self.get_row_count()
+
+        if row_count == 0:
+            if self.verbosity > 2:
+                self.failure("%s is empty." % self.csv)
+        else:
+            # store the start time for the load
+            raw_file.load_start_datetime = now()
+            # reset the finish time for the load
+            raw_file.load_finish_datetime = None
+            # save here in case command doesn't finish
+            raw_file.save()
+
+            if self.verbosity > 2:
+                self.log(" Loading %s" % options['model_name'])
+            self.load()
+
+            # add load counts to raw_file_record
+            raw_file.load_columns_count = len(self.model._meta.fields)
+            raw_file.load_records_count = self.model.objects.count()
+
+            # if not keeping files, remove the csv file
+            if not self.keep_file:
+                os.remove(self.csv)
+
+            # store the finish time for the load
+            raw_file.load_finish_datetime = now()
+            # and save the RawDataFile
+            raw_file.save()
+
+    def load(self):
+        """
+        Loads the source CSV for the provided model based on settings and database connections.
+        """
+        # check if can load into dat
+        if getattr(settings, 'CALACCESS_DAT_SOURCE', None) and six.PY2:
+            self.load_dat()
+
+        # if not using default db, make sure the database is set up in django's settings
+        if self.database:
+            try:
+                engine = settings.DATABASES[self.database]['ENGINE']
+            except KeyError:
+                raise TypeError(
+                    "{} not configured in DATABASES settings.".format(self.database)
+                )
+
+        # set up database connection
+        self.connection = connections[self.database]
+        self.cursor = self.connection.cursor()
+
+        # check the kind of database before calling db-specific load method
+        if engine == 'django.db.backends.mysql':
+            self.load_mysql()
+        elif engine in (
+            'django.db.backends.postgresql_psycopg2',
+            'django.db.backends.postgresql',
+            'django.contrib.gis.db.backends.postgis'
+        ):
+            self.load_postgresql()
+        else:
+            self.failure("Sorry your database engine is unsupported")
+            raise CommandError("Only MySQL and PostgresSQL backends supported.")
+
+    def load_dat(self):
+        """
+        Takes a model and a csv file and loads it into dat.
+        """
+        import datpy
+        dat_source = settings.CALACCESS_DAT_SOURCE
+        self.dat = datpy.Dat(dat_source['source'])
+        dataset = self.dat.dataset(self.model._meta.db_table)
+        try:
+            dataset.import_file(self.csv, format='csv')
+            if self.verbosity > 2:
+                dat_status = self.dat.status()
+                model_count = dat_status['rows']
+                csv_count = self.get_row_count(self.csv)
+                self.finish_load_message(model_count, csv_count)
+        except datpy.DatException:
+            raise CommandError(
+                'Failed to load dat for %s, %s' % (
+                    self.model._meta.db_table,
+                    self.csv
+                )
+            )
+
+    def load_mysql(self):
+        """
+        Load the file into a MySQL database using LOAD DATA INFILE.
+        """
+        import warnings
+        import MySQLdb
+        warnings.filterwarnings("ignore", category=MySQLdb.Warning)
+
+        # Flush the target model
+        self.cursor.execute('TRUNCATE TABLE %s' % self.model._meta.db_table)
+
+        # Build the MySQL LOAD DATA INFILE command
+        bulk_sql_load_part_1 = """
+            LOAD DATA LOCAL INFILE '%s'
+            INTO TABLE %s
+            FIELDS TERMINATED BY ','
+            OPTIONALLY ENCLOSED BY '"'
+            LINES TERMINATED BY '\\n'
+            IGNORE 1 LINES
+            (
+        """ % (
+            self.csv,
+            self.model._meta.db_table
+        )
+
+        # Get the headers and the row count from the source CSV
+        csv_headers = self.get_headers()
+        csv_record_cnt = self.get_row_count()
+
+        header_sql_list = []
+        field_types = dict(
+            (f.db_column, f.db_type(self.connection))
+            for f in self.model._meta.fields
+        )
+        date_set_list = []
+        char_set_list = []
+
+        for h in csv_headers:
+            # Pull the data type of the field
+            data_type = field_types[h]
+            # If it is a date field, we need to reformat the data
+            # so that MySQL will properly parse it on the way in.
+            if data_type == 'date':
+                header_sql_list.append('@`%s`' % h)
+                date_set_list.append(
+                    "`%s` =  %s" % (h, self.date_sql % h)
+                )
+            elif data_type == 'datetime':
+                header_sql_list.append('@`%s`' % h)
+                date_set_list.append(
+                    "`%s` =  %s" % (h, self.datetime_sql % h)
+                )
+            elif 'char' in data_type:
+                header_sql_list.append('@`%s`' % h)
+                char_set_list.append(
+                    r"`{0}` = TRIM(TRAILING '\n' FROM @`{0}`)".format(h)
+                )
+            else:
+                header_sql_list.append('`%s`' % h)
+
+        bulk_sql_load = bulk_sql_load_part_1 + ','.join(header_sql_list) + ')'
+        if date_set_list or char_set_list:
+            bulk_sql_load += " set %s" % ",".join(date_set_list + char_set_list)
+
+        # Run the query
+        cnt = self.cursor.execute(bulk_sql_load)
+
+        # Report back on how we did
+        if self.verbosity > 2:
+            self.finish_load_message(cnt, csv_record_cnt)
+
+    def load_postgresql(self):
+        """
+        Load the file into a PostgreSQL database using COPY.
+        """
+        # Drop all the records from the target model's real table
+        self.cursor.execute('TRUNCATE TABLE "%s" RESTART IDENTITY CASCADE' % (
+            self.model._meta.db_table
+        ))
+
+        # Create a mapping between our django models and the CSV headers
+        model_mapping = dict(
+            (f.name, f.db_column) for f in self.model._meta.fields
+            if f.db_column
+        )
+
+        # Load the data
+        self.model.objects.from_csv(
+            self.csv,
+            model_mapping,
+            using=self.database,
+        )
+
+        # Print out the results
+        if self.verbosity > 2:
+            csv_count = self.get_row_count()
+            model_count = self.model.objects.count()
+            self.finish_load_message(model_count, csv_count)
+
+    def get_headers(self):
+        """
+        Returns the column headers from the csv as a list.
+        """
+        with open(self.csv, 'r') as infile:
+            csv_reader = reader(infile)
+            try:
+                headers = next(csv_reader)
+            except StopIteration:
+                headers = []
+        return headers
+
+    def get_row_count(self):
+        """
+        Returns the number of rows in the file, not counting headers.
+        """
+        with open(self.csv) as infile:
+            row_count = sum(1 for line in infile) - 1
+
+        if row_count < 0:
+            row_count = 0
+
+        return row_count
+
+    def finish_load_message(self, model_count, csv_count):
+        """
+        The message displayed about whether or not a load finished successfully.
+        """
+        if model_count != csv_count:
+            msg = '  Table record count doesn\'t match CSV. Table: %s\tCSV: %s'
+            self.failure(msg % (
+                model_count,
+                csv_count,
+            ))
