4d3151801bcae992c1b62e67f60b4e051dfe3f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package otcextensions/0.1.0/otcextensions-0.1.0.tar.gz

---
+++ packages/otcextensions/otcextensions-0.1.0.tar.gz/otcextensions-0.1.0/doc/source/cli/index.rst
@@ -0,0 +1,8 @@
+.. toctree::
+
+   rds.rst
+   obs.rst
+   auto_scaling.rst
+   kms.rst
+   cce.rst
+   load_balancer.rst
---
+++ packages/otcextensions/otcextensions-0.1.0.tar.gz/otcextensions-0.1.0/otcextensions/tests/unit/osclient/rds/v1/test_flavor.py
@@ -0,0 +1,117 @@
+#   Copyright 2013 Nebula Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+import mock
+# from osc_lib import utils as common_utils
+
+from otcextensions.osclient.rds.v1 import flavor
+from otcextensions.tests.unit.osclient.rds.v1 import fakes as rds_fakes
+
+
+class TestListDatabaseFlavors(rds_fakes.TestRds):
+
+    column_list_headers = [
+        'ID',
+        'Name',
+        'RAM',
+        # 'Str_ID',
+        # 'vCPUs'
+    ]
+
+    def setUp(self):
+        super(TestListDatabaseFlavors, self).setUp()
+
+        self.cmd = flavor.ListDatabaseFlavors(self.app, None)
+
+        self.app.client_manager.rds.flavors = mock.Mock()
+
+        self.flavors = self.flavor_mock.create_multiple(3)
+        self.flavor_data = []
+        for s in self.flavors:
+            self.flavor_data.append((
+                s.id,
+                s.name,
+                s.ram,
+            ))
+
+    def test_list_flavors(self):
+        arglist = [
+        ]
+
+        verifylist = [
+        ]
+
+        # Verify cm is triggereg with default parameters
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        # Set the response
+        self.app.client_manager.rds.flavors.side_effect = [
+            self.flavors
+        ]
+
+        # Trigger the action
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.app.client_manager.rds.flavors.assert_called()
+
+        self.assertEqual(self.column_list_headers, columns)
+        self.assertEqual(tuple(self.flavor_data), tuple(data))
+
+
+class TestShowDatabaseFlavors(rds_fakes.TestRds):
+
+    columns = ['ID', 'Name', 'RAM', 'vCPUs']
+
+    def setUp(self):
+        super(TestShowDatabaseFlavors, self).setUp()
+
+        self.cmd = flavor.ShowDatabaseFlavor(self.app, None)
+
+        self.app.client_manager.rds.find_flavor = mock.Mock()
+
+        self.flavor = self.flavor_mock.create_one()
+
+        self.flavor_data = (
+            self.flavor.id,
+            self.flavor.name,
+            self.flavor.ram,
+            # s.flavor,
+            self.flavor.flavor_detail[0]['value'],
+            # self.flavor.price_detail
+        )
+
+    def test_show_flavor(self):
+        arglist = [
+            'test_flavor',
+        ]
+
+        verifylist = [
+            ('flavor', 'test_flavor'),
+        ]
+
+        # Verify cm is triggereg with default parameters
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        # Set the response
+        self.app.client_manager.rds.find_flavor.side_effect = [
+            self.flavor
+        ]
+
+        # Trigger the action
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.app.client_manager.rds.find_flavor.assert_called()
+
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.flavor_data, data)
