07b0718df3f4c02830646bac14763bbdc09a3518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package django-db-cascade-2/0.2.2/django-db-cascade-2-0.2.2.tar.gz

---
+++ packages/django-db-cascade-2/django-db-cascade-2-0.2.2.tar.gz/django-db-cascade-2-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-db-cascade-2',
+    version='0.2.2',
+    description='Fork of django-db-cascade for Django 2.0.  Optionally use postgres db ON CASCADE DELETE on django foreign keys',
+    url='http://github.com/jkapelner/django-db-cascade',
+    author='Nick Stefan, Jordan Kapelner',
+    author_email='nickstefan12@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'Django >= 2.0',
+        'psycopg2 >= 2.5'
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Topic :: Database',
+    ],
+    zip_safe=False
+)
