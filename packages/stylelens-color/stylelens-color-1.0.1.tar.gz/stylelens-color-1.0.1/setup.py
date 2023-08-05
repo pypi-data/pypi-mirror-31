302603e3850c09a7f8d82918172166c1f2cc0b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package stylelens-color/1.0.1/stylelens-color-1.0.1.tar.gz

---
+++ packages/stylelens-color/stylelens-color-1.0.1.tar.gz/stylelens-color-1.0.1/setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+"""
+    stylelens-feature
+
+    Feature extrator from a image file
+
+    OpenAPI spec version: 0.0.1
+    Contact: master@bluehack.net
+"""
+
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "stylelens-color"
+VERSION = "1.0.1"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", \
+            "tensorflow >= 1.3"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="stylelens-color",
+    author_email="lion@bluehack.net",
+    url="",
+    keywords=["Swagger", "stylelens-color"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    This is a API document for Object Detection on fashion items\&quot;
+    """
+)
