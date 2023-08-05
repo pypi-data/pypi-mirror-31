300e1382e332733d8a31c1f8e8bd39f8560e3139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:17 +0000

    Add package lemongo/1.0.0b4/lemongo-1.0.0b4-py3-none-any.whl

---
+++ packages/lemongo/lemongo-1.0.0b4-py3-none-any.whl/lemongo.py
@@ -0,0 +1,36 @@
+"""Provides a unified PyMongo interface for LeMaster plugins."""
+
+
+from argparse import ArgumentParser as _ArgumentParser
+from logging import getLogger as _getLogger
+
+from pymongo import MongoClient as _MongoClient
+
+
+_DEFAULT_CLIENT_VALUE = None
+_client = _DEFAULT_CLIENT_VALUE
+
+_parser = _ArgumentParser(fromfile_prefix_chars='@', add_help=False)
+
+_parser.add_argument(
+    '-m', '--mongodb-uri', help='MongoDB connection URI')
+
+_logger = _getLogger(__name__)
+
+
+def _reset_module_state():
+    global _client
+
+    _client = _DEFAULT_CLIENT_VALUE
+
+
+def get_client():
+    """Return a MongoClient instance."""
+    global _client
+
+    if not _client:
+        args, _ = _parser.parse_known_args()
+        _client = _MongoClient(args.mongodb_uri)
+        _logger.debug('A new client was created: %s', _client)
+
+    return _client
