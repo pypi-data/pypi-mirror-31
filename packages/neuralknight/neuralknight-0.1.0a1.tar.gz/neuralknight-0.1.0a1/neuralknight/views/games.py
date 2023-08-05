042db77052771858dc5403f870252b2a14706dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:48 +0000

    Add package neuralknight/0.1.0a1/neuralknight-0.1.0a1.tar.gz

---
+++ packages/neuralknight/neuralknight-0.1.0a1.tar.gz/neuralknight-0.1.0a1/neuralknight/views/games.py
@@ -0,0 +1,109 @@
+from cornice import Service
+from pyramid.httpexceptions import HTTPBadRequest
+
+from ..models.board import Board, NoBoard
+
+games = Service(
+    name='games',
+    path='/v1.0/games',
+    description='Create game')
+game_states = Service(
+    name='game_states',
+    path='/v1.0/games/{game}/states',
+    description='Game states')
+game_interaction = Service(
+    name='game_interaction',
+    path='/v1.0/games/{game}',
+    description='Game interaction')
+game_info = Service(
+    name='game_info',
+    path='/v1.0/games/{game}/info',
+    description='Game info')
+
+
+class BlankBoard:
+    def __str__(self):
+        return '\n' * 8
+
+    def add_player_v1(self, *args, **kwargs):
+        return {}
+
+    def current_state_v1(self, *args, **kwargs):
+        return {'state': [[0 for _ in range(8)] for _ in range(8)]}
+
+    def slice_cursor_v1(self, *args, **kwargs):
+        return {'cursor': None, 'boards': []}
+
+    def update_state_v1(self, *args, **kwargs):
+        return {'end': True}
+
+
+def get_game(request):
+    """
+    Retrieve board for request.
+    """
+    try:
+        return Board.get_game(request.matchdict['game'])
+    except NoBoard:
+        pass
+    return BlankBoard()
+
+
+@games.get()
+def get_games(request):
+    """
+    Retrieve all game ids.
+    """
+    return {'ids': list(Board.GAMES.keys())}
+
+
+@games.post()
+def post_game(request):
+    """
+    Create a new game and provide an id for interacting.
+    """
+    return {'id': Board().id}
+
+
+@game_states.get()
+def get_states(request):
+    """
+    Start or continue a cursor of next board states.
+    """
+    return get_game(request).slice_cursor_v1(**request.GET)
+
+
+@game_interaction.get()
+def get_state(request):
+    """
+    Provide current state on the board.
+    """
+    return get_game(request).current_state_v1()
+
+
+@game_interaction.post()
+def join_game(request):
+    """
+    Add player to board.
+    """
+    try:
+        user_id = request.json['id']
+    except KeyError:
+        raise HTTPBadRequest
+    return get_game(request).add_player_v1(request.dbsession, user_id)
+
+
+@game_interaction.put()
+def put_state(request):
+    """
+    Make a move to a new state on the board.
+    """
+    return get_game(request).update_state_v1(request.dbsession, **request.json)
+
+
+@game_info.get()
+def get_info(request):
+    """
+    Provide current state on the board.
+    """
+    return {'print': str(get_game(request))}
---
+++ packages/neuralknight/neuralknight-0.1.0a1.tar.gz/neuralknight-0.1.0a1/setup.py
@@ -0,0 +1,77 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'cornice',
+    'plaster_pastedeploy',
+    'psycopg2-binary',
+    'pyramid >= 1.9a',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
+    'pyramid_retry',
+    'pyramid_tm',
+    'requests',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+    'requests',
+]
+
+setup(
+    name='neuralknight',
+    version='0.1.0a1',
+    description='A Chess-playing AI',
+    long_description=README + '\n\n' + CHANGES,
+    author='David Snowberger, Shannon Tully, and Adam Grandquist',
+    author_email='david.snowberger@fartherout.org',
+    url='https://www.github.com/dsnowb/neuralknight',
+    licence='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment :: Board Games',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'License :: Freely Distributable',
+        'Framework :: Pyramid',
+        'Environment :: Console',
+    ],
+    keywords='chess entertainment game ai',
+    project_urls={
+        'Source': 'https://www.github.com/dsnowb/neuralknight',
+        'Tracker': 'https://www.github.com/dsnowb/neuralknight/issues',
+        'Remote API': 'https://neuralknight.heroku.com',
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    python_requires='>=3.5',
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = neuralknight:main',
+        ],
+        'console_scripts': [
+            'initialize_neuralknight_db = neuralknight.scripts.initializedb:main',
+            'neuralknight = neuralknight.scripts.cli:main',
+        ],
+    },
+)
