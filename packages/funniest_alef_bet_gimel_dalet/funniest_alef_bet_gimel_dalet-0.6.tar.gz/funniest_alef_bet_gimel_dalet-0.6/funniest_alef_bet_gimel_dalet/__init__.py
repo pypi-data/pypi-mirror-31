0f46355c5f2da2f148ec0e7563ad844f06a81a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package funniest_alef_bet_gimel_dalet/0.6/funniest_alef_bet_gimel_dalet-0.6.tar.gz

---
+++ packages/funniest_alef_bet_gimel_dalet/funniest_alef_bet_gimel_dalet-0.6.tar.gz/funniest_alef_bet_gimel_dalet-0.6/funniest_alef_bet_gimel_dalet/__init__.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+def joke():
+    x = np.sum([9,14])
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.'
+            u'ilyada roman olsun.',
+            x,x**2)
---
+++ packages/funniest_alef_bet_gimel_dalet/funniest_alef_bet_gimel_dalet-0.6.tar.gz/funniest_alef_bet_gimel_dalet-0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='funniest_alef_bet_gimel_dalet',
+      version='0.6',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Caesar Divi Filius Mortos Augustus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest_alef_bet_gimel_dalet'],
+      install_requires = ['numpy','pandas',],
+      zip_safe=False)
