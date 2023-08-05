fe43fe59fe116ab2c179e198e04c7e555cfd1a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package pytraj/2.0.3/pytraj-2.0.3-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/pytraj/pytraj-2.0.3-cp27-cp27mu-manylinux1_x86_64.whl/pytraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYTRAJ SETUP.PY
+short_version = '2.0.3'
+version = '2.0.3'
+full_version = '2.0.3'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
