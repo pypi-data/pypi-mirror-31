9380e6ca62bd887bcf28fef136efa26b8985a524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:01 +0000

    Add package sanjose-helloworld/0.1/sanjose-helloworld-0.1.tar.gz

---
+++ packages/sanjose-helloworld/sanjose-helloworld-0.1.tar.gz/sanjose-helloworld-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='sanjose-helloworld',
+    version='0.1',
+    description='Saying hello with a funny face',
+    author='sanjose',
+    scripts=['sanjose-helloworld']
+)
