8f06ba7ca4d10da2499feab03cac0686d4d2a72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:32 +0000

    Add package edpanalyst/0.1.14/edpanalyst-0.1.14.tar.gz

---
+++ packages/edpanalyst/edpanalyst-0.1.14.tar.gz/edpanalyst-0.1.14/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup  # type: ignore
+
+setup(
+    name='edpanalyst',
+    packages=['edpanalyst'],
+    version='0.1.14',
+    description='The python API to the Empirical Data Platform.',
+    license='Apache License 2.0',
+    install_requires=[
+        'beautifulsoup4', 'configparser', 'future', 'natsort', 'pandas',
+        'requests', 'typing', 'enum34', 'python-dateutil'
+    ]
+)  # yapf: disable
