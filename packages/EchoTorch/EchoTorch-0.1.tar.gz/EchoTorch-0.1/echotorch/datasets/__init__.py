36a91528a4c67413b61445bf0321288bdc847857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:08 +0000

    Add package EchoTorch/0.1/EchoTorch-0.1.tar.gz

---
+++ packages/EchoTorch/EchoTorch-0.1.tar.gz/EchoTorch-0.1/echotorch/datasets/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from .LogisticMapDataset import LogisticMapDataset
+from .MackeyGlassDataset import MackeyGlassDataset
+from .MemTestDataset import MemTestDataset
+from .NARMADataset import NARMADataset
+from .ReutersC50Dataset import ReutersC50Dataset
+from .SFGramDataset import SFGramDataset
+
+__all__ = [
+    'LogisticMapDataset', 'MackeyGlassDataset', 'MemTestDataset', 'NARMADataset', 'ReutersC50Dataset', 'SFGramDataset'
+]
---
+++ packages/EchoTorch/EchoTorch-0.1.tar.gz/EchoTorch-0.1/echotorch/nn/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from .ESNCell import ESNCell
+from .ESN import ESN
+from .LiESNCell import LiESNCell
+from .LiESN import LiESN
+from .ICACell import ICACell
+from .Identity import Identity
+from .PCACell import PCACell
+from .RRCell import RRCell
+from .SFACell import SFACell
+from .StackedESN import StackedESN
+
+__all__ = [
+    'ESNCell', 'ESN', 'LiESNCell', 'LiESN', 'ICACell', 'Identity', 'PCACell', 'RRCell', 'SFACell', 'StackedESN'
+]
