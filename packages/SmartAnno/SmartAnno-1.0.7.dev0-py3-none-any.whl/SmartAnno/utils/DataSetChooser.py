9629fdde9107b968bcca66fa6cc2bb247fbaba70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:02 +0000

    Add package SmartAnno/1.0.7.dev0/SmartAnno-1.0.7.dev0-py3-none-any.whl

---
+++ packages/SmartAnno/SmartAnno-1.0.7.dev0-py3-none-any.whl/SmartAnno/utils/DataSetChooser.py
@@ -0,0 +1,31 @@
+from IPython.core.display import display
+
+from SmartAnno.db.ORMs import Document, Task
+from SmartAnno.gui.PreviousNextWidgets import PreviousNextWithOptions
+
+
+class DataSetChooser(PreviousNextWithOptions):
+
+    def start(self):
+        self.updateBox()
+        display(self.box)
+        pass
+
+    def updateBox(self):
+        dao = self.workflow.dao
+        options = []
+        with dao.create_session() as session:
+            results = session.query(Document.DATASET_ID).distinct()
+            for res in results:
+                options.append(res.DATASET_ID)
+            value = session.query(Task.DATASET_ID).filter(Task.TASK_NAME == self.workflow.task_name).first()[0]
+        self.toggle.options = options
+        self.toggle.value = value
+
+        # display(self.box)
+        pass
+
+    def complete(self):
+        self.workflow.dataset_id = self.toggle.value
+        super().complete()
+        pass
