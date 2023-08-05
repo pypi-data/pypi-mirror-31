0d4467eaf79ab30e095d0ecb1c01bfe428ab07b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package q2k/1.0.8a1/q2k-1.0.8a1.tar.gz

---
+++ packages/q2k/q2k-1.0.8a1.tar.gz/q2k-1.0.8a1/q2k/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2018 2Cas
+# Licensed under the MIT license (http://opensource.org/licenses/MIT)
+
+from q2k.reference import *
+from q2k.core import *
+from q2k.gui import *
---
+++ packages/q2k/q2k-1.0.8a1.tar.gz/q2k-1.0.8a1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='q2k',
+    version='1.0.8.a1',
+    description='Convert AVR C based QMK keymap and matrix files to YAML Keyplus format',
+    url='https://github.com/2Cas/Q2K',
+    author='2Cas',
+    author_email='reasonstouninstall@tutanota.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Text Processing',
+        'License :: OSI Approved :: MIT License',
+    ],
+    license='MIT',
+    packages=find_packages(include=['q2k', 'q2k.*']),
+    python_requires='>=3.0',
+    install_requires=[
+        'pyyaml', 'pyparsing', 'termcolor'
+    ],
+    entry_points = {
+        'console_scripts': ['q2k-cli=q2k.core:q2keyplus', 'q2k=q2k.gui:main']  #, 'q2kb=q2k.core:q2kbfirmware']
+    },
+    keywords = ['keyboard', 'usb', 'hid', 'qmk', 'keyplus'],
+    zip_safe=False,
+    include_package_data=True,
+)
