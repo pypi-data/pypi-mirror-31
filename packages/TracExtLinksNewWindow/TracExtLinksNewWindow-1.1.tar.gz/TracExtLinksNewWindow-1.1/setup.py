8690ca029e5368ebd949877eb1c20a804fb260bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package TracExtLinksNewWindow/1.1/TracExtLinksNewWindow-1.1.tar.gz

---
+++ packages/TracExtLinksNewWindow/TracExtLinksNewWindow-1.1.tar.gz/TracExtLinksNewWindow-1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TracExtLinksNewWindow',
+    version='1.1',
+    description='Trac plugin to open external links in a new window.',
+    license='GPLv3',
+    zip_safe=False,
+    url='https://trac-hacks.org/wiki/ExternalLinksNewWindow',
+    download_url='https://pypi.python.org/pypi/ExternalLinksNewWindowPlugin',
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    packages=['tracextlinksnewwindow'],
+    package_data={
+        'tracextlinksnewwindow': ['htdocs/*.js'],
+    },
+    install_requires=['Trac'],
+    entry_points={
+        'trac.plugins': [
+            'tracextlinksnewwindow.plugin = tracextlinksnewwindow.plugin',
+        ],
+    }
+)
