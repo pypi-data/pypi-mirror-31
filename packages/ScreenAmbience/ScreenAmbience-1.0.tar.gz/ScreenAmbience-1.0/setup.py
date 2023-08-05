682ad6fc6736391c411355e81fd16c47445e6302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:10 +0000

    Add package ScreenAmbience/1.0/ScreenAmbience-1.0.tar.gz

---
+++ packages/ScreenAmbience/ScreenAmbience-1.0.tar.gz/ScreenAmbience-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup (
+    name = 'ScreenAmbience',
+    version = '1.0',
+    description = 'Matches zones on a LIFX-Z strip to the colors on your screen.',
+    url = 'https://github.com/EdwinP7/ScreenAmbience',
+    author = 'Edwin Perea',
+    license = 'MIT',
+    install_requires=['PIL'],
+    python_requires='>=3',
+)
