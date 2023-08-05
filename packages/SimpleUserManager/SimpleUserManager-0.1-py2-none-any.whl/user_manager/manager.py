28c4daf370e3e08e8c95fb36bf323dcaf394ce5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:14 +0000

    Add package SimpleUserManager/0.1/SimpleUserManager-0.1-py2-none-any.whl

---
+++ packages/SimpleUserManager/SimpleUserManager-0.1-py2-none-any.whl/user_manager/manager.py
@@ -0,0 +1,132 @@
+'''
+UserManager is a simple user manager which provides basic 
+functionality to manage groups of users.
+'''
+
+import os
+import json
+
+class UserManager(object):
+    """A very simple user Manager, that saves it's data as json."""
+    def __init__(self, path, file_name):
+        self.file = os.path.join(path, file_name)
+
+    def read(self):
+        if not os.path.exists(self.file):
+            return {}
+        with open(self.file) as f:
+            data = json.loads(f.read())
+        return data
+
+    def write(self, data):
+        with open(self.file, 'w') as f:
+            f.write(json.dumps(data, indent=2))
+
+    def add_user(self, name, password,
+                 active=True, roles=[], authentication_method=None):
+        users = self.read()
+        if users.get(name):
+            return False
+        if authentication_method is None:
+            authentication_method = 'cleartext'
+        new_user = {
+            'active': active,
+            'roles': roles,
+            'authentication_method': authentication_method,
+            'authenticated': False
+        }
+
+        if authentication_method == 'hash':
+            new_user['hash'] = make_salted_hash(password)
+        elif authentication_method == 'cleartext':
+            new_user['password'] = password
+        else:
+            raise NotImplementedError(authentication_method)
+        users[name] = new_user
+        self.write(users)
+        userdata = users.get(name)
+        return User(self, name, userdata)
+
+    def get_user(self, name):
+        users = self.read()
+        userdata = users.get(name)
+        if not userdata:
+            return None
+        return User(self, name, userdata)
+
+    def get_all_users(self):
+        users = self.read()
+        # users = users.viewkeys()
+        return users
+
+    def delete_user(self, name):
+        users = self.read()
+        if not users.pop(name, False):
+            return False
+        self.write(users)
+        return True
+
+    def update(self, name, userdata):
+        data = self.read()
+        data[name] = userdata
+        self.write(data)
+
+
+class User(object):
+    def __init__(self, manager, name, data):
+        self.manager = manager
+        self.name = name
+        self.data = data
+
+    def get(self, option):
+        return self.data.get(option)
+
+    def set(self, option, value):
+        self.data[option] = value
+        self.save()
+
+    def save(self):
+        self.manager.update(self.name, self.data)
+
+    def is_authenticated(self):
+        return self.data.get('authenticated')
+
+    def is_active(self):
+        return self.data.get('active')
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.name
+
+    def check_password(self, password):
+        """Return True, return False, or raise NotImplementedError if the
+        authentication_method is missing or unknown."""
+        authentication_method = self.data.get('authentication_method', None)
+        if authentication_method is None:
+            authentication_method = 'cleartext'
+        # See comment in UserManager.add_user about authentication_method.
+        if authentication_method == 'hash':
+            result = check_hashed_password(password, self.get('hash'))
+        elif authentication_method == 'cleartext':
+            result = (self.get('password') == password)
+        else:
+            raise NotImplementedError(authentication_method)
+        return result
+
+import hashlib
+import binascii
+
+def make_salted_hash(password, salt=None):
+    if not salt:
+        salt = os.urandom(64)
+    d = hashlib.sha512()
+    d.update(salt[:32])
+    d.update(password)
+    d.update(salt[32:])
+    return binascii.hexlify(salt) + d.hexdigest()
+
+def check_hashed_password(password, salted_hash):
+    salt = binascii.unhexlify(salted_hash[:128])
+    return make_salted_hash(password, salt) == salted_hash
