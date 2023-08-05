b3fa5a370849852c5f239757bdca50aa6e8993a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package ledge/0.0.6/ledge-0.0.6-py3-none-any.whl

---
+++ packages/ledge/ledge-0.0.6-py3-none-any.whl/ledge/datatypes.py
@@ -0,0 +1,14 @@
+"""
+This file is generated using an accompanying org file.
+Do not edit manually.
+"""
+
+import xarray as xr
+
+Truth = xr.DataArray
+
+Prediction = xr.DataArray
+
+Loss = xr.DataArray
+
+Weight = xr.DataArray
