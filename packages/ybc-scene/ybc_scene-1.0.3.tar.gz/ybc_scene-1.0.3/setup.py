89af5cc961f2520adac6c10ef106c9d8851c011c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package ybc-scene/1.0.3/ybc_scene-1.0.3.tar.gz

---
+++ packages/ybc-scene/ybc_scene-1.0.3.tar.gz/ybc_scene-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_scene',
+  packages = ['ybc_scene'],
+  version = '1.0.3',
+  description = 'Scene Object Recognition',
+  long_description='Scene Object Recognition',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','object','scene object recognition'],
+  license='MIT',
+  install_requires=['requests']
+)
