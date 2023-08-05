b8d686103088dc3d1b4c51e10f25daf7c3bce77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package essentia-sia-api/0.0.1a0/essentia-sia-api-0.0.1a0.tar.gz

---
+++ packages/essentia-sia-api/essentia-sia-api-0.0.1a0.tar.gz/essentia-sia-api-0.0.1a0/sia/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from sia.sia import Sia
+except ImportError:
+    from sia import Sia
+
+__authors__ = 'Dmytro Striletskyi, Alexander Ruban'
+__email__ = 'dev@essentia.one'
+__version__ = '0.0.1a0'
