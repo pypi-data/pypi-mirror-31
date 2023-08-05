537f970ad86802bb3f2f7e745a17839e332fec77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:13 +0000

    Add package wosclient/0.1.3/wosclient-0.1.3.tar.gz

---
+++ packages/wosclient/wosclient-0.1.3.tar.gz/wosclient-0.1.3/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click',
+    'requests'
+]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Michał Pasternak",
+    author_email='michal.dtz@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Web of Science Article Match Retrival Client",
+    entry_points={
+        'console_scripts': [
+            'wosclient=wosclient.cli:main',
+            'lookup_ids=wosclient.cli:lookup_ids',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='wosclient',
+    name='wosclient',
+    packages=find_packages(include=['wosclient']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/mpasternak/wosclient',
+    version='0.1.3',
+    zip_safe=False,
+)
