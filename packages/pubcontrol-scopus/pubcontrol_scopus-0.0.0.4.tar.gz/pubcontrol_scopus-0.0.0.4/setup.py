32348c2478a5d92ef2ae6c93e8e1a2fba43f89e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:07 +0000

    Add package pubcontrol-scopus/0.0.0.4/pubcontrol_scopus-0.0.0.4.tar.gz

---
+++ packages/pubcontrol-scopus/pubcontrol_scopus-0.0.0.4.tar.gz/pubcontrol_scopus-0.0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='pubcontrol_scopus',
+    version='0.0.0.4',
+    description='A tool for a wordpress server which will automatically post science publications from scopus database',
+    url='https://github.com/the16thpythonist/ScopusWp',
+    author='Jonas Teufel',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=['pubcontrol_scopus'],
+    include_package_data=True,
+    install_requires=[
+        'pubcontrol_core',
+        'requests>=2.0',
+        'mysqlclient>=1.2',
+        'unidecode>=0.4',
+        'tabulate>=0.8',
+        'python-wordpress-xmlrpc>=2.3',
+        'jinja2>=2.10',
+        'sqlalchemy>=1.2',
+        'memory_profiler',
+        'jutily'
+    ],
+    python_requires='>=3, <4',
+    package_data={
+        '': []
+    },
+)
