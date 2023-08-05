1b020116738fda4487bc4a31237c09e8a85e4465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:25 +0000

    Add package pymyob/0.4/pymyob-0.4.tar.gz

---
+++ packages/pymyob/pymyob-0.4.tar.gz/pymyob-0.4/setup.py
@@ -0,0 +1,37 @@
+import re
+
+from setuptools import setup
+
+# Get version this way, so that we don't load any modules.
+with open('./myob/__init__.py') as f:
+    exec(re.search(r'VERSION = .*', f.read(), re.DOTALL).group())
+
+try:
+    setup(
+        name='pymyob',
+        packages=['myob'],
+        version=__version__,
+        description="A Python API around MYOB's AccountRight API.",
+        license='BSD',
+        author='Jarek Głowacki',
+        author_email='jarekwg@gmail.com',
+        url='https://github.com/uptick/pymyob',
+        keywords=['myob'],
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Office/Business',
+        ],
+        install_requires=[
+            'requests>=2.10.0',
+            'requests-oauthlib>=0.6.0',
+        ],
+        test_suite="tests",
+    )
+except NameError:
+    raise RuntimeError("Unable to determine version.")
