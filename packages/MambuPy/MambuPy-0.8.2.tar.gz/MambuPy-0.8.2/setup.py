1173f7cc9f60480ccaa7e6de0c115afee1efd8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package MambuPy/0.8.2/MambuPy-0.8.2.tar.gz

---
+++ packages/MambuPy/MambuPy-0.8.2.tar.gz/MambuPy-0.8.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:license: GPLv3, see LICENSE for more details.
+"""
+
+import os, sys
+import setuptools
+
+VERSION = "0.8.2"
+
+
+def readme():
+    """print long description"""
+    with open('MambuPy/README.md') as f:
+        return f.read()
+
+
+class VerifyVersionCommand():
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            sys.exit(info)
+
+
+setuptools.setup(name             = "MambuPy",
+                 version          = VERSION,
+                 description      = "A python lib for using Mambu APIs.",
+                 long_description = readme(),
+                 url              = "https://jstitch.github.io/MambuPy",
+                 author           = "Javier Novoa C.",
+                 author_email     = "jstitch@gmail.com",
+                 license          = "GPLv3",
+                 keywords         = ["mambu",],
+                 packages         = ['MambuPy',],
+                 python_requires  = '>=2.7',
+                 cmdclass         = {'verify' : VerifyVersionCommand},
+    )
