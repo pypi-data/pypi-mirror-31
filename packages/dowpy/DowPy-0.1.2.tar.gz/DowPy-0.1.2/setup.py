b913406a1e6c464aba03d3d015d9c57f222fc0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:05 +0000

    Add package dowpy/0.1.2/DowPy-0.1.2.tar.gz

---
+++ packages/dowpy/DowPy-0.1.2.tar.gz/DowPy-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+
+setup(name='DowPy',
+      version='0.1.2',
+      description='Module for downloading HTTP(s) files efficiently',
+      url='http://github.com/jhnbrunelle/dowpy',
+      author='JohnBrunelle',
+      author_email='devjohnb@gmail.com',
+      license='MIT',
+      packages=['dowpy'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      install_requires=['requests'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
