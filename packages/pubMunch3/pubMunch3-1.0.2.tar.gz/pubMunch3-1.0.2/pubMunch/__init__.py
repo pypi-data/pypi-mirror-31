8869f35d427edb54138bf599ea90f8960e0f2e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package pubMunch3/1.0.2/pubMunch3-1.0.2.tar.gz

---
+++ packages/pubMunch3/pubMunch3-1.0.2.tar.gz/pubMunch3-1.0.2/pubMunch/__init__.py
@@ -0,0 +1,22 @@
+import argparse
+
+from . import pubCrawlLib
+
+def download_pmid(pmid):
+    meta = pubCrawlLib.downloadPubmedMeta(pmid)
+    pdf_file = pubCrawlLib.crawlOneDoc(meta, doc_type='pdf')
+    return pdf_file
+
+def download_pmid_program():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('pmid', type=str, help='The pubmed id to download')
+
+    args = parser.parse_args()
+
+    print(args)
+
+    pdf_file = download_pmid(args.pmid)
+
+    with open('{}.pdf'.format(args.pmid), 'wb') as file:
+        file.write(pdf_file)
+
