b7f22c346c8b1a1300f90f9bec1abf0a2d4fb828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package django-cal/0.3.0/django-cal-0.3.0.tar.gz

---
+++ packages/django-cal/django-cal-0.3.0.tar.gz/django-cal-0.3.0/README.md
@@ -0,0 +1,104 @@
+# django-cal #
+
+Django app to enable exporting of events to iCalendar files.
+Imitates behavior of django.contrib.syndication and is based upon vobject
+(<http://vobject.skyhouseconsulting.com/>).
+
+Heavy inspiration came from Christian Joergensen
+(<http://www.technobabble.dk/2008/mar/06/exposing-calendar-events-using-icalendar-django/>)
+and Derek Willis (<http://blog.thescoop.org/archives/2007/07/31/django-ical-and-vobject/>).
+
+This project is loosely maintained and contributions will be happily accepted, but no feature 
+developments are planned. 
+
+`django-cal` has been used successfully with Django 1.3 - 1.11 and Python 2.7 & 3.6.
+
+## Installation ##
+
+    pip install django-cal
+
+## Documentation ##
+
+### Overview ###
+
+Please see Django's syndication feed framework documentation, django_cal imitates its
+behavior: <https://docs.djangoproject.com/en/dev/ref/contrib/syndication/>.
+
+
+### Defining event properties ###
+
+The following parameters work analagous to how they're implemented in
+django.contrib.syndication. That means, the framework checks in the following
+order: `self.$param(obj)`, `self.$param()`, `self.$param`; `obj` being the object
+returned by `self.get_object`.
+
+    items           Returns the list of events.
+                    Must be set.
+    filename        Filename of the file returned in the view.
+                    Optional, defaults to 'events.ics'.
+    cal_name        Name of the calendar.
+                    Optional, defaults to None.
+    cal_desc        Description of the calendar.
+                    Optional, defaults to None.
+
+    item_summary    The "title" of the item.
+                    Optional, defaults to unicode representation of item.
+
+    item_end        Duration or end time of item.
+    item_duration   Optional, defaults to None. Must not define both.
+
+    item_rruleset   Optional, defaults to None.
+                    Should return dateutil.rruleset instance
+                    for recurrent events.
+
+    item_url        Optional, default calls item.get_absolute_url()
+                    Should return a URL with the fully-qualified domain and
+                    protocol (e.g. 'http://www.example.com/blog/') or an
+                    absolute path (e.g. '/events/'). If only a path is
+                    present, the 'django.contrib.sites' app will be used
+                    to insert the domain of the current site.
+                    Note: To find the current site, 'django.contrib.sites'
+                          must be in your settings.INSTALLED_APPS (it is
+                          there by default)
+
+    item_uid        All correspond to their vEvent equivalents.
+    item_start      All optional, all default to None.
+    item_description
+    item_categories
+    item_comment
+    item_location
+    item_last_modified
+    item_created
+
+### Duration of events ###
+
+django-cal imitates vobject behavior regarding start and end of events. In short:
+Use Date objects for all-day events, DateTime for more granular control.
+Define either duration or end time, never both.
+
+## Timezones ###
+
+If you need timezone support, use `pytz.timezone` to create an "aware" datetime object for
+`item_start` and `item_end` and set it to UTC. A user reported that Gmail, Outlook,
+Apple Mail, etc. are properly displaying it in the user's local timezone upon receipt.
+
+Example::
+```
+from pytz import timezone
+
+# dt is a naive datetime object known to represent US/Eastern time
+loc_dt = timezone('US/Eastern').localize(dt)
+utc = timezone('UTC')
+aware_datetime = loc_dt.astimezone(utc)
+```
+
+### Complex behavior ###
+
+`self.get_object` can be overridden to allow for more complex events, as is possible for
+[syndication feeds](https://docs.djangoproject.com/en/1.6/ref/contrib/syndication/#a-complex-example).
+
+## Dependencies ##
+
+  * [vObject](http://vobject.skyhouseconsulting.com/)
+  * [dateutil](http://labix.org/python-dateutil/)
+  * [Django](http://djangoproject.com/)
---
+++ packages/django-cal/django-cal-0.3.0.tar.gz/django-cal-0.3.0/django_cal/views.py
@@ -0,0 +1,172 @@
+from __future__ import unicode_literals, absolute_import
+
+import vobject
+from django.db.models import ObjectDoesNotExist
+from django.http import HttpResponse, Http404
+from django.conf import settings
+from django.utils import six
+
+from django.contrib.syndication.views import add_domain
+import django
+
+
+if add_domain.__code__.co_argcount < 3:
+    # Django <= 1.2
+    # Source: Django 1.4 django.contrib.syndication.views
+    from django.utils.encoding import iri_to_uri
+
+    def add_domain(domain, url, secure=False):
+        protocol = 'https' if secure else 'http'
+        if url.startswith('//'):
+            # Support network-path reference (see #16753)
+            # RSS requires a protocol
+            url = '{}:{}'.format(protocol, url)
+        elif (
+            not url.startswith('http://')
+            or url.startswith('https://')
+            or url.startswith('mailto:')
+        ):
+            # 'url' must already be ASCII and URL-quoted, so no need
+            # for encoding conversions here.
+            url = iri_to_uri('{}://{}{}'.format(protocol, domain, url))
+        return url
+
+if 'django.contrib.sites' in settings.INSTALLED_APPS:
+    if django.VERSION >= (1, 7):
+        from django.contrib.sites.shortcuts import get_current_site
+    elif django.VERSION >= (1, 3):
+        # Django >= 1.3
+        from django.contrib.sites.models import get_current_site
+    else:
+        # Django <= 1.2
+        # Source: Django 1.4 django.contrib.sites.models
+        from django.contrib.sites.models import Site, RequestSite
+
+        def get_current_site(request):
+            """
+            Checks if contrib.sites is installed and returns either the current
+            ``Site`` object or a ``RequestSite`` object based on the request.
+            """
+            if Site._meta.installed:
+                current_site = Site.objects.get_current()
+            else:
+                current_site = RequestSite(request)
+            return current_site
+else:
+    get_current_site = None
+
+
+# Mapping of iCalendar event attributes to prettier names.
+EVENT_ITEMS = (
+    ('uid', 'item_uid'),
+    ('dtstart', 'item_start'),
+    ('dtend', 'item_end'),
+    ('duration', 'item_duration'),
+    ('summary', 'item_summary'),
+    ('description', 'item_description'),
+    ('location', 'item_location'),
+    ('url', 'item_url'),
+    ('comment', 'item_comment'),
+    ('last-modified', 'item_last_modified'),
+    ('created', 'item_created'),
+    ('categories', 'item_categories'),
+    ('rruleset', 'item_rruleset')
+)
+
+
+class Events(object):
+    def __call__(self, request, *args, **kwargs):
+        """ Makes Events callable for easy use in your urls.py """
+        try:
+            obj = self.get_object(request, *args, **kwargs)
+        except ObjectDoesNotExist:
+            raise Http404('Events object does not exist.')
+        ical = self.get_ical(obj, request)
+        response = HttpResponse(
+            ical.serialize(),
+            content_type='text/calendar;charset={}'.format(
+                settings.DEFAULT_CHARSET
+            ))
+        filename = self.__get_dynamic_attr('filename', obj)
+        # following added for IE, see
+        # http://blog.thescoop.org/archives/2007/07/31/django-ical-and-vobject/
+        response['Filename'] = filename
+        response['Content-Disposition'] = 'attachment; filename={}'.format(
+            filename
+        )
+        return response
+
+    def __get_dynamic_attr(self, attname, obj, default=None):
+        """ Returns first defined occurence of the following:
+                self.$attname(obj)
+                self.$attname()
+                self.$attname
+                default
+            Taken from django.contrib.syndication.views.Feed
+        """
+        try:
+            attr = getattr(self, attname)
+        except AttributeError:
+            return default
+        if callable(attr):
+            # Check co_argcount rather than try/excepting the function and
+            # catching the TypeError, because something inside the function
+            # may raise the TypeError. This technique is more accurate.
+            try:
+                code = six.get_function_code(attr)
+            except AttributeError:
+                code = six.get_function_code(attr.__call__)
+            if code.co_argcount == 2:       # one argument is 'self'
+                return attr(obj)
+            else:
+                return attr()
+        return attr
+
+    def get_ical(self, obj, request):
+        """ Returns a populated iCalendar instance. """
+        cal = vobject.iCalendar()
+        cal.add('method').value = 'PUBLISH'  # IE/Outlook needs this
+        items = self.__get_dynamic_attr("items", obj)
+        cal_name = self.__get_dynamic_attr("cal_name", obj)
+        cal_desc = self.__get_dynamic_attr("cal_desc", obj)
+        # Add calendar name and description if set
+        if cal_name:
+            cal.add('x-wr-calname').value = cal_name
+        if cal_desc:
+            cal.add('x-wr-caldesc').value = cal_desc
+
+        if get_current_site:
+            current_site = get_current_site(request)
+        else:
+            current_site = None
+
+        for item in items:
+            event = cal.add('vevent')
+            for vkey, key in EVENT_ITEMS:
+                value = self.__get_dynamic_attr(key, item)
+                if value:
+                    if vkey == 'rruleset':
+                        event.rruleset = value
+                    else:
+                        if vkey == 'url' and current_site:
+                            value = add_domain(
+                                current_site.domain,
+                                value,
+                                request.is_secure(),
+                            )
+                        event.add(vkey).value = value
+        return cal
+
+    # ONLY DEFAULT PARAMETERS FOLLOW #
+
+    def get_object(self, request, *args, **kwargs):
+        return None
+
+    def item_summary(self, item):
+        return item
+
+    def item_url(self, item):
+        return getattr(item, 'get_absolute_url', lambda: None)()
+
+    def filename(self, item):
+        return "events.ics"
