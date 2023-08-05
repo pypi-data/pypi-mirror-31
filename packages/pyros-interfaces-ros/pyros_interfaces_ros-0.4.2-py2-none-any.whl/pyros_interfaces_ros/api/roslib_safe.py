1bbbbdff5f7dadf06445af5d1e95a5400692ebc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package pyros-interfaces-ros/0.4.2/pyros_interfaces_ros-0.4.2-py2-none-any.whl

---
+++ packages/pyros-interfaces-ros/pyros_interfaces_ros-0.4.2-py2-none-any.whl/pyros_interfaces_ros/api/roslib_safe.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import socket
+import time
+import roslib
+
+
+# We wrap rospy function into safeguards for socket error
+# since the master seems to be quite sensitive to Network health.
+# TODO : refine which ones need which exception handling...
+
+def load_manifest(package_name):
+    roslib.load_manifest(package_name)
