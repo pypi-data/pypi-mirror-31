5d4be0e77dc384c62f5dbcc9ba34f35c772d30d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:45 +0000

    Add package tada0-toddler-neural-network/1.1.2/tada0-toddler-neural-network-1.1.2.tar.gz

---
+++ packages/tada0-toddler-neural-network/tada0-toddler-neural-network-1.1.2.tar.gz/tada0-toddler-neural-network-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tada0-toddler-neural-network',
+    version='1.1.2',
+    url='https://github.com/Tada0/Toddler-Neural-Network',
+    description='Little neural network library',
+    author='Tomasz Hołda',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tnn=toddlernetwork.__main__:main'
+            ]
+        },
+    install_requires=['numpy'],
+)
