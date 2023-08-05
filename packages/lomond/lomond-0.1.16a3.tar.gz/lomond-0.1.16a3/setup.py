02f9c6c3dc3fd3a7d4364203d8cb0d7fb4178b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:41 +0000

    Add package lomond/0.1.16a3/lomond-0.1.16a3.tar.gz

---
+++ packages/lomond/lomond-0.1.16a3.tar.gz/lomond-0.1.16a3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet'
+]
+
+# http://bit.ly/2alyerp
+with open('lomond/_version.py') as f:
+    exec(f.read())
+
+
+setup(
+    name='lomond',
+    version=__version__,
+    description="WebSocket Client Library",
+    author='WildFoundry',
+    author_email='willmcgugan@gmail.com',
+    url='https://github.com/wildfoundry/dataplicity-lomond',
+    platforms=['any'],
+    packages=find_packages(),
+    classifiers=classifiers,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[
+        'six>=1.10.0',
+    ],
+    zip_safe=True
+)
