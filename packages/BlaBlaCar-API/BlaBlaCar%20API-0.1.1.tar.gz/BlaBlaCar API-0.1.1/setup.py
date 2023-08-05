f785e68d7041351f351c6d33293df9892a27b172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package BlaBlaCar-API/0.1.1/BlaBlaCar%20API-0.1.1.tar.gz

---
+++ packages/BlaBlaCar-API/BlaBlaCar%20API-0.1.1.tar.gz/BlaBlaCar API-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='BlaBlaCar API',
+    version='0.1.1',
+
+    description='BlaBlaCar Client Api',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/arrrlo/BlaBlaCar-Client-Api',
+    licence='MIT',
+
+    author='Ivan Arar',
+    author_email='ivan.arar@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='blablacar, api, trip, distance',
+
+    packages=['blablacarapi'],
+    install_requires=[
+        'requests ~= 2.11.0',
+        'click ~= 6.7',
+        'colorama~=0.3'
+    ],
+
+    project_urls={
+        'Source': 'https://github.com/arrrlo/BlaBlaCar-Client-Api',
+    },
+)
