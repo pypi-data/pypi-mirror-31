fc53e10494e65d69d84a8fd723a4e93bfc773ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:40 +0000

    Add package infi.multipathtools/0.1.37/infi.multipathtools-0.1.37.tar.gz

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.37.tar.gz/infi.multipathtools-0.1.37/src/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.37"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.37)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '51669251faf0f9a0443934fa351fdcad724042c5'
+__git_head_subject__ = u'Finished Release v0.1.37'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
