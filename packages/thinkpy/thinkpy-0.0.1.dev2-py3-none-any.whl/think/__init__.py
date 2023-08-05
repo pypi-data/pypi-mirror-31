8f9a5ceb10e20465464ee44ecaaf0fe6c8609fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package thinkpy/0.0.1.dev2/thinkpy-0.0.1.dev2-py3-none-any.whl

---
+++ packages/thinkpy/thinkpy-0.0.1.dev2-py3-none-any.whl/think/__init__.py
@@ -0,0 +1,7 @@
+from think.agent import *
+from think.item import *
+from think.memory import *
+from think.vision import *
+from think.motor import *
+from think.speech import *
+from think.analysis import *
