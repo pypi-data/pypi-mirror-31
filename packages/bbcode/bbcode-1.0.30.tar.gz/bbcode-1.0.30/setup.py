8c848b8c94f561da6e43a1237483e5cdea321326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package bbcode/1.0.30/bbcode-1.0.30.tar.gz

---
+++ packages/bbcode/bbcode-1.0.30.tar.gz/bbcode-1.0.30/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import bbcode
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='bbcode',
+    version=bbcode.__version__,
+    description='A pure python bbcode parser and formatter.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dan Watson',
+    author_email='dcwatson@gmail.com',
+    url='https://github.com/dcwatson/bbcode',
+    license='BSD',
+    py_modules=['bbcode'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 3",
+        'Topic :: Text Processing :: Markup',
+    ]
+)
