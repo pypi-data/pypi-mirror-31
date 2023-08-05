0491eec8df549e8dfa6737abcb000350a2e27ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:34 +0000

    Add package bi_powerhouse/0.1/bi_powerhouse-0.1.tar.gz

---
+++ packages/bi_powerhouse/bi_powerhouse-0.1.tar.gz/bi_powerhouse-0.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Main setup file"""
+
+from setuptools import setup
+
+##########################################
+##########################################
+##########################################
+##########################################
+
+setup(name='bi_powerhouse',
+      version='0.1',
+      description='Functions to simplify day to day BI work.',
+      url='https://github.com/ksco92/bi_powerhouse',
+      author='Rodrigo Carvajal',
+      author_email='rodrigocf_92@hotmail.com',
+      license='MIT',
+      packages=['bi_powerhouse'],
+      zip_safe=False)
