ec9f1809d5dea51f4821bb6dc58eb6b463f312fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package funniest_alef_bet_gimel_dalet/0.3/funniest_alef_bet_gimel_dalet-0.3.tar.gz

---
+++ packages/funniest_alef_bet_gimel_dalet/funniest_alef_bet_gimel_dalet-0.3.tar.gz/funniest_alef_bet_gimel_dalet-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_alef_bet_gimel_dalet',
+      version='0.3',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Caesar Divi Filius Mortos Augustus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest_alef_bet_gimel_dalet'],
+      zip_safe=False)
