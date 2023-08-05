b75b6db0217a63160876d8ef7487031bd11589d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package matrix-client/0.2.0/matrix_client-0.2.0-py2.py3-none-any.whl

---
+++ packages/matrix-client/matrix_client-0.2.0-py2.py3-none-any.whl/matrix_client/errors.py
@@ -0,0 +1,32 @@
+class MatrixError(Exception):
+    """A generic Matrix error. Specific errors will subclass this."""
+    pass
+
+
+class MatrixUnexpectedResponse(MatrixError):
+    """The home server gave an unexpected response. """
+
+    def __init__(self, content=""):
+        super(MatrixError, self).__init__(content)
+        self.content = content
+
+
+class MatrixRequestError(MatrixError):
+    """ The home server returned an error response. """
+
+    def __init__(self, code=0, content=""):
+        super(MatrixRequestError, self).__init__("%d: %s" % (code, content))
+        self.code = code
+        self.content = content
+
+
+class MatrixHttpLibError(MatrixError):
+    """The library used for http requests raised an exception."""
+
+    def __init__(self, original_exception, method, endpoint):
+        super(MatrixHttpLibError, self).__init__(
+            "Something went wrong in {} requesting {}: {}".format(method,
+                                                                  endpoint,
+                                                                  original_exception)
+        )
+        self.original_exception = original_exception
---
+++ packages/matrix-client/matrix_client-0.2.0-py2.py3-none-any.whl/matrix_client/user.py
@@ -0,0 +1,50 @@
+class User(object):
+    """ The User class can be used to call user specific functions.
+    """
+    def __init__(self, api, user_id, displayname=None):
+        if not user_id.startswith("@"):
+            raise ValueError("UserIDs start with @")
+
+        if ":" not in user_id:
+            raise ValueError("UserIDs must have a domain component, seperated by a :")
+
+        self.user_id = user_id
+        self.displayname = displayname
+        self.api = api
+
+    def get_display_name(self):
+        """ Get this users display name.
+            See also get_friendly_name()
+
+        Returns:
+            str: Display Name
+        """
+        if not self.displayname:
+            self.displayname = self.api.get_display_name(self.user_id)
+        return self.displayname
+
+    def get_friendly_name(self):
+        display_name = self.api.get_display_name(self.user_id)
+        return display_name if display_name is not None else self.user_id
+
+    def set_display_name(self, display_name):
+        """ Set this users display name.
+
+        Args:
+            display_name (str): Display Name
+        """
+        self.displayname = display_name
+        return self.api.set_display_name(self.user_id, display_name)
+
+    def get_avatar_url(self):
+        mxcurl = self.api.get_avatar_url(self.user_id)
+        url = self.api.get_download_url(mxcurl)
+        return url
+
+    def set_avatar_url(self, avatar_url):
+        """ Set this users avatar.
+
+        Args:
+            avatar_url (str): mxc url from previously uploaded
+        """
+        return self.api.set_avatar_url(self.user_id, avatar_url)
