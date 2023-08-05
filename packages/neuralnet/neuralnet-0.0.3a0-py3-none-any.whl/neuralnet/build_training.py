2b553e6dc1bdd2f2f9571ec890375a13b9f4f791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:11 +0000

    Add package neuralnet/0.0.3a0/neuralnet-0.0.3a0-py3-none-any.whl

---
+++ packages/neuralnet/neuralnet-0.0.3a0-py3-none-any.whl/neuralnet/build_training.py
@@ -0,0 +1,30 @@
+import theano
+import time
+
+from neuralnet import ConfigParser
+
+
+class Training(ConfigParser):
+    def __init__(self, config_file, **kwargs):
+        super(Training, self).__init__(config_file, **kwargs)
+
+        self.n_epochs = self.config['training']['n_epochs']
+        self.continue_training = self.config['training']['continue']
+        self.batch_size = self.config['training']['batch_size']
+        self.validation_frequency = self.config['training']['validation_frequency']
+        self.validation_batch_size = self.config['training']['validation_batch_size']
+        self.extract_params = self.config['training']['extract_params']
+        self.param_file = self.config['training']['param_file']
+        self.testing_batch_size = self.config['testing']['batch_size']
+
+    def compile(self, inputs, outputs=None, mode=None, updates=None, givens=None, no_default_updates=False,
+                accept_inplace=False, name='theano_function', rebuild_strict=True, allow_input_downcast=False, profile=None,
+                on_unused_input='warn'):
+        start_time = time.time()
+        print('Compiling %s graph...' % name)
+        f = theano.function(inputs, outputs=outputs, mode=mode, updates=updates, givens=givens,
+                               no_default_updates=no_default_updates, accept_inplace=accept_inplace, name=name,
+                               rebuild_strict=rebuild_strict, allow_input_downcast=allow_input_downcast, profile=profile,
+                               on_unused_input=on_unused_input)
+        print('Compilation took %.2f minutes.' % ((time.time() - start_time) / 60.))
+        return f
