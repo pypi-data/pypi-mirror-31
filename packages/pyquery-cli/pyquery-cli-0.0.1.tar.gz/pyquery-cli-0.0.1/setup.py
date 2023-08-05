f4a38c0e68c7c2f2a42fe64dbf5c3e5c708ed465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:53 +0000

    Add package pyquery-cli/0.0.1/pyquery-cli-0.0.1.tar.gz

---
+++ packages/pyquery-cli/pyquery-cli-0.0.1.tar.gz/pyquery-cli-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyquery-cli',
+    version='0.0.1',
+    scripts=['pyquery-cli'],
+    author='Fernando Iazeolla',
+    uthor_email='xnando@yahoo.it',
+    description='A command line interface for JQuery like search and extract data from HTML. see the README.md file.',
+    keywords=['jquery', 'pyquery', 'html'],
+    install_requires=['pyquery'],
+    license='L-Beerware',
+    url='https://github.com/elboza/PyQuery',
+    download_url='https://github.com/elboza/PyQuery/archive/0.0.1.tar.gz',
+    classifiers=["Programming Language :: Python", "Programming Language :: Python :: 3", "Operating System :: OS Independent"]
+)
