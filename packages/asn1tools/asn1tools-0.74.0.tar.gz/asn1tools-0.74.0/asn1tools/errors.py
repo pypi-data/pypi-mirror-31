ee5be807a2645e64ab88d5eb6dbe38b50606fa29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:02 +0000

    Add package asn1tools/0.74.0/asn1tools-0.74.0.tar.gz

---
+++ packages/asn1tools/asn1tools-0.74.0.tar.gz/asn1tools-0.74.0/asn1tools/errors.py
@@ -0,0 +1,27 @@
+"""Exceptions.
+
+"""
+
+
+class CompileError(Exception):
+    """General ASN.1 compile error.
+
+    """
+
+
+class EncodeError(Exception):
+    """General ASN.1 encode error.
+
+    """
+
+
+class DecodeError(Exception):
+    """General ASN.1 decode error.
+
+    """
+
+
+class ConstraintsError(Exception):
+    """General ASN.1 constraints.
+
+    """
