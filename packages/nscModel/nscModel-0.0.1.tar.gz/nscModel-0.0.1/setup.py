1e4cba2b4a184d80f4e0cd26778827b7eb460513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package nscModel/0.0.1/nscModel-0.0.1.tar.gz

---
+++ packages/nscModel/nscModel-0.0.1.tar.gz/nscModel-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'nscModel',
+  version = '0.0.1',
+  py_modules = ['nscModel'],
+  author = 'nishaochun',
+  author_email = 'nishaochn@189.cn',
+  url = 'http://www.jianai.hk',
+  description = 'nsc is public',
+)
