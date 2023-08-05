dcf2a60b9676ecee95dd86e96d60da44dcafa3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package ctbf/0.1.4/ctbf-0.1.4.tar.gz

---
+++ packages/ctbf/ctbf-0.1.4.tar.gz/ctbf-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+     name='ctbf',    # This is the name of your PyPI-package.
+     version='0.1.4',
+     description='Crypto Trading Bot Platform',
+     url='http://github.com/storborg/funniest',
+     author='Sergey Bryukov',
+     author_email='sbryukov@gmail.com',
+     license='MIT',
+     packages=['ctbf'],
+     zip_safe=False
+     )
