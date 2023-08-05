8d0b9c37dff8af020b01575bcf8af73319444e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package arlpy/1.2.0/arlpy-1.2.0.tar.gz

---
+++ packages/arlpy/arlpy-1.2.0.tar.gz/arlpy-1.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='arlpy',
+    version='1.2.0',
+    description='ARL Python Tools',
+    long_description=readme,
+    author='Mandar Chitre',
+    author_email='mandar@arl.nus.edu.sg',
+    url='https://github.com/org-arl/arlpy',
+    license='BSD (3-clause)',
+    keywords='underwater acoustics signal processing communication',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'numpy>=1.11',
+        'scipy>=0.18',
+        'utm>=0.4'
+    ]
+)
