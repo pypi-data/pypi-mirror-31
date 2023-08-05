442b21b34d326a73c808471fff3cfb3ac84c0480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:08 +0000

    Add package pysctp3/0.7/pysctp3-0.7.tar.gz

---
+++ packages/pysctp3/pysctp3-0.7.tar.gz/pysctp3-0.7/setup.py
@@ -0,0 +1,37 @@
+"""
+setup.py
+
+Created by Philippe Langlois on 2009-11-02.
+Copyright (c) 2009 Philippe Langlois. All rights reserved.
+"""
+
+from distutils.core import setup, Extension
+
+setup(name='pysctp3',
+      version='0.7',
+      license = "LGPL",
+      description = 'pysctp is a python module for the SCTP protocol stack and library. ',
+      long_description = 'pysctp is a python module for the SCTP protocol stack and library. It is the socket api implementation, not the SCTPlib implementation (this latest one is not supported anymore by their original developers). On Mac OS X you will need the SCTP NKE (Kernel Extensions) to make it work, use: http://sctp.fh-muenster.de/sctp-nke.html. On Debian-based systems, you need a SCTP-aware kernel (most are) and install the following packages: apt-get install libsctp-dev libsctp1 lksctp-tools',
+      url = "http://www.epx.com.br/pysctp/",
+      keywords = "SCTP SIGTRAN",
+      platforms = ["Linux", "Debian", "Ubuntu", "Mac OS X (tested on 10.4)"],
+      classifiers = ['Intended Audience :: Developers',
+                     'Natural Language :: English',
+                     'Operating System :: POSIX',
+                     'Programming Language :: C',
+                     'Programming Language :: Python :: 3.6',
+                     'Topic :: Software Development :: Libraries',
+                     'Topic :: System :: Networking'],
+      py_modules=['sctp'],
+      ext_modules=[Extension('_sctp', sources=['_sctp.c'],
+                             include_dirs=['.', '/usr/include'],
+                             libraries=['sctp'],
+                             library_dirs=['/usr/lib/', '/usr/local/lib/'],
+                            )
+                  ],
+      data_files=[('include', ['_sctp.h'])],
+      author='Elvis Pfutzenreuter',
+      author_email='epx@epx.com.br',
+      maintainer='Philippe Langlois',
+      maintainer_email='Philippe.Langlois@gmail.com',
+     )
