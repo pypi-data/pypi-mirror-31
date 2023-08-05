2a85eed62f53ca28a88121f62b126791c4d8f67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:51 +0000

    Add package modelvis/0.1.4/modelvis-0.1.4.tar.gz

---
+++ packages/modelvis/modelvis-0.1.4.tar.gz/modelvis-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name="modelvis",
+  version="0.1.4",
+  description="Visualising Machine Learning Model",
+  license="MIT",
+  install_requires=[
+    'numpy',
+    'pandas',
+    'scikit-learn',
+    'matplotlib',
+    'requests',
+    'seaborn'
+  ],
+  py_modules=['modelvis'],
+  author="Amit Kapoor",
+  author_email="amitkaps@gmail.com",
+  platforms="any"
+)
