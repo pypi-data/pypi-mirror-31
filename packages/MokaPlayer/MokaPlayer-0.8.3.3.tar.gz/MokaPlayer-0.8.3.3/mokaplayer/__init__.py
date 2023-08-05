8773057bdc209eae8d2d0ef8eaf596daafb86758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:14 +0000

    Add package MokaPlayer/0.8.3.3/MokaPlayer-0.8.3.3.tar.gz

---
+++ packages/MokaPlayer/MokaPlayer-0.8.3.3.tar.gz/MokaPlayer-0.8.3.3/mokaplayer/__init__.py
@@ -0,0 +1,8 @@
+from mokaplayer.application import Application
+
+__version__ = '0.8.3.3'
+
+
+def main():
+    app = Application()
+    app.run()
---
+++ packages/MokaPlayer/MokaPlayer-0.8.3.3.tar.gz/MokaPlayer-0.8.3.3/mokaplayer/ui/gtk/adapter/adapter_tab.py
@@ -0,0 +1,20 @@
+from gi.repository import Gtk
+
+
+class AdapterTab:
+    @staticmethod
+    def create_row(tab):
+        return [
+            str(tab['type']),
+            tab['name'],
+            tab['rating'],
+            tab['url']
+        ]
+
+    @staticmethod
+    def create_store():
+        return Gtk.ListStore(str, str, int, str)
+
+    @staticmethod
+    def create_col_number():
+        return [0, 1, 2, 3]
