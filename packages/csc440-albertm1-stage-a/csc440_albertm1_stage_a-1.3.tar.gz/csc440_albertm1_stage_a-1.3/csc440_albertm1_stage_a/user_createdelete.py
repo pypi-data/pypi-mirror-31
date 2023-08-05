8def0878025148f5125497710d4b28a33a1a2274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package csc440-albertm1-stage-a/1.3/csc440_albertm1_stage_a-1.3.tar.gz

---
+++ packages/csc440-albertm1-stage-a/csc440_albertm1_stage_a-1.3.tar.gz/csc440_albertm1_stage_a-1.3/csc440_albertm1_stage_a/user_createdelete.py
@@ -0,0 +1,79 @@
+import os
+import json
+
+""" create_user opens a json file of user-account data and inserts new user account data into the file, assuming
+that the new user isn't already present in the data-file
+    """
+
+
+def create_user(form, jsonpath, authentication_method=None, roles=[], active=True):
+    if form.validate_on_submit():
+        usersfile = read_file(jsonpath)
+        print(usersfile)
+        if usersfile.get(form.name.data):
+            return False
+        user = {
+        'active': active,
+        'roles': roles,
+        'authentication_method': authentication_method,
+        'authenticated': False
+        }
+        if authentication_method == 'hash':
+            user['hash'] = 'bah'
+            # make_salted_hash(password)
+        elif authentication_method == 'cleartext':
+            user['password'] = form.password.data
+        else:
+            raise NotImplementedError(authentication_method)
+        usersfile[form.name.data] = user
+        print(usersfile)
+        write_file(jsonpath, usersfile)
+        response = usersfile.get(form.name.data)
+        return response
+    else:
+        return False
+
+
+def delete_user(form, jsonpath, username):
+    if form.validate_on_submit:
+        usersfile = read_file(jsonpath)
+        credentialscheck = usersfile.get(username)
+        if credentialscheck.get('password') == form.id.data:
+            if not usersfile.pop(form.name.data):
+                return False
+        write_file(jsonpath, usersfile)
+        return True
+
+
+def delete_users_by_role(jsonpath, role):
+    if not os.path.exists(jsonpath):
+        return False
+    usersfile = read_file(jsonpath)
+    keys = []
+    for user, data in usersfile.iteritems():
+        for element in data.keys():
+            if element == 'roles':
+                copy = data[element]
+                for r in copy:
+                    print(role)
+                    if r == role:
+                        keys.append(user)
+    for u in keys:
+        print("Attempting to delete user: ", u)
+        if not usersfile.pop(u):
+            print("An error occurred while trying to delete a user.")
+            return False
+        print("Deleted user successfully!")
+    write_file(jsonpath, usersfile)
+    return usersfile
+
+
+def read_file(jsonpath):
+    with open(jsonpath, "r") as f:
+        data = json.loads(f.read())
+    return data
+
+
+def write_file(jsonpath, data):
+    with open(jsonpath, 'w') as f:
+        f.write(json.dumps(data, indent=2))
