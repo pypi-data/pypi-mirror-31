ab86a9491bc0940aef7f2d5b234d324a33bac99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:28 +0000

    Add package assemblerflow/1.1.0/assemblerflow-1.1.0.tar.gz

---
+++ packages/assemblerflow/assemblerflow-1.1.0.tar.gz/assemblerflow-1.1.0/setup.py
@@ -0,0 +1,40 @@
+import assemblerflow
+
+from setuptools import setup
+
+VERSION = assemblerflow.__version__
+
+with open("README.md") as fh:
+    README = fh.read()
+
+setup(
+    name="assemblerflow",
+    version="{}".format(VERSION),
+    packages=["assemblerflow",
+              "assemblerflow.templates",
+              "assemblerflow.generator",
+              "assemblerflow.generator.components"],
+    package_dir={"assemblerflow": "assemblerflow"},
+    package_data={"assemblerflow": ["nextflow.config",
+                                    "profiles.config",
+                                    "bin/*",
+                                    "lib/*",
+                                    "generator/templates/*"]},
+    install_requires=[
+        "argparse",
+        "jinja2",
+    ],
+    description="A Nextflow pipeline assembler for genomics. Pick your "
+                "modules. Assemble them. Run the pipeline.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ODiogoSilva/assemblerflow",
+    author="Diogo N Silva",
+    author_email="o.diogosilva@gmail.com",
+    license="GPL3",
+    entry_points={
+        "console_scripts": [
+            "assemblerflow = assemblerflow.assemblerflow:main"
+        ]
+    }
+)
