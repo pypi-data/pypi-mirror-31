dfd8a94ae2f3907c3d3e3f6712e47a46853a0b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:09 +0000

    Add package ec2-enc-check/1.2/ec2-enc-check-1.2.tar.gz

---
+++ packages/ec2-enc-check/ec2-enc-check-1.2.tar.gz/ec2-enc-check-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='ec2-enc-check',
+    version='1.2',
+    url='https://github.com/Kyle1668/AWS-Ec2-Volumes-Encryption-Check/tree/package',
+    author='Kyle O\'Brien',
+    author_email='kyledevinobrien1@gmail.com',
+    scripts=['ec2-enc-check.py'],
+)
