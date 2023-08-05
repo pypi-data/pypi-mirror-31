5a1447bed1b32a89de595f40efd64751ff9891bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package rinzler/2.0.2/rinzler-2.0.2-py2.py3-none-any.whl

---
+++ packages/rinzler/rinzler-2.0.2-py2.py3-none-any.whl/rinzler/exceptions/gone_exception.py
@@ -0,0 +1,12 @@
+"""
+Exception raised when the requested resource(route, file, database record) is no longer available and will not be
+available again
+"""
+__author__ = "Rinzler<github.com/feliphebueno>"
+
+
+class GoneException(BaseException):
+    """
+    GoneException
+    """
+    pass
---
+++ packages/rinzler/rinzler-2.0.2-py2.py3-none-any.whl/rinzler/exceptions/internal_exception.py
@@ -0,0 +1,11 @@
+"""
+Exception raised when the app fails to process the request and due to an error it couldn't handle correctly
+"""
+__author__ = "Rinzler<github.com/feliphebueno>"
+
+
+class InternalException(BaseException):
+    """
+    InternalException
+    """
+    pass
