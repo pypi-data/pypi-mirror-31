ecfea6d760bcb1aa0cd8cc9e5e451f409e7c71d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:00 +0000

    Add package zaach/1.0.0/zaach-1.0.0.tar.gz

---
+++ packages/zaach/zaach-1.0.0.tar.gz/zaach-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='zaach',
+    version='1.0.0',
+    packages=['zaach',],
+    author="Fabian Topfstedt",
+    license='The MIT Licence',
+    long_description="A library of helpers that are used in various projects",
+)
