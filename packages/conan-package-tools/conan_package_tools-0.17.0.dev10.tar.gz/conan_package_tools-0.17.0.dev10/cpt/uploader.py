dbddcd93c26b9bc667815fbcfe20ebee49152fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package conan-package-tools/0.17.0.dev10/conan_package_tools-0.17.0.dev10.tar.gz

---
+++ packages/conan-package-tools/conan_package_tools-0.17.0.dev10.tar.gz/conan_package_tools-0.17.0.dev10/cpt/uploader.py
@@ -0,0 +1,19 @@
+from cpt.printer import print_message
+
+
+class Uploader(object):
+
+    def __init__(self, conan_api, remote_manager, auth_manager):
+        self.conan_api = conan_api
+        self.remote_manager = remote_manager
+        self.auth_manager = auth_manager
+
+    def upload_packages(self, reference):
+        remote_name = self.remote_manager.upload_remote_name
+        if not remote_name or not self.auth_manager.credentials_ready(remote_name):
+            print_message("Upload skipped, not upload remote available")
+            return
+
+        print_message("Uploading packages for '%s'" % str(reference))
+        self.auth_manager.login(remote_name)
+        self.conan_api.upload(str(reference), all_packages=True, remote=remote_name)
