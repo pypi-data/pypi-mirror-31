092637616e8b9017fd9e3b8153b6fcc021e3d83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:30 +0000

    Add package simple-rl/0.784/simple_rl-0.784.tar.gz

---
+++ packages/simple-rl/simple_rl-0.784.tar.gz/simple_rl-0.784/simple_rl/__init__.py
@@ -0,0 +1,46 @@
+'''
+simple_rl
+	abstraction/
+		action_abs/
+		state_abs/
+	agents/
+		AgentClass.py
+		QLearningAgentClass.py
+		RandomAgentClass.py
+		RMaxAgentClass.py
+		...
+	experiments/
+		ExperimentClass.py
+		ExperimentParameters.py
+	mdp/
+		MDPClass.py
+		StateClass.py
+	tasks/
+		chain/
+			ChainMDPClass.py
+			ChainStateClass.py
+		grid_world/
+			GridWorldMPDClass.py
+			GridWorldStateClass.py
+		...
+	utils/
+		chart_utils.py
+		make_mdp.py
+	run_experiments.py
+
+Author and Maintainer: David Abel (cs.brown.edu/~dabel/)
+Last Updated: April 23rd, 2018
+Contact: dabel@cs.brown.edu
+License: Apache
+'''
+# Fix xrange to cooperate with python 2 and 3.
+try:
+    xrange
+except NameError:
+    xrange = range
+
+# Imports.
+import simple_rl.abstraction, simple_rl.agents, simple_rl.experiments, simple_rl.mdp, simple_rl.planning, simple_rl.tasks, simple_rl.utils
+import simple_rl.run_experiments
+
+from simple_rl._version import __version__
