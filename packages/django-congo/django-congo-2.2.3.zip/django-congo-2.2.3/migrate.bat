acfad379f5d168df733defc7234bf3bdc3cf39d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:06 +0000

    Add package django-congo/2.2.3/django-congo-2.2.3.zip

---
+++ packages/django-congo/django-congo-2.2.3.zip/django-congo-2.2.3/migrate.bat
@@ -0,0 +1,4 @@
+:: https://docs.djangoproject.com/en/1.8/ref/django-admin/#django-admin-makemigrations
+python manage.py makemigrations
+:: https://docs.djangoproject.com/en/1.8/ref/django-admin/#django-admin-migrate
+python manage.py migrate
