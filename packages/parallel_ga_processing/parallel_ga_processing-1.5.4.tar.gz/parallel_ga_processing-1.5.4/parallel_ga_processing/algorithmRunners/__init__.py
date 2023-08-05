cefe30f1d16aff93b758eaf0d85c5c0210d0adf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package parallel_ga_processing/1.5.4/parallel_ga_processing-1.5.4.tar.gz

---
+++ packages/parallel_ga_processing/parallel_ga_processing-1.5.4.tar.gz/parallel_ga_processing-1.5.4/parallel_ga_processing/algorithmRunners/__init__.py
@@ -0,0 +1,6 @@
+from .coarseGrainedGaRunner import run_coarse_grained_ga
+from .fineGrainedGaRunner import run_fine_grained_ga
+from .coarseGrainedGaRunner import run_coarse_grained_ga_remote
+from .fineGrainedGaRunner import run_fine_grained_ga_remote
+from .masterSlaveGaRunner import run_master_slave_ga
+from .launcher import launch
