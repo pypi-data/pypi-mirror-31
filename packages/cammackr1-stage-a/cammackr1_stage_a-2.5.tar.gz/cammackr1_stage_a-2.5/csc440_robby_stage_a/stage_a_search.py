e4859f8769a433eec96f91c6af7ad2516b1eb110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package cammackr1-stage-a/2.5/cammackr1_stage_a-2.5.tar.gz

---
+++ packages/cammackr1-stage-a/cammackr1_stage_a-2.5.tar.gz/cammackr1_stage_a-2.5/csc440_robby_stage_a/stage_a_search.py
@@ -0,0 +1,43 @@
+class Search:
+    search_area = []
+    term = ""
+    matches = []
+
+    def __init__(self, term, search_area):
+        self.search_area = search_area
+        self.term = term
+        self.matches = []
+
+    def clear(self):
+        self.matches = []
+
+    def simple_search(self, term=term,):  # Search through the the search area using the value of term.
+        for item in self.search_area:
+            if item == term:
+                self.matches.append(item)
+
+    def attr_search(self, term, attr):  # Search for a specific attribute within the objects in the search area.
+        for item in self.search_area:
+            if getattr(item, attr) == term:
+                self.matches.append(item)
+
+    def range_search(self, low_term, high_term, inclusive=False):  # Search within a specific range
+        if inclusive:
+            for item in self.search_area:
+                if low_term <= item <= high_term:
+                    self.matches.append(item)
+        else:
+            for item in self.search_area:
+                if low_term < item < high_term:
+                    self.matches.append(item)
+
+
+class Tester:
+    tots = 0
+    title = ""
+
+    def __init__(self, tots, title):
+        self.tots = tots
+        self.title = title
+
+
