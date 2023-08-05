22f48b645bfb2a53bc7ba0e2fbbc8c11915efd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package smarthome-assistant-frontend/0.1/smarthome-assistant-frontend-0.1.tar.gz

---
+++ packages/smarthome-assistant-frontend/smarthome-assistant-frontend-0.1.tar.gz/smarthome-assistant-frontend-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='smarthome-assistant-frontend',
+  packages=['smarthome-assistant-frontend'],
+  version='0.1',
+  description='Initial commit',
+  author='Marc Khouri',
+  author_email='marc.khouri@ubnt.com',
+  url='https://www.github.com/ubiquiti/smarthome-assistant-frontend',
+  keywords=[],
+  classifiers=[],
+)
