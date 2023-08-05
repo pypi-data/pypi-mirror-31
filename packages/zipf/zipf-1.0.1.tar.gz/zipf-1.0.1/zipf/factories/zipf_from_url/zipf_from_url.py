feb6c02f29ad33eefb4b490782786d92f19c00ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:22 +0000

    Add package zipf/1.0.1/zipf-1.0.1.tar.gz

---
+++ packages/zipf/zipf-1.0.1.tar.gz/zipf-1.0.1/zipf/factories/zipf_from_url/zipf_from_url.py
@@ -0,0 +1,23 @@
+import requests
+from ...factories import zipf_from_text
+
+class zipf_from_url(zipf_from_text):
+    def __init__(self):
+        super().__init__()
+        self._request_interface = lambda r: r.text
+
+    def set_interface(self, request_interface):
+        """Sets the interface with which read the text content of the file"""
+        self._request_interface = request_interface
+
+    def _download_file(self, url):
+        r = requests.get(url)
+        return self._request_interface(r)
+
+    def run(self, url):
+        """Loads and extracts a zipf from the given url"""
+        return super().run(self._download_file(url))
+
+    def enrich(self, url, _zipf):
+        """Loads and enriches a given zipf from the given url"""
+        return super().enrich(self._download_file(url), _zipf)
