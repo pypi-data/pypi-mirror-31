52324aafb79a1a7d73b15448509b2e8287f83f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package FirstApp/1.3.0/FirstApp-1.3.0.tar.gz

---
+++ packages/FirstApp/FirstApp-1.3.0.tar.gz/FirstApp-1.3.0/setup.py
@@ -0,0 +1,12 @@
+# import set up function from python distribution utilities
+from distutils.core import setup
+
+setup(
+    name='FirstApp',
+    version='1.3.0',
+    py_modules=['swami_nester'],
+    author='swamic',
+    author_email='chandrashekhar.swami@gslab.com',
+    url='https://gslab.com',
+    description='A simple printer of nested lists'
+)
