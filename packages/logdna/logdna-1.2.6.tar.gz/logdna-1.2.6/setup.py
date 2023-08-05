f4d1520cf137873dfe6daf996a844c175a32e8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package logdna/1.2.6/logdna-1.2.6.tar.gz

---
+++ packages/logdna/logdna-1.2.6.tar.gz/logdna-1.2.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'logdna',
+  packages = ['logdna'],
+  version = '1.2.6',
+  description = 'A python package for sending logs to LogDNA',
+  author = 'Answerbook Inc.',
+  author_email = 'help@logdna.com',
+  url = 'https://github.com/logdna/python',
+  download_url = 'https://github.com/logdna/python/tarball/1.2.6',
+  keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
+  install_requires=[
+    'requests',
+  ],
+  classifiers = [],
+)
