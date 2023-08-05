1d74037928bec0baa02c5b3e6a42b64dd21f1bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:38 +0000

    Add package pyphone/0.1.4/pyphone-0.1.4.tar.gz

---
+++ packages/pyphone/pyphone-0.1.4.tar.gz/pyphone-0.1.4/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+setup(
+  name = 'pyphone',
+  packages = ['pyphone'], # this must be the same as the name above
+  version = '0.1.4',
+  description = 'Phonetic manipulation library',
+  author = 'Ling Zhang',
+  author_email = 'lz@ling.nz',
+  url = 'https://github.com/lingz/pyphone', # use the URL to the github repo
+  install_requires = [
+    'scipy'
+  ],
+  include_package_data = True,
+  keywords = ['nlp', 'phonetic', 'ipa'],
+)
+
