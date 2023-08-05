9a26d67fb6f708595078e65523ec634be5a67454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:55 +0000

    Add package worms/0.1.21/worms-0.1.21.tar.gz

---
+++ packages/worms/worms-0.1.21.tar.gz/worms-0.1.21/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'numpy',
+    'homog >= 0.1.10',
+    'tqdm',
+]
+
+setup_requirements = [
+]
+
+test_requirements = [
+    'pip',
+    'wheel',
+    'watchdog',
+    'flake8',
+    'tox',
+    'coverage',
+    'PyYAML',
+    'pytest',
+    'pytest-runner',
+    'numpy',
+    'homog >= 0.1.10',
+    'xbin >= 0.1.5',
+    'tqdm',
+    # https://storage.googleapis.com/protein-design-ipd-public/wheelhouse/pyrosetta-2017.48.post0.dev0%2B93.fordas.dev.f126926bdc8-cp35-cp35m-linux_x86_64.whl
+    'pytest',
+]
+
+setup(
+    name='worms',
+    version='0.1.21',
+    description="Protion Origami via Genetic Fusions",
+    long_description=readme + '\n\n' + history,
+    author="Will Sheffler",
+    author_email='willsheffler@gmail.com',
+    url='https://github.com/willsheffler/worms',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=True,
+    keywords='worms',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='worms/tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
---
+++ packages/worms/worms-0.1.21.tar.gz/worms-0.1.21/worms/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for worms."""
+
+__author__ = """Will Sheffler"""
+__email__ = 'willsheffler@gmail.com'
+__version__ = '0.1.21'
+
+from .worms import *
+from .search import *
+from .criteria import *
+from . import data
+from . import vis
