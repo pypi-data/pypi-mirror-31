3605ab7230169ce0b77193fd7ef6e5f59cc06470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package nrask/0.0.110/nrask-0.0.110.tar.gz

---
+++ packages/nrask/nrask-0.0.110.tar.gz/nrask-0.0.110/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+setup(
+    description='Viking Makt Framework',
+    install_requires=[
+        'pika',
+        'tornado',
+        'simplejson',
+        'colorlog',
+        'bencode',
+    ],
+    license='https://bitbucket.org/habboi/nrask/raw/master/LICENSE',
+    maintainer='Guilherme Nemeth',
+    maintainer_email='guilherme.nemeth@gmail.com',
+    name='nrask',
+    packages=find_packages(),
+    url='https://bitbucket.org/habboi/nrask',
+    version="0.0.110"
+)
