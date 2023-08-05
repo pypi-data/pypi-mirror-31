e47e7f32a992fb9795dc8e1fe7609cb1b34c6155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:17 +0000

    Add package pymipago/1.0b2/pymipago-1.0b2.tar.gz

---
+++ packages/pymipago/pymipago-1.0b2.tar.gz/pymipago-1.0b2/HISTORY.rst
@@ -0,0 +1,16 @@
+=======
+History
+=======
+
+1.0b2 (2018-04-20)
+------------------
+
+- Add a parameter to use the testing environment of the Payment service.
+  [erral]
+
+
+1.0b1 (2018-04-18)
+------------------
+
+* Implementation of notebook 60 payments in short format (521)
+  [erral]
---
+++ packages/pymipago/pymipago-1.0b2.tar.gz/pymipago-1.0b2/tests/test_pymipago.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pymipago` package."""
+
+import datetime
+import os
+import unittest
+
+from pymipago import make_payment_request
+from pymipago.exceptions import InvalidCPRValue
+from pymipago.exceptions import InvalidFormatValue
+from pymipago.exceptions import InvalidReferenceNumber
+
+
+class TestPymipago(unittest.TestCase):
+    """Tests for `pymipago` package."""
+
+    def test_invalid_cpr_raises_exception(self):
+        cpr = '010101'
+        sender = '123456'
+        format = '521'
+        suffix = '002'
+        reference_number = '1234567890'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        with self.assertRaises(InvalidCPRValue):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes,
+                test_environment=True)
+
+    def test_invalid_format_raises_exception(self):
+        cpr = '9052180'
+        sender = '123456'
+        format = '855'
+        suffix = '002'
+        reference_number = '1234567890'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        with self.assertRaises(InvalidFormatValue):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes,
+                test_environment=True)
+
+    def test_invalid_formated_reference_number_raises_exception(self):
+        cpr = '9052180'
+        sender = '123456'
+        format = '521'
+        suffix = '002'
+        reference_number = '12345678'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        with self.assertRaises(InvalidReferenceNumber):
+            make_payment_request(
+                cpr,
+                sender,
+                format,
+                suffix,
+                reference_number,
+                payment_limit_date,
+                quantity,
+                language,
+                return_url,
+                payment_modes,
+                test_environment=True)
+
+    def test_correct_payment_request(self):
+        cpr = '9052180'
+        sender = '481166'
+        format = '521'
+        suffix = '002'
+        reference_number = '8123456789'
+        payment_limit_date = datetime.datetime.now()
+        quantity = '1100'
+        language = 'eu'
+        return_url = 'http://localhost:8000'
+        payment_modes = ['01']
+
+        html, payment_code = make_payment_request(
+            cpr,
+            sender,
+            format,
+            suffix,
+            reference_number,
+            payment_limit_date,
+            quantity,
+            language,
+            return_url,
+            payment_modes,
+            test_environment=True)
+
+        self.assertTrue(html.find(payment_code) != -1)
+        self.assertTrue(payment_code.isdigit())
