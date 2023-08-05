6772eeff2bd71637cf6b0a4a2969b968d91e5430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:23 +0000

    Add package taskgraph/0.4.0/taskgraph-0.4.0.tar.gz

---
+++ packages/taskgraph/taskgraph-0.4.0.tar.gz/taskgraph-0.4.0/setup.py
@@ -0,0 +1,31 @@
+"""taskgraph setup.py."""
+from setuptools import setup
+
+README = open('README.rst').read()
+
+setup(
+    name='taskgraph',
+    use_scm_version={'version_scheme': 'post-release',
+                     'local_scheme': 'node-and-date'},
+    setup_requires=['setuptools_scm'],
+    description='Parallel task graph framework.',
+    long_description=README,
+    maintainer='Rich Sharp',
+    maintainer_email='richpsharp@gmail.com',
+    url='https://bitbucket.org/richsharp/taskgraph',
+    packages=['taskgraph'],
+    license='BSD',
+    keywords='parallel multiprocessing distributed computing',
+    extras_require={
+        'niced_processes': ['psutil'],
+        },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2 :: Only',
+        'License :: OSI Approved :: BSD License'
+    ])
