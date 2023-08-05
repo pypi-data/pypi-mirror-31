5dff52b1c8678e9a91a57ca86d347dfef4cbf723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package spc-hamlpy/0.9.1/spc-hamlpy-0.9.1.tar.gz

---
+++ packages/spc-hamlpy/spc-hamlpy-0.9.1.tar.gz/spc-hamlpy-0.9.1/hamlpy/templatize.py
@@ -0,0 +1,32 @@
+"""
+This module decorates the django templatize function to parse haml templates
+before the translation utility extracts tags from it.
+
+--Modified to ignore non-haml files.
+"""
+
+try:
+    from django.utils import translation
+    _django_available = True
+except ImportError, e:
+    _django_available = False
+
+import hamlpy
+import os
+
+
+def decorate_templatize(func):
+    def templatize(src, origin=None):
+        # if the template has no origin file then do not attempt to parse it with haml
+        if origin:
+            # if the template has a source file, then only parse it if it is haml
+            if os.path.splitext(origin)[1].lower() in ['.' + x.lower() for x in hamlpy.VALID_EXTENSIONS]:
+                hamlParser = hamlpy.Compiler()
+                html = hamlParser.process(src.decode('utf-8'))
+                src = html.encode('utf-8')
+        return func(src, origin)
+    return templatize
+
+
+if _django_available:
+    translation.templatize = decorate_templatize(translation.templatize)
---
+++ packages/spc-hamlpy/spc-hamlpy-0.9.1.tar.gz/spc-hamlpy-0.9.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# Note to Jesse - only push sdist to PyPi, bdist seems to always break pip installer
+setup(
+    name='spc-hamlpy',
+    version='0.9.1',
+    author='blacktear23',
+    author_email='blacktear23@gmail.com',
+    description='HAML like syntax for Django templates',
+    keywords='haml django converter',
+    url='http://github.com/jessemiller/HamlPy',
+    download_url='http://github.com/jessemiller/HamlPy',
+    packages=['hamlpy', 'hamlpy.template'],
+    license='MIT',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'hamlpy = hamlpy.hamlpy:convert_files',
+            'hamlpy-watcher = hamlpy.hamlpy_watcher:watch_folder',
+        ]
+    }
+)
