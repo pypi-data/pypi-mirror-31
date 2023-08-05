eb4ab1f8cf4fbfcdc730d3ebc07163e19f28c252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:57 +0000

    Add package simple-rl/0.782/simple_rl-0.782-py2.py3-none-any.whl

---
+++ packages/simple-rl/simple_rl-0.782-py2.py3-none-any.whl/simple_rl/mdp/ImageStateClass.py
@@ -0,0 +1,22 @@
+''' ImageStateClass.py: Contains the ImageState Class. '''
+
+# Python imports.
+import numpy
+
+# Other imports.
+from StateClass import State
+
+class ImageState(State):
+    ''' Abstract Image State class '''
+
+    def __init__(self, image, features=None):
+        '''
+        Args:
+            image (np matrix)
+            features (1d numpy matrix)
+        '''
+        self.image = image
+        State.__init__(self, data=features)
+
+    def get_image(self):
+        return self.image
---
+++ packages/simple-rl/simple_rl-0.782-py2.py3-none-any.whl/simple_rl/tasks/atari/AtariStateClass.py
@@ -0,0 +1,15 @@
+''' AtariStateClass.py: Contains the AtariState class. '''
+
+# Other imports.
+from ...mdp.ImageStateClass import ImageState
+from ...mdp.oomdp.OOMDPImageStateClass import OOMDPImageState
+
+class AtariState(ImageState):
+    ''' Class for Atari States: Creates the state based on the raw image/ram. '''
+
+    def __init__(self, image, ram_data, terminal=False):
+        ImageState.__init__(self, image=image, features=ram_data)
+        self._is_terminal = terminal
+
+    def is_terminal(self):
+        return self._is_terminal
