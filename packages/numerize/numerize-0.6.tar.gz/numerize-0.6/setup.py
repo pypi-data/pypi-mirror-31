74e13518670618ec27b2e8d007f3ed870ed59cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package numerize/0.6/numerize-0.6.tar.gz

---
+++ packages/numerize/numerize-0.6.tar.gz/numerize-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='numerize',
+   version='0.6',
+   description='Convert large numbers into readable numbers for humans.',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
+   author='David Sa',
+   author_email='davidsa03@gmail.com',
+   packages=['numerize'],  #same as name
+   url='https://github.com/davidsa03/numerize',
+   license='MIT',
+   install_requires=[
+      # -*- Extra requirements: -*-
+   ],
+)
