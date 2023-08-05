e218dab93a7c81ac70107bfac5ed48a260d72c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:40 +0000

    Add package youtube-sm/1.1.2/youtube_sm-1.1.2.tar.gz

---
+++ packages/youtube-sm/youtube_sm-1.1.2.tar.gz/youtube_sm-1.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+params = dict()
+params['entry_points'] = {'console_scripts': ['youtube-sm = youtube_sm.commands:main']}
+
+setup(
+	name='youtube_sm',
+	version='1.1.2',
+	url='https://github.com/sawyerf/Youtube_subscription_manager',
+	author='Sawyerf',
+	author_email='sawyer.flink@protonmail.ch',
+	description='Youtube subscription manager',
+	long_description=open('README.md', 'r', encoding='utf8').read().replace('\r', ''),
+	long_description_content_type='text/markdown',
+	keywords='youtube subscription api manager html',
+	license='MLP-2.0',
+	packages=find_packages(),
+	classifiers=[
+		'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+		'Environment :: Console',
+		'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3 :: Only'
+	],
+	**params
+)
---
+++ packages/youtube-sm/youtube_sm-1.1.2.tar.gz/youtube_sm-1.1.2/youtube_sm/src/tools.py
@@ -0,0 +1,57 @@
+class Progress():
+	"""Print a progress bar"""
+	def __init__(self, xmax=0):
+		self.xmin = 0
+		self.xmax = xmax
+
+	def add(self):
+		self.xmin += 1
+		self.progress_bar()
+
+	def progress_bar(self):
+		load = ''
+		if self.xmax == 0:
+			pc = 0
+		else:
+			pc = (self.xmin/self.xmax)
+		for i in range(int(pc*40)):
+			load += '█'
+		for i in range(int(40 - pc*40 + (pc*40)%1)):
+			load += ' '
+		print('{} %|{}| {}/{} analyzed'.format(str(pc*100)[:3], load, str(self.xmin), str(self.xmax)), end='\r')
+		if pc == 1:
+			print()
+
+def type_id(id):
+	"""True = Channel; False = Playlist"""
+	if id[:2] == 'UC':
+		return True
+	elif id[:2] == 'PL':
+		return False
+	else:
+		return True
+
+def check_id(id):
+	if id[:2] == 'UC' or id[:2] == 'PL':
+		return True
+	else:
+		return False
+
+def del_data(path='', prin=True):
+	from shutil import rmtree
+	import os
+	if os.path.exists(path + 'data'):
+		for i in range(2):
+			try:
+				rmtree(path + 'data')
+			except:
+				pass
+			else:
+				continue
+		try:
+			os.makedirs(path + 'data/')
+		except:
+			pass
+	else:
+		if prin:
+			print('[!] Data do not exist')
