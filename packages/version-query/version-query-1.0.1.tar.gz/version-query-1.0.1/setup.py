dc76c475e32b73a1a5947f6b225805fece430ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:26 +0000

    Add package version-query/1.0.1/version-query-1.0.1.tar.gz

---
+++ packages/version-query/version-query-1.0.1.tar.gz/version-query-1.0.1/setup.py
@@ -0,0 +1,31 @@
+"""Setup script for version_query package."""
+
+import setup_boilerplate
+
+
+class Package(setup_boilerplate.Package):
+
+    """Package metadata."""
+
+    name = 'version-query'
+    description = 'Package version query toolkit for Python'
+    download_url = 'https://github.com/mbdevpl/version-query'
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities']
+    keywords = ['abstract syntax tree', 'ast', 'parser', 'xml']
+
+
+if __name__ == '__main__':
+    Package.setup()
