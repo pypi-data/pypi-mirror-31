048155446bec463799afd4109ad8c831aeb00e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:46 +0000

    Add package meta-sparse/0.1.1/meta-sparse-0.1.1.tar.gz

---
+++ packages/meta-sparse/meta-sparse-0.1.1.tar.gz/meta-sparse-0.1.1/docs/index.rst
@@ -0,0 +1,47 @@
+.. SPARSE documentation master file, created by
+   sphinx-quickstart on Tue Nov  7 11:59:42 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SPARSE's documentation!
+==================================
+
+SPARSE indexes >100,000 reference genomes in public databases into hierarchical clusters and uses it to predict origins of metagenomic reads.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Getting Started:
+   
+   installation
+   toyexample
+   parameter
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Building Databases:
+   
+   refseq
+   representative
+   custom
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Mapping reads:
+
+   mash
+   map
+
+
+Citation
+--------
+
+SPARSE has not been formally published yet. If you use SPARSE please cite the preprint https://www.biorxiv.org/content/early/2017/11/07/215707
+
+Zhemin Zhou, Nina Luhmann, Nabil-Fareed Alikhan, Christopher Quince, Mark Achtman, 'Accurate Reconstruction of Microbial Strains Using Representative Reference Genomes' bioRxiv 215707; doi: https://doi.org/10.1101/215707
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/meta-sparse/meta-sparse-0.1.1.tar.gz/meta-sparse-0.1.1/setup.py
@@ -0,0 +1,57 @@
+"""A setuptools based setup module for SPARSE.
+
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path, walk, chdir, system
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='meta-sparse',  
+    version= '0.1.1',  
+    description='SPARSE indexes reference genomes in public databases into hierarchical clusters and uses it to predict origins of metagenomic reads.',
+    long_description=long_description, 
+    long_description_content_type='text/markdown',  
+    url='https://github.com/zheminzhou/SPARSE/',
+    author='Zhemin Zhou',  
+    author_email= 'zhemin.zhou@warwick.ac.uk', 
+    classifiers=[ 
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+#    py_modules=['SPARSE', 'parameter'],
+    entry_points={
+        'console_scripts': [
+            'sparse = SPARSE.SPARSE:SPARSE',
+        ],
+    },
+    keywords=['bioinformatics', 'microbial', 'metagenomics'], 
+    package_data={'SPARSE':[
+        'EM/*',
+        'bin/*',
+        'docs/*',
+        'modules/*',
+        'EM/Ipopt/include/*',
+        'EM/Ipopt/lib64/*',
+        'EM/Ipopt/*',
+    ]},
+    packages = ['SPARSE'], 
+    package_dir = {'SPARSE':'.'},
+    install_requires=['pycapnp', 'numpy', 'pandas', 'Cython', 'scipy', 'msgpack'],
+    include_package_data=True,
+    project_urls={ 
+        'Bug Reports': 'https://github.com/zheminzhou/SPARSE/issues',
+        'Source': 'https://github.com/zheminzhou/SPARSE/',
+    },
+)
