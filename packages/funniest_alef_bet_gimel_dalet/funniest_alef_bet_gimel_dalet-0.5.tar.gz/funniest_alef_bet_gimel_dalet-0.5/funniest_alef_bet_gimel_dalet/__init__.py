899aaf09ca32c9ecf36b170c5c7d0e61fbdc931e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package funniest_alef_bet_gimel_dalet/0.5/funniest_alef_bet_gimel_dalet-0.5.tar.gz

---
+++ packages/funniest_alef_bet_gimel_dalet/funniest_alef_bet_gimel_dalet-0.5.tar.gz/funniest_alef_bet_gimel_dalet-0.5/funniest_alef_bet_gimel_dalet/__init__.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.'
+            u'ilyada roman olsun.'
+            np.sum(9,14))
+
