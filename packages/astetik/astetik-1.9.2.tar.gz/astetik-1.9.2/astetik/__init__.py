370b7cbf8f995dc4aa0d9a90d5487ee51fe4146d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:21 +0000

    Add package astetik/1.9.2/astetik-1.9.2.tar.gz

---
+++ packages/astetik/astetik-1.9.2.tar.gz/astetik-1.9.2/astetik/__init__.py
@@ -0,0 +1,33 @@
+# PLOTS
+from .plots.corr import corr
+from .plots.hist import hist
+from .plots.kde import kde
+from .plots.pie import pie
+from .plots.swarm import swarm
+from .plots.scat import scat
+from .plots.line import line
+from .plots.grid import grid
+from .plots.box import box
+from .plots.violin import violin
+from .plots.strip import strip
+from .plots.count import count
+from .plots.bargrid import bargrid
+from .plots.overlap import overlap
+from .plots.multikde import multikde
+from .plots.compare import compare
+from .plots.multicount import multicount
+from .plots.bar import bar
+from .plots.bartwo import bartwo
+from .plots.animate import Animation
+# from .plots.words import words
+
+
+# TABLES
+from .tables.ols import ols
+from .tables.table import table
+from .tables.text import text
+# from .tables.timeseries import timeseries
+
+from .utils.transform import boolcols_to_cat
+
+__version__ = "1.9.2"
