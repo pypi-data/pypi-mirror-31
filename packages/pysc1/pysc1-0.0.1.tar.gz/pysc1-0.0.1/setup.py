0986eb56da928c20c4633c3bfc11a94a3fb78dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package pysc1/0.0.1/pysc1-0.0.1.tar.gz

---
+++ packages/pysc1/pysc1-0.0.1.tar.gz/pysc1-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pysc1',
+	version='0.0.1',
+	description='StarCraft I Learning Environment',
+	url='https://github.com/bboyseiok/pysc1',
+	author='bboyseiok',
+	author_email='bboyseiok@deepest.ai',
+	licence='BSP',
+	packages=['pysc1'],
+	zip_safe=False)
