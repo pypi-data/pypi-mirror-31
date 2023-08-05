f7f3754bb9f1b410d9843962f0c3df64333c509c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:37 +0000

    Add package betacode/0.1.2/betacode-0.1.2.tar.gz

---
+++ packages/betacode/betacode-0.1.2.tar.gz/betacode-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name = 'betacode',
+    packages = ['betacode'],
+    version = '0.1.2',
+    description = 'Betacode to Unicode converter.',
+    author = 'Matias Grioni',
+    author_email = 'matgrioni@gmail.com',
+    url = 'https://github.com/matgrioni/betacode',
+    keywords = ['encoding', 'unicode', 'betacode', 'greek'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Text Processing :: Linguistic',
+        'Natural Language :: Greek',
+    ],
+)
