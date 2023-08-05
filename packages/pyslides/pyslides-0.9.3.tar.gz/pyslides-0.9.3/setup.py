a1a1f6e0d93ea2cfeea4c4eef88b25ec81374bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package pyslides/0.9.3/pyslides-0.9.3.tar.gz

---
+++ packages/pyslides/pyslides-0.9.3.tar.gz/pyslides-0.9.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from os.path import join as pjoin
+
+setup(name='pyslides',
+      version='0.9.3',
+      description='Python-based tool for presentation-ready slides',
+      author='Giuseppe Romano',
+      author_email='romanog@mit.edu',
+      classifiers=['Programming Language :: Python :: 2.7'],
+      long_description=open('README.rst').read(),
+      install_requires=['pybtex'], 
+      license='GPLv2',
+      packages=['pyslides'],
+      zip_safe=False)
