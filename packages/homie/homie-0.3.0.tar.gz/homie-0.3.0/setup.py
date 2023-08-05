3754716116ee0e73a2b3007564b92f9fbe200051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:25 +0000

    Add package homie/0.3.0/homie-0.3.0.tar.gz

---
+++ packages/homie/homie-0.3.0.tar.gz/homie-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = "0.3.0"
+
+setup(
+    name="homie",
+    packages=["homie"],
+    version=version,
+    description="Experimental implementation of the IoT convention called homie",
+    author="Jan Almeroth",
+    author_email="homie-python@almeroth.com",
+    url="https://github.com/jalmeroth/homie-python",
+    download_url="https://github.com/jalmeroth/homie-python/tarball/" + version,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation"
+    ],
+    install_requires=['paho-mqtt>=1.3.0']
+)
