dabead61b1af0bc41f6b9ec8eb6960637be93dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:01 +0000

    Add package django-s3-direct-view/0.0.5/django-s3-direct-view-0.0.5.tar.gz

---
+++ packages/django-s3-direct-view/django-s3-direct-view-0.0.5.tar.gz/django-s3-direct-view-0.0.5/s3_direct/views.py
@@ -0,0 +1,52 @@
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework import status
+
+from . import serializers, s3
+
+
+class S3DirectUploadView(GenericViewSet):
+
+    params_serializer_class = serializers.S3DirectSerializer
+    file_model = None
+    file_field = 'file'
+
+    def get_additional_fields(self):
+        return dict()
+
+    def _get_upload_serializer(self):
+        return serializers.confirm_serializer(
+            model=self.file_model,
+            file_field=self.file_field,
+        )
+
+    @action(
+        methods=['GET'],
+        url_path='upload',
+        serializer_class=params_serializer_class,
+        detail=True,
+    )
+    def upload_params(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        param_generator = s3.S3DirectUploadParam()
+
+        return Response(data=param_generator())
+
+    @action(
+        methods=['POST'],
+        url_path='confirm',
+        detail=True,
+    )
+    def upload_confirm(self, request, *args, **kwargs):
+        data = request.data
+        serializer_class = self._get_upload_serializer()
+        serializer = serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        validated_data.update(
+            {**self.get_additional_fields()}
+        )
+        serializer.create(validated_data)
+        return Response(status=status.HTTP_201_CREATED)
