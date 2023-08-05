34fb03a9a50794f5a81fec8047317ac78574eb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:29 +0000

    Add package pynebl/0.0.1/pynebl-0.0.1.tar.gz

---
+++ packages/pynebl/pynebl-0.0.1.tar.gz/pynebl-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pynebl',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.nebl.io',
+    author='',
+    author_email='info@nebl.io',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
