a6bd42eeb5686eb3b25fcfcda96efc5be63f1468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:32 +0000

    Add package sqlite-framework/1.1.0/sqlite-framework-1.1.0.tar.gz

---
+++ packages/sqlite-framework/sqlite-framework-1.1.0.tar.gz/sqlite-framework-1.1.0/sqlite_framework/sql/item/constants/conflict_resolution.py
@@ -0,0 +1,8 @@
+from sqlite_framework.sql.item.base import NamedItem
+
+
+class ConflictResolution(NamedItem):
+    pass
+
+
+REPLACE = ConflictResolution("replace")
---
+++ packages/sqlite-framework/sqlite-framework-1.1.0.tar.gz/sqlite-framework-1.1.0/sqlite_framework_test/main.py
@@ -0,0 +1,28 @@
+import time
+
+from sqlite_framework.log.impl import BasicSqliteLogger
+from sqlite_framework.session.session import SqliteSession
+from sqlite_framework_test.factory import TestSqliteStorageComponentFactory
+
+
+session = SqliteSession(":memory:", debug=True)
+
+with session:
+    session.init()
+    component_factory = TestSqliteStorageComponentFactory(session, BasicSqliteLogger())
+    test_component = component_factory.test()
+
+with session:
+    test_component.save_test(1, "test 1")
+    test_component.save_test(2, "test 2")
+    test_component.save_test(55, "test 55")
+    test_component.save_test2(55)
+    time.sleep(1.1)
+    test_component.save_test2(2)
+    time.sleep(1.1)
+    test_component.save_test2(55)
+
+print(test_component.get_test(1, "test 1"))
+print(test_component.get_test(2, "test 4"))
+print(test_component.get_all_test())
+print(test_component.get_all_test2())
