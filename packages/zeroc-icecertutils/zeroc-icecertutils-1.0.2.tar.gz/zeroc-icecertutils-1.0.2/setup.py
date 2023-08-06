7ec25de3798144c82e90cca981646fe629956d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:45 +0000

    Add package zeroc-icecertutils/1.0.2/zeroc-icecertutils-1.0.2.tar.gz

---
+++ packages/zeroc-icecertutils/zeroc-icecertutils-1.0.2.tar.gz/zeroc-icecertutils-1.0.2/setup.py
@@ -0,0 +1,54 @@
+# **********************************************************************
+#
+# Copyright (c) 2015-2018-2018 ZeroC, Inc. All rights reserved.
+#
+# **********************************************************************
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import sys
+
+hasOpenSSL=False
+try:
+    import OpenSSL
+    hasOpenSSL=True
+except:
+    pass
+
+setup(
+  name = 'zeroc-icecertutils',
+  packages = ['IceCertUtils'],
+  version = '1.0.2',
+  description = 'ZeroC Ice certificate utilities',
+  author = 'ZeroC, Inc.',
+  author_email = 'info@zeroc.com',
+  url = 'https://github.com/zeroc-ice/icecertutils',
+  download_url = 'https://github.com/zeroc-ice/icecertutils/archive/v1.0.2.tar.gz',
+  keywords = ['ice', 'certificate', 'ca', 'ssl'],
+  install_requires = (["pyopenssl>=0.14"] if not hasOpenSSL or sys.platform == "win32" else []),
+  license='BSD',
+  entry_points = {
+      'console_scripts' : ["iceca=IceCertUtils.IceCaUtil:main"],
+  },
+  classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+  ],
+  test_suite = "tests.factory",
+)
