322f756e172cfc835601acbc3b9cdad5a5c4f628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:45 +0000

    Add package django-functest/1.0.3/django-functest-1.0.3.tar.gz

---
+++ packages/django-functest/django-functest-1.0.3.tar.gz/django-functest-1.0.3/release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+umask 000
+./setup.py sdist || exit 1
+
+# Different wheels are needed for py2 and py3 because of the faulthandler
+# dependency:
+
+python2.7 setup.py bdist_wheel --python-tag=py2 || exit 1
+python3.5 setup.py bdist_wheel --python-tag=py3 || exit 1
+
+VERSION=$(./setup.py --version) || exit 1
+twine upload dist/django-functest-$VERSION.tar.gz dist/django_functest-$VERSION-py2-none-any.whl dist/django_functest-$VERSION-py3-none-any.whl || exit 1
