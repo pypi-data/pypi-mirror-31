cd4c0340cac4f8e6eabd8e83cac14f73be288fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:16 +0000

    Add package BOFdat/0.1.2/BOFdat-0.1.2.tar.gz

---
+++ packages/BOFdat/BOFdat-0.1.2.tar.gz/BOFdat-0.1.2/BOFdat/__init__.py
@@ -0,0 +1,7 @@
+from BOFdat import update
+from BOFdat import dna
+from BOFdat import rna
+from BOFdat import protein
+from BOFdat import lipid
+from BOFdat import metabolite
+from BOFdat import maintenance
