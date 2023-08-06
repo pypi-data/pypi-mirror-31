7782ea1a0a386fb857db8469639a87c2f499a4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:55 +0000

    Add package perk-mdplib/1.24.2/perk_mdplib-1.24.2.tar.gz

---
+++ packages/perk-mdplib/perk_mdplib-1.24.2.tar.gz/perk_mdplib-1.24.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='perk_mdplib',
+      version='1.24.2',
+      author='Marco Della Putta',
+      author_email='marcodp183@gmail.com',
+      url='https://grigoprg.webnode.it',
+      maintainer='Marco Della Putta',
+      maintainer_email='marcodp183@gmail.com',
+      description='A powerful package that provide some interesting function',
+      license='GNU License',
+      py_modules=['perk_mdplib'],
+      long_description="This package provide a several method to control the flow of the program",   
+      )
