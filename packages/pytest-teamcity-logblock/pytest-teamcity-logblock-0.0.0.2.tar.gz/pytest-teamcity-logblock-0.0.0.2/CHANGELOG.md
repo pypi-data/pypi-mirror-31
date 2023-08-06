405797b519d3fcfdb37840763058b5c955523468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:15 +0000

    Add package pytest-teamcity-logblock/0.0.0.2/pytest-teamcity-logblock-0.0.0.2.tar.gz

---
+++ packages/pytest-teamcity-logblock/pytest-teamcity-logblock-0.0.0.2.tar.gz/pytest-teamcity-logblock-0.0.0.2/CHANGELOG.md
@@ -0,0 +1,7 @@
+# ChangeLog
+
+## 0.0.0.2 (2018 May 15)
+- Add MANIFEST.in.
+
+## 0.0.0.1 (2018 May 14)
+- First release.
---
+++ packages/pytest-teamcity-logblock/pytest-teamcity-logblock-0.0.0.2.tar.gz/pytest-teamcity-logblock-0.0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='pytest-teamcity-logblock',
+    description='py.test plugin to introduce block structure in teamcity build log, if output is not captured',
+    long_description=open("README.md").read(),
+    version='0.0.0.2',
+    url='https://github.com/cielavenir/pytest-teamcity-logblock',
+    license='BSD',
+    author='cielavenir',
+    author_email='cielartisan@gmail.com',
+    py_modules=['pytest_teamcity_logblock'],
+    entry_points={'pytest11': ['forcefail = pytest_teamcity_logblock']},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['pytest>=3.0','teamcity-messages'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
+)
