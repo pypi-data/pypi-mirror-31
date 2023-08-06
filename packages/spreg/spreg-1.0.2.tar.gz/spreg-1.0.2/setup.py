a9526d1ded4cfb0eb810dd00fe47419013ee41bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package spreg/1.0.2/spreg-1.0.2.tar.gz

---
+++ packages/spreg/spreg-1.0.2.tar.gz/spreg-1.0.2/setup.py
@@ -0,0 +1,95 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+import os
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+MAJOR = 1
+MINOR = 0
+MICRO = 2 
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+
+def _get_requirements_from_files(groups_files):
+    groups_reqlist = {}
+
+    for k,v in groups_files.items():
+        with open(v, 'r') as f:
+            pkg_list = f.read().splitlines()
+        groups_reqlist[k] = pkg_list
+
+    return groups_reqlist
+
+def setup_package():
+    # get all file endings and copy whole file names without a file suffix
+    # assumes nested directories are only down one level
+    _groups_files = {
+        'base': 'requirements.txt',
+        'plus': 'requirements_plus.txt',
+        'dev': 'requirements_dev.txt'
+    }
+
+    reqs = _get_requirements_from_files(_groups_files)
+    install_reqs = reqs.pop('base')
+    extras_reqs = reqs
+
+
+    setup(name='spreg',
+        version=VERSION,
+        description="PySAL Spatial Econometrics Package",
+        long_description=long_description,
+        maintainer="PySAL Developers",
+        maintainer_email='pysal-dev@googlegroups.com',
+        url='http://pysal.org',
+        download_url='https://pypi.python.org/pypi/spreg',
+        license='BSD',
+        py_modules=['spreg'],
+        packages=find_packages(),
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        keywords='spatial statistics',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: GIS',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+        ],
+        # package_data={'libpysal':list(example_data_files)},
+          install_requires=['numpy', 'scipy', 'libpysal'],
+        extras_require=extras_reqs,
+        cmdclass={'build_py': build_py}
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
+
+
+
+
+
+
+
+
+
+
+
