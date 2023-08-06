35291a837bc20374f5c62c233d066f741346bfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package pyxin/0.0.1/pyxin-0.0.1.tar.gz

---
+++ packages/pyxin/pyxin-0.0.1.tar.gz/pyxin-0.0.1/pyxin/module.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    reutrn a+b
+
+class People():
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        print("my name is", self.name)
---
+++ packages/pyxin/pyxin-0.0.1.tar.gz/pyxin-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pyxin",
+    version = "0.0.1",
+    description = "a package of pyxin",
+    packages = find_packages(),
+)
