a009b6c7e3984e2adc297c6cf5dc51951fa7bc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package gnacsupdated/1.0.1/gnacsupdated-1.0.1.tar.gz

---
+++ packages/gnacsupdated/gnacsupdated-1.0.1.tar.gz/gnacsupdated-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+GNACS_VERSION_NUMBER = "1.0.1"
+
+if '__main__' == __name__:
+    setup(
+        name='gnacsupdated',
+        version=GNACS_VERSION_NUMBER,
+        author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman, Fiona Pigott, Nikhil Sulegaon',
+        author_email='nikhilsulegaon@gmail.com',
+        packages=['acscsv'],
+        scripts=['gnacs.py'],
+        url='https://github.com/nikhilsu/Gnacs',
+        download_url='https://github.com/nikhilsu/Gnacs/tags/%s'%(GNACS_VERSION_NUMBER),
+        license='LICENSE.txt',
+        description='Gnip normalized activity JSON to csv parser (Twitter, Disqus Comments, and Wordpress Posts and Comments)',
+        install_requires=[
+                    "ujson >= 1.2",
+                        ]
+        )
