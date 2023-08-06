b5cea3b4471c610be33e13302c1a4e039035bbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:55 +0000

    Add package pyfindvs/0.6.291/pyfindvs-0.6.291-cp36-cp36m-win32.whl

---
+++ packages/pyfindvs/pyfindvs-0.6.291-cp36-cp36m-win32.whl/pyfindvs/msbuildcompiler/__init__.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+
+from .compiler import MSBuildCompiler
+from .enable_msbuildcompiler import enable as enable_compiler
