644e7e738f5421bb3e10d6edb3bd84e4bd7ca30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:05 +0000

    Add package CleanFlow/1.2.9a1/CleanFlow-1.2.9a1.tar.gz

---
+++ packages/CleanFlow/CleanFlow-1.2.9a1.tar.gz/CleanFlow-1.2.9a1/cleanflow/exploratory/duplicates.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+
+sc = SparkContext.getOrCreate()
+sqlContext = SQLContext(sc)
+
+def check_duplicates(df, column):
+    """
+    Function to check no.of values that occur more than once.
+
+    Parameter
+    ---------
+    df     : DataFrame to be processed.
+    column : column in df for which we want to find duplicate values.
+    """
+    df.createOrReplaceTempView("df")
+    return sqlContext.sql("SELECT %s as DuplicateValue, COUNT(*) as Count FROM df GROUP BY %s HAVING COUNT(*)>1 order by Count desc"%(column, column))
+
+def find_unique(df, column):
+    """
+    Function to find unique/distinct values in a column of a DataFrame.
+
+    Parameter
+    ---------
+    df     : DataFrame to be processed.
+    column : column in df for which we want to find duplicate values.
+    """
+    df.createOrReplaceTempView("df")
+    return sqlContext.sql('select distinct(%s) as UniqueValues,COUNT(*) as Count from df group by %s order by Count desc'%(column, column))
