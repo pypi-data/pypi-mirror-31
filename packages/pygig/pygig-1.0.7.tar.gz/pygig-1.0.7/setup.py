b8e24989009bb2031cf36f6f97700abfe2ea4c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:38 +0000

    Add package pygig/1.0.7/pygig-1.0.7.tar.gz

---
+++ packages/pygig/pygig-1.0.7.tar.gz/pygig-1.0.7/setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pygig',
+    version='1.0.7',
+    author='lyq',
+    author_email='1033020837@qq.com',
+    url='https://github.com/1033020837/GitIgnoreGenerator',
+    description='A command tool to generate .gitignore file.',
+    packages=['pygig','gitignore'],
+    install_requires=['argparse','GitPython'],
+    include_package_data=True,
+    package_data={"gitignore": ["gitignore/*"]},
+    entry_points={
+        'console_scripts': [
+            'pygig=pygig:main'
+        ]
+    }
+)
