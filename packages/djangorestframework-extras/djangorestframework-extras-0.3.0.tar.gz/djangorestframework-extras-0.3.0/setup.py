93e143631940533ead94756713316271986de4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:56 +0000

    Add package djangorestframework-extras/0.3.0/djangorestframework-extras-0.3.0.tar.gz

---
+++ packages/djangorestframework-extras/djangorestframework-extras-0.3.0.tar.gz/djangorestframework-extras-0.3.0/setup.py
@@ -0,0 +1,28 @@
+import multiprocessing
+from setuptools import setup, find_packages
+
+
+setup(
+    name="djangorestframework-extras",
+    version="0.3.0",
+    description="Autogenerate API's for models. Expose core Django functionality. Validate against existing forms.",
+    long_description = open("README.rst", "r").read(),
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    url="http://github.com/praekelt/djangorestframework-extras",
+    packages = find_packages(),
+    install_requires = [
+        "djangorestframework>=3.8.0",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
