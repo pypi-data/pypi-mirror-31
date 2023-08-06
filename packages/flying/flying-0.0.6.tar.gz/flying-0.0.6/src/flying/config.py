4dadc8d2671b578695a5fb1a5ec1be6dc334a581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:55 +0000

    Add package flying/0.0.6/flying-0.0.6.tar.gz

---
+++ packages/flying/flying-0.0.6.tar.gz/flying-0.0.6/src/flying/config.py
@@ -0,0 +1,51 @@
+FLYING_DEFAULT = {
+    'conditions': [],
+    'pre_release': [],
+    'git': {
+        'enable': False,
+        'remote': 'origin',
+    },
+    'docker': {
+        'enable': False,
+    },
+    'npm': {
+        'enable': False,
+        'upgrade_version_enable': False,
+        'package_path': './package.json',
+    },
+    'pypi': {
+        'enable': False,
+        'build_cmd': 'python setup.py sdist',
+        'upload_cmd': 'twine upload dist/*',
+    },
+}
+FLYING_TEMPLATE = {
+    'name': 'project_name',
+    'version': '0.0.0',
+    'version_prefix': 'v',
+    'conditions': [
+        # 'git status | grep "nothing to commit, working tree clean"'
+    ],
+    'pre_release': [
+        # 'cat ./flying.json'
+    ],
+    'git': {
+        'enable': True,
+        'remote': 'origin',
+    },
+    'docker': {
+        'enable': True,
+        'namespace': 'username/project_name',
+        'dkf_path': './Dockerfile',
+    },
+    'npm': {
+        'enable': True,
+        'upgrade_version_enable': False,
+        'package_path': './package.json',
+    },
+    'pypi': {
+        'enable': True,
+        'build_cmd': 'python setup.py sdist',
+        'upload_cmd': 'twine upload dist/*',
+    },
+}
