7956dae4d9fe00597a6eff4d1421fefd289cdc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:42 +0000

    Add package django-nps/0.5/django-nps-0.5.tar.gz

---
+++ packages/django-nps/django-nps-0.5.tar.gz/django-nps-0.5/net_promoter_score/migrations/0002_promoterscore_source.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('net_promoter_score', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userscore',
+            name='source',
+            field=models.CharField(help_text="Source of user score, used for filtering results, e.g. 'app', 'web', 'email'.", max_length=20, blank=True),
+        ),
+    ]
