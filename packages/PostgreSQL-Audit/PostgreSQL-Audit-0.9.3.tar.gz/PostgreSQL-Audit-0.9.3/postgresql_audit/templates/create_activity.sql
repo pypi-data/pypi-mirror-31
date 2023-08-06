4c3556db0209b9b9e12a6ea6d569418dd0f09f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:29 +0000

    Add package PostgreSQL-Audit/0.9.3/PostgreSQL-Audit-0.9.3.tar.gz

---
+++ packages/PostgreSQL-Audit/PostgreSQL-Audit-0.9.3.tar.gz/PostgreSQL-Audit-0.9.3/postgresql_audit/templates/create_activity.sql
@@ -0,0 +1,61 @@
+CREATE OR REPLACE FUNCTION jsonb_change_key_name(data jsonb, old_key text, new_key text)
+RETURNS jsonb
+IMMUTABLE
+LANGUAGE sql
+AS $$
+    SELECT ('{'||string_agg(to_json(CASE WHEN key = old_key THEN new_key ELSE key END)||':'||value, ',')||'}')::jsonb
+    FROM (
+        SELECT *
+        FROM jsonb_each(data)
+    ) t;
+$$;
+
+CREATE OR REPLACE FUNCTION ${schema_prefix}create_activity() RETURNS TRIGGER AS $$
+DECLARE
+    audit_row ${schema_prefix}activity;
+    excluded_cols text[] = ARRAY[]::text[];
+BEGIN
+    audit_row.id = nextval('${schema_prefix}activity_id_seq');
+    audit_row.schema_name = TG_TABLE_SCHEMA::text;
+    audit_row.table_name = TG_TABLE_NAME::text;
+    audit_row.relid = TG_RELID;
+    audit_row.issued_at = statement_timestamp() AT TIME ZONE 'UTC';
+    audit_row.native_transaction_id = txid_current();
+    audit_row.transaction_id = (
+        SELECT id
+        FROM ${schema_prefix}transaction
+        WHERE
+            native_transaction_id = txid_current() AND
+            issued_at >= (NOW() - INTERVAL '1 day')
+        ORDER BY issued_at DESC
+        LIMIT 1
+    );
+    audit_row.verb = LOWER(TG_OP);
+    audit_row.old_data = '{}'::jsonb;
+    audit_row.changed_data = '{}'::jsonb;
+
+    IF TG_ARGV[0] IS NOT NULL THEN
+        excluded_cols = TG_ARGV[0]::text[];
+    END IF;
+
+    IF (TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW') THEN
+        audit_row.old_data = row_to_json(OLD.*)::jsonb - excluded_cols;
+        audit_row.changed_data = (
+            row_to_json(NEW.*)::jsonb - audit_row.old_data - excluded_cols
+        );
+        IF audit_row.changed_data = '{}'::jsonb THEN
+            -- All changed fields are ignored. Skip this update.
+            RETURN NULL;
+        END IF;
+    ELSIF (TG_OP = 'DELETE' AND TG_LEVEL = 'ROW') THEN
+        audit_row.old_data = row_to_json(OLD.*)::jsonb - excluded_cols;
+    ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW') THEN
+        audit_row.changed_data = row_to_json(NEW.*)::jsonb - excluded_cols;
+    END IF;
+    INSERT INTO ${schema_prefix}activity VALUES (audit_row.*);
+    RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = pg_catalog, public;
