e11c31655c87b8bc7a85080f6b52f44c651a5391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:38 +0000

    Add package aiorcon/0.6.7/aiorcon-0.6.7-py3-none-any.whl

---
+++ packages/aiorcon/aiorcon-0.6.7-py3-none-any.whl/aiorcon/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+from .rcon import RCON
+from .exceptions import (RCONError, RCONAuthenticationError, RCONTimeoutError, RCONClosedError,
+                         RCONCommunicationError, RCONMessageError, RCONStateError)
+__version__ = '0.6.7'
+
+log = logging.getLogger(__name__)
+red_format = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: %(message)s',
+                               datefmt="[%d/%m/%Y %H:%M]")
+logging.basicConfig(format=red_format)
