33341879cfa92e1f0f4a6a25e80a7f7567b9ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package maxtree/0.1.1/maxtree-0.1.1.tar.gz

---
+++ packages/maxtree/maxtree-0.1.1.tar.gz/maxtree-0.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+import numpy
+
+maxtree_extension = Extension('_maxtree',
+                              sources=['./maxtree/maxtree_wrap.cpp','./cppsrc/maxtree.cpp'],
+                              include_dirs=['./cppsrc', numpy.get_include()],
+                              extra_compile_args=['-std=c++11'],
+                              extra_link_args=['-std=c++11'],
+                              )
+
+setup(name='maxtree',
+      version='0.1.1',
+      description='Max Tree algorithm',
+      author='Lionel Gueguen',
+      author_email='gueguenster@gmail.com',
+      url='https://github.com/gueguenster/maxtree',
+      ext_modules = [maxtree_extension],
+      packages =['maxtree'],
+      include_package_data=True,
+     )
