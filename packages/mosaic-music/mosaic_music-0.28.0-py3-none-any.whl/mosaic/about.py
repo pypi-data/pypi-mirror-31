b1e55e731124ea37d860ab21d10c43fc1c525eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:35 +0000

    Add package mosaic-music/0.28.0/mosaic_music-0.28.0-py3-none-any.whl

---
+++ packages/mosaic-music/mosaic_music-0.28.0-py3-none-any.whl/mosaic/about.py
@@ -0,0 +1,42 @@
+import atexit
+import contextlib
+
+import importlib_resources
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
+
+
+class AboutDialog(QDialog):
+    """Contains the necessary elements to show the about dialog."""
+
+    def __init__(self, parent=None):
+        """Display a dialog that shows application information."""
+        super(AboutDialog, self).__init__(parent)
+        self.setWindowTitle('About')
+
+        self.file_manager = contextlib.ExitStack()
+        atexit.register(self.file_manager.close)
+
+        help_icon = str(self.file_manager.enter_context(importlib_resources.path('mosaic.images', 'md_help.png')))
+        self.setWindowIcon(QIcon(help_icon))
+        self.resize(300, 200)
+
+        author = QLabel('Created by Mandeep')
+        author.setAlignment(Qt.AlignCenter)
+
+        icons = QLabel('Material design icons created by Google')
+        icons.setAlignment(Qt.AlignCenter)
+
+        github = QLabel('GitHub: mandeep')
+        github.setAlignment(Qt.AlignCenter)
+
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignVCenter)
+
+        self.layout.addWidget(author)
+        self.layout.addWidget(icons)
+        self.layout.addWidget(github)
+
+        self.setLayout(self.layout)
