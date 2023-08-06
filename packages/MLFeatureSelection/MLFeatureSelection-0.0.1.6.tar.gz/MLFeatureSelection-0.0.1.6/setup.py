6960d49c3da5eb21b8dcc51e6be76e0a73ab8de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:17 +0000

    Add package MLFeatureSelection/0.0.1.6/MLFeatureSelection-0.0.1.6.tar.gz

---
+++ packages/MLFeatureSelection/MLFeatureSelection-0.0.1.6.tar.gz/MLFeatureSelection-0.0.1.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import codecs
+import os
+import sys
+
+
+setup(
+     name = "MLFeatureSelection",
+     version = "0.0.1.6",
+     keywords = ("pypi easy_install pip"),
+     description = "geenral select features based on selected algorithm",
+     long_description = open("README.rst", encoding='utf8').read(),
+     license = "MIT Licence",
+     
+     url = "https://github.com/duxuhao/Feature-Selection",
+     author = "Xuhao(Peter) Du",
+     author_email = "duxuhao88@gmail.com",
+     
+     packages = find_packages(),
+     platforms = "Linux",
+     classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+      ],
+    
+     install_requires = ["requires"]
+     )
