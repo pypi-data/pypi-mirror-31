0e20c34c74c08a10be28cb806d647bdbe20971e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:51 +0000

    Add package pydawn/1.0.6/pydawn-1.0.6.tar.gz

---
+++ packages/pydawn/pydawn-1.0.6.tar.gz/pydawn-1.0.6/pydawn/file_utils.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+import hashlib
+import os
+import chardet
+import codecs
+
+
+def get_file_md5(filename):
+    if not os.path.isfile(filename):
+        return
+    file_hash = hashlib.md5()
+    f = file(filename, 'rb')
+    while True:
+        b = f.read(8096)
+        if not b :
+            break
+        file_hash.update(b)
+    f.close()
+    return file_hash.hexdigest().upper()
+
+
+def detect_file_charset(file_path):
+    f = open(file_path, "rb")
+    test_length = 1024
+    data = f.read(test_length)
+    result = chardet.detect(data)
+    encoding = result['encoding']
+    if encoding is None:
+        encoding = "utf-8"
+    return encoding.lower()
+
+
+def open_file_in_utf8(file_path):
+    encoding = detect_file_charset(file_path)
+    f = codecs.open(file_path, encoding=encoding)
+    return f
+
+
+if __name__ == '__main__':
+    for line in open_file_in_utf8("cities.txt"):
+        print line
+
