c55159fa2aa9eef2b744e09613518e2e3009b00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:54 +0000

    Add package nester261/1.0.0/nester261-1.0.0.tar.gz

---
+++ packages/nester261/nester261-1.0.0.tar.gz/nester261-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='nester261',
+    version='1.0.0',
+    py_modules=['nester261'],
+    author='Maruf',
+    author_email='marufahamed011@gmail.com',
+    url='https://github.com/Maruf261/nester',
+    description='A simple printer of nested list',
+    )
