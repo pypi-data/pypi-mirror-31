935d63f66ff15dd676569628a29b5a72d2664eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:32 +0000

    Add package hatcher/0.11.1/hatcher-0.11.1-py2.py3-none-any.whl

---
+++ packages/hatcher/hatcher-0.11.1-py2.py3-none-any.whl/hatcher/core/v0/user.py
@@ -0,0 +1,46 @@
+# Canopy product code
+#
+# (C) Copyright 2013 Enthought, Inc., Austin, TX
+# All right reserved.
+#
+# This file is confidential and NOT open source.  Do not distribute.
+#
+from __future__ import absolute_import
+
+from ..url_templates import URLS_V0
+
+
+class User(object):
+
+    def __init__(self, organization_name, email, url_handler, model_registry):
+        self.organization_name = organization_name
+        self.email = email
+        self._url_handler = url_handler
+        self._model_registry = model_registry
+
+    def __repr__(self):
+        return '<{cls} organization={organization!r}, email={email!r}>'.format(
+            cls=type(self).__name__,
+            organization=self.organization_name,
+            email=self.email,
+        )
+
+    def delete(self):
+        """Delete this user from the brood server.
+
+        """
+        path = URLS_V0.admin.users.metadata.format(
+            organization_name=self.organization_name,
+            email=self.email,
+        )
+        self._url_handler.delete(path)
+
+    def metadata(self):
+        """Get the user's metadata.
+
+        """
+        path = URLS_V0.admin.users.metadata.format(
+            organization_name=self.organization_name,
+            email=self.email,
+        )
+        return self._url_handler.get_json(path)
