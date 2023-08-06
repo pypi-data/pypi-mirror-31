aaf158158f4894ffd57b5584a9f00392c9292b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:49 +0000

    Add package sequence2hash/1.0.1/sequence2hash-1.0.1.tar.gz

---
+++ packages/sequence2hash/sequence2hash-1.0.1.tar.gz/sequence2hash-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='sequence2hash',
+    version='1.0.1',
+    description='将 序列值 转为 哈希值，并展示出值在序列中对应的路径',
+    keywords='hash dict list key/value',
+    url='https://github.com/Cuile/sequence2hash',
+
+    packages=['sequence2hash'],
+
+    author='cuile',
+    author_email='i@cuile.com'
+)
