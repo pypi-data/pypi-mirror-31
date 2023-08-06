f3f1e39d96da3702b65f58fae893ffe82d82a44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:05 +0000

    Add package iml-common/1.4.2/iml-common-1.4.2.tar.gz

---
+++ packages/iml-common/iml-common-1.4.2.tar.gz/iml-common-1.4.2/iml_common/test/iml_unit_testcase.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2017 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+
+import mock
+
+from django.utils import unittest
+
+
+class ImlUnitTestCase(unittest.TestCase):
+    def setUp(self):
+        super(ImlUnitTestCase, self).setUp()
+
+        self.addCleanup(mock.patch.stopall)
