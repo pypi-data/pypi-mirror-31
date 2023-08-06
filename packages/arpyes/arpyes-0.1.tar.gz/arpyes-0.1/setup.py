d356c3fc34a753650abbf9b4c390e16b8d0ec241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:22 +0000

    Add package arpyes/0.1/arpyes-0.1.tar.gz

---
+++ packages/arpyes/arpyes-0.1.tar.gz/arpyes-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='arpyes',
+      version='0.1',
+      description='Analysis package for Diamond Light Source ARPES data.',
+      url='http://github.com/domherriott/arpyes',
+      author='Dominic Herriott',
+      author_email='herriott95@gmail.com',
+      license='MIT',
+      packages=['arpyes'],
+      zip_safe=False)
