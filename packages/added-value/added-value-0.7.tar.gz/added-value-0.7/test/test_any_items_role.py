f27e2e91dbc11e858bd15762160bfa55e8c0ba48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:27 +0000

    Add package added-value/0.7/added-value-0.7.tar.gz

---
+++ packages/added-value/added-value-0.7.tar.gz/added-value-0.7/test/test_any_items_role.py
@@ -0,0 +1,18 @@
+from test.example_source import with_test_examples_source
+
+basename = "test_any_items_role"
+html_filename = basename + ".html"
+
+
+@with_test_examples_source
+def test_base_name_in_html(app, status, warning):
+    app.build()
+    html = (app.outdir / html_filename).read_text()
+    assert basename in html
+
+
+@with_test_examples_source
+def test_any_items_items_html_contains_value(app, status, warning):
+    app.build()
+    html = (app.outdir / html_filename).read_text()
+    assert "I would choose any of cherry, chocolate, or pear." in html
