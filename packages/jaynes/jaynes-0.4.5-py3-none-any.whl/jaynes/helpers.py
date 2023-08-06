2b7277f6535e7d7008e927e987bca85ccf12cfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package jaynes/0.4.5/jaynes-0.4.5-py3-none-any.whl

---
+++ packages/jaynes/jaynes-0.4.5-py3-none-any.whl/jaynes/helpers.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+
+
+def path_no_ext(path):
+    return '.'.join(path.split('.')[:-1])
+
+
+def get_temp_dir():
+    """returns a temporal directory. Mac OSX /val is a symbolic link, which is why we return the resolved path."""
+    tmp_dir = tempfile.mkdtemp()
+    return os.path.realpath(tmp_dir)
+
+
+def tag_instance(Name=None, **kwargs):
+    from .shell import ck
+    cmd = f'''"EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`" && '''\
+          f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key=Name,Value='{Name}' --region us-west-2"
+    # f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key=exp_prefix,Value={instance_tag} --region us-west-2"
+    return ck(cmd, shell=True)
