2893f8ed4f96b121f219bb9852ae902f375d6b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:01 +0000

    Add package tdata/0.22/tdata-0.22.tar.gz

---
+++ packages/tdata/tdata-0.22.tar.gz/tdata-0.22/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tdata',
+    version='0.22',
+    author='kellerx',
+    author_email='kellerx0319@gmail.com',
+    url='https://github.com/xashes/tdata',
+    packages=find_packages(),
+)
