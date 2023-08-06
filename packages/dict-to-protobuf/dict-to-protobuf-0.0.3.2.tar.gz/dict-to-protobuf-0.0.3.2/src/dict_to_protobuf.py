4fc0077c2985c6c1c2f95e76f64c7c26e09fb92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package dict-to-protobuf/0.0.3.2/dict-to-protobuf-0.0.3.2.tar.gz

---
+++ packages/dict-to-protobuf/dict-to-protobuf-0.0.3.2.tar.gz/dict-to-protobuf-0.0.3.2/src/dict_to_protobuf.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+import logging
+
+l = logging.getLogger('dict_to_protbuf')
+
+__all__ = ['dict_to_protobuf']
+def parse_list(values,message):
+    '''parse list to protobuf message'''
+    if isinstance(values[0],dict):#value needs to be further parsed
+        for v in values:
+            cmd = message.add()
+            parse_dict(v,cmd)
+    else:#value can be set
+        message.extend(values)
+
+
+def parse_dict(values,message):
+    for k,v in values.iteritems():
+        if isinstance(v,dict):#value needs to be further parsed
+            parse_dict(v,getattr(message,k))
+        elif isinstance(v,list):
+            parse_list(v,getattr(message,k))
+        else:#value can be set
+            try:
+                setattr(message, k, v)
+            except AttributeError:
+                logging.basicConfig()
+                l.warning('try to access invalid attributes %r.%r = %r',message,k,v)
+                
+            
+
+def dict_to_protobuf(value,message):
+    if isinstance(value, dict):
+        parse_dict(value,message)
+    else: #api compatible with protobuf-to-dict
+        parse_dict(message, value)
