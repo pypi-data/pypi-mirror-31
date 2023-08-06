fb93b4f27d0d5dc4f7c33b4d5b89a63cc7b27d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:43 +0000

    Add package nesterWhz/1.0/nesterWhz-1.0.tar.gz

---
+++ packages/nesterWhz/nesterWhz-1.0.tar.gz/nesterWhz-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nesterWhz', 
+    version='1.0', 
+    description='test nesterWhz',
+    author='whz',
+    author_email='1217044974@qq.com',
+    url='http://www.baidu.com',
+    py_modules=['nesterWhz'],
+)
