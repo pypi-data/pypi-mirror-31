78a9c17be9c1a08b62cb97b8251ed5d2b00762c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:11 +0000

    Add package psims/0.1.6/psims-0.1.6-py2-none-any.whl

---
+++ packages/psims/psims-0.1.6-py2-none-any.whl/psims/utils.py
@@ -0,0 +1,34 @@
+from lxml import etree
+from six import add_metaclass, string_types as basestring
+from gzip import GzipFile
+
+try:
+    from collections import Iterable, Mapping
+except ImportError:
+    from collections.abc import Iterable, Mapping
+
+from psims import compression
+
+
+def ensure_iterable(obj):
+    if obj is None:
+        return tuple()
+    if not isinstance(obj, Iterable) or isinstance(obj, basestring) or isinstance(obj, Mapping):
+        return [obj]
+    return obj
+
+
+def pretty_xml(path, outpath=None, encoding=b'utf-8'):
+    tree = etree.parse(path)
+    if outpath is None:
+        opener = compression.get(path)
+        outpath = opener(path, 'wb')
+    if hasattr(outpath, 'write'):
+        outstream = outpath
+    else:
+        opener = compression.get(path)
+        outstream = opener(outpath, 'wb')
+    with outstream:
+        outstream.write(b'<?xml version="1.0" encoding="' + encoding + b'"?>\n')
+        outstream.write(
+            etree.tostring(tree, pretty_print=True))
