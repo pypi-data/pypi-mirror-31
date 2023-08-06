e5669837c3cf6c24743a66cbaf68cb72a163fd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:51 +0000

    Add package quite/0.2.0/quite-0.2.0.tar.gz

---
+++ packages/quite/quite-0.2.0.tar.gz/quite-0.2.0/quite/gui/widgets/line_edit.py
@@ -0,0 +1,23 @@
+import prett
+from .. import QLineEdit
+from .. import ui_extension
+from .. import BaseInterface
+
+
+@ui_extension
+class LineEdit(QLineEdit, BaseInterface, prett.WidgetStringInterface):
+    class StringItem(prett.WidgetStringItem):
+        def __init__(self, parent: 'LineEdit'):
+            self.parent = parent
+
+        def get_value(self):
+            return self.parent.text()
+
+        def set_value(self, value):
+            value = value or ''
+            if value != self.get_value():
+                self.parent.setText(value)
+
+        def set_changed_connection(self):
+            # noinspection PyUnresolvedReferences
+            self.parent.textChanged.connect(self.string.check_change)
