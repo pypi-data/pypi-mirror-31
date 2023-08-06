8d445c333146921a9d5006f8f40d942e52294749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:10 +0000

    Add package pystubs/2018.5.3/pystubs-2018.5.3.tar.gz

---
+++ packages/pystubs/pystubs-2018.5.3.tar.gz/pystubs-2018.5.3/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from setuptools import setup
+setup(
+    py_modules=['stubs'],
+    long_description_content_type='text/markdown',
+    setup_requires=['setuptools>=38.6.0'],
+)
