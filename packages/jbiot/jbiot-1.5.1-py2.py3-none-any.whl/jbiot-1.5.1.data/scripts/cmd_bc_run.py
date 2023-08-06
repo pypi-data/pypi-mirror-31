14642f660db68bfc50a3bf543e0155860d19eb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:40 +0000

    Add package jbiot/1.5.1/jbiot-1.5.1-py2.py3-none-any.whl

---
+++ packages/jbiot/jbiot-1.5.1-py2.py3-none-any.whl/jbiot-1.5.1.data/scripts/cmd_bc_run.py
@@ -0,0 +1,34 @@
+#!python
+#coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import os
+dirpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../")
+sys.path.insert(0,dirpath)
+from jbiot.subjobs.alisub.cmd_bc_run import cmd_run
+
+if __name__ == "__main__":
+    from docopt import docopt
+
+    usage = """
+    Usage:
+        cmd_bc_run.py [options] <cmdfile> 
+
+    Options:
+        --wdir=<widr>      oss working directory
+        --cpu=<cpu>        cpu nums want to use [default: 1]
+        --mem=<mem>        memory want to use [default: 2G]
+        --docker=<docker>  docker images wants to use [default: jbioi/alpine-dev]
+
+    """
+    args = docopt(usage)
+    wdir = args["--wdir"]
+    cpu = args["--cpu"]
+    mem = args["--mem"]
+    docker = args["--docker"]
+    cmdfile = args["<cmdfile>"]
+    cmdid = cmdfile.split("/")[-1].split(".")[0]
+    cmd = open(cmdfile).readline().strip()
+    cmd_run(cmdid,cmd,wdir,cpu,mem,docker)
+
