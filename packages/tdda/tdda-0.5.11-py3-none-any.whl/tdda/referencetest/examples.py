0a0476750863fc9ff86e3f04dcd091de45ab2618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:57 +0000

    Add package tdda/0.5.11/tdda-0.5.11-py3-none-any.whl

---
+++ packages/tdda/tdda-0.5.11-py3-none-any.whl/tdda/referencetest/examples.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+The :py:mod:`tdda.referencetest` module includes a set of examples,
+for both :py:mod:`unittest` and :py:mod:`pytest`.
+
+To copy these examples to your own *referencetest-examples* subdirectory
+(or to a location of your choice), run the command::
+
+    tdda examples referencetest [mydirectory]
+
+"""
+from __future__ import absolute_import
+
+from tdda import examples
+
+if __name__ == '__main__':
+    examples.copy_main('referencetest')
+
