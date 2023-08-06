b67c70dcb783296c97d0876f39a255893b6be63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:05 +0000

    Add package wordpress-cd-s3/0.2.0/wordpress-cd-s3-0.2.0.tar.gz

---
+++ packages/wordpress-cd-s3/wordpress-cd-s3-0.2.0.tar.gz/wordpress-cd-s3-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name = 'wordpress-cd-s3',
+    version = '0.2.0',
+    description = 'Rancher driver to deploy WP artifacts to S3 buckets.',
+    author = 'Ross Golder',
+    author_email = 'ross@golder.org',
+    url = 'https://github.com/rossigee/wordpress-cd-s3',
+    install_requires = [
+      'wordpress-cd'
+    ],
+    packages = [
+      'wordpress_cd_s3',
+    ]
+)
