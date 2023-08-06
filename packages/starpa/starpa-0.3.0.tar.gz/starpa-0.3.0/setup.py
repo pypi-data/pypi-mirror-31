c6e55d53592bed5810c18b0e84f2c72ab025e804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:46 +0000

    Add package starpa/0.3.0/starpa-0.3.0.tar.gz

---
+++ packages/starpa/starpa-0.3.0.tar.gz/starpa-0.3.0/setup.py
@@ -0,0 +1,39 @@
+#import src.starpa
+
+import setuptools
+import src.starpa
+
+setuptools.setup(
+    name="starpa",
+    version=src.starpa.__version__,
+    url="https://github.com/luidale/starpa",
+	download_url="https://github.com/luidale/starpa/archive/v0.3.0.tar.gz",
+    author="Hannes Luidalepp",
+    author_email="luidale@gmail.com",
+	license=src.starpa.__license__,
+    description="Stable RNA processing product analyzer",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages('src'),
+	package_dir={'': 'src'},
+	entry_points = {
+        'console_scripts': ['starpa=starpa.__main__:main']},
+		
+    install_requires=["pyfaidx","schema","docopt","cutadapt"],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+		'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Topic :: Scientific/Engineering',
+		'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    package_data={'': ['data/*.*']}
+)
