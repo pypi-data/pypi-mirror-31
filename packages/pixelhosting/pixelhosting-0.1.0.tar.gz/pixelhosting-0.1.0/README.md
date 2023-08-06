0ceb71b930bda12009f2d3764fc01f778a8bd1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:10 +0000

    Add package pixelhosting/0.1.0/pixelhosting-0.1.0.tar.gz

---
+++ packages/pixelhosting/pixelhosting-0.1.0.tar.gz/pixelhosting-0.1.0/README.md
@@ -0,0 +1,1 @@
+# pixelhosting-python-sdk
---
+++ packages/pixelhosting/pixelhosting-0.1.0.tar.gz/pixelhosting-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pixelhosting",
+    version="0.1.0",
+    description="Python package for PixelHosting.",
+    url="https://github.com/amosbastian/pixelhosting-python-sdk",
+    author="Amos Bastian",
+    author_email="amosbastian@googlemail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="pixelhosting wehmoen",
+    packages=find_packages(),
+    install_requires=["requests"],
+)
