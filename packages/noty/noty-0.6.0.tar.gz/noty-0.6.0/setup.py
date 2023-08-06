eecf841925e074aa99a2d1d12e5eef808869d9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package noty/0.6.0/noty-0.6.0.tar.gz

---
+++ packages/noty/noty-0.6.0.tar.gz/noty-0.6.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name = 'noty',
+    packages=["noty"],
+    version = '0.6.0',
+    description = 'Creating sticky notes has never been easier',
+    author = 'GrgBls',
+    author_email = 'gmpalis6@gmail.com',
+    url = 'https://github.com/GrgBls/noty', 
+    py_modules=['noty'],
+    install_requires=[
+        
+    ],
+    entry_points='''
+        [console_scripts]
+        noty=noty.noty:cli
+    ''',
+)
+
+
+
+
+
+
+
+
+
+
