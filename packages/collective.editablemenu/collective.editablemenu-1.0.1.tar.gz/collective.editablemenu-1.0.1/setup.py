d409368b0248e63b7b9a44d3d55eac74993b932e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:35 +0000

    Add package collective.editablemenu/1.0.1/collective.editablemenu-1.0.1.tar.gz

---
+++ packages/collective.editablemenu/collective.editablemenu-1.0.1.tar.gz/collective.editablemenu-1.0.1/setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.editablemenu package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='collective.editablemenu',
+    version='1.0.1',
+    description="A customizable editable menu",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='Python Plone',
+    author='RedTurtle Technology',
+    author_email='sviluppoplone@redturtle.it',
+    url='https://github.com/RedTurtle/collective.editablemenu',
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'Products.CMFPlone>=5.0b2.dev0',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
