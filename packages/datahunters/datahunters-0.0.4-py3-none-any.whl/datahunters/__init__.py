3bd69fe9ad3c657a749d26782aed2bcdba19c6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:12 +0000

    Add package datahunters/0.0.4/datahunters-0.0.4-py3-none-any.whl

---
+++ packages/datahunters/datahunters-0.0.4-py3-none-any.whl/datahunters/__init__.py
@@ -0,0 +1,7 @@
+from datahunters import ecom
+from datahunters import search
+from datahunters import social
+from datahunters import movies
+from datahunters import stock
+from datahunters import shared
+from datahunters import assets
