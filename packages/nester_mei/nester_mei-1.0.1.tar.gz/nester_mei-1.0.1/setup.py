d97530378721f9692cb937473b606c6c01633eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:13 +0000

    Add package nester_mei/1.0.1/nester_mei-1.0.1.tar.gz

---
+++ packages/nester_mei/nester_mei-1.0.1.tar.gz/nester_mei-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_mei',
+	version		= '1.0.1',
+	py_modules	= ['nester_mei'],
+	author		= 'Meiqingli',
+	author_email	= 'meiqingli@gmail.com',
+	url		= 'http://www.zte.com.cn',
+	description 	= 'A simple printer of nested lists',
+	long_description= 'A simple printer of nested lists',
+	license		= 'MIT',
+	classifiers	= [
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+	],
+     )
