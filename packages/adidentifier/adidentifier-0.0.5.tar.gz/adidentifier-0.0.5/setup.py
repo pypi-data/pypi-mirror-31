b50bdaf809f79001848189edb036b392588363e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:01 +0000

    Add package adidentifier/0.0.5/adidentifier-0.0.5.tar.gz

---
+++ packages/adidentifier/adidentifier-0.0.5.tar.gz/adidentifier-0.0.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python  
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys  
+  
+setup(  
+    name="adidentifier",  
+    version="0.0.5",  
+    author="Alecyrus",  
+    author_email="heyuangunia@gmail.com",  
+    description="AdIdentifier",  
+    long_description=open("README.md").read(),  
+    license="MIT",  
+    url="https://github.com/Alecyrus/AdIdentifier",  
+    packages=['adidentifier'],
+    package_dir={'adidentifier':'adidentifier'},
+    package_data={'adidentifier':['*.*','model/*']},
+    install_requires=[   
+        "adblockparser",
+        "tgrocery"
+        ],  
+    classifiers=[  
+        "Environment :: Web Environment",  
+        "Intended Audience :: Developers",  
+        "Operating System :: OS Independent",  
+        "Topic :: Text Processing :: Indexing",  
+        "Topic :: Utilities",  
+        "Topic :: Internet",  
+        "Topic :: Software Development :: Libraries :: Python Modules",  
+        "Programming Language :: Python",  
+        "Programming Language :: Python :: 2",  
+        "Programming Language :: Python :: 2.7",  
+    ], 
+)  
