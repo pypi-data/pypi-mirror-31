ada9d5542585d6bbe84aa3c782bae5c94e0c3f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package Pebble/4.3.8/Pebble-4.3.8.tar.gz

---
+++ packages/Pebble/Pebble-4.3.8.tar.gz/Pebble-4.3.8/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="Pebble",
+    version="4.3.8",
+    author="Matteo Cafasso",
+    author_email="noxdafox@gmail.com",
+    description=("Threading and multiprocessing eye-candy."),
+    license="LGPL",
+    keywords="thread process pool decorator",
+    url="https://github.com/noxdafox/pebble",
+    packages=find_packages(exclude=["tests"]),
+    extras_require={":python_version<'3'": ["futures"]},
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: " +
+        "GNU Library or Lesser General Public License (LGPL)"
+    ],
+)
