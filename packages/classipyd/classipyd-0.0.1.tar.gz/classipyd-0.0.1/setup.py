89c78b71a166ab73f0769ed92ba2223d631d828e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:24 +0000

    Add package classipyd/0.0.1/classipyd-0.0.1.tar.gz

---
+++ packages/classipyd/classipyd-0.0.1.tar.gz/classipyd-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'classipyd',
+    packages = ['classipyd'],
+    version = '0.0.1',
+    description = '''Create videos of a session that interactively can have 
+                     particular areas of the screen redacted.''',
+    author = 'Michael Cole',
+    author_email = 'mcole2009@aol.com',
+    license = 'MIT',
+    url = 'https://github.com/mcole22266/classipyd',
+    download_url = 'https://github.com/mcole22266/classipyd/archive/0.0.1.tar.gz',
+    keywords = ['redacting', 'screenshot', 'video', 'capture'],
+    classifiers = [],
+)
+
