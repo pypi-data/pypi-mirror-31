c3d37f9da1c039541ead68a0ab03d7dc2e428e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package validemail/1.1/validemail-1.1.tar.gz

---
+++ packages/validemail/validemail-1.1.tar.gz/validemail-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'validemail',
+  py_modules=('validemail',),
+  version = '1.1',
+  description = 'alidemail verifies if an email address is valid and really exists.',
+  author = 'Oleg Borodai',
+  author_email = 'oleg@borodai.com',
+  url = 'https://github.com/oleg-borodai/validate_email',
+  download_url = 'https://github.com/oleg-borodai/validate_email/archive/1.1.tar.gz',
+  keywords = ['email', 'validation', 'dnspython'],
+  classifiers = [],
+)
