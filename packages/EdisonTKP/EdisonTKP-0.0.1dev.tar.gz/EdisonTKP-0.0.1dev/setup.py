3996315c0136a61873737a3dd7bce3558b4745f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:31 +0000

    Add package EdisonTKP/0.0.1dev/EdisonTKP-0.0.1dev.tar.gz

---
+++ packages/EdisonTKP/EdisonTKP-0.0.1dev.tar.gz/EdisonTKP-0.0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='EdisonTKP',
+    version='0.0.1dev',
+    packages=['edisontkp',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
