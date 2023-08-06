c73f9bca3a60a18ad5f3848f293c53b6b40ccc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:42 +0000

    Add package orphanage/0.1.0/orphanage-0.1.0-pp360-pypy3_60-manylinux1_x86_64.whl

---
+++ packages/orphanage/orphanage-0.1.0-pp360-pypy3_60-manylinux1_x86_64.whl/orphanage/poll.h
@@ -0,0 +1,11 @@
+typedef struct orphanage_poll_t orphanage_poll_t;
+
+orphanage_poll_t *orphanage_poll_create(unsigned);
+void orphanage_poll_close(orphanage_poll_t *t);
+
+int orphanage_poll_start(orphanage_poll_t *t);
+int orphanage_poll_stop(orphanage_poll_t *t);
+
+extern "Python+C" {
+    int orphanage_poll_routine_callback(orphanage_poll_t *t);
+}
