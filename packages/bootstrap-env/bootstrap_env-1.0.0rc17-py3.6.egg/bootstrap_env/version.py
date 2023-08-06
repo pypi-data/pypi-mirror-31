c5fbc8d9be69155b125f16ad9ae5d3c6202226e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package bootstrap-env/1.0.0rc17/bootstrap_env-1.0.0rc17-py3.6.egg

---
+++ packages/bootstrap-env/bootstrap_env-1.0.0rc17-py3.6.egg/bootstrap_env/version.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+"""
+    bootstrap-env
+    ~~~~~~~~~~~~~
+
+    :copyleft: 2018 by bootstrap-env team, see AUTHORS for more details.
+    :created: 2018 by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+# https://pypi.org/project/bootstrap_env/#history
+# https://github.com/jedie/bootstrap_env#history
+__version__ = "1.0.0rc17"
