1882042290dae8e8525e79d2005822dad5180908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:13 +0000

    Add package testtest/1.0/testtest-1.0.tar.gz

---
+++ packages/testtest/testtest-1.0.tar.gz/testtest-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+      name='testtest',
+      version='1.0',
+      description='Testing Framework',
+      url='https://github.com/AlexMcCarroll/pypi_play',
+      author='Alex McCarroll',
+      author_email='alexandra@mccarrollhk.com',
+      license='MIT',
+      packages=['testtest'],
+      zip_safe=False
+      )
