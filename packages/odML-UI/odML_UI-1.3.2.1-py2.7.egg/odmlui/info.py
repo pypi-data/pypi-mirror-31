5b0555123167fa16b1651f99f90d0b7b452dcdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:44 +0000

    Add package odML-UI/1.3.2/odML_UI-1.3.2.1-py2.7.egg

---
+++ packages/odML-UI/odML_UI-1.3.2.1-py2.7.egg/odmlui/info.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, "info.json")) as infofile:
+    infodict = json.load(infofile)
+
+VERSION = infodict["VERSION"]
+AUTHOR = infodict["AUTHOR"]
+COPYRIGHT = infodict["COPYRIGHT"]
+CONTACT = infodict["CONTACT"]
+HOMEPAGE = infodict["HOMEPAGE"]
+CLASSIFIERS = infodict["CLASSIFIERS"]
+ODMLTABLES_VERSION_MIN = infodict["ODMLTABLES_VERSION_MIN"]
+ODMLTABLES_VERSION_MAX = infodict["ODMLTABLES_VERSION_MAX"]
