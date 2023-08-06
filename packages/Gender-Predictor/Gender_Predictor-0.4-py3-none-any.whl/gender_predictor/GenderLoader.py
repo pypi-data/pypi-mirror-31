680b91a1922535829dbce77dfcae630818ec7bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:27 +0000

    Add package Gender-Predictor/0.4/Gender_Predictor-0.4-py3-none-any.whl

---
+++ packages/Gender-Predictor/Gender_Predictor-0.4-py3-none-any.whl/gender_predictor/GenderLoader.py
@@ -0,0 +1,41 @@
+'''
+This class is responsible for loading files data in memory, This data will be containing gender types.
+a.Male
+b.Female
+c.Unisex
+'''
+import csv
+
+
+def get_name_list():
+    # print('Get the male and female list name')
+    name_dict = get_data_from_csv()
+
+    male_names = list()
+    female_names = list()
+
+    # print('Sorting Names')
+    for name in name_dict:
+        counts = name_dict[name]
+        tuple = (name, counts[0], counts[1])
+        if counts[0] > counts[1]:
+            male_names.append(tuple)
+        elif counts[1] > counts[0]:
+            female_names.append(tuple)
+
+    names = (male_names, female_names)
+    return names
+
+
+def get_data_from_csv():
+    # Convert csv data to dictionary
+    names = dict()
+    with open('data/gender_type.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        for row in reader:
+            names[row['name']] = [int(row['male_count']), int(row['female_count'])]
+    return names
+
+
+if __name__ == "__main__":
+    print(get_name_list())
