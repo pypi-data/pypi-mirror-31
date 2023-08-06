2502c20588c9b11e15fd665fc2fdc372aa56d9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:22 +0000

    Add package perftool/0.1.1/perftool-0.1.1.tar.gz

---
+++ packages/perftool/perftool-0.1.1.tar.gz/perftool-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='perftool',
+      version='0.1.1',
+      description='The Performance Tool',
+      url='https://github.com/Yajan/Perftool.git',
+      author='Yajana',
+      author_email='yajananrao@gmail.com',
+      license='Apache License',
+      packages=['perftool'],
+      zip_safe=False)
