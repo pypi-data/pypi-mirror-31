505dbb74149b4322f9ea9d248d0b5554cd7422a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:04 +0000

    Add package padar/1.0.6/padar-1.0.6.win-amd64.zip

---
+++ packages/padar/padar-1.0.6.win-amd64.zip/Users/tqshe/Miniconda3/Scripts/dar-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\tqshe\Miniconda3\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'padar==1.0.6','console_scripts','dar'
+__requires__ = 'padar==1.0.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('padar==1.0.6', 'console_scripts', 'dar')()
+    )
