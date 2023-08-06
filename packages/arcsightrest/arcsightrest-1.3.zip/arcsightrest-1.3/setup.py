bc7c2278270cd0d5c8f786dc366f265d7bc4f8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:28 +0000

    Add package arcsightrest/1.3/arcsightrest-1.3.zip

---
+++ packages/arcsightrest/arcsightrest-1.3.zip/arcsightrest-1.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='arcsightrest',
+    version='1.3',
+    description='Python library to connect to the HP Arcsight Logger REST API',
+    author='Marius Iversen',
+    license='MIT',
+    author_email='marius@chasenet.org',
+    url='https://github.com/P1llus/ArcSight-Rest',
+    download_url='https://github.com/P1llus/ArcSight-Rest/tarball/1.3',
+    keywords=['arcsight', 'logger', 'rest'],
+    include_package_data=True,
+    zip_safe=True,
+    py_modules=['arcsightrest'],
+    install_requires=[
+        'untangle',
+        'requests',
+    ],
+)
