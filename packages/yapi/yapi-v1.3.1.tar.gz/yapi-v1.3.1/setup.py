dc5a5b44ce8a8e9e154e7da4b0a6c3f7e74f422b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:19 +0000

    Add package yapi/v1.3.1/yapi-v1.3.1.tar.gz

---
+++ packages/yapi/yapi-v1.3.1.tar.gz/yapi-v1.3.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+classifiers = ["Development Status :: Beta",
+               "Intended Audience :: Developers",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: 3.5",
+               "Operating System :: OS Independent",
+               'Topic :: Software Development :: Libraries :: Python Modules']
+
+try:
+    long_description = open('README.md').read()
+except IOError:
+    long_description = ""
+
+setup(
+    name='yapi',
+    version='v1.3.1',
+    packages=[''],
+    install_requires=['requests'],
+    url='http://www.ahmetkotan.com.tr',
+    license='GPL',
+    author='Ahmet Kotan',
+    author_email='ahmtkotan@gmail.com',
+    description='Python Youtube Data API v3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="youtube data api",
+)
