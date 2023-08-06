94b646d57acc1d0c7fcfe4c408c123866d9197c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:40 +0000

    Add package pma-python/2.0.0.29/pma_python-2.0.0.29.tar.gz

---
+++ packages/pma-python/pma_python-2.0.0.29.tar.gz/pma_python-2.0.0.29/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='pma_python',
+      version='2.0.0.29',
+      description='Universal viewing of digital microscopy, whole slide imaging and digital pathology data',
+      url='http://github.com/pathomation/pma_python',
+      author='Pathomation',
+	  author_email='info@pathomation.com',
+      license='http://free.pathomation.com/eula/',
+      packages=['pma_python'],
+	  classifiers = [
+		'Development Status :: 3 - Alpha', 
+		'Programming Language :: Python :: 3'],
+	  keywords='wsi whole slide imaging gigapixel microscopy histology pathology',
+	  install_requires=['pillow', 'requests'],
+	  python_requires='>=3',	# assume this only works in Python 3
+      zip_safe=False)
