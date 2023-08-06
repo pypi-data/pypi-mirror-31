b0ea3cec8595aee0e6fb17735d9c2fe46989d4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:28 +0000

    Add package fakespotipy/0.4.0/fakespotipy-0.4.0.tar.gz

---
+++ packages/fakespotipy/fakespotipy-0.4.0.tar.gz/fakespotipy-0.4.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    author="@LoisaidaSam",
+    author_email="sam.sandberg@gmail.com",
+    description="A fake spotipy client. For unit tests and stuff.",
+    keywords=["spotify", "spotipy", "wrapper", "testing", "unit testing"],
+    license="MIT",
+    name="fakespotipy",
+    packages=["fakespotipy"],
+    test_suite="tests",
+    url="https://github.com/rcrdclub/fakespotipy",
+    version="0.4.0",
+)
