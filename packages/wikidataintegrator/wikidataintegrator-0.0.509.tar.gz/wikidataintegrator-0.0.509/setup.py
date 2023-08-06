59adb25592efb098392c62114f118ad7bf0d3443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:56 +0000

    Add package wikidataintegrator/0.0.509/wikidataintegrator-0.0.509.tar.gz

---
+++ packages/wikidataintegrator/wikidataintegrator-0.0.509.tar.gz/wikidataintegrator-0.0.509/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+MICRO_VERSION = 509
+
+REPO_URL = 'https://github.com/sulab/WikidataIntegrator'
+
+setup(
+    name='wikidataintegrator',
+    version="{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION),
+    author='Sebastian Burgstaller-Muehlbacher, Greg Stupp, Andra Waagmeester',
+    author_email='sburgs@scripps.edu',
+    description='Python package for reading and writing to/from Wikidata',
+    license='MIT',
+    keywords='Wikidata biology chemistry medicine',
+    url=REPO_URL,
+    # packages=find_packages(),
+    packages=['wikidataintegrator', 'wikidataintegrator.backoff', 'wikidataintegrator.ref_handlers'],
+    include_package_data=True,
+    # long_description=read('README.md'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'simplejson',
+        'mwoauth',
+    ],
+)
