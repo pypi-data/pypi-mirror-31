e4e6363ccd6279103bf0f61f3a7eb9a41f8a3807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:54 +0000

    Add package coachdata/1.0.0/coachdata-1.0.0.tar.gz

---
+++ packages/coachdata/coachdata-1.0.0.tar.gz/coachdata-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'coachdata',
+	version ='1.0.0',
+	py_modules = ['coach6'],
+	author = 'lijiankang',
+	author_email = '1124358467@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'modify a mistake,false-->False',
+)
