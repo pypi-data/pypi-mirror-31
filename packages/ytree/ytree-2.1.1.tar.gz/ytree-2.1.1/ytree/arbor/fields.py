c355f6baf5467fe63da19cde767502fc365942fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package ytree/2.1.1/ytree-2.1.1.tar.gz

---
+++ packages/ytree/ytree-2.1.1.tar.gz/ytree-2.1.1/ytree/arbor/fields.py
@@ -0,0 +1,131 @@
+"""
+Arbor field-related classes
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) ytree development team. All rights reserved.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from collections import \
+    defaultdict
+import numpy as np
+import weakref
+
+from ytree.utilities.exceptions import \
+    ArborFieldCircularDependency, \
+    ArborFieldDependencyNotFound, \
+    ArborFieldNotFound
+
+class FieldInfoContainer(dict):
+    """
+    A container for information about fields.
+    """
+
+    alias_fields = ()
+    known_fields = ()
+    def __init__(self, arbor):
+        self.arbor = weakref.proxy(arbor)
+
+    def setup_known_fields(self):
+        """
+        Add units for fields on disk as defined in the known_fields
+        tuple.
+        """
+        for field, units in self.known_fields:
+            if field not in self:
+                continue
+            funits = self[field].get("units")
+            if funits is None:
+                self[field]["units"] = units
+
+    def setup_aliases(self):
+        """
+        Add aliases defined in the alias_fields tuple for each frontend.
+        """
+        for alias in self.alias_fields:
+            aliasname, fieldname, units = alias
+            if not isinstance(fieldname, tuple):
+                fieldname = (fieldname,)
+            for fname in fieldname:
+                self.arbor.add_alias_field(
+                    aliasname, fname, units=units,
+                    force_add=False)
+
+        # Fields with "/" in the name don't play well with hdf5.
+        for field in self.arbor.field_list:
+            if "/" not in field:
+                continue
+            alias = field.replace("/", "_")
+            self.arbor.add_alias_field(alias, field)
+
+    def setup_derived_fields(self):
+        """
+        Add stock derived fields.
+        """
+        def _redshift(data):
+            return 1. / data["scale_factor"] - 1.
+        self.arbor.add_derived_field(
+            "redshift", _redshift, units="", force_add=False)
+
+    def resolve_field_dependencies(self, fields, fcache=None):
+        """
+        Divide fields into those to be read and those to generate.
+        """
+        if fcache is None:
+            fcache = {}
+        fields_to_read = []
+        fields_to_generate = []
+        fields_to_resolve = fields[:]
+
+        while len(fields_to_resolve) > 0:
+            field = fields_to_resolve.pop(0)
+            if field in fcache:
+                continue
+            if field not in self:
+                raise ArborFieldNotFound(field, self.arbor)
+            ftype = self[field].get("type")
+            if ftype == "derived" or ftype == "alias":
+                deps = self[field]["dependencies"]
+                if field in deps:
+                    raise ArborFieldCircularDependency(
+                        field, self.arbor)
+                fields_to_resolve.extend(
+                    set(deps).difference(set(fields_to_resolve)))
+                if field not in fields_to_generate:
+                    fields_to_generate.append(field)
+            elif ftype == "analysis":
+                fields_to_generate.append(field)
+            else:
+                if field not in fields_to_read:
+                    fields_to_read.append(field)
+        return fields_to_read, fields_to_generate
+
+class FieldContainer(dict):
+    """
+    A container for field data.
+    """
+    def __init__(self, arbor):
+        self.arbor = weakref.proxy(arbor)
+
+class FakeFieldContainer(defaultdict):
+    """
+    A fake field data container used to calculate dependencies.
+    """
+    def __init__(self, arbor, name=None):
+        self.arbor = weakref.proxy(arbor)
+        self.name = name
+
+    def __missing__(self, key):
+        if key not in self.arbor.field_info:
+            raise ArborFieldDependencyNotFound(
+                self.name, key, self.arbor)
+        units = self.arbor.field_info[key].get("units", "")
+        self[key] = self.arbor.arr(np.ones(1), units)
+        return self[key]
---
+++ packages/ytree/ytree-2.1.1.tar.gz/ytree-2.1.1/ytree/utilities/logger.py
@@ -0,0 +1,53 @@
+"""
+ytree logger
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2016, Britton Smith <brittonsmith@gmail.com>
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import logging
+import sys
+
+from yt.utilities.logger import \
+    ytLogger
+
+ytLogger.setLevel(40)
+
+# CRITICAL 50
+# ERROR    40
+# WARNING  30
+# INFO     20
+# DEBUG    10
+# NOTSET   10
+
+ufstring = "%(name)-3s: [%(levelname)-9s] %(asctime)s %(message)s"
+
+ytreeLogger = logging.getLogger("ytree")
+ytree_sh = logging.StreamHandler(stream=sys.stderr)
+formatter = logging.Formatter(ufstring)
+ytree_sh.setFormatter(formatter)
+ytreeLogger.addHandler(ytree_sh)
+ytreeLogger.setLevel(20)
+ytreeLogger.propagate = False
+
+def set_parallel_logger(comm):
+    if comm.size == 1: return
+    f = logging.Formatter("P%03i %s" % (comm.rank, ufstring))
+    if len(ytreeLogger.handlers) > 0:
+        ytreeLogger.handlers[0].setFormatter(f)
+
+class fake_pbar(object):
+    def __init__(self, *args):
+        pass
+    def update(self, *args):
+        pass
+    def finish(self):
+        pass
