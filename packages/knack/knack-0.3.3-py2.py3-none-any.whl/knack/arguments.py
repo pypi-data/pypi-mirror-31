3a4a149fca94380fcd82174944a0d2090bdc9c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:32 +0000

    Add package knack/0.3.3/knack-0.3.3-py2.py3-none-any.whl

---
+++ packages/knack/knack-0.3.3-py2.py3-none-any.whl/knack/arguments.py
@@ -0,0 +1,224 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import argparse
+from collections import defaultdict
+
+from .log import get_logger
+
+logger = get_logger(__name__)
+
+
+class CLIArgumentType(object):
+
+    REMOVE = '---REMOVE---'
+
+    def __init__(self, overrides=None, **kwargs):
+        """A base CLI Argument Type that can be applied to multiple command arguments
+
+        :param overrides: The base argument that you are overriding
+        :type overrides: knack.arguments.CLIArgumentType
+        :param kwargs: Possible values: `options_list`, `validator`, `completer`, `nargs`, `action`, `const`, `default`,
+                       `type`, `choices`, `required`, `help`, `metavar`. See /docs/arguments.md.
+        """
+        if isinstance(overrides, str):
+            raise ValueError("Overrides has to be a {} (cannot be a string)".format(CLIArgumentType.__name__))
+        options_list = kwargs.get('options_list', None)
+        if options_list and isinstance(options_list, str):
+            kwargs['options_list'] = [options_list]
+        self.settings = {}
+        self.update(overrides, **kwargs)
+
+    def update(self, other=None, **kwargs):
+        if other:
+            self.settings.update(**other.settings)
+        self.settings.update(**kwargs)
+
+
+class CLICommandArgument(object):  # pylint: disable=too-few-public-methods
+
+    NAMED_ARGUMENTS = ['options_list', 'validator', 'completer', 'arg_group']
+
+    def __init__(self, dest=None, argtype=None, **kwargs):
+        """An argument that has a specific destination parameter.
+
+        :param dest: The parameter that this argument is for
+        :type dest: str
+        :param argtype: The argument type for this command argument
+        :type argtype: knack.arguments.CLIArgumentType
+        :param kwargs: see knack.arguments.CLIArgumentType
+        """
+        self.type = CLIArgumentType(overrides=argtype, **kwargs)
+        if dest:
+            self.type.update(dest=dest)
+
+        # We'll do an early fault detection to find any instances where we have inconsistent
+        # set of parameters for argparse
+        if not self.options_list and 'required' in self.options:  # pylint: disable=access-member-before-definition
+            raise ValueError(message="You can't specify both required and an options_list")
+        if not self.options.get('dest', False):
+            raise ValueError('Missing dest')
+        if not self.options_list:  # pylint: disable=access-member-before-definition
+            self.options_list = ('--{}'.format(self.options['dest'].replace('_', '-')),)
+
+    def __getattr__(self, name):
+        if name in self.NAMED_ARGUMENTS:
+            return self.type.settings.get(name, None)
+        elif name == 'name':
+            return self.type.settings.get('dest', None)
+        elif name == 'options':
+            return {key: value for key, value in self.type.settings.items()
+                    if key != 'options' and key not in self.NAMED_ARGUMENTS and
+                    not value == CLIArgumentType.REMOVE}
+        elif name == 'choices':
+            return self.type.settings.get(name, None)
+        else:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):  # pylint: disable=inconsistent-return-statements
+        if name == 'type':
+            return super(CLICommandArgument, self).__setattr__(name, value)
+        self.type.settings[name] = value
+
+
+class ArgumentRegistry(object):
+    """A registry of all the arguments registered"""
+
+    def __init__(self):
+        self.arguments = defaultdict(lambda: {})
+
+    def register_cli_argument(self, scope, dest, argtype, **kwargs):
+        """ Add an argument to the argument registry
+
+        :param scope: The command level to apply the argument registration (e.g. 'mygroup mycommand')
+        :type scope: str
+        :param dest: The parameter/destination that this argument is for
+        :type dest: str
+        :param argtype: The argument type for this command argument
+        :type argtype: knack.arguments.CLIArgumentType
+        :param kwargs: see knack.arguments.CLIArgumentType
+        """
+        argument = CLIArgumentType(overrides=argtype,
+                                   **kwargs)
+        self.arguments[scope][dest] = argument
+
+    def get_cli_argument(self, command, name):
+        """ Get the argument for the command after applying the scope hierarchy
+
+        :param command: The command that we want the argument for
+        :type command: str
+        :param name: The name of the argument
+        :type name: str
+        :return: The CLI command after all overrides in the scope hierarchy have been applied
+        :rtype: knack.arguments.CLIArgumentType
+        """
+        parts = command.split()
+        result = CLIArgumentType()
+        for index in range(0, len(parts) + 1):
+            probe = ' '.join(parts[0:index])
+            override = self.arguments.get(probe, {}).get(name, None)
+            if override:
+                result.update(override)
+        return result
+
+
+class ArgumentsContext(object):
+    def __init__(self, command_loader, command_scope):
+        """ Context manager to register arguments
+
+        :param command_loader: The command loader that arguments should be registered into
+        :type command_loader: knack.commands.CLICommandsLoader
+        :param command_scope: The scope to which arguments in this context apply.
+                              More specific scopes will override less specific scopes in the event of a conflict.
+        :type command_scope: str
+        """
+        self.command_loader = command_loader
+        self.command_scope = command_scope
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def argument(self, argument_dest, arg_type=None, **kwargs):
+        """ Register an argument for the given command scope using a knack.arguments.CLIArgumentType
+
+        :param argument_dest: The destination argument to add this argument type to
+        :type argument_dest: str
+        :param arg_type: Predefined CLIArgumentType definition to register, as modified by any provided kwargs.
+        :type arg_type: knack.arguments.CLIArgumentType
+        :param kwargs: Possible values: `options_list`, `validator`, `completer`, `nargs`, `action`, `const`, `default`,
+                       `type`, `choices`, `required`, `help`, `metavar`. See /docs/arguments.md.
+        """
+        self.command_loader.argument_registry.register_cli_argument(self.command_scope,
+                                                                    argument_dest,
+                                                                    arg_type,
+                                                                    **kwargs)
+
+    def ignore(self, argument_dest):
+        """ Register an argument with type knack.arguments.ignore_type (hidden/ignored)
+
+        :param argument_dest: The destination argument to apply the ignore type to
+        :type argument_dest: str
+        """
+        dest_option = ['--__{}'.format(argument_dest.upper())]
+        self.argument(argument_dest, arg_type=ignore_type, options_list=dest_option)
+
+    def extra(self, argument_dest, **kwargs):
+        """Register extra parameters for the given command. Typically used to augment auto-command built
+        commands to add more parameters than the specific SDK method introspected.
+
+        :param argument_dest: The destination argument to add this argument type to
+        :type argument_dest: str
+        :param kwargs: Possible values: `options_list`, `validator`, `completer`, `nargs`, `action`, `const`, `default`,
+                       `type`, `choices`, `required`, `help`, `metavar`. See /docs/arguments.md.
+        """
+        self.command_loader.extra_argument_registry[self.command_scope][argument_dest] = CLICommandArgument(
+            argument_dest, **kwargs)
+
+
+class IgnoreAction(argparse.Action):  # pylint: disable=too-few-public-methods
+    """ Show the argument as unrecognized if it is called """
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        raise argparse.ArgumentError(None, 'unrecognized argument: {} {}'.format(
+            option_string, values or ''))
+
+
+class CaseInsensitiveList(list):
+    """ Determine if a choice is in a choice list in a case-insensitive manner """
+
+    def __contains__(self, other):
+        return next((True for x in self if other.lower() == x.lower()), False)
+
+
+def enum_choice_list(data):
+    """ Creates the argparse choices and type kwargs for a supplied enum type or list of strings """
+
+    # transform enum types, otherwise assume list of string choices
+    if not data:
+        return {}
+    try:
+        choices = [x.value for x in data]
+    except AttributeError:
+        choices = data
+
+    def _type(value):
+        return next((x for x in choices if x.lower() == value.lower()), value) if value else value
+    params = {
+        'choices': CaseInsensitiveList(choices),
+        'type': _type
+    }
+    return params
+
+# GLOBAL ARGUMENT DEFINITIONS
+
+
+ignore_type = CLIArgumentType(
+    help=argparse.SUPPRESS,
+    nargs='?',
+    action=IgnoreAction,
+    required=False)
---
+++ packages/knack/knack-0.3.3-py2.py3-none-any.whl/knack/invocation.py
@@ -0,0 +1,151 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import sys
+
+from collections import defaultdict
+
+from .util import CLIError, CtxTypeError, CommandResultItem, todict
+from .parser import CLICommandParser
+from .commands import CLICommandsLoader
+from .events import (EVENT_INVOKER_PRE_CMD_TBL_CREATE, EVENT_INVOKER_POST_CMD_TBL_CREATE,
+                     EVENT_INVOKER_CMD_TBL_LOADED, EVENT_INVOKER_PRE_PARSE_ARGS,
+                     EVENT_INVOKER_POST_PARSE_ARGS, EVENT_INVOKER_TRANSFORM_RESULT,
+                     EVENT_INVOKER_FILTER_RESULT)
+from .help import CLIHelp
+
+
+class CommandInvoker(object):
+
+    def __init__(self,
+                 cli_ctx=None,
+                 parser_cls=CLICommandParser,
+                 commands_loader_cls=CLICommandsLoader,
+                 help_cls=CLIHelp,
+                 initial_data=None):
+        """ Manages a single invocation of the CLI (i.e. running a command)
+
+        :param cli_ctx: CLI Context
+        :type cli_ctx: knack.cli.CLI
+        :param parser_cls: A class to handle command parsing
+        :type parser_cls: knack.parser.CLICommandParser
+        :param commands_loader_cls: A class to handle loading commands
+        :type commands_loader_cls: knack.commands.CLICommandsLoader
+        :param help_cls: A class to handle help
+        :type help_cls: knack.help.CLIHelp
+        :param initial_data: The initial in-memory collection for this command invocation
+        :type initial_data: dict
+        """
+        from .cli import CLI
+        if cli_ctx is not None and not isinstance(cli_ctx, CLI):
+            raise CtxTypeError(cli_ctx)
+        self.cli_ctx = cli_ctx
+        # In memory collection of key-value data for this current invocation This does not persist between invocations.
+        self.data = initial_data or defaultdict(lambda: None)
+        self.data['command'] = 'unknown'
+        self._global_parser = parser_cls.create_global_parser(cli_ctx=self.cli_ctx)
+        self.help = help_cls(cli_ctx=self.cli_ctx)
+        self.parser = parser_cls(cli_ctx=self.cli_ctx, cli_help=self.help,
+                                 prog=self.cli_ctx.name, parents=[self._global_parser])
+        self.commands_loader = commands_loader_cls(cli_ctx=self.cli_ctx)
+
+    def _filter_params(self, args):  # pylint: disable=no-self-use
+        # Consider - we are using any args that start with an underscore (_) as 'private'
+        # arguments and remove them from the arguments that we pass to the actual function.
+        params = dict([(key, value)
+                       for key, value in args.__dict__.items()
+                       if not key.startswith('_')])
+        params.pop('func', None)
+        params.pop('command', None)
+        return params
+
+    def _rudimentary_get_command(self, args):  # pylint: disable=no-self-use
+        """ Rudimentary parsing to get the command """
+        nouns = []
+        for i, current in enumerate(args):
+            try:
+                if current[0] == '-':
+                    break
+            except IndexError:
+                pass
+            args[i] = current.lower()
+            nouns.append(args[i])
+        return ' '.join(nouns)
+
+    def _validate_cmd_level(self, ns, cmd_validator):  # pylint: disable=no-self-use
+        if cmd_validator:
+            cmd_validator(ns)
+        try:
+            delattr(ns, '_command_validator')
+        except AttributeError:
+            pass
+
+    def _validate_arg_level(self, ns, **_):  # pylint: disable=no-self-use
+        for validator in getattr(ns, '_argument_validators', []):
+            validator(ns)
+        try:
+            delattr(ns, '_argument_validators')
+        except AttributeError:
+            pass
+
+    def _validation(self, parsed_ns):
+        try:
+            cmd_validator = getattr(parsed_ns, '_command_validator', None)
+            if cmd_validator:
+                self._validate_cmd_level(parsed_ns, cmd_validator)
+            else:
+                self._validate_arg_level(parsed_ns)
+        except CLIError:
+            raise
+        except Exception:  # pylint: disable=broad-except
+            err = sys.exc_info()[1]
+            getattr(parsed_ns, '_parser', self.parser).validation_error(str(err))
+
+    def execute(self, args):
+        """ Executes the command invocation
+
+        :param args: The command arguments for this invocation
+        :type args: list
+        :return: The command result
+        :rtype: knack.util.CommandResultItem
+        """
+        self.cli_ctx.raise_event(EVENT_INVOKER_PRE_CMD_TBL_CREATE, args=args)
+        cmd_tbl = self.commands_loader.load_command_table(args)
+        command = self._rudimentary_get_command(args)
+        self.commands_loader.load_arguments(command)
+        self.cli_ctx.raise_event(EVENT_INVOKER_POST_CMD_TBL_CREATE, cmd_tbl=cmd_tbl)
+        self.parser.load_command_table(cmd_tbl)
+        self.cli_ctx.raise_event(EVENT_INVOKER_CMD_TBL_LOADED, parser=self.parser)
+        if not args:
+            self.cli_ctx.completion.enable_autocomplete(self.parser)
+            subparser = self.parser.subparsers[tuple()]
+            self.help.show_welcome(subparser)
+            return None
+
+        if args[0].lower() == 'help':
+            args[0] = '--help'
+
+        self.cli_ctx.completion.enable_autocomplete(self.parser)
+
+        self.cli_ctx.raise_event(EVENT_INVOKER_PRE_PARSE_ARGS, args=args)
+        parsed_args = self.parser.parse_args(args)
+        self.cli_ctx.raise_event(EVENT_INVOKER_POST_PARSE_ARGS, command=parsed_args.command, args=parsed_args)
+
+        self._validation(parsed_args)
+
+        self.data['command'] = parsed_args.command
+
+        params = self._filter_params(parsed_args)
+
+        cmd_result = parsed_args.func(params)
+        cmd_result = todict(cmd_result)
+
+        event_data = {'result': cmd_result}
+        self.cli_ctx.raise_event(EVENT_INVOKER_TRANSFORM_RESULT, event_data=event_data)
+        self.cli_ctx.raise_event(EVENT_INVOKER_FILTER_RESULT, event_data=event_data)
+
+        return CommandResultItem(event_data['result'],
+                                 table_transformer=cmd_tbl[parsed_args.command].table_transformer,
+                                 is_query_active=self.data['query_active'])
