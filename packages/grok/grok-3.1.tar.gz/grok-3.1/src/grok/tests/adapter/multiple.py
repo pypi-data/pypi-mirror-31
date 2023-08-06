d3c37d8313e9ccdb4153e9f0c6a8cb92c0ffbede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:21 +0000

    Add package grok/3.1/grok-3.1.tar.gz

---
+++ packages/grok/grok-3.1.tar.gz/grok-3.1/src/grok/tests/adapter/multiple.py
@@ -0,0 +1,24 @@
+"""
+Multiple models lead to ambiguity:
+
+  >>> grok.testing.grok(__name__)
+  Traceback (most recent call last):
+    ...
+  martian.error.GrokError: Multiple possible contexts for <class 'grok.tests.adapter.multiple.Home'>, please use the 'context' directive.
+
+"""
+import grok
+from zope import interface
+
+class Cave(grok.Model):
+    pass
+
+class Club(grok.Model):
+    pass
+
+class IHome(interface.Interface):
+    pass
+
+@grok.implementer(IHome)
+class Home(grok.Adapter):
+    pass
---
+++ packages/grok/grok-3.1.tar.gz/grok-3.1/src/grok/tests/conflict/rest.py
@@ -0,0 +1,16 @@
+"""
+We cannot register two rest protocols under the same name::
+
+  >>> grok.testing.grok(__name__)
+  Traceback (most recent call last):
+    ...
+  zope.configuration.config.ConfigurationConflictError: Conflicting configuration actions For: ('restprotocol', 'foo')
+"""
+
+import grok
+
+class Protocol1(grok.IRESTLayer):
+    grok.restskin('foo')
+
+class Protocol2(grok.IRESTLayer):
+    grok.restskin('foo')
