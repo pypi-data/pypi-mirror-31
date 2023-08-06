b85d9a6ea8adf31bc2d249f1ac4803c49f1874ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:00 +0000

    Add package strict_functions/2018.5.7/strict_functions-2018.5.7.tar.gz

---
+++ packages/strict_functions/strict_functions-2018.5.7.tar.gz/strict_functions-2018.5.7/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-11-09 11:50:41
+# @Last Modified 2018-03-15
+# @Last Modified time: 2018-03-15 11:51:55
+
+from distutils.core import setup
+
+version = '2018.5.7'
+
+setup(
+  name = 'strict_functions',
+  packages = ['strict_functions'], # this must be the same as the name above
+  version = version,
+  description = 'collection of helpful strict_functions that should have been in itertools',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/strict_functions',
+  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/{}'.format(version),
+  keywords = ['strict_functions', 'type', 'enforce', 'strict', 'restrict', 'strict defaults', 'defaults'],
+  classifiers = [],
+)
