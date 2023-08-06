0a5c768785504fd8b3ee5a0017b297d9e2699612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:35 +0000

    Add package AlphaHousePython/1.0.1/alphahousepython-1.0.1-cp36-cp36m-macosx_10_6_x86_64.whl

---
+++ packages/AlphaHousePython/alphahousepython-1.0.1-cp36-cp36m-macosx_10_6_x86_64.whl/alphahousepython/cpp_src/alphahousenative/IndividualHelper.cpp
@@ -0,0 +1,19 @@
+//
+// Created by david on 09/04/2018.
+//
+#include "alphahousepython.h"
+
+using namespace alphahousepython;
+
+
+
+
+
+//void Individual::makeIndividualPhaseCompliment() {
+//
+//    auto comp2 = genotype->complement(haplotypes[0].get());
+//    auto comp1 = genotype->complement(haplotypes[1].get());
+//
+//    haplotypes[0]->setFromOtherIfMissing(comp1);
+//    haplotypes[1]->setFromOtherIfMissing(comp2);
+//}
