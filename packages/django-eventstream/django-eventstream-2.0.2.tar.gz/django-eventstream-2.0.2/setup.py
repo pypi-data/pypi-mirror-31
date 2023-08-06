9dfd95c1e698e9d7126cc7d3ce47041c29475731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:58 +0000

    Add package django-eventstream/2.0.2/django-eventstream-2.0.2.tar.gz

---
+++ packages/django-eventstream/django-eventstream-2.0.2.tar.gz/django-eventstream-2.0.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+	readme = f.read()
+
+setup(
+name='django-eventstream',
+version='2.0.2',
+description='Django EventStream library',
+long_description=readme,
+author='Justin Karneges',
+author_email='justin@fanout.io',
+url='https://github.com/fanout/django-eventstream',
+license='MIT',
+zip_safe=False,
+packages=['django_eventstream', 'django_eventstream.migrations', 'django_eventstream.management', 'django_eventstream.management.commands'],
+package_data={'django_eventstream': ['static/django_eventstream/*']},
+install_requires=['gripcontrol>=3.1.0,<4', 'django_grip>=1.7,<2', 'Werkzeug>=0.12,<1', 'six>=1.10,<2'],
+tests_require=['Django'],
+test_suite='tests.runtests.runtests',
+classifiers=[
+	'Development Status :: 4 - Beta',
+	'Topic :: Utilities',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 3',
+	'Framework :: Django',
+]
+)
