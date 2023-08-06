b58a91175ae6956810069e11ebf8af9cfd2c2408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package pysat/1.0.1/pysat-1.0.1.tar.gz

---
+++ packages/pysat/pysat-1.0.1.tar.gz/pysat-1.0.1/pysat/instruments/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+
+__all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi', 'cosmic2013_gps', 'cosmic_gps',
+           'icon_ivm', 'icon_euv', 'iss_fpmu',
+           'omni_hro', 'rocsat1_ivm', 'superdarn_grdex', 'sw_dst', 'sw_kp', 'timed_see',
+           ]
+from . import *
