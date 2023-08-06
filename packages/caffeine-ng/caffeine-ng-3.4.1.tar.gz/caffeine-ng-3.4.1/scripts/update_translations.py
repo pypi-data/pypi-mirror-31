3e2d97c9df8720fc320fc6d3a70baf69becfd3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:57 +0000

    Add package caffeine-ng/3.4.1/caffeine-ng-3.4.1.tar.gz

---
+++ packages/caffeine-ng/caffeine-ng-3.4.1.tar.gz/caffeine-ng-3.4.1/scripts/update_translations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import subprocess
+
+version = open("VERSION").read()
+version = version.strip()
+print(version)
+
+print(subprocess.getoutput("python ./generate_pot.py . caffeine " + version +
+      " translations/caffeine.pot"))
+
+print(subprocess.getoutput("python ./compile_translations.py caffeine" +
+      "translations"))
