6bf239d1b2694d3311fab5091b22ee760ac21acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:09 +0000

    Add package onesignal-notifications/0.2.0/onesignal-notifications-0.2.0.tar.gz

---
+++ packages/onesignal-notifications/onesignal-notifications-0.2.0.tar.gz/onesignal-notifications-0.2.0/onesignal/segment_notification.py
@@ -0,0 +1,20 @@
+from .notification import Notification
+
+
+class SegmentNotification(Notification):
+    ALL = "All"
+
+    def __init__(self,
+                 included_segments=None,
+                 excluded_segments=None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.included_segments = included_segments
+        self.excluded_segments = excluded_segments
+
+    def get_data(self):
+        return {
+            "included_segments": self.included_segments,
+            "excluded_segments": self.excluded_segments,
+            **self.get_common_data()
+        }
