a553d3e23b32a9ea92e2de4f5fa8247c341cab45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:51 +0000

    Add package funny-hello/0.1/funny_hello-0.1.tar.gz

---
+++ packages/funny-hello/funny_hello-0.1.tar.gz/funny_hello-0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 28 12:17:23 2018
+
+@author: amir.rahmani
+"""
+
+from setuptools import setup
+
+setup(name='funny_hello',
+      version='0.1',
+      description='Hello world test packaging',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='ar3717@columbia.com',
+      packages=['funny_hello'],
+      zip_safe=False)
