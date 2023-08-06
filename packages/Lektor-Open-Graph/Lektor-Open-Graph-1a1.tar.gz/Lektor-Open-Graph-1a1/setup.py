5ab4d09967ca884be8eadb2dc1ec04fc5a856eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:21 +0000

    Add package Lektor-Open-Graph/1a1/Lektor-Open-Graph-1a1.tar.gz

---
+++ packages/Lektor-Open-Graph/Lektor-Open-Graph-1a1.tar.gz/Lektor-Open-Graph-1a1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    project_urls={
+        'Website': 'https://www.palletsprojects.com/p/flask/',
+        'Documentation': 'http://flask.pocoo.org/docs/',
+        'Code': 'https://github.com/pallets/pallets-sphinx-themes',
+        'Issue tracker': 'https://github.com/pallets/pallets-sphinx-themes/issues',
+        'Test status': 'https://travis-ci.org/pallets/flask',
+        'Test coverage': 'https://codecov.io/gh/pallets/flask',
+    },
+)
