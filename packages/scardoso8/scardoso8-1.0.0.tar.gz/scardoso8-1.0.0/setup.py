585f4fc22b449265f3163b56e825893aef05513b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:22 +0000

    Add package scardoso8/1.0.0/scardoso8-1.0.0.tar.gz

---
+++ packages/scardoso8/scardoso8-1.0.0.tar.gz/scardoso8-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'scardoso8',
+    version = '1.0.0',
+    py_modules = ['scardoso8'],
+    author = 'scardoso',
+    author_email = 'scardosoabreu@gmail.com',
+    url = 'https://pypi.org/',
+    description = 'A simple printer of nested lists',
+    )
