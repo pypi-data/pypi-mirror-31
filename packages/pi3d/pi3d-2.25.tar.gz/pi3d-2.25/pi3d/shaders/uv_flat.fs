baecf70d99483125e510111fd9c79035ecd20389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:12 +0000

    Add package pi3d/2.25/pi3d-2.25.tar.gz

---
+++ packages/pi3d/pi3d-2.25.tar.gz/pi3d-2.25/pi3d/shaders/uv_flat.fs
@@ -0,0 +1,11 @@
+#include std_head_fs.inc
+
+varying vec2 texcoordout;
+
+void main(void) {
+#include std_main_uv.inc
+  gl_FragColor = (1.0 - ffact) * texc + ffact * vec4(unif[4], unif[5][1]); // ------ combine using factors
+  gl_FragColor.a *= unif[5][2];
+}
+
+
