e4e8f98f73663a88f2a41ef70acc108cc7ca7dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:28 +0000

    Add package fab-support/0.2.2/fab-support-0.2.2.tar.gz

---
+++ packages/fab-support/fab-support-0.2.2.tar.gz/fab-support-0.2.2/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Humphrey Drummond <hum3@drummonds.net>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/fab-support/fab-support-0.2.2.tar.gz/fab-support-0.2.2/fab_support/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fab_support."""
+from ._version import *
+
+from .env_support import *
+from .stages_support import list_stages
+from .utils import *
+
