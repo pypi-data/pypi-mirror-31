b99d96066b078d205a6dcb4ad7cbdbeeb4d2eac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:29 +0000

    Add package DecoHandler/1.0.1/DecoHandler-1.0.1.tar.gz

---
+++ packages/DecoHandler/DecoHandler-1.0.1.tar.gz/DecoHandler-1.0.1/decohandler.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+# noinspection PyPep8Naming
+class handles:
+    def __init__(self, opcode):
+        self.opcode = opcode
+        self.__func__ = None
+
+    def __get__(self, instance=None, owner=None):
+        return self.__func__.__get__(instance, owner)
+
+    def __call__(self, func):
+        self.__func__ = func
+        return self
+
+    def __repr__(self):
+        return '<{} handles opcode {}>'.format(self.__func__, self.opcode)
+
+
+class BaseHandler:
+    def __init__(self):
+        handlers = defaultdict(list)
+
+        for k, v in vars(type(self)).items():
+            if isinstance(v, handles):
+                handlers[v.opcode] += [getattr(self, k)]
+
+        self.handlers = dict(handlers)
+
+    def handle(self, opcode):
+        try:
+            return [handler() for handler in self.handlers[opcode]]
+        except KeyError:
+            raise NotImplementedError(
+                'There is no handler implemented for {}'.format(opcode))
