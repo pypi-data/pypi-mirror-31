14d42fdfcac2e70b8428768bd64ceb2d3282e435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:32 +0000

    Add package coremeter/1.0.1/coremeter-1.0.1.tar.gz

---
+++ packages/coremeter/coremeter-1.0.1.tar.gz/coremeter-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+        name = 'coremeter',
+        packages = ['coremeter'],
+        version = '1.0.1',
+        description = 'A resource monitor for Unix',
+        author = 'Stephen Johnson',
+        author_email = 'stephen@sjoh.net',
+        license='MIT',
+        url = 'https://github.com/S-M-Johnson/coremeter',
+        download_url = 'https://github.com/S-M-Johnson/coremeter/archive/1.0.0.tar.gz',
+        keywords = 'cpu resource monitor unix',
+        classifiers = [],
+    )
