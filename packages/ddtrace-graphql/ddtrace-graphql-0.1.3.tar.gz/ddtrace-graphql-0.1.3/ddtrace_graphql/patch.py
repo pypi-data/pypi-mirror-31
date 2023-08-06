50e90cc27881ec20182c80de4319b1e17c82d0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:43 +0000

    Add package ddtrace-graphql/0.1.3/ddtrace-graphql-0.1.3.tar.gz

---
+++ packages/ddtrace-graphql/ddtrace-graphql-0.1.3.tar.gz/ddtrace-graphql-0.1.3/ddtrace_graphql/patch.py
@@ -0,0 +1,38 @@
+"""
+Tracing for the graphql-core library.
+
+https://github.com/graphql-python/graphql-core
+"""
+
+import logging
+import os
+
+import graphql
+import wrapt
+from ddtrace.util import unwrap
+
+from ddtrace_graphql.base import traced_graphql_wrapped
+
+logger = logging.getLogger(__name__)
+
+
+def patch(span_kwargs=None, span_callback=None, ignore_exceptions=()):
+    """
+    Monkeypatches graphql-core library to trace graphql calls execution.
+    """
+    logger.debug('Patching `graphql.graphql` function.')
+
+    def wrapper(func, _, args, kwargs):
+        return traced_graphql_wrapped(
+            func, args, kwargs,
+            span_kwargs=span_kwargs,
+            span_callback=span_callback,
+            ignore_exceptions=ignore_exceptions
+        )
+
+    wrapt.wrap_function_wrapper(graphql, 'graphql', wrapper)
+
+
+def unpatch():
+    logger.debug('Unpatching `graphql.graphql` function.')
+    unwrap(graphql, 'graphql')
