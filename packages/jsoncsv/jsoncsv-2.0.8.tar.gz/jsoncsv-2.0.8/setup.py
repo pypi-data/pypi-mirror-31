ce2d02b60e7ac16bd361c181dc8080ad060eb1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:50 +0000

    Add package jsoncsv/2.0.8/jsoncsv-2.0.8.tar.gz

---
+++ packages/jsoncsv/jsoncsv-2.0.8.tar.gz/jsoncsv-2.0.8/setup.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+# author@alingse
+# 2016.10.07
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read().decode('utf-8')
+
+setup(
+    name='jsoncsv',
+    version='2.0.8',
+    description='a command tool easily convert json file to csv or xlsx',
+    long_description=readme,
+    author='alingse',
+    author_email='alingse@foxmail.com',
+    url='https://github.com/alingse/jsoncsv',
+    license='Apache 2.0',
+    packages=find_packages(exclude=('tests')),
+    install_requires=[
+        'xlwt',
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jsoncsv = jsoncsv.main:jsoncsv',
+            'mkexcel = jsoncsv.main:mkexcel',
+        ],
+    }
+)
