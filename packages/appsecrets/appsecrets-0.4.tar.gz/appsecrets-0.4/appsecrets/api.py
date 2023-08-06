8440178a8e7e339ae3167c26d4af911ba1b7b6a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:04 +0000

    Add package appsecrets/0.4/appsecrets-0.4.tar.gz

---
+++ packages/appsecrets/appsecrets-0.4.tar.gz/appsecrets-0.4/appsecrets/api.py
@@ -0,0 +1,40 @@
+import pathlib
+
+from . import stores
+from .exc import Error
+
+
+class Secrets(object):
+    def __init__(self, path):
+        self._path = path
+        self._store = stores.build(path)
+
+    @classmethod
+    def create(cls, path, key_id):
+        path = pathlib.Path(path)
+        if path.exists():
+            raise Error(f'A secret store already exists at {path}')
+
+        path.mkdir(parents=True)
+
+        key_id_path = path.joinpath('_google_kms_key_id')
+        key_id_path.write_text(key_id)  # pylint: disable=no-member
+
+    def encrypt_all(self):
+        """Encrypt all cleartext secrets in place."""
+        self._store.encrypt_inplace()
+
+    def decrypt(self, name):
+        """Decrypt and return a secrets."""
+        return self._store.decrypt(name)
+
+    def list_encrypted(self):
+        """List all encrypted secrets."""
+        return self._store.list_encrypted()
+
+    def list_unencrypted(self):
+        """List all unencrypted secrets."""
+        return self._store.list_unencrypted()
+
+    def __repr__(self):
+        return f'Appsecret({type(self._store)}) at {self._path}'
---
+++ packages/appsecrets/appsecrets-0.4.tar.gz/appsecrets-0.4/appsecrets/crypto/google_kms.py
@@ -0,0 +1,62 @@
+from functools import wraps
+import base64
+
+import googleapiclient.discovery
+import googleapiclient.errors
+
+import httplib2
+
+import oauth2client.client
+
+from appsecrets.exc import Error, SecretError, CryptoError
+
+
+def handle_gcloud_error(func):
+    @wraps(func)
+    def wrapper(*args, **kwds):
+        try:
+            return func(*args, **kwds)
+        except googleapiclient.errors.HttpError as err:
+            raise SecretError(str(err))
+        except oauth2client.client.ApplicationDefaultCredentialsError as err:
+            raise Error(str(err))
+        except httplib2.ServerNotFoundError as err:
+            raise CryptoError(str(err))
+    return wrapper
+
+
+class GoogleKMS(object):
+    def __init__(self, key_id):
+        self._key_id = key_id
+        self._resource_obj = None
+
+    @handle_gcloud_error
+    def encrypt(self, plaintext):
+        body = {'plaintext': base64.b64encode(plaintext).decode('ascii')}
+
+        response = self._api_resource.encrypt(name=self._key_id, body=body).execute()
+        payload = response.get('ciphertext')
+
+        if payload is None:
+            raise Error('Failed to encrypt secret with Google KMS')
+
+        return base64.b64decode(payload)
+
+    @handle_gcloud_error
+    def decrypt(self, ciphertext):
+        body = {'ciphertext': base64.b64encode(ciphertext).decode('ascii')}
+
+        response = self._api_resource.decrypt(name=self._key_id, body=body).execute()
+        payload = response.get('plaintext')
+
+        if payload is None:
+            raise Error('Failed to decrypt secret with Google KMS')
+
+        return base64.b64decode(payload)
+
+    @property
+    def _api_resource(self):
+        if self._resource_obj is None:
+            client = googleapiclient.discovery.build('cloudkms', 'v1', cache_discovery=False)
+            self._resource_obj = client.projects().locations().keyRings().cryptoKeys()
+        return self._resource_obj
