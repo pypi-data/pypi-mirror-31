b9995452103e2791b88854dbd021013343254f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:19 +0000

    Add package awspycli/1.0.2/awspycli-1.0.2.tar.gz

---
+++ packages/awspycli/awspycli-1.0.2.tar.gz/awspycli-1.0.2/awspycli/__init__.py
@@ -0,0 +1,4 @@
+from awspycli.model.emr_cli import emr
+from awspycli.model.s3_cli import s3
+
+VERSION = '1.0.2'
---
+++ packages/awspycli/awspycli-1.0.2.tar.gz/awspycli-1.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+from awspycli import VERSION
+
+setup(
+    name='awspycli',
+    description='using python code to communicate with aws client',
+    long_description='',
+    classifiers=[],
+    keywords='',
+    author='Lawes',
+    author_email='haiou_chen@sina.cn',
+    url='https://github.com/MrLawes/awspycli',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'awscli==1.14.63',
+    ],
+    version=VERSION,
+)
