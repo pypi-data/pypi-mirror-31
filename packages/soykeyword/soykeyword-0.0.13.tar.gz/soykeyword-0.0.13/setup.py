318e44981cb841da730dd28238643c10911c267d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:42 +0000

    Add package soykeyword/0.0.13/soykeyword-0.0.13.tar.gz

---
+++ packages/soykeyword/soykeyword-0.0.13.tar.gz/soykeyword-0.0.13/setup.py
@@ -0,0 +1,16 @@
+from description import __version__, __author__
+from setuptools import setup, find_packages
+
+setup(
+   name="soykeyword",
+   version=__version__,
+   author=__author__,
+   author_email='soy.lovit@gmail.com',
+   url='https://github.com/lovit/soynlp',
+   description="Unsupervised Korean Natural Language Processing Toolkits",
+   long_description="""It contains two keyword extraction algorithms. First one uses Lasso logistic regression and the other uses relative proportion ratio
+   """,
+   install_requires=["numpy>=1.12.0", "scikit-learn>=0.18.0", "psutil>=5.0.1"],
+   keywords = ['keyword extractor'],
+   packages=find_packages()
+)
