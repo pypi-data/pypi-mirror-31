c9668fc74119e215ed731f4ac50c7f5155f6cb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:01 +0000

    Add package abjad-ext-cli/3.0.0a1/abjad-ext-cli-3.0.0a1.tar.gz

---
+++ packages/abjad-ext-cli/abjad-ext-cli-3.0.0a1.tar.gz/abjad-ext-cli-3.0.0a1/abjadext/cli/__init__.py
@@ -0,0 +1,6 @@
+from .ManageBuildTargetScript import ManageBuildTargetScript  # noqa
+from .ManageMaterialScript import ManageMaterialScript  # noqa
+from .ManageScoreScript import ManageScoreScript  # noqa
+from .ManageSegmentScript import ManageSegmentScript  # noqa
+from .ScorePackageScript import ScorePackageScript  # noqa
+from ._version import __version__, __version_info__  # noqa
