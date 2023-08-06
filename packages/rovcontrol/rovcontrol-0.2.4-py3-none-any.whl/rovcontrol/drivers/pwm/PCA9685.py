579176b8a2b057f04c2ce74c29bf86235aa28a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:57 +0000

    Add package rovcontrol/0.2.4/rovcontrol-0.2.4-py3-none-any.whl

---
+++ packages/rovcontrol/rovcontrol-0.2.4-py3-none-any.whl/rovcontrol/drivers/pwm/PCA9685.py
@@ -0,0 +1,19 @@
+import rovcontrol.drivers.pwm.PWMTemp as PWMTemp
+import Adafruit_PCA9685
+
+#TODO: implement polarity
+
+class PCA9685(PWMTemp.PWMTemp):
+    def __init__(self, i2caddress = 0x40, i2cbus = 1):
+        super().__init__(x)
+        self.adapwm = Adafruit_PCA9685.PCA9685(address = i2caddress, busnum = i2cbus)
+
+    def update_one_bus(self, bus, data):
+        if data['dirty']:
+            self.adapwm.set_pwm_freq(data['freq'])
+        super()
+
+    def update_one_channel(self, bus, channel, data):
+        if data['dirty']:
+            self.adapwm.set_pwm(channel, 0, data['duty'] * 4096)
+        super()
