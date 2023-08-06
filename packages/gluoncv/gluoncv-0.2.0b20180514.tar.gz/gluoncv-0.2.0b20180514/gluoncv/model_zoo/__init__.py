ceac375e317d6d443aa66bdf501ed5a3033204b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:29 +0000

    Add package gluoncv/0.2.0b20180514/gluoncv-0.2.0b20180514.tar.gz

---
+++ packages/gluoncv/gluoncv-0.2.0b20180514.tar.gz/gluoncv-0.2.0b20180514/gluoncv/model_zoo/__init__.py
@@ -0,0 +1,10 @@
+"""Gluon Vision Model Zoo"""
+# pylint: disable=wildcard-import
+from .model_zoo import get_model
+from .ssd import *
+from .cifarresnet import *
+from .cifarwideresnet import *
+from .fcn import *
+from .pspnet import *
+from . import segbase
+from .resnetv1b import *
