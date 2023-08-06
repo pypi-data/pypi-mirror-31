e812239918f12a023f6f095cf117fc86a4284d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:41 +0000

    Add package dongo/0.4.0/dongo-0.4.0.tar.gz

---
+++ packages/dongo/dongo-0.4.0.tar.gz/dongo-0.4.0/dongo/__init__.py
@@ -0,0 +1,27 @@
+'''
+dongo
+=====
+
+A Django-ORM inspired Mongo ODM.
+'''
+from .odm import (
+    connect, DongoCollection, DongoClient, to_uuid, deref, deref_single,
+    deref_many,
+)
+from .queryset import QuerySet
+from .bulk import DongoBulk, DongoLazyUpdater
+from .exceptions import (
+    DongoError, DongoConnectError, DongoResultError, DongoCollectionError,
+    DongoDerefError,
+)
+
+
+__title__ = 'dongo'
+__version__ = '0.4.0'
+__all__ = ('connect', 'QuerySet', 'DongoCollection', 'DongoError',
+           'DongoConnectError', 'DongoResultError', 'DongoCollectionError',
+           'DongoClient', 'to_uuid', 'DongoDerefError', 'deref', 'deref_single',
+           'deref_many', 'DongoBulk', 'DongoLazyUpdater')
+__author__ = 'Johan Nestaas <johannestaas@gmail.com>'
+__license__ = 'LGPLv3+'
+__copyright__ = 'Copyright 2018 Johan Nestaas'
