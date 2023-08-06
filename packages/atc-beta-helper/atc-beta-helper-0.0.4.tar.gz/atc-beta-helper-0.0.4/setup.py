a284cf0a2d1c83a75baa95bbedc4adf9eb140ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:06 +0000

    Add package atc-beta-helper/0.0.4/atc-beta-helper-0.0.4.tar.gz

---
+++ packages/atc-beta-helper/atc-beta-helper-0.0.4.tar.gz/atc-beta-helper-0.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='atc-beta-helper',
+      version='0.0.4',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      install_requires=[
+          'requests==2.18.4',
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+      ])
