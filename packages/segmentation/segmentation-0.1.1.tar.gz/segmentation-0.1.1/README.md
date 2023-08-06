ddaea29ae5b6ad04d7a0489cf6ece4f8e1ac3043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:41 +0000

    Add package segmentation/0.1.1/segmentation-0.1.1.tar.gz

---
+++ packages/segmentation/segmentation-0.1.1.tar.gz/segmentation-0.1.1/README.md
@@ -0,0 +1,28 @@
+# Fast Mixture Model Segmentation
+
+Fast mixture model segmentation used in Boone and Andrews labs
+
+# Python requirements
+
+  - Cython
+  - scikit-image
+
+# C requirements
+  - Boost
+
+## Installation
+
+Create a virtual environment (optional)
+```sh
+$ virtualenv -ppython3 segmentation-env
+$ source segmentation-env/bin/activate
+```
+
+#### Installing from source
+Install python requirements
+
+```sh
+$ git clone https://github.com/usajusaj/segmentation.git
+$ cd segmentation
+$ pip install -r requirements.txt
+```
---
+++ packages/segmentation/segmentation-0.1.1.tar.gz/segmentation-0.1.1/setup.py
@@ -0,0 +1,39 @@
+import sysconfig
+
+import numpy as np
+from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+
+our_flags = ["-march=native", "-std=c++11", "-DNDEBUG", "-Ofast", "-Wcpp"]
+extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
+
+for f in extra_compile_args[:]:
+    if f.startswith('-march=') or f.startswith('-std=') or f.startswith('-O'):
+        extra_compile_args.remove(f)
+
+extra_compile_args += our_flags
+
+ext_modules = [
+    Extension("segmentation.segmentation", ["segmentation/segmentation.pyx", ],
+              language="c++",
+              include_dirs=[np.get_include()],
+              extra_compile_args=extra_compile_args,
+              extra_link_args=["-std=c++11"],
+              ),
+]
+
+setup(
+    name="segmentation",
+    packages=["segmentation"],
+    version='0.1.1',
+    description='Mixture model segmentation',
+    author='Matej Usaj',
+    author_email='m.usaj@utoronto.ca',
+    url='https://github.com/usajusaj/segmentation',
+    download_url='https://github.com/usajusaj/segmentation/archive/master.zip',
+    keywords=['mixture', 'model', 'segmentation'],
+    classifiers=[],
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules,
+    install_requires=['cython', 'scikit-image', 'numpy'],
+)
