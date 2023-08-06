901c3e349b4f5af1bbf759ceca49df7272b18fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package ETemplator/0.1/ETemplator-0.1.tar.gz

---
+++ packages/ETemplator/ETemplator-0.1.tar.gz/ETemplator-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ETemplator',
+    version='0.1',
+    packages=['templator'],
+    url='http://amitbhat.in',
+    license='MIT',
+    author='amit',
+    author_email='amit.bhat@outlook.com',
+    description='App for creating/populating email templates'
+)
