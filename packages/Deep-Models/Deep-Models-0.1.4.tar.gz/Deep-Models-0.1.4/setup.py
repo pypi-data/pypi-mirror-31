79419d7e7a2538ef2e3480af23b929284d24e3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package Deep-Models/0.1.4/Deep-Models-0.1.4.tar.gz

---
+++ packages/Deep-Models/Deep-Models-0.1.4.tar.gz/Deep-Models-0.1.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+version = '0.1.4'
+
+setup(name='Deep-Models',
+      version=version,
+      description='Keras deep learning model implementations',
+      long_description=long_description,
+      url='http://github.com/tayden/DeepModels',
+      author='Taylor Denouden',
+      author_email='taylordenouden@gmail.com',
+      license='MIT',
+      packages=['deep_models'],
+      install_requires=['keras']
+)
