2c7fa3e5dd3dc1f70a9540bbc69769a8cab7bd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package db_dumper/0.1.2/db_dumper-0.1.2.tar.gz

---
+++ packages/db_dumper/db_dumper-0.1.2.tar.gz/db_dumper-0.1.2/db_dumper/__init__.py
@@ -0,0 +1,38 @@
+"""Load and dump databases to disk."""
+
+
+def load(source, classes, save=None):
+    """Load 0 or more objects from source and instantiate them with the classes
+    found in the classes iterable. They will be returned as a list which can be
+    passed to session.add_all for example. No saving will be performed unless
+    save is not None, in which case save(object) will be called for every
+    created object. Pass a dictionary as source with name: list-of-dictionaries
+    as pairs."""
+    objects = []  # All the created objects.
+    for cls in classes:
+        for data in source[cls.__name__]:
+            # Let's load.
+            obj = cls(**data)
+            if save is not None:
+                save(obj)
+            objects.append(obj)
+    return objects
+
+
+def dump(objects, f, convert_name=None):
+    """Return a dictionary suitable for loading with the load function. Objects
+    will be dumped with the f function. If convert_name is given it must be a
+    callable which takes a class name as the only argument, and returns a
+    suitable alternative name. This is primarily useful for quick convertions
+    in the context of database migrations and the like."""
+    d = {}
+    for obj in objects:
+        name = obj.__class__.__name__
+        if convert_name is not None:
+            name = convert_name(name)
+        d[name] = d.get(name, [])
+        d[name].append(f(obj))
+    return d
+
+
+__all__ = ['load', 'dump']
---
+++ packages/db_dumper/db_dumper-0.1.2.tar.gz/db_dumper-0.1.2/tests/main_test.py
@@ -0,0 +1,56 @@
+from pytest import raises
+from db_dumper import load, dump
+
+
+class Save(Exception):
+    pass
+
+
+def save(obj):
+    raise Save()
+
+
+def dump_object(obj):
+    return {x: getattr(obj, x) for x in dir(obj) if not x.startswith('_')}
+
+
+class DummyObject:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __eq__(self, instance):
+        return instance.name == self.name and instance.age == self.age
+
+
+def test_dump():
+    o = DummyObject('Chris Norman', 28)
+    data = dump([o], dump_object)
+    assert len(data) is 1
+    assert list(data.keys()) == [DummyObject.__name__]
+    assert list(
+        data.values()
+    )[0][0] == {
+        'name': o.name,
+        'age': o.age
+    }
+
+
+def test_load():
+    me = DummyObject('Chris Norman', 28)
+    dog = DummyObject('Fliss', 10)
+    d = dump([me, dog], dump_object)
+    objects = load(d, [DummyObject])
+    assert len(objects) is 2
+    loaded_me, loaded_dog = objects
+    assert loaded_me == me
+    assert loaded_dog == dog
+
+
+def test_save():
+    o = DummyObject('Chris', 28)
+    objects = [o]
+    data = dump(objects, dump_object)
+    with raises(Save):
+        load(data, [DummyObject], save=save)
