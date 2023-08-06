5fb2ffb24af11881dae6e5ce30f2a8b58cdad96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:29 +0000

    Add package zipstreamer/0.1.3/zipstreamer-0.1.3.tar.gz

---
+++ packages/zipstreamer/zipstreamer-0.1.3.tar.gz/zipstreamer-0.1.3/zipstreamer/__version__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-docstring
+
+__title__ = 'zipstreamer'
+__description__ = (
+    'Python library for generating ZIP files on-the-fly with ZIP file'
+    ' size information.'
+)
+__url__ = 'http://github.com/koofr/python-zipstreamer'
+__version__ = '0.1.3'
+__build__ = 0x000103
+__author__ = 'Luka Zakrajsek'
+__author_email__ = 'luka@bancek.net'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Koofr'
