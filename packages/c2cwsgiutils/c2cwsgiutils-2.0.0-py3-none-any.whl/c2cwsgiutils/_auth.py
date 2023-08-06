2a21b27a5874015c9263c9393ee44f654444c285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:37 +0000

    Add package c2cwsgiutils/2.0.0/c2cwsgiutils-2.0.0-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-2.0.0-py3-none-any.whl/c2cwsgiutils/_auth.py
@@ -0,0 +1,13 @@
+from pyramid.httpexceptions import HTTPForbidden
+import pyramid.request
+
+# noinspection PyProtectedMember
+from c2cwsgiutils._utils import env_or_settings
+
+
+def auth_view(request: pyramid.request.Request, env_name: str, config_name: str) -> None:
+    secret = request.params.get('secret')
+    if secret is None:
+        secret = request.headers.get('X-API-Key')
+    if secret != env_or_settings(request.registry.settings, env_name, config_name, False):
+        raise HTTPForbidden('Missing or invalid secret (parameter or X-API-Key header)')
