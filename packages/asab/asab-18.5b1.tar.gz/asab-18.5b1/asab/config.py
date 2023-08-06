742ce0a2af2017d50e5a250aa74c8dacb67705a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:26 +0000

    Add package asab/18.5b1/asab-18.5b1.tar.gz

---
+++ packages/asab/asab-18.5b1.tar.gz/asab-18.5b1/asab/config.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import glob
+import logging
+import configparser
+
+#
+
+L = logging.getLogger(__name__)
+
+#
+
+class ConfigParser(configparser.ConfigParser):
+
+	_default_values = {
+
+		'general': {
+			'verbose': os.environ.get('ASAB_VERBOSE', False),
+			'config_file': os.environ.get('ASAB_CONFIG', ''),
+			'tick_period': 1, # In seconds
+			'var_dir': os.path.expanduser('~/.'+os.path.splitext(os.path.basename(sys.argv[0]))[0]),
+		
+			# Daemonization
+			'pidfile': '!', # '!' has a special meaning => it transforms into platform specific location of pid file
+			'uid': '',
+			'gid': '',
+		},
+
+		"logging:rfc5424": {
+			"sd_id": "sd",
+		},
+
+		"logging:console": {
+			"format": "%%(asctime)s %%(levelname)s %%(name)s %%(struct_data)s: %%(message)s",
+			"datefmt": "%%d-%%b-%%Y %%H:%%M:%%S.%%f",
+		},
+
+	}
+
+
+	def add_defaults(self, dictionary):
+		""" Add defaults to a current configuration """
+
+		for section, keys in dictionary.items():
+			section = str(section)
+
+			if section not in self._sections:
+				try:
+					self.add_section(section)
+				except ValueError:
+					if self._strict:
+						raise
+
+			for key, value in keys.items():
+
+				key = self.optionxform(str(key))
+				if key in self._sections[section]:
+					# Value exists, no default needed
+					continue 
+
+				if value is not None:
+					value = str(value)
+
+				if value is not None and "$" in value:
+					self.set(section, key, os.path.expandvars(value))				
+				else:
+					self.set(section, key, value)
+
+
+	def _load(self):
+		""" This method should be called only once, any subsequent call will lead to undefined behaviour """
+
+		config_fname = ConfigParser._default_values['general']['config_file']
+		if config_fname != '':
+			if not os.path.isfile(config_fname):
+				print("Config file '{}' not found".format(config_fname), file=sys.stderr)
+				sys.exit(1)
+
+			self.read(config_fname)
+
+		includes = self.get('general', 'include', fallback='')
+		for include_glob in includes.split(os.pathsep):
+			for include in glob.glob(include_glob):
+				self.read(include)
+
+		self.add_defaults(ConfigParser._default_values)
+
+		# Deals with environment variables
+		for each_section in self.sections():
+			for (each_key, each_val) in self.items(each_section):
+				if "$" in each_val:
+					self.set(each_section, each_key, os.path.expandvars(each_val))
+
+###
+
+Config = ConfigParser()
---
+++ packages/asab/asab-18.5b1.tar.gz/asab-18.5b1/asab/log.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import logging
+import logging.handlers
+import traceback
+import time
+import datetime
+import pprint
+import socket
+from .config import Config
+
+
+def _setup_logging():
+
+	root_logger = logging.getLogger()
+	if not root_logger.hasHandlers():
+		
+		# Add console logger
+		#TODO: Don't initialize this when not on console
+		h = logging.StreamHandler(stream=sys.stderr)
+		h.setFormatter(StructuredDataFormatter(
+			fmt = Config["logging:console"]["format"],
+			datefmt = Config["logging:console"]["datefmt"],
+			sd_id = Config["logging:rfc5424"]["sd_id"],
+		))
+		h.setLevel(logging.DEBUG)
+		root_logger.addHandler(h)
+
+		#TODO: If configured, initialize syslog
+
+	else:
+		root_logger.warning("Logging seems to be already configured. Proceed with caution.")
+
+	if Config["general"]["verbose"] == "True":
+		root_logger.setLevel(logging.DEBUG)
+	else:
+		root_logger.setLevel(logging.WARNING)
+
+
+class StructuredDataLogger(logging.Logger):
+
+	def _log(self, level, msg, args, exc_info=None, struct_data=None, extra=None, stack_info=False):
+		if struct_data is not None:
+			if extra is None: extra = dict()
+			extra['_struct_data'] = struct_data
+
+		super()._log(level, msg, args, exc_info=exc_info, extra=extra, stack_info=stack_info)
+
+logging.setLoggerClass(StructuredDataLogger)
+
+
+class StructuredDataFormatter(logging.Formatter):
+
+	def __init__(self, fmt=None, datefmt=None, style='%', sd_id=None):
+		super().__init__(fmt, datefmt, style)
+		self.sd_id = sd_id
+
+
+	def format(self, record):
+		record.struct_data=self.render_struct_data(record.__dict__.get("_struct_data"))
+		return super().format(record)
+
+
+	def formatTime(self, record, datefmt=None):
+		try:
+			ct = datetime.datetime.fromtimestamp(record.created)
+			if datefmt is not None:
+				s = ct.strftime(datefmt)
+			else:
+				t = ct.strftime("%Y-%m-%d %H:%M:%S")
+				s = "%s.%03d" % (t, record.msecs)
+			return s
+		except BaseException as e:
+			print("ERROR when logging: {}".format(e), file=sys.stderr)
+			return str(ct)
+
+	def render_struct_data(self, struct_data):
+		if struct_data is None:
+			return ""
+		else:
+			return "[{sd_id} {sd_params}]".format(
+				sd_id=self.sd_id,
+				sd_params=" ".join(['{}="{}"'.format(key, val) for key, val in struct_data.items()]))
+
+
+
+class RFC5424Formatter(StructuredDataFormatter):
+	""" This formatter is meant for a SysLogHandler """
+
+	def __init__(self, fmt=None, datefmt=None, style='%'):
+		super().__init__(fmt, datefmt, style)
+		app_name=Config["logging:formatter_rfc5424"]["app_name"]
+
+		# RFC5424 format
+		fmt = '{header} {structured_data} {message}'.format(
+			header='{version} {timestamp} {hostname} {app_name} {proc_id} {msg_id}'.format(
+				version="1",
+				timestamp='%(asctime)s.%(msecs)dZ',
+				hostname=socket.gethostname(),
+				app_name=app_name,
+				proc_id=os.getpid(),
+				msg_id='-'),
+			structured_data='%(struct_data)s',
+			message='%(message)s'
+		)
+
+		# Initialize formatter
+		super().__init__(
+			fmt=fmt,
+			datefmt='%Y-%m-%dT%H:%M:%S',
+			style='%')
+
+		# Convert time to GMT
+		self.converter = time.gmtime
+
+
+
+def _loop_exception_handler(loop, context):
+	exception = context.pop('exception', None)
+	
+	message = context.pop('message', '')
+	if len(message) > 0:
+		message += '\n'
+
+	if len(context) > 0:
+		message += pprint.pformat(context)
+
+	if exception is not None:
+		ex_traceback = exception.__traceback__
+		tb_lines = [ line.rstrip('\n') for line in traceback.format_exception(exception.__class__, exception, ex_traceback)]
+		message += '\n' + '\n'.join(tb_lines)
+
+	logging.getLogger().error(message)
+
+
+
+if __name__ == '__main__':
+	handler = logging.handlers.SysLogHandler()
+	handler.setFormatter(RFC5424Formatter())
+	handler.setLevel(logging.INFO)
+	L = logging.getLogger(__name__)
+	L.setLevel(logging.INFO)
+	L.addHandler(handler)
+
+	L.info("Info message with structured data", struct_data={"sd1": "test"})
+	L.warn("Warn message, with structured data", struct_data={"sd1": "test", "sd2": "test2"})
+	L.error("Error message without structured data")
