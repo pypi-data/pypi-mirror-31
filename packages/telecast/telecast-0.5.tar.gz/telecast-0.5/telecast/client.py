ecf8fd784e798236b4de7c73949c0d160eddd792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package telecast/0.5/telecast-0.5.tar.gz

---
+++ packages/telecast/telecast-0.5.tar.gz/telecast-0.5/telecast/client.py
@@ -0,0 +1,47 @@
+from json import dumps, loads
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
+from urllib.parse import urlencode
+
+from telecast import exceptions, codes
+
+
+def api_call(url, **kwargs):
+    # This is a proxy method to make mocking easier.
+    # The reason for this is that api_call can be imported anywhere
+    # while _api_call will always be used only from within this module.
+    return _api_call(url, **kwargs)
+
+
+def _api_call(url, **kwargs):
+    request = Request(url, dumps(kwargs).encode(), headers={
+        'Content-Type': 'application/json'
+    })
+    try:
+        response = urlopen(request)
+        data = response.read()
+        status = response.status
+    except HTTPError as error:
+        data = error.read()
+        status = error.code
+    except URLError as error:
+        raise exceptions.RPCProtocolError(str(error))
+
+    if status != 200:
+        raise exceptions.RPCProtocolError('HTTP ' + str(status) + ': ' + str(data))
+
+    try:
+        data = loads(data)
+        result = data['result']
+        code = data['code']
+    except Exception as error:
+        raise exceptions.RPCProtocolError(str(error))
+    else:
+        if code == codes.RPC_CODE_PROTOCOL_ERROR:
+            # Not used at the moment.
+            raise exceptions.RPCProtocolError(str(result))
+        if code == codes.RPC_CODE_NOT_ALLOWED:
+            raise exceptions.RPCNotAllowedError(str(result))
+        if code == codes.RPC_CODE_REMOTE_ERROR:
+            raise exceptions.RPCRemoteError(str(result))
+        return result
---
+++ packages/telecast/telecast-0.5.tar.gz/telecast-0.5/telecast/contrib/rest_framework/server.py
@@ -0,0 +1,86 @@
+from functools import wraps
+import inspect
+
+import coreapi
+import coreschema
+from django.conf import settings
+from rest_framework.decorators import api_view, schema
+from rest_framework.response import Response
+from rest_framework.schemas import AutoSchema
+
+from telecast import codes
+
+IGNORED_FIELDS = ['request']
+
+
+def get_is_port_allowed(port):
+    """
+    Return TELECAST_PORTS.
+    """
+    if not hasattr(settings, 'TELECAST_PORTS'):
+        return True
+    ports = settings.TELECAST_PORTS
+    if not isinstance(ports, (list, tuple)):
+        ports = [ports]
+    return str(port) in map(str, ports)
+
+
+def method(**decorator_kwargs):
+    """
+    Return decorator that mimics api_view.
+    """
+    def decorator(func):
+        """
+        Decorate a method with api_view and add proper schema that matches
+        function arguments and return a proxy that transforms this function
+        into a callable REST endpoint.
+        """
+        if 'http_method_names' not in decorator_kwargs:
+            decorator_kwargs['http_method_names'] = ['POST']
+
+        @api_view(**decorator_kwargs)
+        @schema(AutoSchema(manual_fields=[
+            coreapi.Field(
+                field,
+                required=False,
+                location='form',
+                schema=coreschema.Anything()
+            )
+            for field
+            in inspect.signature(func).parameters
+            if field not in IGNORED_FIELDS
+        ]))
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            """
+            Proxy that wraps a function into a REST endpoint.
+            """
+            assert not args
+            assert not kwargs
+            port = request.META.get('HTTP_X_FORWARDED_PORT')
+            if port and not get_is_port_allowed(port):
+                return Response(dict(
+                    code=codes.RPC_CODE_NOT_ALLOWED,
+                    result='You are not allowed to call this TELECAST method.'
+                ))
+
+            if hasattr(request, 'data'):
+                arguments = request.data
+            else:
+                # e. g. GET request
+                arguments = {}
+
+            try:
+                result = func(request, **arguments)
+            except Exception as error:
+                return Response(dict(
+                    code=codes.RPC_CODE_REMOTE_ERROR,
+                    result=str(error)
+                ))
+
+            return Response(dict(
+                code=codes.RPC_CODE_OK,
+                result=result
+            ))
+        return wrapper
+    return decorator
