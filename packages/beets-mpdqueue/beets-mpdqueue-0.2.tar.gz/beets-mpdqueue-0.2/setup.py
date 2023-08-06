c09e1075d6b6b2a9635e8c1cba65c2459350b067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:50 +0000

    Add package beets-mpdqueue/0.2/beets-mpdqueue-0.2.tar.gz

---
+++ packages/beets-mpdqueue/beets-mpdqueue-0.2.tar.gz/beets-mpdqueue-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="beets-mpdqueue",
+    version="0.2",
+    description='Beets plugin to add imported files to MPD queue.',
+    url='https://github.com/Hamuko/beets-mpdqueue',
+    author='Hamuko',
+    author_email='hamuko@burakku.com',
+    license='Apache2',
+    packages=find_packages(),
+)
