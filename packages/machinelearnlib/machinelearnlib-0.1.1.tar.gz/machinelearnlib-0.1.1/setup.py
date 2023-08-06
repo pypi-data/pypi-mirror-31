811916866509072d1ee61f670c9627b0cc16f619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:12 +0000

    Add package machinelearnlib/0.1.1/machinelearnlib-0.1.1.tar.gz

---
+++ packages/machinelearnlib/machinelearnlib-0.1.1.tar.gz/machinelearnlib-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='machinelearnlib',
+      version='0.1.1',
+      description='Implementation of common machine learning algorithms',
+      long_description=long_description,
+      url='https://github.com/Kai-Bailey/machinelearnlib',
+      author='Kai Bailey',
+      author_email='kbailey1@ualberta.ca',
+      license='MIT',
+      keywords='machine learning data science',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=['numpy', 'matplotlib'],
+      entry_points={'console_scripts':['ml=machinelearnlib.testing:testing']})
