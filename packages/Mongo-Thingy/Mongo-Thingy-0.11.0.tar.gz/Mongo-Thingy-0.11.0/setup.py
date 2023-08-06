be2e92a1d4068f7ad0ad9548d9b29d04d67dda72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:59 +0000

    Add package Mongo-Thingy/0.11.0/Mongo-Thingy-0.11.0.tar.gz

---
+++ packages/Mongo-Thingy/Mongo-Thingy-0.11.0.tar.gz/Mongo-Thingy-0.11.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def get_description():
+    with open("README.rst") as file:
+        return file.read()
+
+
+setup(
+    name="Mongo-Thingy",
+    version="0.11.0",
+    url="https://github.com/numberly/mongo-thingy",
+    license="MIT",
+    author="Guillaume Gelin",
+    author_email="ramnes@1000mercis.com",
+    description=("The most Pythonic and friendly-yet-powerful way to use "
+                 "MongoDB"),
+    long_description=get_description(),
+    packages=["mongo_thingy"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    install_requires=[
+        "thingy>=0.8.3",
+        "pymongo>=3.0"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
