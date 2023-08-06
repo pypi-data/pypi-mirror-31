73b57650dce2423a5ea18c86225847f28e76fc8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:04 +0000

    Add package appsecrets/0.4/appsecrets-0.4-py3-none-any.whl

---
+++ packages/appsecrets/appsecrets-0.4-py3-none-any.whl/appsecrets-0.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+
+See `github repo <https://github.com/pior/appsecrets>`_ for information.
+
+
---
+++ packages/appsecrets/appsecrets-0.4-py3-none-any.whl/appsecrets/exc.py
@@ -0,0 +1,10 @@
+class Error(Exception):
+    pass
+
+
+class SecretError(Error):
+    pass
+
+
+class CryptoError(Error):
+    pass
