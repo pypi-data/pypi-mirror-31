5c432e242b33a201bc0496b6963efdec43ebd58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:13 +0000

    Add package Flask-Helper/0.6/Flask-Helper-0.6.tar.gz

---
+++ packages/Flask-Helper/Flask-Helper-0.6.tar.gz/Flask-Helper-0.6/flask_helper/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+from flask2 import Flask2
+from cross_domain import FlaskCrossDomain
+from template import RenderTemplate
+from upload import support_upload
+
+
+__author__ = 'meisanggou'
