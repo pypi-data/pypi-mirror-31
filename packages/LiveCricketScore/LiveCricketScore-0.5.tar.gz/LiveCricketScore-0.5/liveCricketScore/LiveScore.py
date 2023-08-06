3b332d84415a149008c537c869b8fa377842f1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:20 +0000

    Add package LiveCricketScore/0.5/LiveCricketScore-0.5.tar.gz

---
+++ packages/LiveCricketScore/LiveCricketScore-0.5.tar.gz/LiveCricketScore-0.5/liveCricketScore/LiveScore.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 00:16:50 2018
+
+@author: Manvenddra
+"""
+
+import bs4 as bs
+from urllib import request
+from  win10toast import ToastNotifier
+import time
+#from plyer import notification
+toaster = ToastNotifier()
+
+url = "http://www.cricbuzz.com/cricket-match/live-scores"
+i=1
+while True:
+    sauce = request.urlopen(url).read()
+    soup = bs.BeautifulSoup(sauce,"lxml")
+    score = []
+    results = []
+    for div_tags in soup.find_all('div', attrs={"class": "cb-lv-scrs-col text-black"}):
+        score.append(div_tags.text)
+    try:
+        for result in soup.find_all('div', attrs={"class": "cb-lv-scrs-col cb-text-live"}):
+            results.append(result.text)
+        print(results[0])
+    except Exception as e:
+        for result in soup.find_all('div', attrs={"class": "cb-lv-scrs-col cb-text-complete"}):
+            results.append(result.text)
+    print(score[0])
+    print(results[0])
+    toaster.show_toast(title=score[0],msg=results[0])
+    time.sleep(60*5)
+    if i==(180/5):
+        break
+    i=i+1
