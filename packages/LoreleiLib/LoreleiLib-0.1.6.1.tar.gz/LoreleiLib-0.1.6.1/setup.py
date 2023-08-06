d2d97bbb391ed653e9eae3eb009c8e971a5e902f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:00 +0000

    Add package LoreleiLib/0.1.6.1/LoreleiLib-0.1.6.1.tar.gz

---
+++ packages/LoreleiLib/LoreleiLib-0.1.6.1.tar.gz/LoreleiLib-0.1.6.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name='LoreleiLib',
+    version='0.1.6.1',
+    description='The text based MUD made by Lorinthio',
+    author='Lorinthio',
+    author_email='benrandallswg@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+      'twisted',
+      'pygame',
+      'enum34'
+    ],
+    zip_safe=False)
