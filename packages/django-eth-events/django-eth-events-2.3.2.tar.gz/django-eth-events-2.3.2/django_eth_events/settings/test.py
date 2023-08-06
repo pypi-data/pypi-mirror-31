24656630b2bc752f8a76d542e89a47f4c8ee73b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:30 +0000

    Add package django-eth-events/2.3.2/django-eth-events-2.3.2.tar.gz

---
+++ packages/django-eth-events/django-eth-events-2.3.2.tar.gz/django-eth-events-2.3.2/django_eth_events/settings/test.py
@@ -0,0 +1,43 @@
+from .base import *
+
+SECRET_KEY = 'testtest'
+DEBUG = True
+
+INSTALLED_APPS = (
+    'solo',
+    'django_eth_events',
+)
+
+ETHEREUM_NODE_HOST = 'localhost'
+ETHEREUM_NODE_PORT = 8545
+ETHEREUM_NODE_SSL = 0
+ETHEREUM_MAX_WORKERS = 5
+
+RABBIT_HOSTNAME = 'rabbit'
+RABBIT_USER = 'gnosisdb'
+RABBIT_PASSWORD = 'gnosisdb'
+RABBIT_PORT = '5672'
+BROKER_URL = 'amqp://{user}:{password}@{hostname}:{port}'.format(
+    user=RABBIT_USER,
+    password=RABBIT_PASSWORD,
+    hostname=RABBIT_HOSTNAME,
+    port=RABBIT_PORT
+)
+
+# IPFS
+IPFS_HOST = 'http://ipfs'  # 'ipfs'
+IPFS_PORT = 5001
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
