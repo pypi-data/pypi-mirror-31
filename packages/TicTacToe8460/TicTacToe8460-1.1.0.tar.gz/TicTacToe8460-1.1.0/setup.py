10e873189b57670dab205b1ed7e857b510b4cd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:56 +0000

    Add package TicTacToe8460/1.1.0/TicTacToe8460-1.1.0.tar.gz

---
+++ packages/TicTacToe8460/TicTacToe8460-1.1.0.tar.gz/TicTacToe8460-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+packages = find_packages()
+
+setup(
+    name='TicTacToe8460',
+    version='1.1.0',
+    description="My first python project.Name:Durgesh Kumar Yadav",
+    packages=packages,
+    install_requires=["PyQt5"]
+)
