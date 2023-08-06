81f702e72934736c26de912a1b36b3791cf22b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:52 +0000

    Add package qingmi/0.1.1/qingmi-0.1.1-py2.7.egg

---
+++ packages/qingmi/qingmi-0.1.1-py2.7.egg/qingmi/__about__.py
@@ -0,0 +1,12 @@
+__name__ = 'qingmi'
+__description__ = 'Common modules and toolsets for rapid and efficient development of flask Web.'
+__url__ = 'https://www.zhuxiongxian.cc'
+__version_info__ = ('0', '1', '1')
+__version__ = '.'.join(__version_info__)
+__fullname__ = '-'.join((__name__, __version__))
+__author__ = 'zhuxiongxian'
+__author_email__ = 'zhuxiongxian@gmail.com'
+__maintainer__ = 'zhuxiongxian'
+__maintainer_email__ = 'zhuxiongxian@gmail.com'
+__license__ = 'BSD'
+__copyright__ = '(c) 2018 by zhuxiongxian'
