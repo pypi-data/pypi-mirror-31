401c27e6a200af2cf2e1c45b258e2db5cfd8a43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:08 +0000

    Add package pyrsl/2.0.1/pyrsl-2.0.1.tar.gz

---
+++ packages/pyrsl/pyrsl-2.0.1.tar.gz/pyrsl-2.0.1/rsl/version.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+# Copyright (C) 2016 John Törnblom
+'''
+Keep version info in one single place
+'''
+
+
+name = 'pyrsl'
+date = '2018-05-02'
+version = '2.0'
+release = '2.0.1' #  # ensure that this is the same as in setup.py
+
+complete_string = '%s v%s (%s)' % (name, release, date)
