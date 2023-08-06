0f102603504162dd0753ed8a1e3557e062d4e553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package aletheia/0.3.0/aletheia-0.3.0.tar.gz

---
+++ packages/aletheia/aletheia-0.3.0.tar.gz/aletheia-0.3.0/aletheia/file_types/video/mp4.py
@@ -0,0 +1,9 @@
+from ..base import LargeFile
+
+
+class Mp4File(LargeFile):
+    """
+    This is a placeholder.  If you feel like helping out, this would be an
+    awesome place to start :-)
+    """
+    pass
