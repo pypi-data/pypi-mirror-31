91fb76f2be0f19f8c5f4204f53b8798f8e1e4744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:10 +0000

    Add package TagScriptEngine/0.7.5/TagScriptEngine-0.7.5.tar.gz

---
+++ packages/TagScriptEngine/TagScriptEngine-0.7.5.tar.gz/TagScriptEngine-0.7.5/TagScriptEngine/filters/variable.py
@@ -0,0 +1,30 @@
+import re
+
+ASSIGNMENT_REGEX = re.compile(r"!{(.+)=(.+)}") # Regex to find assignment blocks.
+INTERP_REGEX = re.compile(r"\$(\w+)(=?\w*)")
+
+class VariableFilter():
+    def __init__(self):
+        pass
+
+    def Process(self, engine, text):
+        value = text
+        variable_block = engine.variable_bin
+
+        # register and delete each !{assignment=variable}
+        for (name, val) in ASSIGNMENT_REGEX.findall(value):
+            variable_block[name] = val
+            value = value.replace("!{"+name+"="+val+"}", '').strip("\n").strip(' ')
+
+        if INTERP_REGEX.search(value) is None:
+            return value
+
+        # Replace each $var with its variable block value.
+        for (varname, modifier) in INTERP_REGEX.findall(value): 
+            if varname in variable_block:
+                value = value.replace("$"+varname+modifier, variable_block[varname])
+            elif "=" in modifier: # Substitution required.
+                substitute = modifier.strip("=")
+                value = value.replace("$"+varname+modifier, substitute)
+
+        return value
