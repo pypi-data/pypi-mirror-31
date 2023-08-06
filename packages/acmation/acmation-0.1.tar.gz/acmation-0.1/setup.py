7259b646fa701cbba4e74ef9b0c78aa34b7c2d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:36 +0000

    Add package acmation/0.1/acmation-0.1.tar.gz

---
+++ packages/acmation/acmation-0.1.tar.gz/acmation-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='acmation',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/luzhongqiu/acmation',
+    license='mit',
+    author='zq',
+    author_email='zq.lu@foxmail.com',
+    description='ac automation, sample: https://github.com/luzhongqiu/acmation'
+)
