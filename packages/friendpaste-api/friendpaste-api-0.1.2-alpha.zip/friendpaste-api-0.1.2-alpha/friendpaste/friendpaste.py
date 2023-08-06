7aa64c36e37465ff70bfdccaabf4356d025cf080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:18 +0000

    Add package friendpaste-api/0.1.2-alpha/friendpaste-api-0.1.2-alpha.zip

---
+++ packages/friendpaste-api/friendpaste-api-0.1.2-alpha.zip/friendpaste-api-0.1.2-alpha/friendpaste/friendpaste.py
@@ -0,0 +1,47 @@
+import requests
+
+
+def fp_read(hash, json=False):
+    # return data on sucess
+    url = '%s/%s/raw' % (FP_URL, hash)
+    try:
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise requests.HTTPError
+    except requests.ConnectionError:
+        return None
+    return r.json() if json else r.text
+
+
+def fp_write(hash, text, title="", language="text"):
+    # return True on sucess
+    payload = {
+        "paste_snippet": text,
+        "paste_language": language,
+        "paste_title": title,
+        "psubmit": "Save changes"
+    }
+    r = requests.post('%s/%s/edit' % (FP_URL, hash), data=payload)
+    if r.status_code != 200:
+        raise requests.HTTPError
+    return r.text
+
+
+def fp_create(text, title="", language="text", code=""):
+    # returns new hash on success
+    payload = {
+        "paste_snippet": text,
+        "paste_language": language,
+        "paste_title": title,
+        "paste_code": code,
+        "paste_privacy": "open",
+        "paste_password": "",
+        "psubmit": "Submit post"
+    }
+    r = requests.post('%s/' % FP_URL, data=payload)
+    if r.status_code != 200:
+        raise requests.HTTPError
+    return r.url.replace('%s/' % FP_URL, '')
+
+
+FP_URL = 'https://friendpaste.com'
---
+++ packages/friendpaste-api/friendpaste-api-0.1.2-alpha.zip/friendpaste-api-0.1.2-alpha/setup.py
@@ -0,0 +1,28 @@
+import io
+import os
+from distutils.core import setup
+
+
+version="0.1.2-alpha"
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+  name='friendpaste-api',
+  packages=['friendpaste'],
+  version=version,
+  description='Simple Python API for friendpaste.com',
+  long_description=long_description,
+  author='Jorjon',
+  author_email='jorjon@jorjon.com',
+  url='https://github.com/Jorjon/friendpaste-api',
+  # download_url='https://github.com/Jorjon/friendpaste-api/archive/{0}.tar.gz'.format(version),
+  keywords=['friendpaste', 'api', 'scrapper'],
+  classifiers=[],
+  license='MIT',
+  install_requires=[
+      'requests',
+  ]
+)
