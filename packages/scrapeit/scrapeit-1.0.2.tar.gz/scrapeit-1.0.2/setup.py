292b28e48dc73539d07251e9e6e1f50582d681e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:00 +0000

    Add package scrapeit/1.0.2/scrapeit-1.0.2.tar.gz

---
+++ packages/scrapeit/scrapeit-1.0.2.tar.gz/scrapeit-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = '1.0.2'
+
+requirements = [
+    'requests>=2.18.4',
+    'beautifulsoup4==4.6.0'
+
+]
+dev_requirements = [
+      'pytest>=3.5.1'
+]
+
+setup(name='scrapeit',
+      version=version,
+      description='Configuration driven web scraping framework',
+      author='Jonathon Carlyon',
+      author_email='JonathonCarlyon@gmail.com',
+      url='https://github.com/JonnyFb421/scrapeit',
+      install_requires=requirements,
+      extras_require={'dev': dev_requirements},
+      packages=['scrapeit'],
+      )
