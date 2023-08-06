dcfa381939657e9362fa5056cd5f216f03206e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package django-package-monitor/0.6/django_package_monitor-0.6-py3-none-any.whl

---
+++ packages/django-package-monitor/django_package_monitor-0.6-py3-none-any.whl/package_monitor/settings.py
@@ -0,0 +1,19 @@
+from os import path, getenv
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+REQUIREMENTS_FILE = (
+    getattr(settings, 'PACKAGE_MONITOR_REQUIREMENTS_FILE', None) or
+    getenv('PACKAGE_MONITOR_REQUIREMENTS_FILE', None)
+)
+
+if REQUIREMENTS_FILE is None:
+    raise ImproperlyConfigured('Missing PACKAGE_MONITOR_REQUIREMENTS_FILE setting.')
+
+if not path.exists(REQUIREMENTS_FILE):
+    raise ImproperlyConfigured('Invalid REQUIREMENTS_FILE setting: %s' % REQUIREMENTS_FILE)
+
+
+# length of time to cache return data from PyPI
+PYPI_CACHE_EXPIRY = getattr(settings, 'PACKAGE_MONITOR_PYPI_CACHE_EXPIRY', 30)
