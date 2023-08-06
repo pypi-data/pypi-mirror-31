c4e0331282b8baf6845b6646bb72f688ca593f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:28 +0000

    Add package django-calaccess-processed-data/0.2.7/django-calaccess-processed-data-0.2.7.tar.gz

---
+++ packages/django-calaccess-processed-data/django-calaccess-processed-data-0.2.7.tar.gz/django-calaccess-processed-data-0.2.7/calaccess_processed_filings/admin/campaign/form496.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Admins for Schedule 497, the late contribution report.
+"""
+from __future__ import unicode_literals
+from django.contrib import admin
+from calaccess_processed_filings import models
+from calaccess_raw.admin.base import BaseAdmin
+
+
+@admin.register(models.Form496Filing)
+class Form496FilingAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496Filing model.
+    """
+    list_display = (
+        'filing_id',
+        'amendment_count',
+        'filer_id',
+        'filer_lastname',
+        'filer_firstname',
+        'date_filed',
+        'election_date',
+    )
+
+
+@admin.register(models.Form496FilingVersion)
+class Form496FilingVersionAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496FilingVersion model.
+    """
+    list_display = (
+        'filing',
+        'amend_id',
+        'filer_id',
+        'filer_lastname',
+        'filer_firstname',
+        'date_filed',
+        'election_date',
+    )
+
+
+@admin.register(models.Form496Part1Item)
+class Form496Part1ItemAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496Part1Item model.
+    """
+    list_display = (
+        'filing',
+        "candidate_title",
+        "candidate_lastname",
+        "candidate_firstname",
+        "candidate_name_suffix",
+        "candidate_office_code",
+        "ballot_measure_name",
+        "ballot_measure_number",
+        "ballot_measure_jurisdiction",
+        "support_opposition_code"
+    )
+
+
+@admin.register(models.Form496Part1ItemVersion)
+class Form496Part1ItemVersionAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496Part1ItemVersion model.
+    """
+    list_display = (
+        'filing_version',
+        "candidate_title",
+        "candidate_lastname",
+        "candidate_firstname",
+        "candidate_name_suffix",
+        "candidate_office_code",
+        "ballot_measure_name",
+        "ballot_measure_number",
+        "ballot_measure_jurisdiction",
+        "support_opposition_code"
+    )
+
+
+
+@admin.register(models.Form496Part2Item)
+class Form496Part2ItemAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496Part2Item model.
+    """
+    list_display = (
+        'filing',
+        "line_item",
+        "expense_date",
+        "amount",
+        "transaction_id",
+    )
+
+
+@admin.register(models.Form496Part2ItemVersion)
+class Form496Part2ItemVersionAdmin(BaseAdmin):
+    """
+    Custom admin for the Form496Part2ItemVersion model.
+    """
+    list_display = (
+        'filing_version',
+        "line_item",
+        "expense_date",
+        "amount",
+        "transaction_id",
+    )
