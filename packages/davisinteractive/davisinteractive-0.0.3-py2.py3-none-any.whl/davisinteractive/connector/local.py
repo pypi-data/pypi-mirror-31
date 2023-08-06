89d23c77d06b15602063271bc09a43f37751ed24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:39 +0000

    Add package davisinteractive/0.0.3/davisinteractive-0.0.3-py2.py3-none-any.whl

---
+++ packages/davisinteractive/davisinteractive-0.0.3-py2.py3-none-any.whl/davisinteractive/connector/local.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, division
+
+import json
+import os
+import random
+from glob import glob
+
+import pandas as pd
+
+from ..evaluation import EvaluationService
+from .abstract import AbstractConnector
+
+
+class LocalConnector(AbstractConnector):
+    """ Proxy class to run the EvaluationService locally.
+    """
+
+    VALID_SUBSETS = ['train', 'val']
+
+    def __init__(self):
+        self.service = None
+
+    def start_session(self, subset, davis_root=None):
+        if subset not in self.VALID_SUBSETS:
+            raise ValueError(
+                'For local connector, `subset` must be a valid subset: {}'.
+                format(self.VALID_SUBSETS))
+
+        self.service = EvaluationService(davis_root=davis_root)
+        return self.service.start(subset)
+
+    def get_starting_scribble(self, sequence, scribble_idx):
+        return self.service.get_starting_scribble(sequence, scribble_idx)
+
+    def submit_masks(self, sequence, scribble_idx, pred_masks, timming,
+                     interaction):
+        return self.service.submit_masks(sequence, scribble_idx, pred_masks,
+                                         timming, interaction)
+
+    def get_report(self):
+        return self.service.get_report()
+
+    def close(self):
+        self.service.close()
