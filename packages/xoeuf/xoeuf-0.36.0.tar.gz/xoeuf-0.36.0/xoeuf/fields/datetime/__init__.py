30ec675ac0e757c6470870e807d6fcc4a747edc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:10 +0000

    Add package xoeuf/0.36.0/xoeuf-0.36.0.tar.gz

---
+++ packages/xoeuf/xoeuf-0.36.0.tar.gz/xoeuf-0.36.0/xoeuf/fields/datetime/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from xoeuf.odoo.release import version_info as ODOO_VERSION_INFO
+
+if ODOO_VERSION_INFO[0] >= 10:
+    from ._dt10 import LocalizedDatetime  # noqa: reexport
+else:
+    assert False
+
+LocalizedDatetime.__module__ = __name__
