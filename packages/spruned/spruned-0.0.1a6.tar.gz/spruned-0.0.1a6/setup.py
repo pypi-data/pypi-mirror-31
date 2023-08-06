61c95e0375ef1bf977d12e78c2a7d50af8b9382d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:00 +0000

    Add package spruned/0.0.1a6/spruned-0.0.1a6.tar.gz

---
+++ packages/spruned/spruned-0.0.1a6.tar.gz/spruned-0.0.1a6/setup.py
@@ -0,0 +1,92 @@
+import argparse
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+import sys
+sys.path.insert(0, '.')
+import spruned
+import sys
+
+def read_pip_requirements():
+    reqs = []
+    with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if '://' not in line:
+            reqs.append(line)
+        else:
+            u, p = line.split('#egg=')
+            p = p.replace('-', '==', 1)
+            reqs.append(p)
+    return reqs
+
+
+def read_dependency_links():
+    links = []
+    with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), 'r') as f:
+        lines = f.readlines()
+    for l in lines:
+        line = l.strip()
+        if '://' in line:
+            links.append(line)
+    return links
+
+    #res = []
+    #for link in links:
+    #    link = link.replace('git+', '')
+    #    res.append(link.replace('.git@spruned-support#', '/tarball/spruned-support#'))
+    #return res
+
+
+def read_file(name):
+    with open(name, 'r', encoding='utf-8') as f:
+        file = f.read()
+    return file
+
+
+if 'install' in sys.argv:
+    github_links = '\n- '.join(read_dependency_links())
+    github_libraries = '\n- '.join([x.split('#egg=')[1] for x in read_dependency_links()])
+
+    print('\nWarning! Know what you do!')
+    print('\nSpruned installer will also install custom dependencies from the following github links:\n\n-', github_links)
+    print('\nIs warmly advised to run spruned into a virtual environment, '
+          'especially if you have installed the following libraries:\n\n-', github_libraries)
+    input('\n\nPress Ctrl+C to stop the installation or Enter other key to continue\n\n')
+
+
+    def install_github_dependencies():
+
+        for dep in read_dependency_links():
+            subprocess.call(['pip', 'install', '-e', dep])
+
+
+    install_github_dependencies()
+
+setup(
+    name='spruned',
+    version=spruned.__version__,
+    url='https://github.com/gdassori/spruned/',
+    license='MIT',
+    author='Guido Dassori',
+    author_email='guido.dassori@gmail.com',
+    python_requires='>=3.5.2',
+    description='Bitcoin Lightweight Pseudonode',
+    long_description=read_file('README.rst'),
+    install_requires=read_pip_requirements(),
+    packages=find_packages(exclude=['htmlcov']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spruned = spruned.app:main'
+        ]
+    },
+    include_package_data=True
+)
