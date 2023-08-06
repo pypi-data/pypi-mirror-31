ec26e110d57f91482ef886298437fd7fe0202392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:32 +0000

    Add package OpenFisca-Core/23.0.0/OpenFisca_Core-23.0.0-py2-none-any.whl

---
+++ packages/OpenFisca-Core/OpenFisca_Core-23.0.0-py2-none-any.whl/openfisca_core/model_api.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from datetime import date  # noqa analysis:ignore
+
+from numpy import (   # noqa analysis:ignore
+    logical_not as not_,
+    maximum as max_,
+    minimum as min_,
+    round as round_,
+    select,
+    where,
+)
+
+from .holders import (  # noqa analysis:ignore
+    set_input_dispatch_by_period,
+    set_input_divide_by_period,
+    )
+from .indexed_enums import Enum  # noqa analysis:ignore
+from .entities import (ADD, DIVIDE)  # noqa analysis:ignore
+from .simulations import (  # noqa analysis:ignore
+    calculate_output_add,
+    calculate_output_divide,
+    )
+from .base_functions import (   # noqa analysis:ignore
+    missing_value,
+    requested_period_default_value,
+    requested_period_last_or_next_value,
+    requested_period_last_value,
+    )
+from .variables import Variable  # noqa analysis:ignore
+from .formula_helpers import apply_thresholds, concat, switch  # noqa analysis:ignore
+from .periods import MONTH, YEAR, ETERNITY, period  # noqa analysis:ignore
+from .reforms import Reform  # noqa analysis:ignore
+from .parameters import load_parameter_file, ParameterNode, Scale, Bracket, Parameter, ValuesHistory  # noqa analysis:ignore
