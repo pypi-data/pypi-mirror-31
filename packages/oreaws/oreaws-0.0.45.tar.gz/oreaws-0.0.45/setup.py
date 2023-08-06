14c1d8a2be46fcc92f4a9820ac1b1e592020935b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:19 +0000

    Add package oreaws/0.0.45/oreaws-0.0.45.tar.gz

---
+++ packages/oreaws/oreaws-0.0.45.tar.gz/oreaws-0.0.45/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#from setuptools import setup
+from setuptools import find_packages, setup
+
+setup(
+      name='oreaws',
+      version='0.0.45',
+      description='Python AWS CLI Tool',
+      license='MIT',
+      author='yhidetoshi',
+      author_email='example@python.net',
+      url='https://github.com/yhidetoshi/python-awscli-tool',
+#      packages=['oreaws.oreaws'],
+#      packages=['oreaws'],
+      packages=find_packages(),
+#      packages=find_packages(exclude=['tests']),
+#      namespace_packages=['oreaws'],
+#      packages=find_packages('oreaws'),
+#      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      install_requires=['boto3','prettytable','click'],
+      py_modules=['oreaws.modules.ec2','oreaws.modules.s3','oreaws.modules.route53','oreaws.modules.autoscaling'],
+      platforms='any',
+      entry_points={
+              'console_scripts': [
+                     'oreaws = oreaws.awscli:main',
+               ],
+      },
+)
