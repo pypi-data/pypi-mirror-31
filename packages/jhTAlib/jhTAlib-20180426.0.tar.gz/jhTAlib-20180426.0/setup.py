8c0c2b6ee4cbf3b1fd4bc83030e23134a00d0614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:14 +0000

    Add package jhTAlib/20180426.0/jhTAlib-20180426.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20180426.0.tar.gz/jhTAlib-20180426.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jhTAlib',
+    version='20180426.0',
+    description='Technical Analysis Library',
+    keywords='Technical Analysis Library',
+    url='https://github.com/joosthoeks/jhTAlib',
+    author='Joost Hoeks',
+    author_email='joosthoeks@gmail.com',
+    license='GNU',
+    packages=find_packages(),
+    install_requires=[
+#        'numpy',
+#        'pandas',
+    ],
+    zip_safe=False
+)
+
