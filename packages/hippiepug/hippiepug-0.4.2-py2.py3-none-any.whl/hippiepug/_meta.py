3f3b3451b7ed249aa553fe295ad07d532fa7fc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:48 +0000

    Add package hippiepug/0.4.2/hippiepug-0.4.2-py2.py3-none-any.whl

---
+++ packages/hippiepug/hippiepug-0.4.2-py2.py3-none-any.whl/hippiepug/_meta.py
@@ -0,0 +1,20 @@
+__version__ = '0.1'
+__title__ = 'hippiepug'
+__author__ = 'Bogdan Kulynych'
+__email__ = 'hello@bogdankulynych.me'
+__copyright__ = 'Copyright (c) 2018 Bogdan Kulynych, EPFL SPRING Lab'
+__description__ = ('Sublinear-traversal blockchains and efficient key-value '
+                   'Merkle trees with a flexible storage backend.')
+__doc__ = __description__
+__license__ = "AGPLv3"
+
+
+__all__ = [
+    '__version__',
+    '__title__',
+    '__author__',
+    '__email__',
+    '__copyright__',
+    '__description__',
+    '__doc__',
+    '__license__']
