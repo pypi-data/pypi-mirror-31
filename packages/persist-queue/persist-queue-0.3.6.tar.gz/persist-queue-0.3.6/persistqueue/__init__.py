529e553cd2ee58fa6af13a0b6bff4a3459ca2eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:03 +0000

    Add package persist-queue/0.3.6/persist-queue-0.3.6.tar.gz

---
+++ packages/persist-queue/persist-queue-0.3.6.tar.gz/persist-queue-0.3.6/persistqueue/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+__author__ = 'Peter Wang'
+__license__ = 'BSD'
+__version__ = '0.3.6'
+
+from .exceptions import Empty, Full  # noqa
+from .pdict import PDict  # noqa
+from .queue import Queue  # noqa
+from .sqlqueue import SQLiteQueue, FIFOSQLiteQueue, FILOSQLiteQueue, UniqueQ  # noqa
+
+__all__ = ["Queue", "SQLiteQueue", "FIFOSQLiteQueue", "FILOSQLiteQueue",
+           "UniqueQ", "PDict", "Empty", "Full", "__author__", "__license__",
+           "__version__"]
