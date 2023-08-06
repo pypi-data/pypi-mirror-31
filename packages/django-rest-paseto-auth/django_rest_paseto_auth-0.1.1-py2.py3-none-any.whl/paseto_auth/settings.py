3ed99b6e1bc5e29a220a9979f9a05493a1445d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:07 +0000

    Add package django-rest-paseto-auth/0.1.1/django_rest_paseto_auth-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-rest-paseto-auth/django_rest_paseto_auth-0.1.1-py2.py3-none-any.whl/paseto_auth/settings.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+user_settings = getattr(settings, 'PASETO_AUTH', {})
+
+AUTH_SETTINGS = {
+    'SECRET_KEY': settings.PASETO_KEY,
+    'HEADER_PREFIX': user_settings.get('HEADER_PREFIX', 'Paseto'),
+    'ACCESS_LIFETIME': min(user_settings.get('ACCESS_LIFETIME', 5*60), 10*60),
+    'REFRESH_SHORT_LIFETIME': min(
+        user_settings.get('REFRESH_SHORT_LIFETIME', 12*3600), 24*3600
+    ),
+    'REFRESH_LONG_LIFETIME': min(
+        user_settings.get('REFRESH_LONG_LIFETIME', 30*24*3600), 60*24*3600
+    ),
+    'REFRESH_PERMANENT_LIFETIME': user_settings.get(
+        'REFRESH_PERMANENT_LIFETIME', 2*365*24*3600
+    )
+}
