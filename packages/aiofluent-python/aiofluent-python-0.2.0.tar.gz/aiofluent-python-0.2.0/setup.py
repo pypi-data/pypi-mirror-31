9e292a70cc282e6c25558aaf6b5b144f6bf3dfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:28 +0000

    Add package aiofluent-python/0.2.0/aiofluent-python-0.2.0.tar.gz

---
+++ packages/aiofluent-python/aiofluent-python-0.2.0.tar.gz/aiofluent-python-0.2.0/setup.py
@@ -0,0 +1,41 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os.path
+import re
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+BASE_PATH = os.path.dirname(__file__)
+
+
+with open(os.path.join(BASE_PATH, 'aiofluent.py')) as f:
+    try:
+        version = VERSION_RE.search(f.read()).group(1)
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+setup(
+    name='aiofluent-python',
+    description='A fluentd client libary intended to work with asyncio.',
+    license='MIT',
+    version=version,
+    author='Yingbo Gu',
+    author_email='tensiongyb@gmail.com',
+    maintainer='Yingbo Gu',
+    maintainer_email='tensiongyb@gmail.com',
+    url='https://github.com/guyingbo/aiofluent',
+    py_modules=['aiofluent'],
+    python_requires='>=3.5',
+    install_requires=[
+        'msgpack-python',
+        'async_timeout',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
