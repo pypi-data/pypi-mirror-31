bb05d6194ec5a0b050c1848402b42155c2c16259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:44 +0000

    Add package flying/0.0.1/flying-0.0.1.tar.gz

---
+++ packages/flying/flying-0.0.1.tar.gz/flying-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='flying',
+    version='0.0.1',
+    keywords=('release', 'cli', 'docker', 'git', 'npm'),
+    description='Flying : cli tools for manage releases',
+    license='MIT License',
+
+    url='https://github.com/joway/flying',
+    author='joway',
+    author_email='joway.w@gmail.com',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'fire==0.1.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'flying=flying.main:main'
+        ]
+    },
+)
