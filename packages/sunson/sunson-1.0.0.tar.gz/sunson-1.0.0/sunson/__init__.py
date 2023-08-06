c03004296ff121db5b603b11f44c445cead20336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:53 +0000

    Add package sunson/1.0.0/sunson-1.0.0.tar.gz

---
+++ packages/sunson/sunson-1.0.0.tar.gz/sunson-1.0.0/sunson/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#  ____    _   _   _   _   ____     ___    _   _ 
+# / ___|  | | | | | \ | | / ___|   / _ \  | \ | |
+# \___ \  | | | | |  \| | \___ \  | | | | |  \| |
+#  ___) | | |_| | | |\  |  ___) | | |_| | | |\  |
+# |____/   \___/  |_| \_| |____/   \___/  |_| \_|
+
+"""
+Sunson Library
+~~~~~~~~~~~~~~~~~~~~~
+
+TODO
+
+"""
+
+
+from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __build__, __author__, __author_email__, __license__
+from .__version__ import __copyright__, __cake__
+
+from .SunSon import Sunson, main
+
