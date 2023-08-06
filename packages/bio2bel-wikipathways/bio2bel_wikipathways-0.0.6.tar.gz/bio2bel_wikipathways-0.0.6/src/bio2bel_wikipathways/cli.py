e856262febcb5edd0796fd872a38a9b06ea1519c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package bio2bel-wikipathways/0.0.6/bio2bel_wikipathways-0.0.6.tar.gz

---
+++ packages/bio2bel-wikipathways/bio2bel_wikipathways-0.0.6.tar.gz/bio2bel_wikipathways-0.0.6/src/bio2bel_wikipathways/cli.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import logging
+import os
+
+import click
+from pandas import DataFrame, Series
+
+from bio2bel import build_cli
+from bio2bel_wikipathways.constants import DEFAULT_CACHE_CONNECTION
+from bio2bel_wikipathways.manager import Manager
+
+log = logging.getLogger(__name__)
+
+main = build_cli(Manager)
+
+
+@main.command()
+@click.option('-c', '--connection', help="Defaults to {}".format(DEFAULT_CACHE_CONNECTION))
+def export(connection):
+    """Export all pathway - gene info to a excel file"""
+    m = Manager(connection=connection)
+
+    log.info("Querying the database")
+
+    # https://stackoverflow.com/questions/19736080/creating-dataframe-from-a-dictionary-where-entries-have-different-lengths
+    genesets = DataFrame(
+        dict([
+            (k, Series(list(v)))
+            for k, v in m.export_genesets().items()
+        ])
+    )
+
+    log.info("Geneset exported to '{}/wikipathways_gene_sets.xlsx'".format(os.getcwd()))
+
+    genesets.to_excel('wikipathways_gene_sets.xlsx', index=False)
+
+
+if __name__ == '__main__':
+    main()
