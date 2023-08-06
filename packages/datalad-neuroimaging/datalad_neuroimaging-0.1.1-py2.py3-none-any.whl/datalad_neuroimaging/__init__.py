ebd496d14d90f87b47f7442364dc68e50634b66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:59 +0000

    Add package datalad-neuroimaging/0.1.1/datalad_neuroimaging-0.1.1-py2.py3-none-any.whl

---
+++ packages/datalad-neuroimaging/datalad_neuroimaging-0.1.1-py2.py3-none-any.whl/datalad_neuroimaging/__init__.py
@@ -0,0 +1,23 @@
+"""DataLad neuroimaging extension"""
+
+__docformat__ = 'restructuredtext'
+
+from .version import __version__
+
+# defines a datalad command suite
+# this symbold must be indentified as a setuptools entrypoint
+# to be found by datalad
+command_suite = (
+    # description of the command suite, displayed in cmdline help
+    "Neuroimaging tools",
+    [
+        # specification of a command, any number of commands can be defined
+        (
+            # importable module that contains the command implementation
+            'datalad_neuroimaging.bids2scidata',
+            # name of the command class implementation in above module
+            'BIDS2Scidata',
+            'bids2scidata',
+        ),
+    ]
+)
