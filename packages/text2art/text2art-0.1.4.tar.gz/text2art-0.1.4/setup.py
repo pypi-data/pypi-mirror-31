57aa5326a269705472f7e379660a272251726e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:58 +0000

    Add package text2art/0.1.4/text2art-0.1.4.tar.gz

---
+++ packages/text2art/text2art-0.1.4.tar.gz/text2art-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.4"
+
+with open('README.rst') as fp:
+    README = fp.read()
+
+setup(
+    name = "text2art",
+    version = VERSION,
+    long_description = README,
+    description = "Generating controlled terminal art fonts",
+    license = "MIT",
+    include_package_data = True,
+    author = "hangfengYang",
+    author_email = "yhf5fhy@gmail.com",
+    classifiers = [],
+    url = "https://github.com/Fenghuapiao/text2art",
+    packages = ["text2art"],
+    install_requires = [
+        "fire",
+        "colorama",
+        "pyfiglet"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "text2art  =  text2art.main:main",
+        ],
+    }
+)
