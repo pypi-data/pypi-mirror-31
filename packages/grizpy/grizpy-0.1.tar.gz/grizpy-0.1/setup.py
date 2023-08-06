0769def54aa27a41077a91bf97b4346820ceb64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:43 +0000

    Add package grizpy/0.1/grizpy-0.1.tar.gz

---
+++ packages/grizpy/grizpy-0.1.tar.gz/grizpy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='grizpy',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Mahmoud Alfayoumi',
+      author_email='mah.fay.g@gmail.com',
+      license='',
+      packages=['grizpy'],
+      zip_safe=False)
