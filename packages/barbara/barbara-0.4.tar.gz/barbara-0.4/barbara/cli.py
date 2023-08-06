17a1f5f88aa72acb5248de189b736f27c60a712e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:54 +0000

    Add package barbara/0.4/barbara-0.4.tar.gz

---
+++ packages/barbara/barbara-0.4.tar.gz/barbara-0.4/barbara/cli.py
@@ -0,0 +1,32 @@
+import os
+
+import click
+
+from .utils import confirm_target_file, merge_with_prompts, print_version
+from .reader import Reader
+from .writer import Writer
+
+
+@click.command()
+@click.option('-s', '--skip-existing', default=True,
+              type=click.BOOL, help='Skip over any keys which already exist in the destination file')
+@click.option('-d', '--destination', default='',
+              type=str, help='Destination for serialized environment variables')
+@click.option('-t', '--template', default='.env.template',
+              type=click.File(), help='Source for environment and default values')
+@click.option('-v', '--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True,
+              help='Print version and exit.')
+def barbara(skip_existing, destination, template):
+    confirmed_target = destination if os.path.exists(destination) else confirm_target_file(destination)
+
+    click.echo(f'Creating environment: {confirmed_target}')
+
+    environment_template = Reader(template).read()
+    existing_environment = Reader(confirmed_target).read()
+    click.echo(f'Skip Existing: {skip_existing}')
+
+    environment = merge_with_prompts(existing_environment, environment_template, skip_existing)
+
+    Writer(confirmed_target, environment).write()
+
+    click.echo('Environment ready!')
