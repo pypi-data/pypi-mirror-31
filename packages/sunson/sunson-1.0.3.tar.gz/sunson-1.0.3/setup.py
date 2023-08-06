2754df2b3c690aa772718617a3c041c6dde4093b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:18 +0000

    Add package sunson/1.0.3/sunson-1.0.3.tar.gz

---
+++ packages/sunson/sunson-1.0.3.tar.gz/sunson-1.0.3/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# Learn more: https://github.com/kennethreitz/setup.py
+
+import os
+import shutil
+import re
+import sys
+
+from codecs import open
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    try:
+        os.system('twine upload dist/*')
+    except:
+        print('upload failed:', sys.info[0], sys.info[1])
+        pass
+    shutil.rmtree('build')
+    shutil.rmtree('dist')
+    shutil.rmtree('sunson.egg-info')
+    sys.exit()
+
+packages = ['sunson']
+
+requires = [
+    'requests>=2.18.4',
+    'pymongo>=3.5',
+
+]
+
+about = {}
+with open(os.path.join(here, 'sunson', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme + '\n\n' + history,
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    python_requires=">=3.0",
+    install_requires=requires,
+    license=about['__license__'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ),
+)
