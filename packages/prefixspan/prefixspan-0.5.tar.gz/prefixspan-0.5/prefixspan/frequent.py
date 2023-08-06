7453cc459ede54f489988a09d15f33c9422087c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package prefixspan/0.5/prefixspan-0.5.tar.gz

---
+++ packages/prefixspan/prefixspan-0.5.tar.gz/prefixspan-0.5/prefixspan/frequent.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+from .localtyping import *
+
+from .prefixspan import PrefixSpan
+from .scan import scan
+
+from .closed import isclosed, canclosedprune
+from .generator import isgenerator, cangeneratorprune
+
+def PrefixSpan_frequent(
+        self, minsup, closed=False, generator=False,
+        key=None, bound=None,
+        filter=None
+    ):
+    # type: (PrefixSpan, int, bool, bool, Union[None, Key], Union[None, Key], Union[None, Filter]) -> Results
+    if generator:
+        occursstack = [] # type: List[Occurs]
+
+    def canpass(sup):
+        # type: (int) -> bool
+        return sup < minsup
+
+
+    def verify(patt, matches):
+        # type: (Pattern, Matches) -> None
+        sup = key(patt, matches)
+        if canpass(sup):
+            return
+
+        if (filter is None or filter(patt, matches)) and (
+                (not closed or isclosed(self._db, patt, matches)) and
+                (not generator or isgenerator(self._db, patt, matches, occursstack))
+            ):
+            self._results.append((sup, patt))
+
+
+    def frequent_rec(patt, matches):
+        # type: (Pattern, Matches) -> None
+        if len(patt) >= self.minlen:
+            verify(patt, matches)
+
+            if len(patt) == self.maxlen:
+                return
+
+        occurs = scan(self._db, matches)
+        if generator:
+            occursstack.append(occurs)
+
+        for newitem, newmatches in occurs.items():
+            newpatt = patt + [newitem]
+            if canpass(bound(newpatt, newmatches)) or (
+                    closed and canclosedprune(self._db, newpatt, newmatches) or
+                    generator and cangeneratorprune(self._db, newpatt, newmatches, occursstack)
+                ):
+                continue
+
+            frequent_rec(newpatt, newmatches)
+
+        if generator:
+            occursstack.pop()
+
+
+    if key is None:
+        key = bound = PrefixSpan.defaultkey
+
+    return self._mine(frequent_rec)
---
+++ packages/prefixspan/prefixspan-0.5.tar.gz/prefixspan-0.5/prefixspan/localtyping.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from typing import *
+
+DB = List[List[int]]
+
+Matches = List[Tuple[int, int]]
+Occurs = Dict[int, Matches]
+
+Pattern = List[int]
+Results = List[Tuple[int, Pattern]]
+
+Key = Callable[[Pattern, Matches], int]
+Filter = Callable[[Pattern, Matches], bool]
