d08a38f96de4bd10a6f00c0f30853a3466f6bdb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:53 +0000

    Add package openformat/0.1.3/openformat-0.1.3.tar.gz

---
+++ packages/openformat/openformat-0.1.3.tar.gz/openformat-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+
+setup(
+    name='openformat',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.3'
+    ],
+    version="0.1.3",
+    author="Till Hoffmann",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
