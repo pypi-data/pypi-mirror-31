d7f25821e3b9f43dd9e2eaf9796465a0c1b29ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:10 +0000

    Add package pytest-session-fixture-globalize/0.0.0.2/pytest-session-fixture-globalize-0.0.0.2.tar.gz

---
+++ packages/pytest-session-fixture-globalize/pytest-session-fixture-globalize-0.0.0.2.tar.gz/pytest-session-fixture-globalize-0.0.0.2/pytest_session_fixture_globalize.py
@@ -0,0 +1,36 @@
+import os.path
+import pytest
+from _pytest.fixtures import FixtureManager
+
+def __parsefactories__(self, *args, **kwargs):
+    self.parsefactories_real(*args,**kwargs)
+    for k in self._arg2fixturedefs:
+        flist = self._arg2fixturedefs[k]
+        session_test_list = set()
+        for i,e in enumerate(flist):
+            if e.scope!='session':
+                continue
+            if '.py' in e.baseid:
+                e.baseid=os.path.dirname(e.baseid)
+            key = tuple([e.argname,e.baseid])
+            if key in session_test_list:
+                flist[i]=None
+            else:
+                session_test_list.add(key)
+        for i in range(len(flist)-1,-1,-1):
+            if flist[i] is None: flist.pop(i)
+        self._arg2fixturedefs[k] = flist
+
+def pytest_addoption(parser):
+    parser.addoption("--session-fixture-globalize", action='store_true', help="make session fixtures behave as if written in conftest, even if it is written in some modules")
+
+def pytest_configure(config):
+    if getattr(config.option,"session_fixture_globalize",False) or os.environ.get("PYTEST_SESSION_FIXTURE_GLOBALIZE"):
+        if 'parsefactories_real' not in FixtureManager.__dict__:
+            FixtureManager.parsefactories_real = FixtureManager.parsefactories
+            FixtureManager.parsefactories = __parsefactories__
+
+def pytest_unconfigure(config):
+    if 'parsefactories_real' in FixtureManager.__dict__:
+        FixtureManager.parsefactories = FixtureManager.parsefactories_real
+        del FixtureManager.parsefactories_real
