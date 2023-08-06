2c1c241118587d41285f1f513602810a4162186d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:49 +0000

    Add package matialvarezs_time_sleep/0.1.42/matialvarezs_time_sleep-0.1.42.tar.gz

---
+++ packages/matialvarezs_time_sleep/matialvarezs_time_sleep-0.1.42.tar.gz/matialvarezs_time_sleep-0.1.42/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'matialvarezs_time_sleep',
+  packages = ['matialvarezs_time_sleep'], # this must be the same as the name above
+  version = '0.1.42',
+  description = 'Long time sleep divided into small times',
+  author = 'Matias Alvarez Sabate',
+  author_email = 'matialvarezs@gmail.com',
+  #url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  #keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
