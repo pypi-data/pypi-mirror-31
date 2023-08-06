61387f6677f57afc3f6038dcca1ff15b72fe1282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:45 +0000

    Add package LoreleiClient/0.1.6.5/LoreleiClient-0.1.6.5.tar.gz

---
+++ packages/LoreleiClient/LoreleiClient-0.1.6.5.tar.gz/LoreleiClient-0.1.6.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(name='LoreleiClient',
+    version='0.1.6.5',
+    description='The text based MUD made by Lorinthio',
+    author='Lorinthio',
+    author_email='benrandallswg@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'twisted',
+        'pygame',
+        'enum34',
+        'LoreleiLib'
+    ],
+    include_package_data = True,
+    package_data = {
+    '' : ['*.png'],
+    },
+    zip_safe=False)
