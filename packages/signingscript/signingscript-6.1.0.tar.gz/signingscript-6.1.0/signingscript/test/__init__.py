3f17335d4d3ef362056daa88437bee839ec820bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package signingscript/6.1.0/signingscript-6.1.0.tar.gz

---
+++ packages/signingscript/signingscript-6.1.0.tar.gz/signingscript-6.1.0/signingscript/test/__init__.py
@@ -0,0 +1,41 @@
+import asyncio
+import os
+import pytest
+import tempfile
+
+
+def read_file(path):
+    with open(path, 'r') as fh:
+        return fh.read()
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+PUB_KEY_PATH = os.path.join(TEST_DATA_DIR, "id_rsa.pub")
+PUB_KEY = read_file(PUB_KEY_PATH)
+
+
+def noop_sync(*args, **kwargs):
+    pass
+
+
+async def noop_async(*args, **kwargs):
+    pass
+
+
+@pytest.yield_fixture(scope='function')
+def tmpdir():
+    with tempfile.TemporaryDirectory() as tmp:
+        yield tmp
+
+
+@pytest.yield_fixture(scope='function')
+def tmpfile():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(bytes("fake", "utf-8"))
+        tmp.flush()
+        return tmp.name
+
+
+def die(*args, **kwargs):
+    raise SigningScriptError("dying!")
