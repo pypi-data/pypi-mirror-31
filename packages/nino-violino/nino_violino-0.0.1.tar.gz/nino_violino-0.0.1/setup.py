07333a79ccb4e0d34562666a2440c65a22f40956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:10 +0000

    Add package nino-violino/0.0.1/nino_violino-0.0.1.tar.gz

---
+++ packages/nino-violino/nino_violino-0.0.1.tar.gz/nino_violino-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+  name='nino_violino',
+  packages=['block_generator'],
+  version='0.0.1',
+  description='Yet another music generator. ',
+  keywords=['nino_violino'],
+  author='Nikola Dokoski',
+  install_requires=[
+    'midiutil',
+  ],
+)
