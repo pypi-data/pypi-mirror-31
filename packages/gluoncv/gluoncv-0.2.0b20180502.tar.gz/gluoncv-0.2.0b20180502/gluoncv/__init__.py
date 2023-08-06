3e9386c100831817d49c6c6abca3327ad4c3b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:28 +0000

    Add package gluoncv/0.2.0b20180502/gluoncv-0.2.0b20180502.tar.gz

---
+++ packages/gluoncv/gluoncv-0.2.0b20180502.tar.gz/gluoncv-0.2.0b20180502/gluoncv/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""GluonCV: a deep learning vision toolkit powered by Gluon."""
+from __future__ import absolute_import
+
+__version__ = '0.2.0'
+
+from . import data
+from . import model_zoo
+from . import utils
