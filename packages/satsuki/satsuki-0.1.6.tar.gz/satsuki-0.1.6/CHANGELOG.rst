241bf6fff32c13b4dfc9c68fabe03b00f0311444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:43 +0000

    Add package satsuki/0.1.6/satsuki-0.1.6.tar.gz

---
+++ packages/satsuki/satsuki-0.1.6.tar.gz/satsuki-0.1.6/CHANGELOG.rst
@@ -0,0 +1,29 @@
+CHANGE LOG
+==========
+
+0.1.6 - 2018.05.10
+------------------
+* [BUG FIX] Wasn't deleting release asset if file given that didn't
+  exist locally.
+* [ENHANCEMENT] Added --force option to delete tags and releases
+  using pattern matching.
+
+0.1.5 - 2018.05.10
+------------------
+* [BUG FIX] Implemented file (release asset) delete
+* [ENHANCEMENT] Generally improved asset handling including SHA256 sums
+  for files and better error handling
+
+0.1.4 - 2018.05.08
+------------------
+* [BUG FIX] Looked like an error was occuring because returning True
+  instead of os.EX_OK.
+
+0.1.3 - 2018.05.07
+------------------
+* [ENHANCEMENT] Added POSIX-style filename pattern matching for
+  cleaning up tags.
+
+0.1.2 - 2018.05.07
+------------------
+* Initial release!
---
+++ packages/satsuki/satsuki-0.1.6.tar.gz/satsuki-0.1.6/tests/test_satsuki.py
@@ -0,0 +1,95 @@
+# test_pyppyn.py
+
+import pytest
+import uuid
+import os
+
+from satsuki import Arguments, ReleaseMgr, EXIT_OK
+
+TEST_VERBOSE = True
+TEST_BODY = str(uuid.uuid1())
+TEST_SLUG = "YakDriver/satsuki"
+TEST_TAG = "Test-v" + TEST_BODY[:6]
+TEST_REL_NAME = "Test Release v" + TEST_BODY[:6]
+TEST_COMMITISH = "5aacf6b744ec379afafbf3bac2131474a464ee9d"
+TEST_FILENAME = 'tests/release-asset.exe'
+
+def test_blank_arguments():
+    """ Returns an Arguments instance with nothing set. """
+    with pytest.raises(PermissionError):
+        Arguments()
+
+@pytest.fixture
+def arguments_base(token):
+    """ Basic arguments with authorization (must provide token) """
+    return Arguments(
+        verbose = TEST_VERBOSE,
+        token = token,
+        slug = TEST_SLUG,
+        tag = TEST_TAG,
+        body = TEST_BODY,
+        rel_name = TEST_REL_NAME,
+        commitish = TEST_COMMITISH
+    )
+
+def test_create_release(arguments_base):
+    rm = ReleaseMgr(arguments_base)
+    rm.execute() # <== should create
+    compare_args = Arguments(
+        verbose = TEST_VERBOSE,
+        token = arguments_base.api_token,
+        slug = TEST_SLUG,
+        tag = TEST_TAG
+    )
+
+    assert compare_args.body == TEST_BODY \
+        and compare_args.rel_name == TEST_REL_NAME
+
+def test_get_latest(arguments_base):
+    rm = ReleaseMgr(arguments_base)
+    rm.execute() # <== should create
+    compare_args = Arguments(
+        verbose = TEST_VERBOSE,
+        token = arguments_base.api_token,
+        slug = TEST_SLUG,
+        latest = True
+    )
+
+    if compare_args.tag == TEST_TAG:
+        assert compare_args.tag == TEST_TAG \
+            and compare_args.body == TEST_BODY \
+            and compare_args.rel_name == TEST_REL_NAME
+    else:
+        # a real tag has gotten in first, forget the test
+        assert True
+
+def test_upload_file(token):
+    with open(TEST_FILENAME, 'wb') as fout:
+        fout.write(os.urandom(1024000))
+
+    args = Arguments(
+        verbose = TEST_VERBOSE,
+        token = token,
+        slug = TEST_SLUG,
+        tag = TEST_TAG,
+        file_file = "tests/test.file"
+    )
+
+    ul_rel = ReleaseMgr(args)
+    ul_rel.execute()
+    assert True
+
+def test_delete_release(token):
+
+    delete_args = Arguments(
+        verbose = TEST_VERBOSE,
+        token = token,
+        slug = TEST_SLUG,
+        tag = TEST_TAG,
+        command = Arguments.COMMAND_DELETE,
+        include_tag = True
+    )
+
+    del_rel = ReleaseMgr(delete_args)
+    del_rel.execute()
+    assert True
