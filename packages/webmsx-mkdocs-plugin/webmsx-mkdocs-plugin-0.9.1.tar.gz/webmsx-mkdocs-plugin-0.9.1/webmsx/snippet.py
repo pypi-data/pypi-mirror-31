be2a2c3e21894325e0c33c7a6f7f62c0d97de273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:47 +0000

    Add package webmsx-mkdocs-plugin/0.9.1/webmsx-mkdocs-plugin-0.9.1.tar.gz

---
+++ packages/webmsx-mkdocs-plugin/webmsx-mkdocs-plugin-0.9.1.tar.gz/webmsx-mkdocs-plugin-0.9.1/webmsx/snippet.py
@@ -0,0 +1,29 @@
+from os.path import join, dirname
+# from os import getcwd
+
+
+class HTMLText:
+
+    def __init__(self, p_game, p_machine):
+        self.html = ""
+        f = open(join(dirname(__file__), "./code.inc.html"), "r")
+        self.html = f.read()
+        self.html = self.html.replace("{% GAME %}", p_game)
+        self.html = self.html.replace("{% MACHINE %}", p_machine)
+        f.close()
+
+    def add(self, p_text):
+        self.html += p_text + "\n"
+
+    def adn(self, p_text):
+        self.html += p_text.strip()
+
+    def __str__(self):
+        return self.html
+
+
+def webmsx_snippet(p_game, p_machine):
+    html = HTMLText(p_game, p_machine)
+
+    return str(html)
+
