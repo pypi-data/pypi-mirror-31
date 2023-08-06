559e82bc302c0489d5cafb8ba6b73bc9b54ae1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package pulp-smash/2018.4.26/pulp-smash-2018.4.26.tar.gz

---
+++ packages/pulp-smash/pulp-smash-2018.4.26.tar.gz/pulp-smash-2018.4.26/pulp_smash/tests/pulp3/file/utils.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""Utilities for tests for the file plugin."""
+from pulp_smash.tests.pulp3 import utils
+
+
+def set_up_module():
+    """Skip tests Pulp 3 isn't under test or if pulp-file isn't installed."""
+    utils.require_pulp_3()
+    utils.require_pulp_plugins({'pulp_file'})
