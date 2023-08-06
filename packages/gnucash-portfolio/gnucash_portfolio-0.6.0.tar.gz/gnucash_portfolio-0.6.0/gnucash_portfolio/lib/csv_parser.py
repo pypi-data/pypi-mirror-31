629cd2c4edc305ae3819463a01281d2eb3e75944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package gnucash-portfolio/0.6.0/gnucash_portfolio-0.6.0.tar.gz

---
+++ packages/gnucash-portfolio/gnucash_portfolio-0.6.0.tar.gz/gnucash_portfolio-0.6.0/gnucash_portfolio/lib/csv_parser.py
@@ -0,0 +1,42 @@
+""" Parse CSV into prices and rates """
+
+from logging import log, DEBUG
+from typing import List
+import csv
+from gnucash_portfolio.model.price_model import PriceModel
+
+
+class CsvPriceParser:
+    """ Parses .csv into list of prices """
+    def __init__(self, currency: str):
+        self.currency = currency
+
+    def parse_prices_from_file_stream(self, file_stream) -> List[PriceModel]:
+        """
+        Reads a file stream (i.e. from web form) containing a csv prices
+        into a list of Price models.
+        """
+        content = file_stream.read().decode("utf-8")
+        file_stream.close()
+
+        if not content:
+            raise ValueError("The file is empty!")
+
+        result = self.get_prices_from_csv(content)
+
+        return result
+
+
+    def get_prices_from_csv(self, content: str) -> List[PriceModel]:
+        """ Imports prices from CSV content. See data folder for a sample file/content. """
+        lines = content.splitlines()
+        prices = []
+
+        reader = csv.reader(lines)
+        for row in reader:
+            price = PriceModel().parse(row)
+            #log(DEBUG, "parsed price. date is %s", price.date)
+            price.currency = self.currency
+
+            prices.append(price)
+        return prices
