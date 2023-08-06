4b543d2a0c163254745868d0018ae9719a41e47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:06 +0000

    Add package print-name-dmt25725/0.1.1/print_name_dmt25725-0.1.1-py2.py3-none-any.whl

---
+++ packages/print-name-dmt25725/print_name_dmt25725-0.1.1-py2.py3-none-any.whl/testdist/__init__.py
@@ -0,0 +1,5 @@
+from printmyname import *
+
+
+def joke():
+    print("A man walked into a bar, and said 'ow'.")
---
+++ packages/print-name-dmt25725/print_name_dmt25725-0.1.1-py2.py3-none-any.whl/testdist/printmyname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+def printname():
+    print("Dave")
+
+if __name__ == "__main__":
+    printname()
