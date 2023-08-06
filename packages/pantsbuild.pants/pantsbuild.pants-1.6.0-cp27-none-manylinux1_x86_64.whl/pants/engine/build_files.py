98cbc13baac0329022082c3523135ea1c8f2e0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:15 +0000

    Add package pantsbuild.pants/1.6.0/pantsbuild.pants-1.6.0-cp27-none-manylinux1_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-1.6.0-cp27-none-manylinux1_x86_64.whl/pants/engine/build_files.py
@@ -0,0 +1,381 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import collections
+from os.path import dirname, join
+
+import six
+
+from pants.base.project_tree import Dir
+from pants.base.specs import (AscendantAddresses, DescendantAddresses, SiblingAddresses,
+                              SingleAddress, Specs)
+from pants.build_graph.address import Address, BuildFileAddress
+from pants.build_graph.address_lookup_error import AddressLookupError
+from pants.engine.addressable import AddressableDescriptor, BuildFileAddresses, TypeConstraintError
+from pants.engine.fs import FilesContent, PathGlobs, Snapshot
+from pants.engine.mapper import AddressFamily, AddressMap, AddressMapper, ResolveError
+from pants.engine.objects import Locatable, SerializableFactory, Validatable
+from pants.engine.rules import RootRule, SingletonRule, TaskRule, rule
+from pants.engine.selectors import Select, SelectDependencies, SelectProjection
+from pants.engine.struct import Struct
+from pants.util.dirutil import fast_relpath_optional
+from pants.util.objects import Collection, datatype
+
+
+class ResolvedTypeMismatchError(ResolveError):
+  """Indicates a resolved object was not of the expected type."""
+
+
+def _key_func(entry):
+  key, value = entry
+  return key
+
+
+class BuildDirs(datatype('BuildDirs', ['dependencies'])):
+  """A list of Stat objects for directories containing build files."""
+
+
+class BuildFiles(datatype('BuildFiles', ['files_content'])):
+  """The FileContents of BUILD files in some directory"""
+
+
+class BuildFileGlobs(datatype('BuildFilesGlobs', ['path_globs'])):
+  """A wrapper around PathGlobs that are known to match a build file pattern."""
+
+
+@rule(BuildFiles,
+      [SelectProjection(FilesContent, PathGlobs, 'path_globs', BuildFileGlobs)])
+def build_files(files_content):
+  return BuildFiles(files_content)
+
+
+@rule(BuildFileGlobs, [Select(AddressMapper), Select(Dir)])
+def buildfile_path_globs_for_dir(address_mapper, directory):
+  patterns = tuple(join(directory.path, p) for p in address_mapper.build_patterns)
+  return BuildFileGlobs(PathGlobs.create('',
+                                         include=patterns,
+                                         exclude=address_mapper.build_ignore_patterns))
+
+
+@rule(AddressFamily, [Select(AddressMapper), Select(Dir), Select(BuildFiles)])
+def parse_address_family(address_mapper, path, build_files):
+  """Given the contents of the build files in one directory, return an AddressFamily.
+
+  The AddressFamily may be empty, but it will not be None.
+  """
+  files_content = build_files.files_content.dependencies
+  if not files_content:
+    raise ResolveError('Directory "{}" does not contain build files.'.format(path))
+  address_maps = []
+  for filecontent_product in files_content:
+    address_maps.append(AddressMap.parse(filecontent_product.path,
+                                         filecontent_product.content,
+                                         address_mapper.parser))
+  return AddressFamily.create(path.path, address_maps)
+
+
+class UnhydratedStruct(datatype('UnhydratedStruct', ['address', 'struct', 'dependencies'])):
+  """A product type that holds a Struct which has not yet been hydrated.
+
+  A Struct counts as "hydrated" when all of its members (which are not themselves dependencies
+  lists) have been resolved from the graph. This means that hydrating a struct is eager in terms
+  of inline addressable fields, but lazy in terms of the complete graph walk represented by
+  the `dependencies` field of StructWithDeps.
+  """
+
+  def __eq__(self, other):
+    if type(self) != type(other):
+      return NotImplemented
+    return self.struct == other.struct
+
+  def __ne__(self, other):
+    return not (self == other)
+
+  def __hash__(self):
+    return hash(self.struct)
+
+
+def _raise_did_you_mean(address_family, name):
+  names = [a.target_name for a in address_family.addressables]
+  possibilities = '\n  '.join(':{}'.format(target_name) for target_name in sorted(names))
+  raise ResolveError('"{}" was not found in namespace "{}". '
+                     'Did you mean one of:\n  {}'
+                     .format(name, address_family.namespace, possibilities))
+
+
+@rule(UnhydratedStruct,
+      [Select(AddressMapper),
+       SelectProjection(AddressFamily, Dir, 'spec_path', Address),
+       Select(Address)])
+def resolve_unhydrated_struct(address_mapper, address_family, address):
+  """Given an Address and its AddressFamily, resolve an UnhydratedStruct.
+
+  Recursively collects any embedded addressables within the Struct, but will not walk into a
+  dependencies field, since those are requested explicitly by tasks using SelectDependencies.
+  """
+
+  struct = address_family.addressables.get(address)
+  addresses = address_family.addressables
+  if not struct or address not in addresses:
+    _raise_did_you_mean(address_family, address.target_name)
+
+  dependencies = []
+  def maybe_append(outer_key, value):
+    if isinstance(value, six.string_types):
+      if outer_key != 'dependencies':
+        dependencies.append(Address.parse(value,
+                                          relative_to=address.spec_path,
+                                          subproject_roots=address_mapper.subproject_roots))
+    elif isinstance(value, Struct):
+      collect_dependencies(value)
+
+  def collect_dependencies(item):
+    for key, value in sorted(item._asdict().items(), key=_key_func):
+      if not AddressableDescriptor.is_addressable(item, key):
+        continue
+      if isinstance(value, collections.MutableMapping):
+        for _, v in sorted(value.items(), key=_key_func):
+          maybe_append(key, v)
+      elif isinstance(value, collections.MutableSequence):
+        for v in value:
+          maybe_append(key, v)
+      else:
+        maybe_append(key, value)
+
+  collect_dependencies(struct)
+
+  return UnhydratedStruct(
+    filter(lambda build_address: build_address == address, addresses)[0], struct, dependencies)
+
+
+def hydrate_struct(address_mapper, unhydrated_struct, dependencies):
+  """Hydrates a Struct from an UnhydratedStruct and its satisfied embedded addressable deps.
+
+  Note that this relies on the guarantee that DependenciesNode provides dependencies in the
+  order they were requested.
+  """
+  address = unhydrated_struct.address
+  struct = unhydrated_struct.struct
+
+  def maybe_consume(outer_key, value):
+    if isinstance(value, six.string_types):
+      if outer_key == 'dependencies':
+        # Don't recurse into the dependencies field of a Struct, since those will be explicitly
+        # requested by tasks. But do ensure that their addresses are absolute, since we're
+        # about to lose the context in which they were declared.
+        value = Address.parse(value,
+                              relative_to=address.spec_path,
+                              subproject_roots=address_mapper.subproject_roots)
+      else:
+        value = dependencies[maybe_consume.idx]
+        maybe_consume.idx += 1
+    elif isinstance(value, Struct):
+      value = consume_dependencies(value)
+    return value
+  # NB: Some pythons throw an UnboundLocalError for `idx` if it is a simple local variable.
+  maybe_consume.idx = 0
+
+  # 'zip' the previously-requested dependencies back together as struct fields.
+  def consume_dependencies(item, args=None):
+    hydrated_args = args or {}
+    for key, value in sorted(item._asdict().items(), key=_key_func):
+      if not AddressableDescriptor.is_addressable(item, key):
+        hydrated_args[key] = value
+        continue
+
+      if isinstance(value, collections.MutableMapping):
+        container_type = type(value)
+        hydrated_args[key] = container_type((k, maybe_consume(key, v))
+                                            for k, v in sorted(value.items(), key=_key_func))
+      elif isinstance(value, collections.MutableSequence):
+        container_type = type(value)
+        hydrated_args[key] = container_type(maybe_consume(key, v) for v in value)
+      else:
+        hydrated_args[key] = maybe_consume(key, value)
+    return _hydrate(type(item), address.spec_path, **hydrated_args)
+
+  return consume_dependencies(struct, args={'address': address})
+
+
+def _hydrate(item_type, spec_path, **kwargs):
+  # If the item will be Locatable, inject the spec_path.
+  if issubclass(item_type, Locatable):
+    kwargs['spec_path'] = spec_path
+
+  try:
+    item = item_type(**kwargs)
+  except TypeConstraintError as e:
+    raise ResolvedTypeMismatchError(e)
+
+  # Let factories replace the hydrated object.
+  if isinstance(item, SerializableFactory):
+    item = item.create()
+
+  # Finally make sure objects that can self-validate get a chance to do so.
+  if isinstance(item, Validatable):
+    item.validate()
+
+  return item
+
+
+@rule(BuildFileAddresses,
+      [Select(AddressMapper),
+       SelectDependencies(AddressFamily, BuildDirs, field_types=(Dir,)),
+       Select(Specs)])
+def addresses_from_address_families(address_mapper, address_families, specs):
+  """Given a list of AddressFamilies matching a list of Specs, return matching Addresses.
+
+  Raises a AddressLookupError if:
+     - there were no matching AddressFamilies, or
+     - the Spec matches no addresses for SingleAddresses.
+  """
+
+  # NB: `@memoized` does not work on local functions.
+  def by_directory():
+    if by_directory.cached is None:
+      by_directory.cached = {af.namespace: af for af in address_families}
+    return by_directory.cached
+  by_directory.cached = None
+
+  def raise_empty_address_family(spec):
+    raise ResolveError('Path "{}" contains no BUILD files.'.format(spec.directory))
+
+  def exclude_address(address):
+    if address_mapper.exclude_patterns:
+      address_str = address.spec
+      return any(p.search(address_str) is not None for p in address_mapper.exclude_patterns)
+    return False
+
+  addresses = []
+  included = set()
+  def include(address_families, predicate=None):
+    matched = False
+    for af in address_families:
+      for a in af.addressables.keys():
+        if not exclude_address(a) and (predicate is None or predicate(a)):
+          matched = True
+          if a not in included:
+            addresses.append(a)
+            included.add(a)
+    return matched
+
+  for spec in specs.dependencies:
+    if type(spec) is DescendantAddresses:
+      matched = include(
+        af
+        for af in address_families
+        if fast_relpath_optional(af.namespace, spec.directory) is not None
+      )
+      if not matched:
+        raise AddressLookupError(
+          'Spec {} does not match any targets.'.format(spec))
+    elif type(spec) is SiblingAddresses:
+      address_family = by_directory().get(spec.directory)
+      if not address_family:
+        raise_empty_address_family(spec)
+      include([address_family])
+    elif type(spec) is SingleAddress:
+      address_family = by_directory().get(spec.directory)
+      if not address_family:
+        raise_empty_address_family(spec)
+      if not include([address_family], predicate=lambda a: a.target_name == spec.name):
+        _raise_did_you_mean(address_family, spec.name)
+    elif type(spec) is AscendantAddresses:
+      include(
+        af
+        for af in address_families
+        if fast_relpath_optional(spec.directory, af.namespace) is not None
+      )
+    else:
+      raise ValueError('Unrecognized Spec type: {}'.format(spec))
+
+  return BuildFileAddresses(addresses)
+
+
+@rule(BuildDirs, [Select(AddressMapper), Select(Snapshot)])
+def filter_build_dirs(address_mapper, snapshot):
+  """Given a Snapshot matching a build pattern, return parent directories as BuildDirs."""
+  dirnames = set(dirname(f.stat.path) for f in snapshot.files)
+  return BuildDirs(tuple(Dir(d) for d in dirnames))
+
+
+@rule(PathGlobs, [Select(AddressMapper), Select(Specs)])
+def spec_to_globs(address_mapper, specs):
+  """Given a Spec object, return a PathGlobs object for the build files that it matches.
+  """
+  patterns = set()
+  for spec in specs.dependencies:
+    if type(spec) is DescendantAddresses:
+      patterns.update(join(spec.directory, '**', pattern)
+                      for pattern in address_mapper.build_patterns)
+    elif type(spec) in (SiblingAddresses, SingleAddress):
+      patterns.update(join(spec.directory, pattern)
+                      for pattern in address_mapper.build_patterns)
+    elif type(spec) is AscendantAddresses:
+      patterns.update(join(f, pattern)
+                      for pattern in address_mapper.build_patterns
+                      for f in _recursive_dirname(spec.directory))
+    else:
+      raise ValueError('Unrecognized Spec type: {}'.format(spec))
+  return PathGlobs.create('', include=patterns, exclude=address_mapper.build_ignore_patterns)
+
+
+def _recursive_dirname(f):
+  """Given a relative path like 'a/b/c/d', yield all ascending path components like:
+
+        'a/b/c/d'
+        'a/b/c'
+        'a/b'
+        'a'
+        ''
+  """
+  while f:
+    yield f
+    f = dirname(f)
+  yield ''
+
+
+BuildFilesCollection = Collection.of(BuildFiles)
+
+
+def create_graph_rules(address_mapper, symbol_table):
+  """Creates tasks used to parse Structs from BUILD files.
+
+  :param address_mapper_key: The subject key for an AddressMapper instance.
+  :param symbol_table: A SymbolTable instance to provide symbols for Address lookups.
+  """
+  symbol_table_constraint = symbol_table.constraint()
+  return [
+    TaskRule(BuildFilesCollection,
+             [SelectDependencies(BuildFiles, BuildDirs, field_types=(Dir,))],
+             BuildFilesCollection),
+    # A singleton to provide the AddressMapper.
+    SingletonRule(AddressMapper, address_mapper),
+    # Support for resolving Structs from Addresses.
+    TaskRule(
+      symbol_table_constraint,
+      [Select(AddressMapper),
+       Select(UnhydratedStruct),
+       SelectDependencies(symbol_table_constraint, UnhydratedStruct, field_types=(Address,))],
+      hydrate_struct
+    ),
+    resolve_unhydrated_struct,
+    # BUILD file parsing.
+    parse_address_family,
+    build_files,
+    buildfile_path_globs_for_dir,
+    # Spec handling: locate directories that contain build files, and request
+    # AddressFamilies for each of them.
+    addresses_from_address_families,
+    filter_build_dirs,
+    spec_to_globs,
+    # Root rules representing parameters that might be provided via root subjects.
+    RootRule(Address),
+    RootRule(BuildFileAddress),
+    RootRule(BuildFileAddresses),
+    RootRule(Specs),
+  ]
---
+++ packages/pantsbuild.pants/pantsbuild.pants-1.6.0-cp27-none-manylinux1_x86_64.whl/pants/engine/selectors.py
@@ -0,0 +1,160 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+from abc import abstractproperty
+
+import six
+
+from pants.engine.addressable import Exactly
+from pants.util.meta import AbstractClass
+from pants.util.objects import datatype
+
+
+def type_or_constraint_repr(constraint):
+  """Generate correct repr for types and TypeConstraints"""
+  if isinstance(constraint, type):
+    return constraint.__name__
+  elif isinstance(constraint, Exactly):
+    return repr(constraint)
+
+
+def constraint_for(type_or_constraint):
+  """Given a type or an `Exactly` constraint, returns an `Exactly` constraint."""
+  if isinstance(type_or_constraint, Exactly):
+    return type_or_constraint
+  elif isinstance(type_or_constraint, type):
+    return Exactly(type_or_constraint)
+  else:
+    raise TypeError("Expected a type or constraint: got: {}".format(type_or_constraint))
+
+
+class Selector(AbstractClass):
+  # The type constraint for the product type for this selector.
+
+  @property
+  def type_constraint(self):
+    return constraint_for(self.product)
+
+  @abstractproperty
+  def optional(self):
+    """Return true if this Selector is optional. It may result in a `None` match."""
+
+  @abstractproperty
+  def product(self):
+    """The product that this selector produces."""
+
+
+class Select(datatype('Select', ['product', 'optional']), Selector):
+  """Selects the given Product for the Subject provided to the constructor.
+
+  If optional=True and no matching product can be produced, will return None.
+  """
+
+  def __new__(cls, product, optional=False):
+    obj = super(Select, cls).__new__(cls, product, optional)
+    return obj
+
+  def __repr__(self):
+    return '{}({}{})'.format(type(self).__name__,
+                             type_or_constraint_repr(self.product),
+                             ', optional=True' if self.optional else '')
+
+
+class SelectVariant(datatype('Variant', ['product', 'variant_key']), Selector):
+  """Selects the matching Product and variant name for the Subject provided to the constructor.
+
+  For example: a SelectVariant with a variant_key of "thrift" and a product of type ApacheThrift
+  will only match when a consumer passes a variant value for "thrift" that matches the name of an
+  ApacheThrift value.
+  """
+  optional = False
+
+  def __new__(cls, product, variant_key):
+    if not isinstance(variant_key, six.string_types):
+      raise ValueError('Expected variant_key to be a string, but was {!r}'.format(variant_key))
+    return super(SelectVariant, cls).__new__(cls, product, variant_key)
+
+  def __repr__(self):
+    return '{}({}, {})'.format(type(self).__name__,
+                               type_or_constraint_repr(self.product),
+                               repr(self.variant_key))
+
+
+class SelectDependencies(datatype('Dependencies', ['product', 'dep_product', 'field', 'field_types']),
+                         Selector):
+  """Selects a product for each of the dependencies of a product for the Subject.
+
+  The dependencies declared on `dep_product` (in the optional `field` parameter, which defaults
+  to 'dependencies' when not specified) will be provided to the requesting task in the
+  order they were declared.
+
+  Field types are used to statically declare the types expected to be contained by the
+  `dep_product`.
+  """
+
+  DEFAULT_FIELD = 'dependencies'
+
+  optional = False
+
+  def __new__(cls, product, dep_product, field=DEFAULT_FIELD, field_types=tuple()):
+    return super(SelectDependencies, cls).__new__(cls, product, dep_product, field, field_types)
+
+  @property
+  def input_product_selector(self):
+    return Select(self.dep_product)
+
+  @property
+  def projected_product_selector(self):
+    return Select(self.product)
+
+  def __repr__(self):
+    if self.field_types:
+      field_types_portion = ', field_types=({},)'.format(', '.join(f.__name__ for f in self.field_types))
+    else:
+      field_types_portion = ''
+    if self.field is not self.DEFAULT_FIELD:
+      field_name_portion = ', {}'.format(repr(self.field))
+    else:
+      field_name_portion = ''
+    return '{}({}, {}{}{})'.format(type(self).__name__,
+                                     type_or_constraint_repr(self.product),
+                                     type_or_constraint_repr(self.dep_product),
+                                     field_name_portion,
+                                     field_types_portion)
+
+
+class SelectProjection(datatype('Projection', ['product', 'projected_subject', 'field', 'input_product']), Selector):
+  """Selects a field of the given Subject to produce a Subject, Product dependency from.
+
+  Projecting an input allows for deduplication in the graph, where multiple Subjects
+  resolve to a single backing Subject instead.
+
+  For convenience, if a single field is requested and it is of the requested type, the field value
+  is projected directly rather than attempting to use it to construct the projected type.
+  """
+  optional = False
+
+  def __new__(cls, product, projected_subject, field, input_product):
+    if not isinstance(field, six.string_types):
+      raise ValueError('Expected `field` to be a string, but was: {!r}'.format(field))
+    return super(SelectProjection, cls).__new__(cls, product, projected_subject, field, input_product)
+
+  @property
+  def input_product_selector(self):
+    return Select(self.input_product)
+
+  @property
+  def projected_product_selector(self):
+    return Select(self.product)
+
+  def __repr__(self):
+    return '{}({}, {}, {}, {})'.format(type(self).__name__,
+                                       type_or_constraint_repr(self.product),
+                                       self.projected_subject.__name__,
+                                       repr(self.field),
+                                       getattr(
+                                         self.input_product, '__name__', repr(self.input_product)))
