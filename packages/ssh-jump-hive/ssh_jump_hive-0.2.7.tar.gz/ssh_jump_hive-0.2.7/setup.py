66a25ce8855d00826df99e86b6fb8ba13d36ae5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package ssh-jump-hive/0.2.7/ssh_jump_hive-0.2.7.tar.gz

---
+++ packages/ssh-jump-hive/ssh_jump_hive-0.2.7.tar.gz/ssh_jump_hive-0.2.7/setup.py
@@ -0,0 +1,56 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+# See this web page for explanations:
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+PACKAGES = ["ssh_jump_hive"]
+KEYWORDS = ["hive", "ssh-tunnel", "hfds", "machine learning"]
+CLASSIFIERS = [
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering",
+]
+# Project root
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="ssh_jump_hive",
+    description="ssh_jump_hive is a tools could  jump the jump machine  to connect hive get hive data to pandas dataframe",
+    license="Apache 2.0",
+    url='https://github.com/mullerhai/sshjumphive',
+    version="0.2.7",
+    author="mullerhai",
+    author_email="hai710459649@foxmail.com",
+    maintainer="muller helen",
+    maintainer_email="hai710459649@foxmail.com",
+    keywords=KEYWORDS,
+    packages=find_packages(),
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'pandas>=0.20.3',
+        'PyHive>=0.5.1',
+        'paramiko>=2.4.1',
+        'selectors>=0.0.14',
+        'sasl>=0.2.1',
+        'thrift>=0.11.0',
+        'thrift-sasl>=0.3.0',
+        'hdfs>=2.1.0',
+        'sklearn-pandas>=1.6.0',
+        'scikit-learn>=0.19.1',
+
+    ],
+)
+
+
