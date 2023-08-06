e5bab27d20e6be1c44b46d3113317f27bb3bc420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:33 +0000

    Add package statsmodels/0.9.0rc1/statsmodels-0.9.0rc1-cp34-cp34m-win_amd64.whl

---
+++ packages/statsmodels/statsmodels-0.9.0rc1-cp34-cp34m-win_amd64.whl/statsmodels/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.9.0rc1'
+version = '0.9.0rc1'
+full_version = '0.9.0rc1'
+git_revision = '40e4f5609560c0fa79fdfec653db7bf0c8c814ef'
+release = True
+
+if not release:
+    version = full_version
