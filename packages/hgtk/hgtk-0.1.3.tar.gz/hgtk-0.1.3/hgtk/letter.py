6c9f16b63c3067d9d9d620db9e1278ded1d35740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:43 +0000

    Add package hgtk/0.1.3/hgtk-0.1.3.tar.gz

---
+++ packages/hgtk/hgtk-0.1.3.tar.gz/hgtk-0.1.3/hgtk/letter.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import division
+
+from .const import CHO, JOONG, JONG, FIRST_HANGUL_UNICODE, NUM_CHO, NUM_JOONG, NUM_JONG, ENG_KOR_SUBSTITUENT
+from .exception import NotHangulException, NotLetterException
+
+from six import unichr
+import string
+
+################################################################################
+# Decomposition & Combination
+################################################################################
+
+
+def compose(chosung, joongsung, jongsung=u''):
+    """This function returns a Hangul letter by composing the specified chosung, joongsung, and jongsung.
+    @param chosung
+    @param joongsung
+    @param jongsung the terminal Hangul letter. This is optional if you do not need a jongsung."""
+
+    if jongsung is None: jongsung = u''
+
+    try:
+        chosung_index = CHO.index(chosung)
+        joongsung_index = JOONG.index(joongsung)
+        jongsung_index = JONG.index(jongsung)
+    except Exception:
+        raise NotHangulException('No valid Hangul character index')
+
+    return unichr(0xAC00 + chosung_index * NUM_JOONG * NUM_JONG + joongsung_index * NUM_JONG + jongsung_index)
+
+
+def hangul_index(letter):
+    return ord(letter) - FIRST_HANGUL_UNICODE
+
+
+def decompose_index(code):
+    jong = int(code % NUM_JONG)
+    code /= NUM_JONG
+    joong = int(code % NUM_JOONG)
+    code /= NUM_JOONG
+    cho = int(code)
+
+    return cho, joong, jong
+
+
+def decompose(hangul_letter):
+    """This function returns letters by decomposing the specified Hangul letter."""
+
+    from . import checker
+
+    if len(hangul_letter) < 1:
+        raise NotLetterException('')
+    elif not checker.is_hangul(hangul_letter):
+        raise NotHangulException('')
+
+    if hangul_letter in CHO:
+        return hangul_letter, '', ''
+
+    if hangul_letter in JOONG:
+        return '', hangul_letter, ''
+
+    if hangul_letter in JONG:
+        return '', '', hangul_letter
+
+    code = hangul_index(hangul_letter)
+    cho, joong, jong = decompose_index(code)
+
+    if cho < 0:
+        cho = 0
+
+    try:
+        return CHO[cho], JOONG[joong], JONG[jong]
+    except:
+        print("%d / %d  / %d"%(cho, joong, jong))
+        print("%s / %s " %( JOONG[joong].encode("utf8"), JONG[jong].encode('utf8')))
+        raise Exception()
+
+def get_substituent_of(letter):
+    return ENG_KOR_SUBSTITUENT.get(letter.upper(), '')
---
+++ packages/hgtk/hgtk-0.1.3.tar.gz/hgtk-0.1.3/hgtk/text.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import division
+
+from .const import CHO, JOONG, JONG
+from . import checker
+from . import letter
+
+# 코딩 효율과 가독성을 위해서 index대신 unicode사용 by bluedisk
+JONG_COMP = {
+    u'ㄱ': {
+        u'ㄱ': u'ㄲ',
+        u'ㅅ': u'ㄳ',
+    },
+    u'ㄴ': {
+        u'ㅈ': u'ㄵ',
+        u'ㅎ': u'ㄶ',
+    },
+    u'ㄹ': {
+        u'ㄱ': u'ㄺ',
+        u'ㅁ': u'ㄻ',
+        u'ㅂ': u'ㄼ',
+        u'ㅅ': u'ㄽ',
+        u'ㅌ': u'ㄾ',
+        u'ㅍ': u'ㄿ',
+        u'ㅎ': u'ㅀ',
+    }
+}
+
+DEFAULT_COMPOSE_CODE = u'ᴥ'
+
+
+################################################################################
+# Hangul Automata functions by bluedisk@gmail.com
+################################################################################
+
+
+def decompose(text, latin_filter=True, compose_code=DEFAULT_COMPOSE_CODE):
+    result=u""
+
+    for c in list(text):
+        if checker.is_hangul(c):
+
+            if checker.is_jamo(c):
+                result = result + c + compose_code
+            else:
+                result = result + "".join(letter.decompose(c)) + compose_code
+
+        else:
+            if latin_filter:    # 한글 외엔 Latin1 범위까지만 포함 (한글+영어)
+                if checker.is_latin1(c):
+                    result = result + c
+            else:
+                result = result + c
+
+    return result
+
+
+STATUS_CHO = 0
+STATUS_JOONG = 1
+STATUS_JONG1 = 2
+STATUS_JONG2 = 3
+
+
+def compose(text, compose_code=DEFAULT_COMPOSE_CODE):
+    res_text = u""
+
+    status = STATUS_CHO
+
+    for c in text:
+
+        if status == STATUS_CHO:
+
+            if c in CHO:
+                chosung = c
+                status = STATUS_JOONG
+            else:
+                if c != compose_code:
+
+                    res_text = res_text + c
+
+        elif status == STATUS_JOONG:
+
+            if c != compose_code and c in JOONG:
+                joongsung = c
+                status = STATUS_JONG1
+            else:
+                res_text = res_text + chosung
+
+                if c in CHO:
+                    chosung = c
+                    status = STATUS_JOONG
+                else:
+                    if c != compose_code:
+
+                        res_text = res_text + c
+                    status = STATUS_CHO
+
+        elif status == STATUS_JONG1:
+
+            if c != compose_code and c in JONG:
+                jongsung = c
+
+                if c in JONG_COMP:
+                    status = STATUS_JONG2
+                else:
+                    res_text = res_text + letter.compose(chosung, joongsung, jongsung)
+                    status = STATUS_CHO
+
+            else:
+                res_text = res_text + letter.compose(chosung, joongsung)
+
+                if c in CHO:
+                    chosung = c
+                    status = STATUS_JOONG
+                else:
+                    if c != compose_code:
+
+                        res_text = res_text + c
+
+                    status = STATUS_CHO
+
+        elif status == STATUS_JONG2:
+
+            if c != compose_code and c in JONG_COMP[jongsung]:
+                jongsung = JONG_COMP[jongsung][c]
+                c = compose_code  # 종성 재 출력 방지
+
+            res_text = res_text + letter.compose(chosung, joongsung, jongsung)
+
+            if c != compose_code:
+
+                res_text = res_text + c
+
+            status = STATUS_CHO
+
+    return res_text
