cb1e8065d3a7917e03e956f59fbd9ad749a790ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:50 +0000

    Add package mllearn/1.2.1/mllearn-1.2.1.tar.gz

---
+++ packages/mllearn/mllearn-1.2.1.tar.gz/mllearn-1.2.1/mllearn/metrics/metrics.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+def _check_targets(y_true, y_pred):
+    if y_true.shape != y_pred:
+        raise ValueError('the shape of y_true is inconsistent with the shape of y_pred')
+
+def _intersection(a, b):
+    """
+    a.shape = (m, )
+    b.shape = (m, )
+    """
+    count = 0
+    for i_a, i_b in zip(a, b):
+        if i_a == 1 and i_b == 1:
+            count += 1
+    return count
+
+def _convergence(a, b):
+    count = 0
+    for i_a, i_b in zip(a, b):
+        if i_a == 1 or i_b == 1:
+            count += 1
+    return count
+
+def _sym_difference(a, b):
+    count = 0
+    for i_a, i_b in zip(a, b):
+        if i_a != i_b:
+            count += 1
+    return count
+
+def subset_acc(y_true, y_pred):
+    _check_targets(y_true, y_pred)
+    sample_num, label_count = y_true.shape
+    acc_count = 0
+    for i in range(sample_num):
+        if (y_true[i] == y_pred[i]).sum() == label_count:
+            acc_count += 1
+    return acc_count / sample_num
+
+def hamming_loss(y_true, y_pred):
+    _check_targets(y_true, y_pred)
+    sample_num, label_count = y_true.shape
+    count = 0
+    for i in range(sample_num):
+        count += _sym_difference(y_true[i], y_pred[i])
+    return count / (sample_num * label_count)
+
+def accuracy(y_true, y_pred):
+    _check_targets(y_true, y_pred)
+    sample_num, label_count = y_true.shape
+    count = 0.
+    for i in range(sample_num):
+        count += _intersection(y_true[i], y_pred[i]) / _convergence(y_true[i], y_pred[i])
+    return count / sample_num
+
+def precision(y_true, y_pred):
+    _check_targets(y_true, y_pred)
+    sample_num, label_count = y_true.shape
+    count = 0.
+    for i in range(sample_num):
+        count += _intersection(y_true[i], y_pred[i]) / y_pred[i].sum()
+    return count / sample_num
+
+def recall(y_true, y_pred):
+    _check_targets(y_true, y_pred)
+    sample_num, label_count = y_true.shape
+    count = 0.
+    for i in range(sample_num):
+        count += _intersection(y_true[i], y_pred[i]) / y_true[i].sum()
+    return count / sample_num
+
+def F_beta(y_true, y_pred, beta=1):
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    return (1+beta**2) * p * r / (beta**2 * p + r)
