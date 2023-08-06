b5551c39e484648cee54413a82bd013a7a7a1657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:23 +0000

    Add package marshmallow-peewee/2.0.0/Marshmallow-Peewee-2.0.0.tar.gz

---
+++ packages/marshmallow-peewee/Marshmallow-Peewee-2.0.0.tar.gz/Marshmallow-Peewee-2.0.0/marshmallow_peewee/__init__.py
@@ -0,0 +1,6 @@
+__license__ = "MIT"
+__project__ = "Marshmallow-Peewee"
+__version__ = "2.0.0"
+
+from .schema import ModelSchema # noqa
+from .fields import Timestamp, MSTimestamp, Related, ForeignKey # noqa
