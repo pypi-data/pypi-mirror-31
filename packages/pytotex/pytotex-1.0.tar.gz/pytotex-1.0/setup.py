d82e9262b9a508bbba3f06e18c646944387135b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:39 +0000

    Add package pytotex/1.0/pytotex-1.0.tar.gz

---
+++ packages/pytotex/pytotex-1.0.tar.gz/pytotex-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = 'pytotex',
+    version = '1.0',
+    url = 'https://github.com/zhafen/py2tex',
+    author = 'Zachary Hafen',
+    author_email = 'zachary.h.hafen@gmail.com',
+)
