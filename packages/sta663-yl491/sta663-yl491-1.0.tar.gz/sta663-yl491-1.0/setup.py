9975a1793b35f4aa58951da8c83c9346013d0fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:50 +0000

    Add package sta663-yl491/1.0/sta663-yl491-1.0.tar.gz

---
+++ packages/sta663-yl491/sta663-yl491-1.0.tar.gz/sta663-yl491-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(name = "sta663-yl491",
+      version = "1.0",
+      author='Yunxuan Li',
+      author_email='yunxuan.li@duke.edu',
+      url='https://github.com/yl491',
+      packages=setuptools.find_packages(),
+      license='MIT'
+      )
