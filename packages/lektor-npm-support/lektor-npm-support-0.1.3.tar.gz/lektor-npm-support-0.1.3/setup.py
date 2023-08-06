3e8134e4f65b35606bcad1edeb25b69174f62df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:41 +0000

    Add package lektor-npm-support/0.1.3/lektor-npm-support-0.1.3.tar.gz

---
+++ packages/lektor-npm-support/lektor-npm-support-0.1.3.tar.gz/lektor-npm-support-0.1.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+tests_require = [
+    'lektor',
+    'pytest',
+    'pytest-cov',
+    'pytest-mock'
+]
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='lektor-npm-support',
+    author=u'Baruch Sterin',
+    author_email='lektor-npm-support@bsterin.com',
+    version='0.1.3',
+    url='http://github.com/sterin/lektor-npm-support',
+    license='BSD',
+    description="Adds support for using npm/yarn to build assets in Lektor",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    py_modules=['lektor_npm_support'],
+    install_requires=['future'],
+    setup_requires=['pytest-runner'],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    entry_points={
+        'lektor.plugins': [
+            'npm-support = lektor_npm_support:NPMSupportPlugin',
+        ]
+    }
+)
