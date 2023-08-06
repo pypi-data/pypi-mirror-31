2b26caf7eaf72a507458edc02d898d8471dfc076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:27 +0000

    Add package gwcelery/0.0.2/gwcelery-0.0.2.tar.gz

---
+++ packages/gwcelery/gwcelery-0.0.2.tar.gz/gwcelery-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import sys
+from setuptools import setup
+
+setup_requires = ['setuptools >= 30.3.0']
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+if {'build_sphinx'}.intersection(sys.argv):
+    setup_requires.extend(['celery_eternal>=0.0.2', 'recommonmark', 'sphinx'])
+
+setup(setup_requires=setup_requires)
