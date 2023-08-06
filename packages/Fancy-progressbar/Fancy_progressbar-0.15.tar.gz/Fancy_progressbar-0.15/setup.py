d06e08246c99cc9495736ab0692b14a076194347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:08 +0000

    Add package Fancy-progressbar/0.15/Fancy_progressbar-0.15.tar.gz

---
+++ packages/Fancy-progressbar/Fancy_progressbar-0.15.tar.gz/Fancy_progressbar-0.15/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+name='Fancy_progressbar',
+version='0.15',
+description='An easy to use multi progressbar manager',
+packages = ['Fancy_progressbar'],
+url='https://github.com/Plawn/Fancy_progressbar',
+license='MIT',
+author='Plawn',
+author_email='plawn.yay@gmail.com',
+
+)
