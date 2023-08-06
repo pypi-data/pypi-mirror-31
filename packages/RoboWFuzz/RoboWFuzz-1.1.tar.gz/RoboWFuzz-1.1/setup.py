090634d8551b9aff3694d9fe3ce95703bcdeebe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:49 +0000

    Add package RoboWFuzz/1.1/RoboWFuzz-1.1.tar.gz

---
+++ packages/RoboWFuzz/RoboWFuzz-1.1.tar.gz/RoboWFuzz-1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='RoboWFuzz',
+    version='1.1',
+    packages=[''],
+    package_dir={'': 'robowfuzz'},
+    url='',
+    license='MIT',
+    author='Abhay Bhargav',
+    install_requires = ['wfuzz','robotframework'],
+    description='RoboWFuzz - Generic Robot Framework Library for the WFuzz Fuzzing Framework. Currently, only a Directory Bruter',
+    long_description = long_description,
+    long_description_content_type='text/markdown'
+)
