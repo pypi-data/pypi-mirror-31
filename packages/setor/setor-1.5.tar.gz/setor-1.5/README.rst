add310f3139df812be14e7330d6669f7a76de5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:15 +0000

    Add package setor/1.5/setor-1.5.tar.gz

---
+++ packages/setor/setor-1.5.tar.gz/setor-1.5/README.rst
@@ -0,0 +1,44 @@
+SETOR: SEO TOR
+====================================
+
+SETOR is a Bot TOR to visit the webpages with unique IP's and random times.
+
+.. image:: https://raw.githubusercontent.com/agusmakmun/setor/master/screenshot/setor.png
+
+
+Install
+------------
+
+SETOR is available directly from `PyPI`_:
+
+::
+
+    $ sudo pip install setor
+
+
+Features
+------------
+
+- Single URL - Visit to single url.
+
+- Multiple URL's - Visit to multiple url's/links.
+
+- Logs *(Rename the log every midnight)* if you need to save as log to ``/var/logs/setor``.
+
+- Already detected by **goo.gl** and **bit.ly**.
+
+
+Usage
+------------
+
+    I think common people know how to use it, especially if you are a Linux User.
+
+
+License
+------------
+
+- `MIT`_
+
+
+.. _PyPI: https://pypi.python.org/pypi/setor
+.. _MIT: https://github.com/agusmakmun/setor/blob/master/LICENSE
---
+++ packages/setor/setor-1.5.tar.gz/setor-1.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+setup(
+    name="setor",
+    packages=['setor'],
+    version="1.5",
+    platforms=['Linux'],
+    url='https://github.com/agusmakmun/setor/',
+    download_url='https://github.com/agusmakmun/setor/tarball/v1.4',
+    description="Bot TOR to visit the webpages with unique IP's and random times.",
+    long_description=open("README.rst").read(),
+    license='MIT',
+    author='Agus Makmun (Summon Agus)',
+    author_email='ags@dracos-linux.id',
+    keywords=['SETOR', 'SEO TOR', 'Bot TOR'],
+    entry_points={
+        'console_scripts': ['setor=setor.setor:main',],
+    },
+    install_requires=['PySocks==1.5.7'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Networking'
+    ]
+)
