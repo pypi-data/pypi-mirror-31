62f9cb85f2981b4f76358264b181a4424611b121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:39 +0000

    Add package alfa/0.21/ALFA-0.21.tar.gz

---
+++ packages/alfa/ALFA-0.21.tar.gz/ALFA-0.21/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'ALFA',
+      py_modules = ['ALFA'],
+      version = '0.21',
+      description = 'A simple software to get a quick overview of features composing NGS dataset(s).',
+      author = 'Mathieu Bahin',
+      author_email = 'mathieu.bahin@biologie.ens.fr',
+      maintainer = 'Mathieu Bahin',
+      maintainer_email = 'mathieu.bahin@biologie.ens.fr',
+      url = 'https://github.com/biocompibens/ALFA',
+      long_description = 'README',
+      license = 'MIT',
+      scripts = ['ALFA.py'],
+      install_requires = [
+        're==2.2.1',
+        'numpy==1.13.3',
+        'argparse==1.1',
+        'matplotlib==2.1.1',
+        'multiprocessing==0.70a1',
+        'pysam==0.13',
+        'pybedtools==0.7.10',
+        'progressbar==2.3']
+)
