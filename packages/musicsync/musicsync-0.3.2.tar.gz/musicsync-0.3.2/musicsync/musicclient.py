fdf23a30713a8b0e1238a7885716ad6dbc655451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:33 +0000

    Add package musicsync/0.3.2/musicsync-0.3.2.tar.gz

---
+++ packages/musicsync/musicsync-0.3.2.tar.gz/musicsync-0.3.2/musicsync/musicclient.py
@@ -0,0 +1,43 @@
+from .auth import GPMClientAuth, SpotifyClientAuth
+from .gpm import GPM
+from .spotify import Spotify
+
+
+class MusicClient:
+    _client = None
+
+    def get_playlist(self, name):
+        '''
+        Retrieve a playlist from the music client
+
+        :param str name: The name of the playlist to retrieve
+        :return: The playlist
+        :rtype: Playlist serializer object
+        '''
+        return self._client.get_playlist(name)
+
+    def create_playlist(self, playlist, override_existing=True):
+        '''
+        Create a playlist from the music client
+
+        :param playlist: The playlist to be created
+        :type playlist: Playlist serializer object
+        :param bool override_existing: Delete playlist if it already exists
+        :return: Number of songs matched
+        :rtype: Int
+        '''
+        return self._client.create_playlist(playlist, override_existing)
+
+
+class SpotifyClient(MusicClient):
+
+    def __init__(self, auth=SpotifyClientAuth()):
+        self._client = Spotify(auth)
+        super()
+
+
+class GPMClient(MusicClient):
+
+    def __init__(self, auth=GPMClientAuth()):
+        self._client = GPM(auth)
+        super()
