231e5c084cba9bc54d02c63a8a4f4c633dfb9e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:57 +0000

    Add package sqlwhat/2.2.1/sqlwhat-2.2.1.tar.gz

---
+++ packages/sqlwhat/sqlwhat-2.2.1.tar.gz/sqlwhat-2.2.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('sqlwhat/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+	name='sqlwhat',
+	version=version,
+	packages=['sqlwhat', 'sqlwhat.checks'],
+	install_requires=['protowhat', 'antlr-plsql>=0.1.0', 'antlr-tsql>=0.1.0'],
+        description = 'Submission correctness tests for sql',
+        author = 'Michael Chow',
+        author_email = 'michael@datacamp.com',
+        url = 'https://github.com/datacamp/sqlwhat')
