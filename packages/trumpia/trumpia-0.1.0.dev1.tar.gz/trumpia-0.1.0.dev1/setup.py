391221ae360f3d26603785c20ec179d64ac6dd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:48 +0000

    Add package trumpia/0.1.0.dev1/trumpia-0.1.0.dev1.tar.gz

---
+++ packages/trumpia/trumpia-0.1.0.dev1.tar.gz/trumpia-0.1.0.dev1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(name='trumpia',
+      packages=['trumpia'],
+      version='0.1.0.dev1',
+      description='Python wrapper for Trumpia API',
+      license='MIT',
+      author='Alex Luis Arias',
+      author_email='alex@alexarias.io',
+      url='http://github.com/aleksarias/trumpia-python',
+      keywords=['trumpia', 'api', 'python'],
+      setup_requires=['setuptools-git-version'],
+      classifiers=[],
+      zip_safe=False)
