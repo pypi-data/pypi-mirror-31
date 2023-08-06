c4fa6035226297b1193b3c3d06218c2ffdaf3f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:26 +0000

    Add package whata4445re23-nish-ab-i/1.0.0/whata4445re23_nish_ab___i-1.0.0.tar.gz

---
+++ packages/whata4445re23-nish-ab-i/whata4445re23_nish_ab___i-1.0.0.tar.gz/whata4445re23_nish_ab___i-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+		name		 = "whata4445re23_nish_ab___i",
+		version		 = "1.0.0",
+		py_modules	 = ["zyyexpone"],
+		author		 = "aqr_vaan",
+		author_email = "",
+		url			 = "https://upload.pypi.org/legacy/",
+		description  = "A simple printer of nested lists",
+		license      = 'MIT'
+
+	)
