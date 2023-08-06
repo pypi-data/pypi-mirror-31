ec2d9fa5173070dab24b8b2c728269a770cf86ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:43 +0000

    Add package djangocms-hubspot-blog/0.2.3/djangocms-hubspot-blog-0.2.3.tar.gz

---
+++ packages/djangocms-hubspot-blog/djangocms-hubspot-blog-0.2.3.tar.gz/djangocms-hubspot-blog-0.2.3/djangocms_hubspot_blog/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.apps import AppConfig
+
+
+class HubspotBlogConfig(AppConfig):
+    name = 'djangocms_hubspot_blog'
