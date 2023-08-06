3f82641da99daea8f91bf4ec3cf7f4b44c2b5dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:57 +0000

    Add package celer/0.1.1/celer-0.1.1.tar.gz

---
+++ packages/celer/celer-0.1.1.tar.gz/celer-0.1.1/celer/homotopy.py
@@ -0,0 +1,132 @@
+# Author: Mathurin Massias <mathurin.massias@gmail.com>
+#         Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Joseph Salmon <joseph.salmon@telecom-paristech.fr>
+# License: BSD 3 clause
+
+import time
+import numpy as np
+
+from sklearn.utils import check_array
+
+from .wrapper import celer
+
+
+def celer_path(X, y, eps=1e-3, n_alphas=100, alphas=None, max_iter=20,
+               gap_freq=10, max_epochs=50000, p0=10, verbose=0,
+               verbose_inner=0, tol=1e-6, prune=0, return_thetas=False):
+    """Compute Lasso path with Celer as inner solver.
+
+    Parameters
+    ----------
+    X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        Training data. Pass directly as Fortran-contiguous data or column
+        sparse format (CSC) to avoid unnecessary memory duplication.
+
+    y : ndarray, shape (n_samples,)
+        Target values
+
+    eps : float, optional
+        Length of the path. ``eps=1e-3`` means that
+        ``alpha_min = 1e-3 * alpha_max``
+
+    n_alphas : int, optional
+        Number of alphas along the regularization path
+
+    alphas : ndarray, optional
+        List of alphas where to compute the models.
+        If ``None`` alphas are set automatically
+
+    max_iter : int, optional
+        The maximum number of iterations (subproblem definitions)
+
+    gap_freq : int, optional
+        Number of coordinate descent epochs between each duality gap
+        computations.
+
+    max_epochs : int, optional
+        Maximum number of CD epochs on each subproblem.
+
+    p0 : int, optional
+        First working set size.
+
+    verbose : bool or integer, optional
+        Amount of verbosity.
+
+    verbose_inner : bool or integer
+        Amount of verbosity in the inner solver.
+
+    tol : float, optional
+        The tolerance for the optimization: the solver runs until the duality
+        gap is smaller than ``tol`` or the maximum number of iteration is
+        reached.
+
+    prune : 0 | 1, optional
+        Whether or not to use pruning when growing working sets.
+
+    return_thetas : bool, optional
+        If True, dual variables along the path are returned.
+
+    Returns
+    -------
+    alpha : array, shape (n,_alphas,)
+        The alphas along the path where models are computed.
+
+    coefs : array, shape (n_features, n_alphas)
+        Coefficients along the path.
+
+    dual_gaps : array, shape (n_alphas,)
+        Duality gaps returned by the solver along the path.
+
+    thetas : array, shape (n_alphas, n_samples)
+        The dual variables along the path.
+        (Is returned only when ``return_thetas`` is set to True).
+    """
+
+    X = check_array(X, 'csc', dtype=[np.float64, np.float32],
+                    order='F', copy=False)
+    y = check_array(y, 'csc', dtype=X.dtype.type, order='F', copy=False,
+                    ensure_2d=False)
+
+    n_samples, n_features = X.shape
+    if alphas is None:
+        alpha_max = np.max(np.abs(X.T.dot(y))) / n_samples
+        alphas = alpha_max * np.logspace(0, np.log10(eps), n_alphas,
+                                         dtype=X.dtype)
+    else:
+        alphas = np.sort(alphas)[::-1]
+
+    n_alphas = len(alphas)
+
+    coefs = np.zeros((n_features, n_alphas), order='F', dtype=X.dtype)
+    thetas = np.zeros((n_alphas, n_samples), dtype=X.dtype)
+    dual_gaps = np.zeros(n_alphas)
+    all_times = np.zeros(n_alphas)
+
+    # do not skip alphas[0], it is not always alpha_max
+    for t in range(n_alphas):
+        if verbose:
+            print("#" * 60)
+            print(" ##### Computing %dth alpha" % (t + 1))
+            print("#" * 60)
+        if t > 0:
+            beta_init = coefs[:, t - 1].copy()
+            p_t = max(len(np.where(beta_init != 0)[0]), 1)
+        else:
+            beta_init = coefs[:, t].copy()
+            p_t = 10
+
+        alpha = alphas[t]
+        t0 = time.time()
+        sol = celer(X, y, alpha,
+                    beta_init, max_iter=max_iter, gap_freq=gap_freq,
+                    max_epochs=max_epochs, p0=p_t,
+                    verbose=verbose, verbose_inner=verbose_inner,
+                    tol=tol, prune=prune)
+
+        all_times[t] = time.time() - t0
+        coefs[:, t], thetas[t], dual_gaps[t] = sol[0], sol[1], sol[2][-1]
+
+    if return_thetas:
+        return alphas, coefs, dual_gaps, thetas
+    else:
+        return alphas, coefs, dual_gaps
---
+++ packages/celer/celer-0.1.1.tar.gz/celer-0.1.1/celer/wrapper.py
@@ -0,0 +1,119 @@
+# Author: Mathurin Massias <mathurin.massias@gmail.com>
+#         Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Joseph Salmon <joseph.salmon@telecom-paristech.fr>
+# License: BSD 3 clause
+
+import numpy as np
+import warnings
+
+from scipy import sparse
+from sklearn.exceptions import ConvergenceWarning
+
+from .sparse import celer_sparse
+from .dense import celer_dense
+
+
+def celer(X, y, alpha, beta_init=None, max_iter=100, gap_freq=10,
+          max_epochs=50000, p0=10, verbose=1, verbose_inner=0,
+          tol=1e-6, prune=1):
+    """
+    Compute the Lasso solution with the Celer algorithm.
+
+    The minimized objective function is::
+
+        ||y - X beta||_2^2 / (2 * n_samples) + alpha * ||beta||_1
+
+    Parameters
+    ----------
+    X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        Training data. Pass directly as Fortran-contiguous data or column
+        sparse format (CSC) to avoid unnecessary memory duplication.
+
+    y : array-like, shape (n_samples,)
+        Observation vector.
+
+    alpha : float
+        Value of the Lasso regularization parameter.
+
+    beta_init : array-like, shape (n_features,), optional
+        Initial value for the coefficients vector.
+
+    max_iter : int, optional
+        Maximum number of outer loop (working set definition) iterations.
+
+    max_epochs : int, optional
+        Maximum number of epochs for the coordinate descent solver called on
+        the subproblems.
+
+    gap_freq : int, optional
+        Number of epochs between every gap computation in the inner solver.
+
+    p0 : int, optional
+        Size of the first working set.
+
+    verbose : (0, 1), optional
+        Verbosity level of the outer loop.
+
+    verbose_inner : (0, 1), optional
+        Verbosity level of the inner solver.
+
+    tol : float, optional
+        Optimization tolerance: the solver stops when the duality gap goes
+        below ``tol`` or the maximum number of iteration is reached.
+
+    prune : (0, 1), optional
+        Whether or not to use pruning when growing the working sets.
+
+    Returns
+    -------
+    beta : array, shape (n_features,)
+        Estimated coefficient vector.
+
+    theta : array, shape (n_samples,)
+        Dual point (potentially accelerated) when the solver exits.
+
+    gaps : array
+        Duality gap at each outer loop iteration.
+
+    times : array
+        Time elapsed since entering the solver, at each outer loop iteration.
+    """
+    data_is_sparse = sparse.issparse(X)
+    if not data_is_sparse:
+        if not np.isfortran(X):
+            X = np.asfortranarray(X)
+    else:
+        if X.getformat() != 'csc':
+            raise TypeError("Sparse X must be in column sparse format.")
+        if not X.has_sorted_indices:
+            X.sort_indices()
+
+    n_features = X.shape[1]
+    if beta_init is None:
+        beta_init = np.zeros(n_features)
+
+    if data_is_sparse:
+        sol = celer_sparse(X.data, X.indices, X.indptr, y, alpha,
+                           beta_init, max_iter=max_iter, gap_freq=gap_freq,
+                           max_epochs=max_epochs, p0=p0,
+                           verbose=verbose,
+                           verbose_inner=verbose_inner,
+                           use_accel=1, tol=tol, prune=prune)
+    else:
+        sol = celer_dense(X, y, alpha,
+                          beta_init, max_iter=max_iter, gap_freq=gap_freq,
+                          max_epochs=max_epochs, p0=p0,
+                          verbose=verbose,
+                          verbose_inner=verbose_inner,
+                          use_accel=1, tol=tol, prune=prune)
+
+    final_gap = sol[2][-1]
+    if final_gap > tol:
+        warnings.warn('Objective did not converge.' +
+                      ' You might want' +
+                      ' to increase the number of iterations.' +
+                      ' Fitting data with very small alpha' +
+                      ' may cause precision problems.',
+                      ConvergenceWarning)
+
+    return sol
