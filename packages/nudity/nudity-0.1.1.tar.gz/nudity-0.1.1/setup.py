28494f75bbfc16ad8eba39c07de95ed693e90499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:16 +0000

    Add package nudity/0.1.1/nudity-0.1.1.tar.gz

---
+++ packages/nudity/nudity-0.1.1.tar.gz/nudity-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+
+setup(
+    name='nudity',
+    version='0.1.1',
+    description="Nudity detection with re-trained Tensorflow MobileNet Model",
+    long_description=open('README.rst').read(),
+    author='Can Aydogan',
+    author_email='canaydogan89@gmail.com',
+    url='https://github.com/canaydogan/nudity',
+    license='MIT',
+    py_modules=['nudity'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords="nude, nudity, detection, pornographic, inappropriate content",
+    install_requires=['tensorflow'],
+    python_requires='>=3',
+    entry_points={'console_scripts': ['nudity = nudity:main']}
+)
