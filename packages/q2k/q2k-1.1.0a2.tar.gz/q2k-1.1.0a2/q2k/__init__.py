c632f015e0bf46a4d3af2dc4f48b4b79ed83b253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package q2k/1.1.0a2/q2k-1.1.0a2.tar.gz

---
+++ packages/q2k/q2k-1.1.0a2.tar.gz/q2k-1.1.0a2/q2k/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2018 2Cas
+# Licensed under the MIT license (http://opensource.org/licenses/MIT)
+
+from q2k.reference import *
+from q2k.core import *
+from q2k.gui import *
+from q2k.version import *
