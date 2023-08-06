5b8dbd4f5a729d8ac0b1a6294d8cb161a3d7bc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:26 +0000

    Add package pfxbrick/0.6.2/pfxbrick-0.6.2-py3.6.egg

---
+++ packages/pfxbrick/pfxbrick-0.6.2-py3.6.egg/pfxbrick/__init__.py
@@ -0,0 +1,16 @@
+"""Package for PFx Brick"""
+
+import os
+
+__project__ = 'pfxbrick'
+__version__ = '0.6.2'
+
+VERSION = __project__ + '-' + __version__
+
+script_dir = os.path.dirname(__file__)
+
+from .pfxbrick import PFxBrick, find_bricks
+from .pfxaction import PFxAction
+from .pfxconfig import PFxConfig
+from .pfxfiles import PFxFile, PFxDir
+
