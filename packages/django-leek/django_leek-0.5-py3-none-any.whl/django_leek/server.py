b76d32675626c2e714557d4f4657bac17ee32790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:42 +0000

    Add package django-leek/0.5/django_leek-0.5-py3-none-any.whl

---
+++ packages/django-leek/django_leek-0.5-py3-none-any.whl/django_leek/server.py
@@ -0,0 +1,42 @@
+import socketserver
+import threading
+
+from . import worker_manager
+
+
+Dcommands = {
+    'ping': worker_manager.ping,
+    'waiting': worker_manager.waiting,
+    'handled': worker_manager.hanled,
+    'stop': worker_manager.stop
+}
+
+
+class TaskSocketServer(socketserver.BaseRequestHandler):
+    def handle(self):
+        try:
+            data = self.request.recv(5000).strip() #like the pickled task field
+        except Exception as e:
+            response = (False, "SocketServer: {}".format(e).encode())
+            self.request.send(response)
+
+        if data in Dcommands.keys():
+            try:
+                worker_response = Dcommands[data]()
+                if worker_response == 'Worker Off':
+                    response = (False, worker_response.encode())
+                else:
+                    response = (True, worker_response.encode(),)
+            except Exception as e:
+                response =  (False, "TaskServer Command: {}".format(e).encode(),)
+        else:
+            try:
+                worker_response = worker_manager.put_task(data) #a tuple
+                response = worker_response
+            except Exception as e:
+                response =  (False, "TaskServer Put: {}".format(e).encode(),)
+
+        try:
+            self.request.send(str(response))
+        except Exception as e:
+            self.request.send("SocketServer Response: {}".format(e).encode())
