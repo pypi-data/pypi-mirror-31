9cfbf6bb551177fafef25ae269be9369753d6e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:27 +0000

    Add package pytyphoon/1.5.2/pytyphoon-1.5.2.tar.gz

---
+++ packages/pytyphoon/pytyphoon-1.5.2.tar.gz/pytyphoon-1.5.2/typhoon/tasks/base.py
@@ -0,0 +1,19 @@
+import time
+import json
+
+
+class BaseTask:
+    def __init__(self, config, task, type_, queue_name=None):
+
+        self.type = type_
+        self.config = config
+        self.task = task
+        self.queue_name = queue_name
+        self.added_at = time.time()
+        self.created_at = time.time()
+
+    def serialize(self):
+        return json.dumps(self.task)
+
+    def set_timestamp(self):
+        self.added_at = time.time()
