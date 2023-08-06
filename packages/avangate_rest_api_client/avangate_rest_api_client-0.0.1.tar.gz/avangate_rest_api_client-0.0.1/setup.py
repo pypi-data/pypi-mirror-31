72ed25ac87c296ef7e2db84c9fd9e7078b1aba11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:20 +0000

    Add package avangate_rest_api_client/0.0.1/avangate_rest_api_client-0.0.1.tar.gz

---
+++ packages/avangate_rest_api_client/avangate_rest_api_client-0.0.1.tar.gz/avangate_rest_api_client-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='avangate_rest_api_client',
+    version='0.0.1',
+    packages=['avangate_rest_api_client'],
+    url='https://github.com/onursa/avangate_rest_api_client',
+    license='MIT',
+    author='Onur Salgit',
+    author_email='mail@onur.org',
+    description='Python Client for Avangate REST API',
+    install_requires=["requests"]
+)
