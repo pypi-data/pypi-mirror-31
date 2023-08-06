950ed4b18fb131df463321c1965efa11adbcbd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:44 +0000

    Add package docassemble/0.2.52/docassemble-0.2.52.tar.gz

---
+++ packages/docassemble/docassemble-0.2.52.tar.gz/docassemble-0.2.52/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+setup(name='docassemble',
+      version='0.2.52',
+      description=('A system for assembling documents from templates while automatically querying a user for necessary information.'),
+      author='Jonathan Pyle',
+      author_email='jhpyle@gmail.com',
+      license='MIT',
+      url='https://docassemble.org',
+      download_url='https://download.docassemble.org/docassemble.tar.gz',
+      packages=find_packages(),
+      zip_safe = False,
+     )
