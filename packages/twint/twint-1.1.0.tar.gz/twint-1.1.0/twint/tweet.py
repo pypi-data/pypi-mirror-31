f27c32ea9076553bd68c63fb8ba3de9b32487b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package twint/1.1.0/twint-1.1.0.tar.gz

---
+++ packages/twint/twint-1.1.0.tar.gz/twint-1.1.0/twint/tweet.py
@@ -0,0 +1,15 @@
+class Tweet:
+	id = ""
+	date = ""
+	datestamp = ""
+	time = ""
+	timestamp = ""
+	timezone = ""
+	user_id = ""
+	username = ""
+	tweet = "" # Text
+	replies = "0"
+	likes = "0"
+	retweets = "0"
+	hashtags = ""
+	location = ""
