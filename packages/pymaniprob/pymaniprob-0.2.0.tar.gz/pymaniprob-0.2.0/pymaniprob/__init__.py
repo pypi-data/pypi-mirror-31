0dee243e427e833ab1efb285a68f5db84169fd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package pymaniprob/0.2.0/pymaniprob-0.2.0.tar.gz

---
+++ packages/pymaniprob/pymaniprob-0.2.0.tar.gz/pymaniprob-0.2.0/pymaniprob/__init__.py
@@ -0,0 +1,6 @@
+from . import probabilitydistributions
+from . import sphere
+
+def message():
+    print(VonMisesFisher)
+    return "Hello, world!"
---
+++ packages/pymaniprob/pymaniprob-0.2.0.tar.gz/pymaniprob-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymaniprob',
+      version='0.2.0',
+      author='Daniel Tait',
+      author_email='tait.djk@gmail.com',
+      url='http://github.com/danieljtait/pymaniprob',
+      license='MIT',
+      packages=['pymaniprob'],
+      install_requires=['numpydoc', 'numpy', 'matplotlib', 'scipy'],
+      zip_safe=False)
