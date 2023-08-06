6ad6b620e38d9255b48ef896b84c945916cec182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:38 +0000

    Add package dotrunner/0.1.5/dotrunner-0.1.5.tar.gz

---
+++ packages/dotrunner/dotrunner-0.1.5.tar.gz/dotrunner-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from version import VERSION
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='dotrunner',
+    version=VERSION,
+    description='Links dotfiles',
+    long_description=readme,
+    author='Dotan Nahum',
+    data_files=[('', ['LICENSE', 'README.md', 'version.py'])],
+    author_email='jondotan@gmail.com',
+    url='https://github.com/jondot/dotrunner',
+    license=license,
+    py_modules=['dotrunner'],
+    entry_points='''
+        [console_scripts]
+        dotrunner=dotrunner:main
+    ''',
+    # packages=find_packages(exclude=('tests', 'docs', 'jest-pytest')),
+    install_requires=[
+        'toolz', 'docopt', 'networkx', 'pyyaml', 'delegator.py', 'colorama'
+    ])
