f3c9885ddb69d5258e3f98a08db6b57785b9c1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package poliastro/0.9.0/poliastro-0.9.0-py3-none-any.whl

---
+++ packages/poliastro/poliastro-0.9.0-py3-none-any.whl/poliastro/twobody/angles.py
@@ -0,0 +1,285 @@
+"""Angles and anomalies.
+
+"""
+import numpy as np
+
+from astropy import units as u
+
+from scipy import optimize
+
+
+def _kepler_equation(E, M, ecc):
+    return E - ecc * np.sin(E) - M
+
+
+def _kepler_equation_prime(E, M, ecc):
+    return 1 - ecc * np.cos(E)
+
+
+def _kepler_equation_hyper(F, M, ecc):
+    return -F + ecc * np.sinh(F) - M
+
+
+def _kepler_equation_prime_hyper(F, M, ecc):
+    return ecc * np.cosh(F) - 1
+
+
+def nu_to_E(nu, ecc):
+    """Eccentric anomaly from true anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    nu : float
+        True anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    E : float
+        Eccentric anomaly.
+
+    """
+    E = 2 * np.arctan(np.sqrt((1 - ecc) / (1 + ecc)) * np.tan(nu / 2))
+    return E
+
+
+def nu_to_F(nu, ecc):
+    """Hyperbolic eccentric anomaly from true anomaly.
+
+    Parameters
+    ----------
+    nu : float
+        True anomaly (rad).
+    ecc : float
+        Eccentricity (>1).
+
+    Returns
+    -------
+    F : float
+        Hyperbolic eccentric anomaly.
+
+    Note
+    -----
+    Taken from Curtis, H. (2013). *Orbital mechanics for engineering students*. 167
+
+    """
+    F = np.log((np.sqrt(ecc + 1) + np.sqrt(ecc - 1) * np.tan(nu / 2)) /
+               (np.sqrt(ecc + 1) - np.sqrt(ecc - 1) * np.tan(nu / 2))) * u.rad
+    return F
+
+
+def E_to_nu(E, ecc):
+    """True anomaly from eccentric anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    E : float
+        Eccentric anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    nu : float
+        True anomaly (rad).
+
+    """
+    nu = 2 * np.arctan(np.sqrt((1 + ecc) / (1 - ecc)) * np.tan(E / 2))
+    return nu
+
+
+def F_to_nu(F, ecc):
+    """True anomaly from hyperbolic eccentric anomaly.
+
+    Parameters
+    ----------
+    F : float
+        Hyperbolic eccentric anomaly (rad).
+    ecc : float
+        Eccentricity (>1).
+
+    Returns
+    -------
+    nu : float
+        True anomaly (rad).
+
+    """
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        nu = 2 * np.arctan((np.exp(F) * np.sqrt(ecc + 1) - np.sqrt(ecc + 1)) /
+                           (np.exp(F) * np.sqrt(ecc - 1) + np.sqrt(ecc - 1)))
+    return nu
+
+
+def M_to_E(M, ecc):
+    """Eccentric anomaly from mean anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    M : float
+        Mean anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    E : float
+        Eccentric anomaly.
+
+    """
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        E = optimize.newton(_kepler_equation, M, _kepler_equation_prime,
+                            args=(M, ecc))
+    return E
+
+
+def M_to_F(M, ecc):
+    """Hyperbolic eccentric anomaly from mean anomaly.
+
+    Parameters
+    ----------
+    M : float
+        Mean anomaly (rad).
+    ecc : float
+        Eccentricity (>1).
+
+    Returns
+    -------
+    F : float
+        Hyperbolic eccentric anomaly.
+
+    """
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        F = optimize.newton(_kepler_equation_hyper, np.arcsinh(M / ecc), _kepler_equation_prime_hyper,
+                            args=(M, ecc), maxiter=100)
+    return F
+
+
+def E_to_M(E, ecc):
+    """Mean anomaly from eccentric anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    E : float
+        Eccentric anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    M : float
+        Mean anomaly (rad).
+
+    """
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        M = _kepler_equation(E, 0.0 * u.rad, ecc)
+    return M
+
+
+def F_to_M(F, ecc):
+    """Mean anomaly from eccentric anomaly.
+
+    Parameters
+    ----------
+    F : float
+        Hyperbolic eccentric anomaly (rad).
+    ecc : float
+        Eccentricity (>1).
+
+    Returns
+    -------
+    M : float
+        Mean anomaly (rad).
+
+    """
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        M = _kepler_equation_hyper(F, 0.0 * u.rad, ecc)
+    return M
+
+
+def M_to_nu(M, ecc):
+    """True anomaly from mean anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    M : float
+        Mean anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    nu : float
+        True anomaly (rad).
+
+    Examples
+    --------
+    >>> nu = M_to_nu(np.radians(30.0), 0.06)
+    >>> np.rad2deg(nu)
+    33.673284930211658
+
+    """
+    if ecc > 1:
+        F = M_to_F(M, ecc)
+        nu = F_to_nu(F, ecc)
+    else:
+        E = M_to_E(M, ecc)
+        nu = E_to_nu(E, ecc)
+    return nu
+
+
+def nu_to_M(nu, ecc):
+    """Mean anomaly from true anomaly.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    nu : float
+        True anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Returns
+    -------
+    M : float
+        Mean anomaly (rad).
+
+    """
+    if ecc > 1:
+        F = nu_to_F(nu, ecc)
+        M = F_to_M(F, ecc)
+    else:
+        E = nu_to_E(nu, ecc)
+        M = E_to_M(E, ecc)
+    return M
+
+
+def fp_angle(nu, ecc):
+    """Flight path angle.
+
+    .. versionadded:: 0.4.0
+
+    Parameters
+    ----------
+    nu : float
+        True anomaly (rad).
+    ecc : float
+        Eccentricity.
+
+    Note
+    -----
+    Algorithm taken from Vallado 2007, pp. 113.
+
+    """
+    return np.arctan2(ecc * np.sin(nu), 1 + ecc * np.cos(nu))
---
+++ packages/poliastro/poliastro-0.9.0-py3-none-any.whl/poliastro/twobody/classical.py
@@ -0,0 +1,181 @@
+"""Functions to define orbits from classical orbital elements.
+
+"""
+import numpy as np
+from numpy import sin, cos, sqrt
+from astropy import units as u
+
+from poliastro.util import transform
+
+from poliastro.twobody import rv
+from poliastro.twobody import equinoctial
+
+from ._base import BaseState
+
+
+def rv_pqw(k, p, ecc, nu):
+    """Returns r and v vectors in perifocal frame.
+
+    """
+    r_pqw = (np.array([cos(nu), sin(nu), 0 * nu]) * p / (1 + ecc * cos(nu))).T
+    v_pqw = (np.array([-sin(nu), (ecc + cos(nu)), 0]) * sqrt(k / p)).T
+    return r_pqw, v_pqw
+
+
+def coe2rv(k, p, ecc, inc, raan, argp, nu):
+    """Converts from classical orbital elements to vectors.
+
+    Parameters
+    ----------
+    k : float
+        Standard gravitational parameter (km^3 / s^2).
+    p : float
+        Semi-latus rectum or parameter (km).
+    ecc : float
+        Eccentricity.
+    inc : float
+        Inclination (rad).
+    omega : float
+        Longitude of ascending node (rad).
+    argp : float
+        Argument of perigee (rad).
+    nu : float
+        True anomaly (rad).
+
+    """
+    r_pqw, v_pqw = rv_pqw(k, p, ecc, nu)
+
+    r_ijk = transform(r_pqw, -argp, 'z', u.rad)
+    r_ijk = transform(r_ijk, -inc, 'x', u.rad)
+    r_ijk = transform(r_ijk, -raan, 'z', u.rad)
+    v_ijk = transform(v_pqw, -argp, 'z', u.rad)
+    v_ijk = transform(v_ijk, -inc, 'x', u.rad)
+    v_ijk = transform(v_ijk, -raan, 'z', u.rad)
+
+    return r_ijk, v_ijk
+
+
+def coe2mee(p, ecc, inc, raan, argp, nu):
+    """Converts from classical orbital elements to modified equinoctial
+    orbital elements.
+
+    The definition of the modified equinoctial orbital elements is taken from
+    [Walker, 1985].
+
+    Parameters
+    ----------
+    k : float
+        Standard gravitational parameter (km^3 / s^2).
+    p : float
+        Semi-latus rectum or parameter (km).
+    ecc : float
+        Eccentricity.
+    inc : float
+        Inclination (rad).
+    omega : float
+        Longitude of ascending node (rad).
+    argp : float
+        Argument of perigee (rad).
+    nu : float
+        True anomaly (rad).
+
+    Note
+    -----
+    The conversion equations are taken directly from the original paper.
+
+    """
+    lonper = raan + argp
+    f = ecc * np.cos(lonper)
+    g = ecc * np.sin(lonper)
+    # TODO: Check polar case (see [Walker, 1985])
+    h = np.tan(inc / 2) * np.cos(raan)
+    k = np.tan(inc / 2) * np.sin(raan)
+    L = lonper + nu
+    return p, f, g, h, k, L
+
+
+class ClassicalState(BaseState):
+    """State defined by its classical orbital elements.
+
+    """
+    def __init__(self, attractor, a, ecc, inc, raan, argp, nu):
+        super(ClassicalState, self).__init__(attractor)
+        self._a = a
+        self._ecc = ecc
+        self._inc = inc
+        self._raan = raan
+        self._argp = argp
+        self._nu = nu
+
+    @property
+    def a(self):
+        """Semimajor axis. """
+        return self._a
+
+    @property
+    def ecc(self):
+        """Eccentricity. """
+        return self._ecc
+
+    @property
+    def inc(self):
+        """Inclination. """
+        return self._inc
+
+    @property
+    def raan(self):
+        """Right ascension of the ascending node. """
+        return self._raan
+
+    @property
+    def argp(self):
+        """Argument of the perigee. """
+        return self._argp
+
+    @property
+    def nu(self):
+        """True anomaly. """
+        return self._nu
+
+    def to_vectors(self):
+        """Converts to position and velocity vector representation.
+
+        """
+        r, v = coe2rv(self.attractor.k.to(u.km ** 3 / u.s ** 2).value,
+                      self.p.to(u.km).value,
+                      self.ecc.value,
+                      self.inc.to(u.rad).value,
+                      self.raan.to(u.rad).value,
+                      self.argp.to(u.rad).value,
+                      self.nu.to(u.rad).value)
+
+        return rv.RVState(
+            self.attractor,
+            r * u.km,
+            v * u.km / u.s)
+
+    def to_classical(self):
+        """Converts to classical orbital elements representation.
+
+        """
+        return self
+
+    def to_equinoctial(self):
+        """Converts to modified equinoctial elements representation.
+
+        """
+        p, f, g, h, k, L = coe2mee(self.p.to(u.km).value,
+                                   self.ecc.value,
+                                   self.inc.to(u.rad).value,
+                                   self.raan.to(u.rad).value,
+                                   self.argp.to(u.rad).value,
+                                   self.nu.to(u.rad).value)
+
+        return equinoctial.ModifiedEquinoctialState(
+            self.attractor,
+            p * u.km,
+            f * u.rad,
+            g * u.rad,
+            h * u.rad,
+            k * u.rad,
+            L * u.rad)
