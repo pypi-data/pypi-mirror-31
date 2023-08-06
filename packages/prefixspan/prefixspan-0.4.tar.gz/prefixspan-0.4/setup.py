b1efa3bea3aa3ee6b13324b4e6ac229899f18c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:27 +0000

    Add package prefixspan/0.4/prefixspan-0.4.tar.gz

---
+++ packages/prefixspan/prefixspan-0.4.tar.gz/prefixspan-0.4/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+url = "https://github.com/chuanconggao/PrefixSpan-py"
+version = "0.4"
+
+setup(
+    name="prefixspan",
+
+    packages=["prefixspan"],
+    scripts=["prefixspan-cli"],
+
+    url=url,
+
+    version=version,
+    download_url=f"{url}/tarball/{version}",
+
+    license="MIT",
+
+    author="Chuancong Gao",
+    author_email="chuancong@gmail.com",
+
+    description="PrefixSpan and BIDE in Python 3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+
+    keywords=[
+        "data-mining",
+        "pattern-mining"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+
+    python_requires=">= 3",
+    install_requires=[
+        "docopt >= 0.6.2",
+    ]
+)
