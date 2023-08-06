7f2cab2065568575b74abdc590dfa3756cbdd949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:42 +0000

    Add package extraction-network/0.0.3/extraction_network-0.0.3.tar.gz

---
+++ packages/extraction-network/extraction_network-0.0.3.tar.gz/extraction_network-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='extraction_network',
+      version='0.0.3',
+      description='Create network of extractions and analyse/compare such networks',
+      url='http://github.com/r-kapoor/extraction_network',
+      author='Rahul Kapoor',
+      author_email='rahulkap@isi.edu',
+      license='MIT',
+      classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    ],
+      keywords='extraction network',
+      packages=find_packages(),
+      install_requires=['networkx', 'sklearn', 'numpy'],
+      python_requires='>=3',
+      include_package_data=True,
+      zip_safe=False)
