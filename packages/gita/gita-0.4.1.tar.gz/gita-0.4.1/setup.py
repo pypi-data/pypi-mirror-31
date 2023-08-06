2a504306ec754b76062ab011cf80921aafd2879f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:53 +0000

    Add package gita/0.4.1/gita-0.4.1.tar.gz

---
+++ packages/gita/gita-0.4.1.tar.gz/gita-0.4.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+long_description = None
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='gita',
+    packages=['gita',],
+    version='0.4.1',
+    description='Manage multiple git repos',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nosarthur/gita',
+    download_url='https://github.com/nosarthur/gita/archive/v0.4.tar.gz',
+    keywords=['git', 'manage multiple repositories'],
+    author='Dong Zhou',
+    author_email='zhou.dong@gmail.com',
+    license='MIT',
+    entry_points={
+        'console_scripts':[
+            'gita = gita.__main__:main'
+            ]
+        },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        ],
+)
