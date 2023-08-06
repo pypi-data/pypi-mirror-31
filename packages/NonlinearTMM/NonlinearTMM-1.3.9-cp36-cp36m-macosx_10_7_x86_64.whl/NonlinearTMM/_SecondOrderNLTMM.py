52f06786fa8fd34cf28f03e564837a39ab9753f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:20 +0000

    Add package NonlinearTMM/1.3.9/NonlinearTMM-1.3.9-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/NonlinearTMM/NonlinearTMM-1.3.9-cp36-cp36m-macosx_10_7_x86_64.whl/NonlinearTMM/_SecondOrderNLTMM.py
@@ -0,0 +1,8 @@
+from  NonlinearTMM import _SecondOrderNLTMMCython  # @UnresolvedImport
+
+__all__ = ["SecondOrderNLTMM"]
+    
+SecondOrderNLTMM = _SecondOrderNLTMMCython.SecondOrderNLTMM
+    
+if __name__ == "__main__":
+    pass
