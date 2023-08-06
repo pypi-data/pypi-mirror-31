f85d5e298c0fb2097f885ecf3a59635c6613342e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package splain/0.1.1/splain-0.1.1.tar.gz

---
+++ packages/splain/splain-0.1.1.tar.gz/splain-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup( name='splain',
+       version='0.1.1',
+       description='A module to explain Python exceptions as they occur.',
+       url='https://github.com/davidbpython/splain',
+       author='David Blaikie',
+       author_email='david@davidbpython.com',
+       license='MIT',
+       packages=['splain'],
+       install_requires=[],
+       zip_safe=False )
+
+
