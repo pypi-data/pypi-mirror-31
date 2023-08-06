16f50ab9667e64a2a886e68e06eedb55c763c671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package PyEpiDAG/1.3.1/PyEpiDAG-1.3.1.tar.gz

---
+++ packages/PyEpiDAG/PyEpiDAG-1.3.1.tar.gz/PyEpiDAG-1.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='PyEpiDAG',
+    version='1.3.1',
+    packages=['epidag',
+              'epidag.factory',
+              'epidag.bayesnet',
+              'epidag.simulation',
+              'epidag.fitting',
+              'epidag.fitting.alg',
+              'epidag.causality'],
+    url='https://github.com/TimeWz667/PyEpiDAG',
+    license='MIT',
+    author='TimeWz',
+    author_email='TimeWz667@gmail.com',
+    description='Epidemiological inference with DAG',
+    install_requires=['pandas', 'networkx', 'astunparse', 'numpy', 'scipy', 'matplotlib']
+
+)
