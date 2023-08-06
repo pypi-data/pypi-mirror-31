de0f65c38d34eac62c306f4e9a0047b5531f0792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:00 +0000

    Add package ianitor/0.1.0/ianitor-0.1.0.tar.gz

---
+++ packages/ianitor/ianitor-0.1.0.tar.gz/ianitor-0.1.0/setup.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS.md).
+
+# This file is part of ianitor.
+
+# mirakuru is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ianitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with ianitor.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+import os
+
+
+def strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+
+def reqs(*f):
+    return list(filter(None, [strip_comments(l) for l in open(
+        os.path.join(os.getcwd(), *f)).readlines()]))
+
+
+def get_version(version_tuple):
+    if not isinstance(version_tuple[-1], int):
+        return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
+    return '.'.join(map(str, version_tuple))
+
+
+def read(filename):
+    with open(filename, 'r') as file_handle:
+        return file_handle.read()
+
+
+init = os.path.join(os.path.dirname(__file__), 'src', 'ianitor', '__init__.py')
+version_line = list(filter(lambda l: l.startswith('VERSION'), open(init)))[0]
+VERSION = get_version(eval(version_line.split('=')[-1]))
+
+INSTALL_REQUIRES = reqs('requirements.txt')
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+PACKAGES = find_packages('src')
+PACKAGE_DIR = {'': 'src'}
+
+setup(
+    name='ianitor',
+    version=VERSION,
+    author='Clearcode - The A Room',
+    author_email='thearoom@clearcode.cc',
+    description='Doorkeeper for consul discovered services.',
+    long_description=read(README),
+    long_description_content_type="text/markdown",
+
+    packages=PACKAGES,
+    package_dir=PACKAGE_DIR,
+
+    url='https://github.com/ClearcodeHQ/ianitor',
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    zip_safe=False,
+
+    license="LGPL",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',  # noqa
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'ianitor = ianitor.script:main'
+        ]
+    }
+)
