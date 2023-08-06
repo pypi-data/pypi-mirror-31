2cde0d9e3349a7d8cef76e9a6ea5dfa46fd2d898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:32 +0000

    Add package knack/0.3.3/knack-0.3.3.tar.gz

---
+++ packages/knack/knack-0.3.3.tar.gz/knack-0.3.3/knack/__init__.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from .cli import CLI
+from .commands import CLICommandsLoader, CLICommand
+from .arguments import ArgumentsContext
+from .help import CLIHelp
+
+__all__ = ['CLI', 'CLICommandsLoader', 'CLICommand', 'CLIHelp', 'ArgumentsContext']
