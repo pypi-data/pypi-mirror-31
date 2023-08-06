ab9ec371818dc5d85107e073452277e80446401a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:43 +0000

    Add package t3-core/0.2.14/t3-core-0.2.14.tar.gz

---
+++ packages/t3-core/t3-core-0.2.14.tar.gz/t3-core-0.2.14/src/t3/django/urls.py
@@ -0,0 +1,35 @@
+"""URL Configuration."""
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.http import HttpResponse
+from django.urls import include, path
+
+
+def build_urlpatterns(*paths):
+    """
+    Generate urlpatterns, adding URL_PREFIX, health checks, and prometheus urls.
+
+    Valid input is what would normally be assigned in urlpatterns
+
+    ```
+    paths = [
+        path('', include('{project}.apps.{app}.urls'))
+    ]
+    ```
+    """
+    urlpatterns = [path(settings.URL_PREFIX, include(list(paths))), ]
+
+    # Paths that check services health
+    urlpatterns += [path(settings.URL_PREFIX, include([
+        path('healthz', lambda request: HttpResponse(status=200), name='health-check'),
+        path('readyz', lambda request: HttpResponse(status=200), name='ready-check'),
+    ])), ]
+
+    # Prometheus paths
+    urlpatterns += [path('', include('django_prometheus.urls')), ]
+
+    # Include static file urls in development mode
+    if settings.AWS_STORAGE is False:
+        urlpatterns += staticfiles_urlpatterns()
+
+    return urlpatterns
