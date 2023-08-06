67f4b67bd09bab69884c1cf313900302fc3051f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:07 +0000

    Add package supercell-slack/0.0.2/supercell-slack-0.0.2.tar.gz

---
+++ packages/supercell-slack/supercell-slack-0.0.2.tar.gz/supercell-slack-0.0.2/README.txt
@@ -0,0 +1,9 @@
+===========
+Support Bot V0.0.2
+===========
+
+Support Bot makes it easy to send a message via slack from our Supercell support bot!
+
+import support_bot
+
+support_bot.send_message(channel='some_channel',message='This is a message')
---
+++ packages/supercell-slack/supercell-slack-0.0.2.tar.gz/supercell-slack-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='supercell-slack',
+    version='0.0.2',
+    packages=['support_bot'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author="Scott Snyder",
+    author_email="scott.snyder@capitalone.com",
+    long_description=open('README.txt').read()
+)
