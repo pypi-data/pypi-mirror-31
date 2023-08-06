3b2f2f6fd5f1970012750a6f23a3f88c977f62d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package prometheus-pgbouncer-exporter/1.0.0/prometheus-pgbouncer-exporter-1.0.0.tar.gz

---
+++ packages/prometheus-pgbouncer-exporter/prometheus-pgbouncer-exporter-1.0.0.tar.gz/prometheus-pgbouncer-exporter-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup
+
+if sys.version_info.major < 3:
+    raise RuntimeError('Installing requires Python 3 or newer')
+
+setup(
+  name             = 'prometheus-pgbouncer-exporter',
+  packages         = ['prometheus_pgbouncer_exporter'],
+  version          = '1.0.0',
+  description      = 'Prometheus exporter for PgBouncer',
+  author           = 'Marco Pracucci',
+  author_email     = 'marco@pracucci.com',
+  url              = 'https://github.com/spreaker/prometheus-pgbouncer-exporter',
+  download_url     = 'https://github.com/spreaker/prometheus-pgbouncer-exporter/archive/1.0.0.tar.gz',
+  keywords         = ['prometheus', 'pgbouncer'],
+  classifiers      = [],
+  python_requires  = ' >= 3',
+  install_requires = ['psycopg2 == 2.7.3.2', 'prometheus_client==0.0.21', 'python-json-logger==0.1.5', 'PyYAML==3.12'],
+  entry_points     = {
+    'console_scripts': [
+        'pgbouncer-exporter=prometheus_pgbouncer_exporter.cli:main',
+    ]
+  }
+)
