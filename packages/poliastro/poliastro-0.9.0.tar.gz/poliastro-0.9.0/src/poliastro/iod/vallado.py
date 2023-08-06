7260effa0750afeb6a864f391a83ae634daeace1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package poliastro/0.9.0/poliastro-0.9.0.tar.gz

---
+++ packages/poliastro/poliastro-0.9.0.tar.gz/poliastro-0.9.0/src/poliastro/iod/vallado.py
@@ -0,0 +1,115 @@
+"""Initial orbit determination.
+
+"""
+import numpy as np
+from astropy import units as u
+
+from poliastro.jit import jit
+from poliastro.stumpff import c2, c3
+
+
+def lambert(k, r0, r, tof, short=True, numiter=35, rtol=1e-8):
+    """Solves the Lambert problem.
+
+    .. versionadded:: 0.3.0
+
+    Parameters
+    ----------
+    k : ~astropy.units.Quantity
+        Gravitational constant of main attractor (km^3 / s^2).
+    r0 : ~astropy.units.Quantity
+        Initial position (km).
+    r : ~astropy.units.Quantity
+        Final position (km).
+    tof : ~astropy.units.Quantity
+        Time of flight (s).
+    short : boolean, optional
+        Find out the short path, default to True. If False, find long path.
+    numiter : int, optional
+        Maximum number of iterations, default to 35.
+    rtol : float, optional
+        Relative tolerance of the algorithm, default to 1e-8.
+
+    Raises
+    ------
+    RuntimeError
+        If it was not possible to compute the orbit.
+
+    Note
+    ----
+    This uses the universal variable approach found in Battin, Mueller & White
+    with the bisection iteration suggested by Vallado. Multiple revolutions
+    not supported.
+
+    """
+    k_ = k.to(u.km ** 3 / u.s ** 2).value
+    r0_ = r0.to(u.km).value
+    r_ = r.to(u.km).value
+    tof_ = tof.to(u.s).value
+
+    v0, v = _lambert(k_, r0_, r_, tof_, short, numiter, rtol)
+
+    yield v0 * u.km / u.s, v * u.km / u.s
+
+
+@jit
+def _lambert(k, r0, r, tof, short, numiter, rtol):
+    if short:
+        t_m = +1
+    else:
+        t_m = -1
+
+    norm_r0 = np.dot(r0, r0)**.5
+    norm_r = np.dot(r, r)**.5
+    norm_r0_times_norm_r = norm_r0 * norm_r
+    norm_r0_plus_norm_r = norm_r0 + norm_r
+
+    cos_dnu = np.dot(r0, r) / norm_r0_times_norm_r
+
+    A = t_m * (norm_r * norm_r0 * (1 + cos_dnu))**.5
+
+    if A == 0.0:
+        raise RuntimeError("Cannot compute orbit, phase angle is 180 degrees")
+
+    psi = 0.0
+    psi_low = -4 * np.pi
+    psi_up = 4 * np.pi
+
+    count = 0
+
+    while count < numiter:
+        y = norm_r0_plus_norm_r + A * (psi * c3(psi) - 1) / c2(psi)**.5
+        if A > 0.0:
+            # Readjust xi_low until y > 0.0
+            # Translated directly from Vallado
+            while y < 0.0:
+                psi_low = psi
+                psi = (0.8 * (1.0 / c3(psi)) *
+                       (1.0 - norm_r0_times_norm_r * np.sqrt(c2(psi)) / A))
+                y = norm_r0_plus_norm_r + A * (psi * c3(psi) - 1) / c2(psi)**.5
+
+        xi = np.sqrt(y / c2(psi))
+        tof_new = (xi**3 * c3(psi) + A * np.sqrt(y)) / np.sqrt(k)
+
+        # Convergence check
+        if np.abs((tof_new - tof) / tof) < rtol:
+            break
+        count += 1
+        # Bisection check
+        condition = (tof_new <= tof)
+        psi_low = psi_low + (psi - psi_low) * condition
+        psi_up = psi_up + (psi - psi_up) * (not condition)
+
+        psi = (psi_up + psi_low) / 2
+    else:
+        raise RuntimeError("Maximum number of iterations reached")
+
+    f = 1 - y / norm_r0
+    g = A * np.sqrt(y / k)
+
+    gdot = 1 - y / norm_r
+
+    v0 = (r - f * r0) / g
+    v = (gdot * r - r0) / g
+
+    return v0, v
---
+++ packages/poliastro/poliastro-0.9.0.tar.gz/poliastro-0.9.0/src/poliastro/util.py
@@ -0,0 +1,109 @@
+"""Function helpers.
+
+"""
+import numpy as np
+
+from astropy.coordinates import matrix_utilities
+from astropy.time import Time
+
+
+def circular_velocity(k, a):
+    """Compute circular velocity for a given body (k) and semimajor axis (a).
+
+    """
+    return np.sqrt(k / a)
+
+
+def rotate(vector, angle, axis='z', unit=None):
+    """Rotates a vector around axis a right-handed positive angle.
+
+    This is just a convenience function around
+    :py:func:`astropy.coordinates.matrix_utilities.rotation_matrix`.
+
+    Parameters
+    ----------
+    vector : array_like
+        Dimension 3 vector.
+    angle : convertible to Angle
+        Angle of rotation.
+    axis : str or 3-sequence
+        Either 'x','y', 'z', or a (x,y,z) specifying an axis to rotate
+        about. If 'x','y', or 'z', the rotation sense is
+        counterclockwise looking down the + axis (e.g. positive
+        rotations obey left-hand-rule).
+    unit : ~astropy.units.UnitBase, optional
+        If `angle` does not have associated units, they are in this
+        unit.  If neither are provided, it is assumed to be degrees.
+
+    Note
+    -----
+    This is just a convenience function around
+    :py:func:`astropy.coordinates.matrix_utilities.rotation_matrix`.
+    This performs a so-called *active* or *alibi* transformation: rotates the
+    vector while the coordinate system remains unchanged. To do the opposite
+    operation (*passive* or *alias* transformation) call the function as
+    ``rotate(vec, ax, -angle, unit)`` or use the convenience function
+    :py:func:`transform`, see [1]_.
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/Rotation_matrix#Ambiguities
+
+    """
+    rot = np.asarray(matrix_utilities.rotation_matrix(-angle, axis, unit))
+    return np.dot(rot, vector)
+
+
+def transform(vector, angle, axis='z', unit=None):
+    """Rotates a coordinate system around axis a positive right-handed angle.
+
+    Note
+    -----
+    This is a convenience function, equivalent to
+    ``rotate(vec, -angle, axis, unit)``.
+    Refer to the documentation of :py:func:`rotate` for further information.
+
+    """
+    return rotate(vector, -angle, axis, unit)
+
+
+def norm(vec):
+    """Norm of a Quantity vector that respects units.
+
+    """
+    return np.sqrt(vec.dot(vec))
+
+
+def time_range(start, *, periods=50, spacing=None, end=None):
+    """Generates range of astronomical times.
+
+    .. versionadded:: 0.8.0
+
+    Parameters
+    ----------
+    periods : int, optional
+        Number of periods, default to 50.
+    spacing : Time or Quantity, optional
+        Spacing between periods, optional.
+    end : Time or equivalent, optional
+        End date.
+
+    Returns
+    -------
+    Time
+        Array of time values.
+
+    """
+    start = Time(start)
+
+    if spacing is not None and end is None:
+        result = start + spacing * np.arange(0, periods)
+
+    elif end is not None and spacing is None:
+        end = Time(end)
+        result = start + (end - start) * np.linspace(0, 1, periods)
+
+    else:
+        raise ValueError("Either 'end' or 'spacing' must be specified")
+
+    return result
