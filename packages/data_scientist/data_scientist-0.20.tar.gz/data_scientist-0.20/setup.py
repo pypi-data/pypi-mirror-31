00866753589741c020193f77695144ab20f18ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:24 +0000

    Add package data_scientist/0.20/data_scientist-0.20.tar.gz

---
+++ packages/data_scientist/data_scientist-0.20.tar.gz/data_scientist-0.20/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='data_scientist',
+      version='0.20',
+      description='Fundamental tools for DS',
+      author='0100110101000001',
+      author_email='0b0100110101000001@gmail.com',
+      license='MIT',
+      packages=['data_scientist'],
+      zip_safe=False)
