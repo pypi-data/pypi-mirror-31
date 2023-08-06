8f2e23efb7fa131e5c04fcc20699127746a2630a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:21 +0000

    Add package aspy.yaml/1.1.1/aspy.yaml-1.1.1.tar.gz

---
+++ packages/aspy.yaml/aspy.yaml-1.1.1.tar.gz/aspy.yaml-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='aspy.yaml',
+    description="A few extensions to pyyaml.",
+    url='https://github.com/asottile/aspy.yaml',
+    version='1.1.1',
+
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+
+    platforms='all',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=['aspy', 'aspy.yaml'],
+    namespace_packages=['aspy'],
+    install_requires=['pyyaml'],
+)
