1331b127ac2f720a431d9d15edebe773ef980bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:58 +0000

    Add package longling/0.0.10/longling-0.0.10.tar.gz

---
+++ packages/longling/longling-0.0.10.tar.gz/longling-0.0.10/setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+# created by tongshiwei on 17-12-17
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='longling',
+    version='0.0.10',
+    author='Sherlock, Shiwei Tong',
+    author_email='tongsw@mail.ustc.edu.cn',
+    packages=find_packages(),
+    scripts=[],
+    url='https://gitlab.com/tswsxk/longling.git',
+    license='LICENSE.txt',
+    description='handy wrapper for many libs',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+)
