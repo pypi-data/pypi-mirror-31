9a49c1b368025e4f9e62d90a01de6dcdbdd48707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:21 +0000

    Add package sieve_sdk/0.1.0/sieve_sdk-0.1.0.tar.gz

---
+++ packages/sieve_sdk/sieve_sdk-0.1.0.tar.gz/sieve_sdk-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+from sieve_sdk import __VERSION__
+
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.md'),
+                        encoding='utf-8').read()
+
+setup(
+    name='sieve_sdk',
+    version=__VERSION__,
+    description='Sieve SDK',
+    long_description=long_description,
+    author='Diogo Magalhaes Martins',
+    author_email='diogo.martins@sieve.com.br',
+    url='https://bitbucket.org/sievetech/sieve-python-sdk',
+    packages=find_packages(exclude=['tests', 's3_upload']),
+    install_requires=[
+        "aiohttp>=3.1.2",
+        "simple_json_logger>=0.2.2"
+    ],
+    include_package_data=True,
+)
