ca1d3a7d763ff651becfe4b8e02b3ab962d974ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:12 +0000

    Add package pyspeckle/0.1.0/pyspeckle-0.1.0.tar.gz

---
+++ packages/pyspeckle/pyspeckle-0.1.0.tar.gz/pyspeckle-0.1.0/pyspeckle/__init__.py
@@ -0,0 +1,12 @@
+"""
+================================================================================
+pyspeckle: Design and Analysis of Speckle
+================================================================================
+
+    http://github.com/scottprahl/pyspeckle
+"""
+
+__author__ = 'Scott Prahl'
+__version__ = '0.1.0'
+
+from .pyspeckle import *
