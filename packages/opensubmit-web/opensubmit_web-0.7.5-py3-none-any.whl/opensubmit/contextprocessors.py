24e8bf37a46afced872fd6e14a3d2ee790530a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:29 +0000

    Add package opensubmit-web/0.7.5/opensubmit_web-0.7.5-py3-none-any.whl

---
+++ packages/opensubmit-web/opensubmit_web-0.7.5-py3-none-any.whl/opensubmit/contextprocessors.py
@@ -0,0 +1,9 @@
+from opensubmit import settings
+
+
+def footer(request):
+    return {
+        'main_url': settings.MAIN_URL,
+        'opensubmit_version': settings.VERSION,
+        'opensubmit_admin': settings.ADMINS[0][1],
+    }
