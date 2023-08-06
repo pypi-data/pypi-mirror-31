718aee729930d9d598b119b5070f4eff4b25ab14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:09 +0000

    Add package tfeature/0.21/tfeature-0.21.tar.gz

---
+++ packages/tfeature/tfeature-0.21.tar.gz/tfeature-0.21/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tfeature',
+    version='0.21',
+    author='kellerx',
+    author_email='kellerx0319@gmail.com',
+    url='https://github.com/xashes/tfeature',
+    packages=find_packages(),
+)
