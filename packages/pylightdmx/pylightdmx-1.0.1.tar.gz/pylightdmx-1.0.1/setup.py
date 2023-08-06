90444431d0590842c1c504d3a2789b838aab3ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:43 +0000

    Add package pylightdmx/1.0.1/pylightdmx-1.0.1.tar.gz

---
+++ packages/pylightdmx/pylightdmx-1.0.1.tar.gz/pylightdmx-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name = "pylightdmx",
+	version = "1.0.1",
+	description = "Enttec DMX USB Pro Mk2 Lighting Controller",
+	licence = "GPLV3",
+	author = "ayork02",
+	author_email = "ayork02@outlook.com",
+	url = "https://github.com/ayork02/pylightdmx",
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Programming Language :: Python :: 3"
+	],
+	install_requires = ["pyserial", "Pillow"],
+	)
