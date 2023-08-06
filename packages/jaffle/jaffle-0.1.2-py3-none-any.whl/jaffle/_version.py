05792798fc2cdda9f7f399d4e425211a3601e237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:35 +0000

    Add package jaffle/0.1.2/jaffle-0.1.2-py3-none-any.whl

---
+++ packages/jaffle/jaffle-0.1.2-py3-none-any.whl/jaffle/_version.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+version_info = (0, 1, 2)
+__version__ = '.'.join(map(str, version_info))
+
+protocol_version_info = (0, 1)
+protocol_version = "%i.%i" % protocol_version_info
