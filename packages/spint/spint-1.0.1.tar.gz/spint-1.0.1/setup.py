4e84276aab5ab16f79f31f8b8c9d521fa96f1baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:35 +0000

    Add package spint/1.0.1/spint-1.0.1.tar.gz

---
+++ packages/spint/spint-1.0.1.tar.gz/spint-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+from distutils.command.build_py import build_py
+
+setup(name='spint', #name of package
+      version='1.0.1',
+      description='SPatial INTeraction models', #short <80chr description
+      url='https://github.com/TaylorOshan/spint', #github repo
+      download_url='https://pypi.python.org/pypi/spint',
+      maintainer='Taylor M. Oshan', 
+      maintainer_email='tayoshan@gmail.com', 
+      test_suite = 'nose.collector',
+      tests_require=['nose'],
+      keywords='spatial statistics',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+        ],
+      license='3-Clause BSD',
+      packages=['spint'],
+      install_requires=['scipy', 'numpy', 'pysal', 'libpysal', 'spglm'],
+      zip_safe=False,
+      cmdclass = {'build.py':build_py})
