88d0f07b04e87eeb3906d0cec7db577ffb5762a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:45 +0000

    Add package prometheus-framework/0.1.3/prometheus_framework-0.1.3-py35-none-any.whl

---
+++ packages/prometheus-framework/prometheus_framework-0.1.3-py35-none-any.whl/prometheus/conf/default_settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = (
+    'rest_framework',
+)
+
+PROMETHEUS = {
+    'models': {
+        'template_variables': None,  # 'basic.TemplateVariable'
+        'flat_pages': None,  # 'basic.FlatPage',
+    }
+}
