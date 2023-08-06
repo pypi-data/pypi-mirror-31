542c728e3ea04e4a6e862ee40003362171e080eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:56 +0000

    Add package xlxnester/1.1.1/xlxnester-1.1.1.tar.gz

---
+++ packages/xlxnester/xlxnester-1.1.1.tar.gz/xlxnester-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'xlxnester',
+    version='1.1.1',
+    py_modules=['xlxnester'],
+    author='lx',
+    author_email="xu_li_xin@163.com",
+    description='just test for fun'
+    )
