9f7fa77af5bcc9b00e8c75eb758bd8edd36fa666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:13 +0000

    Add package supervise_api/0.5.2/supervise_api-0.5.2.tar.gz

---
+++ packages/supervise_api/supervise_api-0.5.2.tar.gz/supervise_api-0.5.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='supervise_api',
+      version='0.5.2',
+      description='An API for running processes safely and securely',
+      long_description=("This package uses the supervise utility, a separately-available C binary,"
+                        " to provide a better process API for Linux."),
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+      ],
+      keywords='linux subprocess processes',
+      url='https://github.com/catern/supervise',
+      author='catern',
+      author_email='sbaugh@catern.com',
+      license='MIT',
+      packages=['supervise_api', 'supervise_api.tests'])
