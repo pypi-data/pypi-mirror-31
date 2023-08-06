8015afc5277fbe11452a1b501f2cae25b919de3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:36 +0000

    Add package BioPlate/0.1.0/BioPlate-0.1.0-py3-none-any.whl

---
+++ packages/BioPlate/BioPlate-0.1.0-py3-none-any.whl/BioPlate/Count.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from BioPlate.Iterate import BioPlateIterate
+from collections import OrderedDict
+
+
+class BioPlateCount:
+    """A row is symbolise by it's letter, a column by a number"""    
+      
+    def __new__(cls, plate, reverse=False):
+        cls.plate = plate
+        cls.reverse = reverse
+        cls.plate_iterated = BioPlateIterate(plate, OnlyValue=True)
+        return cls.count()
+   
+    @classmethod     
+    def __count(cls, plate):
+        """
+
+        :param plate:
+        :return:
+        """      
+        unique, count = np.unique(plate, return_counts=True)
+        count_in_dict = dict(zip(unique, count))
+        count_ordered = dict(sorted(count_in_dict.items(), key=lambda x:x[1], reverse=cls.reverse))
+        return count_ordered
+
+    @classmethod
+    def count(cls):
+        if cls.plate.name == "BioPlate":
+            return cls.count_BioPlate()
+        elif cls.plate.name == "BioPlateInserts":
+            return cls.count_BioPlateInserts()
+        elif cls.plate.name == "BioPlateStack":
+            return cls.count_BioPlateStack()
+    
+    @classmethod
+    def count_BioPlateInserts(cls):
+        result = {}
+        result["top"] = cls.__count(next( cls.plate_iterated))
+        result["bot"] =  cls.__count(next(cls.plate_iterated))
+        return result
+
+    @classmethod
+    def count_BioPlate(cls):
+        return cls.__count(next(cls.plate_iterated))
+       
+    @classmethod    
+    def count_BioPlateStack(cls):
+        result = {}
+        n = 0
+        for index, plate in enumerate(BioPlateIterate(cls.plate, OnlyValue=True)):
+            if plate.name == "BioPlate":
+                result[index] = cls.count_BioPlate()
+                pass
+            elif plate.name == "BioPlateInserts":
+                try:
+                    result[index] = cls.count_BioPlateInserts()
+                except StopIteration:
+                    break            
+        return result
+     
