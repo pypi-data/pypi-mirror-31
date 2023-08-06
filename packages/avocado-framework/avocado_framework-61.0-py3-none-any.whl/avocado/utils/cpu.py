65ef4d40181b8206b832cada1e4990db294ea4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:04 +0000

    Add package avocado-framework/61.0/avocado_framework-61.0-py3-none-any.whl

---
+++ packages/avocado-framework/avocado_framework-61.0-py3-none-any.whl/avocado/utils/cpu.py
@@ -0,0 +1,190 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# This code was inspired in the autotest project,
+#
+# client/base_utils.py
+# Original author: Martin J Bligh <mbligh@google.com>
+# Original author: John Admanski <jadmanski@google.com>
+
+
+"""
+Get information from the current's machine CPU.
+"""
+
+import re
+import os
+import platform
+
+
+def _list_matches(lst, pattern):
+    """
+    True if any item in list matches the specified pattern.
+    """
+    compiled = re.compile(pattern)
+    for element in lst:
+        match = compiled.search(element)
+        if match:
+            return 1
+    return 0
+
+
+def _get_cpu_info():
+    """
+    Returns info on the 1st CPU entry from /proc/cpuinfo as a list of lines
+
+    :returns: `list` of lines 1st CPU entry from /proc/cpuinfo file
+    :rtype: `list`
+    """
+    cpuinfo = []
+    with open('/proc/cpuinfo') as proc_cpuinfo:
+        for line in proc_cpuinfo:
+            if line == '\n':
+                break
+            cpuinfo.append(line)
+    return cpuinfo
+
+
+def _get_cpu_status(cpu):
+    """
+    Check if a CPU is online or offline
+
+    :pram cpu: CPU number 1 or 2 or 39
+    :type cpu: integer
+    :returns: `bool` True if online or False if not
+    :rtype: 'bool'
+    """
+    with open('/sys/devices/system/cpu/cpu%s/online' % cpu) as online:
+        if '1' in online.read():
+            return True
+    return False
+
+
+def cpu_has_flags(flags):
+    """
+    Check if a list of flags are available on current CPU info
+
+    :param flags: A `list` of cpu flags that must exists on the current CPU.
+    :type flags: `list`
+    :returns: `bool` True if all the flags were found or False if not
+    :rtype: `list`
+    """
+    cpu_info = _get_cpu_info()
+
+    if not isinstance(flags, list):
+        flags = [flags]
+
+    for flag in flags:
+        if not _list_matches(cpu_info, '.*%s.*' % flag):
+            return False
+    return True
+
+
+def get_cpu_vendor_name():
+    """
+    Get the current cpu vendor name
+
+    :returns: string 'intel' or 'amd' or 'power7' depending on the
+         current CPU architecture.
+    :rtype: `string`
+    """
+    vendors_map = {
+        'intel': ("GenuineIntel", ),
+        'amd': ("AMD", ),
+        'power7': ("POWER7", ),
+        'power8': ("POWER8", )
+    }
+
+    cpu_info = _get_cpu_info()
+    for vendor, identifiers in vendors_map.items():
+        for identifier in identifiers:
+            if _list_matches(cpu_info, identifier):
+                return vendor
+    return None
+
+
+def get_cpu_arch():
+    """
+    Work out which CPU architecture we're running on
+    """
+    cpu_table = [('^cpu.*(RS64|POWER3|Broadband Engine)', 'power'),
+                 ('^cpu.*POWER4', 'power4'),
+                 ('^cpu.*POWER5', 'power5'),
+                 ('^cpu.*POWER6', 'power6'),
+                 ('^cpu.*POWER7', 'power7'),
+                 ('^cpu.*POWER8', 'power8'),
+                 ('^cpu.*POWER9', 'power9'),
+                 ('^cpu.*PPC970', 'power970'),
+                 ('(ARM|^CPU implementer|^CPU part|^CPU variant'
+                  '|^Features|^BogoMIPS|^CPU revision)', 'arm'),
+                 ('(^cpu MHz dynamic|^cpu MHz static|^features'
+                  '|^bogomips per cpu|^max thread id)', 's390'),
+                 ('^type', 'sparc64'),
+                 ('^flags.*:.* lm .*', 'x86_64'),
+                 ('^flags', 'i386')]
+    cpuinfo = _get_cpu_info()
+    for (pattern, arch) in cpu_table:
+        if _list_matches(cpuinfo, pattern):
+            return arch
+    return platform.machine()
+
+
+def cpu_online_list():
+    """
+    Reports a list of indexes of the online cpus
+    """
+    cpus = []
+    search_str = 'processor'
+    index = 2
+    if platform.machine() == 's390x':
+        search_str = 'cpu number'
+        index = 3
+    with open('/proc/cpuinfo', 'r') as proc_cpuinfo:
+        for line in proc_cpuinfo:
+            if line.startswith(search_str):
+                cpus.append(int(line.split()[index]))  # grab cpu number
+    return cpus
+
+
+def total_cpus_count():
+    """
+    Return Number of Total cpus in the system including offline cpus
+    """
+    return os.sysconf('SC_NPROCESSORS_CONF')
+
+
+def online_cpus_count():
+    """
+    Return Number of Online cpus in the system
+    """
+    return os.sysconf('SC_NPROCESSORS_ONLN')
+
+
+def online(cpu):
+    """
+    Online given CPU
+    """
+    with open("/sys/devices/system/cpu/cpu%s/online" % cpu, "w") as fd:
+        fd.write('1')
+    if _get_cpu_status(cpu):
+        return 0
+    return 1
+
+
+def offline(cpu):
+    """
+    Offline given CPU
+    """
+    with open("/sys/devices/system/cpu/cpu%s/online" % cpu, "w") as fd:
+        fd.write('0')
+    if _get_cpu_status(cpu):
+        return 1
+    return 0
