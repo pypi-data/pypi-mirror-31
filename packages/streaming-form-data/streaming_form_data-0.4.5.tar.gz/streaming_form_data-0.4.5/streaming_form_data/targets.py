80cb9e9ebac33955ec66ea895b20a252f268a508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:54 +0000

    Add package streaming-form-data/0.4.5/streaming_form_data-0.4.5.tar.gz

---
+++ packages/streaming-form-data/streaming_form_data-0.4.5.tar.gz/streaming_form_data-0.4.5/streaming_form_data/targets.py
@@ -0,0 +1,64 @@
+import hashlib
+
+
+class BaseTarget:
+    """Targets determine what to do with some input once the parser is done with
+    it. Any new Target should inherit from this class and override
+    data_received.
+    """
+
+    def start(self):
+        pass
+
+    def data_received(self, chunk):
+        raise NotImplementedError()
+
+    def finish(self):
+        pass
+
+
+class NullTarget(BaseTarget):
+    def data_received(self, chunk):
+        pass
+
+
+class ValueTarget(BaseTarget):
+    def __init__(self):
+        self._values = []
+
+    def data_received(self, chunk):
+        self._values.append(chunk)
+
+    @property
+    def value(self):
+        return b''.join(self._values)
+
+
+class FileTarget(BaseTarget):
+    def __init__(self, filename):
+        self.filename = filename
+
+        self._fd = None
+
+    def start(self):
+        self._fd = open(self.filename, 'wb')
+
+    def data_received(self, chunk):
+        self._fd.write(chunk)
+        self._fd.flush()
+
+    def finish(self):
+        self._fd.flush()
+        self._fd.close()
+
+
+class SHA256Target(BaseTarget):
+    def __init__(self):
+        self._hash = hashlib.sha256()
+
+    def data_received(self, chunk):
+        self._hash.update(chunk)
+
+    @property
+    def value(self):
+        return self._hash.hexdigest()
