a3d7090fae4dc4662f21c1be365934ca7efc6ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:07 +0000

    Add package pipper/0.2.1/pipper-0.2.1.tar.gz

---
+++ packages/pipper/pipper-0.2.1.tar.gz/pipper-0.2.1/pipper/wrapper.py
@@ -0,0 +1,57 @@
+import pip
+import pkg_resources
+
+try:
+    # Starting with pip10 the `main` function has moved
+    # to the `_internal` module.
+    from pip import _internal as pip10
+except ImportError:
+    pip10 = None
+
+from pipper import versioning
+
+
+def update_required(package_name: str, install_version: str) -> bool:
+    """ """
+
+    existing = status(package_name)
+
+    if not existing:
+        return True
+
+    version = (
+        versioning.deserialize(install_version)
+        if install_version.startswith('v') else
+        install_version
+    )
+
+    current = pkg_resources.parse_version(existing.version)
+    target = pkg_resources.parse_version(version)
+    return current != target
+
+
+def status(package_name: str):
+    """ """
+
+    try:
+        return pkg_resources.get_distribution(package_name)
+    except pkg_resources.DistributionNotFound:
+        return None
+    except Exception:
+        raise
+
+
+def install_wheel(wheel_path: str, to_user: bool = False):
+    """ """
+
+    cmd = ['install', wheel_path]
+    cmd += ['--user'] if to_user else []
+    print('COMMAND:', ' '.join(cmd))
+
+    # pip 10.0.0 moves the main function. Handle both 9.x and 10
+    # cases here for maximum compatibility.
+    execution_function = (
+        getattr(pip10, 'main', None)
+        or getattr(pip, 'main', None)
+    )
+    execution_function(cmd)
