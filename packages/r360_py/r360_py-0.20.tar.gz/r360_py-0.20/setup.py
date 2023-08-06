4b3c3c67fceb4877be16bc6dd9107e9f0a7ab4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:36 +0000

    Add package r360_py/0.20/r360_py-0.20.tar.gz

---
+++ packages/r360_py/r360_py-0.20.tar.gz/r360_py-0.20/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+  name = 'r360_py',
+  packages = find_packages(),
+  version = '0.20',
+  description = 'A python client library to query the Route360° API',
+  author = 'Motion Intelligence GmbH',
+  author_email = 'mail@motionintelligence.net',
+  url = 'https://github.com/route360/r360-py',
+  download_url = 'https://github.com/route360/r360-py/tarball/0.20',
+  install_requires=[
+     'requests',
+ ],
+  keywords = ['isochrone', 'routing', 'polygon', 'openstreetmaps', 'gtfs', 'map'],
+  classifiers = [],
+)
