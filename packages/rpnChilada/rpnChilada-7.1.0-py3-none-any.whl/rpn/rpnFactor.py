71407b33a1ca460cc7805ca950116f46e3afc80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:41 +0000

    Add package rpnChilada/7.1.0/rpnChilada-7.1.0-py3-none-any.whl

---
+++ packages/rpnChilada/rpnChilada-7.1.0-py3-none-any.whl/rpn/rpnFactor.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# //******************************************************************************
+# //
+# //  rpnFactor.py
+# //
+# //  RPN command-line calculator factoring utilities
+# //  copyright (c) 2018, Rick Gutleber (rickg@his.com)
+# //
+# //  License: GNU GPL 3.0 (see <http://www.gnu.org/licenses/gpl.html> for more
+# //  information).
+# //
+# //******************************************************************************
+
+import bz2
+import collections
+import contextlib
+import fractions
+import os
+import pickle
+import random
+
+from mpmath import ceil, fabs, fdiv, floor, fmod, fneg, fprod, log, log10, mp, power
+
+from rpn.rpnKeyboard import DelayedKeyboardInterrupt
+from rpn.rpnPersistence import loadFactorCache
+from rpn.rpnPrimes import primes
+from rpn.rpnPrimeUtils import isPrimeNumber
+from rpn.rpnSettings import setAccuracy
+from rpn.rpnUtils import getExpandedFactorList, oneArgFunctionEvaluator, real, real_int
+
+import rpn.rpnGlobals as g
+
+
+# //******************************************************************************
+# //
+# //  getFactors
+# //
+# //******************************************************************************
+
+@oneArgFunctionEvaluator( )
+def getFactors( target ):
+    if target < -1:
+        result = [ -1 ]
+        result.extend( getFactors( fneg( target ) ) )
+        return result
+    elif target == -1:
+        return [ -1 ]
+    elif target == 0:
+        return [ 0 ]
+    elif target == 1:
+        return [ 1 ]
+
+    n = int( floor( target ) )
+
+    if g.factorCache is None:
+        loadFactorCache( )
+
+    if not g.ignoreCache:
+        if n in g.factorCache:
+            if g.verbose and n != 1:
+                print( 'cache hit:', n )
+                print( )
+
+            return g.factorCache[ n ]
+
+    try:
+        result = factorByTrialDivision( n )   # throws if n is too big
+
+        if n > g.minValueToCache and n not in g.factorCache:
+            g.factorCache[ n ] = result
+
+        return result
+
+    except ValueError as error:
+        pass
+
+    verbose = g.verbose
+
+    if verbose:
+        print( '\nfactoring', n, '(', int( floor( log10( n ) ) ), ' digits)...' )
+
+    from rpn.factorise import factorise
+    result = factorise( int( n ) )
+
+    if n > g.minValueToCache and n not in g.factorCache:
+        g.factorCache[ n ] = result
+
+    return result
+
+
+# //******************************************************************************
+# //
+# //  getFactorList
+# //
+# //******************************************************************************
+
+def getFactorList( n ):
+    return list( collections.Counter( getFactors( n ) ).items( ) )
+
+
+# //******************************************************************************
+# //
+# //  factorByTrialDivision
+# //
+# //  This is sufficiently fast for small numbers... at least for now.
+# //
+# //******************************************************************************
+
+def factorByTrialDivision( n ):
+    if n > 100000000:  # 10,000 squared because we check all primes below 10,000
+        raise ValueError( 'value', n, 'is too big to factor by trial division' )
+
+    result = [ ]
+
+    for i in primes:
+        while n > 1 and fmod( n, i ) == 0:
+            n = fdiv( n, i )
+            result.append( i )
+
+        if isPrimeNumber( n ):
+            break
+
+    if n > 1:
+        result.append( int( n ) )
+
+    return result
+
