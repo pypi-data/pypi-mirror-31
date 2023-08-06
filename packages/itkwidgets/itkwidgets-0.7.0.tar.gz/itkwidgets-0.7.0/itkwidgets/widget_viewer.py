31a61c66393de90474b34073dd61707c92a22192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:06 +0000

    Add package itkwidgets/0.7.0/itkwidgets-0.7.0.tar.gz

---
+++ packages/itkwidgets/itkwidgets-0.7.0.tar.gz/itkwidgets-0.7.0/itkwidgets/widget_viewer.py
@@ -0,0 +1,31 @@
+"""Viewer class
+
+Visualization of an image.
+
+In the future, will add optional segmentation mesh overlay.
+"""
+
+import ipywidgets as widgets
+from traitlets import Unicode
+import numpy as np
+import itk
+from .trait_types import ITKImage, itkimage_serialization
+
+@widgets.register
+class Viewer(widgets.DOMWidget):
+    _view_name = Unicode('ViewerView').tag(sync=True)
+    _model_name = Unicode('ViewerModel').tag(sync=True)
+    _view_module = Unicode('itk-jupyter-widgets').tag(sync=True)
+    _model_module = Unicode('itk-jupyter-widgets').tag(sync=True)
+    _view_module_version = Unicode('^0.2.1').tag(sync=True)
+    _model_module_version = Unicode('^0.2.1').tag(sync=True)
+    image = ITKImage(default_value=None, allow_none=True).tag(sync=True, **itkimage_serialization)
+
+def view(image):
+    viewer = Viewer()
+    if isinstance(image, np.ndarray):
+        image_from_array = itk.GetImageViewFromArray(image)
+        viewer.image = image_from_array
+    else:
+        viewer.image = image
+    return viewer
