b00704595c1cfeb279922ad05ff796fb50859578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:42 +0000

    Add package beem/0.19.27/beem-0.19.27.tar.gz

---
+++ packages/beem/beem-0.19.27.tar.gz/beem-0.19.27/beemapi/exceptions.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import str
+import re
+from beemgrapheneapi.graphenerpc import RPCError, RPCErrorDoRetry, NumRetriesReached
+
+
+def decodeRPCErrorMsg(e):
+    """ Helper function to decode the raised Exception and give it a
+        python Exception class
+    """
+    found = re.search(
+        (
+            "(10 assert_exception: Assert Exception\n|"
+            "3030000 tx_missing_posting_auth)"
+            ".*: (.*)\n"
+        ),
+        str(e),
+        flags=re.M)
+    if found:
+        return found.group(2).strip()
+    else:
+        return str(e)
+
+
+class MissingRequiredActiveAuthority(RPCError):
+    pass
+
+
+class UnkownKey(RPCError):
+    pass
+
+
+class NoMethodWithName(RPCError):
+    pass
+
+
+class NoApiWithName(RPCError):
+    pass
+
+
+class ApiNotSupported(RPCError):
+    pass
+
+
+class UnhandledRPCError(RPCError):
+    pass
+
+
+class NoAccessApi(RPCError):
+    pass
+
+
+class InvalidEndpointUrl(Exception):
+    pass
+
+
+class UnnecessarySignatureDetected(Exception):
+    pass
