8344a98413690630d280de7eadcdcbccd736514c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:45 +0000

    Add package dj-auth/1.4.2/dj_auth-1.4.2.tar.gz

---
+++ packages/dj-auth/dj_auth-1.4.2.tar.gz/dj_auth-1.4.2/dj_auth/urls.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from .views import ObjectFilterValuesWidgetView
+
+urlpatterns = [url(r'^loadvalues/(?P<content_type_id>[0-9]+)/$', ObjectFilterValuesWidgetView, name='loadvalues'), ]
