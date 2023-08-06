2848b5d0a2f63863d8df464256f1708064a22c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:44 +0000

    Add package chrhyme/0.1.3/chrhyme-0.1.3.tar.gz

---
+++ packages/chrhyme/chrhyme-0.1.3.tar.gz/chrhyme-0.1.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="chrhyme",
+    version="0.1.3",
+    author="Jiajie Yan",
+    author_email="jiaeyan@gmail.com",
+    description="Cleaned code a little bit.",
+    long_description=open("README.md").read(),
+    license="MIT",
+    url="https://github.com/jiaeyan/Chinese-Rhyme",
+    keywords=['chinese', 'rhymes', 'rhythm', 'rap', 'rapper', 'hip-pop', 'poem'],
+    packages=find_packages(),
+    install_requires=["pypinyin"],
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': ['chrhyme = chrhyme.__main__:main']
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ]
+)
