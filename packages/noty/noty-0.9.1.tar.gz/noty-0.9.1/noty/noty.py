029643d3630f775b2ae70215371213890b2558e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:10 +0000

    Add package noty/0.9.1/noty-0.9.1.tar.gz

---
+++ packages/noty/noty-0.9.1.tar.gz/noty-0.9.1/noty/noty.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+def cli():
+	import time
+	current_time = time.strftime("%H:%M:%S")
+	#gets the current time
+	print("Welcome to Noty.You can now create sticky notes, easily.")
+	time.sleep(1)
+	x = input("Type your notes here: ")
+	time.sleep(1)
+	#time.sleep prevents GUI from popping up before it receives input.
+	root = Tk()
+	root.title("Noty")
+	root.geometry("300x300")
+	#changes the width and height of the GUI.
+	Label(root, text=current_time).pack()
+	#prints the current time.
+	Label(root, text=x).pack()
+	#prints the input.
+	root.mainloop()
---
+++ packages/noty/noty-0.9.1.tar.gz/noty-0.9.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'noty',
+    packages=["noty"],
+    version = '0.9.1',
+    description = 'Creating sticky notes has never been easier',
+    long_desciption = open('README.md').read(),
+    author = 'GrgBls',
+    author_email = 'grgbls647@gmail.com',
+    url = 'https://github.com/GrgBls/noty',
+    py_modules=['noty'],
+    install_requires=['tkinter'],
+    entry_points='''
+        [console_scripts]
+        noty=noty.noty:cli
+    ''',
+)
