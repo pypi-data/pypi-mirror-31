951c030551abbb3f78006ba2b9f963cb3b4e17d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:41 +0000

    Add package MATE-for-Dummies/0.4.1/MATE_for_Dummies-0.4.1-py3-none-any.whl

---
+++ packages/MATE-for-Dummies/MATE_for_Dummies-0.4.1-py3-none-any.whl/mate4dummies/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright © 2015 - 2018 Stephan Zevenhuizen
+#   MATE for Dummies, (12-05-2018).
+#
+
+'''
+    MATE for Dummies. MATE Scripting System in Python
+'''
+
+__version__ = '0.4.1'
+
+__all__ = ['MATE']
+
+from .mate import *
