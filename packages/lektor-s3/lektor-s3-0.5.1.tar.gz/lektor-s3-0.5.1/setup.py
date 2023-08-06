743b74230421fe0b36a9dd6e49181c7b0e9b6421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:48 +0000

    Add package lektor-s3/0.5.1/lektor-s3-0.5.1.tar.gz

---
+++ packages/lektor-s3/lektor-s3-0.5.1.tar.gz/lektor-s3-0.5.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='lektor-s3',
+    description='Lektor plugin to support publishing to S3',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version='0.5.1',
+    author='Spencer Nelson',
+    author_email='s@spenczar.com',
+    url='https://github.com/spenczar/lektor-s3',
+    license='MIT',
+    platforms='any',
+    py_modules=['lektor_s3'],
+    entry_points={
+        'lektor.plugins': [
+            's3 = lektor_s3:S3Plugin',
+        ]
+    },
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'boto3>=1.1.4',
+        's3transfer',
+    ],
+    install_requires=[
+        'Lektor',
+        'boto3>=1.1.4',
+        's3transfer',
+        'inifile',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Lektor',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
