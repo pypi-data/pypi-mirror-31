6f351b5ff6f54f4392e64e0548b58f38aa06cd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:46 +0000

    Add package vipkid-common/0.0.1/vipkid_common-0.0.1.tar.gz

---
+++ packages/vipkid-common/vipkid_common-0.0.1.tar.gz/vipkid_common-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name="vipkid_common",
+    version="0.0.1",
+    description="vipkid's common module",
+    license = "MIT",
+    author="zhaomingming",
+    author_email="13271929138@163.com",
+    url="http://www.zhaomingming.cn",
+    py_modules=['vipkid_common'],
+    platforms = 'any'
+)
