8a0c498d219031f6ef4d9fe3435a6a8f5c5d46bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:53 +0000

    Add package odltools/0.1.3/odltools-0.1.3.tar.gz

---
+++ packages/odltools/odltools-0.1.3.tar.gz/odltools-0.1.3/odltools/netvirt/tests/test_request.py
@@ -0,0 +1,27 @@
+import logging
+import unittest
+
+import os
+
+from odltools import logg
+from odltools.netvirt import request
+
+
+class TestRequest(unittest.TestCase):
+    def setUp(self):
+        logg.Logger(logging.DEBUG, logging.DEBUG)
+        self.filename = "../../tests/resources/flow_dumps.1.txt"
+        self.outpath = "/tmp/flow_dumps.1.out.txt"
+
+    def test_read_file(self):
+        data = request.read_file(self.filename)
+        self.assertEquals(len(data), 76)
+
+    def test_write_file(self):
+        data = request.read_file(self.filename)
+        self.assertEquals(len(data), 76)
+        request.write_file(self.outpath, data)
+        self.assertTrue(os.path.exists(self.outpath))
+
+if __name__ == '__main__':
+    unittest.main()
