ccc3ddc44e00e71c387cb2b1d74d8dc727889e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package bio-minos/0.4.1/bio-minos-0.4.1.tar.gz

---
+++ packages/bio-minos/bio-minos-0.4.1.tar.gz/bio-minos-0.4.1/minos/tasks/check_with_ref.py
@@ -0,0 +1,17 @@
+from minos import mapping_based_verifier
+
+def run(options):
+    verifier = mapping_based_verifier.MappingBasedVerifier(
+        options.vcf_file,
+        options.vcf_ref,
+        options.truth_ref,
+        options.outprefix,
+        flank_length=options.flank_length,
+        expected_variants_vcf=options.expected_variants_vcf,
+        run_dnadiff=options.run_dnadiff,
+        filter_and_cluster_vcf=not options.no_filter_cluster,
+        allow_flank_mismatches=options.allow_flank_mismatches,
+        merge_length=options.variant_merge_length,
+    )
+    verifier.run()
+
