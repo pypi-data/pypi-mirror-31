7fbfd9fa37c9476b8f3d52e1deb1977ac09ecfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:48 +0000

    Add package pyreclab/0.1.10/pyreclab-0.1.10-cp27-cp27mu-manylinux1_i686.whl

---
+++ packages/pyreclab/pyreclab-0.1.10-cp27-cp27mu-manylinux1_i686.whl/pyreclab-0.1.10.data/purelib/pyreclab/__init__.py
@@ -0,0 +1,10 @@
+from pyreclab.libpyreclab import MostPopular, UserAvg, ItemAvg, SlopeOne, UserKnn, ItemKnn, SVD
+
+__all__ = [ 'MostPopular',
+            'UserAvg',
+            'ItemAvg',
+            'SlopeOne',
+            'UserKnn',
+            'ItemKnn',
+            'SVD' ]
+
