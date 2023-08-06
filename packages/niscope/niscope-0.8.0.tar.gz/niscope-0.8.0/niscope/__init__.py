a5ea53bf51dc73c5f649bc34b1ce4a84ea0e0738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package niscope/0.8.0/niscope-0.8.0.tar.gz

---
+++ packages/niscope/niscope-0.8.0.tar.gz/niscope-0.8.0/niscope/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# This file was generated
+
+from niscope.enums import *          # noqa: F403,F401,H303
+from niscope.errors import Error     # noqa: F401
+from niscope.errors import NiscopeWarning   # noqa: F401
+from niscope.session import Session  # noqa: F401
+
+from niscope.waveform_info import WaveformInfo  # noqa: F401
+
+from niscope.waveform_info import struct_niScope_wfmInfo  # noqa: F401
+
