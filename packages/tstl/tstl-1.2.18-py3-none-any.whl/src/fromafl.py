53e0562df4d789b1d2952f53e2c7208d5b30a016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:24 +0000

    Add package tstl/1.2.18/tstl-1.2.18-py3-none-any.whl

---
+++ packages/tstl/tstl-1.2.18-py3-none-any.whl/src/fromafl.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+
+import random
+import struct
+import time
+import sys
+import os
+
+# Appending current working directory to sys.path
+# So that user can run randomtester from the directory where sut.py is located
+current_working_dir = os.getcwd()
+sys.path.append(current_working_dir)
+
+import sut as SUT
+
+def main():
+    if "--help" in sys.argv:
+        print("Usage:  tstl_fromafl <outputdir> <files> [--aflswarm]")
+        sys.exit(0)
+
+    sut = SUT.sut()
+
+    aflswarm = "--aflswarm" in sys.argv
+    if aflswarm:
+        sys.argv.remove("--aflswarm")
+
+    outputDir = sys.argv[1]
+    files = sys.argv[2:]
+
+    acts = sut.actions()
+
+    i = 0
+    for f in files:
+        t = sut.loadTest(f,afl=True,swarm=aflswarm)
+        sut.saveTest(t,outputDir+"/"+os.path.basename(f)+".test")
+        i += 1
+
+
