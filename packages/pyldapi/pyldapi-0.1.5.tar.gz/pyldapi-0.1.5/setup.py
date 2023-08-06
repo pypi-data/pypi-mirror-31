f7798f7643204b208fd0ca52e26275fac8bae780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package pyldapi/0.1.5/pyldapi-0.1.5.tar.gz

---
+++ packages/pyldapi/pyldapi-0.1.5.tar.gz/pyldapi-0.1.5/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from setuptools import setup
+import io
+
+with io.open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='pyldapi',
+    packages=['pyldapi'],
+    version='0.1.5',
+    description='A library for enabling Linked Data API functionality on top of Flask',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Nicholas Car',
+    author_email='nicholas.car@csiro.au',
+    url='http://github.com/CSIRO-enviro-informatics/pyldapi',
+    download_url='https://github.com/CSIRO-enviro-informatics/pyldapi/archive/0.1.5.tar.gz',
+    keywords=['Linked pData API', 'Flask', 'HTTP API'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/CSIRO-enviro-informatics/pyldapi/issues',
+        'Source': 'https://github.com/CSIRO-enviro-informatics/pyldapi/',
+    },
+    python_requires='>=3.0',
+    install_requires=[
+        'Flask>=0.12.0',
+    ],
+)
