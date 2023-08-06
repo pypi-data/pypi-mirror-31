171f9d4a25feeee34558f0e95c713053da92760a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:41 +0000

    Add package im-task-flask/0.1.1/im_task_flask-0.1.1.tar.gz

---
+++ packages/im-task-flask/im_task_flask-0.1.1.tar.gz/im_task_flask-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+dist = setup(
+    name='im_task_flask',
+    version='0.1.1',
+    description='flask utilities for @task. Use this in your flask app',
+    author='Emlyn O\'Regan',
+    author_email='emlynoregan@gmail.com',
+    url='https://github.com/emlynoregan/im_task',
+    license='../LICENSE.txt',
+    packages=['im_task_flask'],
+    install_requires=['im_task', 'flask'],#, 'werkzeug==0.12.2'],
+    long_description=open('../README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Distributed Computing',
+    ]
+)
