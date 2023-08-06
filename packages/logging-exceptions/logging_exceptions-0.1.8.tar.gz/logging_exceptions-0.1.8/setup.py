b0cc313c8da2c3ea7ebd54796b138e8526b366ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:18 +0000

    Add package logging-exceptions/0.1.8/logging_exceptions-0.1.8.tar.gz

---
+++ packages/logging-exceptions/logging_exceptions-0.1.8.tar.gz/logging_exceptions-0.1.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os.path
+
+# Get the long description from the README file
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(name='logging_exceptions',
+      version='0.1.8',
+      py_modules=['logging_exceptions'],
+      author="Bernhard C. Thiel",
+      author_email="thiel@tbi.univie.ac.at",
+      description="Self-logging exceptions: Attach log messages to exceptions and output them conditionally.",
+      long_description=long_description,
+      url='https://github.com/Bernhard10/logging_exceptions',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5'
+      ],
+      keywords='logging exceptions'
+
+      )
