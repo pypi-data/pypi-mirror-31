ed263e81cc4152a493625d11112329aec35b647d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:06 +0000

    Add package korora/0.0.1/korora-0.0.1.tar.gz

---
+++ packages/korora/korora-0.0.1.tar.gz/korora-0.0.1/korora/__init__.py
@@ -0,0 +1,3 @@
+
+class Korora():
+    pass
---
+++ packages/korora/korora-0.0.1.tar.gz/korora-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='korora',
+      version='0.0.1',
+      description='korora backup',
+      author='Mario Verbelen',
+      author_email='mario@verbelen.org',
+      license='MIT',
+      packages=['korora']
+      )
