5a485813f08262795a35dee8de9ef84ef89a45d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:56 +0000

    Add package pybithumb/0.0.3/pybithumb-0.0.3.tar.gz

---
+++ packages/pybithumb/pybithumb-0.0.3.tar.gz/pybithumb-0.0.3/pybithumb/client.py
@@ -0,0 +1,168 @@
+from pybithumb.core import *
+
+
+class Bithumb:
+    def __init__(self, conkey, seckey):
+        self.api = privateApi(conkey, seckey)
+
+    @staticmethod
+    def get_tickers():
+        return ["BTC", "ETH", "DASH", "LTC", "ETC", "XRP", "BCH", "XMR", "ZEC", "QTUM",
+                "BTG", "EOS", "ICX", "VEN", "TRX", "ELF", "MITH", "MCO", "OMG", "KNC"]
+
+    @staticmethod
+    def get_market_detail(currency):
+        """
+        거래소 마지막 거래 정보 조회
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :return        : (24시간저가, 24시간고가, 24시간평균거래금액, 24시간거래량)
+        """
+        resp = publicApi.ticker(currency)
+        low    = resp['min_price']
+        high   = resp['max_price']
+        avg    = resp['average_price']
+        volume = resp['units_traded']
+        return float(low), float(high), float(avg), float(volume)
+
+    @staticmethod
+    def get_current_price(currency):
+        """
+        최종 체결 가격 조회
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :return        : price
+        """
+        return float(publicApi.recent_transactions(currency)[0]['price'])
+
+    @staticmethod
+    def get_orderbook(currency):
+        """
+        매수/매도 호가 조회
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :return        : 매수/매도 호가
+        """
+        return publicApi.orderbook(currency)
+
+    def get_balance(self, currency):
+        """
+        거래소 회원의 잔고 조회
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :return        : (보유코인, 사용중코인, 보유원화, 사용중원화)
+        """
+        resp = self.api.balance(currency=currency)
+        specifier = currency.lower()
+        return (float(resp["total_" + specifier]), float(resp["in_use_" + specifier]),
+                float(resp["total_krw"]), float(resp["in_use_krw"]))
+
+    def buy_limit_order(self, currency, price, unit):
+        """
+        매수 주문
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :param price   : 주문 가격
+        :param unit    : 주문 수량
+        :return        : (주문Type, currency, 주문ID)
+        """
+        unit = "{0:.4f}".format(unit)
+        order_id = self.api.place(type="bids", price=price, units=unit, order_currency=currency)
+        return "bids", currency, order_id
+
+    def sell_limit_order(self, currency, price, unit):
+        """
+        매도 주문
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :param price   : 주문 가격
+        :param unit    : 주문 수량
+        :return        : (주문Type, currency, 주문ID)
+        """
+        unit = "{0:.4f}".format(unit)
+        order_id = self.api.place(type="asks", price=price, units=unit, order_currency=currency)
+        return "asks", currency, order_id
+
+    def get_outstanding_order(self, order_desc):
+        """
+        거래 미체결 수량 조회
+        :param order_desc: (주문Type, currency, 주문ID)
+        :return          : 거래 미체결 수량
+        """
+        resp =  self.api.orders(type=order_desc[0], currency=order_desc[1], order_id=order_desc[2])
+        # HACK : 빗썸이 데이터를 리스트에 넣어줌
+        return resp[0]['units_remaining']
+
+    def cancel_order(self, order_desc):
+        """
+        매수/매도 주문 취소
+        :param order_desc: (주문Type, currency, 주문ID)
+        :return          : 성공: 0000 / 실패: message
+        """
+        return self.api.cancel(type=order_desc[0], currency=order_desc[1], order_id=order_desc[2])
+
+    def buy_market_order(self, currency, unit):
+        """
+        시장가 매수
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :param unit    : 주문수량
+        :return        : (판매단가, 판매수량)
+        """
+        resp = self.api.market_buy(currency=currency, units=unit)
+        return resp['price'], resp['units']
+
+    def sell_market_order(self, currency, unit):
+        """
+        시장가 매도
+        :param currency: BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/BTG/EOS/ICX/VEN,TRX/ELF/MITH/MCO/OMG/KNC
+        :param unit    : 주문수량
+        :return        : (판매단가, 판매수량)
+        """
+        resp = self.api.market_sell(currency=currency, units=unit)
+        return resp['price'], resp['units']
+
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------------------------------
+    # 최종 체결 가격
+    # ----------------------------------------------------------------------------------------------
+    # for coin in Bithumb.get_tickers():
+    #     print(coin, Bithumb.get_current_price(coin))
+
+    # ----------------------------------------------------------------------------------------------
+    # 시장 현황 상세정보
+    # ----------------------------------------------------------------------------------------------
+    # for coin in  Bithumb.get_tickers():
+    #     print(coin, Bithumb.get_market_detail(coin))
+
+    # ----------------------------------------------------------------------------------------------
+    # 매수/매도 호가
+    # ----------------------------------------------------------------------------------------------
+    # for coin in  Bithumb.get_tickers():
+    #     print(coin, Bithumb.get_orderbook(coin))
+
+    import csv
+    keys = next(csv.reader(open("keys.csv")))
+    bithumb = Bithumb(keys[0].strip(), keys[1].strip())
+
+    # ----------------------------------------------------------------------------------------------
+    # 잔고 조회
+    # ----------------------------------------------------------------------------------------------
+    # for coin in Bithumb.get_tickers():
+    #     print(coin, bithumb.get_balance(coin))
+
+
+    # ----------------------------------------------------------------------------------------------
+    # 매도 주문
+    # ----------------------------------------------------------------------------------------------
+    # desc = bithumb.sell_limit_order("BTC", 11000000, 0.00844498)
+    # print(desc)
+    # desc = bithumb.buy_limit_order("BTC", 11000000, 0.00844498)
+    # print(desc)
+
+    # ----------------------------------------------------------------------------------------------
+    # 매수/매도 잔량 확인
+    # ----------------------------------------------------------------------------------------------
+    # quanity = bithumb.get_outstanding_order(desc)
+    # print(quanity)
+
+    # ----------------------------------------------------------------------------------------------
+    # 매수/매도 주문 취소
+    # ----------------------------------------------------------------------------------------------
+    # resp = bithumb.cancel_order(desc)
+    # print(resp)
+
---
+++ packages/pybithumb/pybithumb-0.0.3.tar.gz/pybithumb-0.0.3/pybithumb/core.py
@@ -0,0 +1,111 @@
+import base64
+import hashlib
+import hmac
+import time
+import urllib
+import requests
+
+
+class publicApi:
+    @staticmethod
+    def ticker(currency):
+        uri = "/public/ticker/" + currency
+        return BithumbHttp().get(uri)
+
+    @staticmethod
+    def recent_transactions(currency):
+        uri = "/public/recent_transactions/" + currency
+        return BithumbHttp().get(uri)
+
+    @staticmethod
+    def orderbook(currency):
+        uri = "/public/orderbook/" + currency
+        return BithumbHttp().get(uri)
+
+
+class privateApi:
+    def __init__(self, conkey, seckey):
+        self.http = BithumbHttp(conkey, seckey)
+
+    def balance(self, **kwargs):
+        return self.http.post('/info/balance', **kwargs)
+
+    def place(self, **kwargs):
+        return self.http.post('/trade/place', **kwargs)
+
+    def orders(self, **kwargs):
+        return self.http.post('/info/orders', **kwargs)
+
+    def cancel(self, **kwargs):
+        return self.http.post('/trade/cancel', **kwargs)
+
+    def market_buy(self, **kwargs):
+        return self.http.post('/trade/market_buy', **kwargs)
+
+    def market_sell(self, **kwargs):
+        return self.http.post('/trade/market_sell', **kwargs)
+
+
+class HttpMethod:
+    def __init__(self):
+        self.session = requests.session()
+
+    @property
+    def base_url(self):
+        return ""
+
+    def _handle_response(self, response):
+        return response.json()
+
+    def update_headers(self, headers):
+        self.session.headers.update(headers)
+
+    def post(self, path, timeout=3, **kwargs):
+        uri = self.base_url + path
+        response = self.session.post(url=uri, data=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+    def get(self, path, timeout=3, **kwargs):
+        uri = self.base_url + path
+        response = self.session.get(url=uri, params=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+
+class BithumbHttp(HttpMethod):
+    def __init__(self, conkey="", seckey=""):
+        self.API_CONKEY = conkey.encode('utf-8')
+        self.API_SECRET = seckey.encode('utf-8')
+        super(BithumbHttp, self).__init__()
+
+    @property
+    def base_url(self):
+        return "https://api.bithumb.com"
+
+    def _signature(self, path, nonce, **kwargs):
+        query_string = path + chr(0) + urllib.parse.urlencode(kwargs) + chr(0) + nonce
+        h = hmac.new(self.API_SECRET, query_string.encode('utf-8'), hashlib.sha512)
+        return base64.b64encode(h.hexdigest().encode('utf-8'))
+
+    def post(self, path, **kwargs):
+        kwargs['endpoint'] = path
+        nonce = str(int(time.time() * 1000))
+
+        self.update_headers({
+            'Api-Key': self.API_CONKEY,
+            'Api-Sign': self._signature(path, nonce, **kwargs),
+            'Api-Nonce': nonce
+        })
+        return super().post(path, **kwargs)
+
+    def _handle_response(self, response):
+        response = super()._handle_response(response)
+        if response['status'] != '0000':
+            return [response['message']]
+        # [예외] buy/sell API의 결괏값은 'order_id'로 전송
+        if response.get('order_id'):
+            return response['order_id']
+        # [예외] calcel API의 결괏값은 'status'만 전송
+        if not response.get('data'):
+            return response['status']
+        # 빗썸 API 대부분의 정보는 'data'로 전송
+        return response['data']
