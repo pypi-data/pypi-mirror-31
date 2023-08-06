35f384156ac2e1679ae9c5729142722fcb19d678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:36 +0000

    Add package dsdobjects/0.5.2/dsdobjects-0.5.2-py2-none-any.whl

---
+++ packages/dsdobjects/dsdobjects-0.5.2-py2-none-any.whl/dsdobjects/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# dsdobjects
+#
+# Written by Stefan Badelt (badelt@caltech.edu)
+#
+# Distributed under the MIT License, use at your own risk.
+#
+
+__version__='0.5.2'
+
+from dsdobjects.base_classes import clear_memory
+from dsdobjects.base_classes import DSDObjectsError, DSDDuplicationError 
+from dsdobjects.base_classes import DL_Domain, SL_Domain, DSD_Complex, DSD_RestingSet, DSD_Reaction
+
