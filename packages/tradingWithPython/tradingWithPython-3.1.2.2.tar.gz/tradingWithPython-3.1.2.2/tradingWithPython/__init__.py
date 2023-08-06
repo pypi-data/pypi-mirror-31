6d8c5eead18df0ea9cfe1ada2b21c7848e6e3b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:46 +0000

    Add package tradingWithPython/3.1.2.2/tradingWithPython-3.1.2.2.tar.gz

---
+++ packages/tradingWithPython/tradingWithPython-3.1.2.2.tar.gz/tradingWithPython-3.1.2.2/tradingWithPython/__init__.py
@@ -0,0 +1,11 @@
+__docformat__ = 'restructuredtext'
+
+# python ver, major ,  minor, bugfix
+__version__ = '3.1.2.2'
+
+from .lib import extra
+from .lib import yahooFinance
+from .lib import backtest
+from .lib.functions import *
+from .lib import cboe
+
