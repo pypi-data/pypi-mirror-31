e64803ff352256c04cc4eff824e2273eb11c07be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package FreeGS/0.1.2/FreeGS-0.1.2-py2.py3-none-any.whl

---
+++ packages/FreeGS/FreeGS-0.1.2-py2.py3-none-any.whl/freegs/geqdsk.py
@@ -0,0 +1,255 @@
+"""
+Handles reading and writing of Equilibrium objects
+
+Writing is relatively straightforward, but reading requires inferring
+the currents in the PF coils
+
+Copyright 2016 Ben Dudson, University of York. Email: benjamin.dudson@york.ac.uk
+
+This file is part of FreeGS.
+
+FreeGS is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreeGS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with FreeGS.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from . import _geqdsk
+from . import critical
+from .equilibrium import Equilibrium
+from .machine import Wall
+from . import jtor
+from . import control
+from . import picard
+
+from scipy import interpolate
+from numpy import linspace, amin, amax, reshape, ravel, zeros, argmax, clip, sin, cos, pi
+
+def write(eq, fh, label=None, oxpoints=None, fileformat=_geqdsk.write):
+    """
+    Write a GEQDSK equilibrium file, given a FreeGS Equilibrium object
+    
+    eq - Equilibrium object
+    fh - file handle
+    
+    label - Text label to put in the file
+    oxpoints - O- and X-points  (opoint, xpoint) returned by critical.find_critical
+          If not given, it will be calculated using critical.find_critical
+    """
+    # Get poloidal flux
+    psi = eq.psi()
+
+    # Get size of the grid
+    nx,ny = psi.shape
+
+    if oxpoints:
+        opoint, xpoint = oxpoints
+    else:
+        # Find the O- and X-points
+        opoint, xpoint = critical.find_critical(eq.R, eq.Z, psi)
+
+    rmin = eq.Rmin
+    rmax = eq.Rmax
+    zmin = eq.Zmin
+    zmax = eq.Zmax
+
+    fvac = eq.fvac() # Vacuum f = R*Bt
+    R0 = 1.0 # Reference location
+    B0 = fvac / R0 # Reference vacuum toroidal magnetic field
+    
+    data = {"nx":nx, "ny":ny,
+            "rdim":rmax-rmin, # Horizontal dimension in meter of computational box
+            "zdim":zmax-zmin, # Vertical dimension in meter of computational box
+            "rcentr":R0, # R in meter of vacuum toroidal magnetic field BCENTR
+            "bcentr":fvac/R0, # Vacuum magnetic field at rcentr
+            "rleft":rmin, # Minimum R in meter of rectangular computational box
+            "zmid":0.5*(zmin + zmax)} # Z of center of computational box in meter
+            
+    
+    data["rmagx"], data["zmagx"], data["simagx"] = opoint[0] # magnetic axis
+    
+    data["sibdry"] = xpoint[0][2]  # Psi at boundary
+    
+    data["cpasma"] = eq.plasmaCurrent() # Plasma current [A]
+
+    psinorm = linspace(0.0, 1.0, nx, endpoint=False) # Does not include separatrix
+
+    data["fpol"] = eq.fpol(psinorm)
+    data["pres"] = eq.pressure(psinorm)
+
+    data["psi"] = psi
+    
+    qpsi = zeros([nx])
+    qpsi[1:] = eq.q(psinorm[1:]) # Exclude axis
+    qpsi[0] = qpsi[1]
+    data["qpsi"] = qpsi
+    
+    if eq.tokamak.wall:
+        data["rlim"] = eq.tokamak.wall.R
+        data["zlim"] = eq.tokamak.wall.Z
+    
+    # Call fileformat to write the data
+    fileformat(data, fh, label=label)
+
+import matplotlib.pyplot as plt
+
+def read(fh, machine, rtol=1e-3, ntheta=8, show=False, axis=None):
+    """
+    Reads a G-EQDSK format file
+    
+    fh      - file handle
+    machine - a Machine object defining coil locations
+    rtol    - Relative error in nonlinear solve
+    ntheta  - Number of points in poloidal angle on the separatrix
+              this is used to constrain the plasma shape
+    show    - Set to true to show solution in a new figure
+    axis    - Set to an axis for plotting. Implies show=True
+    
+    A nonlinear solve will be performed, using Picard iteration
+    
+    Returns
+    -------
+
+    An Equilibrium object eq. In addition, the following is available:
+
+    eq.control   - The control system
+    eq._profiles - The profiles object
+    
+    """
+
+    if axis is not None:
+        show = True
+
+    # Read the data as a Dictionary
+    data = _geqdsk.read(fh)
+
+    # If data contains a limiter, set the machine wall
+    if "rlim" in data and len(data["rlim"]) > 0:
+        machine.wall = Wall(data["rlim"], data["zlim"])
+    
+    # Create an Equilibrium object
+    eq = Equilibrium(tokamak = machine,
+                     Rmin = data["rleft"],
+                     Rmax = data["rleft"] + data["rdim"],
+                     Zmin = data["zmid"] - 0.5*data["zdim"],
+                     Zmax = data["zmid"] + 0.5*data["zdim"],
+                     nx=data["nx"], ny=data["ny"]         # Number of grid points
+                 )
+
+    # Range of psi normalises psi derivatives
+    psirange = data["sibdry"] - data["simagx"]
+    
+    psinorm = linspace(0.0, 1.0, data["nx"], endpoint=True)
+    
+    # Create a spline fit to pressure, f and f**2
+    p_spl = interpolate.InterpolatedUnivariateSpline(psinorm, data["pres"])
+    pprime_spl = interpolate.InterpolatedUnivariateSpline(psinorm, data["pres"] / psirange).derivative()
+    
+    f_spl = interpolate.InterpolatedUnivariateSpline(psinorm, data["fpol"])
+    ffprime_spl = interpolate.InterpolatedUnivariateSpline(psinorm, 0.5*data["fpol"]**2/psirange).derivative() 
+
+    # functions to return p, pprime, f and ffprime
+    def p_func(psinorm):
+        if hasattr(psinorm, "shape"):
+            return reshape(p_spl(ravel(psinorm)),psinorm.shape)
+        return p_spl(psinorm)
+
+    def f_func(psinorm):
+        if hasattr(psinorm, "shape"):
+            return reshape(f_spl(ravel(psinorm)),psinorm.shape)
+        return f_spl(psinorm)
+
+    def pprime_func(psinorm):
+        if hasattr(psinorm, "shape"):
+            return reshape(pprime_spl(ravel(psinorm)),psinorm.shape)
+        return pprime_spl(psinorm)
+    
+    def ffprime_func(psinorm):
+        if hasattr(psinorm, "shape"):
+            return reshape(ffprime_spl(ravel(psinorm)),psinorm.shape)
+        return ffprime_spl(psinorm)
+
+    
+    # Create a set of profiles to calculate toroidal current density Jtor
+    profiles = jtor.ProfilesPprimeFfprime(pprime_func,
+                                          ffprime_func,
+                                          data["rcentr"] * data["bcentr"],
+                                          p_func=p_func, 
+                                          f_func=f_func)
+
+    # Use these profiles to calculate plasma psi
+    # This requires a bit of a hack to set the poloidal flux
+    
+    coil_psi = machine.psi(eq.R, eq.Z)
+    eq._updatePlasmaPsi(data["psi"] - coil_psi)
+    
+    # Perform a linear solve, calculating plasma psi
+    eq.solve(profiles)
+
+    print("Plasma current: {0} Amps, input: {1} Amps".format(eq.plasmaCurrent(), data["cpasma"]))
+    
+    # Identify points to constrain: X-points, O-points and separatrix shape
+
+    psi_in = interpolate.RectBivariateSpline(eq.R[:,0], eq.Z[0,:], data["psi"])
+    
+    # Find all the O- and X-points
+    opoint, xpoint = critical.find_critical(eq.R, eq.Z, data["psi"])
+    
+    if show:
+        if axis is None:
+            fig = plt.figure()
+            axis = fig.add_subplot(111)
+        axis.set_aspect('equal')
+        axis.set_xlabel("Major radius [m]")
+        axis.set_ylabel("Height [m]")
+    
+        levels = linspace(amin(data["psi"]), amax(data["psi"]), 50)
+        axis.contour(eq.R,eq.Z,data["psi"], levels=levels, colors='k')
+        
+        # Put red dots on X-points
+        for r,z,_ in xpoint:
+            axis.plot(r,z,'ro')
+
+        sep_contour=axis.contour(eq.R, eq.Z, data["psi"], levels=[xpoint[0][2]], colors='r')
+        
+    # Find the separatrix
+    isoflux = critical.find_separatrix(eq, opoint, xpoint, ntheta=ntheta, psi=data["psi"], axis=axis)
+
+    # Find best fit for coil currents
+    controlsystem = control.constrain(xpoints=xpoint, isoflux=isoflux, gamma=1e-14)
+    controlsystem(eq)
+    
+    if show:
+        axis.contour(eq.R,eq.Z, eq.psi(), levels=levels, colors='r')
+        plt.pause(1)
+    
+    # Print the coil currents
+    machine.printCurrents()
+
+    ####################################################################
+    # Refine the equilibrium to ensure consistency 
+    # Solve using Picard iteration
+    #
+    
+    picard.solve(eq,          # The equilibrium to adjust
+                 profiles,    # The toroidal current profile function
+                 controlsystem, show=show, axis=axis,
+                 rtol=rtol)
+    
+    print("Plasma current: {0} Amps, input: {1} Amps".format(eq.plasmaCurrent(), data["cpasma"]))
+    print("Plasma pressure on axis: {0} Pascals".format(eq.pressure(0.0)))
+    machine.printCurrents()
+
+    # Save the control system to eq
+    eq.control = controlsystem
+    
+    return eq
---
+++ packages/FreeGS/FreeGS-0.1.2-py2.py3-none-any.whl/freegs/machine.py
@@ -0,0 +1,544 @@
+"""
+Classes and routines to represent coils and circuits
+
+License
+-------
+
+Copyright 2016 Ben Dudson, University of York. Email: benjamin.dudson@york.ac.uk
+
+This file is part of FreeGS.
+
+FreeGS is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreeGS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with FreeGS.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from .gradshafranov import Greens, GreensBr, GreensBz
+
+from numpy import linspace
+
+class Coil:
+    """
+    Represents a poloidal field coil
+
+    public members
+    --------------
+    
+    R, Z - Location of the coil
+    current - current in the coil
+    control - enable or disable control system
+    
+    """
+    
+    def __init__(self, R, Z, current=0.0, control=True):
+        """
+        R, Z - Location of the coil
+        
+        current - current in the coil
+        control - enable or disable control system
+        """
+        self.R = R
+        self.Z = Z
+        
+        self.current = current
+        self.control = control
+
+    def psi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z)
+        """
+        return self.controlPsi(R,Z) * self.current
+
+    def createPsiGreens(self, R, Z):
+        """
+        Calculate the Greens function at every point, and return
+        array. This will be passed back to evaluate Psi in
+        calcPsiFromGreens()
+        """
+        return self.controlPsi(R,Z)
+
+    def calcPsiFromGreens(self, pgreen):
+        """
+        Calculate plasma psi from Greens functions and current
+        """
+        return self.current * pgreen
+
+    def Br(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z)
+        """
+        return self.controlBr(R,Z) * self.current
+
+    def Bz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z)
+        """
+        return self.controlBz(R,Z) * self.current
+
+    def controlPsi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z) due to a unit current
+        """
+        return Greens(self.R, self.Z, R, Z)
+        
+    def controlBr(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z) due to a unit current
+        """
+        return GreensBr(self.R,self.Z, R, Z)
+        
+    def controlBz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z) due to a unit current
+        """
+        return GreensBz(self.R,self.Z, R, Z)
+        
+    def __repr__(self):
+        return "Coil(R={0},Z={1},current={2},control={3})".format(self.R, self.Z, self.current, self.control)
+
+
+class Circuit:
+    """
+    Represents a collection of coils connected to the same circuit
+    
+    Public members
+    --------------
+    
+    current  Current in the circuit [Amps]
+    control  Use feedback control? [bool]
+    """
+    def __init__(self, coils, current=0.0, control=True):
+        """
+        coils - A list [ (label, Coil, multiplier) ]
+        """
+        
+        self.coils = coils
+        self.current = current
+        self.control = control
+
+    def psi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z)
+        """
+        psival = 0.0
+        for label, coil, multiplier in self.coils:
+            coil.current = self.current * multiplier
+            psival += coil.psi(R,Z)
+        return psival
+
+    def createPsiGreens(self, R, Z):
+        """
+        Calculate Greens functions
+        """
+        pgreen = {}
+        for label, coil, multiplier in self.coils:
+            pgreen[label] = coil.createPsiGreens(R, Z)
+        return pgreen
+
+    def calcPsiFromGreens(self, pgreen):
+        """
+        Calculate psi from Greens functions
+        
+        """
+        psival = 0.0
+        for label, coil, multiplier in self.coils:
+            coil.current = self.current * multiplier
+            psival += coil.calcPsiFromGreens(pgreen[label])
+        return psival
+        
+    def Br(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z)
+        """
+        result = 0.0
+        for label, coil, multiplier in self.coils:
+            coil.current = self.current * multiplier
+            result += coil.Br(R,Z)
+        return result
+
+    def Bz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z)
+        """
+        result = 0.0
+        for label, coil, multiplier in self.coils:
+            coil.current = self.current * multiplier
+            result += coil.Bz(R,Z)
+        return result
+
+    def controlPsi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z) due to a unit current
+        """
+        result = 0.0
+        for label, coil, multiplier in self.coils:
+            result += multiplier * coil.controlPsi(R, Z)
+        return result
+        
+    def controlBr(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z) due to a unit current
+        """
+        result = 0.0
+        for label, coil, multiplier in self.coils:
+            result += multiplier * coil.controlBr(R, Z)
+        return result
+        
+    def controlBz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z) due to a unit current
+        """
+        result = 0.0
+        for label, coil, multiplier in self.coils:
+            result += multiplier * coil.controlBz(R, Z)
+        return result
+        
+    def __repr__(self):
+        result = "Circuit( " 
+        for label, coil, multiplier in self.coils:
+            result += label+ ":" + str(coil)+ " "
+        return result + ")"
+        
+    
+
+class Solenoid:
+    """
+    Represents a central solenoid
+
+    Public members
+    --------------
+    
+    current - current in each turn
+    control - enable or disable control system
+    
+    """
+    def __init__(self, Rs, Zsmin, Zsmax, Ns, current=0.0, control=True):
+        """
+        Rs - Radius of the solenoid
+        Zsmin, Zsmax - Minimum and maximum Z
+        Ns - Number of turns
+        
+        current - current in each turn
+        control - enable or disable control system
+        """
+        self.Rs = Rs
+        self.Zsmin = Zsmin
+        self.Zsmax = Zsmax
+        self.Ns = Ns
+        
+        self.current = current
+        self.control = control
+        
+    def psi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z)
+        """
+        return self.controlPsi(R,Z) * self.current
+
+    def createPsiGreens(self, R, Z):
+        """
+        Calculate Greens functions
+        """
+        return self.controlPsi(R,Z)
+
+    def calcPsiFromGreens(self, pgreen):
+        """
+        Calculate psi from Greens functions
+        """
+        return self.current * pgreen
+
+    def Br(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z)
+        """
+        return self.controlBr(R,Z) * self.current
+
+    def Bz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z)
+        """
+        return self.controlBz(R,Z) * self.current
+
+    def controlPsi(self, R, Z):
+        """
+        Calculate poloidal flux at (R,Z) due to a unit current
+        
+        R and Z should have the same dimensions, but can be multi-dimensional
+        Return should have the same shape
+        """
+        result = 0.0
+        for Zs in linspace(self.Zsmin, self.Zsmax, self.Ns):
+            result += Greens(self.Rs, Zs, R, Z)
+        return result
+        
+    def controlBr(self, R, Z):
+        """
+        Calculate radial magnetic field Br at (R,Z) due to a unit current
+        """
+        result = 0.0
+        for Zs in linspace(self.Zsmin, self.Zsmax, self.Ns):
+            result += GreensBr(self.Rs, Zs, R, Z)
+        return result
+        
+    def controlBz(self, R, Z):
+        """
+        Calculate vertical magnetic field Bz at (R,Z) due to a unit current
+        """
+        result = 0.0
+        for Zs in linspace(self.Zsmin, self.Zsmax, self.Ns):
+            result += GreensBz(self.Rs, Zs, R, Z)
+        return result
+
+    def __repr__(self):
+        return "Solenoid(R={0},Zmin={1},Zmax={2},current={3},N={4},control={5})".format(self.Rs, self.Zsmin, self.Zsmax, self.current, self.Ns, self.control)
+
+
+class Wall:
+    """
+    Represents the wall of the device. 
+    Consists of an ordered list of (R,Z) points
+    """
+    def __init__(self, R, Z):
+        self.R = R
+        self.Z = Z
+    
+    
+class Machine:
+    """
+    Represents the machine (Tokamak), including
+    coils and power supply circuits
+    
+    coils[(label, Coil|Circuit|Solenoid] - List of coils
+    
+    Note: a list is used rather than a dict, so that the coils
+    remain ordered, and so can be updated easily by the control system.
+    Instead __getitem__ is implemented to allow access to coils
+
+    """
+
+    def __init__(self, coils, wall=None):
+        """
+        coils - A list of coils [(label, Coil|Circuit|Solenoid)]
+        """
+        
+        self.coils = coils
+        self.wall = wall
+
+    def __getitem__(self, name):
+        for label, coil in self.coils:
+            if label == name:
+                return coil
+            raise KeyError("Machine does not contain coil with label '{0}'".format(name))
+        
+    def psi(self, R, Z):
+        """
+        Poloidal flux due to coils
+        """
+        psi_coils = 0.0
+        for label, coil in self.coils:
+            psi_coils += coil.psi(R, Z)
+        
+        return psi_coils
+
+    def createPsiGreens(self, R, Z):
+        """
+        An optimisation, which pre-computes the Greens functions
+        and puts into arrays for each coil. This map can then be
+        called at a later time, and quickly return the field
+        """
+        pgreen = {}
+        for label, coil in self.coils:
+            pgreen[label] = coil.createPsiGreens(R, Z)
+        return pgreen
+        
+    def calcPsiFromGreens(self, pgreen):
+        """
+        Uses the object returned by createPsiGreens to quickly
+        compute the plasma psi
+        """
+        psi_coils = 0.0
+        for label, coil in self.coils:
+            psi_coils += coil.calcPsiFromGreens(pgreen[label])
+        return psi_coils
+
+    def Br(self, R, Z):
+        """
+        Radial magnetic field at given points
+        """
+        Br = 0.0;    
+        for label, coil in self.coils:
+            Br += coil.Br(R,Z)
+        
+        return Br
+    
+    def Bz(self, R, Z):
+        """
+        Vertical magnetic field
+        """
+        Bz = 0.0
+        for label, coil in self.coils:
+            Bz += coil.Bz(R, Z)
+        
+        return Bz
+
+    def controlBr(self, R, Z):
+        """
+        Returns a list of control responses for Br
+        at the given (R,Z) location(s).
+        """
+        return [ coil.controlBr(R,Z)
+                 for label, coil in self.coils if coil.control ]
+
+    def controlBz(self, R, Z):
+        """
+        Returns a list of control responses for Bz
+        at the given (R,Z) location(s)
+        """
+        return [ coil.controlBz(R,Z)
+                 for label, coil in self.coils if coil.control ]
+
+    def controlPsi(self, R, Z):
+        """
+        Returns a list of control responses for psi
+        at the given (R,Z) location(s)
+        """
+        return [ coil.controlPsi(R,Z)
+                 for label, coil in self.coils if coil.control]
+        
+
+    def controlAdjust(self, current_change):
+        """
+        Add given currents to the controls.
+        Given iterable must be the same length
+        as the list returned by controlBr, controlBz
+        """
+        
+        for lc, dI in zip(self.coils, current_change):
+            label,coil = lc
+            coil.current += dI
+    
+    def printCurrents(self):
+        print("==========================")
+        for label, coil in self.coils:
+            print(label + " : " + str(coil))
+        print("==========================")
+        
+        
+def EmptyTokamak():
+    """
+    Creates a tokamak with no coils
+    """
+    return Machine([])
+
+def TestTokamak():
+    """
+    Create a simple tokamak 
+    """
+    
+    coils = [("P1L", Coil(1.0, -1.1)),
+             ("P1U", Coil(1.0, 1.1)),
+             ("P2L", Coil(1.75, -0.6)),
+             ("P2U", Coil(1.75, 0.6))]
+
+    wall = Wall([ 0.75, 0.75,  1.5,  1.8,   1.8,   1.5],   # R
+                [-0.85, 0.85, 0.85, 0.25, -0.25, -0.85])   # Z
+    
+    return Machine(coils, wall)
+
+
+def DIIID():
+    """
+    PF coil set from ef20030203.d3d
+    Taken from Corsica
+    """
+
+    coils = [{"label":"F1A", "R":0.8608, "Z":0.16830, "current":0.0},
+             {"label":"F2A", "R":0.8614, "Z":0.50810, "current":0.0},
+             {"label":"F3A", "R":0.8628, "Z":0.84910, "current":0.0},
+             {"label":"F4A", "R":0.8611, "Z":1.1899 , "current":0.0},
+             {"label":"F5A", "R":1.0041, "Z":1.5169 , "current":0.0},
+             {"label":"F6A", "R":2.6124, "Z":0.4376 , "current":0.0},
+             {"label":"F7A", "R":2.3733, "Z":1.1171 , "current":0.0},
+             {"label":"F8A", "R":1.2518, "Z":1.6019 , "current":0.0},
+             {"label":"F9A", "R":1.6890, "Z":1.5874 , "current":0.0},
+             {"label":"F1B", "R":0.8608, "Z":-0.1737, "current":0.0},
+             {"label":"F2B", "R":0.8607, "Z":-0.5135, "current":0.0},
+             {"label":"F3B", "R":0.8611, "Z":-0.8543, "current":0.0},
+             {"label":"F4B", "R":0.8630, "Z":-1.1957, "current":0.0},
+             {"label":"F5B", "R":1.0025, "Z":-1.5169, "current":0.0},
+             {"label":"F6B", "R":2.6124, "Z":-0.44376, "current":0.0},
+             {"label":"F7B", "R":2.3834, "Z":-1.1171, "current":0.0},
+             {"label":"F8B", "R":1.2524, "Z":-1.6027, "current":0.0},
+             {"label":"F9B", "R":1.6889, "Z":-1.578, "current":0.0}]
+    
+    return Machine(coils)
+
+def MAST():
+    """
+    Mega-Amp Spherical Tokamak. This version has all independent coils 
+    so that each is powered by a separate coil
+    """
+    
+    coils = [("P2U", Coil(0.49, 1.76)),
+             ("P2L", Coil(0.49, -1.76)),
+             ("P3U", Coil(1.1, 1.1)),
+             ("P3L", Coil(1.1, -1.1)),
+             ("P4U", Coil(1.51, 1.095)),
+             ("P4L", Coil(1.51, -1.095)),
+             ("P5U", Coil(1.66, 0.52)),
+             ("P5L", Coil(1.66, -0.52)),
+             ("P6U", Coil(1.5, 0.9)),
+             ("P6L", Coil(1.5, -0.9))
+             ,("P1", Solenoid(0.15, -1.4, 1.4, 100))
+             ] 
+    
+    return Machine(coils)
+
+def MAST_sym():
+    """
+    Mega-Amp Spherical Tokamak. This version the upper and lower coils 
+    are connected to the same circuits P2 - P6
+    """
+    coils = [("P2", Circuit( [("P2U", Coil(0.49,  1.76), 1.0),
+                              ("P2L", Coil(0.49, -1.76),1.0)] ))
+             ,("P3", Circuit( [("P3U", Coil(1.1,  1.1), 1.0),
+                              ("P3L", Coil(1.1, -1.1), 1.0)] ))
+             ,("P4", Circuit( [("P4U", Coil(1.51,  1.095), 1.0),
+                              ("P4L", Coil(1.51, -1.095), 1.0)] ))
+             ,("P5", Circuit( [("P5U", Coil(1.66,  0.52), 1.0),
+                              ("P5L", Coil(1.66, -0.52), 1.0)] ))
+             ,("P6", Circuit( [("P6U", Coil(1.5,  0.9), 1.0),
+                               ("P6L", Coil(1.5, -0.9), -1.0)] ))
+             ,("P1", Solenoid(0.15, -1.45, 1.45, 100))
+         ]
+    
+    return Machine(coils)
+
+def TCV():
+    coils = ["OH", Circuit( [ ("A1", Coil(0.4, 0.0), 1.0),
+                              ("B1", Coil(0.4, -0.95), 1.0),
+                              ("B2", Coil(0.4, 0.95), 1.0),
+                              ("C1", Coil(0.6, -1.1), 1.0),
+                              ("C2", Coil(0.6, 1.1), 1.0),
+                              ("D1", Coil(1.2, -1.15), 1.0),
+                              ("D2", Coil(1.2, 1.15), 1.0)] ) ]
+
+    return Machine(coils)
+
+if __name__ == "__main__":
+    # Run test case
+    
+    # Define a machine with a single coil
+    coils = [{"R":2.0, "Z":"-1.5", "label":"P1", "current":3.0}]
+    tokamak = Machine(coils)
+    
+    
