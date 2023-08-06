ecba83aa93dd3ca15957c1f1488a157bfbdc90aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:42 +0000

    Add package FTPwalker/0.4.5/FTPwalker-0.4.5.tar.gz

---
+++ packages/FTPwalker/FTPwalker-0.4.5.tar.gz/FTPwalker-0.4.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='FTPwalker',
+      packages=['FTPwalker', "FTPwalker/daemons"],
+      version='0.4.5',
+      description='Optimally traversing extremely large FTP directory trees.',
+      author='Bohdan Khomtchouk and Kasra Vand',
+      author_email='khomtchoukmed@gmail.com, kasraavand@gmail.com',
+      url='https://github.com/Bohdan-Khomtchouk/FTPwalker',
+      download_url='https://github.com/Bohdan-Khomtchouk/FTPwalker/tarball/0.1',
+      keywords=['FTP', 'traverse', 'directory tree', 'optimized'],
+      classifiers=[],
+      long_description=read("README"))
