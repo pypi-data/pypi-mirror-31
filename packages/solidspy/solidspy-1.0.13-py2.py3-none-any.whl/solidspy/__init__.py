0f83cb16e114a0dc427a6971487be53f51707eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:13 +0000

    Add package solidspy/1.0.13/solidspy-1.0.13-py2.py3-none-any.whl

---
+++ packages/solidspy/solidspy-1.0.13-py2.py3-none-any.whl/solidspy/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from solidspy.solids_GUI import solids_GUI
+
+__all__ = ["assemutil",
+           "femutil",
+           "gaussutil",
+           "postprocesor",
+           "preprocesor",
+           "solutil",
+           "uelutil",
+           "solids_GUI"]
