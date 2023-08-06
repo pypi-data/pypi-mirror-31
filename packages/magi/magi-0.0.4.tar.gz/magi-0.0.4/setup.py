1cde2cad2002be1cb6a46939a0a59060e7ed733f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:42 +0000

    Add package magi/0.0.4/magi-0.0.4.tar.gz

---
+++ packages/magi/magi-0.0.4.tar.gz/magi-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='magi',
+      version='0.0.4',
+      description='parallelized univariate time series forecasting library for python',
+      long_description='This package is provides a python wrapper around other time series analysis libraries such as the forecast package in R and the Prophet library. This new layer of abstraction makes it very simple to put many different types of univariate time series models into production by using Dask as the parallelization layer',
+      url='http://github.com/DavisTownsend/forecast',
+      author='Davis Townsend',
+      author_email='dtownsend@ea.com',
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+      ],
+      keywords='time series analysis forecast forecasting predict model parallel',
+      license='MIT',
+      packages=['magi'],
+      python_requires='~=3.5',
+      install_requires=['numpy','pandas','dask','distributed','pystan','rpy2','fbprophet','plotly','cufflinks'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      zip_safe=False)
