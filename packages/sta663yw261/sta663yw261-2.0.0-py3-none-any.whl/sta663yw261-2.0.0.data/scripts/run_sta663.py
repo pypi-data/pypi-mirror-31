dd367a470d9407b777ac2f3847f3d49143795abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package sta663yw261/2.0.0/sta663yw261-2.0.0-py3-none-any.whl

---
+++ packages/sta663yw261/sta663yw261-2.0.0-py3-none-any.whl/sta663yw261-2.0.0.data/scripts/run_sta663.py
@@ -0,0 +1,13 @@
+from pkg import dist 
+from pkg import k_means_int
+from pkg import k_means_pp
+from pkg import k_means
+from pkg import scalable_k_means_pp
+
+print dist.dist()
+print k_means_int.k_means_int()
+print k_means_pp.k_meanspp()
+print k_means.k_means()
+print scalable_k_means_pp.weights_c()
+print scalable_k_means_pp.weighted_kmeans()
+print scalable_k_means_pp.k_meansll()
