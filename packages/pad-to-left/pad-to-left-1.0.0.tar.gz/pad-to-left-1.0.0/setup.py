f1be4490fad0492ea4deef845b63e532f44f1e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:32 +0000

    Add package pad-to-left/1.0.0/pad-to-left-1.0.0.tar.gz

---
+++ packages/pad-to-left/pad-to-left-1.0.0.tar.gz/pad-to-left-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pad-to-left',
+    version='1.0.0',
+    packages=['pad_to_left'],
+    url='https://github.com/giswqs/pad-to-left',
+    license='Apache 2.0',
+    author='Qiusheng Wu',
+    author_email='giswqs@gmail.com',
+    description='A python implementation of left-pad'
+)
