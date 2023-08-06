a9843747e2a0c755237472057b32bb36a14a8dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:42 +0000

    Add package semaphore/0.0/semaphore-0.0.tar.gz

---
+++ packages/semaphore/semaphore-0.0.tar.gz/semaphore-0.0/setup.py
@@ -0,0 +1,18 @@
+import re
+import ast
+from setuptools import setup
+
+
+setup(
+    name='semaphore',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='0.0',
+    url='http://github.com/getsentry/semaphore',
+    py_module='semaphore',
+    description='A bridge to the sentry semaphore',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+)
