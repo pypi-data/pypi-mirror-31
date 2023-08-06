35d9206eeb555642240572db41ac2365e4fc1467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package oyaml/0.4/oyaml-0.4-py2.py3-none-any.whl

---
+++ packages/oyaml/oyaml-0.4-py2.py3-none-any.whl/oyaml.py
@@ -0,0 +1,35 @@
+import sys
+from collections import OrderedDict
+
+import yaml as pyyaml
+
+
+_items = 'viewitems' if sys.version_info < (3,) else 'items'
+
+
+def map_representer(dumper, data):
+    return dumper.represent_dict(getattr(data, _items)())
+
+
+def map_constructor(loader, node):
+    loader.flatten_mapping(node)
+    return OrderedDict(loader.construct_pairs(node))
+
+
+pyyaml.add_representer(dict, map_representer)
+pyyaml.add_representer(OrderedDict, map_representer)
+pyyaml.add_representer(dict, map_representer, Dumper=pyyaml.dumper.SafeDumper)
+pyyaml.add_representer(OrderedDict, map_representer, Dumper=pyyaml.dumper.SafeDumper)
+
+
+if sys.version_info < (3, 7):
+    pyyaml.add_constructor('tag:yaml.org,2002:map', map_constructor)
+
+
+del map_constructor, map_representer
+
+
+# Merge PyYAML namespace into ours.
+# This allows users a drop-in replacement:
+#   import oyaml as yaml
+from yaml import *
