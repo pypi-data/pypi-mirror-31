baeffeba1be5ba34d2d7d77156089b9e45ce955c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:54 +0000

    Add package storified/0.0.9/storified-0.0.9.tar.gz

---
+++ packages/storified/storified-0.0.9.tar.gz/storified-0.0.9/setup.py
@@ -0,0 +1,17 @@
+version = '0.0.9'
+
+from setuptools import setup
+
+setup(
+    name = 'storified',
+    version = version,
+    url = 'http://github.com/docnow/storified',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['storified',],
+    scripts = ['storified.py',],
+    install_requires = ['requests', 'beautifulsoup4', 'six'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    description = 'Download your Storify data',
+)
