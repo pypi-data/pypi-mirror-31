faf435683bdd15b391d18f85d7c8a81891d174e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:35 +0000

    Add package rdict/0.1.1/rdict-0.1.1.tar.gz

---
+++ packages/rdict/rdict-0.1.1.tar.gz/rdict-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='rdict',
+    version='0.1.1',
+    author='songwei',
+    author_email='songwei@songwei.io',
+    description='A Pythonic tool is used for operating redis commands.',
+    long_description='',
+    url='https://github.com/xdusongwei/rdict',
+    python_requires='>=3',
+    packages=['rdict'],
+    ext_modules=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=["redis"],
+)
