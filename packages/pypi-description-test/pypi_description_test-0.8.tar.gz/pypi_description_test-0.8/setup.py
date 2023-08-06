3226000a40bace5ed61050b7d5f3a9b5ff19ec3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:43 +0000

    Add package pypi-description-test/0.8/pypi_description_test-0.8.tar.gz

---
+++ packages/pypi-description-test/pypi_description_test-0.8.tar.gz/pypi_description_test-0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+#Read the description.md file
+description_md_file = open("pypi_long_description.md", "r")
+long_description = description_md_file.read()
+description_md_file.close()
+
+
+setup(
+  name = 'pypi_description_test',
+  packages = ['pypi_description_test'], # this must be the same as the name above
+  version = '0.8',
+  description = 'py_description_test',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = 'David Vitale',
+  keywords = [],
+  classifiers = [],
+)
