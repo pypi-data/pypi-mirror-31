6a998dd1b4a2dc76b780254d9b3246ae3442e252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:11 +0000

    Add package mayonenester/1.4.1/mayonenester-1.4.1.tar.gz

---
+++ packages/mayonenester/mayonenester-1.4.1.tar.gz/mayonenester-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'mayonenester',
+	version ='1.4.1',
+	py_modules = ['mayonenester'],
+	author = 'lijiankang',
+	author_email = '1124358467@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'modify a mistake,false-->False',
+)
