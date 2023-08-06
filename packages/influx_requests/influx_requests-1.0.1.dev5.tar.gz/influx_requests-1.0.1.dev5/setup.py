1d588336cea836a151d60005172bba9a6e38afef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:37 +0000

    Add package influx_requests/1.0.1.dev5/influx_requests-1.0.1.dev5.tar.gz

---
+++ packages/influx_requests/influx_requests-1.0.1.dev5.tar.gz/influx_requests-1.0.1.dev5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+python3 setup.py sdist bdist_wheel upload
+"""
+from setuptools import setup
+
+setup(
+    name='influx_requests',
+    version='1.0.1.dev5',
+    description='monkey patch for requests, that save requests by influxdb',
+    author='ttm1234',
+    author_email='',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='influx_requests',
+    py_modules=['influx_requests'],
+)
