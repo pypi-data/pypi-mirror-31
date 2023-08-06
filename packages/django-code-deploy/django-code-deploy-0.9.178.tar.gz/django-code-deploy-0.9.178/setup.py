144200778875a4acffd201039001283c4ce3fb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:40 +0000

    Add package django-code-deploy/0.9.178/django-code-deploy-0.9.178.tar.gz

---
+++ packages/django-code-deploy/django-code-deploy-0.9.178.tar.gz/django-code-deploy-0.9.178/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name='django-code-deploy',
+    packages=['django_code_deploy'],  # this must be the same as the name above
+
+    version='0.9.178',
+
+    description='Deploys Django code to AWS based on tags',
+    author='Josh devops.center, Bob devops.center, Gregg devops.center',
+    author_email='josh@devops.center, bob@devops.center, gjensen@devops.center',
+    # use the URL to the github repo
+    url='https://github.com/devopscenter/django-code-deploy',
+    # I'll explain this in a second
+    download_url='https://github.com/devopscenter/django-code-deploy/tarball/0.1',
+    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+    classifiers=[],
+)
