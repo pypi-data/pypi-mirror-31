7aa92dcabee91d86876d2be45c79897625699b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:23 +0000

    Add package hydrobox/0.1.1/hydrobox-0.1.1.tar.gz

---
+++ packages/hydrobox/hydrobox-0.1.1.tar.gz/hydrobox-0.1.1/hydrobox/tests/__init__.py
@@ -0,0 +1,5 @@
+from .decorators import TestAcceptDecorator, TestEnforceDecorator
+from .discharge import TestRegime, TestFlowDurationCurve, TestIndices
+from .signal import TestSimplify
+from .io import TestTimeSeriesFromDistribution
+from .stats import TestMovingWindow
---
+++ packages/hydrobox/hydrobox-0.1.1.tar.gz/hydrobox-0.1.1/hydrobox/toolbox.py
@@ -0,0 +1,12 @@
+"""
+This is the main toolbox file.
+
+Here, all major functions will be imported at top level.
+"""
+from .discharge.catchment import flow_duration_curve, regime
+from .discharge import indices
+from .signal import simplify
+from .preprocessing.scale import aggregate, cut
+from .stats import moving_window
+from . import io
+from . import stats
