88700545395b6033968292dba960da3c5af2ba15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:05 +0000

    Add package airr/1.1.0/airr-1.1.0.tar.gz

---
+++ packages/airr/airr-1.1.0.tar.gz/airr-1.1.0/airr/__init__.py
@@ -0,0 +1,9 @@
+"""
+Reference library for AIRR schema for Ig/TCR rearrangements
+"""
+from airr.interface import *
+
+# versioneer-generated
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
