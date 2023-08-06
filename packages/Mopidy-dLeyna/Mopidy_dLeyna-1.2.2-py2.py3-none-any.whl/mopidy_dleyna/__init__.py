a861097cf696483961abae25b81603666a26bd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:58 +0000

    Add package Mopidy-dLeyna/1.2.2/Mopidy_dLeyna-1.2.2-py2.py3-none-any.whl

---
+++ packages/Mopidy-dLeyna/Mopidy_dLeyna-1.2.2-py2.py3-none-any.whl/mopidy_dleyna/__init__.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+import os
+
+from mopidy import config, exceptions, ext
+
+__version__ = '1.2.2'
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-dLeyna'
+    ext_name = 'dleyna'
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(os.path.join(os.path.dirname(__file__), 'ext.conf'))
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['upnp_browse_limit'] = config.Integer(minimum=0)
+        schema['upnp_lookup_limit'] = config.Integer(minimum=0)
+        schema['upnp_search_limit'] = config.Integer(minimum=0)
+        schema['dbus_start_session'] = config.String()
+        return schema
+
+    def setup(self, registry):
+        from .backend import dLeynaBackend
+        registry.add('backend', dLeynaBackend)
+
+    def validate_environment(self):
+        try:
+            import dbus  # noqa
+        except ImportError:
+            raise exceptions.ExtensionError('Cannot import dbus')
