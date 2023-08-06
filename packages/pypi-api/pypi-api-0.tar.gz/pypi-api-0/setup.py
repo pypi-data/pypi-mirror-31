cb1fb7730e3272c5f03c1bd34069f1c28054b0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:12 +0000

    Add package pypi-api/0/pypi-api-0.tar.gz

---
+++ packages/pypi-api/pypi-api-0.tar.gz/pypi-api-0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pypi-api',
+    version='0',
+    description='pypi-api',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pypa/pypi-api',
+    author='The Python Packaging Authority',
+    author_email='pypa-dev@googlegroups.com',
+    packages=[],
+)
