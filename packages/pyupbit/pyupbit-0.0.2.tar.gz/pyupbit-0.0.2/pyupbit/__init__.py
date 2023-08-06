09d3f4b8462d45376d468878ea0a5c66946da524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:25 +0000

    Add package pyupbit/0.0.2/pyupbit-0.0.2.tar.gz

---
+++ packages/pyupbit/pyupbit-0.0.2.tar.gz/pyupbit-0.0.2/pyupbit/__init__.py
@@ -0,0 +1,17 @@
+from pyupbit.client import Upbit
+
+
+def get_tickers():
+    pass
+
+
+def get_market_detail(currency):
+    pass
+
+
+def get_current_price(currency):
+    return Upbit.get_current_price(currency)
+
+
+def get_orderbook(currency):
+    pass
---
+++ packages/pyupbit/pyupbit-0.0.2.tar.gz/pyupbit-0.0.2/pyupbit/core.py
@@ -0,0 +1,57 @@
+import requests
+
+
+class publicApi:
+    @staticmethod
+    def ticker(currency):
+        pass
+
+    @staticmethod
+    def recent_transactions(currency):
+        coin = "CRIX.UPBIT.KRW-" + currency
+        uri = "/candles/ticks/1"
+        return UpbitHttp().get(uri, code=coin, count = 1)
+
+    @staticmethod
+    def orderbook(currency):
+        pass
+
+
+class HttpMethod:
+    def __init__(self):
+        self.session = requests.session()
+
+    @property
+    def base_url(self):
+        return ""
+
+    def _handle_response(self, response):
+        return response.json()
+
+    def update_headers(self, headers):
+        self.session.headers.update(headers)
+
+    def post(self, path, timeout=3, **kwargs):
+        uri = self.base_url + path
+        response = self.session.post(url=uri, data=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+    def get(self, path, timeout=3, **kwargs):
+        uri = self.base_url + path
+        response = self.session.get(url=uri, headers=headers, params=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+
+class UpbitHttp(HttpMethod):
+    def __init__(self):
+        super(UpbitHttp, self).__init__()
+
+    def get(self, path, timeout=3, **kwargs):
+        headers = {"User-Agent": "HACK"}
+        uri = self.base_url + path
+        response = self.session.get(url=uri, headers=headers, params=kwargs, timeout=timeout)
+        return self._handle_response(response)
+
+    @property
+    def base_url(self):
+        return "https://crix-api-endpoint.upbit.com/v1/crix"
