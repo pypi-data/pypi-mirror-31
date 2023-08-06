d14978a8e2471b09b24c5cdb71fe95f0cc40b8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:27 +0000

    Add package dependency-parser/0.0.4/dependency_parser-0.0.4.tar.gz

---
+++ packages/dependency-parser/dependency_parser-0.0.4.tar.gz/dependency_parser-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+class About(object):
+    NAME='dependency_parser'
+    VERSION='0.0.4'
+    AUTHOR='blester125'
+    EMAIL=f'{AUTHOR}@gmail.com'
+    URL=f'https://github.com/{AUTHOR}/{NAME}'
+    DL_URL=f'{URL}/archive/{VERSION}.tar.gz'
+    LICENSE='MIT'
+
+setup(
+    name=About.NAME,
+    version=About.VERSION,
+    description='Dependency Parsing',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author=About.AUTHOR,
+    author_email=About.EMAIL,
+    url=About.URL,
+    download_url=About.DL_URL,
+    license=About.LICENSE,
+    packages=find_packages(),
+    package_data={
+        'dependency_parser': [
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+    ],
+    keywords=[],
+)
