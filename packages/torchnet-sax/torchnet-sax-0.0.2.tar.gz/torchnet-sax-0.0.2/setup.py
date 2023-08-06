a6dda8f349b7ec06fb8336dcd7c4106836d329ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:56 +0000

    Add package torchnet-sax/0.0.2/torchnet-sax-0.0.2.tar.gz

---
+++ packages/torchnet-sax/torchnet-sax-0.0.2.tar.gz/torchnet-sax-0.0.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+
+long_description = "an abstraction to train neural networks"
+
+setup_info = dict(
+    # Metadata
+    name='torchnet-sax',
+    version=VERSION,
+    author='Sergey Zagoruyko',
+    author_email='sergey.zagoruyko@enpc.fr',
+    url='https://github.com/alexsax/tnt/archive/v0.0.1-alpha.1.tar.gz',
+    description='an abstraction to train neural networks',
+    long_description=long_description,
+    license='BSD',
+
+    # Package info
+    packages=find_packages(exclude=('test', 'docs')),
+
+    zip_safe=True,
+
+    install_requires=[
+        'torch',
+        'six',
+        'visdom'
+    ]
+)
+
+setup(**setup_info)
