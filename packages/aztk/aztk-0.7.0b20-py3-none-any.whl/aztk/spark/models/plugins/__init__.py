a1d600ed624d6026b072151f6395cec9383a8d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:15 +0000

    Add package aztk/0.7.0b20/aztk-0.7.0b20-py3-none-any.whl

---
+++ packages/aztk/aztk-0.7.0b20-py3-none-any.whl/aztk/spark/models/plugins/__init__.py
@@ -0,0 +1,6 @@
+from .hdfs import HDFSPlugin
+from .jupyter import JupyterPlugin
+from .jupyter_lab import JupyterLabPlugin
+from .rstudio_server import RStudioServerPlugin
+from .simple import SimplePlugin
+from .spark_ui_proxy import SparkUIProxyPlugin
