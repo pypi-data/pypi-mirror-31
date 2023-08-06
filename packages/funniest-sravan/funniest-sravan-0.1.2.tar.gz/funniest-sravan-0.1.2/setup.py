0b0a4d6ac1f44d9e6bfa0aad1ce848646ab590a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:24 +0000

    Add package funniest-sravan/0.1.2/funniest-sravan-0.1.2.tar.gz

---
+++ packages/funniest-sravan/funniest-sravan-0.1.2.tar.gz/funniest-sravan-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='funniest-sravan',
+      version='0.1.2',
+      description='The funniest joke in the world',
+      url='http://github.com/sravankorumilli/funniest',
+      install_requires=['elasticsearch'],
+      author='Sravan',
+      author_email='sravan@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
