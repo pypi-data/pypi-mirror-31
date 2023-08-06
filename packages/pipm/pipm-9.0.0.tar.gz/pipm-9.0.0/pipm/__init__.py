76a6c46096375cc0d70b22cbd5f30070808eda9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:20 +0000

    Add package pipm/9.0.0/pipm-9.0.0.tar.gz

---
+++ packages/pipm/pipm-9.0.0.tar.gz/pipm-9.0.0/pipm/__init__.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '9.0.0'
+
+import pip
+import pip.basecommand
+import pip.operations.freeze
+import pip.utils
+
+# patch for program name
+from pipm.commands import InstallCommandPlus, UninstallCommandPlus, UpdateCommand, FreezeCommandPlus
+
+
+def get_prog():
+    return 'pipm'
+
+
+pip.get_prog = get_prog
+pip.utils.get_prog = get_prog
+pip.basecommand.get_prog = get_prog
+
+pip.commands_dict[InstallCommandPlus.name] = InstallCommandPlus
+pip.commands_dict['i'] = InstallCommandPlus
+
+pip.commands_dict[UninstallCommandPlus.name] = UninstallCommandPlus
+pip.commands_dict['rm'] = UninstallCommandPlus
+
+pip.commands_dict[FreezeCommandPlus.name] = FreezeCommandPlus
+pip.commands_dict['save'] = FreezeCommandPlus
+
+pip.commands_dict['update'] = UpdateCommand
+pip.commands_dict['upgrade'] = UpdateCommand
+pip.commands_dict['u'] = UpdateCommand
+
+
+# endpatch
+
+def main():
+    pip.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pipm/pipm-9.0.0.tar.gz/pipm-9.0.0/pipm/operations.py
@@ -0,0 +1,92 @@
+from __future__ import absolute_import
+
+import logging
+import pip
+from pip.utils import get_installed_distributions
+from pip.utils import pkg_resources
+from pkg_resources import RequirementParseError, DistInfoDistribution
+from pip.compat import stdlib_pkgs
+from pip.commands.freeze import DEV_PKGS
+
+DEV_PKGS = DEV_PKGS + ('pipm',)
+logger = logging.getLogger(__name__)
+try:
+    reload
+except NameError:
+    from importlib import reload
+except ImportError:
+    from imp import reload
+
+STD_PKGS = stdlib_pkgs + DEV_PKGS
+
+
+def get_dependency_links():
+    dep_links = []
+    reload(pkg_resources)
+
+    for dist in pkg_resources.working_set:
+        if dist.has_metadata('dependency_links.txt'):
+            dep_links.extend(
+                dist.get_metadata_lines('dependency_links.txt')
+            )
+    return dep_links
+
+
+def get_frozen_reqs():
+    dependency_links = get_dependency_links()
+    installations = {}
+
+    for _, dist in get_distributions().items():
+        try:
+            req = pip.FrozenRequirement.from_dist(
+                dist,
+                dependency_links
+            )
+        except RequirementParseError:
+            logger.warning("Could not parse requirement: %s", dist.project_name)
+            continue
+        installations[req.name] = req
+
+    return installations
+
+
+def get_distributions():
+    """
+
+    Returns:
+        dict:
+    """
+    reload(pkg_resources)
+    return {dist.project_name.lower(): dist for dist in
+            get_installed_distributions(local_only=None, skip=STD_PKGS, user_only=None)}
+
+
+def get_orphaned_packages(pkgs):
+    """
+        return list of packages that is only required by the pkgs given but not other installed packages
+    Args:
+        pkgs (list):
+
+    Returns:
+        list:
+    """
+
+    dists = get_distributions()
+    removed_packages = []
+    for pkg in pkgs:  # type: str
+        pkgl = pkg.lower()
+        if pkgl in dists:
+            removed_packages.append(dists.pop(pkgl))
+
+    orphaned_pkgs = set()
+    for dist in removed_packages:  # type: DistInfoDistribution
+        for r in dist.requires():
+            orphaned_pkgs.add(r.name)
+
+    all_requires = set()
+    for dist in dists:
+        for r in dists[dist].requires():
+            all_requires.add(r.name)
+
+    orphaned_pkgs = orphaned_pkgs.difference(all_requires)
+    return list(orphaned_pkgs.difference(set(STD_PKGS)))
