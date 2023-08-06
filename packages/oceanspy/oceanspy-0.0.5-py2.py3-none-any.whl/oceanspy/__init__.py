9acb5444e8b029316fb1c2a535710bad195a7e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:38 +0000

    Add package oceanspy/0.0.5/oceanspy-0.0.5-py2.py3-none-any.whl

---
+++ packages/oceanspy/oceanspy-0.0.5-py2.py3-none-any.whl/oceanspy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for OceanSpy."""
+
+__author__ = """Mattia Almansi"""
+__email__ = 'mattia.almansi@jhu.edu'
+__version__ = '0.0.5'
+
+from .autogenerate import generate_ds_grid
+from .oceanspy import *
