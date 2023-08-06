6485c7d4c4b9f688e148d4ad7ae6af29ada1ea36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:42 +0000

    Add package sfsimodels/0.6.2/sfsimodels-0.6.2.tar.gz

---
+++ packages/sfsimodels/sfsimodels-0.6.2.tar.gz/sfsimodels-0.6.2/sfsimodels/exceptions.py
@@ -0,0 +1,14 @@
+class DesignError(Exception):
+    pass  # deprecated
+
+
+class AnalysisError(Exception):
+    pass
+
+
+class ModelError(Exception):
+    pass
+
+
+class ModelWarning(Warning):
+    pass
