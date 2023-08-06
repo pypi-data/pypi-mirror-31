83275db29a8bd1246c6295654fa4e6ea7981a580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:48 +0000

    Add package hone/0.1.0/hone-0.1.0.win32.zip

---
+++ packages/hone/hone-0.1.0.win32.zip/Python/Scripts/hone-script.py
@@ -0,0 +1,12 @@
+#!C:\Python\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'hone==0.1.0','console_scripts','hone'
+__requires__ = 'hone==0.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('hone==0.1.0', 'console_scripts', 'hone')()
+    )
