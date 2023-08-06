38efee38a5efdbfaf80624db15a9af88d100c393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:43 +0000

    Add package Thestral/0.5.1/Thestral-0.5.1.tar.gz

---
+++ packages/Thestral/Thestral-0.5.1.tar.gz/Thestral-0.5.1/README.rst
@@ -0,0 +1,5 @@
+A Simple Single-cell RNA-seq analysis pipeline.
+###
+For QuLab USTC.
+###
+http://qulab.ustc.edu.cn/index.html
---
+++ packages/Thestral/Thestral-0.5.1.tar.gz/Thestral-0.5.1/setup.py
@@ -0,0 +1,33 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Thestral',
+    version='0.5.1',
+    description='A simple single cell RNA-seq analysis package',
+    long_description=long_description,
+    url='https://github.com/Fang-Kevin/Thestral',
+    author='Fang Jingwen',
+    author_email='fjingwen@mail.ustc.edu.cn',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='Singel-cell RNA-seq',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','matplotlib','seaborn>=0.8.1','scipy','sklearn','fbpca','annoy','tables','python-louvain','networkx','fastcluster'],
+    python_requires='>=2.6, !=3.*.*,<4',
+)
