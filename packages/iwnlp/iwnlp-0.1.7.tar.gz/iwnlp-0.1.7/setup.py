b99facba116df299aab199a4d3981fbc62a39ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:32 +0000

    Add package iwnlp/0.1.7/iwnlp-0.1.7.tar.gz

---
+++ packages/iwnlp/iwnlp-0.1.7.tar.gz/iwnlp-0.1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='iwnlp',
+    packages=['iwnlp'],
+    version='0.1.7',
+    description='Python implementation for IWNLP',
+    author='Matthias Liebeck',
+    author_email='liebeck@cs.uni-duesseldorf.de',
+    url='https://github.com/Liebeck/IWNLP-py',
+    download_url='https://github.com/Liebeck/IWNLP-py/archive/0.1.6.tar.gz',
+    keywords=['IWNLP', 'NLP', 'German', 'lemmatization', 'Wiktionary'],
+    classifiers=[],
+)
