1d1db582e61b9b8f893bdec923470510a0df47d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:54 +0000

    Add package haldane/0.2.2/haldane-0.2.2.tar.gz

---
+++ packages/haldane/haldane-0.2.2.tar.gz/haldane-0.2.2/app/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.2.2'
+
+
+def make_application():
+    from flask import Flask
+    from app.config import Config
+    import app.views
+
+    flask_app = Flask(__name__, static_url_path='')
+
+    if Config.BUGSNAG_API_KEY:
+        import bugsnag
+        from bugsnag.flask import handle_exceptions
+        bugsnag.configure(api_key=Config.BUGSNAG_API_KEY)
+        handle_exceptions(flask_app)
+    elif Config.SENTRY_DSN:
+        from raven.contrib.flask import Sentry
+        sentry = Sentry()
+        sentry.init_app(flask_app, dsn=Config.SENTRY_DSN)
+
+    flask_app.config.from_object('app.config.Config')
+    flask_app.register_blueprint(app.views.blueprint_http)
+    app.views.blueprint_http.config = flask_app.config
+
+    return flask_app
