43a60a759e8d402c13d2a1a982f85b5fdc41ba8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package spfy/3.0.27/spfy-3.0.27.tar.gz

---
+++ packages/spfy/spfy-3.0.27.tar.gz/spfy-3.0.27/spfy/__init__.py
@@ -0,0 +1,28 @@
+__version__ = "3.0.27"
+
+import pathlib  # isort:skip
+
+root = pathlib.Path(__file__).parent  # isort:skip
+APP_NAME = "SPFY"
+
+import os  # isort:skip
+
+ENV = os.getenv(f"{APP_NAME.upper()}_ENV", "config")  # isort:skip
+
+import kick  # isort:skip
+
+kick.start(APP_NAME.lower(), config_variant=ENV)  # isort:skip
+
+from kick import config, logger  # isort:skip
+
+Unsplash = None
+if config.unsplash.auth.client_id:
+    from unsplash import Auth, Api
+
+    auth = Auth(**config.unsplash.auth)
+    Unsplash = Api(auth)
+from .client import SpotifyClient
+from .result import SpotifyResult
+from .wrapper import Spotify
+from .constants import *
+from .exceptions import *
