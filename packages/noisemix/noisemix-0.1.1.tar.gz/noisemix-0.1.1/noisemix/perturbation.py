2c0fdd57c9248ba6b26489464a2e4937d980a3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:57 +0000

    Add package noisemix/0.1.1/noisemix-0.1.1.tar.gz

---
+++ packages/noisemix/noisemix-0.1.1.tar.gz/noisemix-0.1.1/noisemix/perturbation.py
@@ -0,0 +1,18 @@
+class Perturbation:
+    name = "default"
+    function=None
+
+    def __init__(self, name, function,frequency,repeat_num):
+        self.name = name
+        self.function = function
+        self.frequency = frequency
+        self.repeat_num = repeat_num
+
+    def change_frequency(self, frequency):
+        self.frequency = frequency
+
+    def change_repeatable(self, repeat_num):
+        self.repeat_num = repeat_num
+
+
+
