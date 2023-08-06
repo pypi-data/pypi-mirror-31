d448234940b5c740d52aecc87d279411acf3534a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:26 +0000

    Add package dotdrop/0.10.5/dotdrop-0.10.5.tar.gz

---
+++ packages/dotdrop/dotdrop-0.10.5.tar.gz/dotdrop-0.10.5/dotdrop/dotdrop.py
@@ -0,0 +1,348 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2017, deadc0de6
+entry point
+"""
+
+import os
+import sys
+import subprocess
+from docopt import docopt
+
+# local imports
+try:
+    from . import __version__ as VERSION
+except ImportError:
+    errmsg = '''
+Dotdrop has been updated to be included in pypi and
+the way it needs to be called has slightly changed.
+
+See https://github.com/deadc0de6/dotdrop/wiki/migrate-from-submodule
+'''
+    print(errmsg)
+    sys.exit(1)
+from .logger import Logger
+from .templategen import Templategen
+from .installer import Installer
+from .dotfile import Dotfile
+from .config import Cfg
+from .utils import *
+
+CUR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG = Logger()
+HOSTNAME = os.uname()[1]
+TILD = '~'
+TRANS_SUFFIX = 'trans'
+
+BANNER = """     _       _      _
+  __| | ___ | |_ __| |_ __ ___  _ __
+ / _` |/ _ \| __/ _` | '__/ _ \| '_ |
+ \__,_|\___/ \__\__,_|_|  \___/| .__/  v%s
+                               |_|""" % (VERSION)
+
+USAGE = """
+%s
+
+Usage:
+  dotdrop install   [-fndV] [-c <path>] [-p <profile>]
+  dotdrop import    [-ldV]  [-c <path>] [-p <profile>] <paths>...
+  dotdrop compare   [-V]    [-c <path>] [-p <profile>]
+                            [-o <opts>] [--files=<files>]
+  dotdrop update    [-fdV]  [-c <path>] <path>
+  dotdrop listfiles [-V]    [-c <path>] [-p <profile>]
+  dotdrop list      [-V]    [-c <path>]
+  dotdrop --help
+  dotdrop --version
+
+Options:
+  -p --profile=<profile>  Specify the profile to use [default: %s].
+  -c --cfg=<path>         Path to the config [default: config.yaml].
+  --files=<files>         Comma separated list of files to compare.
+  -o --dopts=<opts>       Diff options [default: ].
+  -n --nodiff             Do not diff when installing.
+  -l --link               Import and link.
+  -f --force              Do not warn if exists.
+  -V --verbose            Be verbose.
+  -d --dry                Dry run.
+  -v --version            Show version.
+  -h --help               Show this screen.
+
+""" % (BANNER, HOSTNAME)
+
+###########################################################
+# entry point
+###########################################################
+
+
+def install(opts, conf):
+    dotfiles = conf.get_dotfiles(opts['profile'])
+    if dotfiles == []:
+        LOG.err('no dotfiles defined for this profile (\"%s\")' %
+                (str(opts['profile'])))
+        return False
+    t = Templategen(base=opts['dotpath'])
+    inst = Installer(create=opts['create'], backup=opts['backup'],
+                     dry=opts['dry'], safe=opts['safe'], base=opts['dotpath'],
+                     diff=opts['installdiff'], quiet=opts['quiet'])
+    installed = []
+    for dotfile in dotfiles:
+        if hasattr(dotfile, 'link') and dotfile.link:
+            r = inst.link(dotfile.src, dotfile.dst)
+        else:
+            src = dotfile.src
+            tmp = None
+            if dotfile.trans:
+                tmp = '%s.%s' % (src, TRANS_SUFFIX)
+                err = False
+                for trans in dotfile.trans:
+                    s = os.path.join(opts['dotpath'], src)
+                    temp = os.path.join(opts['dotpath'], tmp)
+                    if not trans.transform(s, temp):
+                        msg = 'transformation \"%s\" failed for %s'
+                        LOG.err(msg % (trans.key, dotfile.key))
+                        err = True
+                        break
+                if err:
+                    if tmp and os.path.exists(tmp):
+                        remove(tmp)
+                    continue
+                src = tmp
+            r = inst.install(t, opts['profile'], src, dotfile.dst)
+            if tmp:
+                tmp = os.path.join(opts['dotpath'], tmp)
+                if os.path.exists(tmp):
+                    remove(tmp)
+        if len(r) > 0 and len(dotfile.actions) > 0:
+            # execute action
+            for action in dotfile.actions:
+                action.execute()
+        installed.extend(r)
+    LOG.log('\n%u dotfile(s) installed.' % (len(installed)))
+    return True
+
+
+def compare(opts, conf, tmp, focus=None):
+    '''compare dotfiles and return True if all same'''
+    dotfiles = conf.get_dotfiles(opts['profile'])
+    if dotfiles == []:
+        LOG.err('no dotfiles defined for this profile (\"%s\")' %
+                (str(opts['profile'])))
+        return True
+    t = Templategen(base=opts['dotpath'])
+    inst = Installer(create=opts['create'], backup=opts['backup'],
+                     dry=opts['dry'], base=opts['dotpath'],
+                     quiet=opts['quiet'])
+
+    # compare only specific files
+    ret = True
+    selected = dotfiles
+    if focus:
+        selected = []
+        for selection in focus.replace(' ', '').split(','):
+            df = next((x for x in dotfiles if x.dst == selection), None)
+            if df:
+                selected.append(df)
+            else:
+                LOG.err('no dotfile matches \"%s\"' % (selection))
+                ret = False
+
+    if len(selected) < 1:
+        return ret
+
+    for dotfile in selected:
+        if dotfile.trans:
+            msg = 'ignore %s as it uses transformation(s)'
+            LOG.log(msg % (dotfile.key))
+            continue
+        same, diff = inst.compare(t, tmp, opts['profile'],
+                                  dotfile.src, dotfile.dst,
+                                  opts=opts['dopts'])
+        if same:
+            if not opts['quiet']:
+                LOG.log('diffing \"%s\" VS \"%s\"' % (dotfile.key,
+                                                      dotfile.dst))
+                LOG.raw('same file')
+        else:
+            LOG.log('diffing \"%s\" VS \"%s\"' % (dotfile.key, dotfile.dst))
+            LOG.emph(diff)
+            ret = False
+
+    return ret
+
+
+def update(opts, conf, path):
+    if not os.path.exists(path):
+        LOG.err('\"%s\" does not exist!' % (path))
+        return False
+    home = os.path.expanduser(TILD)
+    path = os.path.expanduser(path)
+    path = os.path.expandvars(path)
+    # normalize the path
+    if path.startswith(home):
+        path = path.lstrip(home)
+        path = os.path.join(TILD, path)
+    dotfiles = conf.get_dotfiles(opts['profile'])
+    subs = [d for d in dotfiles if d.dst == path]
+    if not subs:
+        LOG.err('\"%s\" is not managed!' % (path))
+        return False
+    if len(subs) > 1:
+        found = ','.join([d.src for d in dotfiles])
+        LOG.err('multiple dotfiles found: %s' % (found))
+        return False
+    dotfile = subs[0]
+    src = os.path.join(conf.get_abs_dotpath(opts['dotpath']), dotfile.src)
+    if Templategen.get_marker() in open(src, 'r').read():
+        LOG.warn('\"%s\" uses template, please update manually' % (src))
+        return False
+    cmd = ['cp', '-R', '-L', os.path.expanduser(path), src]
+    if opts['dry']:
+        LOG.dry('would run: %s' % (' '.join(cmd)))
+    else:
+        msg = 'Overwrite \"%s\" with \"%s\"?' % (src, path)
+        if opts['safe'] and not LOG.ask(msg):
+            return False
+        else:
+            run(cmd, raw=False, log=False)
+            LOG.log('\"%s\" updated from \"%s\".' % (src, path))
+    return True
+
+
+def importer(opts, conf, paths):
+    home = os.path.expanduser(TILD)
+    cnt = 0
+    for path in paths:
+        if not os.path.exists(path):
+            LOG.err('\"%s\" does not exist, ignored !' % (path))
+            continue
+        dst = path.rstrip(os.sep)
+        key = dst.split(os.sep)[-1]
+        if key == 'config':
+            key = '_'.join(dst.split(os.sep)[-2:])
+        key = key.lstrip('.').lower()
+        if os.path.isdir(dst):
+            key = 'd_%s' % (key)
+        else:
+            key = 'f_%s' % (key)
+        src = dst
+        if dst.startswith(home):
+            src = dst[len(home):]
+        src = src.lstrip('.' + os.sep)
+        dotfile = Dotfile(key, dst, src)
+        srcf = os.path.join(CUR, opts['dotpath'], src)
+        retconf = conf.new(dotfile, opts['profile'], opts['link'])
+        if not os.path.exists(srcf):
+            cmd = ['mkdir', '-p', '%s' % (os.path.dirname(srcf))]
+            if opts['dry']:
+                LOG.dry('would run: %s' % (' '.join(cmd)))
+            else:
+                run(cmd, raw=False, log=False)
+            cmd = ['cp', '-R', '-L', dst, srcf]
+            if opts['dry']:
+                LOG.dry('would run: %s' % (' '.join(cmd)))
+                if opts['link']:
+                    LOG.dry('would symlink %s to %s' % (srcf, dst))
+            else:
+                run(cmd, raw=False, log=False)
+                if opts['link']:
+                    remove(dst)
+                    os.symlink(srcf, dst)
+        if retconf:
+            LOG.sub('\"%s\" imported' % (path))
+            cnt += 1
+        else:
+            LOG.warn('\"%s\" ignored' % (path))
+    if opts['dry']:
+        LOG.dry('new config file would be:')
+        LOG.raw(conf.dump())
+    else:
+        conf.save()
+    LOG.log('\n%u file(s) imported.' % (cnt))
+
+
+def list_profiles(conf):
+    LOG.log('Available profile(s):')
+    for p in conf.get_profiles():
+        LOG.sub(p)
+    LOG.log('')
+
+
+def list_files(opts, conf):
+    if not opts['profile'] in conf.get_profiles():
+        LOG.warn('unknown profile \"%s\"' % (opts['profile']))
+        return
+    LOG.log('Dotfile(s) for profile \"%s\":\n' % (opts['profile']))
+    for dotfile in conf.get_dotfiles(opts['profile']):
+        LOG.log('%s (file: \"%s\", link: %s)' % (dotfile.key, dotfile.src,
+                                                 str(dotfile.link)))
+        LOG.sub('%s' % (dotfile.dst))
+    LOG.log('')
+
+
+def header():
+    LOG.log(BANNER)
+    LOG.log('')
+
+
+def main():
+    ret = True
+    args = docopt(USAGE, version=VERSION)
+    try:
+        conf = Cfg(os.path.expanduser(args['--cfg']))
+    except ValueError as e:
+        LOG.err('error: %s' % (str(e)))
+        return False
+
+    opts = conf.get_configs()
+    opts['dry'] = args['--dry']
+    opts['profile'] = args['--profile']
+    opts['safe'] = not args['--force']
+    opts['installdiff'] = not args['--nodiff']
+    opts['link'] = args['--link']
+    opts['quiet'] = not args['--verbose']
+
+    header()
+
+    try:
+
+        if args['list']:
+            # list existing profiles
+            list_profiles(conf)
+
+        elif args['listfiles']:
+            # list files for selected profile
+            list_files(opts, conf)
+
+        elif args['install']:
+            # install the dotfiles stored in dotdrop
+            ret = install(opts, conf)
+
+        elif args['compare']:
+            # compare local dotfiles with dotfiles stored in dotdrop
+            tmp = get_tmpdir()
+            opts['dopts'] = args['--dopts']
+            ret = compare(opts, conf, tmp, args['--files'])
+            if os.listdir(tmp):
+                LOG.raw('\ntemporary files available under %s' % (tmp))
+            else:
+                os.rmdir(tmp)
+
+        elif args['import']:
+            # import dotfile(s)
+            importer(opts, conf, args['<paths>'])
+
+        elif args['update']:
+            # update a dotfile
+            update(opts, conf, args['<path>'])
+
+    except KeyboardInterrupt:
+        LOG.err('interrupted')
+        ret = False
+
+    return ret
+
+
+if __name__ == '__main__':
+    if main():
+        sys.exit(0)
+    sys.exit(1)
---
+++ packages/dotdrop/dotdrop-0.10.5.tar.gz/dotdrop-0.10.5/dotdrop/installer.py
@@ -0,0 +1,202 @@
+"""
+author: deadc0de6 (https://github.com/deadc0de6)
+Copyright (c) 2017, deadc0de6
+handle the installation of dotfiles
+"""
+
+import os
+
+# local imports
+from dotdrop.logger import Logger
+import dotdrop.utils as utils
+
+
+class Installer:
+
+    BACKUP_SUFFIX = '.dotdropbak'
+
+    def __init__(self, base='.', create=True, backup=True,
+                 dry=False, safe=False, quiet=False, diff=True):
+        self.create = create
+        self.backup = backup
+        self.dry = dry
+        self.safe = safe
+        self.base = base
+        self.quiet = quiet
+        self.diff = diff
+        self.comparing = False
+        self.log = Logger()
+
+    def install(self, templater, profile, src, dst):
+        '''Install the dotfile for profile "profile"'''
+        src = os.path.join(self.base, os.path.expanduser(src))
+        dst = os.path.join(self.base, os.path.expanduser(dst))
+        if os.path.isdir(src):
+            return self._handle_dir(templater, profile, src, dst)
+        return self._handle_file(templater, profile, src, dst)
+
+    def link(self, src, dst):
+        '''Sets src as the link target of dst'''
+        src = os.path.join(self.base, os.path.expanduser(src))
+        dst = os.path.join(self.base, os.path.expanduser(dst))
+        if os.path.exists(dst):
+            if os.path.realpath(dst) == os.path.realpath(src):
+                self.log.sub('ignoring "%s", link exists' % dst)
+                return []
+            if self.dry:
+                self.log.dry('would remove %s and link it to %s'
+                             % (dst, src))
+                return []
+            if self.safe and \
+                    not self.log.ask('Remove "%s" for link creation?' % dst):
+                self.log.warn('ignoring "%s", link was not created' % dst)
+                return []
+            try:
+                utils.remove(dst)
+            except OSError:
+                self.log.err('something went wrong with %s' % src)
+                return []
+        if self.dry:
+            self.log.dry('would link %s to %s' % (dst, src))
+            return []
+        base = os.path.dirname(dst)
+        if not self._create_dirs(base):
+            self.log.err('creating directory for \"%s\"' % (dst))
+            return []
+        os.symlink(src, dst)
+        self.log.sub('linked %s to %s' % (dst, src))
+        # Follows original developer's behavior
+        return [(src, dst)]
+
+    def _handle_file(self, templater, profile, src, dst):
+        '''Install a file using templater for "profile"'''
+        content = templater.generate(src, profile)
+        if content is None:
+            self.log.err('generate from template \"%s\"' % (src))
+            return []
+        if not os.path.exists(src):
+            self.log.err('source dotfile does not exist: \"%s\"' % (src))
+            return []
+        st = os.stat(src)
+        ret = self._write(dst, content, st.st_mode)
+        if ret < 0:
+            self.log.err('installing \"%s\" to \"%s\"' % (src, dst))
+            return []
+        if ret > 0:
+            if not self.quiet:
+                self.log.sub('ignoring \"%s\", same content' % (dst))
+            return []
+        if ret == 0:
+            if not self.dry and not self.comparing:
+                self.log.sub('copied \"%s\" to \"%s\"' % (src, dst))
+            return [(src, dst)]
+        return []
+
+    def _handle_dir(self, templater, profile, src, dst):
+        '''Install a folder using templater for "profile"'''
+        ret = []
+        for entry in os.listdir(src):
+            f = os.path.join(src, entry)
+            if not os.path.isdir(f):
+                res = self._handle_file(
+                    templater, profile, f, os.path.join(dst, entry))
+                ret.extend(res)
+            else:
+                res = self._handle_dir(
+                    templater, profile, f, os.path.join(dst, entry))
+                ret.extend(res)
+        return ret
+
+    def _fake_diff(self, dst, content):
+        '''Fake diff by comparing file content with "content"'''
+        cur = ''
+        with open(dst, 'br') as f:
+            cur = f.read()
+        return cur == content
+
+    def _write(self, dst, content, rights):
+        '''Write file
+        returns  0 for success,
+                 1 when already exists
+                -1 when error'''
+        if self.dry:
+            self.log.dry('would install %s' % (dst))
+            return 0
+        if os.path.exists(dst):
+            samerights = os.stat(dst).st_mode == rights
+            if self.diff and self._fake_diff(dst, content) and samerights:
+                return 1
+            if self.safe and not self.log.ask('Overwrite \"%s\"' % (dst)):
+                self.log.warn('ignoring \"%s\", already present' % (dst))
+                return 1
+        if self.backup and os.path.exists(dst):
+            self._backup(dst)
+        base = os.path.dirname(dst)
+        if not self._create_dirs(base):
+            self.log.err('creating directory for \"%s\"' % (dst))
+            return -1
+        try:
+            with open(dst, 'wb') as f:
+                f.write(content)
+        except NotADirectoryError as e:
+            self.log.err('opening dest file: %s' % (e))
+            return -1
+        os.chmod(dst, rights)
+        return 0
+
+    def _create_dirs(self, folder):
+        '''mkdir -p "folder"'''
+        if not self.create and not os.path.exists(folder):
+            return False
+        if os.path.exists(folder):
+            return True
+        os.makedirs(folder)
+        return os.path.exists(folder)
+
+    def _backup(self, path):
+        '''Backup the file'''
+        if self.dry:
+            return
+        dst = path.rstrip(os.sep) + self.BACKUP_SUFFIX
+        self.log.log('backup %s to %s' % (path, dst))
+        os.rename(path, dst)
+
+    def _install_to_temp(self, templater, profile, src, dst, tmpfolder):
+        '''Install a dotfile to a tempfolder for comparing'''
+        sub = dst
+        if dst[0] == os.sep:
+            sub = dst[1:]
+        tmpdst = os.path.join(tmpfolder, sub)
+        return self.install(templater, profile, src, tmpdst), tmpdst
+
+    def compare(self, templater, tmpfolder, profile, src, dst, opts=''):
+        '''Compare temporary generated dotfile with local one'''
+        self.comparing = True
+        retval = False, ''
+        drysaved = self.dry
+        self.dry = False
+        diffsaved = self.diff
+        self.diff = False
+        createsaved = self.create
+        self.create = True
+        src = os.path.expanduser(src)
+        dst = os.path.expanduser(dst)
+        if not os.path.exists(dst):
+            retval = False, '\"%s\" does not exist on local\n' % (dst)
+        else:
+            ret, tmpdst = self._install_to_temp(templater,
+                                                profile,
+                                                src, dst,
+                                                tmpfolder)
+            if ret:
+                diff = utils.diff(tmpdst, dst, log=False,
+                                  raw=False, opts=opts)
+                if diff == '':
+                    retval = True, ''
+                else:
+                    retval = False, diff
+        self.dry = drysaved
+        self.diff = diffsaved
+        self.comparing = False
+        self.create = createsaved
+        return retval
