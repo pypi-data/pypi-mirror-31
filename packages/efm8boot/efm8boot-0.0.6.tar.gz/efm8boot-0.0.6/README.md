ecd70e5588f7e671f30360619617249616cdde8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:41 +0000

    Add package efm8boot/0.0.6/efm8boot-0.0.6.tar.gz

---
+++ packages/efm8boot/efm8boot-0.0.6.tar.gz/efm8boot-0.0.6/README.md
@@ -0,0 +1,30 @@
+efm8 boot
+===============================
+
+Overview
+--------
+
+A python package for working with efm8 bootloaders.
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install efm8boot
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/ahtn/efm8boot.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ packages/efm8boot/efm8boot-0.0.6.tar.gz/efm8boot-0.0.6/efm8boot/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2018 jem@seethis.link
+# Licensed under the MIT license (http://opensource.org/licenses/MIT)
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import efm8boot.records
+import efm8boot.ids
+
+from efm8boot.bootloader import EFM8Bootloader
+from efm8boot.hid_bootloader import EFM8BootloaderHID, find_devices
+
+if __name__ == '__main__':
+    from efm8boot.records import *
