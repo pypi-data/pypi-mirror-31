4f418ddd82d1879b9a3372dfdb2952b26f4ecf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:33 +0000

    Add package magi/0.0.7/magi-0.0.7.tar.gz

---
+++ packages/magi/magi-0.0.7.tar.gz/magi-0.0.7/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sphinx_doc
+import sys
+from os.path import exists
+import setuptools
+
+setuptools.setup(name='magi',
+      version=sphinx_doc.__version__,
+      description='high level wrapper for parallel univariate time series forecasting',
+      long_description=open('README.rst').read() if exists('README.rst') else '',
+      url='http://github.com/DavisTownsend/forecast',
+      author= sphinx_doc.__author__,
+      author_email='dtownsend@ea.com',
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+      ],
+      keywords='time series analysis forecast forecasting predict model parallel',
+      license='MIT',
+      packages=[sphinx_doc.__name__],
+      python_requires='~=3.5',
+      install_requires=['numpy','pandas','dask','distributed','pystan','rpy2','fbprophet','plotly','cufflinks'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+     )
