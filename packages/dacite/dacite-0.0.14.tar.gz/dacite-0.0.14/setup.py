cf13d8f688bfd5c91351191de164590fd5ba7200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:03 +0000

    Add package dacite/0.0.14/dacite-0.0.14.tar.gz

---
+++ packages/dacite/dacite-0.0.14.tar.gz/dacite-0.0.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='dacite',
+    version='0.0.14',
+    description='Simple creation of data classes from dictionaries.',
+    long_description=long_description,
+    author='Konrad Hałas',
+    author_email='halas.konrad@gmail.com',
+    url='https://github.com/konradhalas/dacite',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+    keywords='dataclasses',
+    py_modules=['dacite'],
+    install_requires=['dataclasses'],
+)
