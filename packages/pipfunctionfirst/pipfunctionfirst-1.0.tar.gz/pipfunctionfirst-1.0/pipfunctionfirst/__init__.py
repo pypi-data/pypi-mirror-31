a7b479b2c7c64214f1b548990d9fe78fa6008c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package pipfunctionfirst/1.0/pipfunctionfirst-1.0.tar.gz

---
+++ packages/pipfunctionfirst/pipfunctionfirst-1.0.tar.gz/pipfunctionfirst-1.0/pipfunctionfirst/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def joke():
+    return ('How do you tell HTML from HTML5?'
+            'Try it out in Internet Explorer.'
+            'Does it work?'
+            'No?'
+            'It\'s HTML5.')
---
+++ packages/pipfunctionfirst/pipfunctionfirst-1.0.tar.gz/pipfunctionfirst-1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pipfunctionfirst',
+    description='The pipfunctionfirst joke in the world',
+    version='1.0',
+    url='https://github.com/zksfyz/pipfunctionfirst',
+    author='MagicNet',
+    author_email='zksfyz@gmail.com',
+    license='MIT',
+    packages=['pipfunctionfirst'],
+    zip_safe=False
+)
