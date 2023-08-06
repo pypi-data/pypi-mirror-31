c7b1996fb0fdd6cdf236ac2a073a3837c180d520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:48 +0000

    Add package pointpats/1.0.1/pointpats-1.0.1.tar.gz

---
+++ packages/pointpats/pointpats-1.0.1.tar.gz/pointpats-1.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import os.path
+
+from distutils.command.build_py import build_py
+
+pth = os.path.dirname(os.path.abspath(__file__))+ '/requirements.txt'
+
+REQUIREMENTS = [i.strip() for i in open(pth).readlines()]
+
+
+setup(name='pointpats',
+      version='1.0.1',
+      description='Methods and Functions for planar point pattern analysis',
+      url='https://github.com/pysal/pointpats',
+      maintainer='Hu Shao',
+      maintainer_email='shaohutiger@gmail.com',
+      test_suite = 'nose.collector',
+      tests_require=['nose'],
+      keywords='spatial statistics',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        ],
+      license='3-Clause BSD',
+      packages=['pointpats'],
+      install_requires=REQUIREMENTS,
+      zip_safe=False,
+      cmdclass = {'build.py':build_py})
