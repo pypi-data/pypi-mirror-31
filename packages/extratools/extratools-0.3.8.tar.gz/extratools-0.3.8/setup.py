e028b64443597cf649809e9c4efd20df97f46615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:25 +0000

    Add package extratools/0.3.8/extratools-0.3.8.tar.gz

---
+++ packages/extratools/extratools-0.3.8.tar.gz/extratools-0.3.8/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+url = "https://github.com/chuanconggao/extratools"
+version = "0.3.8"
+
+setup(
+    name="extratools",
+
+    packages=["extratools"],
+    include_package_data=True,
+
+    url=url,
+
+    version=version,
+    download_url=f"{url}/tarball/{version}",
+
+    license="MIT",
+
+    author="Chuancong Gao",
+    author_email="chuancong@gmail.com",
+
+    description="Extra Functional Tools beyond Standard and Third-Party Libraries",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+
+    keywords=[
+        "functional",
+        "tools"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+
+    python_requires=">= 3",
+    install_requires=[
+        line.strip() for line in open("requirements.txt")
+    ]
+)
