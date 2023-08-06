e5ff890cfe553503fca8cc77593da93542a7ff0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:32 +0000

    Add package spaken/0.0.7/spaken-0.0.7.tar.gz

---
+++ packages/spaken/spaken-0.0.7.tar.gz/spaken-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+
+with open('README.rst', 'r') as fh:
+    description = '\n'.join(fh.readlines())
+
+
+setup(
+    name='spaken',
+    version='0.0.7',
+    description=description,
+    url='https://github.com/labd/spaken',
+    author="Michael van Tellingen",
+    author_email="michaelvantellingen@gmail.com",
+    install_requires=[
+        'boto3',
+        'click>=6.7',
+        'pip>=10.0',
+        'packaging>=17.0',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'console_scripts': {
+            'spaken = spaken.main:main'
+        }
+    },
+    zip_safe=False,
+)
