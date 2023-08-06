67d19de9ab04a6ef97572ef7234a1a310e940ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:18 +0000

    Add package Pyckson/1.4/Pyckson-1.4.tar.gz

---
+++ packages/Pyckson/Pyckson-1.4.tar.gz/Pyckson-1.4/src/pyckson/helpers.py
@@ -0,0 +1,66 @@
+import re
+import sys
+from typing import _ForwardRef
+
+from pyckson.const import PYCKSON_ATTR, BASIC_TYPES, PYCKSON_NAMERULE, PYCKSON_SERIALIZER, PYCKSON_PARSER
+from pyckson.parsers.base import Parser
+from pyckson.serializers.base import Serializer
+
+
+def is_pyckson(obj_type):
+    return getattr(obj_type, PYCKSON_ATTR, False)
+
+
+def name_by_dict(name_mapping, default_rule):
+    def name_function(python_name):
+        if python_name in name_mapping:
+            return name_mapping[python_name]
+        else:
+            return default_rule(python_name)
+
+    return name_function
+
+
+def camel_case_name(python_name):
+    return re.sub('_([a-z])', lambda match: match.group(1).upper(), python_name)
+
+
+def same_name(python_name):
+    return python_name
+
+
+def get_name_rule(obj_type):
+    return getattr(obj_type, PYCKSON_NAMERULE, camel_case_name)
+
+
+def is_base_type(obj):
+    for btype in BASIC_TYPES:
+        if isinstance(obj, btype):
+            return True
+    return False
+
+
+class TypeProvider:
+    def __init__(self, cls, name):
+        self.cls = cls
+        self.name = name.__forward_arg__ if type(name) is _ForwardRef else name
+
+    def get(self):
+        try:
+            return getattr(sys.modules[self.cls.__module__], self.name)
+        except AttributeError:
+            raise TypeError('could not resolve string annotation {} in class {}'.format(self.name, self.cls.__name__))
+
+
+def get_custom_serializer(cls) -> Serializer:
+    serializer = getattr(cls, PYCKSON_SERIALIZER)
+    if isinstance(serializer, str):
+        serializer = TypeProvider(cls, serializer).get()
+    return serializer()
+
+
+def get_custom_parser(cls) -> Parser:
+    parser = getattr(cls, PYCKSON_PARSER)
+    if isinstance(parser, str):
+        parser = TypeProvider(cls, parser).get()
+    return parser()
