55ef4122dde99c34b398225b04c16b4506083b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:27 +0000

    Add package uname2/0.1.0/uname2-0.1.0.tar.gz

---
+++ packages/uname2/uname2-0.1.0.tar.gz/uname2-0.1.0/setup.py
@@ -0,0 +1,43 @@
+import os
+from io import open
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+requires = [
+    "click",
+]
+
+setup(
+    name="uname2",
+    version="0.1.0",
+    description="Print certain system information.",
+    long_description=long_description,
+    url="https://github.com/appstore-zencore/uname2",
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['uname2'],
+    requires=requires,
+    install_requires=requires,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=['uname2'],
+    scripts=["src/uname2.py"],
+    entry_points={
+        'console_scripts': ['uname2 = uname2:cli']
+    },
+    
+)
