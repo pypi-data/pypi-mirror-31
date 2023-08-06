888f09332302c37ca8ce32af81f11081aa3d07d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:23 +0000

    Add package webdriver-test-tools/1.0.0a0/webdriver_test_tools-1.0.0a0.tar.gz

---
+++ packages/webdriver-test-tools/webdriver_test_tools-1.0.0a0.tar.gz/webdriver_test_tools-1.0.0a0/webdriver_test_tools/common/utils.py
@@ -0,0 +1,32 @@
+"""Various utility methods for webdriver_test_tools submodules"""
+
+import re
+from urllib.parse import urlparse
+
+
+def get_base_url(url):
+    """Strips query string from a url and returns just the base url
+
+    :param url: The full url to get the base url from
+
+    :return: The url with any query string removed
+    """
+    current = urlparse(url)
+    return current.scheme + '://' + current.netloc + current.path
+
+def validate_filename(filename, allow_spaces=False):
+    """Strips invalid characters from a filename
+
+    Considers POSIX "fully portable filenames" valid. These include:
+        A-Z a-z 0-9 ._-
+
+    Filenames cannot begin with a hyphen.
+
+    :param filename: The desired file name (without path)
+    :param allow_spaces: (Default = False) If True, spaces will be considered
+        valid characters
+
+    :return: Filename with invalid characters removed
+    """
+    regex = r'^-|[^\d\w\. -]' if allow_spaces else r'^-|[^\d\w\.-]'
+    return re.sub(regex, '', filename)
