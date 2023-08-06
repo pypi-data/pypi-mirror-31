4132052005e76ddc822b6e0d87994231867e2a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:48 +0000

    Add package bestcaptchasolverapi2/0.1/bestcaptchasolverapi2-0.1.tar.gz

---
+++ packages/bestcaptchasolverapi2/bestcaptchasolverapi2-0.1.tar.gz/bestcaptchasolverapi2-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bestcaptchasolverapi2',
+  packages = ['bestcaptchasolverapi2'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Bestcaptchasolver python2 API library',
+  author = 'BestCaptchaSolver',
+  author_email = 'bcsolver@gmail.com',
+  url = 'https://github.com/bestcaptchasolver/bestcaptchasolver-python2', # use the URL to the github repo
+  download_url = 'https://github.com/bestcaptchasolver/bestcaptchasolver-python2', # I'll explain this in a second
+  keywords = ['bestcaptchasolver', 'captcha', 'bypasscaptcha', 'decaptcher', 'decaptcha', '2captcha', 'deathbycaptcha', 'anticaptcha', 'bypass-recaptcha-v2', 'google-recaptcha-solver', 'recaptcha-v2-captcha-solver', 'captcha-services-for-recaptcha-v2', 'bypass-invisible-recaptcha', 'bypass-no-captcha-recaptcha', 'recaptcha-solver-python', 'recaptcha-bypass-script'],
+  classifiers = [],
+)
