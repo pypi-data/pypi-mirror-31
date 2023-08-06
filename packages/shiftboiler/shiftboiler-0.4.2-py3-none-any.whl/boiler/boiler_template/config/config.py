ff74cb4956f9ee28198f68cdacbfc036e1d0fc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:43 +0000

    Add package shiftboiler/0.4.2/shiftboiler-0.4.2-py3-none-any.whl

---
+++ packages/shiftboiler/shiftboiler-0.4.2-py3-none-any.whl/boiler/boiler_template/config/config.py
@@ -0,0 +1,21 @@
+from project import config
+
+class DefaultConfig(config.DefaultConfig):
+    """ Local development config """
+    pass
+
+
+class DevConfig(config.DevConfig):
+    """ Local development config """
+    pass
+
+
+class TestingConfig(config.TestingConfig):
+    """ Local testing config """
+    pass
+
+
+
+
+
+
