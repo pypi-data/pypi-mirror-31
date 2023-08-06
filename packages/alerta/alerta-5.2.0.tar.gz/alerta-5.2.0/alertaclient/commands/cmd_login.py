cf32fa2126c29a1fd4d25981abb53127b18caab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:49 +0000

    Add package alerta/5.2.0/alerta-5.2.0.tar.gz

---
+++ packages/alerta/alerta-5.2.0.tar.gz/alerta-5.2.0/alertaclient/commands/cmd_login.py
@@ -0,0 +1,45 @@
+
+import sys
+import click
+
+from alertaclient.auth.utils import save_token
+from alertaclient.auth import github, gitlab, google
+from alertaclient.exceptions import AuthError
+from alertaclient.auth.token import Jwt
+
+
+@click.command('login', short_help='Login with user credentials')
+@click.pass_obj
+def cli(obj):
+    """Authenticate using Github, Gitlab, Google OAuth2 or Basic Auth
+    username/password instead of using an API key."""
+    client = obj['client']
+    provider = obj['provider']
+    client_id = obj['client_id']
+
+    try:
+        if provider == 'github':
+            token = github.login(client, obj['github_url'], client_id)['token']
+        elif provider == 'gitlab':
+            token = gitlab.login(client, obj['gitlab_url'], client_id)['token']
+        elif provider == 'google':
+            username = click.prompt('Email')
+            token = google.login(client, username, client_id)['token']
+        elif provider == 'basic':
+            username = click.prompt('Email')
+            password = click.prompt('Password', hide_input=True)
+            token = client.login(username, password)['token']
+        else:
+            click.echo('ERROR: unknown provider {provider}'.format(provider=provider))
+            sys.exit(1)
+    except Exception as e:
+        raise AuthError(e)
+
+    jwt = Jwt()
+    username = jwt.parse(token)['preferred_username']
+    if username:
+        save_token(client.endpoint, username, token)
+        click.echo('Logged in as {}'.format(username))
+    else:
+        click.echo('Failed to login.')
+        sys.exit(1)
