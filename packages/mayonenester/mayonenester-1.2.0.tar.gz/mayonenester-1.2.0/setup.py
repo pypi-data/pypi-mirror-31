8d69886054cb1b2e56625d7aedc1432dc3b6ebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:56 +0000

    Add package mayonenester/1.2.0/mayonenester-1.2.0.tar.gz

---
+++ packages/mayonenester/mayonenester-1.2.0.tar.gz/mayonenester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'mayonenester',
+	version ='1.2.0',
+	py_modules = ['mayonenester'],
+	author = 'lijiankang',
+	author_email = '1124358467@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'Add an optional parameter, that is, if you call a function, you have no parameter.',
+)
