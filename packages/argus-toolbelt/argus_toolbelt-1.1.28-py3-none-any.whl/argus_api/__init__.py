d132f22eaebaa5170e6e946f6a1a37b8dec2d207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:20 +0000

    Add package argus-toolbelt/1.1.28/argus_toolbelt-1.1.28-py3-none-any.whl

---
+++ packages/argus-toolbelt/argus_toolbelt-1.1.28-py3-none-any.whl/argus_api/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.0"
+
+from .argus import load as ArgusAPI
+
+__all__ = ["ArgusAPI", "__version__"]
+
---
+++ packages/argus-toolbelt/argus_toolbelt-1.1.28-py3-none-any.whl/argus_api/helpers/http.py
@@ -0,0 +1,4 @@
+"""HTTP Helper methods and constants"""
+from argus_api import __version__
+
+USER_AGENT = 'ArgusToolbelt/%s' % str(__version__)
