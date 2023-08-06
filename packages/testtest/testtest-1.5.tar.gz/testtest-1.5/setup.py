d3ca91afba92b443024de94d47659f03da3a30da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:24 +0000

    Add package testtest/1.5/testtest-1.5.tar.gz

---
+++ packages/testtest/testtest-1.5.tar.gz/testtest-1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+      name='testtest',
+      version='1.5',
+      py_modules=['using_click'],
+      install_requires=[
+            'Click',
+      ],
+      description='Testing Framework',
+      url='https://github.com/AlexMcCarroll/pypi_play',
+      author='Alex McCarroll',
+      author_email='alexandra@mccarrollhk.com',
+      license='MIT',
+      packages=['testtest'],
+      zip_safe=False,
+      entry_points={
+            'console_scripts': [
+                'using_click=using_click:cli',
+            ],
+      }
+      )
