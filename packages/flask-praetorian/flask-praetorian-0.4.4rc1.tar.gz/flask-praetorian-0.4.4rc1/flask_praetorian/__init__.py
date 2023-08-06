1641aedd3f7f05acede0498e0ec335eb47f78926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:41 +0000

    Add package flask-praetorian/0.4.4rc1/flask-praetorian-0.4.4rc1.tar.gz

---
+++ packages/flask-praetorian/flask-praetorian-0.4.4rc1.tar.gz/flask-praetorian-0.4.4rc1/flask_praetorian/__init__.py
@@ -0,0 +1,21 @@
+"""
+flask-praetorian is a security extension for flask. It is modelled heavily on
+flask-security (https://github.com/mattupstate/flask-security), but is targeted
+at providing authentication for api-only applications that use token based
+authentication. It builds on flask-jwt-extended and provides some additional
+functionality such as password encryption upon storage and decorators that
+check the current users roles
+"""
+
+from flask_praetorian.base import Praetorian  # noqa
+from flask_praetorian.exceptions import PraetorianError  # noqa
+from flask_praetorian.decorators import (  # noqa
+    auth_required,
+    roles_required,
+    roles_accepted,
+)
+from flask_praetorian.utilities import (  # noqa
+    current_user,
+    current_user_id,
+    current_rolenames,
+)
