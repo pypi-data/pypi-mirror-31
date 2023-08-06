94e397bfcd7fbb64a80fc19137c996a1b49f3312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:38 +0000

    Add package wpiformat/2018.91/wpiformat-2018.91-py3-none-any.whl

---
+++ packages/wpiformat/wpiformat-2018.91-py3-none-any.whl/wpiformat/bracecomment.py
@@ -0,0 +1,54 @@
+"""This task ensures the trailing comment on the closing brace of extern and
+namespace declarations matches that of the declaration name.
+"""
+
+import regex
+
+from wpiformat.task import Task
+
+
+class BraceComment(Task):
+
+    def should_process_file(self, config_file, name):
+        return config_file.is_c_file(name) or config_file.is_cpp_file(name)
+
+    def run_pipeline(self, config_file, name, lines):
+        linesep = Task.get_linesep(lines)
+        output = ""
+
+        brace_prefix = "(?P<prefix>(extern|namespace)\s+[\w\"]*)"
+        brace_postfix = "\s*/(/|\*)[^\r\n]*"
+
+        brace_regex = regex.compile(
+            "(" + brace_prefix + "\s*)?{|"  # "{" with optional prefix
+            "\}(" + brace_postfix + ")?")  # "}" with optional comment postfix
+
+        name_stack = []
+        brace_count = 0
+        extract_location = 0
+        for match in brace_regex.finditer(lines):
+            token = match.group()
+
+            if match.group("prefix"):
+                brace_count += 1
+                name_stack.append((brace_count, match.group("prefix").rstrip()))
+            elif "{" in token:
+                brace_count += 1
+            elif token.startswith("}"):
+                output += lines[extract_location:match.start()]
+                if len(name_stack) > 0 and name_stack[len(name_stack) -
+                                                      1][0] == brace_count:
+                    output += "}  // " + name_stack.pop()[1]
+                else:
+                    output += lines[match.start():match.end()]
+                extract_location = match.end()
+                brace_count -= 1
+
+        # If input has unprocessed lines, write them to output
+        if extract_location < len(lines):
+            output += lines[extract_location:]
+
+        if output != lines:
+            return (output, True, True)
+        else:
+            return (lines, False, True)
---
+++ packages/wpiformat/wpiformat-2018.91-py3-none-any.whl/wpiformat/cidentlist.py
@@ -0,0 +1,117 @@
+"""This task replaces empty C identifier lists "()" with "(void)"."""
+
+import regex
+
+from wpiformat.task import Task
+
+
+class CIdentList(Task):
+
+    def should_process_file(self, config_file, name):
+        return config_file.is_c_file(name) or config_file.is_cpp_file(name)
+
+    def run_pipeline(self, config_file, name, lines):
+        linesep = Task.get_linesep(lines)
+        file_changed = False
+
+        output = ""
+        pos = 0
+
+        # C files use C linkage by default
+        is_c = config_file.is_c_file(name)
+
+        # Tokenize as extern "C" or extern "C++" with optional {, open brace,
+        # close brace, or () folllowed by { to disambiguate function calls.
+        # extern is first to try matching a brace to it before classifying the
+        # brace as generic.
+        #
+        # Valid function prototypes and definitions have return type, spaces,
+        # function name, optional spaces, then braces. They are followed by ; or
+        # {.
+        #
+        # "def\\s+\w+" matches preprocessor directives "#ifdef" and "#ifndef" so
+        # their contents aren't used as a return type.
+        comment_str = "/\*|\*/|//|" + linesep + "|"
+        extern_str = "(?P<ext_decl>extern \"C(\+\+)?\")\s+(?P<ext_brace>\{)?|"
+        braces_str = "\{|\}|;|def\s+\w+|\w+\s+\w+\s*(?P<paren>\(\))"
+        postfix_str = "(?=\s*(;|\{))"
+        token_regex = regex.compile(
+            comment_str + extern_str + braces_str + postfix_str)
+
+        EXTRA_POP_OFFSET = 2
+
+        # If value is greater than pop offset, the value needs to be restored in
+        # addition to an extra stack pop being performed. The pop offset is
+        # removed before assigning to is_c.
+        #
+        # is_c + pop offset == 2: C lang restore that needs extra brace pop
+        # is_c + pop offset == 3: C++ lang restore that needs extra brace pop
+        extern_brace_indices = [is_c]
+
+        in_multicomment = False
+        in_comment = False
+        for match in token_regex.finditer(lines):
+            token = match.group()
+
+            if token == "/*":
+                in_multicomment = True
+            elif token == "*/":
+                in_multicomment = False
+                in_comment = False
+            elif token == "//":
+                print("into comment")
+                in_comment = True
+            elif token == linesep:
+                print("out of comment")
+                in_comment = False
+            elif not in_multicomment and not in_comment:
+                if token == "{":
+                    extern_brace_indices.append(is_c)
+                elif token == "}":
+                    is_c = extern_brace_indices.pop()
+
+                    # If the next stack frame is from an extern without braces,
+                    # pop it.
+                    if extern_brace_indices[-1] >= EXTRA_POP_OFFSET:
+                        is_c = extern_brace_indices[-1] - EXTRA_POP_OFFSET
+                        extern_brace_indices.pop()
+                elif token == ";":
+                    # If the next stack frame is from an extern without braces,
+                    # pop it.
+                    if extern_brace_indices[-1] >= EXTRA_POP_OFFSET:
+                        is_c = extern_brace_indices[-1] - EXTRA_POP_OFFSET
+                        extern_brace_indices.pop()
+                elif token.startswith("extern"):
+                    # Back up language setting first
+                    if match.group("ext_brace"):
+                        extern_brace_indices.append(is_c)
+                    else:
+                        # Handling an extern without braces changing the
+                        # language type is done by treating it as a pseudo-brace
+                        # that gets popped as well when the next "}" or ";" is
+                        # encountered. The "extra pop" offset is used as a flag
+                        # on the top stack value that is checked whenever a pop
+                        # is performed.
+                        extern_brace_indices.append(is_c + EXTRA_POP_OFFSET)
+
+                    # Change language based on extern declaration
+                    if match.group("ext_decl") == "extern \"C\"":
+                        is_c = True
+                    else:
+                        is_c = False
+                elif match.group(
+                        "paren") and "return " not in match.group() and is_c:
+                    # Replaces () with (void)
+                    output += lines[pos:match.span("paren")[0]] + "(void)"
+                    pos = match.span("paren")[0] + len("()")
+
+                    file_changed = True
+
+        # Write rest of file if it wasn't all processed
+        if pos < len(lines):
+            output += lines[pos:]
+
+        if file_changed:
+            return (output, file_changed, True)
+        else:
+            return (lines, file_changed, True)
