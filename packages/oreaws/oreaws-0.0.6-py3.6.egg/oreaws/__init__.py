81473c0119b49c6db7cca47b35cbda7ff0f0b56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:51 +0000

    Add package oreaws/0.0.6/oreaws-0.0.6-py3.6.egg

---
+++ packages/oreaws/oreaws-0.0.6-py3.6.egg/oreaws/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from Modules.def_ec2 import Modules.def_ec2
+from Modules.def_s3 import Modules.def_s3
+from Modules.def_autoscaling import Modules.def_autoscaling
+from Modules.def_route53 import Modules.def_route53
