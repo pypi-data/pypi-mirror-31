7f982bb05da6039b2ffbb396426b780088611cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:11 +0000

    Add package SRF/0.0.5/SRF-0.0.5.tar.gz

---
+++ packages/SRF/SRF-0.0.5.tar.gz/SRF-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(name='SRF',
+      version='0.0.5',
+      description='Scalable Reconstruction Framework.',
+      url='https://github.com/Hong-Xiang/SRF',
+      author='Hong Xiang',
+      author_email='hx.hongxiang@gmail.com',
+      license='MIT',
+      packages=find_packages('src/python'),
+      package_dir={'': 'src/python'},
+      install_requires=['dxl-learn', 'dxl-core'],
+      entry_points="""
+            [console_scripts]
+            srf=srf.cli.main:srf
+      """,
+      zip_safe=False)
