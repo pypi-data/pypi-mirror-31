1545842ec5c2417a23cae59ddb124ac7244598f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:20 +0000

    Add package ndev_nester/1.2.0/ndev_nester-1.2.0.tar.gz

---
+++ packages/ndev_nester/ndev_nester-1.2.0.tar.gz/ndev_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(  
+    name            = 'ndev_nester',
+    version         = '1.2.0',
+    py_modules      = ['ndev_nester'],
+    author          = 'ndevelop',
+    author_email    = 'ndevelop@gmail.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nesteed lists'
+)
