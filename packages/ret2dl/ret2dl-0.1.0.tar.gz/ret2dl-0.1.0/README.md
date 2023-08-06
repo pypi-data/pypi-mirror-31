bb13a10e7ab86dbc0f6c96a73e83a0924542e89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:55 +0000

    Add package ret2dl/0.1.0/ret2dl-0.1.0.tar.gz

---
+++ packages/ret2dl/ret2dl-0.1.0.tar.gz/ret2dl-0.1.0/README.md
@@ -0,0 +1,7 @@
+# ret2dl
+
+## Installation
+
+```bash
+pip install ret2dl
+```
---
+++ packages/ret2dl/ret2dl-0.1.0.tar.gz/ret2dl-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ret2dl',
+    version='0.1.0',
+    description='Return to dl resolve exploit helper module',
+    long_description=readme,
+    author='Wei Bo Chen',
+    author_email='bananaappletw@gmail.com',
+    url='https://github.com/bananaappletw/ret2dl',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[]
+)
