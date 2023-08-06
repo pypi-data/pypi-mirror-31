9efa4d72f3c5ff8b5ad67b3584d96d7d735c82e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:03 +0000

    Add package flask-buzz/0.1.4/flask-buzz-0.1.4.tar.gz

---
+++ packages/flask-buzz/flask-buzz-0.1.4.tar.gz/flask-buzz-0.1.4/.project_metadata.py
@@ -0,0 +1,10 @@
+{
+    'author': 'Tucker Beck',
+    'author_email': 'tucker.beck@gmail.com',
+    'copyright': '2017, Tucker Beck',
+    'description': 'Extra bindings on py-buzz specifically for flask apps',
+    'license': 'MIT',
+    'name': 'flask-buzz',
+    'release': '0.1.4',
+    'version': '0.1',
+}
