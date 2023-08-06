103f753d75bba3954f22a88fb257fa4197339f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package ytdownloader/0.1.9/ytdownloader-0.1.9-py2.py3-none-any.whl

---
+++ packages/ytdownloader/ytdownloader-0.1.9-py2.py3-none-any.whl/ytdownloader/command_line.py
@@ -0,0 +1,23 @@
+import argparse
+import ytdownloader
+from ytdownloader import YoutubeDownloader
+
+
+def main():
+    ''' main function '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', nargs='?', default=None,
+                        help='''configuration file for ytdownloader''')
+    parser.add_argument('--version', action="store_true",
+                        help='current version of ytdownloader')
+    parser.add_argument('--about', action="store_true",
+                        help='YoutubeDownloader is an automated service to download videos from youtube')
+
+    args = parser.parse_args()
+
+    if args.version:
+        return ytdownloader.__VERSION__
+
+    yt = YoutubeDownloader()
+    yt.read_config(args.config)
+    yt.start_download()
