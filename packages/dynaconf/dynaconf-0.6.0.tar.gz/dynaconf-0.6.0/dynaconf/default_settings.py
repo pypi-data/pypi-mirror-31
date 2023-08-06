0e545962c9a23e45715b72c1aaf866d668ed63e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:52 +0000

    Add package dynaconf/0.6.0/dynaconf-0.6.0.tar.gz

---
+++ packages/dynaconf/dynaconf-0.6.0.tar.gz/dynaconf-0.6.0/dynaconf/default_settings.py
@@ -0,0 +1,40 @@
+# default proj root
+# pragma: no cover
+PROJECT_ROOT = "."
+
+# Default settings file
+SETTINGS_MODULE_FOR_DYNACONF = (
+    'settings.py,'
+    'settings.yaml,settings.yml,'
+    'settings.toml,settings.tml,'
+    'settings.ini,settings.conf,settings.properties,'
+    'settings.json'
+)
+
+# Namespace for envvars
+DYNACONF_NAMESPACE = 'DYNACONF'
+
+# The env var specifying settings module
+ENVVAR_FOR_DYNACONF = "DYNACONF_SETTINGS"
+
+# Default values for redis configs
+REDIS_FOR_DYNACONF = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0
+}
+
+# Loaders to read namespace based vars from different data stores
+LOADERS_FOR_DYNACONF = [
+    'dynaconf.loaders.env_loader',
+    # 'dynaconf.loaders.redis_loader'
+]
+
+# Errors in loaders should be silenced?
+DYNACONF_SILENT_ERRORS = SILENT_ERRORS = False
+
+# always fresh variables
+DYNACONF_ALWAYS_FRESH_VARS = ALWAYS_FRESH_VARS = []
+
+# debug
+DEBUG_LEVEL_FOR_DYNACONF = 'ERROR'
---
+++ packages/dynaconf/dynaconf-0.6.0.tar.gz/dynaconf-0.6.0/example/app/app.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from conf import settings
+
+print(settings.MYSQL_HOST)  # noqa
+print(settings.MYSQL_PASSWD)  # noqa
+print(settings.EXAMPLE)  # noqa
