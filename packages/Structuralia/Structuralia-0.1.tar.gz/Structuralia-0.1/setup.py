d33e7bc514119cc75853d16031fb2aa0536dd0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package Structuralia/0.1/Structuralia-0.1.tar.gz

---
+++ packages/Structuralia/Structuralia-0.1.tar.gz/Structuralia-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Structuralia',
+  packages = ['Structuralia'], 
+  version = '0.1',
+  description = 'A toolset to work with proteins',
+  author = 'Pedro Torres',
+  author_email = 'monteirotorres@gmail.com',
+  url = 'https://github.com/monteirotorres/Structuralia', 
+  download_url = 'https://github.com/monteirotorres/Structuralia/archive/0.1.tar.gz', 
+  keywords = ['PDB', 'structure', 'protein'], 
+  classifiers = [],
+)
