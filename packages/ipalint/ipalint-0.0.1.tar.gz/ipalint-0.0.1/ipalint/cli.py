c7f596a2ad31b9505b4ced9086d35a886d9403f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:02 +0000

    Add package ipalint/0.0.1/ipalint-0.0.1.tar.gz

---
+++ packages/ipalint/ipalint-0.0.1.tar.gz/ipalint-0.0.1/ipalint/cli.py
@@ -0,0 +1,89 @@
+import argparse
+import sys
+
+from ipalint.core import Core
+from ipalint import __version__
+
+
+
+class Cli:
+	"""
+	Singleton that handles the user input, inits the whole machinery, and takes
+	care of exiting the programme.
+	"""
+
+	def __init__(self):
+		"""
+		Constructor. Inits the argparse parser.
+		"""
+		usage = 'ipalint dataset [options]'
+		desc = ('simple linter that checks datasets for '
+				'IPA errors and inconsistencies')
+
+		self.parser = argparse.ArgumentParser(usage=usage,
+				description=desc, add_help=False)
+
+		input_args = self.parser.add_argument_group('dataset arguments')
+		input_args.add_argument('dataset', nargs='?', default=sys.stdin, help=(
+			'the dataset file to be linted; '
+			'if omitted, ipalint reads from stdin '
+			'(thus, ipalint X and cat X | ipalint are equivalent)'))
+		input_args.add_argument('--col', help=(
+			'specify the column containing the IPA data; '
+			'this could be the column index (starting from 0) '
+			'or the column name (if there is a header row)'))
+		input_args.add_argument('--no-header', action='store_true', help=(
+			'do not skip the first row of the file; '
+			'if this flag is not set, the first row will be skipped'))
+
+		output_args = self.parser.add_argument_group('output arguments')
+		output_args.add_argument('--ignore-nfd', action='store_true', help=(
+			'ignore warnings about strings that are not compliant with '
+			'Unicode\'s NFD normal form'))
+		output_args.add_argument('--ignore-ws', action='store_true', help=(
+			'ignore warnings about whitespace issues '
+			'(e.g. leading or trailing whitespace)'))
+		output_args.add_argument('--linewise', action='store_true', help=(
+			'show errors line-by-line; '
+			'by default each error is only shown once with '
+			'the offending lines\' numbers stacked together'))
+		output_args.add_argument('--no-lines', action='store_true', help=(
+			'only show the error messages, '
+			'without the line numbers where the errors originate; '
+			'ignored if --linewise is set'))
+
+		meta_args = self.parser.add_argument_group('meta arguments')
+		meta_args.add_argument('-h', '--help', action='help', help=(
+			'show this help message and exit'))
+		meta_args.add_argument('-v', '--version', action='version',
+			version=__version__,
+			help='show the version number and exit')
+
+
+	def run(self, raw_args=None):
+		"""
+		Parses the given arguments (if these are None, then argparse's parser
+		defaults to parsing sys.argv), inits a Core instance, calls its lint
+		method with the respective arguments, and then exits.
+		"""
+		args = self.parser.parse_args(raw_args)
+
+		core = Core()
+
+		try:
+			report = core.lint(**vars(args))
+		except Exception as err:
+			self.parser.error(str(err))
+
+		print(report)
+		self.parser.exit()
+
+
+
+def main():
+	"""
+	The (only) entry point for the command-line interface as registered in
+	setup.py. Inits a Cli instance and runs it with sys.argv.
+	"""
+	cli = Cli()
+	cli.run()
---
+++ packages/ipalint/ipalint-0.0.1.tar.gz/ipalint-0.0.1/setup.py
@@ -0,0 +1,57 @@
+import os.path
+
+from setuptools import setup, find_packages
+
+from ipalint import __version__
+
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(BASE_DIR, 'README.rst')) as f:
+	README = f.read()
+
+
+
+setup(
+	name = 'ipalint',
+	version = __version__,
+
+	description = (
+		'simple linter that checks datasets '
+		'for IPA errors and inconsistencies'),
+	long_description = README,
+
+	url = 'https://github.com/pavelsof/ipalint',
+
+	author = 'Pavel Sofroniev',
+	author_email = 'pavelsof@gmail.com',
+
+	license = 'MIT',
+
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3 :: Only',
+		'Topic :: Text Processing :: Linguistic'
+	],
+	keywords = 'IPA lint',
+
+	packages = find_packages(),
+	package_data = {'ipalint': ['data/*', 'tests/fixtures/*']},
+
+	install_requires = [],
+
+	test_suite = 'ipalint.tests',
+	tests_require = ['hypothesis >= 3.5'],
+
+	entry_points = {
+		'console_scripts': [
+			'ipalint = ipalint.cli:main'
+		]
+	}
+)
