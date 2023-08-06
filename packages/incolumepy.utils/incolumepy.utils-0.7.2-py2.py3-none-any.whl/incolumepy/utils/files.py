f89a241d9c452890b3c4c1b763d63390ec35200e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:56 +0000

    Add package incolumepy.utils/0.7.2/incolumepy.utils-0.7.2-py2.py3-none-any.whl

---
+++ packages/incolumepy.utils/incolumepy.utils-0.7.2-py2.py3-none-any.whl/incolumepy/utils/files.py
@@ -0,0 +1,103 @@
+import os
+from shutil import rmtree
+
+def ll(path='.', string=True):
+    ''' recursive list of file on directory.
+    string True return list of string, string False return list of tuple(path, file)'''
+
+    if not string:
+        return [(p, file) for p, _, files in os.walk(os.path.abspath(path)) for file in files]
+    else:
+        return [os.path.join(p, file) for p, _, files in os.walk(os.path.abspath(path)) for file in files]
+
+def realfilename(filebase, ext=None, digits=2, separador=True):
+    count = 0
+    sufix = {'default': 'txt', 0: 'txt', 1: None, 2: None}
+
+    if len(filebase.split('.')) > 1:
+        prefix = os.path.abspath(os.path.dirname(filebase))
+        basename = os.path.basename(filebase)
+        #print('1: ', prefix, basename)
+
+        filebase, sufix[1] = basename.split('.')
+        #print('2: ', filebase, sufix[1])
+
+        filebase = '{}/{}'.format(prefix, filebase)
+        #print(filebase, sufix[1])
+
+    if ext:
+        sufix[2] = ''.join([i for i in ext if i.isalpha()])
+        ext = sufix[2]
+    elif sufix[1]:
+        ext = sufix[1]
+    else:
+        ext = sufix['default']
+
+    dir = os.path.dirname(filebase)
+    #print(dir)
+    os.makedirs(os.path.abspath(dir), exist_ok=True, mode=0o777)
+    if separador:
+        sep = '_'
+    else:
+        sep = ''
+    while True:
+        try:
+            if count <= 0:
+                filename = '{}.{}'.format(filebase, ext)
+            else:
+                filename = ('{}{}{:0>%s}.{}' % digits).format(filebase, sep, count, ext)
+            if os.path.isfile(filename): raise IOError('Arquivo existente: {}'.format(filename))
+            print('Criado arquivo: {}'.format(filename))
+            return filename
+        except IOError as e:
+            print(e)
+        except:
+            raise
+        finally:
+            count += 1
+
+def run(remove=False):
+    with open(realfilename(
+            os.path.join('tmp', 'britodfbr','diretorio', 'para', 'teste'),
+            ext='.dat', separador=True), 'w') as file:
+        file.write('teste ok')
+
+    with open(realfilename(
+            os.path.join('tmp', 'diretorio', 'para', 'teste'),
+            separador=True, ext='md'),'w') as file:
+        file.write('teste ok')
+
+    with open(realfilename(('tmp/teste/test.json'),
+            separador=True, ext='bash'),'w') as file:
+        file.write('teste ok')
+
+    with open(realfilename(('tmp/teste/lll'),
+            separador=True),'w') as file:
+        file.write('teste ok')
+
+    with open(realfilename(('tmp/teste/jjj.json'),
+            separador=True),'w') as file:
+        file.write('teste ok')
+
+    with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+            digits=4, ext='log', separador=False), 'a') as file:
+        file.write(file.name)
+
+    with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+            digits=5, ext='log', separador=True), 'a') as file:
+        file.write(file.name)
+
+    with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+            digits=5, ext='csv'), 'a') as file:
+        file.write('{}'.format(file.name))
+
+    with open(realfilename('../utils/tmp/registro.xml'), 'w') as file:
+        file.write(file.name)
+
+    if remove:
+        dirlist=['tmp']
+        for i in dirlist:
+            rmtree(i)
+
+if __name__ == '__main__':
+    run(False)
