01fc2c24d68d2dc9a709109d81c45f2463d77ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:28 +0000

    Add package lemoncheesecake/0.18.2/lemoncheesecake-0.18.2.tar.gz

---
+++ packages/lemoncheesecake/lemoncheesecake-0.18.2.tar.gz/lemoncheesecake-0.18.2/lemoncheesecake/cli/commands/report.py
@@ -0,0 +1,27 @@
+from lemoncheesecake.cli.command import Command
+from lemoncheesecake.cli.utils import auto_detect_reporting_backends, add_report_path_cli_arg, get_report_path
+from lemoncheesecake.reporting import load_report
+from lemoncheesecake.reporting.backends.console import display_report_suites
+from lemoncheesecake.filter import add_report_filter_cli_args, make_report_filter, filter_suites
+
+
+class ReportCommand(Command):
+    def get_name(self):
+        return "report"
+
+    def get_description(self):
+        return "Display a report"
+
+    def add_cli_args(self, cli_parser):
+        group = cli_parser.add_argument_group("Show report")
+        add_report_path_cli_arg(group)
+        add_report_filter_cli_args(cli_parser, no_positional_argument=True)
+
+    def run_cmd(self, cli_args):
+        report_path = get_report_path(cli_args)
+        report = load_report(report_path, auto_detect_reporting_backends())
+        suites = filter_suites(report.get_suites(), make_report_filter(cli_args))
+
+        display_report_suites(suites)
+
+        return 0
