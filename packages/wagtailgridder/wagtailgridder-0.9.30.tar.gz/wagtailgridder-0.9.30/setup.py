340406e07510d2666520c34002e8f2135bbd73d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:46 +0000

    Add package wagtailgridder/0.9.30/wagtailgridder-0.9.30.tar.gz

---
+++ packages/wagtailgridder/wagtailgridder-0.9.30.tar.gz/wagtailgridder-0.9.30/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='wagtailgridder',
+    version="0.9.30",
+    description='Gridder layout for the Django CMS Wagtail.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Timothy Allen',
+    author_email='tallen@wharton.upenn.edu',
+    url='https://github.com/wharton/wagtailgridder',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'wagtail>=1.9',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
