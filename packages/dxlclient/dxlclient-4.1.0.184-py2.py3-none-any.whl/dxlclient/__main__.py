8c9757441ef9c90e43384574b0ed77d3bc835769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package dxlclient/4.1.0.184/dxlclient-4.1.0.184-py2.py3-none-any.whl

---
+++ packages/dxlclient/dxlclient-4.1.0.184-py2.py3-none-any.whl/dxlclient/__main__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# Copyright (c) 2017 McAfee Inc. - All Rights Reserved.
+################################################################################
+
+""" dxlclient bootstrap for CLI subcommands. """
+
+from __future__ import absolute_import
+from dxlclient._cli import cli_run
+
+cli_run()
