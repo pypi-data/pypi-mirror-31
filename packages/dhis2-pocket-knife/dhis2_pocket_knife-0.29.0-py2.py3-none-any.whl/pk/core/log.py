5feb580fc5aa4c3c602489f9b6b985955b7e0a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:13 +0000

    Add package dhis2-pocket-knife/0.29.0/dhis2_pocket_knife-0.29.0-py2.py3-none-any.whl

---
+++ packages/dhis2-pocket-knife/dhis2_pocket_knife-0.29.0-py2.py3-none-any.whl/pk/core/log.py
@@ -0,0 +1,33 @@
+import logging
+import os
+import errno
+
+import logzero
+
+
+def create_folders(file_path):
+    if not os.path.exists(os.path.dirname(file_path)):
+        try:
+            os.makedirs(os.path.dirname(file_path))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+
+def init(logging_to_file='', debug=False):
+    log_format = '%(color)s[%(asctime)s %(levelname)s]%(end_color)s %(message)s'
+    formatter = logzero.LogFormatter(fmt=log_format)
+    logzero.setup_default_logger(formatter=formatter)
+    if logging_to_file:
+        create_folders(logging_to_file)
+        print(u"Logging to {}".format(logging_to_file))
+
+        # 5 files max 10MB each
+        logzero.logfile(logging_to_file, loglevel=logging.INFO, maxBytes=int(1e7), backupCount=5)
+    else:
+        logzero.logfile(None)
+
+    if debug:
+        logzero.loglevel(logging.DEBUG)
+    else:
+        logzero.loglevel(logging.INFO)
