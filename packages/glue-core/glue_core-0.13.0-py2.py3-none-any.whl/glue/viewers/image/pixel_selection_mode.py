fc2abe7d66b570cb458d9adab1f3aca488c0ad61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package glue-core/0.13.0/glue_core-0.13.0-py2.py3-none-any.whl

---
+++ packages/glue-core/glue_core-0.13.0-py2.py3-none-any.whl/glue/viewers/image/pixel_selection_mode.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import, division, print_function
+
+from glue.config import viewer_tool
+from glue.viewers.common.qt.toolbar_mode import ToolbarModeBase
+from glue.core.command import ApplySubsetState
+from glue.core.subset import SliceSubsetState
+
+__all__ = ['PixelSelectionTool']
+
+
+class PixelSubsetState(SliceSubsetState):
+    def copy(self):
+        return PixelSubsetState(self.reference_data, self.slices)
+
+
+@viewer_tool
+class PixelSelectionTool(ToolbarModeBase):
+    """
+    Selects pixel under mouse cursor.
+    """
+
+    icon = "glue_crosshair"
+    tool_id = 'image:point_selection'
+    action_text = 'Pixel'
+    tool_tip = 'Select a single pixel based on mouse location'
+    status_tip = ('Mouse over to select a point. Click on the image to enable or disable selection.')
+
+    _on_move = False
+
+    def __init__(self, *args, **kwargs):
+        super(PixelSelectionTool, self).__init__(*args, **kwargs)
+        self._move_callback = self._select_pixel
+        self._press_callback = self._on_press
+
+    def _on_press(self, mode):
+        self._on_move = not self._on_move
+
+    def _select_pixel(self, mode):
+        """
+        Select a pixel
+        """
+
+        if not self._on_move:
+            return
+
+        x, y = self._event_xdata, self._event_ydata
+
+        if x is None or y is None:
+            return None
+
+        x = int(round(x))
+        y = int(round(y))
+
+        slices = [slice(None)] * self.viewer.state.reference_data.ndim
+        slices[self.viewer.state.x_att.axis] = slice(x, x + 1)
+        slices[self.viewer.state.y_att.axis] = slice(y, y + 1)
+
+        subset_state = PixelSubsetState(self.viewer.state.reference_data, slices)
+
+        cmd = ApplySubsetState(data_collection=self.viewer._data,
+                               subset_state=subset_state,
+                               use_current=False)
+        self.viewer._session.command_stack.do(cmd)
