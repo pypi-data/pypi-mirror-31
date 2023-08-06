841a7375f7405f119834f41323091ea1834a2c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:58 +0000

    Add package pretixGWW/4.2.1/pretixGWW-4.2.1.tar.gz

---
+++ packages/pretixGWW/pretixGWW-4.2.1.tar.gz/pretixGWW-4.2.1/pretix/api/views/common.py
@@ -0,0 +1,54 @@
+from rest_framework import viewsets
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from django.template.loader import render_to_string
+from pretix.api.serializers.order import OrderSerializer
+from pretix.api.serializers.voucher import  VoucherSerializer
+from pretix.base.models import Event,Order,Item,Voucher
+from django.conf import settings
+from rest_framework.response import Response
+import  json
+from django.core import serializers
+from django.db.models import Q
+from datetime import datetime
+
+@api_view(['GET','POST'])
+def order_print_status_update(request,*args,**kwargs):
+    order_code=kwargs['pk']
+    model_obj = Order.objects.get(code=order_code)
+    model_obj.printed_status=True
+    model_obj.save()
+    return Response({"data": "Changed successfully"}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def couponcodes(request,*args,**kwargs):
+    items=request.data['items']
+    today = datetime.today()
+    event_id = Event.objects.get(slug=kwargs['event']).id
+    model_obj=Voucher.objects.filter(item_id__in=items,event=event_id).filter(Q(valid_until__gte=today))
+    voucher_list=[]
+    for voucher in model_obj:
+        item_name= Item.objects.get(pk=voucher.item_id)
+        item_name_val=item_name.name;
+        #   item_name_val=json.loads(item_name_val)
+        voucher_dict={
+            "id":voucher.id,
+            "code":voucher.code,
+            "item": voucher.item_id,
+
+            "valid_until": voucher.valid_until,
+            "redeemed": voucher.redeemed,
+            "block_quota": voucher.block_quota,
+            "value": voucher.value,
+            "comment": voucher.comment,
+            "max_usages": voucher.max_usages,
+            "subevent": voucher.subevent,
+            "price_mode": voucher.price_mode,
+            "tag": voucher.tag,
+            "quota":voucher.quota
+        }
+        voucher_list.append(voucher_dict)
+    return Response({"data": voucher_list}, status=status.HTTP_200_OK)
+
+    #return  Response({"data":"raj"})
