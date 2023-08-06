3a8513321fbff146fb36d4b3cf743e69de1b8ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:31 +0000

    Add package zsq_test/0.1.0/zsq_test-0.1.0.tar.gz

---
+++ packages/zsq_test/zsq_test-0.1.0.tar.gz/zsq_test-0.1.0/pkg/main.py
@@ -0,0 +1,5 @@
+#coding=utf8
+
+class Demo:
+    def test(self):
+        return "hello"
---
+++ packages/zsq_test/zsq_test-0.1.0.tar.gz/zsq_test-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zsq_test',
+    version='0.1.0',
+    description=('test'),
+    long_description=open('README.rst').read(),
+    author='zsq',
+    author_email='1574904986@qq.com',
+    license='BSD License',
+    url='https://github.com/desion/tidy_page',
+    packages=['pkg'],
+    platforms=["all"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
