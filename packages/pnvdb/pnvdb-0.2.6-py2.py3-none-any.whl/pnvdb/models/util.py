46cd9fd79e35e969893144fd6fc4009d03d1cfed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:01 +0000

    Add package pnvdb/0.2.6/pnvdb-0.2.6-py2.py3-none-any.whl

---
+++ packages/pnvdb/pnvdb-0.2.6-py2.py3-none-any.whl/pnvdb/models/util.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+""" Contains various helper functions """
+import requests
+from .pnvdb_exceptions import *
+
+
+def _fetch_data(nvdb, url_add, payload=None, file_format='json'):
+    url = '{baseUrl}/{url_add}'.format(baseUrl=nvdb.base_url, url_add=url_add)
+    resp = requests.get(url, params=payload, headers=nvdb.headers)
+    #print(resp.headers) # For debugging
+    #print(resp.url) # For debugging
+    data = _check_response(resp, file_format)
+    return data
+
+def _check_response(resp, file_format='json'):
+    """Function verifes that a 200 code was returned from the API
+    and returns the data as Json.
+    If a 200 code was not returned, it tries to return the error recived 
+    from the API."""
+    if resp.status_code == requests.codes.ok and file_format == 'json':
+        return resp.json()
+    elif resp.status_code == requests.codes.ok and file_format == 'xml':
+        return resp.text
+    else:
+        print(resp.url)
+        raise ApiError(read_api_error(resp))
