f68c3246791e2e334b779cb36f178a6cea5ffb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:01 +0000

    Add package apistar-mail/0.3.0/apistar-mail-0.3.0.tar.gz

---
+++ packages/apistar-mail/apistar-mail-0.3.0.tar.gz/apistar-mail-0.3.0/apistar_mail/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for apistar-mail"""
+
+__author__ = """Drew Bednar"""
+__email__ = 'drew@androiddrew.com'
+__version__ = '0.3.0'
+
+from .mail import MailComponent, Message, Mail  # NOQA: F401
