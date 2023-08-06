17db609e628c9ccef51178056bfe27e7c317b0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:55 +0000

    Add package piwifi/0.3/piwifi-0.3.tar.gz

---
+++ packages/piwifi/piwifi-0.3.tar.gz/piwifi-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'piwifi',
+  packages = ['piwifi'],
+  version = '0.3',
+  description = 'Manage wpa_supplicant wifi networks on Raspberry Pi',
+  author = 'Jeff Leary',
+  author_email = 'sillymonkeysoftware@gmail.com',
+  url = 'https://github.com/jeffleary00/piwifi',
+  download_url = 'https://github.com/jeffleary00/piwifi/tarball/0.3',
+  classifiers = [],
+  keywords = ['wpa_supplicant', 'raspberry pi', 'linux'], 
+)
