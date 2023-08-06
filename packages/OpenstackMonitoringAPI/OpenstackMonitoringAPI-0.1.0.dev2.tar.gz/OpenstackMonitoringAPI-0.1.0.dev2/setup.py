6a8630ef0b3cb32497e3240f230ecb4235c49367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:23 +0000

    Add package OpenstackMonitoringAPI/0.1.0.dev2/OpenstackMonitoringAPI-0.1.0.dev2.tar.gz

---
+++ packages/OpenstackMonitoringAPI/OpenstackMonitoringAPI-0.1.0.dev2.tar.gz/OpenstackMonitoringAPI-0.1.0.dev2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='OpenstackMonitoringAPI',
+    version='0.1.0.dev2',
+    description="Thrift API for the de.NBI Openstack monitoring project",
+    author='Marius Dieckmann',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['OpenstackCloudMonitoringAPI'],
+    install_requires=['thrift'],
+)
