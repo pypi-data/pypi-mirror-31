8e1200d3a4d3e05079b8b2930f8d9f7f46b4d767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:58 +0000

    Add package MiAgDa/0.0.1/MiAgDa-0.0.1.tar.gz

---
+++ packages/MiAgDa/MiAgDa-0.0.1.tar.gz/MiAgDa-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MiAgDa',
+      version='0.0.1',
+      description='Minimalistic Agile Dashboard',
+      url='https://github.com/HaBaLeS/MiAgDa',
+      author='falko lehmann',
+      author_email='falko@lehmann-carpzov.de',
+      license='beer',
+      packages=['miagda'],
+      zip_safe=False)
