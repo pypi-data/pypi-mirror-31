14a8169086b181397be6b57ed8a163d7bc444f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:52 +0000

    Add package clustertree/0.0.3/clustertree-0.0.3.tar.gz

---
+++ packages/clustertree/clustertree-0.0.3.tar.gz/clustertree-0.0.3/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import find_packages, Extension, Command
+from distutils.core import setup
+
+
+from Cython.Build import cythonize
+
+from clustertree import __version__
+
+extensions = []
+extensions.append( Extension( "clustertree.src.clustertree",
+                            [ "clustertree/src/clustertree.pyx",
+                              "clustertree/src/cluster.c"], ) )
+
+setup(version=__version__,
+	  name='clustertree',
+      description="Clustertrees.",
+      long_description="Clustertrees.",
+      # Kanwei Li is the actual author, I just maintain it.
+      author="Endre Bakken Stovner",
+      author_email="endrebak85@gmail.com",
+      # package_dir = { '': 'lib' },
+      packages=find_packages(),
+      setup_requires=['cython'],
+      ext_modules=cythonize(extensions),
+      package_data={'': ['*.pyx', '*.pxd', '*.h', '*.c']},
+      include_dirs=["."],
+)
