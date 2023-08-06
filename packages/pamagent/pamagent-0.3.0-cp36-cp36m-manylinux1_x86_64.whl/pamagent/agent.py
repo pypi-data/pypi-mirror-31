9d25cc62d9a15b1c0147d05c651609fea120d47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:17 +0000

    Add package pamagent/0.3.0/pamagent-0.3.0-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pamagent/pamagent-0.3.0-cp36-cp36m-manylinux1_x86_64.whl/pamagent/agent.py
@@ -0,0 +1,25 @@
+import logging
+from itertools import count
+
+from pamagent.hooks import requests_hook, django_hook, sqlite_hook, psycopg2_hook, mysql_hook, redis_hook
+# noinspection PyUnresolvedReferences
+from pamagent import pamagent_core
+
+_logger = logging.getLogger(__name__)
+
+
+def _init_builtin() -> None:
+        requests_hook.patch()
+        django_hook.patch()
+        sqlite_hook.patch()
+        psycopg2_hook.patch()
+        mysql_hook.patch()
+        redis_hook.path()
+
+
+def init(token: str, collector_host: str="pamcollector.pushamp.com", _count=count()) -> None:
+    if next(_count):
+        _logger.warning("The PamAgent The was already initialized and activate")
+        return
+    _init_builtin()
+    pamagent_core.activate(token, collector_host)
---
+++ packages/pamagent/pamagent-0.3.0-cp36-cp36m-manylinux1_x86_64.whl/pamagent/transaction_cache.py
@@ -0,0 +1,59 @@
+import logging
+
+import _thread
+
+# noinspection PyUnresolvedReferences
+from pamagent import pamagent_core
+
+_logger = logging.getLogger(__name__)
+
+
+def current_thread_id():
+    """
+    Returns the thread ID for the caller.
+    """
+    return _thread.get_ident()
+
+
+def current_transaction():
+    """
+    Return the transaction object if one exists for the currently executing thread.
+    """
+    return pamagent_core.get_transaction(_thread.get_ident())
+
+
+def save_transaction(transaction):
+    """
+    Saves the specified transaction away under the thread ID of the current executing thread.
+    """
+    res = pamagent_core.set_transaction(id=transaction.thread_id, transaction=transaction.name, path=transaction.path)
+    if not res:
+        raise RuntimeError('Transaction already active')
+
+
+def drop_transaction(transaction):
+    """
+    Drops the specified transaction, validating that it is actually saved away under the current executing thread.
+    """
+
+    res = pamagent_core.drop_transaction(id=transaction.thread_id)
+    if not res:
+        raise RuntimeError('No active transaction')
+
+
+def set_transaction_path(path):
+    """
+    :param path: path of request URI
+    :return: None
+    """
+    res = pamagent_core.set_transaction_path(id=current_thread_id(), path=path)
+    if not res:
+        raise RuntimeError("No active transaction")
+
+
+def get_start_time(transaction):
+    return pamagent_core.get_transaction_start_time(id=transaction.thread_id)
+
+
+def get_end_time(transaction):
+    return pamagent_core.get_transaction_end_time(id=transaction.thread_id)
