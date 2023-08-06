bc709863d8cc323b987db5fcadc0d7624c584ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:54 +0000

    Add package pyats.robot/4.1.0/pyats.robot-4.1.0-py3-none-any.whl

---
+++ packages/pyats.robot/pyats.robot-4.1.0-py3-none-any.whl/ats/robot/__init__.py
@@ -0,0 +1,6 @@
+# metadata
+__version__ = '4.1.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2017, Cisco Systems Inc.'
+
