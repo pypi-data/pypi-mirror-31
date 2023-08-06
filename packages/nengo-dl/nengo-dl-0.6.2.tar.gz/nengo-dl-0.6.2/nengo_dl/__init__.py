27636bb77f5b22fc95e00f7726b9ed64177fa560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package nengo-dl/0.6.2/nengo-dl-0.6.2.tar.gz

---
+++ packages/nengo-dl/nengo-dl-0.6.2.tar.gz/nengo-dl-0.6.2/nengo_dl/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+import os
+import warnings
+
+__copyright__ = "2015-2018, Applied Brain Research"
+__license__ = "Free for non-commercial use; see LICENSE.rst"
+from .version import version as __version__
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
+
+# need to explicitly import these to trigger the builder registration
+from nengo_dl import (
+    op_builders, neuron_builders, process_builders, learning_rule_builders)
+
+# import into top-level namespace
+from nengo_dl import dists
+from nengo_dl.simulator import Simulator
+from nengo_dl.tensor_node import (
+    TensorNode, tensor_layer, reshaped)
+from nengo_dl.utils import configure_settings
+from nengo_dl.neurons import SoftLIFRate
+
+# apply tensorflow monkey patches
+from nengo_dl import tensorflow_patch
+tensorflow_patch.patch_dynamic_stitch_grad()
+tensorflow_patch.patch_state_grads()
---
+++ packages/nengo-dl/nengo-dl-0.6.2.tar.gz/nengo-dl-0.6.2/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import io
+import runpy
+import os
+import pkg_resources
+import sys
+
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    raise ImportError(
+        "'pip'/'setuptools' is required but not installed. To install it, "
+        "follow the instructions at "
+        "https://pip.pypa.io/en/stable/installing/#installing-with-get-pip-py")
+
+if "bdist_wheel" in sys.argv:
+    # when building wheels we have to pick a requirement ahead of time (can't
+    # check it at install time). so we'll go with tensorflow (non-gpu), since
+    # that is the safest option
+    tf_req = "tensorflow"
+else:
+    # check if one of the tensorflow packages is already installed (so that we
+    # don't force tensorflow to be installed if e.g. tensorflow-gpu is already
+    # there)
+    tf_dists = ["tf-nightly-gpu", "tf-nightly", "tensorflow-gpu"]
+    installed_dists = [d.project_name for d in pkg_resources.working_set]
+    for d in tf_dists:
+        if d in installed_dists:
+            tf_req = d
+            break
+    else:
+        tf_req = "tensorflow"
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+root = os.path.dirname(os.path.realpath(__file__))
+version = runpy.run_path(os.path.join(
+    root, 'nengo_dl', 'version.py'))['version']
+
+# TODO: set up extra requires
+
+setup(
+    name="nengo-dl",
+    version=version,
+    author="Daniel Rasmussen",
+    author_email="daniel.rasmussen@appliedbrainresearch.com",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[],
+    url="https://github.com/nengo/nengo-dl",
+    license="Free for non-commercial use",
+    description="Deep learning integration for Nengo",
+    long_description=read('README.rst', 'CHANGES.rst'),
+    install_requires=["nengo>=2.7.0", "numpy>=1.11", "%s>=1.3.0" % tf_req,
+                      "progressbar2>=3.37.1",
+                      "backports.tempfile;python_version<'3.4'"],
+    entry_points={"nengo.backends": ["dl = nengo_dl:Simulator"]},
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'License :: Free for non-commercial use',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Scientific/Engineering'],
+)
