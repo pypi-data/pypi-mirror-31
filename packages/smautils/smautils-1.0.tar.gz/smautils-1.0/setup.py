550dae6993143e48601e7a9bbdd23c42235646c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:34 +0000

    Add package smautils/1.0/smautils-1.0.tar.gz

---
+++ packages/smautils/smautils-1.0.tar.gz/smautils-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='smautils',
+	version='1.0',
+	description='Wrapper for the Miriad Interferometric Reduction Package',
+	author='Mohammed Chamma',
+	author_email='mchamma@uwo.ca',
+	url='https://github.com/mef51/smautils',
+	keywords='radio interferometry SMA wrapper miriad',
+	classifiers=["Programming Language :: Python :: 3"],
+	license='MIT')
