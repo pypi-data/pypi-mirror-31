170c156cc7694c3c0b32576efae6af0f62ac22da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package virtapi/1.3.4/virtapi-1.3.4.tar.gz

---
+++ packages/virtapi/virtapi-1.3.4.tar.gz/virtapi-1.3.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='virtapi',
+      version='1.3.4',
+      description='VirtAPI is powerful libvirt/kvm wrapper with plugable API and bonus CLI tool out of the box.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/x-rst',
+      url='http://github.com/spiperac/virtapi',
+      author='Strahinja Piperac',
+      author_email='spiperac@denkei.org',
+      license='BSD',
+      packages=['virtapi', 'virtapi.controller', 'virtapi.model', 'virtapi.tests'],
+      include_package_data=True,
+      scripts=['vatool'],
+      install_requires=[
+	'libvirt-python',
+	'lxml==4.1.0',
+	'paramiko==2.2.1',
+	'prettytable==0.7.2',
+	'xmltodict==0.10.1',
+	'pyyaml==3.12',
+	'requests==2.13.0',
+	'tqdm==4.19.4',
+      ],
+      zip_safe=False)
