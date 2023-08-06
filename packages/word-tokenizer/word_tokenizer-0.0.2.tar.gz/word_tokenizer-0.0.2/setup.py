0da437962d5b6ea5900f124c95f293fa51a161a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:38 +0000

    Add package word-tokenizer/0.0.2/word_tokenizer-0.0.2.tar.gz

---
+++ packages/word-tokenizer/word_tokenizer-0.0.2.tar.gz/word_tokenizer-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+class About(object):
+    NAME='word_tokenizer'
+    VERSION='0.0.2'
+    AUTHOR='blester125'
+    EMAIL=f'{AUTHOR}@gmail.com'
+    URL=f'https://github.com/{AUTHOR}/{NAME}'
+    DL_URL=f'{URL}/archive/{VERSION}.tar.gz'
+    LICENSE='MIT'
+
+setup(
+    name=About.NAME,
+    version=About.VERSION,
+    description='word tokenizer',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author=About.AUTHOR,
+    author_email=About.EMAIL,
+    url=About.URL,
+    download_url=About.DL_URL,
+    license=About.LICENSE,
+    packages=find_packages(),
+    package_data={
+        'word_tokenizer': [
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+    ],
+    keywords=[],
+)
