7931d4c9ec236961b17b7e0869f94abd2604c6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:13 +0000

    Add package raindrop/0.2.2/raindrop-0.2.2.tar.gz

---
+++ packages/raindrop/raindrop-0.2.2.tar.gz/raindrop-0.2.2/raindrop/raindrop.py
@@ -0,0 +1,118 @@
+import httplib2, json, secrets
+
+sandbox = "https://sandbox.hydrogenplatform.com/hydro/v1"
+production = "https://api.hydrogenplatform.com/hydro/v1"
+
+environment = "none"
+
+# Define a function
+def health():
+    print("Hello, World!")
+
+def setEnvironment(setter):
+    global environment
+    if setter == "sandbox":
+        environment = sandbox
+        return environment
+    elif setter == "production":
+        environment = production
+        return environment
+    else:
+        return "Please call this function with either 'sandbox' or 'production'"
+
+def verify(username, password, user, message, application_id):
+    global environment
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/verify_signature?username="+user+"&msg="+message+"&application_id="+application_id,
+        "GET",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def addClientToApp(username, password, user, application_id):
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/application/client?username="+user+"&application_id="+application_id,
+        "POST",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def removeClientFromApp(username, password, user, application_id):
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/application/client?username="+user+"&application_id="+application_id,
+        "DELETE",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def generateMessage():
+    code = str(secrets.randbelow(int(1e6))).zfill(6)
+    return(code)
+
+def whitelist(username, password, address):
+    global environment
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/whitelist/"+address,
+        "GET",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def challenge(username, password, hydroAddressId):
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/challenge?hydro_address_id="+hydroAddressId,
+        "GET",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def authenticate(username, password, hydroAddressId):
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/authenticate?hydro_address_id="+hydroAddressId,
+        "GET",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
+
+def verify_transaction(username, password, transactionHash):
+    if environment == "none":
+        return "Please set the environment variable"
+
+    h = httplib2.Http(".cache")
+    h.add_credentials(username, password)
+    resp, content = h.request(environment + "/transaction?transaction_hash="+transactionHash,
+        "POST",
+        headers={'content-type':'application/json'} )
+
+    resp_json = json.loads(content.decode("utf-8"))
+    return(resp_json)
