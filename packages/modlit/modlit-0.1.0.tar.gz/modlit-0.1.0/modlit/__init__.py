73a157b9a7b68c5d52ccb70777425ef77bda4e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package modlit/0.1.0/modlit-0.1.0.tar.gz

---
+++ packages/modlit/modlit-0.1.0.tar.gz/modlit-0.1.0/modlit/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: modlit
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Helpful utilities for SQLAlchemy (and GeoAlchemy) data model projects.
+"""
+
+__version__ = '0.1.0'  #: the working version
+__release__ = '0.1.0'  #: the release version
