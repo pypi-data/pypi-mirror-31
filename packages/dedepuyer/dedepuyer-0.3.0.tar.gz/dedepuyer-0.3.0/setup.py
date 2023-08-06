c4707dd6f9cdf7eb2a10715ec6a99526e33c7174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package dedepuyer/0.3.0/dedepuyer-0.3.0.tar.gz

---
+++ packages/dedepuyer/dedepuyer-0.3.0.tar.gz/dedepuyer-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="dedepuyer",
+    version='0.3.0',
+    description="A project to find git information.",
+    long_description="A project to find git information about authors' commits,most active days and time.",
+    author="AdhiTj",
+    author_email="adhitjatur@gmail.com",
+    url="http://gitlab.h-seo.com/adhitj/dedepuyer",
+    license="GPLv3+",
+    py_modules=['dedepuyer'],
+		include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        dedepuyer=dedepuyer:main
+    ''',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7'
+    )
+)
