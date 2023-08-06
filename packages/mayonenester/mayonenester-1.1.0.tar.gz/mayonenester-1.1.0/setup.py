e485c16f21efa133776ab41d0a9978bfb74b8e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:54 +0000

    Add package mayonenester/1.1.0/mayonenester-1.1.0.tar.gz

---
+++ packages/mayonenester/mayonenester-1.1.0.tar.gz/mayonenester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'mayonenester',
+	version ='1.1.0',
+	py_modules = ['mayonenester'],
+	author = 'lijiankang',
+	author_email = '1124358467@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
