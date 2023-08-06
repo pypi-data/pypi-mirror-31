f06b989b46b8c24435400649a172cc7acbe1a2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package django-openid-op/0.2.4/django-openid-op-0.2.4.tar.gz

---
+++ packages/django-openid-op/django-openid-op-0.2.4.tar.gz/django-openid-op-0.2.4/openid_connect_op/signals.py
@@ -0,0 +1,30 @@
+import django.dispatch
+
+before_user_consent = django.dispatch.Signal(providing_args=["openid_client", "user"])
+"""
+A signal that is sent after the user is logged in but before the consent screen is shown. The signal is sent even
+when the consent step is skipped, if it was signed previously. In that case, consent_already_signed is set to True.
+If the signal listener returns an HttpResponse, this response is returned to the user agent instead of the standard 
+openid redirection response. For example, user can be directed to a page that states that he/she has no rights 
+to access the application.
+
+Params:
+
+openid_client : an instance of OpenID client that the user is trying to log into
+user          : the user that is logging to
+"""
+
+
+after_user_consent = django.dispatch.Signal(providing_args=["openid_client", "user"])
+"""
+A signal that is sent after the user has accepted consent. If the consent has been accepted in a previous login, 
+this signal is not sent. If the signal listener
+returns an HttpResponse, this response is returned to the user agent instead of the standard openid redirection
+response. For example, user can be directed to a page that states that he/she has no rights to access the application.
+"""
+
+
+before_userinfo_token = django.dispatch.Signal(providing_args=["openid_client", "userinfo_token", "user"])
+"""
+A signal that is sent before the userinfo token is returned to the openid client.
+"""
