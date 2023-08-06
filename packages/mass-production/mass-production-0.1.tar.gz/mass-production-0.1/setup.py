27358be03e37844172f88df29711b61169dc8a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:56 +0000

    Add package mass-production/0.1/mass-production-0.1.tar.gz

---
+++ packages/mass-production/mass-production-0.1.tar.gz/mass-production-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mass-production',
+      version='0.1',
+      description="Call a function (e.g a factory-boy Factory's create method "
+                  "according to a defined pattern.",
+      url='http://github.com/RobinRamael/mass-production',
+      author='Robin Ramael',
+      author_email='robin.ramael@gmail.com',
+      license='MIT',
+      packages=['mass_production'],
+      zip_safe=False)
