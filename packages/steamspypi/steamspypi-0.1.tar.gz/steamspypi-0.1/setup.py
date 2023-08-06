bc0246266471897b32356f0ae06e93795b9cd56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:40 +0000

    Add package steamspypi/0.1/steamspypi-0.1.tar.gz

---
+++ packages/steamspypi/steamspypi-0.1.tar.gz/steamspypi-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='steamspypi',
+    packages=['steamspypi'],
+    version='0.1',
+    description='SteamSpy API on PyPI',
+    author='Wok',
+    author_email='wok@tuta.io',
+    url='https://github.com/woctezuma/steamspypi',
+    download_url='https://github.com/woctezuma/steamspypi/archive/0.1.tar.gz',
+    keywords=['steam', 'steamspy', 'api'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
