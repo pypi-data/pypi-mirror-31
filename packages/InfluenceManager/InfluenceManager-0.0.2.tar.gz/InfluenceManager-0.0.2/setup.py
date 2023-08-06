318915926fdc2d93a3f2ef072dd28b2d3d0bbe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package InfluenceManager/0.0.2/InfluenceManager-0.0.2.tar.gz

---
+++ packages/InfluenceManager/InfluenceManager-0.0.2.tar.gz/InfluenceManager-0.0.2/setup.py
@@ -0,0 +1,21 @@
+"""
+Backend module for retrieving top influencers from Twitter.
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="InfluenceManager",
+    version="0.0.2",
+    description="Backend module for retrieving top influencers from Twitter.",
+    author="Ash Prince",
+    author_email="i7629228@bournemouth.ac.uk",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=["InfluenceManager/bin/topic_query"],
+    install_requires=[]
+)
