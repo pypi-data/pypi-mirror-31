c7ae5ec423b5dba42a13c916eed8004e24f60488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:30 +0000

    Add package waldur-cost-planning/0.6.2/waldur-cost-planning-0.6.2.tar.gz

---
+++ packages/waldur-cost-planning/waldur-cost-planning-0.6.2.tar.gz/waldur-cost-planning-0.6.2/README.rst
@@ -0,0 +1,4 @@
+Waldur cost planning plugin
+===========================
+
+Waldur cost planning plugin allows to get a price estimate without actually creating the infrastructure.
---
+++ packages/waldur-cost-planning/waldur-cost-planning-0.6.2.tar.gz/waldur-cost-planning-0.6.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+dev_requires = [
+    'Sphinx==1.2.2',
+]
+
+install_requires = [
+    'waldur-core>=0.151.1',
+    'waldur_openstack>=0.38.2',
+    'waldur_digitalocean>=0.10.2',
+    'waldur_aws>=0.11.2',
+    'waldur_azure>=0.3.4',
+]
+
+setup(
+    name='waldur-cost-planning',
+    version='0.6.2',
+    author='OpenNode Team',
+    author_email='info@opennodecloud.com',
+    url='http://waldur.com',
+    description='Waldur cost planning plugin allows to get a price estimate without actually creating the infrastructure.',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=install_requires,
+    zip_safe=False,
+    extras_require={
+        'dev': dev_requires,
+    },
+    entry_points={
+        'waldur_extensions': (
+            'waldur_cost_planning = waldur_cost_planning.extension:CostPlanningExtension',
+        ),
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
