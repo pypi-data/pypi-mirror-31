d7c25be433a22fb8eada141c163e86bdf3bacbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package thug/0.9.21/thug-0.9.21.tar.gz

---
+++ packages/thug/thug-0.9.21.tar.gz/thug-0.9.21/thug/DOM/W3C/Core/ProcessingInstruction.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from .Node import Node
+
+
+class ProcessingInstruction(Node):
+    def __init__(self, doc, target, data):
+        self._target = target
+        self.data    = data
+        Node.__init__(self, doc)
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def nodeName(self):
+        return self._target
+
+    @property
+    def nodeType(self):
+        return Node.PROCESSING_INSTRUCTION_NODE
+
+    def getNodeValue(self):
+        return self.data
+
+    def setNodeValue(self, value):
+        self.data = value
+
+    nodeValue = property(getNodeValue, setNodeValue)
---
+++ packages/thug/thug-0.9.21.tar.gz/thug-0.9.21/thug/DOM/W3C/HTML/HTMLFieldSetElement.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from .HTMLElement import HTMLElement
+
+
+class HTMLFieldSetElement(HTMLElement):
+    def __init__(self, doc, tag):
+        HTMLElement.__init__(self, doc, tag)
+
+    @property
+    def form(self):
+        pass
