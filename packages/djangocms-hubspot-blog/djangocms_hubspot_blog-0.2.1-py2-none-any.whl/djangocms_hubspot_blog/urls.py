742777cdb4de464241a1656d823be18c11151627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:40 +0000

    Add package djangocms-hubspot-blog/0.2.1/djangocms_hubspot_blog-0.2.1-py2-none-any.whl

---
+++ packages/djangocms-hubspot-blog/djangocms_hubspot_blog-0.2.1-py2-none-any.whl/djangocms_hubspot_blog/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from djangocms_hubspot_blog.views import HubspotPostsList, HubspotPostDetail
+
+urlpatterns = [
+    url(r'^$', HubspotPostsList.as_view(), name='hubspot-posts-list'),
+    url(r'^(?P<pk>\d+)/(?P<slug>[-\w]+)$', HubspotPostDetail.as_view(), name='hubspot-post'),
+]
