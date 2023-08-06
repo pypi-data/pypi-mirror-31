0d51f935526c66c57953990dd20b2a535a87eb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:02 +0000

    Add package fints2ledger/0.4/fints2ledger-0.4.tar.gz

---
+++ packages/fints2ledger/fints2ledger-0.4.tar.gz/fints2ledger-0.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='fints2ledger',
+      version='0.4',
+      description='A tool for downloading transactions from FinTS banking APIs and sorting them into a ledger journal.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/MoritzR/fints2ledger',
+      author='Moritz Rumpf',
+      author_email='moritz.rumpf@gmail.com',
+      license='MIT',
+
+      entry_points={
+          'console_scripts': ['fints2ledger=fints2ledger.main:main'],
+      },
+
+      install_requires=[
+          'mt-940',
+          'fints',
+          'PyYAML'
+      ],
+      setup_requires=['green'],
+      packages=['fints2ledger'],
+      dependency_links=[
+          'https://github.com/raphaelm/python-fints/tarball/master/#egg=fints-0.2.1'],
+      zip_safe=False,
+
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"
+      ]
+      )
