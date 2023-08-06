31780ef00cf096222ea8fc6b3514cbdab6e0f53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:23 +0000

    Add package pytorch-sconce/0.7.0/pytorch-sconce-0.7.0.tar.gz

---
+++ packages/pytorch-sconce/pytorch-sconce-0.7.0.tar.gz/pytorch-sconce-0.7.0/docs/_sources/data_generator.rst.txt
@@ -0,0 +1,22 @@
+.. role:: hidden
+    :class: hidden-section
+
+
+Data Generators
+===============
+
+.. currentmodule:: sconce.data_generators
+
+
+:hidden:`DataGenerator`
+~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: DataGenerator
+    :members:
+
+
+:hidden:`ImageDataGenerator`
+~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: ImageDataGenerator
+    :members:
---
+++ packages/pytorch-sconce/pytorch-sconce-0.7.0.tar.gz/pytorch-sconce-0.7.0/sconce/monitors/stdout_monitor.py
@@ -0,0 +1,33 @@
+from sconce.monitors.base import Monitor
+from sconce.utils import Progbar
+from torch.autograd import Variable
+
+
+class StdoutMonitor(Monitor):
+    def __init__(self, metric_names, progbar_kwargs={},
+            name='stdout_monitor'):
+        super().__init__(name=name)
+        self._metric_names = metric_names
+        self._progbar_kwargs = progbar_kwargs
+        self._progress_bar = None
+
+    def start_session(self, num_steps, **kwargs):
+        self._progress_bar = Progbar(num_steps, **self._progbar_kwargs)
+
+    def write(self, data, step, **kwargs):
+        if step != int(step):
+            # don't update for partial steps
+            return
+
+        if self._progress_bar is None:
+            raise RuntimeError("You must call 'start_session' before "
+                    "calling 'write'")
+
+        values = []
+        for key, name in self._metric_names.items():
+            if key in data.keys():
+                value = data[key]
+                if isinstance(value, Variable):
+                    value = value.data[0]
+                values.append((name, value))
+        self._progress_bar.add(1, values)
