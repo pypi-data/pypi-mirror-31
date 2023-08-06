a2383a9ba5b0919004f1012285c739e150005447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:33 +0000

    Add package nn-generator/1.0.4/nn_generator-1.0.4.tar.gz

---
+++ packages/nn-generator/nn_generator-1.0.4.tar.gz/nn_generator-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+setup(name='nn_generator',
+      version='1.0.4',
+      description='FC NN configurable via json/YML config file',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      keywords='configurable nn deep_nn fully connected',
+      url='https://github.com/Alisa-lisa/nn_generator',
+      author='Alisa Dammer',
+      author_email='alisa.dammer@gmail.com',
+      license='MIT',
+      packages=['nn_generator'],
+      include_package_data=True,
+      install_requires=[
+            'matplotlib',
+            'numpy',
+            'pandas',
+            'pyparsing',
+            'pytz',
+            'PyYAML',
+      ],
+      zip_safe=False)
