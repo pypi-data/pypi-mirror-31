3fd11fd9c3a5618dbad7c885bbc8876a40400665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:03 +0000

    Add package xia-nester/1.1.0/xia_nester-1.1.0.tar.gz

---
+++ packages/xia-nester/xia_nester-1.1.0.tar.gz/xia_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'xia_nester',
+    version = '1.1.0',
+    py_modules = ['xia_nester'],
+    author = 'xiayankun',
+    author_email = 'xiayankun@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists'
+)
