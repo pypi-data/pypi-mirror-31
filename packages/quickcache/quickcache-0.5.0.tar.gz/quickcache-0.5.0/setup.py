6d3ee356d9907ac64613e39c078662b9ff9aab55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:35 +0000

    Add package quickcache/0.5.0/quickcache-0.5.0.tar.gz

---
+++ packages/quickcache/quickcache-0.5.0.tar.gz/quickcache-0.5.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from setuptools import setup
+
+setup(
+    name='quickcache',
+    version='0.5.0',
+    description='caching has never been easier',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='https://github.com/dimagi/quickcache',
+    packages=['quickcache'],
+    test_suite='test_quickcache',
+    install_requires=[
+        'six==1.11.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
