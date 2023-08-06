8862c5c08129e2fb224dcb61bad36850d9f123b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:00 +0000

    Add package nester-lmb/1.0.0/nester_lmb-1.0.0.zip

---
+++ packages/nester-lmb/nester_lmb-1.0.0.zip/nester_lmb-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='nester_lmb',
+        version='1.0.0',
+        py_modules=['nester'],
+        author='cardigan',
+        author_email='polohk@outlook.com',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of nested lists',
+    )
