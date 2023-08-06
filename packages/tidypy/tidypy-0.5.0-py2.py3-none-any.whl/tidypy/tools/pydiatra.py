9c12a63989dda30f24e163a892da79ae86518002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:41 +0000

    Add package tidypy/0.5.0/tidypy-0.5.0-py2.py3-none-any.whl

---
+++ packages/tidypy/tidypy-0.5.0-py2.py3-none-any.whl/tidypy/tools/pydiatra.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+
+import warnings
+
+from .base import PythonTool, Issue, AccessIssue
+
+
+class PyDiatraIssue(Issue):
+    tool = 'pydiatra'
+
+
+class PyDiatraTool(PythonTool):
+    """
+    pydiatra is yet another static checker for Python code.
+    """
+
+    @classmethod
+    def can_be_used(cls):
+        try:
+            import pydiatra  # noqa: unused-variable
+            return True
+        except ImportError:
+            return False
+
+    @classmethod
+    def get_all_codes(cls):
+        # Not currently a way to introspect all the codes pydiatra can return
+        return []
+
+    def execute(self, finder):
+        from pydiatra.checks import check_source
+        issues = []
+
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            for filepath in finder.files(self.config['filters']):
+                try:
+                    source = finder.read_file(filepath)
+                except EnvironmentError as exc:
+                    issues.append(
+                        AccessIssue(exc, filepath)
+                    )
+                else:
+                    for tag in check_source(str(filepath), source):
+                        issues.append(self.make_issue(filepath, tag))
+
+        return [
+            issue
+            for issue in issues
+            if issue.code not in self.config['disabled']
+        ]
+
+    def make_issue(self, filepath, tag):
+        return PyDiatraIssue(
+            tag.args[0],
+            ' '.join(tag.args[1:]) or tag.args[0],
+            filepath,
+            tag.lineno,
+        )
+
