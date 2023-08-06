61a331908fd3e41938dc8efa1d2275047c5e535d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:00 +0000

    Add package SteemAX/0.1.dev1/SteemAX-0.1.dev1.tar.gz

---
+++ packages/SteemAX/SteemAX-0.1.dev1.tar.gz/SteemAX-0.1.dev1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SteemAX',
+    version='0.1.dev1',
+    packages=['steemax'],
+    license='MIT',
+    long_description=open('README.txt').read(),
+    keywords='steemit steem upvote exchange',
+    url='http://github.com/artolabs/steemax',
+    author='ArtoLabs',
+    author_email='artopium@gmail.com',
+    install_requires=[
+        'python-dateutil',
+        'steem',
+        'pymysql',
+    ],
+    py_modules=['steemax'],
+    entry_points = {
+        'console_scripts': [
+            'steemax=steemax.steemax:run',
+        ],
+    },
+    python_requires='>=3.0',
+    include_package_data=True,
+    zip_safe=False
+)
+
+# sudo apt install python3 python3-pip libssl-dev python3-dev
+# pip install --upgrade setuptools pip --user pip
