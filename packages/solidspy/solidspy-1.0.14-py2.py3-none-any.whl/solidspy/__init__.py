695472cdbfc40054c5c4b8cd2080c1f0301367dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:36 +0000

    Add package solidspy/1.0.14/solidspy-1.0.14-py2.py3-none-any.whl

---
+++ packages/solidspy/solidspy-1.0.14-py2.py3-none-any.whl/solidspy/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from solidspy.solids_GUI import solids_GUI
+
+__all__ = ["assemutil",
+           "femutil",
+           "gaussutil",
+           "postprocesor",
+           "preprocesor",
+           "solutil",
+           "uelutil",
+           "solids_GUI"]
+
+
+__citation__ = """@software{solidspy,
+ title = {SolidsPy: 2D-Finite Element Analysis with Python},
+ author = {Gómez, Juan and Guarín-Zapata, Nicolás},
+ year = 2018,
+ keywords = {Python, Computer algebra system, Symbolics},
+ abstract = {SolidsPy is a simple finite element analysis code for
+   2D elasticity problems. The code uses as input simple-to-create text
+   files defining a model in terms of nodal, element, material and
+   load data.},
+ url = {https://github.com/AppliedMechanics-EAFIT/SolidsPy}
+}"""
