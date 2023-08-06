f8fbd026b29311f87e20b2914c9aac6ee45908e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:17 +0000

    Add package morphsnakes/0.0.11/morphsnakes-0.0.11.tar.gz

---
+++ packages/morphsnakes/morphsnakes-0.0.11.tar.gz/morphsnakes-0.0.11/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+# from distutils.core import setup
+
+setup(name='morphsnakes',
+      version='0.0.11',
+      requires=['scipy'],
+      py_modules=['morphsnakes'])
