8a75ae1004334439c7de56172499927f70c0cde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:31 +0000

    Add package id-phonenumbers/0.3.2/id_phonenumbers-0.3.2.tar.gz

---
+++ packages/id-phonenumbers/id_phonenumbers-0.3.2.tar.gz/id_phonenumbers-0.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='id_phonenumbers',
+    packages=['id_phonenumbers'],
+    version='0.3.2',
+    description='Indonesian Phonenumbers Parser',
+    license='MIT',
+    author='Steven',
+    author_email='stvn.bu@gmail.com',
+    url='https://github.com/SeiryuZ/id_phonenumbers',
+    download_url='https://github.com/SeiryuZ/id_phonenumbers/tarball/0.2.0',
+    keywords=['phonenumbers', 'Indonesia'],
+    install_requires=['phonenumberslite'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
