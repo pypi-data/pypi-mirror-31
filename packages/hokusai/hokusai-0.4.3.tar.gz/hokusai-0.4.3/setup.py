a9df5826576aa1d0d8abb8351561750f19bc5a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:20 +0000

    Add package hokusai/0.4.3/hokusai-0.4.3.tar.gz

---
+++ packages/hokusai/hokusai-0.4.3.tar.gz/hokusai-0.4.3/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(name='hokusai',
+      version=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'hokusai', 'VERSION.txt'), 'r').read(),
+      description='Artsy Docker development toolkit',
+      url='http://github.com/artsy/hokusai',
+      author='Isac Petruzzi',
+      author_email='isac@artsymail.com',
+      license='MIT',
+      packages=['hokusai'],
+      package_data={'hokusai': ['VERSION.txt', 'cli/*', 'commands/*', 'lib/*', 'services/*', 'templates/*']},
+      install_requires=[
+          'click==6.7',
+          'MarkupSafe==1.0',
+          'Jinja2==2.9.6',
+          'PyYAML==3.12',
+          'termcolor==1.1.0',
+          'boto3==1.4.4',
+          'botocore==1.5.41'
+      ],
+      zip_safe=False,
+      include_package_data = True,
+      scripts = ['bin/hokusai'])
