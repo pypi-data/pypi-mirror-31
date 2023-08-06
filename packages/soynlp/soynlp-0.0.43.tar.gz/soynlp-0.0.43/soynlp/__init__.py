65de9afaf07f05763af2dc1c690e9ccc803bebc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:50 +0000

    Add package soynlp/0.0.43/soynlp-0.0.43.tar.gz

---
+++ packages/soynlp/soynlp-0.0.43.tar.gz/soynlp-0.0.43/soynlp/__init__.py
@@ -0,0 +1,21 @@
+__title__ = 'soynlp'
+__version__ = '0.0.43'
+__author__ = 'Lovit'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2017 Lovit'
+
+from . import hangle
+from . import normalizer
+from . import noun
+from . import pos
+from . import tokenizer
+from . import vectorizer
+from . import word
+from .utils import get_available_memory
+from .utils import get_process_memory
+from .utils import sort_by_alphabet
+from .utils import DoublespaceLineCorpus
+
+__all__ = ['hangle', 'normalizer', 'noun', 'pos', 'tokenizer', 
+           'vectorizer', 'word', 'get_available_memory',
+           'get_process_memory', 'sort_by_alphabet', 'DoublespaceLineCorpus']
