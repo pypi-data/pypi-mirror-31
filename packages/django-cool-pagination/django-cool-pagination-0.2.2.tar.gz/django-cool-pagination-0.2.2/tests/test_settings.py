54113c3cfd608e907d6fdea8bfc3baee06e15b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:44 +0000

    Add package django-cool-pagination/0.2.2/django-cool-pagination-0.2.2.tar.gz

---
+++ packages/django-cool-pagination/django-cool-pagination-0.2.2.tar.gz/django-cool-pagination-0.2.2/tests/test_settings.py
@@ -0,0 +1,23 @@
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+SECRET_KEY = 'fake-key'
+
+INSTALLED_APPS = [
+    'tests',
+    'django_cool_paginator'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    }
+]
+
+
+COOL_PAGINATOR_SIZE = 'SMALL'
+COOL_PAGINATOR_NEXT_NAME = 'Go'
+COOL_PAGINATOR_PREVIOUS_NAME = 'Previous'
