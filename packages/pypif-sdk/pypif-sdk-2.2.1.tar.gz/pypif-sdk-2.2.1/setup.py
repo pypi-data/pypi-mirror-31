d0449e6e6498f72b5bcc347455d5d36e5d0d525d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:18 +0000

    Add package pypif-sdk/2.2.1/pypif-sdk-2.2.1.tar.gz

---
+++ packages/pypif-sdk/pypif-sdk-2.2.1.tar.gz/pypif-sdk-2.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pypif-sdk',
+      version='2.2.1',
+      url='http://github.com/CitrineInformatics/pypif-sdk',
+      description='High level support for working with Physical Information Files (PIF)',
+      author='Max Hutchinson',
+      author_email='maxhutch@citrine.io',
+      packages=find_packages(),
+      install_requires=[
+          'pypif>=2.0.0,<4',
+          'citrination_client>=3,<5',
+          'toolz'
+      ])
