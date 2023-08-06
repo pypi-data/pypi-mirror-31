982451126d486c4aad2fc06feac02e281bdb1950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:35 +0000

    Add package CatKit/0.3.0/CatKit-0.3.0.tar.gz

---
+++ packages/CatKit/CatKit-0.3.0.tar.gz/CatKit-0.3.0/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('readme.org') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="CatKit",
+    version="0.3.0",
+    url="https://github.com/SUNCAT-Center/CatKit",
+
+    author="Jacob Boes",
+    author_email="jrboes@stanford.edu",
+
+    description="General purpose tools for high-throughput catalysis.",
+    long_description=readme,
+
+    license='GPL-3.0',
+
+    packages=[
+        'catkit',
+        'catgen',
+        'catgen.api',
+        'catflow'
+    ],
+    package_dir={'catkit': 'catkit'},
+    package_data={'catkit': ['data/*.db', 'data/*.json']},
+
+    install_requires=required,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
