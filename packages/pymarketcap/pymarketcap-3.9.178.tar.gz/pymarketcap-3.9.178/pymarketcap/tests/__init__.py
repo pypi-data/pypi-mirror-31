3ba372b04ff6f604e08f8b2c62bde1a3369c1453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:53 +0000

    Add package pymarketcap/3.9.178/pymarketcap-3.9.178.tar.gz

---
+++ packages/pymarketcap/pymarketcap-3.9.178.tar.gz/pymarketcap-3.9.178/pymarketcap/tests/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""Shared interfaces tests modules."""
+
+def type_test(map_types, key, value):
+    """Test every type of the dictionary pairs
+    key-values.
+
+    Args:
+        map_types (dict): Name of the keys and type
+            or types that will be tested against it
+            using ``isinstance(value, map_types[key])``
+        key (str): Key that will be tested.
+        value (str): Value name that will be tested.
+
+    Raises:
+       ``AssertionError`` if types and keys doesn't match.
+    """
+    try:
+        assert isinstance(value, map_types[key])
+    except AssertionError as err:
+        print("Key: %s\nValue: %r\n" % (key, value))
+        raise err
+
+def restart_if_http_error(res):
+    def real_decorator(func):
+        def wrapper(*args, **kwargs):
+            ATTEMPTS = 10
+            THROTTLE = 3  # Seconds between attempts
+            error = True
+            while error:
+                try:
+                    if ATTEMPTS > 0:
+                        response = func()
+                    else:
+                        raise RuntimeError(
+                            "Maximum number of attempts reached"
+                        )
+                except IndexError:
+                    sleep(THROTTLE)
+                else:
+                    error = False
+                    ATTEMPTS -= 1
+            return response
+        return wrapper
+    return real_decorator
+
+def disabled_decorator(func):
+    """This decorator disables the provided function, and does nothing."""
+    def empty_func(*args,**kargs):
+        pass
+    return empty_func
