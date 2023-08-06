6766b945f274edb8b4aa8abb887fc3d9b0a9a61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:38 +0000

    Add package xuyingying/1.0.0/xuyingying-1.0.0.tar.gz

---
+++ packages/xuyingying/xuyingying-1.0.0.tar.gz/xuyingying-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name        = 'xuyingying',
+      version     = '1.0.0',
+      py_modules  =['xuyingying'],
+      author      ='veryshine',
+      author_email='263059108@qq.com',
+      url         ='http://url',
+      description ='A simple printer of list',
+     )
---
+++ packages/xuyingying/xuyingying-1.0.0.tar.gz/xuyingying-1.0.0/xuyingying.py
@@ -0,0 +1,8 @@
+movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman", ["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry Jone"]]]
+def print_lol (the_list) :
+	for each_item in the_list:
+		if isinstance (each_item,list):
+		   print_lol (each_item)
+		else:
+		   print(each_item)
+print_lol(movies)
