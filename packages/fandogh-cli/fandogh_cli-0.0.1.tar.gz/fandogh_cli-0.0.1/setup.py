05d1d25ce1c588cf93c06305091d9a0ce2c89db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:40 +0000

    Add package fandogh-cli/0.0.1/fandogh_cli-0.0.1.tar.gz

---
+++ packages/fandogh-cli/fandogh_cli-0.0.1.tar.gz/fandogh_cli-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name='fandogh_cli',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    version='0.0.1',
+    description='Official CLI for Fandogh PaaS',
+    author='Soroosh Sarabadani',
+    install_requires=["click",
+                      "requests",
+                      "beautifultable",
+                      "pyyaml"],
+    author_email='soroosh.sarabadani@gmail.com',
+    url='https://github.com/psycho-ir/fandogh-cli',
+    keywords=['PaaS', 'Fandogh'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
