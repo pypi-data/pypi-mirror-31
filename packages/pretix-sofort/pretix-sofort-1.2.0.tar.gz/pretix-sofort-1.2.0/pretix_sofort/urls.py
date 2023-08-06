85c5323aca36f2420d8dbb087d517da807b0d923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:16 +0000

    Add package pretix-sofort/1.2.0/pretix-sofort-1.2.0.tar.gz

---
+++ packages/pretix-sofort/pretix-sofort-1.2.0.tar.gz/pretix-sofort-1.2.0/pretix_sofort/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from pretix.multidomain import event_url
+
+from .views import ReturnView, redirect_view, refund, webhook
+
+urlpatterns = [
+    url(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/sofort/refund/(?P<id>\d+)/',
+        refund, name='refund'),
+]
+
+event_patterns = [
+    url(r'^sofort/', include([
+        url(r'^redirect/$', redirect_view, name='redirect'),
+        url(r'^return/(?P<order>[^/]+)/(?P<hash>[^/]+)/$', ReturnView.as_view(), name='return'),
+        event_url(r'^webhook/$', webhook, name='webhook', require_live=False),
+    ])),
+]
