dbf313a4e1c4d6ab5232097df1ad8f7f26fc18da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:36 +0000

    Add package multidict/4.3.1/multidict-4.3.1.tar.gz

---
+++ packages/multidict/multidict-4.3.1.tar.gz/multidict-4.3.1/tests/test_mypy.py
@@ -0,0 +1,12 @@
+import multidict
+
+
+def test_classes_not_abstract() -> None:
+    d1 = multidict.MultiDict({'a': 'b'})  # type: multidict.MultiDict[str]
+    d2 = multidict.CIMultiDict({'a': 'b'})  # type: multidict.CIMultiDict[str]
+
+    d3 = multidict.MultiDictProxy(d1)
+    d4 = multidict.CIMultiDictProxy(d2)
+
+    d3.getone('a')
+    d4.getall('a')
