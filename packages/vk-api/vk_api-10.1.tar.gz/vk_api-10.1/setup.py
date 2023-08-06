5b3154c199643cf295eef303ffa8271e5762dc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:33 +0000

    Add package vk-api/10.1/vk_api-10.1.tar.gz

---
+++ packages/vk-api/vk_api-10.1.tar.gz/vk_api-10.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+"""
+@author: python273
+@contact: https://vk.com/python273
+@license Apache License, Version 2.0, see LICENSE file
+
+Copyright (C) 2018
+"""
+
+
+setup(
+    name='vk_api',
+    version='10.1',
+    author='python273',
+    author_email='whoami@python273.pw',
+    url='https://github.com/python273/vk_api',
+    description='Module for writing scripts for vk.com (vkontakte)',
+    download_url='https://github.com/python273/vk_api/archive/master.zip',
+    license='Apache License, Version 2.0, see LICENSE file',
+
+    packages=['vk_api', 'jconfig'],
+    install_requires=['requests', 'enum34', 'beautifulsoup4'],
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
