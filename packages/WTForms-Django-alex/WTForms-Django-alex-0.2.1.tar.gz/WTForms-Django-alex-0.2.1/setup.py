80332c53f71f35367a16c74cd45d42aad04a3b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:06 +0000

    Add package WTForms-Django-alex/0.2.1/WTForms-Django-alex-0.2.1.tar.gz

---
+++ packages/WTForms-Django-alex/WTForms-Django-alex-0.2.1.tar.gz/WTForms-Django-alex-0.2.1/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='WTForms-Django-alex',
+    version='0.2.1',
+    url='http://github.com/wtforms/wtforms-appengine/',
+    license='BSD',
+    author='Thomas Johansson, James Crasta',
+    author_email='wtforms@simplecodes.com',
+    description='Django tools for WTForms',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=[
+        'wtforms_django',
+        'wtforms_django.templatetags',
+    ],
+    package_data={
+    },
+    test_suite='tests.runtests',
+    install_requires=['WTForms>=1.0.5', 'Django>=1.3'],
+)
