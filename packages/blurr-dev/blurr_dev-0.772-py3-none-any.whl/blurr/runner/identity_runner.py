f51990dc48439f28715237666a1f5f1e8627c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:19 +0000

    Add package blurr-dev/0.772/blurr_dev-0.772-py3-none-any.whl

---
+++ packages/blurr-dev/blurr_dev-0.772-py3-none-any.whl/blurr/runner/identity_runner.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from typing import List, Dict, Tuple, Any, Optional
+
+from blurr.core import logging
+from blurr.core.aggregate_block import BlockAggregate
+from blurr.core.errors import PrepareWindowMissingBlocksError
+from blurr.core.evaluation import Context
+from blurr.core.record import Record
+from blurr.core.schema_loader import SchemaLoader
+from blurr.core.store_key import Key
+from blurr.core.transformer_streaming import StreamingTransformer, \
+    StreamingTransformerSchema
+from blurr.core.transformer_window import WindowTransformer
+from blurr.core.type import Type
+
+
+def execute_dtc(identity_events: List[Tuple[datetime, Record]], identity: str,
+                stream_dtc_spec: Dict, window_dtc_spec: Dict) -> Tuple[Dict[Key, Any], List[Dict]]:
+    schema_loader = SchemaLoader()
+    identity_events.sort(key=lambda x: x[0])
+
+    block_data = execute_stream_dtc(identity_events, identity, schema_loader, stream_dtc_spec)
+    window_data = execute_window_dtc(identity, schema_loader, window_dtc_spec)
+
+    return block_data, window_data
+
+
+def execute_stream_dtc(identity_events: List[Tuple[datetime, Record]], identity: str,
+                       schema_loader: SchemaLoader,
+                       stream_dtc_spec: Optional[Dict]) -> Dict[Key, Any]:
+    if stream_dtc_spec is None:
+        return {}
+
+    stream_dtc_name = schema_loader.add_schema(stream_dtc_spec)
+    stream_transformer_schema = schema_loader.get_schema_object(stream_dtc_name)
+
+    stream_transformer = StreamingTransformer(stream_transformer_schema, identity)
+    for time, event in identity_events:
+        stream_transformer.evaluate(event)
+    stream_transformer.finalize()
+
+    return get_store(schema_loader).get_all(identity)
+
+
+def execute_window_dtc(identity: str, schema_loader: SchemaLoader,
+                       window_dtc_spec: Optional[Dict]) -> List[Dict]:
+    if window_dtc_spec is None:
+        logging.debug('Window DTC not provided')
+        return []
+
+    stream_transformer = StreamingTransformer(
+        get_streaming_transformer_schema(schema_loader), identity)
+    all_data = get_store(schema_loader).get_all()
+    stream_transformer.restore(all_data)
+
+    exec_context = Context()
+    exec_context.add(stream_transformer._schema.name, stream_transformer)
+
+    block_obj = None
+    for aggregate in stream_transformer._nested_items.values():
+        if not isinstance(aggregate, BlockAggregate):
+            continue
+        if block_obj is not None:
+            raise Exception(('Window operation is supported against Streaming ',
+                             'DTC with only one BlockAggregate'))
+        block_obj = aggregate
+
+    if block_obj is None:
+        raise Exception('No BlockAggregate found in the Streaming DTC file')
+
+    window_data = []
+
+    window_dtc_name = schema_loader.add_schema(window_dtc_spec)
+    window_transformer_schema = schema_loader.get_schema_object(window_dtc_name)
+    window_transformer = WindowTransformer(window_transformer_schema, identity, exec_context)
+
+    logging.debug('Running Window DTC for identity {}'.format(identity))
+
+    anchors = 0
+    blocks = 0
+    for key, data in all_data.items():
+        if key.group != block_obj._schema.name:
+            continue
+        try:
+            blocks += 1
+            if window_transformer.evaluate(block_obj.restore(data)):
+                anchors += 1
+                window_data.append(window_transformer.flattened_snapshot)
+        except PrepareWindowMissingBlocksError as err:
+            logging.debug('{} with {}'.format(err, key))
+
+    if anchors == 0:
+        logging.debug('No anchors found for identity {} out of {} blocks'.format(identity, blocks))
+
+    return window_data
+
+
+# TODO Clean up dirty implementation
+def get_store(schema_loader: SchemaLoader):
+    memory_store_schemas = schema_loader.get_schemas_of_type(Type.BLURR_STORE_MEMORY)
+    dynamo_store_schemas = schema_loader.get_schemas_of_type(Type.BLURR_STORE_DYNAMO)
+    return schema_loader.get_schema_object(memory_store_schemas[0][
+        0] if len(memory_store_schemas) > 0 else dynamo_store_schemas[0][0])
+
+
+def get_streaming_transformer_schema(schema_loader: SchemaLoader) -> StreamingTransformerSchema:
+    streaming_transformer_schema = schema_loader.get_schemas_of_type(Type.BLURR_TRANSFORM_STREAMING)
+    return schema_loader.get_schema_object(streaming_transformer_schema[0][0])
