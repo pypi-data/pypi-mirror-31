7e9a7daecdf1e6d1a43640449e416fc98cf76d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:37 +0000

    Add package flaskrouting/1.0.2/flaskrouting-1.0.2.tar.gz

---
+++ packages/flaskrouting/flaskrouting-1.0.2.tar.gz/flaskrouting-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md") as f:
+  readme = f.read()
+
+setup(name="flaskrouting",
+  description="Create simple, nestable routes in flask.",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  version="1.0.2",
+  author="kangaroux",
+  author_email="roux.jesse@gmail.com",
+  packages=["flaskrouting"],
+  url="https://github.com/Kangaroux/flaskrouting",
+  license="MIT",
+)
