15025d85b3c0c8250ce93cd5af83763dd4b8c5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:19 +0000

    Add package scikit-discovery/0.9.13/scikit-discovery-0.9.13.tar.gz

---
+++ packages/scikit-discovery/scikit-discovery-0.9.13.tar.gz/scikit-discovery-0.9.13/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'scikit-discovery'
+
+package_list = find_packages()
+
+setup(name     = package_name,
+      version  = '0.9.13',
+      packages = package_list,
+      zip_safe = False,
+
+      install_requires = [
+          'astropy>=1.1.2',
+          'basemap',
+          'boto3>=1.4.4',
+          'dispy==4.8.1',
+          'graphviz >= 0.7.0',
+          'ipython >= 6.0.0',
+          'ipywidgets >= 6.0.0',
+          'matplotlib >= 2',
+          'numpy>=1.10',
+          'pandas>=0.17',
+          'paramiko >= 2.1.0',
+          'psutil >= 5.2',
+          'psutil>=5',
+          'pyhht',
+          'PyWavelets',
+          'scikit-dataaccess>=1.9.0',
+          'scikit-learn >= 0.17',
+          'scipy',
+          'seaborn >= 0.8',
+          'setuptools',
+          'six >= 1.10.0',
+          'statsmodels >= 0.8',
+          'tqdm',
+      ],
+
+      description = 'A package for Computer-Aided Discovery',
+      author = 'MITHAGI',
+      author_email='skdaccess@mit.edu',
+      classifiers=[
+          'Topic :: Scientific/Engineering',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Programming Language :: Python :: 3 :: Only'
+          ],
+      python_requires='>=3.4',
+      url = 'https://github.com/MITHaystack/scikit-discovery',
+      package_data={'skdiscovery': ['license/LICENSE','license/LGPL_LICENSE','license/MIT_LICENSE',
+                                    'docs/skdiscovery_doxygen.pdf','examples/Amazon_GUI.ipynb',
+                                    'examples/Amazon_Offload.ipynb']},
+)
