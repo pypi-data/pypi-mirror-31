cf2c7bdac61aa4ed070853ba7b025ea5213bd638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package nester_aleatorio/1.0.0/nester_aleatorio-1.0.0.tar.gz

---
+++ packages/nester_aleatorio/nester_aleatorio-1.0.0.tar.gz/nester_aleatorio-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_aleatorio',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'eumesmo',
+    description = 'nester Nº20191928374'
+    )
