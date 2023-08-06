fa18c8e578cffa236fef5c6bfe6bd28628e2000a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:33 +0000

    Add package dingtalk-sdk/1.0.8/dingtalk-sdk-1.0.8.tar.gz

---
+++ packages/dingtalk-sdk/dingtalk-sdk-1.0.8.tar.gz/dingtalk-sdk-1.0.8/dingtalk/client/api/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from dingtalk.client.api.department import Department  # NOQA
+from dingtalk.client.api.user import User  # NOQA
