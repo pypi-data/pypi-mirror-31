4e9faf5cc29fcdd3b9fa754aa802fd1f1ceb5f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package my-scp/2018.5.16/my_scp-2018.5.16.tar.gz

---
+++ packages/my-scp/my_scp-2018.5.16.tar.gz/my_scp-2018.5.16/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='my_scp',
+    version      ='2018.5.16',
+    py_modules   =['my_scp'],
+    author       ='xiuyuanjun',
+    author_email ='729125866@qq.com',
+    description  ='Remote copy and download files or directories via SCP.',
+    )
