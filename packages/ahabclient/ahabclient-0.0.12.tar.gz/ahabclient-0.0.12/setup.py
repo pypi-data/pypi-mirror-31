0ac90490df808d74faa3981ea5ae370313fd6721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:47 +0000

    Add package ahabclient/0.0.12/ahabclient-0.0.12.tar.gz

---
+++ packages/ahabclient/ahabclient-0.0.12.tar.gz/ahabclient-0.0.12/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup
+ 
+setup(
+    name='ahabclient',
+    version='0.0.12',
+    license='MIT',
+    description='AHAB Docker Client Wrapper',
+    author="Marco Montanari",
+    author_email="marco.montanari@gmail.com",
+    py_modules=['ahab'],
+    entry_points={
+        'console_scripts':[
+            'ahab=ahab:main',
+        ]
+    },
+    install_requires=[
+        'requests',
+        'docker',
+        'semantic_version',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Terminals',
+        'Topic :: Utilities'
+    ],
+
+)
