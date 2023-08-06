9f8f19929216bc5a89b99d6944de3b57532feb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:59 +0000

    Add package django-tinymce-4/0.0.22/django-tinymce-4-0.0.22.tar.gz

---
+++ packages/django-tinymce-4/django-tinymce-4-0.0.22.tar.gz/django-tinymce-4-0.0.22/tinymce_4/static/tinymce_4/settings/full/settings.js
@@ -0,0 +1,31 @@
+var tinySettings = {
+  selector: 'textarea.tinymce',
+  theme: 'modern',
+  relative_urls: false,
+  convert_urls: false,
+  valid_elements: '*[*]',
+  valid_children: '+a[h1|h2|h3|h4|h5|h6|p|span|div|img]',
+  forced_root_block: false,
+  force_br_newlines: false,
+  force_p_newlines: true,
+  height: '350px',
+
+  language_url: '',
+
+  content_css: [
+    '/static/tinymce_4/css/__tinymce_4.css?' + new Date().getTime()
+  ],
+
+  plugin_preview_width: '730',
+  plugin_preview_height: '550',
+
+  image_class_list: [],
+  table_class_list: [],
+
+  toolbar1: 'undo redo | styleselect removeformat | image insertzoomimage | link media template anchor | code | preview | image_alignment',
+  toolbar2: 'fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor',
+
+  fontsize_formats: '8px 10px 12px 14px 16px 18px 20px 22px 24px 26px 28px 36px 48px 72px',
+
+  file_browser_callback: DjangoFilebrowser
+}
