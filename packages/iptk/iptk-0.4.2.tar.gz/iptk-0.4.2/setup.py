66addd418911c1dbc20f6c5d60411e428eab0955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package iptk/0.4.2/iptk-0.4.2.tar.gz

---
+++ packages/iptk/iptk-0.4.2.tar.gz/iptk-0.4.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+  name = 'iptk',
+  packages = ['iptk', 'iptk.metadata', 'iptk.utils'],
+  version = '0.4.2',
+  description = 'Python interface to the Imaging Pipeline Toolkit',
+  author = 'Jan-Gerd Tenberge',
+  author_email = 'jan-gerd.tenberge@uni-muenster.de',
+  install_requires=[
+    'zipstream',
+    'requests'
+  ],
+  url = 'https://github.com/iptk/iptk-py',
+  download_url = 'https://github.com/iptk/iptk-py/archive/0.4.2.tar.gz', 
+  keywords = ['neuroimaging', 'docker'],
+  classifiers = [],
+)
