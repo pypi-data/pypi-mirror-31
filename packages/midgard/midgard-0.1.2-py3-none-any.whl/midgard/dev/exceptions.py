440b049a11b892222a9c6166a2300650e40770a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:42 +0000

    Add package midgard/0.1.2/midgard-0.1.2-py3-none-any.whl

---
+++ packages/midgard/midgard-0.1.2-py3-none-any.whl/midgard/dev/exceptions.py
@@ -0,0 +1,44 @@
+"""Definition of Midgard-specific exceptions
+
+Description:
+------------
+
+Custom exceptions used by Midgard for more specific error messages and handling.
+
+"""
+
+
+class MidgardException(Exception):
+    pass
+
+
+class MidgardExit(SystemExit, MidgardException):
+    pass
+
+
+class InitializationError(MidgardException):
+    pass
+
+
+class FieldExistsError(MidgardException):
+    pass
+
+
+class FieldDoesNotExistError(MidgardException):
+    pass
+
+
+class MissingDataError(MidgardException):
+    pass
+
+
+class UnknownEnumError(MidgardException):
+    pass
+
+
+class UnknownPluginError(MidgardException):
+    pass
+
+
+class UnitError(MidgardException):
+    pass
