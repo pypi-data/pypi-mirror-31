6713a571470eb0142eaead58c8c677ad985c48b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:19 +0000

    Add package synapseclient/1.8.0/synapseclient-1.8.0.tar.gz

---
+++ packages/synapseclient/synapseclient-1.8.0.tar.gz/synapseclient-1.8.0/synapseutils/__init__.py
@@ -0,0 +1,27 @@
+"""
+********
+Overview
+********
+
+The ``synapseutils`` package provides both higher level functions as well as utilites for 
+interacting with `Synapse <http://www.synapse.org>`_.  These funtionalities include:
+
+- :py:func:`copy.copy`
+- :py:func:`copy.copyWiki`
+- :py:func:`walk.walk`
+- :py:func:`sync.syncFromSynapse`
+- :py:func:`sync.syncToSynapse`
+- :py:func:`monitor.notifyMe`
+"""
+
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .copy import copy, copyWiki, copyFileHandles, changeFileMetaData
+from .walk import walk
+from .sync import syncFromSynapse, syncToSynapse
+from .monitor import notifyMe, with_progress_bar
