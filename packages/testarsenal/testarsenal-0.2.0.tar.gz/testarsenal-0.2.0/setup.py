4809fb86fcf6687c7cf5fe391f82734abb25bdd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:20 +0000

    Add package testarsenal/0.2.0/testarsenal-0.2.0.tar.gz

---
+++ packages/testarsenal/testarsenal-0.2.0.tar.gz/testarsenal-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name="testarsenal",
+      version="0.2.0",
+      description="Testing tools",
+      long_description="Tools for making using selenium and django testing easier",
+      url="https://github.com/samirelanduk/testarsenal",
+      author="Sam Ireland",
+      author_email="mail@samireland.com",
+      license="MIT",
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Other Audience",
+                   "License :: OSI Approved :: MIT License",
+                   "Topic :: Communications",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"],
+      py_modules=["testarsenal"],
+      install_requires=[])
