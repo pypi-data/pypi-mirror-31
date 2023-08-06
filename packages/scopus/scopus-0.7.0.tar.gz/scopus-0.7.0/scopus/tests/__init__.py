9eddf4df250213bec176475ec1155ca9f7ba763a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package scopus/0.7.0/scopus-0.7.0.tar.gz

---
+++ packages/scopus/scopus-0.7.0.tar.gz/scopus-0.7.0/scopus/tests/__init__.py
@@ -0,0 +1,5 @@
+from scopus.tests.test_CitationOverview import *
+from scopus.tests.test_ScopusAbstract import *
+from scopus.tests.test_ScopusAffiliation import *
+from scopus.tests.test_ScopusAuthor import *
+from scopus.tests.test_ScopusSearch import *
