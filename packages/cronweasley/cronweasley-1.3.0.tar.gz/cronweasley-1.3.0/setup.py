22312d2a28ae6b222bae9ec8c8c452b34b12e49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package cronweasley/1.3.0/cronweasley-1.3.0.tar.gz

---
+++ packages/cronweasley/cronweasley-1.3.0.tar.gz/cronweasley-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronweasley',
+    version='1.3.0',
+    description='Cronjobs for Wizards',
+    long_description_content_type='text/markdown',
+    url='http://github.com/alairock/cron-weasley',
+    author='alairock',
+    author_email='sblnog@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='cron crontab cronjob',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[],
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    }
+)
