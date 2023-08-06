151f4fee6e38ead11520aa4ad14bc41a3c0aadf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:58 +0000

    Add package django-domain-specific-language/1.2.2/django_domain_specific_language-1.2.2-py3-none-any.whl

---
+++ packages/django-domain-specific-language/django_domain_specific_language-1.2.2-py3-none-any.whl/django_dsl/run.py
@@ -0,0 +1,11 @@
+from lexer import build_lexer
+from parser import build_parser
+
+
+def compile(expr):
+    # create separate lexer and parser for each compilation
+    # to be thread-safe
+    lexer = build_lexer()
+    parser = build_parser()
+    # now, parse!
+    return parser.parse(expr, lexer=lexer)
