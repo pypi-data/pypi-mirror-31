3a14e29fe27797048a168bec77750245694f0a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:58 +0000

    Add package core-dashboard-app/1.0.0b1/core_dashboard_app-1.0.0b1.tar.gz

---
+++ packages/core-dashboard-app/core_dashboard_app-1.0.0b1.tar.gz/core_dashboard_app-1.0.0b1/core_dashboard_app/settings.py
@@ -0,0 +1,30 @@
+"""
+    Django settings for core_dashboard_app app
+"""
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+SERVER_URI = getattr(settings, 'SERVER_URI', "http://localhost")
+
+INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', [])
+
+menu = {
+    'Templates': ('core_dashboard_templates', 1000),
+    'Records': ('core_dashboard_records', 2000),
+    'Files': ('core_dashboard_files', 3000),
+    'Workspaces': ('core_dashboard_workspaces', 4000),
+}
+
+if 'core_composer_app' in INSTALLED_APPS:
+    menu['Types'] = ('core_dashboard_types', 1500)
+if 'core_curate_app' in INSTALLED_APPS:
+    menu['Forms'] = ('core_dashboard_forms', 2500)
+
+# Menu
+DASHBOARD_MENU = getattr(settings, 'DASHBOARD_MENU', menu)
+
+# Number of records to display per page for pagination
+RECORD_PER_PAGE_PAGINATION = getattr(settings, 'RESULTS_PER_PAGE', 10)
