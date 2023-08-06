429610f64a7b58de425fab147b69de5c22d66ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:32 +0000

    Add package pyCompare/1.0.1/pyCompare-1.0.1.tar.gz

---
+++ packages/pyCompare/pyCompare-1.0.1.tar.gz/pyCompare-1.0.1/pyCompare/__init__.py
@@ -0,0 +1,9 @@
+"""
+`pyCompare` implements functions for generating Bland-Altman plots of agreement between measurements.
+"""
+
+from ._plotBlandAltman import blandAltman
+
+__version__ = '1.0.1'
+
+__all__ = ['blandAltman']
---
+++ packages/pyCompare/pyCompare-1.0.1.tar.gz/pyCompare-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(name='pyCompare',
+	version='1.0.1',
+	description='Bland-Altman plots for Python',
+	url='https://github.com/jaketmp/pyCompare',
+	author='Jake TM Pearce',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=[
+		'numpy>=1.14.2',
+		'scipy>=1.0.1',
+		'matplotlib>=2.2.2',
+	],
+	classifiers = [
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.6",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		"Topic :: Scientific/Engineering :: Visualization",
+		],
+	long_description = """\
+		A Python module for generating `Bland-Altman <https://en.wikipedia.org/wiki/Bland–Altman_plot>`_ plots to compare two sets of measurements.
+
+		.. image:: https://travis-ci.org/jaketmp/pyCompare.svg?branch=master
+		   :target: https://travis-ci.org/jaketmp/pyCompare
+		   :alt: Travis CI build status
+
+		.. image:: https://codecov.io/gh/jaketmp/pyCompare/branch/master/graph/badge.svg
+		   :target: https://codecov.io/gh/jaketmp/pyCompare
+		   :alt: Test coverage
+		""",
+		documentation='https://github.com/jaketmp/pyCompare',
+		include_package_data=True,
+		zip_safe=True
+	)
