ad59c09a9f8e7994f2312fa078037da674f50403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:49 +0000

    Add package mlang/0.1.15/mlang-0.1.15.tar.gz

---
+++ packages/mlang/mlang-0.1.15.tar.gz/mlang-0.1.15/setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='mlang',
+    version='0.1.15',
+    packages=['mlang.res', 'mlang_client'],
+    py_modules=['mlang.config', 'mlang.utils'],
+    url='',
+    license='MIT',
+    author='lishouguang',
+    author_email='lishouguang@meizu.com',
+    description='mlang client',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3',
+    install_requires=['progressbar2', 'requests-toolbelt', 'numpy', 'cachetools']
+)
