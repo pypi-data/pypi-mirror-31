6dfe13cb2a00bea73c670acc35ef0f5c2c07db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package pyconrad/0.1.1/pyconrad-0.1.1.tar.gz

---
+++ packages/pyconrad/pyconrad-0.1.1.tar.gz/pyconrad-0.1.1/pyconrad/_imageutils.py
@@ -0,0 +1,54 @@
+
+# Copyright (C) 2010-2017 - Andreas Maier
+# CONRAD is developed as an Open Source project under the GNU General Public License (GPL-3.0)
+
+import jpype
+import numpy as np
+import pyconrad
+
+class ImageUtil:
+
+    ########################
+    ## Convert Grid/numpy ##
+    ########################
+
+    @staticmethod
+    def grid_from_numpy(grid):
+        return np.ndarray.view(pyconrad.PyGrid.from_numpy(grid))
+
+    @staticmethod
+    def numpy_from_grid(ndarray):
+        return np.ndarray.view(pyconrad.PyGrid.from_grid(ndarray))
+
+    #################
+    ## Save Images ##
+    #################
+
+    @staticmethod
+    def save_grid_as_tiff(grid, path):
+        jpype.JPackage('edu').stanford.rsl.conrad.utils.ImageUtil.saveAs(grid, path)
+
+    @staticmethod
+    def save_numpy_as_tiff(array, path):
+        grid = ImageUtil.grid_from_numpy(array)
+        jpype.JPackage('edu').stanford.rsl.conrad.utils.ImageUtil.saveAs(grid, path)
+
+    #################
+    ## Load Images ##
+    #################
+
+    @staticmethod
+    def grid_from_tiff(path):
+        ij = jpype.JPackage('ij').IJ.openImage(path)
+        if not ij:
+            raise RuntimeError('Error opening file \'%s\'' % path)
+        grid = jpype.JPackage('edu').stanford.rsl.conrad.utils.ImageUtil.wrapImagePlus(ij)
+        return grid
+
+    @staticmethod
+    def array_from_tiff(path):
+        ij = jpype.JPackage('ij').IJ.openImage(path)
+        if not ij:
+            raise RuntimeError('Error opening file \'%s\'' % path)
+        grid = jpype.JPackage('edu').stanford.rsl.conrad.utils.ImageUtil.wrapImagePlus(ij)
+        return ImageUtil.numpy_from_grid(grid)
