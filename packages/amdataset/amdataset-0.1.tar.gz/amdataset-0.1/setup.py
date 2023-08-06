7cf58064514d2958e7f9607dde353f1eaa84627f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:09 +0000

    Add package amdataset/0.1/amdataset-0.1.tar.gz

---
+++ packages/amdataset/amdataset-0.1.tar.gz/amdataset-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+name = 'amdataset',
+version = '0.1',
+packages = find_packages(),
+author = 'Ammad Khalid',
+description = open('README.md', 'r').read(),
+install_requires = ['dataset'],
+keywords = 'database,dataset,ORM',
+url = 'https://github.com/Ammadkhalid/am-dataset.git',
+author_email = 'Ammadkhalid12@gmail.com'
+)
