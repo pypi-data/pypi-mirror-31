d77f7cd66344333c8c4769a7f9e95343811bd35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:31 +0000

    Add package c2cwsgiutils/1.11.1/c2cwsgiutils-1.11.1-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-1.11.1-py3-none-any.whl/c2cwsgiutils/wsgi_app.py
@@ -0,0 +1,14 @@
+"""
+Module used by c2cwsgiutils_run.sh to provide a WSGI application when starting gunicorn
+"""
+from typing import Callable
+from c2cwsgiutils import coverage_setup  # pragma: no cover
+coverage_setup.init()  # pragma: no cover
+
+
+def create() -> Callable:  # pragma: no cover
+    from c2cwsgiutils import wsgi, sentry, profiler
+    return sentry.filter_wsgi_app(profiler.filter_wsgi_app(wsgi.create_application()))
+
+
+application = create()  # pragma: no cover
