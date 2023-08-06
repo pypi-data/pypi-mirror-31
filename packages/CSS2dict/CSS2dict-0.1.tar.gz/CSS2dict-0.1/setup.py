89af7d040810fe4bfcfd9792d92f06a176a33d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:17 +0000

    Add package CSS2dict/0.1/CSS2dict-0.1.tar.gz

---
+++ packages/CSS2dict/CSS2dict-0.1.tar.gz/CSS2dict-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'CSS2dict',
+  packages = ['CSS2dict'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A module to convert CSS code into a Python dictionary',
+  author = 'FranchuFranchu',
+  author_email = 'udontcare@gmail.com',
+  url = 'https://github.com/FranchuFranchu/CSS2dict', 
+  download_url = 'https://github.com/FranchuFranchu/CSS2dict/archive/0.1.tar.gz', 
+  keywords = ['python', 'CSS', 'dictionary'], # arbitrary keywords
+  classifiers = [],
+)
