9d936ffc29804371ec1019f3b4f90329e8898f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:07 +0000

    Add package matplotboard/0.5.0/matplotboard-0.5.0.tar.gz

---
+++ packages/matplotboard/matplotboard-0.5.0.tar.gz/matplotboard-0.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('requirements.txt') as req:
+    install_requires = [l.strip() for l in req.readlines()]
+
+with open('README.md') as f:
+    desc = f.read()
+
+setup(
+    name='matplotboard',
+    version='0.5.0',
+    description='Generate simple HTML dashboards using matplotlib',
+    long_description=desc,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    dependency_links=[
+        "git+ssh://git@github.com/cfangmeier/latexipy.git#egg=latexipy"
+    ],
+    packages=['matplotboard'],
+    package_data={'matplotboard': ['templates/*.j2',
+                                   'static/css/*.css',
+                                   'static/js/*.js',
+                                   ]},
+)
