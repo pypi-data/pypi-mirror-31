e97d76ebaedb9cf8a6a97ee726233646529a1cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:26 +0000

    Add package pynwb/0.3.0/pynwb-0.3.0-py2.py3-none-any.whl

---
+++ packages/pynwb/pynwb-0.3.0-py2.py3-none-any.whl/pynwb/io/core.py
@@ -0,0 +1,29 @@
+from ..form.build import ObjectMapper, RegionBuilder
+from .. import register_map
+
+from pynwb.core import NWBData
+
+
+@register_map(NWBData)
+class NWBDataMap(ObjectMapper):
+
+    @ObjectMapper.constructor_arg('name')
+    def carg_name(self, builder, manager):
+        return builder.name
+
+    @ObjectMapper.constructor_arg('data')
+    def carg_data(self, builder, manager):
+        return builder.data
+
+
+class NWBTableRegionMap(NWBDataMap):
+
+    @ObjectMapper.constructor_arg('table')
+    def carg_table(self, builder, manager):
+        return manager.construct(builder.data.builder)
+
+    @ObjectMapper.constructor_arg('region')
+    def carg_region(self, builder, manager):
+        if not isinstance(builder.data, RegionBuilder):
+            raise ValueError("'builder' must be a RegionBuilder")
+        return builder.data.region
