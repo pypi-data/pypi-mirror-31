6de5131b1f5c086a9e433063e7b93b7f41e58cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package class_extension/1.0.2/class_extension-1.0.2.tar.gz

---
+++ packages/class_extension/class_extension-1.0.2.tar.gz/class_extension-1.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='class_extension',
+    version='1.0.2',
+    description='Allows classes to be extended',
+    long_description=open('README.md').read(),
+    author='Michael Amy',
+    author_email='mikeamycoder@gmail.com',
+    url='https://github.com/MikeAmy/class_extension',
+    packages=['class_extension']
+)
