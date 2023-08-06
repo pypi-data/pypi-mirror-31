96c6ae3cf95e1f7fc900268e0be9925c1c12882a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:29 +0000

    Add package ls.joyous/0.2.3/ls.joyous-0.2.3.tar.gz

---
+++ packages/ls.joyous/ls.joyous-0.2.3.tar.gz/ls.joyous-0.2.3/shell.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'ls.joyous.tests.settings'
+    os.environ['PYTHONSTARTUP'] = __file__
+    argv = sys.argv[:1] + ['shell'] + sys.argv[1:]
+    execute_from_command_line(argv)
+
+elif __name__ == "builtins":
+    # useful once we are in the shell
+    import datetime as dt
+    from django.utils import timezone
+    from django.conf import settings
+    import pytz
+    import pprint
+    import ls.joyous
+    from ls.joyous.models import *
+    from ls.joyous.recurrence import Recurrence
+    from ls.joyous.utils import manythings, telltime, ical
+    L=list
+    sys.displayhook = pprint.pprint
+    timezone.activate("Pacific/Auckland")
+
