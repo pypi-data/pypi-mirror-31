11e74640ab6061b815abc155fa76046ac99582b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:50 +0000

    Add package django-leek/0.3/django_leek-0.3-py3-none-any.whl

---
+++ packages/django-leek/django_leek-0.3-py3-none-any.whl/django_leek/service-stop-worker.py
@@ -0,0 +1,16 @@
+import socket
+from .settings import TASKS_HOST, TASKS_PORT
+    
+
+def stop_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((TASKS_HOST, TASKS_PORT))
+    sock.send("stop")
+    received = sock.recv(1024)
+    sock.close()
+    print("Sent: %s" % "stop")
+    print("Received: %s" % received)
+
+
+if __name__ == "__main__":
+    stop_server()
