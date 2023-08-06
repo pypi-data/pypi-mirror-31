25cd8b806007e82cf61feee62d865d28ca61892f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:01 +0000

    Add package mayonenester/1.3.0/mayonenester-1.3.0.tar.gz

---
+++ packages/mayonenester/mayonenester-1.3.0.tar.gz/mayonenester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'mayonenester',
+	version ='1.3.0',
+	py_modules = ['mayonenester'],
+	author = 'lijiankang',
+	author_email = '1124358467@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'Add an optional parameter, it is used to determine whether the indentation function is realized,and the default is false.',
+)
