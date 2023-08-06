cc137bc7d5f1b08d195de990aafa1a6bba57fe53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package dark-matter/2.0.3/dark-matter-2.0.3.tar.gz

---
+++ packages/dark-matter/dark-matter-2.0.3.tar.gz/dark-matter-2.0.3/dark/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+if sys.version_info < (2, 7):
+    raise Exception('The dark matter code needs Python 2.7 or later.')
+
+# Note that the version string must have the following format, otherwise it
+# will not be found by the version() function in ../setup.py
+__version__ = '2.0.3'
