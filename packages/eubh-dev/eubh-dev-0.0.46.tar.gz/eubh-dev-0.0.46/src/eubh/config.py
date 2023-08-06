7e192429de9357fa5e63801075630b3b6c388dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package eubh-dev/0.0.46/eubh-dev-0.0.46.tar.gz

---
+++ packages/eubh-dev/eubh-dev-0.0.46.tar.gz/eubh-dev-0.0.46/src/eubh/config.py
@@ -0,0 +1,7 @@
+VERSION = '0.0.46'
+ROOT_DIR = '/root'
+HOST_ADDRESS = 'dev.eubflow.eubchain.com'
+HTTP_ADDRESS = 'http://%s' % HOST_ADDRESS
+WS_ADDRESS = 'ws://%s:54188/' % HOST_ADDRESS
+ENABLE_VERBOSE = False
+DOCKER_WHITE_LIST = ['rancher_agent']
