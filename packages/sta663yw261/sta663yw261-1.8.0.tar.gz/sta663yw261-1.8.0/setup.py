71a5c9207be0216bc6b11c7b81dbf5fbef30921e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:06 +0000

    Add package sta663yw261/1.8.0/sta663yw261-1.8.0.tar.gz

---
+++ packages/sta663yw261/sta663yw261-1.8.0.tar.gz/sta663yw261-1.8.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from setuptools import setup
+
+setup(name = "sta663yw261",
+      version = "1.8.0",
+      description='Sta663 Final Project',
+      author='Yixuan Wang',
+      license='MIT',
+      author_email='yixuan.wang@duke.edu',
+      url='https://github.com/wyx951029',
+      py_modules = ['sta663yw261'],
+      packages=setuptools.find_packages(),
+      scripts = ['run_sta663.py'],
+      python_requires='>=3',
+      )
