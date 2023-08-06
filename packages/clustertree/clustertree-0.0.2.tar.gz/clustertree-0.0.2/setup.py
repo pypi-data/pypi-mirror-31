bf6bb180f0eb12e1f5572c7b47f421132016feda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:04 +0000

    Add package clustertree/0.0.2/clustertree-0.0.2.tar.gz

---
+++ packages/clustertree/clustertree-0.0.2.tar.gz/clustertree-0.0.2/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import find_packages, Extension, Command
+from distutils.core import setup
+
+
+from Cython.Build import cythonize
+
+from clustertree import __version__
+
+extensions = []
+extensions.append( Extension( "clustertree.src.clustertree",
+                            [ "clustertree/src/clustertree.pyx",
+                              "clustertree/src/cluster.c"], ) )
+
+setup(version=__version__,
+	  name='clustertree',
+      description="Kanwei Li's clustertree code",
+      long_description="Kanwei Li's clustertree code from bx-python",
+      author="Kanwei Li (Maintainer=Endre Bakken Stovner)",
+      author_email="endrebak85@gmail.com",
+      # package_dir = { '': 'lib' },
+      packages=find_packages(),
+      setup_requires=['cython'],
+      ext_modules=cythonize(extensions),
+      package_data={'': ['*.pyx', '*.pxd', '*.h', '*.c']},
+      include_dirs=["."],
+)
