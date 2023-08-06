6701816a0056b7276654490658a183366a0075b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package image-augmentor/0.1.4/image_augmentor-0.1.4.tar.gz

---
+++ packages/image-augmentor/image_augmentor-0.1.4.tar.gz/image_augmentor-0.1.4/image_augmentor/augmentor.py
@@ -0,0 +1,39 @@
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import math
+import numpy as np
+from tqdm import trange, tqdm
+
+from .options import AugmentOptions
+from .loader import Loader
+from .transform import Transform
+from .sample import Sample
+
+class Augmentor:
+    def __init__(self):
+        self.options = AugmentOptions() 
+
+    def run(self, filepath, count):
+        
+        print('Loading image files ... ')
+        files = os.listdir(filepath)
+        data = []
+        for file in tqdm(files):
+            _file = os.path.join(filepath, file)
+            data.append(Loader.load(_file))
+
+        print('Augmenting image set ... ')
+        aug_images = []
+        for _ in tqdm(range(count)):
+            for image in data:
+                options = self.options 
+                scale = np.random.rand() * (options.scale[1] - options.scale[0]) + options.scale[0]
+                rotate = np.random.rand() * (options.rotate[1] - options.rotate[0]) + options.rotate[0]
+                tx = np.random.rand() * (options.tx[1] - options.tx[0]) + options.tx[0]
+                ty = np.random.rand() * (options.ty[1] - options.ty[0]) + options.ty[0]
+                transform = Transform(scale = scale, rotation = rotate, translation = [tx, ty])
+                aug_images.append(Sample.sample(image, transform) )
+
+        return aug_images
---
+++ packages/image-augmentor/image_augmentor-0.1.4.tar.gz/image_augmentor-0.1.4/image_augmentor/point.py
@@ -0,0 +1,21 @@
+
+from __future__ import absolute_import, division, print_function
+
+class Point:
+
+    def __init__(self, value = None, x = None, y = None, bounding_box = None):
+        self.value = value
+        self.x = x
+        self.y = y
+
+        ''' Normalize x, y points between (-1, 1) '''
+        if(bounding_box != None):
+            self.y = (x / (bounding_box[0] - 1)) * 2.0 - 1.0
+            self.x = (y / (bounding_box[1] - 1)) * 2.0 - 1.0
+
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+    def __repr__(self):
+        return "Point( %.1g, %.1g )" % (self.x, self.y)
+        
