88244edc1dc138c8bdcc9fc9eec2e75be50ecfc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:02 +0000

    Add package diay/0.1.2/diay-0.1.2.tar.gz

---
+++ packages/diay/diay-0.1.2.tar.gz/diay-0.1.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+# allow setup.py to be ran from anywhere
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+setup(
+	name='diay',
+	version='0.1.2',
+	license='MIT',
+	description='diay - a dependency injection library',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	author='Andreas Lutro',
+	author_email='anlutro@gmail.com',
+	url='https://github.com/anlutro/diay.py',
+	# packages does not work for single file modules
+	py_modules=['diay'],
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+	],
+	keywords=['dependency injection', 'inversion of control'],
+)
