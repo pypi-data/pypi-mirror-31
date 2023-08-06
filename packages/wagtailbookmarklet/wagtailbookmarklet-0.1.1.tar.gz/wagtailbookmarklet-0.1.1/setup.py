297f9727f4654a8c8067b258fc152e6f7c0aa893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:15 +0000

    Add package wagtailbookmarklet/0.1.1/wagtailbookmarklet-0.1.1.tar.gz

---
+++ packages/wagtailbookmarklet/wagtailbookmarklet-0.1.1.tar.gz/wagtailbookmarklet-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wagtailbookmarklet',
+    version='0.1.1',
+    description="Gives Wagtail editors an 'edit this page' bookmarklet",
+    long_description=open('README.md').read(),
+    url='https://github.com/torchbox/wagtail-bookmarklet',
+    author='Tom Dyson',
+    author_email='tom+wagtailbookmarklet@torchbox.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "wagtail>=1.12"
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Topic :: Internet :: WWW/HTTP',
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    keywords='development',
+)
