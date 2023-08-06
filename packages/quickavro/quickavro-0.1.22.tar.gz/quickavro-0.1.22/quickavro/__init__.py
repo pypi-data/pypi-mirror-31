10036d98a0faf62fa7f06365b497680cbde98394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:45 +0000

    Add package quickavro/0.1.22/quickavro-0.1.22.tar.gz

---
+++ packages/quickavro/quickavro-0.1.22.tar.gz/quickavro-0.1.22/quickavro/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+quickavro
+~~~~~~~~~~~~
+
+quickavro is a library for working with the avro file format.
+
+:copyright: (c) 2016 by Chris Marshall.
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+__title__ = 'quickavro'
+__version__ = '0.1.22'
+__authors__ = ['Chris Marshall']
+__license__ = 'Apache 2.0'
+__all__ = ['BinaryEncoder', 'Enum', 'FileReader', 'FileWriter']
+
+from .constants import (
+    DEFAULT_SYNC_INTERVAL,
+    HEADER_SCHEMA
+)
+from ._quickavro import (
+    AvroError,
+    ReadError,
+    SchemaError,
+    WriteError
+)
+from .errors import (
+    CodecNotSupported,
+    InvalidSchemaError
+)
+from .encoder import BinaryEncoder, Enum
+from .reader import FileReader
+from .writer import FileWriter
