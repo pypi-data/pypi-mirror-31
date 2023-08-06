9685119d4670a6b71f38893366a6658f4577baf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:46 +0000

    Add package randomuser_generator_gcse/1.2.0/randomuser_generator_gcse-1.2.0.tar.gz

---
+++ packages/randomuser_generator_gcse/randomuser_generator_gcse-1.2.0.tar.gz/randomuser_generator_gcse-1.2.0/setup.py
@@ -0,0 +1,21 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='randomuser_generator_gcse',  # Required
+    version='1.2.0',  # Required
+    description='A random user generator.',
+    install_requires=['requests']
+)
