783c568a193b6833c7625523eefbb1716d852005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package beam-up/0.38/beam-up-0.38.tar.gz

---
+++ packages/beam-up/beam-up-0.38.tar.gz/beam-up-0.38/beam/settings.py
@@ -0,0 +1,48 @@
+from .processors import JinjaProcessor, MarkdownProcessor, PlainProcessor
+from .builders import BlogBuilder, PagesBuilder, StaticFilesBuilder
+from .loaders import FileLoader
+
+SETTINGS = {
+    'defaults' : {
+        'src-dir' : 'src',
+        'build-dir' : 'build',
+    },
+    'loaders' : [
+        {
+            'scheme' : 'file',
+            'loader' : FileLoader
+        }
+    ],
+    'builders' : [
+        {
+            'builder' : BlogBuilder,
+            'name' : 'blog'
+        },
+        {
+            'builder' : PagesBuilder,
+            'name' : 'pages'
+        },
+        {
+            'builder' : StaticFilesBuilder,
+            'providers' : ['static_file'], 
+            'name' : 'static_files'
+        },
+    ],
+    'processors' : [
+        {
+            'type' : 'html',
+            'processors' : [JinjaProcessor],
+            'name' : 'HTML'
+        },
+        {
+            'type' : 'md',
+            'processors' : [MarkdownProcessor, JinjaProcessor],
+            'name' : 'Markdown'
+        },
+        {
+            'type' : 'plain',
+            'processors' : [PlainProcessor],
+            'name' : 'Plain'
+        }
+    ],
+}
