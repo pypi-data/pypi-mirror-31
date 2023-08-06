fdbaee90a1dd02696f8f900fe8ed835fe4ec3b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:08 +0000

    Add package rovcontrol/0.5.8/rovcontrol-0.5.8-py3-none-any.whl

---
+++ packages/rovcontrol/rovcontrol-0.5.8-py3-none-any.whl/rovcontrol/drivers/adc/MCP3008.py
@@ -0,0 +1,16 @@
+import rovcontrol.drivers.adc.ADCTemp as ADCTemp
+
+import Adafruit_MCP3008
+import Adafruit_GPIO.SPI as SPI
+
+class MCP3008(ADCTemp.ADCTemp):
+    def __init__(self):
+        self.adaadc = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(0, 0))
+        super().__init__()
+
+    def scale(self, value, bus):
+        return (value / 1023) * self.get_vref(bus)
+
+    def update_one_channel(self, bus, channel, data):
+        if data['dirty']:
+            data['mv'] = self.scale(self.adaadc.read_adc(channel), bus)
