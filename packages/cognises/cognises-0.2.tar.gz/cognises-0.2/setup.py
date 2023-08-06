d6b23c13dbaa5b917702b0055eff07328451b2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package cognises/0.2/cognises-0.2.tar.gz

---
+++ packages/cognises/cognises-0.2.tar.gz/cognises-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(name='cognises',
+      version='0.2',
+      description='Implement application and cloud resource level authorization'
+                  ' based on user groups.',
+      classifiers=[
+          'Programming Language :: Python :: 2.7'
+      ],
+      url='https://github.com/plotlabs/cognises.git',
+      keywords=["aws", "cognito", "security", "authorization", "flask login",
+                "flask"],
+      author='Shalini Aggarwal',
+      author_email='aggarwalshalini1993@gmail.com',
+      license='LICENSE.txt',
+      packages=['cognises'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Flask', 'boto3', 'requests', 'simplejson',
+                        'python-jose']
+      )
