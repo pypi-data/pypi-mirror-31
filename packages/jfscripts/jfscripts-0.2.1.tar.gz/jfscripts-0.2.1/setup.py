88807fb032b8b861c926dcfe40bbb1e9781d167e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:06 +0000

    Add package jfscripts/0.2.1/jfscripts-0.2.1.tar.gz

---
+++ packages/jfscripts/jfscripts-0.2.1.tar.gz/jfscripts-0.2.1/setup.py
@@ -0,0 +1,37 @@
+import versioneer
+from setuptools import setup
+import six
+
+if six.PY2:
+    raise SystemError('jfscripts are not compatible to python2. Use python3!')
+
+
+setup(
+    name='jfscripts',
+    author='Josef Friedrich',
+    author_email='josef@friedrich.rocks',
+    description=('A collection of my scripts.'),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='MIT',
+    project_urls={
+        'Documentation': 'http://jfscripts.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/Josef-Friedrich/python-scripts',
+        'Tracker': 'https://github.com/Josef-Friedrich/python-scripts/issues',
+    },
+    packages=['jfscripts'],
+    url='https://github.com/Josef-Friedrich/python-scripts',
+    install_requires=['termcolor'],
+    entry_points = {
+        'console_scripts': [
+            'dns-ipv6-prefix.py = jfscripts.dns_ipv6_prefix:main',
+            'extract-pdftext.py = jfscripts.extract_pdftext:main',
+            'find-dupes-by-size.py = jfscripts.find_dupes_by_size:main',
+            'list-files.py = jfscripts.list_files:main',
+            'mac-to-eui64.py = jfscripts.mac_to_eui64:main',
+            'magick-imslp.py = jfscripts.magick_imslp:main',
+            'replace-pdfpage.py = jfscripts.replace_pdfpage:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
