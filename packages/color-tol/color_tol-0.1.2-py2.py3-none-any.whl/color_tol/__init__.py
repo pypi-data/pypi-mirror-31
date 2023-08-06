dc120ff0a326dbcbc3c5700a485877d6afbb375c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:18 +0000

    Add package color-tol/0.1.2/color_tol-0.1.2-py2.py3-none-any.whl

---
+++ packages/color-tol/color_tol-0.1.2-py2.py3-none-any.whl/color_tol/__init__.py
@@ -0,0 +1,13 @@
+"""
+color_tol is a pure Python package for accessing Paul Tol-style colormaps.
+See personal.sron.nl/~pault/colourschemes.pdf for more information.
+(colourschemes.pdf is also added under /docs in this package.)
+
+"""
+
+__version__ = '0.1'
+__all__ = []
+
+from color_tol.funcs import diverging
+from color_tol.funcs import sequential
+from color_tol.funcs import qualitative
