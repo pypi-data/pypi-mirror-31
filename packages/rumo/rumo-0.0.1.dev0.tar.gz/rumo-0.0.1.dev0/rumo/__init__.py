ac624ec43af3cb0dbfe1069e9a521b61ac4cb499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:45 +0000

    Add package rumo/0.0.1.dev0/rumo-0.0.1.dev0.tar.gz

---
+++ packages/rumo/rumo-0.0.1.dev0.tar.gz/rumo-0.0.1.dev0/rumo/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2018, Maximilian Köhl <mail@koehlma.de>
+
+"""
+Stream-Based Runtime Monitoring (RUMO)
+======================================
+RUMO is a stream-based runtime monitoring framework for Python. Formally RUMO is based on
+the stream-based specification language Lola [1]. RUMO's solid formal foundation and well
+integration into the Python ecosystem makes writing specifications and monitoring them a
+breeze. The rich collection of Python third-party libraries allow RUMO to be interfaced
+with nearly anything from IOT hardware to cloud-based services.
+
+[1] Ben d’Angelo et al, TIME 2005, LOLA: Runtime Monitoring of Synchronous Systems
+"""
+
---
+++ packages/rumo/rumo-0.0.1.dev0.tar.gz/rumo-0.0.1.dev0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2018, Maximilian Köhl <mail@koehlma.de>
+
+from distutils.core import setup
+
+
+setup(
+    name='rumo',
+    version='0.0.1.dev0',
+    description='Stream-Based Runtime Monitoring',
+    author='Maximilian Köhl',
+    author_email='mail@koehlma.de',
+    url='https://www.github.com/koehlma/rumo',
+    packages=['rumo'],
+)
