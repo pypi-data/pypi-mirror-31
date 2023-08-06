c1d20f3c6f6520bf9bb6098241f80cd12789f6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:00 +0000

    Add package dj-rit-auditlog/0.5.1/dj-rit-auditlog-0.5.1.tar.gz

---
+++ packages/dj-rit-auditlog/dj-rit-auditlog-0.5.1.tar.gz/dj-rit-auditlog-0.5.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='dj-rit-auditlog',
+    version='0.5.1',
+    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
+    package_dir={'': 'src'},
+    url='https://github.com/audiolion/dj-rit-auditlog',
+    license='MIT',
+    author='Jan-Jelle Kester, Ryan Castner',
+    description='Audit log app for Django',
+    install_requires=[
+        'python-dateutil>=2.6.0,<3.0'
+    ],
+    zip_safe=False
+)
