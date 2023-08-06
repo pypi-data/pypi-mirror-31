a3ff8ff1695fc380074c480afac792fad69d497d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:34 +0000

    Add package pyecharts/0.5.0/pyecharts-0.5.0.tar.gz

---
+++ packages/pyecharts/pyecharts-0.5.0.tar.gz/pyecharts-0.5.0/test/test_geolines.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals
+
+from nose.tools import assert_raises
+
+from pyecharts import GeoLines, Style
+from pyecharts.datasets.coordinates import search_coordinates_by_keyword
+
+style = Style(
+    title_top="#fff",
+    title_pos="center",
+    width=1200,
+    height=600,
+    background_color="#404a59",
+)
+
+style_geo = style.add(
+    is_label_show=True,
+    line_curve=0.2,
+    line_opacity=0.6,
+    legend_text_color="#eee",
+    legend_pos="right",
+    geo_effect_symbol="plane",
+    geo_effect_symbolsize=15,
+    label_color=['#a6c84c', '#ffa022', '#46bee9'],
+    label_pos="right",
+    label_formatter="{b}",
+    label_text_color="#eee",
+    legend_selectedmode="single",
+)
+
+
+def test_geolines():
+    data_guangzhou = [
+        ["广州", "上海"],
+        ["广州", "北京"],
+        ["广州", "南京"],
+        ["广州", "重庆"],
+        ["广州", "兰州"],
+        ["广州", "杭州"],
+    ]
+    data_beijing = [
+        ["北京", "上海"],
+        ["北京", "广州"],
+        ["北京", "南京"],
+        ["北京", "重庆"],
+        ["北京", "兰州"],
+        ["北京", "杭州"],
+    ]
+    lines = GeoLines("GeoLines 示例", **style.init_style)
+    lines.add("从广州出发", data_guangzhou, **style_geo)
+    lines.add("从北京出发", data_beijing, **style_geo)
+    lines.print_echarts_options()
+    lines.render()
+
+
+def test_with_custom_coordinates():
+    data_guangzhou = [
+        ["广州", "上海"],
+        ["广州", "北京"],
+        ["广州", "南京"],
+        ["广州", "重庆"],
+        ["广州", "兰州"],
+        ["广州", "A市"],
+    ]
+    lines = GeoLines("GeoLines 示例", **style.init_style)
+    coordinate = lines.get_coordinate('广州')
+    assert 2 == len(coordinate)
+    with assert_raises(ValueError):
+        lines.get_coordinate('A市', raise_exception=True)
+
+    lines.add(
+        "从广州出发",
+        data_guangzhou,
+        geo_cities_coords={'A市': (119.3, 26.08)},
+        **style_geo
+    )
+    lines.render()
+
+
+def test_with_full_example():
+    line_data = [["广州", "上海"], ["广州", "北京"], ["广州", "南京"], ['广州', 'A市']]
+
+    lines = GeoLines("GeoLines 示例", **style.init_style)
+
+    with assert_raises(ValueError):
+        lines.add("从广州出发", line_data, **style_geo)
+
+    assert 0 == len(search_coordinates_by_keyword('A市'))
+
+    lines.add_coordinate('A市', 119.3, 26.08)
+    lines.add("从广州出发", line_data, **style_geo)
+    lines.render()
---
+++ packages/pyecharts/pyecharts-0.5.0.tar.gz/pyecharts-0.5.0/test/test_utils.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+import os
+import codecs
+
+from nose.tools import eq_
+
+from pyecharts.utils import (
+    write_utf8_html_file, get_resource_dir, merge_js_dependencies
+)
+from pyecharts.utils.lazy import LazyObject
+
+
+def test_get_resource_dir():
+    path = get_resource_dir('templates')
+    expected = os.path.join(os.getcwd(), '..', 'pyecharts', 'templates')
+    eq_(path, os.path.abspath(expected))
+
+
+def test_write_utf8_html_file():
+    content = "柱状图数据堆叠示例"
+    file_name = 'test.html'
+    write_utf8_html_file(file_name, content)
+    with codecs.open(file_name, 'r', 'utf-8') as f:
+        actual_content = f.read()
+        eq_(content, actual_content)
+
+
+class MockChart(object):
+    """
+    A mock class for a Chart and Page
+    """
+
+    def __init__(self, js_dependencies):
+        self.js_dependencies = js_dependencies
+
+
+def test_merge_js_dependencies_with_one_chart():
+    # Prepare some kinds of charts or page.
+
+    base_chart = MockChart(['echarts'])
+
+    # One chart or page
+    eq_(['echarts'], merge_js_dependencies(base_chart))
+    # A map chart
+    ch1 = MockChart(['echarts', 'fujian', 'zhengjiang', 'anhui'])
+    eq_(
+        ['echarts', 'fujian', 'zhengjiang', 'anhui'],
+        merge_js_dependencies(ch1),
+    )
+
+
+def test_merge_js_dependencies_with_multiple_charts():
+    base_chart = MockChart(['echarts'])
+    map_chart = MockChart(['echarts', 'fujian'])
+    three_d_chart = MockChart(['echarts', 'echartsgl'])
+    # Multiple charts
+    eq_(['echarts', 'fujian'], merge_js_dependencies(base_chart, map_chart))
+    eq_(
+        ['echarts', 'echartsgl', 'fujian'],
+        merge_js_dependencies(base_chart, map_chart, three_d_chart),
+    )
+
+
+def test_merge_js_dependencies_with_mixed_chart_and_string():
+    map_chart = MockChart(['echarts', 'fujian'])
+
+    eq_(['echarts', 'zhejiang'], merge_js_dependencies('echarts', 'zhejiang'))
+    eq_(
+        ['echarts', 'zhejiang'], merge_js_dependencies(['echarts', 'zhejiang'])
+    )
+    eq_(['echarts', 'fujian'], merge_js_dependencies('echarts', map_chart))
+
+
+class MockPoint(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def create_point():
+    return MockPoint(1, 2)
+
+
+def test_lazy_object():
+    p = LazyObject(create_point)
+    assert isinstance(p, LazyObject)
+    p.x = 3
+    assert isinstance(p, MockPoint)
