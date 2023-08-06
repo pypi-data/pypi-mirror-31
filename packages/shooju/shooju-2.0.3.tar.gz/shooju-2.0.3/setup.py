f83e8d3dc3d63bd6094cd5cb99f5fe6fe7f6bc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:12 +0000

    Add package shooju/2.0.3/shooju-2.0.3.tar.gz

---
+++ packages/shooju/shooju-2.0.3.tar.gz/shooju-2.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+version = '2.0.3'
+
+
+def read_description():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='shooju',
+      version=version,
+      description="Official Shooju Client",
+      long_description=read_description(),
+      keywords='data, client, shooju',
+      author='Serge Aluker',
+      author_email='serge@shooju.com',
+      url='http://shooju.com',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "requests>=2.0.0", "six>=1.10.0"
+      ]
+      )
