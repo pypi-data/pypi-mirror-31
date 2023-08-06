9a3e8c2d01e0aca38a91233f0ecfeb8030a76c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:40 +0000

    Add package pclpy/0.6.1/pclpy-0.6.1-cp36-cp36m-win_amd64.whl

---
+++ packages/pclpy/pclpy-0.6.1-cp36-cp36m-win_amd64.whl/pclpy/tests/test_visualization.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pytest
+
+import pclpy
+from pclpy import pcl
+import pclpy.view.vtk
+
+
+def test_data(*args):
+    return os.path.join("test_data", *args)
+
+
+def test_cloud_viewer():
+    pc = pclpy.io.read(test_data("street.las"))
+    viewer = pcl.visualization.CloudViewer("viewer")
+    viewer.showCloud(pc, "hi")
+    # while not viewer.was_stopped(1):
+    #     pass
+
+
+def test_pcl_visualizer_simple():
+    pc = pclpy.io.read(test_data("street.las"))
+    viewer = pcl.visualization.PCLVisualizer("viewer")
+    viewer.setBackgroundColor(0, 0, 0)
+    rgb = pcl.visualization.PointCloudColorHandlerRGBAField.PointXYZRGBA(pc)
+    viewer.addPointCloud(pc, rgb, "sample cloud")
+    viewer.setPointCloudRenderingProperties(0, 3, "sample cloud")
+    viewer.addCoordinateSystem(1.0)
+    viewer.initCameraParameters()
+    # while not viewer.wasStopped():
+    #     viewer.spinOnce(10, False)
+
+
+def test_viewer_intensity():
+    pc = pclpy.io.read(test_data("street.las"), "PointXYZI")
+    viewer = pclpy.view.vtk.Viewer(pc)
+    # viewer.show()
