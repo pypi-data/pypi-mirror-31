232fb6c5badcf810efa8eac349690c592aa383f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:28 +0000

    Add package bestcaptchasolverapi3/0.1/bestcaptchasolverapi3-0.1.tar.gz

---
+++ packages/bestcaptchasolverapi3/bestcaptchasolverapi3-0.1.tar.gz/bestcaptchasolverapi3-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bestcaptchasolverapi3',
+  packages = ['bestcaptchasolverapi3'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Bestcaptchasolver python3 API library',
+  author = 'BestCaptchaSolver',
+  author_email = 'bcsolver@gmail.com',
+  url = 'https://github.com/bestcaptchasolver/bestcaptchasolver-python3', # use the URL to the github repo
+  download_url = 'https://github.com/bestcaptchasolver/bestcaptchasolver-python3', # I'll explain this in a second
+  keywords = ['bestcaptchasolver', 'captcha', 'bypasscaptcha', 'decaptcher', 'decaptcha', '2captcha', 'deathbycaptcha', 'anticaptcha', 'bypass-recaptcha-v2', 'google-recaptcha-solver', 'recaptcha-v2-captcha-solver', 'captcha-services-for-recaptcha-v2', 'bypass-invisible-recaptcha', 'bypass-no-captcha-recaptcha', 'recaptcha-solver-python', 'recaptcha-bypass-script'],
+  classifiers = [],
+)
