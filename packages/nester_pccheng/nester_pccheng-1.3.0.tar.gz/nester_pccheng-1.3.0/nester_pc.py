44c8a510fa08b29fe9874a76739ff123ff4eddcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package nester_pccheng/1.3.0/nester_pccheng-1.3.0.tar.gz

---
+++ packages/nester_pccheng/nester_pccheng-1.3.0.tar.gz/nester_pccheng-1.3.0/nester_pc.py
@@ -0,0 +1,14 @@
+"""This is the "nester.py" module"""
+
+def print_lol(the_list , indent = False, level=0):
+        """takes an list, print each item in the list recurrsively.
+                and on its own line"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item, indent, level+1)
+                else:
+                        if indent:
+                            for tap_range in range(level):
+                                    print("\t", end='')
+                        print(each_item)
+		
