007c5aaebd614a012d89881ba82d1d63217e4a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:07 +0000

    Add package Templer/1.1.4/Templer-1.1.4-py3-none-any.whl

---
+++ packages/Templer/Templer-1.1.4-py3-none-any.whl/templer/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+__author__ = "Andre Lehmann"
+__email__ = "aisberg@posteo.de"
+__version__ = '1.1.4'
+
+from templer.cli import cli
+
+if __name__ == '__main__':
+    cli()
