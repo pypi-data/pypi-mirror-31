dd24d8ae32673ebdf0d957a67923f1ade40e37f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:52 +0000

    Add package pssm/0.0.7/pssm-0.0.7.tar.gz

---
+++ packages/pssm/pssm-0.0.7.tar.gz/pssm-0.0.7/pssm/dglm.py
@@ -0,0 +1,47 @@
+import abc
+import numpy as np
+from scipy.stats import multivariate_normal as mvn, poisson
+import math
+
+ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
+
+
+class DLM(ABC):
+
+    def __init__(self, structure):
+        self._structure = structure
+        self._Ft = np.transpose(structure.F)
+
+    def state(self, previous):
+        mean = np.squeeze(np.asarray(np.dot(self._structure.G, previous)))
+        return mvn(mean=mean, cov=self._structure.W, allow_singular=True).rvs()
+
+    @abc.abstractmethod
+    def observation(self, state): pass
+
+
+class NormalDLM(DLM):
+    """
+    An instance of a Normal DLM
+    """
+
+    def __init__(self, structure, V):
+        super(NormalDLM, self).__init__(structure)
+        self._V = np.matrix([[V]])
+
+    def observation(self, state):
+        mean = np.dot(self._Ft, state)
+        return mvn(mean=mean, cov=self._V).rvs()
+
+
+class PoissonDLM(DLM):
+    """
+    An instance of a Poisson DLM
+    """
+
+    def __init__(self, structure):
+        super(PoissonDLM, self).__init__(structure)
+
+    def observation(self, state):
+        mean = math.exp(np.asscalar(np.dot(self._Ft, state)))
+        return poisson(mean).rvs()
