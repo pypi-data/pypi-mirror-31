cbe642178780458bf56eae66bd0b8e05d5fed02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:43 +0000

    Add package Gender-Predictor/0.5/Gender-Predictor-0.5.tar.gz

---
+++ packages/Gender-Predictor/Gender-Predictor-0.5.tar.gz/Gender-Predictor-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='Gender-Predictor',  # This is the name of your PyPI-package.
+      version='0.5',  # Update the version number for new releases
+      description='Classify the name based on given name',
+      author='Jitesh-Mohite',
+      author_email='jiteshmohite619@gmail.com',
+      maintainer='Mustafa Atik',
+      maintainer_email='muatik@gmail.com',
+      packages=['gender_predictor'],
+      package_data={'gender_predictor': ['data/*']},
+      url='https://github.com/jiteshmohite/Gender-Predictor',
+      install_requires=['nltk'], )
