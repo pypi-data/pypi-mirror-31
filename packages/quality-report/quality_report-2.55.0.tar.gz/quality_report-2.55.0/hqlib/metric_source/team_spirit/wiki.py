bf42e1b6a8d32e1b3c3d6702a1d5d222e481f25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:26 +0000

    Add package quality-report/2.55.0/quality_report-2.55.0.tar.gz

---
+++ packages/quality-report/quality_report-2.55.0.tar.gz/quality_report-2.55.0/hqlib/metric_source/team_spirit/wiki.py
@@ -0,0 +1,93 @@
+"""
+Copyright 2012-2018 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import datetime
+import logging
+import re
+import time
+
+from typing import List
+
+from hqlib.metric_source import beautifulsoup
+from hqlib.metric_source.abstract import team_spirit
+from hqlib.metric_source.url_opener import UrlOpener
+from hqlib.typing import DateTime
+
+
+class Wiki(team_spirit.TeamSpirit, beautifulsoup.BeautifulSoupOpener):
+    """ Class representing the Wiki instance. """
+
+    metric_source_name = 'Wiki'
+
+    def __init__(self, wiki_url: str) -> None:
+        super().__init__(url=wiki_url)
+
+    # Metric source API
+
+    def metric_source_urls(self, *metric_source_ids: str) -> List[str]:
+        """ Return the url(s) to the metric source for the metric source id. """
+        return [self.url()]
+
+    # Team spirit
+
+    def team_spirit(self, team_id: str) -> str:
+        """ Return the team spirit of the team. Team spirit is either :-), :-|, or :-( """
+        try:
+            soup = self.soup(self.url())
+        except UrlOpener.url_open_exceptions as reason:
+            logging.warning("Could not open %s to read spirit of team %s: %s", self.url(), team_id, reason)
+            return ''
+        try:
+            row = soup('tr', id=team_id)[0]('td')
+        except IndexError:
+            logging.warning("Could not find %s in wiki %s", team_id, self.url())
+            return ''
+        try:
+            return re.sub(r'[^:\-()|]', '', row[-1].contents[0])
+        except TypeError as reason:
+            logging.warning("Could not find a string in the last row: %s", reason)
+            return ''
+
+    def datetime(self, *team_ids: str) -> DateTime:
+        """ Return the date that the team spirit of the team was last measured. """
+        try:
+            soup = self.soup(self.url())
+        except UrlOpener.url_open_exceptions:
+            logging.warning("Could not open %s to read date of last spirit measurement of team %s", self.url(),
+                            team_ids[0])
+            return datetime.datetime.min
+        columns = len(soup('tr', id=team_ids[0])[0]('td'))
+        try:
+            date_text = soup('th')[columns - 1].contents[0].strip()
+        except (IndexError, AttributeError) as reason:
+            logging.error("Could not read the date of the last spirit measurement of team %s from %s: %s",
+                          team_ids[0], self.url(), reason)
+            return datetime.datetime.min
+        try:
+            return self.__parse_date(date_text)
+        except ValueError as reason:
+            logging.error("Could not parse the date of the last spirit measurement of team %s from %s: %s",
+                          team_ids[0], self.url(), reason)
+            return datetime.datetime.min
+
+    # Utility methods
+
+    @staticmethod
+    def __parse_date(date_text: str) -> DateTime:
+        """ Return a parsed version of the date text. """
+        year, month, day = time.strptime(date_text, '%d-%m-%Y')[:3]
+        return datetime.datetime(year, month, day)
---
+++ packages/quality-report/quality_report-2.55.0.tar.gz/quality_report-2.55.0/hqlib/metric_source/test_report/jenkins_test_report.py
@@ -0,0 +1,96 @@
+"""
+Copyright 2012-2018 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import ast
+import datetime
+import logging
+from typing import Dict, Optional, Union
+
+from ..abstract import test_report
+from ..url_opener import UrlOpener
+from ...typing import DateTime
+
+
+class JenkinsTestReport(test_report.TestReport):
+    """ Class representing Jenkins test reports. """
+    metric_source_name = 'Jenkins testreport'
+
+    def _passed_tests(self, metric_source_id: str) -> int:
+        """ Return the number of passed tests. """
+        try:
+            return self.__test_count(metric_source_id, 'passCount')
+        except KeyError:
+            # Surefire reports don't have a pass count, calculate it:
+            total = self.__test_count(metric_source_id, 'totalCount')
+            skipped = self._skipped_tests(metric_source_id)
+            failed = self._failed_tests(metric_source_id)
+            return total - skipped - failed
+
+    def _failed_tests(self, metric_source_id: str) -> int:
+        """ Return the number of failed tests. """
+        return self.__test_count(metric_source_id, 'failCount')
+
+    def _skipped_tests(self, metric_source_id: str) -> int:
+        """ Return the number of skipped tests. """
+        return self.__test_count(metric_source_id, 'skipCount')
+
+    def __test_count(self, report_url: str, result_type: str) -> int:
+        """ Return the number of tests with the specified result in the test report. """
+        json = self.__read_json(self.__join_url(report_url, 'lastCompletedBuild/api/python'))
+        if not json:
+            # Last completed build doesn't have the requested information, e.g. because it's aborted.
+            # Fall back to last successful build.
+            json = self.__read_json(self.__join_url(report_url, 'lastSuccessfulBuild/api/python'))
+        return int(json[result_type]) if json else -1
+
+    def _report_datetime(self, metric_source_id: str) -> DateTime:
+        """ Return the date and time of the specified report. """
+        json = self.__read_json(self.__join_url(metric_source_id, 'lastCompletedBuild/api/python'))
+        if not json:
+            # Last completed build doesn't have the requested information, e.g. because it's aborted.
+            # Fall back to last successful build.
+            json = self.__read_json(self.__join_url(metric_source_id, 'lastSuccessfulBuild/api/python'))
+        return datetime.datetime.fromtimestamp(float(json["timestamp"]) / 1000.) if json else datetime.datetime.min
+
+    def __read_json(self, api_url: str) -> Optional[Dict[str, Union[int, str]]]:
+        """ Return the test results and the timestamp from the url, or None when something goes wrong. """
+        try:
+            contents = self._url_read(api_url)
+        except UrlOpener.url_open_exceptions:
+            return None
+        try:
+            build = ast.literal_eval(contents)
+        except (SyntaxError, NameError, TypeError) as reason:
+            logging.warning("Couldn't eval %s: %s\nData received: %s", api_url, reason, contents)
+            return None
+        return self.__test_data(build)
+
+    @staticmethod
+    def __test_data(build: Dict) -> Optional[Dict[str, Union[int, str]]]:
+        """ Return the test data from the build json. """
+        actions = build.get("actions", [])
+        for action in actions:
+            if "totalCount" in action and "failCount" in action:
+                # Assume this is the test action dictionary. Include the timestamp of the build in the dictionary.
+                action["timestamp"] = build["timestamp"]
+                return action
+        return None
+
+    @staticmethod
+    def __join_url(*parts: str) -> str:
+        """ Join the different url parts with forward slashes. """
+        return '/'.join([part.strip('/') for part in parts])
