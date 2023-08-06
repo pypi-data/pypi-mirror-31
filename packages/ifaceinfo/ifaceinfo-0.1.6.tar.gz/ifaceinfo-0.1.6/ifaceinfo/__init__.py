3ca14c90a9b0a12f0cfdc8a091137ea655998795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:56 +0000

    Add package ifaceinfo/0.1.6/ifaceinfo-0.1.6.tar.gz

---
+++ packages/ifaceinfo/ifaceinfo-0.1.6.tar.gz/ifaceinfo-0.1.6/ifaceinfo/__init__.py
@@ -0,0 +1,7 @@
+from ifaceinfo import InterfacesInfos
+from ifaceroutes import InterfacesRoutes
+from ifaceinfotools import IfaceInfoTools
+from ifaceinfotools import FileReader
+from ifaceinfotools import Conn
+from tcpconn import TCPConn
+from udpconn import UDPConn
