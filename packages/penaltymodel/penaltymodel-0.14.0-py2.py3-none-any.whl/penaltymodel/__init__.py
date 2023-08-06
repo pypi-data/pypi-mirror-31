3730e48087d454537bab9443669550735ed9f5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package penaltymodel/0.14.0/penaltymodel-0.14.0-py2.py3-none-any.whl

---
+++ packages/penaltymodel/penaltymodel-0.14.0-py2.py3-none-any.whl/penaltymodel/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from penaltymodel.classes import *
+import penaltymodel.classes
+
+from penaltymodel.exceptions import *
+import penaltymodel.exceptions
+
+from penaltymodel.interface import *
+import penaltymodel.interface
+
+from penaltymodel.package_info import __version__
+import penaltymodel.package_info
+
+from dimod import Vartype, SPIN, BINARY
---
+++ packages/penaltymodel/penaltymodel-0.14.0-py2.py3-none-any.whl/penaltymodel/classes/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from penaltymodel.classes.specification import *
+import penaltymodel.classes.specification
+
+from penaltymodel.classes.penaltymodel import *
+import penaltymodel.classes.penaltymodel
