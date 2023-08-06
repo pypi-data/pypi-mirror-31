f0c38fa152223b9dae17f7783efb9d8db4bab3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:01 +0000

    Add package pyawskit/0.1.57/pyawskit-0.1.57.tar.gz

---
+++ packages/pyawskit/pyawskit-0.1.57.tar.gz/pyawskit-0.1.57/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+setuptools.setup(
+    name='pyawskit',
+    version='0.1.57',
+    description='pyawskit is a collection of utilities to help interact with aws',
+    long_description='pyawskit is a collection of utilities to help interact with aws',
+    url='https://veltzer.github.io/pyawskit',
+    download_url='https://github.com/veltzer/pyawskit',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
+    license='MIT',
+    platforms=['python3'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='aws utils python API command line',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pylogconf',  # for logging configuration
+        'pyfakeuse',  # for coding
+        'pypipegzip',  # used by pyawskit_compress_s3_folder
+        'tqdm',  # used for progress
+        'requests',  # for http
+        'boto3',  # used by pyawskit_generate_ssh_config
+        'pymount',  # used by pyawskit_unify_disks
+        'ujson',  # used by pyawskit_generate_ssh_config
+        'sultan',  # for better ssh
+        'pytconf',  # for command line parsing
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyawskit = pyawskit.endpoints.main:main',
+        ],
+    },
+)
