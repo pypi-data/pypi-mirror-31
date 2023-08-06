5427043b118488131e850559218ec77853ed4555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:07 +0000

    Add package polyinterface/2.0.27/polyinterface-2.0.27.tar.gz

---
+++ packages/polyinterface/polyinterface-2.0.27.tar.gz/polyinterface-2.0.27/polyinterface/__init__.py
@@ -0,0 +1,10 @@
+from .polyinterface import Interface, Node, Controller, LOGGER
+
+__version__     = '2.0.27'
+__description__ = 'UDI Polyglot v2 Interface'
+__url__         = 'https://github.com/UniversalDevicesInc/polyglot-v2-python-interface'
+__author__      = 'James Milne'
+__authoremail__ = 'milne.james@gmail.com'
+__license__     = 'MIT'
+
+LOGGER.info('{} {} Starting...'.format(__description__,__version__))
