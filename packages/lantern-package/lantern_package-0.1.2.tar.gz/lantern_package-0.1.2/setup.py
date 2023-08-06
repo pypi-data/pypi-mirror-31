9dcc6fc379ce7e01837d3ccb2c0d36c65e24c49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:48 +0000

    Add package lantern-package/0.1.2/lantern_package-0.1.2.tar.gz

---
+++ packages/lantern-package/lantern_package-0.1.2.tar.gz/lantern_package-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+        name = 'lantern_package',
+        packages = ['lantern_package'],
+        version = '0.1.2',
+        description = 'A random test lib',
+        author = 'Anthony Munoz',
+        author_email = 'toni5705@gmail.com',
+        url = 'https://github.com/peterldowns/mypackage',
+        download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+        keywords = ['testing', 'logging', 'example']
+)
