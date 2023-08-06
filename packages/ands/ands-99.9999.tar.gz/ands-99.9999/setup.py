d81161c2d24be692307f142c917d43ec836a034a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:51 +0000

    Add package ands/99.9999/ands-99.9999.tar.gz

---
+++ packages/ands/ands-99.9999.tar.gz/ands-99.9999/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ands',
+      version='99.9999',
+      description='Dummy package for ands package name usage',
+      url='',
+      author='mtaifi',
+      author_email='moutai10@gmail.com',
+      license='GPL-3',
+      packages=['ands'],
+      zip_safe=False)
