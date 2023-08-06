dc1b9edca7a39f9c1581504361e272fdbf143d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:54 +0000

    Add package multiJump/0.0.6/multiJump-0.0.6.tar.gz

---
+++ packages/multiJump/multiJump-0.0.6.tar.gz/multiJump-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import codecs  
+  
+import os,sys
+from setuptools import (
+    setup,
+)
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read() 
+
+setup(
+    name="multiJump",
+    version = "0.0.6",
+    author="seqyuan",
+    author_email='seqyuan@gmail.com',
+    url="https://github.com/seqyuan/multiJump/wiki",
+    download_url = "https://codeload.github.com/seqyuan/multiJump/zip/master",
+    description="multiple Jump IP do something",
+    long_description=read("README.rst"),
+    license="MIT",
+    packages=['multiJump'],
+    extras_require = {
+        'sys' : [ 'sys'],
+        'paramiko' : ['paramiko'],
+        'socket' : ['socket'],
+        'time' : ['time'],
+    }
+)
