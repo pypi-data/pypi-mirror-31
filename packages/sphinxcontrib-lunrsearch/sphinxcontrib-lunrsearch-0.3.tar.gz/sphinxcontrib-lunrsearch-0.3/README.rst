5969947426955d52b2ce93bd0bc9ac1e2730392d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:10 +0000

    Add package sphinxcontrib-lunrsearch/0.3/sphinxcontrib-lunrsearch-0.3.tar.gz

---
+++ packages/sphinxcontrib-lunrsearch/sphinxcontrib-lunrsearch-0.3.tar.gz/sphinxcontrib-lunrsearch-0.3/README.rst
@@ -0,0 +1,25 @@
+sphinxcontrib-lunrsearch
+========================
+
+This extension modifies the search box in Sphinx documentation
+to show instant results as you type. It's particularly suited for
+searching through APIs.
+
+To get started, ``$ pip install sphinxcontrib-lunrsearch`` and then add ``'sphinxcontrib.lunrsearch'`` to the list
+of extensions in your Sphinx ``conf.py`` file.
+
+Options
+-------
+
+The following options can be set in conf.py:
+
+- lunrsearch_highlight: bool
+
+  Whether to highlight the seach term after navigating to a results.
+  The default is ``False``.
+
+Example
+-------
+
+.. image:: http://i.giphy.com/3o85xmqkbt14LuRcZO.gif
+  
---
+++ packages/sphinxcontrib-lunrsearch/sphinxcontrib-lunrsearch-0.3.tar.gz/sphinxcontrib-lunrsearch-0.3/setup.py
@@ -0,0 +1,47 @@
+import io
+from setuptools import setup, find_packages
+
+
+def readfile(filename):
+    with io.open(filename, encoding="utf-8") as stream:
+        return stream.read().splitlines()
+
+setup(
+    name='sphinxcontrib-lunrsearch',
+    url='https://github.com/rmcgibbo/sphinxcontrib-lunrsearch',
+    download_url='https://pypi.python.org/pypi/sphinxcontrib-lunrsearch',
+    license="MIT",
+    author="Robert T. McGibbon",
+    author_email="rmcgibbo@gmail.com",
+    description="Instant search for Sphinx",
+    long_description='\n'.join(readfile("README.rst")[3:]),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['sphinxcontrib'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'Sphinx>=1.0',
+        'six>=1.4.1',
+    ]
+)
