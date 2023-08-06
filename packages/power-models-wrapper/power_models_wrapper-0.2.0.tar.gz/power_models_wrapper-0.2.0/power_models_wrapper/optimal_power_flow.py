6e3acad4e4afa4af88895df10abb373453da2366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:06 +0000

    Add package power-models-wrapper/0.2.0/power_models_wrapper-0.2.0.tar.gz

---
+++ packages/power-models-wrapper/power_models_wrapper-0.2.0.tar.gz/power_models_wrapper-0.2.0/power_models_wrapper/optimal_power_flow.py
@@ -0,0 +1,26 @@
+import julia
+#from subprocess import run, PIPE
+from power_models_wrapper.formulation_type import FormulationType
+
+class OptimalPowerFlow:
+
+    def __init__(self):
+        #pass
+        self.__julia_object = julia.Julia()
+        self.__julia_object.using('PowerModels')
+
+    def run(self, input_matlab_data_file_path, formulation_type = FormulationType.ac):
+
+        # TODO: Initial implementation of using PowerModels run method, using hardcoded solver:
+        self.__julia_object.using('Ipopt')
+        julia_code = self.__get_julia_run_method_name(formulation_type) + '("' + input_matlab_data_file_path + '", IpoptSolver())'
+        result = self.__julia_object.eval(julia_code)
+
+        #julia_method_call_code = 'result = ' + self.__get_julia_run_method_name(formulation_type) + '("' + input_matlab_data_file_path + '", IpoptSolver());'
+        #julia_code = "using PowerModels; using Ipopt; " + julia_method_call_code + " println(result)"
+        #result = run(["julia", "-e", julia_code], stdout = PIPE)
+
+        return result
+
+    def __get_julia_run_method_name(self, formulation_type):
+        return "run_%(formulation_type)s_opf" % {'formulation_type':formulation_type.name}
