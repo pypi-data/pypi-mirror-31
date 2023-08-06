15904ae8c7370e5a3803fb8e2fedb3d1ec719486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package Chern/2.2.10/Chern-2.2.10.tar.gz

---
+++ packages/Chern/Chern-2.2.10.tar.gz/Chern-2.2.10/setup.py
@@ -0,0 +1,46 @@
+from distutils import sys
+import os
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+PACKAGE = "Chern"
+NAME = "Chern"
+DESCRIPTION = "A data analysis management toolkit for high energy physics"
+AUTHOR = "Mingrui Zhao"
+AUTHOR_EMAIL = "mingrui.zhao@mail.labz0.org"
+URL = "https://github.com/zhaomr13/Chern"
+VERSION = __import__(PACKAGE).__version__
+here = os.path.dirname(__file__)
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Apache License, Version 2.0",
+    url = URL,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+
+    ],
+    zip_safe=False,
+    keywords = "Analysis Perservation",
+    packages = find_packages(exclude=[]),
+    install_requires = [
+        "click", "colored", "python-daemon", "ipython"
+    ],
+    include_package_data = True,
+    entry_points = {
+        'console_scripts': [
+            'chern = Chern.main:main',
+            'chen = Chern.main:main'
+        ]
+    }
+)
