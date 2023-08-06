f88854049c90452e4f67446670183f7efbb4500c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:12 +0000

    Add package megaradrp/0.6.1/megaradrp-0.6.1-py2.py3-none-any.whl

---
+++ packages/megaradrp/megaradrp-0.6.1-py2.py3-none-any.whl/megaradrp/recipes/calibration/linearity.py
@@ -0,0 +1,23 @@
+#
+# Copyright 2011-2017 Universidad Complutense de Madrid
+#
+# This file is part of Megara DRP
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+"""Calibrate the non linearity correciont of MEGARA Detector"""
+
+import logging
+
+from numina.core import RecipeError
+
+from megaradrp.core.recipe import MegaraBaseRecipe
+
+
+class LinearityTestRecipe(MegaraBaseRecipe):
+    """Process LINTEST images and create LINEARITY_CORRECTON."""
+    pass
+
+
