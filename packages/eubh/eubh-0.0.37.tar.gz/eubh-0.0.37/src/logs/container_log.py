0c0ab7c0056eac951eb40e3ac9f07f5c2b8354de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:05 +0000

    Add package eubh/0.0.37/eubh-0.0.37.tar.gz

---
+++ packages/eubh/eubh-0.0.37.tar.gz/eubh-0.0.37/src/logs/container_log.py
@@ -0,0 +1,19 @@
+from threading import Thread
+
+from docker import from_env
+
+from config import ROOT_DIR
+
+
+def write_docker_log():
+    client = from_env()
+    for container in client.containers.list():
+        for line in container.logs(stream=True):
+            with open('%s/result/%s.log' % (ROOT_DIR, container.attrs['Id']), 'a') as f:
+                f.writelines(line)
+
+
+class ContainerLog:
+    def __init__(self):
+        job = Thread(target=write_docker_log())
+        job.start()
