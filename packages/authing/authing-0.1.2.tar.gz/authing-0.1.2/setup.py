dfc347aa4d62f60e0f1e2d9a76736536791e5c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:46 +0000

    Add package authing/0.1.2/authing-0.1.2.tar.gz

---
+++ packages/authing/authing-0.1.2.tar.gz/authing-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages  
+  
+setup(  
+      name='authing',   #名称  
+      version='0.1.2',  #版本  
+      description="Authing SDK for Python", #描述  
+      keywords='authing sso AaaS IdaaS',  
+      author='authing',  #作者  
+      author_email='xieyang@dodora.cn', #作者邮箱  
+      url='https://github.com/Authing/authing-py-sdk', #作者链接  
+      packages=find_packages('./authing.py'),  
+      include_package_data=True,  
+      zip_safe=False,  
+      install_requires=[      #需求的第三方模块  
+        'requests',
+        'sgqlc'
+      ], 
+)  
