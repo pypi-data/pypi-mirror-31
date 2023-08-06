ba5b705f76eb0651f1e2af7de8fd3a0cf1a1122f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:07 +0000

    Add package pylf/1.1.4/pylf-1.1.4-py3-none-any.whl

---
+++ packages/pylf/pylf-1.1.4-py3-none-any.whl/pylf/__init__.py
@@ -0,0 +1,7 @@
+"""
+PyLf is a lightweight Python library for simulating Chinese handwriting. It introduces a great deal of randomness in the
+process of Chinese handwriting to simulate the uncertainty of glyphs written by human beings. Currently, PyLf is built
+on the top of Pillow library.
+"""
+from ._handwriting import *
+__version__ = '1.1.4'
