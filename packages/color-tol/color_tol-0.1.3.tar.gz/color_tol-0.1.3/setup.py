71bb7b89c83d9441f0354c90135eaeeb97bea9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:24 +0000

    Add package color-tol/0.1.3/color_tol-0.1.3.tar.gz

---
+++ packages/color-tol/color_tol-0.1.3.tar.gz/color_tol-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_descr = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_descr = open('README.md').read()
+
+setup(name='color_tol',
+      version='0.1.3',
+      description='Optimized color schemes for data display',
+      long_description=long_descr,
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Visualization',
+      ],
+      keywords='visualization seaborn matplotib',
+      url='https://github.com/lazarillo/color_tol',
+      author='Mike Williamson',
+      author_email='mwilliamson@pacific.edu',
+      license='MIT',
+      packages=['color_tol'],
+      test_suite='color_tol.tests',
+      include_package_data=True,
+      zip_safe=False)
