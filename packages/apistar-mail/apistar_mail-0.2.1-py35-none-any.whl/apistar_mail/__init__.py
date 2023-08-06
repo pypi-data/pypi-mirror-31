4f0f0e21b373ec5267c10430af14904f4b60e54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:35 +0000

    Add package apistar-mail/0.2.1/apistar_mail-0.2.1-py35-none-any.whl

---
+++ packages/apistar-mail/apistar_mail-0.2.1-py35-none-any.whl/apistar_mail/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for apistar-mail"""
+
+__author__ = """Drew Bednar"""
+__email__ = 'drew@androiddrew.com'
+__version__ = '0.2.1'
+
+from .mail import mail_component, Message, Mail  # NOQA: F401
