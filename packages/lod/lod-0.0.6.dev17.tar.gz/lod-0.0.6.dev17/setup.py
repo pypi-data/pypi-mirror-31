85e19b51c1c0279caeffe9624311ffe11559e73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:12 +0000

    Add package lod/0.0.6.dev17/lod-0.0.6.dev17.tar.gz

---
+++ packages/lod/lod-0.0.6.dev17.tar.gz/lod-0.0.6.dev17/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'lod',
+  packages = ['lod'],
+  version = '0.0.6.dev17',
+  description = 'A library containing basic code useful when creating Docker Images for LegionOfDevs.com',
+  long_description = 'A library containing basic code useful when creating Docker Images for LegionOfDevs.com',
+  author = 'Florian Dietz',
+  author_email = 'floriandietz44@gmail.com',
+  url='https://legionofdevs.com',
+  license = 'MIT',
+  package_data={
+      '': ['*.txt'], # this covers both the LICENSE.txt file in this folder, and the TRUTH.txt file in the /collab/ folder
+   },
+)
