b05ab494325fa684151478bd253c1a0bbd5b5955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:48 +0000

    Add package surface-dynamics/0.4.0/surface_dynamics-0.4.0.tar.gz

---
+++ packages/surface-dynamics/surface_dynamics-0.4.0.tar.gz/surface_dynamics-0.4.0/surface_dynamics/__init__.py
@@ -0,0 +1,13 @@
+r"""
+Flat surfaces
+"""
+
+from __future__ import absolute_import
+
+from .version import version as __version__
+
+from .flat_surfaces.all import *
+from .interval_exchanges.all import *
+from .misc.constellation import Constellation, Constellations
+
+del absolute_import
