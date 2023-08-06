a8abd63109c8719fea383f327961e53878cbfb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:35 +0000

    Add package datasssssplit/1.0.0/datasssssplit-1.0.0.tar.gz

---
+++ packages/datasssssplit/datasssssplit-1.0.0.tar.gz/datasssssplit-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name        = 'datasssssplit',
+      version     = '1.0.0',
+      py_modules  =['datasssssplit'],
+      author      ='veryshine',
+      author_email='263059108@qq.com',
+      url         ='http://url',
+      description ='data split by ":"',
+     )
