9b647bf5d90055a642571467afc9adfecd0c4eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package sealights-python-agent/0.2.147/sealights-python-agent-0.2.147.tar.gz

---
+++ packages/sealights-python-agent/sealights-python-agent-0.2.147.tar.gz/sealights-python-agent-0.2.147/python_agent/test_listener/utils.py
@@ -0,0 +1,27 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+def get_test_name_from_identifier(test_identifier):
+    if not test_identifier:
+        return ""
+    if "/" not in test_identifier:
+        return test_identifier
+    test_name_parts = test_identifier.split("/")[1:]
+    return "/".join(test_name_parts)
+
+
+def get_execution_id_from_identifier(test_identifier):
+    if not test_identifier:
+        return ""
+    return test_identifier.split("/")[0]
