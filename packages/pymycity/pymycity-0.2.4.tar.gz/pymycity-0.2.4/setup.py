aa1a91d4a79278ae149c2676712b8b31e4126677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package pymycity/0.2.4/pymycity-0.2.4.tar.gz

---
+++ packages/pymycity/pymycity-0.2.4.tar.gz/pymycity-0.2.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+  
+install_requires = list(val.strip() for val in open('requirements.txt'))
+tests_require = list(val.strip() for val in open('test_requirements.txt'))
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+long_desc = open('README.rst').read()
+
+setup(name='pymycity',
+      version='0.2.4',
+      description='Get your city live information',
+      long_description=long_desc,
+      author='Thibault Cohen',
+
+      author_email='titilambert@gmail.com',
+      url='http://github.com/titilambert/pymycity',
+      packages=PACKAGES,
+      entry_points={
+          'console_scripts': [
+              'pymycity = pymycity.__main__:main'
+          ]
+      },
+      install_requires=install_requires,
+      tests_require=tests_require,
+      classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+)
