0c27a48e7dea4db73d56ae018a36023ba15c57d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:36 +0000

    Add package kinemathic/0.2.1/kinemathic-0.2.1.tar.gz

---
+++ packages/kinemathic/kinemathic-0.2.1.tar.gz/kinemathic-0.2.1/kinemathic/animation.py
@@ -0,0 +1,172 @@
+from matplotlib.animation import FFMpegWriter
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+
+from .resample import between_rows, num_only
+
+
+def update_figure(df1, n, autoscale=False, ax=None, axis=0, grayed=None, j=0, kind=None, rx=None, ry=None,
+                  start=0, title=None):
+    """ update_figure.
+
+    Recalculates the whole plot for frame n.
+
+    :param df1: pandas dataframe, required
+    :param n: frame to render, required
+    :param autoscale: bool, if True will adjust the scale as data is processed, else will pre-render full scale
+    :param ax: matplotlib ax
+    :param axis: 0 or 1 for horizontal or vertical data
+    :param grayed: grayed out "background" data
+    :param j: this is used to calculate if we are dealing with an even or odd data series, for color selection.
+    :param kind: pandas plot kind
+    :param rx: remove x axis
+    :param ry: remove y axis
+    :param start: will be used in 0.3 for offset
+    :param title: optional, title for the plot
+    :return: affects ax
+    """
+    if j % 2:
+        c0 = 'C0'
+        c1 = 'C1'
+    else:
+        c0 = 'C1'
+        c1 = 'C0'
+    if kind is None:
+        kind = 'line'
+
+    # Just to quiet pep8 warning. 0.3 uses start, so might as well document it
+    if start == 0:
+        pass
+    items = df1.shape[0]
+    ax.cla()
+    if title is not None:
+        ax.set_title(title)
+
+    if grayed is not None:
+        for row in range(grayed.shape[0]-1):
+            grayed.iloc[row].plot(ax=ax, color='k', alpha=0.25)
+    if axis:
+        if not autoscale:
+            df1.iloc[items - 1:items, ].plot(ax=ax, color=c0)
+        df1.iloc[:n, ].plot(ax=ax, color=c0, kind=kind)
+    else:
+        df1.iloc[0].plot(ax=ax, color=c0, alpha=0.25, kind=kind)
+        df1.iloc[items - 1].plot(ax=ax, color=c1, alpha=0.25, kind=kind)
+        if n >= items / 2:
+            df1.iloc[n].plot(ax=ax, color=c1, kind=kind)
+        else:
+            df1.iloc[n].plot(ax=ax, color=c0, kind=kind)
+    if title is not None:
+        ax.legend().set_visible(False)
+        plt.box(on=None)
+
+    if rx:
+        ax.axes.get_xaxis().set_visible(False)
+    if ry:
+        ax.axes.get_yaxis().set_visible(False)
+
+
+def ianimate(df, autoscale=True, ax=None, axis=0, filename=None, fps=5, kind=None,  n=None, rx=None, ry=None,
+             step=1, title=None):
+    """ ianimate.
+
+    Animate plots, output to jupyter notebook.
+
+    :param df: pandas dataframe
+    :param autoscale: bool, if True will adjust the scale as data is processed, else will pre-render full scale
+    :param ax: matplotlib ax
+    :param axis: 0 or 1 for horizontal or vertical data
+    :param filename: file name to save the mp4 file to
+    :param fps: frames per second - defaults to 5
+    :param kind: pandas plot kind
+    :param n: frame to render
+    :param rx: remove x axis
+    :param ry: remove y axis
+    :param step: how many steps between frames
+    :param title: optional, title for the plot
+    :return: HTML video control widget for jupyter notebook
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_fig()
+
+    if filename is None:
+        filename = 'tmp.mp4'
+    if n is None:
+        n = df.shape[0]
+    moviewriter = FFMpegWriter(fps=fps)
+    with moviewriter.saving(fig, filename, dpi=100):
+        for j in range(1, n, step):
+            update_figure(df, n=j, ax=ax, axis=axis, autoscale=autoscale, kind=kind, rx=rx, ry=ry, title=title)
+            moviewriter.grab_frame()
+    return HTML(
+        """
+            <video width="640" height="480" controls loop>
+              <source src="{}" type="video/mp4">
+            </video>
+        """.format(filename)
+    )
+
+
+def animate(df, filename, autoscale=True, ax=None, axis=0, fps=5, kind=None, n=None, rx=None, ry=None,
+            step=1, title=None):
+    """ animate.
+
+    Animate plots and save to mp4 video.
+    :param df: pandas dataframe, required
+    :param filename: file name to save the mp4 file to, required
+    :param autoscale: bool, if True will adjust the scale as data is processed, else will pre-render full scale
+    :param ax: matplotlib ax
+    :param axis: 0 or 1 for horizontal or vertical data
+    :param fps: frames per second - defaults to 5
+    :param kind: pandas plot kind
+    :param n: frame to render
+    :param rx: remove x axis
+    :param ry: remove y axis
+    :param step: how many steps between frames
+    :param title: optional, title for the plot
+    :return: N/A
+    """
+    ianimate(df=df, filename=filename,
+             autoscale=autoscale, ax=ax, axis=axis, fps=fps, kind=kind,
+             n=n, rx=rx, ry=ry, step=step, title=title)
+
+
+def tweening(df, filename, autoscale=True, axis=0, fps=5, kind=None, rx=None, ry=None,
+             step=1, title=None, transitions=10):
+    """ tweening.
+
+    :param df: pandas dataframe, required
+    :param filename: file name to save the mp4 file to, required
+    :param autoscale: bool, if True will adjust the scale as data is processed, else will pre-render full scale
+    :param axis: 0 or 1 for horizontal or vertical data
+    :param fps: frames per second - defaults to 5
+    :param kind: pandas plot kind
+    :param rx: remove x axis
+    :param ry: remove y axis
+    :param step: how many steps between frames
+    :param title: optional, title for the plot
+    :param transitions: how many interimary values to generate
+    :return: HTML video control widget for jupyter notebook
+    """
+
+    fig, ax = plt.subplots()
+    nb_data_points = df.shape[0]
+    moviewriter = FFMpegWriter(fps=fps)
+    grayed = num_only(df)
+    with moviewriter.saving(fig, filename, dpi=100):
+        for j in range(0, nb_data_points-1, step):
+            dfj = df.iloc[j:]
+            df_expanded = between_rows(dfj, n=transitions)
+            for i in range(transitions):
+                update_figure(df_expanded, n=i, ax=ax, axis=axis, autoscale=autoscale, grayed=grayed, j=j,
+                              kind=kind, rx=rx, ry=ry, title=title)
+                moviewriter.grab_frame()
+    return HTML(
+        """
+            <video width="640" height="480" controls loop>
+              <source src="{}" type="video/mp4">
+            </video>
+        """.format(filename)
+    )
---
+++ packages/kinemathic/kinemathic-0.2.1.tar.gz/kinemathic-0.2.1/kinemathic/resample.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+
+def between_rows(df, n=10, skip_objects=True):
+    """ between_rows.
+
+    Create new rows between existing rows.
+
+    :param df: pandas dataframe
+    :param n:  number of rows to insert
+    :param skip_objects: Bool, if True, columns of type object will be skipped.
+    :return: transformed dataframe
+    """
+    transitions = n
+    new_df = []
+    new_cols = []
+    for col in df.columns:
+        col_data = []
+        if df.dtypes[col] in ('int64', 'float64'):
+            start = df[col].values[0]
+            end = df[col].values[1]
+            diff = end - start
+            increment = diff / transitions
+            col_data.append(start)
+            for i in range(transitions):
+                val = start + increment * i
+                col_data.append(val)
+            col_data.append(end)
+            new_df.append(col_data)
+            new_cols.append(col)
+        elif skip_objects:
+            pass
+        else:
+            val = df[col].values[0]
+            col_data.append(val)
+            for i in range(transitions):
+                between = '{}-{:2}'.format(val, i)
+                col_data.append(between)
+            val = df[col].values[1]
+            col_data.append(val)
+            new_df.append(col_data)
+            new_cols.append(col)
+    dft = pd.DataFrame(new_df)
+    new_cols_dict = {i: col for i, col in enumerate(new_cols)}
+    return dft.T.rename(columns=new_cols_dict)
+
+
+def num_only(df):
+    """ num_only.
+
+    Remove non numerical columns from a dataframe
+
+    :param df: pandas dataframe
+    :return: transformed dataframe
+    """
+    for col in df.columns:
+        if df.dtypes[col] in ('int64', 'float64'):
+            pass
+        else:
+            print("dropping", col)
+            df.drop(col, axis=1, inplace=True)
+    return df
