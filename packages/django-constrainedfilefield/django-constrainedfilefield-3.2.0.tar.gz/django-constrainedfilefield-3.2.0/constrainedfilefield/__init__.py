fe8ab48ddc88f78781ed4fc0f9f097df2f7f89b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:17 +0000

    Add package django-constrainedfilefield/3.2.0/django-constrainedfilefield-3.2.0.tar.gz

---
+++ packages/django-constrainedfilefield/django-constrainedfilefield-3.2.0.tar.gz/django-constrainedfilefield-3.2.0/constrainedfilefield/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = 'BSD-3-Clause'
+__author__ = 'Marc Bourqui'
+__version__ = '3.2.0'
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
