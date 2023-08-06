5fa1387baa6377c7393985c1848e0718680e98e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:26 +0000

    Add package eubh/0.0.43/eubh-0.0.43.tar.gz

---
+++ packages/eubh/eubh-0.0.43.tar.gz/eubh-0.0.43/src/eubh/config.py
@@ -0,0 +1,6 @@
+VERSION = '0.0.43'
+ROOT_DIR = '/root'
+HOST_ADDRESS = 'eubflow.eubchain.com'
+HTTP_ADDRESS = 'https://%s' % HOST_ADDRESS
+WS_ADDRESS = 'wss://%s:54188/' % HOST_ADDRESS
+ENABLE_VERBOSE = False
