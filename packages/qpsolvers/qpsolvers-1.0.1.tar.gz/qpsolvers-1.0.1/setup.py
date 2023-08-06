eecfd94dd4bccbe9d9050cd842da21f0066e6b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:18 +0000

    Add package qpsolvers/1.0.1/qpsolvers-1.0.1.tar.gz

---
+++ packages/qpsolvers/qpsolvers-1.0.1.tar.gz/qpsolvers-1.0.1/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2017 Stephane Caron <stephane.caron@normalesup.org>
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+classifiers = """\
+Development Status :: 4 - Beta
+License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+Topic :: Scientific/Engineering :: Mathematics
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+Operating System :: OS Independent"""
+
+i = 'https://raw.githubusercontent.com/stephane-caron/qpsolvers/master/.qp.png'
+
+long_description = """\
+This module provides a single function ``solve_qp(P, q, G, h, A, b, solver=X)``
+with a *solver* keyword argument to select the backend solver. The quadratic
+program it solves is, in standard form:
+
+    .. figure:: %s
+
+where vector inequalities are taken coordinate by coordinate.
+""" % i
+
+setup(
+    name='qpsolvers',
+    version='1.0.1',
+    description="Wrapper for Quadratic Programming solvers with a unified API",
+    long_description=long_description,
+    url="https://github.com/stephane-caron/qpsolvers",
+    author="Stéphane Caron",
+    author_email="stephane.caron@normalesup.org",
+    license="LGPL",
+    keywords="qp, quadratic programming, solver",
+    platforms="any",
+    classifiers=classifiers.split('\n'),
+    packages=['qpsolvers']
+)
