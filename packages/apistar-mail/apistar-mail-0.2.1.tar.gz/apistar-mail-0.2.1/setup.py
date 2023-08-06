631d3baf157127dcf41ef1bb80bb666a6dac1b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:35 +0000

    Add package apistar-mail/0.2.1/apistar-mail-0.2.1.tar.gz

---
+++ packages/apistar-mail/apistar-mail-0.2.1.tar.gz/apistar-mail-0.2.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'apistar~=0.3.9',
+]
+
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'tox'
+]
+
+setup(
+    name='apistar-mail',
+    version='0.2.1',
+    description="A simple email Component for APIStar",
+    long_description=readme + '\n\n' + history,
+    author="Drew Bednar",
+    author_email='drew@androiddrew.com',
+    url='https://github.com/androiddrew/apistar-mail',
+    packages=find_packages(include=['apistar_mail']),
+    include_package_data=True,
+    install_requires=requirements,
+    license='BSD',
+    keywords='apistar-mail apistar_mail mail SMTP',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    extras_require={
+        'testing': test_requirements,
+    }
+)
