d71d2f12bf256b95fe227c81b66423b88f8e4be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:34 +0000

    Add package gpuastro/0.1.dev0/gpuastro-0.1.dev0.tar.gz

---
+++ packages/gpuastro/gpuastro-0.1.dev0.tar.gz/gpuastro-0.1.dev0/setup.py
@@ -0,0 +1,15 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='gpuastro',
+    version='0.1dev',
+    packages=['gpuastro',],
+    license='GNU',
+    long_description=open('README.txt').read(),
+	description = 'A GPU accelerated astrophysics library',
+	author = 'Gill, S.',
+	author_email = 's.gill@keele.ac.uk',
+	url = 'https://github.com/samgill844', # use the URL to the github repo
+	keywords = ['GPU', 'numba', 'astrophysics'], # arbitrary keywords
+	classifiers = [])
