ade991bd60cfdd48d706bccd7afae9b7e96d20fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:54 +0000

    Add package data_scientist/0.13/data_scientist-0.13.tar.gz

---
+++ packages/data_scientist/data_scientist-0.13.tar.gz/data_scientist-0.13/data_scientist/Bugs.py
@@ -0,0 +1,25 @@
+from sys import exc_info as s_e
+from sys import argv as s_a
+import datetime as dt
+import csv
+runtime_log=[]
+runtime_log.append(['0 || START || '+str(dt.datetime.now())])
+exception_handling_index=1
+def successful_event():
+    runtime_log.append([str(exception_handling_index)+' || SUCCESS || '+ str(dt.datetime.now())])
+    globals()['exception_handling_index'] += 1
+def bug_event():
+    runtime_log.append([str(exception_handling_index)+' || BUG || '+ str(dt.datetime.now())
+                        +' || '+str(s_e())])
+    globals()['exception_handling_index'] += 1
+def final_event():
+    runtime_log.append([str(exception_handling_index)+' || END || '+str(dt.datetime.now())])
+    with open('/'.join(s_a[0].split('/')[:-1])
+              +'/LOG_runtime_'+(s_a[0].split('/')[-1]).split('.')[0]+'.csv',
+              'a',newline='') as output:
+        L = csv.writer(output)
+        L.writerow('\n')
+        L.writerow(['RUNTIME RECORD',str(dt.datetime.now())])
+        for log_item in runtime_log:
+            L.writerow(log_item)
+
