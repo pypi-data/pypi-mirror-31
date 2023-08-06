204baabb6a105b39401a791daa21d42bf81bdd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:39 +0000

    Add package baseq/0.1.0/baseq-0.1.0-py2.py3-none-any.whl

---
+++ packages/baseq/baseq-0.1.0-py2.py3-none-any.whl/basematic/bam/stats.py
@@ -0,0 +1,15 @@
+
+def length_distribution(path):
+    pass
+
+def bam_stats(path):
+    pass
+
+def bam_depth(path, interval):
+    pass
+
+def beIndexed(path):
+    pass
+
+def bam_read_counts(bam, chr, star, end):
+    pass
