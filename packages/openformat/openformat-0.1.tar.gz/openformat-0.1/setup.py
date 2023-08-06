d1b2d7541a57e32ee20fb857bbe4bd1d0d8de40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:24 +0000

    Add package openformat/0.1/openformat-0.1.tar.gz

---
+++ packages/openformat/openformat-0.1.tar.gz/openformat-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='openformat',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.3'
+    ],
+    version="0.1",
+    author="Till Hoffmann",
+)
