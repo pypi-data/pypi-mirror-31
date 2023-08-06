147e94511de29ce98dec52210320884b40da7ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:50 +0000

    Add package sxs/2018.4.26.22.22.14.dev140160931/sxs-2018.4.26.22.22.14.dev140160931-py2.py3-none-any.whl

---
+++ packages/sxs/sxs-2018.4.26.22.22.14.dev140160931-py2.py3-none-any.whl/sxs/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import division, print_function, absolute_import
+
+__doc_title__ = "SXS python code"
+__doc__ = "A collection of python code used by the SXS collaboration"
+
+from ._version import __version__
+
+import sxs.doxygen
+import sxs.metadata
+import sxs.references
+import sxs.zenodo
