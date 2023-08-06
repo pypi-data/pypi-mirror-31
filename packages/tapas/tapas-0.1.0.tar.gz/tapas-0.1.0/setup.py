181642d7aba2330c300ae2effb20418885cec761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:57 +0000

    Add package tapas/0.1.0/tapas-0.1.0.tar.gz

---
+++ packages/tapas/tapas-0.1.0.tar.gz/tapas-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='tapas',
+    version='0.1.0',
+    author='Kirill Sulim',
+    author_email='kirillsulim@gmail.com',
+    packages=[
+        'scarf'
+    ],
+)
