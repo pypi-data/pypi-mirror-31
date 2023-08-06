1121419da5ccdc0ad4ee7b1a07ba57fc807f34a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:01 +0000

    Add package booleantools/0.3.1a0/booleantools-0.3.1a0.tar.gz

---
+++ packages/booleantools/booleantools-0.3.1a0.tar.gz/booleantools-0.3.1a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="booleantools",
+      version="0.3.1a",
+      description="A library for boolean functions, functions on GF(4), and Cellular Automata",
+      url=None,
+      author="Wesley Rogers, Andrew Penland",
+      author_email="wsrogers3@catamount.wcu.edu",
+      license="MIT",
+      python_requires=">=3",
+      packages = ["booleantools"],
+      #py_modules=["booleantools"],
+      zip_safe=False)
