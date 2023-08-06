3348e76952065ea16919416c68b5d45c1632b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:34 +0000

    Add package psrqpy/0.4.12/psrqpy-0.4.12-py2.7.egg

---
+++ packages/psrqpy/psrqpy-0.4.12-py2.7.egg/psrqpy/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+""" A Python tool for interacting with the ATNF pulsar catalogue """
+
+__version__ = "0.4.12"
+
+from .search import QueryATNF
+from .pulsar import Pulsar, Pulsars
+from .utils import *
