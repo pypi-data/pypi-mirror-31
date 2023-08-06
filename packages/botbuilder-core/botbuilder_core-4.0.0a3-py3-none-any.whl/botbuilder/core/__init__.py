72f46bdea7fd9de1e4bd46dd39086ba29cdec098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:59 +0000

    Add package botbuilder-core/4.0.0a3/botbuilder_core-4.0.0a3-py3-none-any.whl

---
+++ packages/botbuilder-core/botbuilder_core-4.0.0a3-py3-none-any.whl/botbuilder/core/__init__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from .about import __version__
+
+from .bot_adapter import BotAdapter
+from .bot_framework_adapter import BotFrameworkAdapter, BotFrameworkAdapterSettings
+from .bot_context import BotContext
+from .middleware_set import AnonymousReceiveMiddleware, Middleware, MiddlewareSet
+
+__all__ = ['AnonymousReceiveMiddleware',
+           'BotAdapter',
+           'BotContext',
+           'BotFrameworkAdapter',
+           'BotFrameworkAdapterSettings',
+           'Middleware',
+           'MiddlewareSet',
+           '__version__',]
