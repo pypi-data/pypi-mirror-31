8bf13599b27736b93a82538c2805b6668e1f3634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:00 +0000

    Add package sphinx-bulma-theme/0.2.0/sphinx_bulma_theme-0.2.0.tar.gz

---
+++ packages/sphinx-bulma-theme/sphinx_bulma_theme-0.2.0.tar.gz/sphinx_bulma_theme-0.2.0/sphinx_bulma_theme/__init__.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+
+"""Sphinx Bulma theme.
+
+Based on https://github.com/rtfd/sphinx_rtd_theme
+
+"""
+from docutils.nodes import Admonition
+from docutils.nodes import attention
+from docutils.nodes import caution
+from docutils.nodes import danger
+from docutils.nodes import error
+from docutils.nodes import hint
+from docutils.nodes import important
+from docutils.nodes import note
+from docutils.nodes import tip
+from docutils.nodes import title
+from docutils.nodes import warning
+from sphinx.addnodes import seealso
+# from docutils.nodes import Element  # noqa
+# from docutils.nodes import compound  # noqa
+# from docutils.nodes import section  # noqa
+# from sphinx.addnodes import centered  # noqa
+# from sphinx.addnodes import toctree  # noqa
+
+from six import string_types
+from .fs import module_path
+from .version import version
+
+admonition_map = {
+    seealso: 'is-success',
+    tip: 'is-primary',
+    note: 'is-dark',
+    warning: 'is-danger',
+    attention: 'is-warning',
+    caution: 'is-warning',
+    danger: 'is-danger',
+    error: 'is-danger',
+    hint: 'is-info',
+    important: 'is-link',
+}
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    return str(module_path)
+
+
+def add_classes_to_node(class_names, node):
+    if isinstance(class_names, string_types):
+        class_names = class_names.split()
+
+    return list(map(node.set_class, class_names))
+
+
+def process_admonition_node(node, admonition_type, color, size):
+    add_classes_to_node(['message', size, color], node)
+
+    for child in node.traverse(title):
+        add_classes_to_node('message-header', child)
+
+    for child in node.traverse(admonition_type):
+        add_classes_to_node('message-body', child)
+        break
+
+
+def bulmanize_admonition_nodes(app, doctree, fromdocname):
+    theme_options = app.config.html_theme_options
+    message_size = theme_options.get('admonition_class', '')
+
+    # size = app.env.conf.get('')
+    # nodes = list(filter(lambda n: 'also' in str(n).lower(), doctree.traverse(Element)))
+    # for node in nodes:
+    #     print(node)
+    #     import ipdb; ipdb.set_trace()
+
+    #     atts = dict(node.attlist())
+    #     names = " ".join(atts.get('names') or [])
+    #     for ad_type, color in admonition_map.items():
+    #         ad_name = ad_type.__name__
+    #         if ad_name in names:
+    #             process_admonition_node(node, ad_type, color)
+
+    for node in doctree.traverse(Admonition):
+        message_color = admonition_map.get(node.__class__, 'is-dark')
+        process_admonition_node(node, node.__class__, message_color, message_size)
+
+
+def handle_page_context(app, pagename, templatename, context, doctree):
+    pass
+
+
+def setup(app):
+    app.add_html_theme('bulma', get_html_theme_path())
+    app.connect('doctree-resolved', bulmanize_admonition_nodes)
+    app.connect('html-page-context', handle_page_context)
+
+
+__all__ = (
+    'get_html_theme_path',
+    'version',
+    'setup',
+    'admonition_map',
+)
